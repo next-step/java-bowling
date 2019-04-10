1b9758f40dfe7f0b4d71a38f1c117cbadb284a49
hyoj <chohyojoy@gmail.com>
2019-04-10 15:24:31 +0900

    refactor: 최대 투구 가능 횟수 상수화

--- src/main/java/com/hyoj/bowling/domain/Frame.java
+++ src/main/java/com/hyoj/bowling/domain/Frame.java
@@ -1,17 +1,20 @@
 package com.hyoj.bowling.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static java.util.stream.Collectors.joining;
 
 public class Frame {
+    public static final int MAX_SHOTS_COUNT = 2;
+
     private final List<Shot> shots = new ArrayList<>();
 
     public boolean add(int knockDownPinsCount) {
         final int shotTimes = shots.size();
 
-        if (shotTimes > 2) {
+        if (shotTimes > MAX_SHOTS_COUNT) {
             throw new IllegalArgumentException("유효하지 않은 투구 횟수");
         }
 
@@ -29,7 +32,15 @@ public class Frame {
 
     public boolean isDone() {
         final int shotsCount = shots.size();
-        if (shotsCount == 2 || shots.get(shotsCount - 1).isAllDown()) {
+        if (shotsCount == MAX_SHOTS_COUNT || shots.get(shotsCount - 1).isAllDown()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean canOneMoreShot() {
+        if (isDone() && (Arrays.asList(MarkType.SPARE, MarkType.STRIKE).contains(getFinalMarkType()))) {
             return true;
         }
 
