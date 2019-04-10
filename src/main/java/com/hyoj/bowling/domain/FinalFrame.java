d9d0f0eed87fd8bce021b782998cef33026fe8c8
hyoj <chohyojoy@gmail.com>
2019-04-10 15:27:31 +0900

    feat: 마지막 프레임용 클래스 추가

---
+++ src/main/java/com/hyoj/bowling/domain/FinalFrame.java
@@ -0,0 +1,12 @@
+package com.hyoj.bowling.domain;
+
+public class FinalFrame {
+    private final Frame frame;
+    private final Shot shot;
+
+    public FinalFrame(final Frame frame, final Shot shot) {
+        this.frame = frame;
+        this.shot = shot;
+
+    }
+}
