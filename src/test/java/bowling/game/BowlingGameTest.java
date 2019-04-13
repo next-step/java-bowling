f49399b29278aa17afd913d138c0fe61bfccde14
devson <sonyc5720@gmail.com>
2019-04-13 09:34:49 +0900

    볼링게임 기본 로직 확인 테스트코드 작성

--- src/test/java/bowling/game/BowlingGameTest.java
+++ src/test/java/bowling/game/BowlingGameTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class BowlingGameTest {
     private static final Logger log = LoggerFactory.getLogger(BowlingGameTest.class);
 
--- src/test/java/bowling/game/FinalFrameTest.java
+++ src/test/java/bowling/game/FinalFrameTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 
 public class FinalFrameTest {
