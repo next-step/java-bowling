d92b8e7849bf26a2c249a07fe51b7cb8180a9f24
daioooor36 <pd6016@naver.com>
2019-07-06 12:29:06 +0900

    test(NormalFrame) : 중복되는 테스트코드 제거

--- src/test/java/domain/NormalFrameTest.java
+++ src/test/java/domain/NormalFrameTest.java
@@ -3,7 +3,6 @@ package domain;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 
 public class NormalFrameTest {
     private final int FIRST_BALL = 3;
@@ -11,29 +10,6 @@ public class NormalFrameTest {
     private final int STRIKE = 10;
     private final int SPARE = 7;
     private final int GUTTER = 0;
-
-    @Test
-    void 한번_던짐_3점() {
-        NormalFrame normalFrame = new NormalFrame();
-        assertThat(normalFrame.doBowling(FIRST_BALL)).isEqualTo(FIRST_BALL);
-    }
-
-    @Test
-    void 두번_던짐_총합_7점() {
-        NormalFrame normalFrame = new NormalFrame();
-        normalFrame.doBowling(FIRST_BALL);
-        assertThat(normalFrame.doBowling(SECOND_BALL)).isEqualTo(FIRST_BALL + SECOND_BALL);
-    }
-
-    @Test
-    void 초구10점_두번째_또_던짐() {
-        NormalFrame normalFrame = new NormalFrame();
-        normalFrame.doBowling(STRIKE);
-
-        assertThatIllegalStateException().isThrownBy(() -> {
-            normalFrame.doBowling(SECOND_BALL);
-        });
-    }
 
     @Test
     void 스트라이크_결과_출력() {
@@ -49,7 +25,7 @@ public class NormalFrameTest {
         normalFrame.doBowling(FIRST_BALL);
         normalFrame.doBowling(SPARE);
 
-        assertThat(normalFrame.getScore()).isEqualTo(FIRST_BALL+"|/");
+        assertThat(normalFrame.getScore()).isEqualTo(FIRST_BALL + "|/");
     }
 
     @Test
@@ -58,7 +34,7 @@ public class NormalFrameTest {
         normalFrame.doBowling(FIRST_BALL);
         normalFrame.doBowling(SECOND_BALL);
 
-        assertThat(normalFrame.getScore()).isEqualTo(FIRST_BALL+"|"+SECOND_BALL);
+        assertThat(normalFrame.getScore()).isEqualTo(FIRST_BALL + "|" + SECOND_BALL);
     }
 
     @Test
@@ -67,7 +43,7 @@ public class NormalFrameTest {
         normalFrame.doBowling(FIRST_BALL);
         normalFrame.doBowling(GUTTER);
 
-        assertThat(normalFrame.getScore()).isEqualTo(FIRST_BALL+"|"+"-");
+        assertThat(normalFrame.getScore()).isEqualTo(FIRST_BALL + "|" + "-");
     }
 
 }
