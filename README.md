dc146d411d8bfc388e101f65d4d4a0a7c7dd8f89
Juyoung <juy5790@outlook.com>
2019-07-09 14:25:55 +0900

    feat #step1 pins 구현 및 리팩토링

--- README.md
+++ README.md
@@ -7,3 +7,70 @@
 
 ## 온라인 코드 리뷰 과정
 * [텍스트와 이미지로 살펴보는 온라인 코드 리뷰 과정](https://github.com/next-step/nextstep-docs/tree/master/codereview)
+
+
+- 볼링을 친다 -> 1프레임을 치겠지 
+           -> frame 당 1~3번 칠수있다.
+           -> frame 1~2
+           -> 마지막만 3번 가능
+             
+             
+             
+             
+normalFrame
+bowl(in: downPins, out: frame)
+    frame.isFinish(out: boolean) // 현 프레임을 완료하는 지?
+        frame.next(out: nextFrame) createFrame
+getState(in: index, out: state)
+
+finalFrame
+bowl(in: downPins, out: frame)
+    frame.isFinish(out: boolean)
+        frame.next(out: X)  
+        
+        
+Frame 
+ -> bowl(in: pins, out: frame)
+ -> getState(out: frameState)
+ -> isFinish(out: boolean)
+ 
+normal state
+ -> 1 round (gutter, hit, strike) : frame 생성할 때 같이 준다면?
+ ---
+ -> 2 round (miss, spare)
+1~2번
+
+final state
+ -> 1 round (gutter, hit, strike) 
+ ---
+ -> 2 round (miss, spare, strike) 
+ 
+ -> 3 round (miss, spare, strike)
+ 
+2~3번
+1round : none
+
+2round : FinalState(in: first, second) 
+         // isFinish : T,F (first + second < 10 ) 
+3round :
+
+strike  / strike / strike
+H       /strike  / strike
+H       /spare   / strike
+strike / hit     /  
+-------------------
+= first + second < 10 (한번더)
+= second + third < 10 (stop)
+first + second < 10 
+first : 10 strike
+else
+spare
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/bowling/model/Pins.java
@@ -0,0 +1,50 @@
+package bowling.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Pins {
+
+    private static final Map<Integer, Pins> CACHE = new HashMap<>();
+
+    static final int MIN = 0;
+    static final int MAX = 10;
+
+    static final Pins DOWN_ALL = Pins.valueOf(MAX);
+    static final Pins DOWN_ZERO = Pins.valueOf(MIN);
+
+    private int countOfDownPins;
+
+    private Pins(int countOfDownPins) {
+        this.countOfDownPins = countOfDownPins;
+    }
+
+    static Pins valueOf(int countOfDownPins) {
+        if (MIN > countOfDownPins || MAX < countOfDownPins) {
+            throw new IllegalArgumentException(String.format("핀은 %d~%d개를 쓰러트릴 수 있습니다.", MIN, MAX));
+        }
+        CACHE.computeIfAbsent(countOfDownPins, Pins::new);
+        return CACHE.get(countOfDownPins);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pins pins = (Pins) o;
+        return countOfDownPins == pins.countOfDownPins;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(countOfDownPins);
+    }
+
+    @Override
+    public String toString() {
+        return "Pins{" +
+                "countOfDownPins=" + countOfDownPins +
+                '}';
+    }
+}
