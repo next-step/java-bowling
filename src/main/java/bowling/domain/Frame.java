ed8f1c9cdf1bf6390dc06b4b4cf68033e754ff69
권병윤 <sksggg123@gmail.com>
2019-07-10 22:54:22 +0900

    [feat] : Frame 객체 추가

--- src/main/java/bowling/domain/Frame.java
+++ src/main/java/bowling/domain/Frame.java
@@ -1,5 +1,7 @@
 package bowling.domain;
 
+import java.util.List;
+
 /**
  * author       : gwonbyeong-yun <sksggg123>
  * ------------------------------------------
@@ -10,5 +12,58 @@ package bowling.domain;
  * project      : java-bowling
  * create date  : 2019-07-10 16:25
  */
-public class Frame {
+class Frame {
+    private static int AUTO_INCREASE = 0;
+    private static int FRAME_MAX_NUMBER = 10;
+    private FrameScore frameScore;
+    private int index;
+
+    public Frame() {
+        if (AUTO_INCREASE > FRAME_MAX_NUMBER) {
+            throw new IllegalArgumentException("10번의 경기가 종료되었습니다.");
+        }
+        this.index = AUTO_INCREASE++;
+        this.frameScore = new FrameScore();
+    }
+
+    private Frame(int fallCount) {
+        if (AUTO_INCREASE >= FRAME_MAX_NUMBER) {
+            throw new IllegalArgumentException("10번의 경기가 종료되었습니다.");
+        }
+        FrameScore scores = new FrameScore(fallCount);
+        this.index = AUTO_INCREASE++;
+        this.frameScore = scores;
+    }
+
+    public static Frame of(int fallCount) {
+        return new Frame(fallCount);
+    }
+
+    boolean bowl(int fallCount) {
+        boolean isBowl = frameScore.addScore(fallCount);
+        if (isBowl) {
+            return true;
+        }
+        return false;
+    }
+    int frameNumber() {
+        return index;
+    }
+
+    int getRecentScore() {
+        return frameScore.searchRecentScore();
+    }
+
+    boolean isStrike() {
+        return frameScore.isStrike();
+    }
+
+    FrameScore nowFrameScores() {
+        return new FrameScore(frameScore.getScores());
+    }
+
+    @Override
+    public String toString() {
+        return "Frame{" + frameScore + '}';
+    }
 }
--- src/test/java/bowling/domain/FrameTest.java
+++ src/test/java/bowling/domain/FrameTest.java
@@ -1,5 +1,10 @@
 package bowling.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * author       : gwonbyeong-yun <sksggg123>
  * ------------------------------------------
@@ -11,4 +16,20 @@ package bowling.domain;
  * create date  : 2019-07-10 16:20
  */
 public class FrameTest {
+    @DisplayName("투구를 했을때 가능한지 확인, 2번이미 투구한사람은 새로운 Frame생성 알림을 위해 false로 반환")
+    @Test
+    void bowlCount() {
+        Frame frame = new Frame();
+        frame.bowl(3);
+        frame.bowl(3);
+        assertThat(frame.bowl(3)).isFalse();
+    }
+
+    @DisplayName("투구를 했을때 가능한지 확인, 첫 투구가 Strike일 경우 false반환")
+    @Test
+    void bowlable() {
+        Frame frame = new Frame();
+        frame.bowl(10);
+        assertThat(frame.bowl(1)).isFalse();
+    }
 }
