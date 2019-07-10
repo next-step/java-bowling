9f29b8221317f17c56a0eb8b0d5dce46d0c87ace
권병윤 <sksggg123@gmail.com>
2019-07-10 23:01:25 +0900

    [feat] : Bowl enum 추가

--- src/test/java/bowling/domain/BowlTest.java
+++ src/test/java/bowling/domain/BowlTest.java
@@ -1,5 +1,9 @@
 package bowling.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -13,5 +17,31 @@ import static org.junit.jupiter.api.Assertions.*;
  * create date  : 2019-07-10 19:10
  */
 class BowlTest {
+    @DisplayName("스트라이크 enum 반환")
+    @Test
+    void strikeEnum() {
+        Bowl bowl = Bowl.check(10, true);
+        assertThat(bowl.getScoreDisplay()).isEqualTo("X");
+    }
 
+    @DisplayName("스패어 enum 반환")
+    @Test
+    void spareEnum() {
+        Bowl bowl = Bowl.check(10, false);
+        assertThat(bowl.getScoreDisplay()).isEqualTo("/");
+    }
+
+    @DisplayName("거터 enum 반환")
+    @Test
+    void gutterEnum() {
+        Bowl bowl = Bowl.check(0, false);
+        assertThat(bowl.getScoreDisplay()).isEqualTo("-");
+    }
+
+    @DisplayName("미스 enum 반환")
+    @Test
+    void missEnum() {
+        Bowl bowl = Bowl.check(7, false);
+        assertThat(bowl.getScoreDisplay()).isEqualTo("7");
+    }
 }
