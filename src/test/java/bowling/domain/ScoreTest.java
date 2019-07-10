40e773de502a206c93ff9c94173f6eb6c3c260e8
권병윤 <sksggg123@gmail.com>
2019-07-10 23:02:54 +0900

    [style] : 사용하지 않는 import 삭제

--- src/test/java/bowling/domain/ScoreTest.java
+++ src/test/java/bowling/domain/ScoreTest.java
@@ -3,7 +3,6 @@ package bowling.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 /**
