78cfdc796e3a0f043b01cdc4680fd644df00b409
권병윤 <sksggg123@gmail.com>
2019-07-10 22:54:42 +0900

    [feat] : GameBoard 객체 추가

--- src/main/java/bowling/domain/GameBoard.java
+++ src/main/java/bowling/domain/GameBoard.java
@@ -1,5 +1,10 @@
 package bowling.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  * author       : gwonbyeong-yun <sksggg123>
  * ------------------------------------------
@@ -11,4 +16,44 @@ package bowling.domain;
  * create date  : 2019-07-10 17:15
  */
 public class GameBoard {
+    private static final int LAST_INDEX = 1;
+    private List<Frame> frames;
+
+    GameBoard() {
+        this.frames = new ArrayList<>();
+    }
+
+    int playCount() {
+        return frames.size();
+    }
+
+    Map<Integer, FrameScore> play(int fallCount) {
+        if (isEmpty()) {
+            return nextFrame(fallCount);
+        }
+        return nowFrame(fallCount);
+    }
+
+    private Map<Integer, FrameScore> nowFrame(int fallCount) {
+        boolean bowlSuccess = frames.get(getLastIndex()).bowl(fallCount);
+        if (!bowlSuccess) {
+            nextFrame(fallCount);
+        }
+        return frames.stream()
+                .collect(Collectors.toMap(Frame::frameNumber, Frame::nowFrameScores));
+    }
+
+    private Map<Integer, FrameScore> nextFrame(int fallCount) {
+        frames.add(Frame.of(fallCount));
+        return frames.stream()
+                .collect(Collectors.toMap(Frame::frameNumber, Frame::nowFrameScores));
+    }
+
+    private boolean isEmpty() {
+        return frames.isEmpty();
+    }
+
+    private int getLastIndex() {
+        return frames.size() - LAST_INDEX;
+    }
 }
--- src/test/java/bowling/domain/GameBoardTest.java
+++ src/test/java/bowling/domain/GameBoardTest.java
@@ -1,5 +1,13 @@
 package bowling.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * author       : gwonbyeong-yun <sksggg123>
  * ------------------------------------------
@@ -11,4 +19,24 @@ package bowling.domain;
  * create date  : 2019-07-10 17:11
  */
 public class GameBoardTest {
+    @DisplayName("1~9의 점수 2개가 들어간뒤 투구를 한번 더했을때 List의 사이즈가 증가 (새로운 Frame)")
+    @Test
+    void play_new_frame() {
+        GameBoard gameBoard = new GameBoard();
+        gameBoard.play(5);
+        gameBoard.play(5);
+        gameBoard.play(5);
+        assertThat(gameBoard.playCount()).isEqualTo(2);
+    }
+
+    @DisplayName("1~9의 점수 2개가 들어간뒤 투구를 한번 더했을때 List의 사이즈가 증가 (새로운 Frame)")
+    @Test
+    void play_snapshot() {
+        GameBoard gameBoard = new GameBoard();
+        gameBoard.play(1);
+        gameBoard.play(2);
+        Map<Integer, FrameScore> snapShot = new HashMap<>(gameBoard.play(3));
+
+        assertThat(snapShot.get(1).getScores().get(0)).isEqualTo(Score.of(3));
+    }
 }
