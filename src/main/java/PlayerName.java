c91ccdde00d25ba361afba38b48e16e428d8da2b
1nteger <rhep0820@gmail.com>
2019-07-11 00:47:37 +0900

    [step1] feat : 플레이어 이름 유효성 체크

--- src/main/java/PlayerName.java
+++ src/main/java/PlayerName.java
@@ -1,13 +1,22 @@
 public class PlayerName {
+    private static final String REGEX_FOR_PLAYER_NAME = "^[a-zA-Z]{3}$";
+    static final String ALERT_INVALID_PLAYER_NAME = "플레이어의 이름은 세 자리의 영문만 가능합니다.";
 
     private final String name;
 
     private PlayerName(String inputName) {
+        validationPlayerName(inputName);
         this.name = inputName;
     }
 
     public static PlayerName from(String inputName) {
         return new PlayerName(inputName);
+    }
+
+    private void validationPlayerName(String inputName) {
+        if (!inputName.matches(REGEX_FOR_PLAYER_NAME)) {
+            throw new IllegalArgumentException(ALERT_INVALID_PLAYER_NAME);
+        }
     }
 
     public String getName() {
--- src/test/java/PlayerNameTest.java
+++ src/test/java/PlayerNameTest.java
@@ -1,13 +1,54 @@
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class PlayerNameTest {
     @Test
     void 플레이어의_이름을_입력받아서_생성한다() {
-        String name = "tester";
+        String name = "HJS";
         PlayerName playerName = PlayerName.from(name);
 
         assertThat(playerName.getName()).isEqualTo(name);
+    }
+
+    @Test
+    void 플레이어의_이름이_세_자리_이상이면_예외가_발생한다() {
+        String name = "test";
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    PlayerName.from(name);
+                }).withMessage(PlayerName.ALERT_INVALID_PLAYER_NAME);
+    }
+
+    @Test
+    void 플레이어의_이름이_세_자리_미만이면_예외가_발생한다() {
+        String name = "HJ";
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    PlayerName.from(name);
+                }).withMessage(PlayerName.ALERT_INVALID_PLAYER_NAME);
+    }
+
+    @Test
+    void 플레이어의_이름에_한글이_포함되면_예외가_발생한다() {
+        String name = "한JS";
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    PlayerName.from(name);
+                }).withMessage(PlayerName.ALERT_INVALID_PLAYER_NAME);
+    }
+
+    @Test
+    void 플레이어의_이름에_숫자가_포함되면_예외가_발생한다() {
+        String name = "1nt";
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    PlayerName.from(name);
+                }).withMessage(PlayerName.ALERT_INVALID_PLAYER_NAME);
     }
 }
