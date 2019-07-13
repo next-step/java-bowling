604c0c2cf5719402584eca26154a0ae2d3fc16d0
권병윤 <sksggg123@gmail.com>
2019-07-13 17:31:20 +0900

    [docs] : Frame 도메인 생성 추가

---
+++ src/test/java/bowling/domain/FrameTest.java
@@ -0,0 +1,23 @@
+package bowling.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * author       : gwonbyeong-yun <sksggg123>
+ * ------------------------------------------
+ * | email        : sksggg123               |
+ * | github       : github.com/sksggg123    |
+ * | blog         : sksggg123.github.io     |
+ * ------------------------------------------
+ * project      : java-bowling
+ * create date  : 2019-07-13 15:10
+ */
+public class NormalFrameTest {
+    @DisplayName("현재 투구가 가능한지 확인 - Strike로 인해 불가")
+    @Test
+    void do_not_bowl_by_strike() {
+        NormalFrame normal = new NormalFrame();
+        assertThat(normal.)
+    }
+}
