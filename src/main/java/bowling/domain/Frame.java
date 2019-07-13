06d1830ea2a59d3092b2c10cb1441ce856091905
권병윤 <sksggg123@gmail.com>
2019-07-13 17:31:39 +0900

    [feat] : Frame 도메인 기능 추가

--- src/main/java/bowling/domain/Frame.java
+++ src/main/java/bowling/domain/Frame.java
@@ -1,5 +1,8 @@
 package bowling.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * author       : gwonbyeong-yun <sksggg123>
  * ------------------------------------------
@@ -11,4 +14,32 @@ package bowling.domain;
  * create date  : 2019-07-13 17:16
  */
 public class Frame {
+    private static int AUTO_INCREASE = 1;
+
+    private List<Pin> pins;
+    private final int number;
+
+    public Frame() {
+        this.pins = new ArrayList<>();
+        this.number = AUTO_INCREASE++;
+    }
+
+    public int bowl(int downCount) {
+        pins.add(Pin.fallDown(downCount));
+        return sum();
+    }
+
+    public int countBowl() {
+        return pins.size();
+    }
+
+    public int sum() {
+        return pins.stream()
+                .mapToInt(Pin::fallCount)
+                .sum();
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
--- src/test/java/bowling/domain/FrameTest.java
+++ src/test/java/bowling/domain/FrameTest.java
@@ -3,6 +3,8 @@ package bowling.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * author       : gwonbyeong-yun <sksggg123>
  * ------------------------------------------
@@ -13,11 +15,32 @@ import org.junit.jupiter.api.Test;
  * project      : java-bowling
  * create date  : 2019-07-13 15:10
  */
-public class NormalFrameTest {
-    @DisplayName("현재 투구가 가능한지 확인 - Strike로 인해 불가")
+public class FrameTest {
+    @DisplayName("현재 스코어의 합을 가지고 온다.")
     @Test
-    void do_not_bowl_by_strike() {
-        NormalFrame normal = new NormalFrame();
-        assertThat(normal.)
+    void sum() {
+        Frame frame = new Frame();
+        frame.bowl(4);
+        assertThat(frame.bowl(5)).isEqualTo(9);
+    }
+
+    @DisplayName("현재 스코어의 합을 가지고 온다.")
+    @Test
+    void getNumber() {
+        Frame frame1 = new Frame();
+        Frame frame2 = new Frame();
+        Frame frame3 = new Frame();
+
+        assertThat(frame3.getNumber()).isEqualTo(3);
+    }
+
+    @DisplayName("몇번 투구했는지 가지고 온다.")
+    @Test
+    void bowlCount() {
+        Frame frame = new Frame();
+        frame.bowl(3);
+        frame.bowl(5);
+
+        assertThat(frame.countBowl()).isEqualTo(2);
     }
 }
