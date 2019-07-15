d98f948a597474d4353e31aa9fe443a1764228f9
Jaeyeonling <jaeyeonling@gmail.com>
2019-07-15 19:36:38 +0900

    [feat] frames 추가

---
+++ src/main/java/com/jaeyeonling/bowling/domain/frame/Frames.java
@@ -0,0 +1,69 @@
+package com.jaeyeonling.bowling.domain.frame;
+
+import com.jaeyeonling.bowling.domain.pins.KnockdownPins;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Frames {
+
+    private static final int START_INDEX = 0;
+    private static final int NORMAL_FRAME_COUNT = 9;
+
+    private final List<Frame> frames = new ArrayList<>();
+    private int currentIndex;
+
+    public Frames() {
+        Frame prevFrame = initializeFinalFrame();
+        for (int i = START_INDEX; i < NORMAL_FRAME_COUNT; i++) {
+            prevFrame = initializeNormalFrame(prevFrame);
+        }
+
+        Collections.reverse(frames);
+    }
+
+    public void bowl(final KnockdownPins knockdownPins) {
+        final Frame currentFrame = getCurrentFrame();
+
+        currentFrame.bowl(knockdownPins);
+
+        if (currentFrame.isFinished()) {
+            incrementCurrentIndex();
+        }
+    }
+
+    public boolean isFinished() {
+        return currentIndex >= NORMAL_FRAME_COUNT && getCurrentFrame().isFinished();
+    }
+
+    public int getCurrentFrameIndex() {
+        return currentIndex + 1;
+    }
+
+    public List<Frame> getFrames() {
+        return frames;
+    }
+
+    private Frame getCurrentFrame() {
+        return frames.get(Math.min(currentIndex, NORMAL_FRAME_COUNT));
+    }
+
+    private Frame initializeFinalFrame() {
+        final Frame finalFrame = new FinalFrame();
+        frames.add(finalFrame);
+
+        return finalFrame;
+    }
+
+    private Frame initializeNormalFrame(Frame prevFrame) {
+        prevFrame = new NormalFrame(prevFrame);
+        frames.add(prevFrame);
+
+        return prevFrame;
+    }
+
+    private void incrementCurrentIndex() {
+        currentIndex++;
+    }
+}
---
+++ src/test/java/com/jaeyeonling/bowling/domain/frame/FramesTest.java
@@ -0,0 +1,67 @@
+package com.jaeyeonling.bowling.domain.frame;
+
+import com.jaeyeonling.bowling.domain.frame.state.FinishedFrameStateException;
+import com.jaeyeonling.bowling.domain.pins.KnockdownPins;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class FramesTest {
+
+    private Frames frames;
+
+    @BeforeEach
+    void setUp() {
+        frames = new Frames();
+    }
+
+    @DisplayName("프레임들의 첫 프레임 값은 1이다.")
+    @Test
+    void firstFrameIndex() {
+        final int currentFrameIndex = frames.getCurrentFrameIndex();
+
+        assertThat(currentFrameIndex).isEqualTo(1);
+    }
+
+    @DisplayName("스트라이크 1번 당 프레임이 1씩 증가한다.")
+    @Test
+    void strikeFrameIndex() {
+        for (int i = 1; i <= 10; i++) {
+            final int currentFrameIndex = frames.getCurrentFrameIndex();
+            frames.bowl(KnockdownPins.MAX);
+
+            assertThat(currentFrameIndex).isEqualTo(i);
+        }
+    }
+
+    @DisplayName("스트라이크를 제외하면 2번 당 프레임이 1씩 증가한다.")
+    @Test
+    void otherFrameIndex() {
+        for (int i = 1; i <= 10; i++) {
+            final int currentFrameIndex = frames.getCurrentFrameIndex();
+
+            frames.bowl(KnockdownPins.MIN);
+            frames.bowl(KnockdownPins.MIN);
+
+            assertThat(currentFrameIndex).isEqualTo(i);
+        }
+    }
+
+
+    @DisplayName("모든 프레임이 끝난 후 게임 시 예외처리한다.")
+    @Test
+    void throwFinishedFrameStateException() {
+        // given
+        for (int i = 1; i <= 10; i++) {
+            frames.bowl(KnockdownPins.MIN);
+            frames.bowl(KnockdownPins.MIN);
+        }
+
+        // when / then
+        assertThatExceptionOfType(FinishedFrameStateException.class)
+                .isThrownBy(() -> frames.bowl(KnockdownPins.MIN));
+    }
+}
