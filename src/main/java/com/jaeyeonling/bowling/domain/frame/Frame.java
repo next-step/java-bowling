fe4635f9acaa393227c3b407caaa88802d78dc3e
Jaeyeonling <jaeyeonling@gmail.com>
2019-07-15 15:58:20 +0900

    [feat] 기존 소스 제거

--- src/main/java/com/jaeyeonling/bowling/domain/frame/Frame.java
+++
@@ -1,9 +0,0 @@
-package com.jaeyeonling.bowling.domain.frame;
-
-public interface Frame {
-
-    String getFrameState();
-    Frame bowl(final KnockdownPins knockdownPins);
-    boolean isFinish();
-    FrameIndex getFrameIndex();
-}
--- src/main/java/com/jaeyeonling/bowling/domain/frame/state/HaveBonus.java
+++
@@ -1,7 +0,0 @@
-package com.jaeyeonling.bowling.domain.frame.state;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-@interface HaveBonus { }
