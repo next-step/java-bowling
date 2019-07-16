0cb4beb546e6ccf989c0ca01bca952a762c0006b
권병윤 <sksggg123@gmail.com>
2019-07-17 00:47:51 +0900

    [feat] : FrameNumber 객체 생성 및 Frame종료 조건 설정

---
+++ src/main/java/bowling/domain/FrameNumber.java
@@ -0,0 +1,42 @@
+package bowling.domain;
+
+/**
+ * author       : gwonbyeong-yun <sksggg123>
+ * ------------------------------------------
+ * | email        : sksggg123               |
+ * | github       : github.com/sksggg123    |
+ * | blog         : sksggg123.github.io     |
+ * ------------------------------------------
+ * project      : java-bowling
+ * create date  : 2019-07-17 00:36
+ */
+public class FrameNumber {
+    public static final int INIT_FRAME_NUMBER = 1;
+    public static final int NEXT_FRAME_NUMBER = 1;
+    public static final int LAST_FRAME_NUMBER = 10;
+    public static final int NORMAL_FRAME_MAX_NUMBER = 9;
+    private final int frameNumber;
+
+    private FrameNumber(int frameNumber) {
+        this.frameNumber = frameNumber;
+    }
+
+    public static FrameNumber init() {
+        return new FrameNumber(INIT_FRAME_NUMBER);
+    }
+
+    public FrameNumber next() {
+        if (frameNumber >= LAST_FRAME_NUMBER) {
+            throw new IllegalStateException("10프레임까지만 게임할 수 있습니다.");
+        }
+        return new FrameNumber(frameNumber + NEXT_FRAME_NUMBER);
+    }
+
+    public boolean isOver() {
+        return frameNumber == LAST_FRAME_NUMBER;
+    }
+
+    public boolean isNormalFrameOver() {
+        return frameNumber == NORMAL_FRAME_MAX_NUMBER;
+    }
+}
---
+++ src/test/java/bowling/domain/FrameNumberTest.java
@@ -0,0 +1,52 @@
+package bowling.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+/**
+ * author       : gwonbyeong-yun <sksggg123>
+ * ------------------------------------------
+ * | email        : sksggg123               |
+ * | github       : github.com/sksggg123    |
+ * | blog         : sksggg123.github.io     |
+ * ------------------------------------------
+ * project      : java-bowling
+ * create date  : 2019-07-17 00:30
+ */
+public class FrameNumberTest {
+    @DisplayName("전달 받은 번호를 통해 생성 - 11번째 프레임은 생성 불가")
+    @Test
+    void createFrameNumber() {
+        FrameNumber number = FrameNumber.init();
+        for (int i = 0; i < 9; i++) {
+            number = number.next();
+        }
+        FrameNumber finalNumber = number;
+        assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> {
+            finalNumber.next();
+        }).withMessageContaining("10프레임까지만 게임할 수 있습니다.");
+    }
+
+    @DisplayName("게임이 종료되었는지 (10번째 프레임인지) 확인")
+    @Test
+    void isOverFrameNumber() {
+        FrameNumber number = FrameNumber.init();
+        for (int i = 0; i < 9; i++) {
+            number = number.next();
+        }
+        assertThat(number.isOver()).isTrue();
+    }
+
+    @DisplayName("NormalFrame 종료되었는지 (9번째 프레임인지) 확인")
+    @Test
+    void isOverNormalFrameNumber() {
+        FrameNumber number = FrameNumber.init();
+        for (int i = 0; i < 8; i++) {
+            number = number.next();
+        }
+        assertThat(number.isNormalFrameOver()).isTrue();
+    }
+}
