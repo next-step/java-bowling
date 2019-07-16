303ea101a39a131b596ada91e71e27031927b2cc
권병윤 <sksggg123@gmail.com>
2019-07-17 01:31:25 +0900

    [test] : test 코드 메서드 한글로 변경

--- src/test/java/bowling/domain/FrameNumberTest.java
+++ src/test/java/bowling/domain/FrameNumberTest.java
@@ -19,7 +19,7 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 public class FrameNumberTest {
     @DisplayName("전달 받은 번호를 통해 생성 - 11번째 프레임은 생성 불가")
     @Test
-    void createFrameNumber() {
+    void 열한번째_프레임_생성_불가() {
         FrameNumber number = FrameNumber.init();
         for (int i = 0; i < 9; i++) {
             number = number.next();
@@ -32,7 +32,7 @@ public class FrameNumberTest {
 
     @DisplayName("게임이 종료되었는지 (10번째 프레임인지) 확인")
     @Test
-    void isOverFrameNumber() {
+    void 게임_종료() {
         FrameNumber number = FrameNumber.init();
         for (int i = 0; i < 9; i++) {
             number = number.next();
@@ -42,7 +42,7 @@ public class FrameNumberTest {
 
     @DisplayName("NormalFrame 종료되었는지 (9번째 프레임인지) 확인")
     @Test
-    void isOverNormalFrameNumber() {
+    void 일반프레임_종료() {
         FrameNumber number = FrameNumber.init();
         for (int i = 0; i < 8; i++) {
             number = number.next();
