9ddccdca828509ad6b334d1c1901fa11ff13bb8e
1nteger <rhep0820@gmail.com>
2019-07-17 18:16:27 +0900

    [step1] feat : Spare 클래스 구현

--- src/main/java/domain/state/Spare.java
+++ src/main/java/domain/state/Spare.java
@@ -3,16 +3,26 @@ package domain.state;
 import domain.Pins;
 
 public class Spare implements State {
-    public Spare(Pins firstFallenPins, Pins secondFallenPins) {
+    static final String ALERT_CANNOT_BE_SPARE = "스페어가 아닙니다.";
+
+    private Pins firstFallenPins;
+    private Pins secondFallenPins;
+
+    Spare(Pins firstFallenPins, Pins secondFallenPins) {
+        if (!firstFallenPins.isSpare(secondFallenPins)) {
+            throw new IllegalArgumentException(ALERT_CANNOT_BE_SPARE);
+        }
+        this.firstFallenPins = firstFallenPins;
+        this.secondFallenPins = secondFallenPins;
     }
 
     @Override
     public State update(Pins fallenPins) {
-        return null;
+        throw new ClosedFrameException();
     }
 
     @Override
     public boolean isClosed() {
-        return false;
+        return Boolean.TRUE;
     }
 }
