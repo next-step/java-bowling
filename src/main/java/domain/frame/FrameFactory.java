7d5b3d1c8211ef6f4560d53b2378d1aa7069fbee
sunkyung <sunkyungch@gmail.com>
2019-07-19 02:34:37 +0900

    refactor : 프레임 생성 factory 추가

--- src/main/java/domain/frame/FrameFactory.java
+++ src/main/java/domain/frame/FrameFactory.java
@@ -3,22 +3,27 @@ package domain.frame;
 import java.util.ArrayList;
 import java.util.List;
 
-import static domain.frame.FrameIndex.TOTAL_FRAME;
+import static domain.frame.FrameIndex.LAST_NORMAL_FRAME;
+import static domain.frame.FrameIndex.START_FRAME;
 
 public class FrameFactory {
 
     private final static int FIRST = 0;
 
     public static Frames createFrames() {
-        Frame before = new LastFrame();
-
         List<Frame> frames = new ArrayList<>();
+
+        Frame before = new LastFrame();
         frames.add(FIRST, before);
 
-        for (int i = 0; i < TOTAL_FRAME; i++) {
+        createNormalFrames(before, frames);
+        return Frames.of(frames);
+    }
+
+    private static void createNormalFrames(Frame before, List<Frame> frames) {
+        for (int i = START_FRAME; i < LAST_NORMAL_FRAME; i++) {
             before = new NormalFrame(before);
             frames.add(FIRST, before);
         }
-        return Frames.of(frames);
     }
 }
--- src/test/java/domain/frame/FrameFactoryTest.java
+++ src/test/java/domain/frame/FrameFactoryTest.java
@@ -1,14 +1,21 @@
 package domain.frame;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FrameFactoryTest {
 
     @Test
-    void createFrames() {
+    @DisplayName("생성된 개수 확인")
+    void size() {
         Frames frames = FrameFactory.createFrames();
-        System.out.println();
+        List<Frame> framesList = frames.getFrames();
+        assertThat(framesList.size()).isEqualTo(10);
     }
+
 }
