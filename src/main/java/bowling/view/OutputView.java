67ef02b56024e34ef89297eed5c177c5586cd23b
leechang0423 <leechang0423@naver.com>
2019-07-29 17:39:33 +0900

    step2 feat(bowling) score 출력 기능 추가

--- src/main/java/bowling/view/OutputView.java
+++ src/main/java/bowling/view/OutputView.java
@@ -11,6 +11,7 @@ public class OutputView {
   public static void initialBoardPrint(Player player, BowlingGameResult results) {
     firstLInePrint();
     System.out.println(gameResultPrint(results, player));
+    System.out.println(gameScorePrint(results, player));
   }
 
   private static String gameResultPrint(BowlingGameResult results, Player player) {
@@ -20,6 +21,21 @@ public class OutputView {
     resultView.append("|");
     for (int i = 1; i < 11; i++) {
       resultView.append(TextArrangeUtil.arrange(results.hasResult(i) ? results.result(i) : "", 8));
+      resultView.append("|");
+    }
+    return resultView.toString();
+  }
+
+  private static String gameScorePrint(BowlingGameResult results, Player player) {
+    StringBuffer resultView = new StringBuffer();
+    resultView.append("|");
+    resultView.append(TextArrangeUtil.arrange(player.toString(), 8));
+    resultView.append("|");
+    int score = 0;
+    for (int i = 1; i < 11; i++) {
+      resultView.append(TextArrangeUtil.arrange(
+          (results.hasScore(i) && results.score(i) != -1) ? (score += results.score(i)) + "" : "",
+          8));
       resultView.append("|");
     }
     return resultView.toString();
