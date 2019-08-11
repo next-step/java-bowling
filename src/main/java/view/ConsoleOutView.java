e7999b5d6ca7914d44ddb36317d40844ec6a068f
권병윤 <sksggg123@gmail.com>
2019-08-12 01:30:55 +0900

    [refactor] : View 로직 변경

--- src/main/java/view/ConsoleOutView.java
+++ src/main/java/view/ConsoleOutView.java
@@ -1,6 +1,8 @@
 package view;
 
 import bowling.domain.BowlingCenter;
+import bowling.domain.Player;
+import bowling.domain.Players;
 import view.util.PrintUtil;
 
 /**
@@ -15,34 +17,36 @@ import view.util.PrintUtil;
  */
 public class ConsoleOutView {
 
-    public static void printFrame(BowlingCenter center) {
-        printHeaderArea();
-        printPlayNameArea(center);
-        printScoreArea(center);
-    }
-
     public static void printHeaderArea() {
         System.out.println(PrintUtil.headerArea());
     }
 
-    public static void printPlayNameArea(BowlingCenter center) {
-        StringBuilder sb = new StringBuilder();
-
-        sb.append(PrintUtil.playerNameArea(center.playerName()));
-        center.displayState().stream()
-                .forEach(state -> sb.append(PrintUtil.stateFrmaeArea(state)));
-        sb.append(PrintUtil.idleFrameArea(center.displayState().size()));
-
-        System.out.println(sb.toString());
+    public static void printFrame(BowlingCenter center, Players players) {
+        printHeaderArea();
+        players.getPlayers().stream()
+                .forEach(player -> printPlayNameArea(center, player));
     }
 
-    public static void printScoreArea(BowlingCenter center) {
+    public static void printPlayNameArea(BowlingCenter center, Player player) {
         StringBuilder sb = new StringBuilder();
 
+        sb.append(PrintUtil.playerNameArea(player.getName()));
+
+        center.displayState(player).stream()
+                .forEach(state -> sb.append(PrintUtil.stateFrmaeArea(state)));
+        sb.append(PrintUtil.idleFrameArea(center.displayState(player).size()));
+        System.out.println(sb.toString());
+
+        printScoreArea(center, player);
+    }
+
+    public static void printScoreArea(BowlingCenter center, Player player) {
+        StringBuilder sb = new StringBuilder();
         sb.append(PrintUtil.scoreFrameFirstArea());
-        center.displayScore().stream()
+
+        center.displayScore(player).stream()
                 .forEach(score -> sb.append(PrintUtil.scoreFrameArea(score)));
-        sb.append(PrintUtil.idleFrameArea(center.displayScore().size()));
+        sb.append(PrintUtil.idleFrameArea(center.displayScore(player).size()));
 
         System.out.println(sb.toString());
     }
