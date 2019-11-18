9244e5b23f73f1365ed7ae8392e76e837a5c65f3
chanin <pci2676@gmail.com>
2019-11-18 20:03:04 +0900

    java-bowling step1 - feat 이름값 생성

---
+++ src/test/java/NameTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameTest {
+
+    @Test
+    void 생성() {
+        String rawName = "PCI";
+        Name name = new Name(rawName);
+
+        assertThat(name).isEqualTo(new Name(rawName));
+    }
+
+    @Test
+    void 세글자이상_이름입력시() {
+        String invalidName = "CHAN";
+
+        assertThatThrownBy(() -> new Name(invalidName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(invalidName + "은 3글자보다 긴 이름입니다.");
+    }
+}
