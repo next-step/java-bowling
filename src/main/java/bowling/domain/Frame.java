4e5048ae494eef521491ac792b3e8c69551e0a57
vagabond95 <progjh@gmail.com>
2019-11-20 01:23:04 +0900

    [feat] FrameSet 기본 play 로직 및 test 추가

---
+++ src/main/java/bowling/domain/Frame.java
@@ -0,0 +1,10 @@
+package bowling.domain;
+
+public enum  Frame {
+    STRIKE,
+    SPARE,
+    MISS,
+    GUTTER,
+    HIT,
+    NO_HIT;
+}
---
+++ src/main/java/bowling/domain/FrameSet.java
@@ -0,0 +1,34 @@
+package bowling.domain;
+
+public class FrameSet {
+
+    int playCount = 2;
+    int remainPinCount = 10;
+
+    public Frame play(int hitCount) {
+        playCount--;
+        remainPinCount -= hitCount;
+
+        if (hitCount == 10) {
+            return Frame.STRIKE;
+        }
+
+        if (playCount == 0 && remainPinCount == 0) {
+            return Frame.SPARE;
+        }
+
+        if (playCount == 0 && remainPinCount == 10) {
+            return Frame.GUTTER;
+        }
+
+        if (playCount == 0 && remainPinCount > 0) {
+            return Frame.MISS;
+        }
+
+        if (remainPinCount < 10) {
+            return Frame.HIT;
+        }
+
+        return Frame.NO_HIT;
+    }
+}
