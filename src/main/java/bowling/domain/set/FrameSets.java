154a14d63dd2de1ae035014b00c884e434215e15
vagabond95 <progjh@gmail.com>
2019-12-28 21:24:22 +0900

    [refactor] FrameSets 일급 컬렉션 추가

---
+++ src/main/java/bowling/domain/set/FrameSets.java
@@ -0,0 +1,45 @@
+package bowling.domain.set;
+
+import bowling.domain.FrameResults;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FrameSets {
+
+    private final List<FrameSet> frameSets = new ArrayList<>();
+
+    public static FrameSets create() {
+        return new FrameSets();
+    }
+
+    private FrameSets() {
+        frameSets.add(NormalFrameSet.createFirst());
+    }
+
+    public boolean hasNext() {
+        return !getCurrentFrameSet().isEndedGame();
+    }
+
+    public int getCurrentPlayCount() {
+        return getCurrentFrameSet().getPlayCount();
+    }
+
+    public FrameResults play(int hitCount) {
+        FrameSet frameSet = getCurrentFrameSet();
+        frameSet.play(hitCount);
+
+        FrameResults frameResults = new FrameResults(Collections.unmodifiableList(frameSets));
+
+        if (frameSet.isEndedFrame()) {
+            frameSets.add(frameSet.getNext());
+        }
+
+        return frameResults;
+    }
+
+    private FrameSet getCurrentFrameSet() {
+        return frameSets.get(frameSets.size() - 1);
+    }
+}
