5db898163711224aac2c868dfb99a4404ef58ae1
liquidjoo <liquidjoo@gmail.com>
2020-03-28 00:44:28 +0900

    feat : 스페어 상태 추가

--- src/main/java/bowling/domain/state/NextReady.java
+++ src/main/java/bowling/domain/state/NextReady.java
@@ -3,14 +3,17 @@ package bowling.domain.state;
 import java.util.Objects;
 
 public class NextReady implements State{
-    private int pins;
+    private int fallenPins;
 
-    public NextReady(int pins) {
-        this.pins = pins;
+    public NextReady(int fallenPins) {
+        this.fallenPins = fallenPins;
     }
 
     @Override
     public State bowl(int pins) {
+        if (fallenPins + pins == 10) {
+            return new Spare();
+        }
         return null;
     }
 
@@ -19,11 +22,11 @@ public class NextReady implements State{
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         NextReady nextReady = (NextReady) o;
-        return pins == nextReady.pins;
+        return fallenPins == nextReady.fallenPins;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(pins);
+        return Objects.hash(fallenPins);
     }
 }
--- src/test/java/bowling/domain/state/NextReadyTest.java
+++ src/test/java/bowling/domain/state/NextReadyTest.java
@@ -17,4 +17,13 @@ class NextReadyTest {
 
         assertThat(same).isTrue();
     }
+
+    @Test
+    @DisplayName("스페어 상태 확인")
+    void checkNextReadyToSpare() {
+        State state = new NextReady(7);
+        State checkState = state.bowl(3);
+
+        assertThat(checkState instanceof Spare).isTrue();
+    }
 }
