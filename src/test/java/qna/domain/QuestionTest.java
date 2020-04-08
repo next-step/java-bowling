1c3217b6172ffe30070205b0e05a155fabc48c59
ohtaeg <otk1090@naver.com>
2020-04-09 05:01:24 +0900

    test (Question) : 안쓰는 Q2 제거

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -8,7 +8,6 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-    public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
 
     @DisplayName("로그인한 유저가 작성자가 아니면 예외를 발생한다.")
     @Test
