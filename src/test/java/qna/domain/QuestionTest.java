30c87beda5de4192ed73068daf2ed360c4e5858b
LeeSeungHee <palmseung@gmail.com>
2020-04-08 22:06:29 +0900

    Test (Question) : add test to validate deletion When login user is a writer

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,21 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @DisplayName("로그인 유저가 질문 작성자이면 예외가 발생하지 않음")
+    @Test
+    void validateCanDeleteWhenLoginUserIsWriter() {
+        User loginUser = UserTest.JAVAJIGI;
+
+        assertThatCode(() -> {
+            Q1.delete(loginUser);
+        }).doesNotThrowAnyException();
+    }
 }
