360283650d069794d1e8786ec3bc19e89c679cee
parkeeseul <eeseul.p@gmail.com>
2020-04-08 22:05:48 +0900

    test(AnswerTest): deleted 테스트 추가

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,13 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    void delete() {
+        A1.delete();
+    }
 }
