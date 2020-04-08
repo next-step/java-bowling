c68df324ca950217cae25d2376235b2afe1387a0
LeeSeungHee <palmseung@gmail.com>
2020-04-09 01:12:40 +0900

    Test (Answers) : add test to for changing deleted state for Answers

--- src/test/java/qna/domain/AnswersTest.java
+++ src/test/java/qna/domain/AnswersTest.java
@@ -6,11 +6,15 @@ import qna.CannotDeleteException;
 
 import java.util.Arrays;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.InstanceOfAssertFactories.ARRAY;
 
 public class AnswersTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+    public static final Answer A3 = new Answer(UserTest.SANJIGI, QuestionTest.Q2, "Answers Contents3");
+    public static final Answer A4 = new Answer(UserTest.SANJIGI, QuestionTest.Q2, "Answers Contents4");
 
     @DisplayName("답변 목록 중에 로그인 유저가 쓰지 않은 답변이 있으면 예외를 반환한다. ")
     @Test
@@ -21,5 +25,20 @@ public class AnswersTest {
         assertThatThrownBy(() -> {
             answers.delete(loginUser);
         }).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @DisplayName("답변 목록이 모두 로그인 유저가 쓴 답변이라면 deleted가 true로 바뀐다.")
+    @Test
+    void changeDeletedStateWhenLoginUserIsWriter() throws CannotDeleteException {
+        //given
+        Answers answers = new Answers(Arrays.asList(A3, A4));
+        User loginUser = UserTest.SANJIGI;
+
+        //when
+        answers.delete(loginUser);
+
+        //then
+        assertThat(A3.isDeleted()).isTrue();
+        assertThat(A4.isDeleted()).isTrue();
     }
 }
