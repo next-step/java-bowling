5cdc78f701cba632d0197e0d7c884a879179758a
dongchul <sah3122@naver.com>
2020-04-09 01:02:25 +0900

    test(Question): add Question Test Case

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -2,6 +2,7 @@ package qna.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -9,37 +10,26 @@ public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
 
-    @DisplayName("질문의 소유자인지 테스트")
+    @DisplayName("질문을 삭제 할 권한이 있을 때 정상 동작")
     @Test
     void isOwner() {
-        assertThatCode(() -> Q1.isOwner(UserTest.JAVAJIGI)).doesNotThrowAnyException();
+        assertThatCode(() -> Q1.checkRemovable(UserTest.JAVAJIGI)).doesNotThrowAnyException();
+        assertThatCode(() -> Q2.checkRemovable(UserTest.SANJIGI)).doesNotThrowAnyException();
     }
 
-    @DisplayName("질문의 소유자가 아닐때 에러")
+    @DisplayName("질문의 삭제할 권한이 없을때 throws CannotDeleteException")
     @Test
     void isOwnerThrowException() {
-        assertThatIllegalArgumentException().isThrownBy(() -> Q1.isOwner(UserTest.JAVAJIGI));
+        assertThatExceptionOfType(CannotDeleteException.class).isThrownBy(() -> Q1.checkRemovable(UserTest.SANJIGI));
     }
 
     @DisplayName("질문 삭제")
     @Test
     void delete() {
-        assertThatCode(() -> Q1.delete()).doesNotThrowAnyException();
+        Q1.delete();
         assertThat(Q1.isDeleted()).isTrue();
-    }
 
-    @DisplayName("질문이 삭제 가능한지 테스트")
-    @Test
-    void checkRemovable() {
-        Q1.addAnswer(AnswerTest.A1);
-        assertThatCode(() -> Q1.checkRemovable(UserTest.JAVAJIGI)).doesNotThrowAnyException();
-    }
-
-    @DisplayName("질문이 삭제 불가능할때 에러")
-    @Test
-    void checkRemovable() {
-        Q1.addAnswer(AnswerTest.A1);
-        Q1.addAnswer(AnswerTest.A2);
-        assertThatIllegalArgumentException().isThrownBy(() -> Q1.checkRemovable(UserTest.JAVAJIGI));
+        Q2.delete();
+        assertThat(Q2.isDeleted()).isTrue();
     }
 }
