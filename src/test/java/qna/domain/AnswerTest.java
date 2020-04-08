e79c54449bdf5b1862288f8781cb0770aa686137
Paul Je <dev.gales34@gmail.com>
2020-04-08 22:20:55 +0900

    test: add Answer, Question, User Tests

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,21 @@
 package qna.domain;
 
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @DisplayName("Answer 삭제 테스트")
+    @Test
+    public void deleteAnswerTest() {
+        assertThatCode(A1.deleteByUser(UserTest.JAVAJIGI)).doesNotThrowAnyException();
+
+        assertThatThrownBy(A2.deleteByUser(UserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class);
+    }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,22 @@
 package qna.domain;
 
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @DisplayName("Question 삭제 테스트")
+    @Test
+    public void deleteQuestionTest() {
+        assertThatCode(AnswerTest.A1.deleteByUser(UserTest.JAVAJIGI)).doesNotThrowAnyException();
+
+        assertThatThrownBy(AnswerTest.A2.deleteByUser(UserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class);
+    }
 }
