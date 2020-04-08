a51d5bc88459c83032bc94ab3551a539bca749cc
parkeeseul <eeseul.p@gmail.com>
2020-04-08 22:23:30 +0900

    test(QuestionTest): getDeleteHistory 메소드 테스트 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -2,12 +2,25 @@ package qna.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class QuestionTest {
-    public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+    public static final Question Q1 = new Question(1, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
 
     @Test
     void delete() {
         Q1.delete();
+    }
+
+    @Test
+    void getDeleteHistory() {
+        DeleteHistory deleteHistory = Q1.getDeleteHistory();
+
+        assertThat(deleteHistory).isEqualTo(new DeleteHistory(ContentType.QUESTION, 1l, JAVAJIGI, LocalDateTime.now()));
     }
 }
