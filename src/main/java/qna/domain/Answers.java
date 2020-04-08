2a031753b2f75cd6aee69e4f4610800b1cbf2cdf
ohtaeg <otk1090@naver.com>
2020-04-09 03:08:42 +0900

    refactor (Answers) : 질문자와 답변글의 답변자들이 같은지 체크 메서드 추가

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -1,9 +1,13 @@
 package qna.domain;
 
+import qna.CannotDeleteException;
+
 import java.util.Collections;
 import java.util.List;
 
 public class Answers {
+    private static final String CANNOT_DELETE_MESSAGE = "다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.";
+
     private final List<Answer> answers;
 
     public Answers(final List<Answer> answers) {
@@ -12,5 +16,16 @@ public class Answers {
 
     public List<Answer> getAnswers() {
         return answers;
+    }
+
+    public void checkDeletable(final User user) {
+        if (hasSomethingElse(user)) {
+            throw new CannotDeleteException(CANNOT_DELETE_MESSAGE);
+        }
+    }
+
+    private boolean hasSomethingElse(final User user) {
+        return answers.stream()
+                      .anyMatch(answer -> !answer.isOwner(user));
     }
 }
