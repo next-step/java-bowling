0464956901f97d952b6a239efe1e53081b577280
ohtaeg <otk1090@naver.com>
2020-04-09 01:41:09 +0900

    test (Question) : 로그인 유저와 작성자와 같은지 테스트 코드 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,22 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @DisplayName("로그인한 유저가 작성자가 아니면 예외를 발생한다.")
+    @Test
+    void throwCannotDeleteException() {
+        User loginUser = new User();
+        loginUser.setUserId("1L");
+        assertThatExceptionOfType(CannotDeleteException.class).isThrownBy(
+                () -> Q1.checkSameWriter(loginUser)
+        );
+    }
 }
