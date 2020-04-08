da8c9c12deaafe6227fda69389c9311cd080badf
javajigi <javajigi@gmail.com>
2020-04-08 11:53:51 +0900

    refactor(deletehistory): replace save to saveAll when iterable data

--- src/main/java/qna/service/DeleteHistoryService.java
+++ src/main/java/qna/service/DeleteHistoryService.java
@@ -16,9 +16,7 @@ public class DeleteHistoryService {
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void saveAll(List<DeleteHistory> deleteHistories) {
-        for (DeleteHistory deleteHistory : deleteHistories) {
-            deleteHistoryRepository.save(deleteHistory);
-        }
+        deleteHistoryRepository.saveAll(deleteHistories);
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
