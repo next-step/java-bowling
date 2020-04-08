19ac7a5a585c1d29be993bfc0c75c693f4fb19fb
ohtaeg <otk1090@naver.com>
2020-04-09 07:31:02 +0900

    refactor (DeleteHistories) : 파라미터 변경 및 내부에서 List 생성로직 제거

--- src/main/java/qna/domain/DeleteHistories.java
+++ src/main/java/qna/domain/DeleteHistories.java
@@ -3,40 +3,22 @@ package qna.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class DeleteHistories {
     private final List<DeleteHistory> deleteHistories;
 
-    private DeleteHistories(final List<DeleteHistory> deleteHistories) {
+    public DeleteHistories(final List<DeleteHistory> deleteHistories) {
         this.deleteHistories = Collections.unmodifiableList(deleteHistories);
     }
 
-    public static DeleteHistories of(final Question question, final Answers answers) {
-        return createHistory(question, answers);
+    public DeleteHistories merge(final DeleteHistories other) {
+        List<DeleteHistory> source = new ArrayList<>();
+        source.addAll(deleteHistories);
+        source.addAll(other.deleteHistories);
+        return new DeleteHistories(source);
     }
 
     public List<DeleteHistory> getDeleteHistories() {
         return deleteHistories;
-    }
-
-    private static DeleteHistories createHistory(final Question question, final Answers answers) {
-        List<DeleteHistory> histories = new ArrayList<>();
-        histories.add(createQuestionHistory(question));
-
-        List<DeleteHistory> answerHistory = createAnswerHistory(answers);
-        histories.addAll(answerHistory);
-        return new DeleteHistories(histories);
-    }
-
-    private static DeleteHistory createQuestionHistory(final Question question) {
-        return new DeleteHistory(question);
-    }
-
-    private static List<DeleteHistory> createAnswerHistory(final Answers answers) {
-        return answers.getAnswers()
-                      .stream()
-                      .map(DeleteHistory::new)
-                      .collect(Collectors.toList());
     }
 }
