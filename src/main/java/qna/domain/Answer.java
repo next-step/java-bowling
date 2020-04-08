5efa2621b9d6ca83e8bd2a479503c4644cfd49a6
LeeSeungHee <palmseung@gmail.com>
2020-04-08 22:24:01 +0900

    Feat (Answer) : add feature to validateCanDelete when loginUser is not a writer
    * Add Answer.isWriter
    
    * Add Answer.validateCanDelete
    
    * Add Answer.delete

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -1,5 +1,6 @@
 package qna.domain;
 
+import qna.CannotDeleteException;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
 
@@ -73,7 +74,17 @@ public class Answer extends AbstractEntity {
         return "Answer [id=" + getId() + ", writer=" + writer + ", contents=" + contents + "]";
     }
 
-    public void delete(User loginUser) {
+    public void delete(User loginUser) throws CannotDeleteException {
+        validateCanDelete(loginUser);
+    }
 
+    private void validateCanDelete(User loginUser) throws CannotDeleteException {
+        if(!isWriter(loginUser)){
+            throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+        }
+    }
+
+    private boolean isWriter(User loginUser){
+        return this.writer.equalsNameAndEmail(loginUser);
     }
 }
