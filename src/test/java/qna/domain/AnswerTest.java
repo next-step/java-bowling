ca846263b2966b65b924da7cc793d7312b283ff9
teahyuk <jtaeh57@gmail.com>
2020-04-08 22:11:54 +0900

    refactor(Answer): add delete method

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,29 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("잘 삭제 됐는지")
+    void deleteAnswerTest() throws CannotDeleteException {
+        assertThat(A1.delete(UserTest.JAVAJIGI))
+                .isEqualTo(new DeleteHistory(ContentType.ANSWER, A1.getId(), A1.getWriter(), LocalDateTime.now()));
+    }
+
+    @Test
+    @DisplayName("요청자가 다를 때 삭제 되지 않는지")
+    void deleteAnswerFailTest() {
+        assertThatThrownBy(() -> A1.delete(UserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class);
+
+    }
 }
