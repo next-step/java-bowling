8bc85a0205e4ccaf52309a86c666507c55631d0a
kwanguuuu <kwangyu326@gmail.com>
2020-04-08 22:04:02 +0900

    refactor : setDeleted(true)가 setter를 사용하던 부분을 메서드가 실행하도록 변경함

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,15 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    void deleteTest() {
+        assertThat(Q1.isDeleted()).isFalse();
+        assertThat(Q1.delete().isDeleted()).isTrue();
+    }
 }
