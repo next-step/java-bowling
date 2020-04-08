45a8848dcdf5b0db6a14fae732b93d2c8638e9f9
ohtaeg <otk1090@naver.com>
2020-04-09 04:03:58 +0900

    test (Answer) : 답변글 삭제 테스트 코드 추가

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,26 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AnswerTest {
-    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+    private User javajigi;
+    private Answer answer;
+
+    @BeforeEach
+    void setUp() {
+        javajigi = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
+        answer = new Answer(javajigi, QuestionTest.Q1, "Answers Contents1");
+    }
+
+    @DisplayName("답변글을 삭제할 수 있다.")
+    @Test
+    void delete() {
+        answer.delete();
+
+        assertThat(answer.isDeleted()).isTrue();
+    }
 }
