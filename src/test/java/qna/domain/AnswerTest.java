3b6009bf1a7333f47ee6198acc7cfd0f030efe02
LeeSeungHee <palmseung@gmail.com>
2020-04-08 22:25:45 +0900

    Test (Answer) : add feature to validateCanDelete when loginUser is a writer

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import qna.CannotDeleteException;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
@@ -18,5 +18,15 @@ public class AnswerTest {
         assertThatThrownBy(() -> {
             A1.delete(loginUser);
         }).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @DisplayName("로그인 유저가 답변 작성자라면 아무런 예외를 발생하지 않는다.")
+    @Test
+    void validateCanDeleteWhenLoginUserIsWriter(){
+        User loginUSer = UserTest.JAVAJIGI;
+
+        assertThatCode(() -> {
+            A1.delete(loginUSer);
+        }).doesNotThrowAnyException();
     }
 }
