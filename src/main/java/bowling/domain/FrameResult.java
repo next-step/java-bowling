7e3ba8c7af6bbf22dccf22092492ec20a32e2967
LeeSeungHee <palmseung@gmail.com>
2020-04-10 14:47:36 +0900

    Feat (FrameResult) : add feature to find result when Gutter
    * Add FrameResult.isAllZero

--- src/main/java/bowling/domain/FrameResult.java
+++ src/main/java/bowling/domain/FrameResult.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public enum FrameResult {
     STRIKE(true, true, true, false),
     SPARE(false, true, false),
-    GUTTER,
+    GUTTER(false, true, false, true),
     MISS;
 
     private boolean isFirstTen;
@@ -34,6 +34,7 @@ public enum FrameResult {
                 .filter(value -> value.isFirstTen == isFirstTen(firstPoint))
                 .filter(value -> value.isSecondZero == isSecondZero(secondPoint))
                 .filter(value -> value.isSumTen == isSumTen(firstPoint, secondPoint))
+                .filter(value -> value.isAllZero == isAllZero(firstPoint, secondPoint))
                 .findFirst()
                 .orElse(MISS);
     }
@@ -48,5 +49,9 @@ public enum FrameResult {
 
     private static boolean isSumTen(int firstPoint, int secondPoint){
         return (firstPoint + secondPoint) == 10;
+    }
+
+    private static boolean isAllZero(int firstPoint, int secondPoint){
+        return (firstPoint == 0 && secondPoint == 0);
     }
 }
