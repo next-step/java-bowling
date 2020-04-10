4374533fdd7dabd1fa5efb780aa9478e844d9228
gale34 <gales34@gmail.com>
2020-04-10 11:28:00 +0900

    feat: add Answers, refactor answer

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -61,6 +61,10 @@ public class Question extends AbstractEntity {
         answers.add(answer);
     }
 
+    private Answers getAnswer() {
+        return new Answers(answers);
+    }
+
     public boolean isOwner(User loginUser) {
         return writer.equals(loginUser);
     }
@@ -74,22 +78,13 @@ public class Question extends AbstractEntity {
             throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
         }
 
-        for (Answer answer : answers) {
-            if (!answer.isOwner(loginUser)) {
-                throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
-            }
-        }
-
-        this.deleted = true;
-
         List<DeleteHistory> deleteHistories = new ArrayList<>();
         deleteHistories.add(new DeleteHistory(ContentType.QUESTION, getId(), writer, LocalDateTime.now()));
 
-        for (Answer answer : answers) {
-            answer.setDeleted(true);
-            deleteHistories.add(new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now()));
-        }
+        Answers answers = getAnswer();
+        deleteHistories.addAll(answers.deleteAll(loginUser));
 
+        this.deleted = true;
         return deleteHistories;
     }
 
