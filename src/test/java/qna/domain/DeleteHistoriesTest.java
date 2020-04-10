585f6a9513d8731946a9d974fa2759a90061d458
ohtaeg <otk1090@naver.com>
2020-04-11 02:03:06 +0900

    test (DeleteHistories) : 삭제시간 파라미터 추가

--- src/test/java/qna/domain/DeleteHistoriesTest.java
+++ src/test/java/qna/domain/DeleteHistoriesTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,8 +27,9 @@ class DeleteHistoriesTest {
     @DisplayName("질문을 삭제하면 삭제 히스토리 일급 객체를 생성할 수 있다.")
     @Test
     void create() {
+        LocalDateTime expect = LocalDateTime.of(2019, 4, 11, 1, 52);
         question.addAnswer(answer);
-        DeleteHistories deleteHistories = question.delete(user);
+        DeleteHistories deleteHistories = question.delete(user, expect);
 
         final List<DeleteHistory> actual = deleteHistories.getDeleteHistories();
 
