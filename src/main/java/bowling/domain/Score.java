8ca177612601183b58c48fe3f52affad98dc8f45
LeeSeungHee <palmseung@gmail.com>
2020-04-10 13:37:42 +0900

    Feat (Score) : add feature to create Score

--- src/main/java/bowling/domain/Score.java
+++ src/main/java/bowling/domain/Score.java
@@ -1,6 +1,9 @@
 package bowling.domain;
 
 public class Score {
+    private int score;
+
     public Score(int score) {
+        this.score = score;
     }
 }
--- src/test/java/bowling/ScoreTest.java
+++ src/test/java/bowling/ScoreTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class ScoreTest {
-    @DisplayName("스코어는 0 이상 10 이하의 숫자로만 생성")
+    @DisplayName("스코어는 0 이상 10 이하의 숫자로 생성")
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 5, 9, 10})
     void createTest(int score) {
