bf461849e5564d0b37256bb1021742772cfbf76e
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-10 11:45:11 +0900

    feat (Answers.java) 삭제 가능 여부 체크 로직 추가

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -1,5 +1,8 @@
 package qna.domain;
 
+import qna.CannotDeleteException;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Answers {
@@ -7,6 +10,14 @@ public class Answers {
     private final List<Answer> answers;
 
     public Answers(List<Answer> answers) {
-        this.answers = answers;
+        this.answers = new ArrayList<>(answers);
+    }
+
+    public void validateDeletedAble(User loginUser) {
+        for (Answer answer : answers) {
+            if (!answer.isOwner(loginUser)) {
+                throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+            }
+        }
     }
 }
--- src/test/java/qna/domain/AnswersTest.java
+++ src/test/java/qna/domain/AnswersTest.java
@@ -1,23 +1,43 @@
 package qna.domain;
 
-import org.junit.Before;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
 
 import java.util.Arrays;
 
 class AnswersTest {
 
-    private Answer answer;
-
-    @Before
-    public void setUp() throws Exception {
-        answer = new Answer(11L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    }
+    private Answer answer1 = new Answer(11L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+    private Answer answer2 = new Answer(11L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+    private Answer answer3 = new Answer(11L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
 
     @DisplayName("생성자")
     @Test
     public void constructor() throws Exception {
         Answers answers = new Answers(Arrays.asList(new Answer(), new Answer()));
+    }
+
+    @Test
+    public void validateDeletedAble_success() throws Exception {
+        //given
+        System.out.println(answer1);
+        Answers answers = new Answers(Arrays.asList(answer1, answer2, answer3));
+
+        //when
+        answers.validateDeletedAble(UserTest.JAVAJIGI);
+    }
+
+    @Test
+    public void validateDeletedAble_fail() throws Exception {
+        //given
+        System.out.println(answer1);
+        Answers answers = new Answers(Arrays.asList(answer1, answer2, answer3));
+
+        //when
+        Assertions.assertThatThrownBy(
+                () -> answers.validateDeletedAble(UserTest.SANJIGI)
+        ).isInstanceOf(CannotDeleteException.class);
     }
 }
