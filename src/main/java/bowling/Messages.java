1736459c87de1245469a51fce2952eec08fe4ae5
LeeSeungHee <palmseung@gmail.com>
2020-04-10 12:42:36 +0900

    Feat (ScannerUtil) : add feature to validate name input not empty
    * Add ScannerUtil.validateNullAndEmpty

--- src/main/java/bowling/Messages.java
+++ src/main/java/bowling/Messages.java
@@ -5,4 +5,5 @@ public class Messages {
 
     public static final String WARNING_PLAYERNAME_NOT_ALLOWED_LENGTH = "플레이어 이름은 3글자여야 합니다.";
     public static final String WARNING_PLAYERNAME_MUST_ENGLISH = "플레이어 이름은 영문이어야 합니다.";
+    public static final String WARNING_SCANNERUTIL_NOT_ALLOWED_NULL_EMPTY = "공백 문자열은 입력할 수 없습니다.";
 }
