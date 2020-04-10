072eb71e4c95e0b16707ff67a103128bf1a1784a
LeeSeungHee <palmseung@gmail.com>
2020-04-10 13:42:45 +0900

    Feat (Score) : add feature to validate score input range
    * Add Score.validateRange

--- src/main/java/bowling/domain/Score.java
+++ src/main/java/bowling/domain/Score.java
@@ -1,9 +1,21 @@
 package bowling.domain;
 
+import static bowling.Messages.WARNING_SCORE_NOT_ALLOWED_RANGE;
+
 public class Score {
+    private static final int RANGE_MAX = 10;
+    private static final int RANGE_MIN = 0;
+
     private int score;
 
     public Score(int score) {
+        validateRange(score);
         this.score = score;
+    }
+
+    private void validateRange(int score) {
+        if (score > RANGE_MAX || score < RANGE_MIN) {
+            throw new IllegalArgumentException(WARNING_SCORE_NOT_ALLOWED_RANGE);
+        }
     }
 }
