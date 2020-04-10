8c0bc427229437306afae0335101e7765a08261c
kks <rudtjs9956@gmail.com>
2020-04-11 00:20:35 +0900

    feat(Question): add delete

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,43 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    public void deleteNoAnswerQuestion() throws CannotDeleteException {
+        Q1.delete(UserTest.JAVAJIGI);
+        assertThat(Q1.isDeleted()).isTrue();
+    }
+
+    @Test
+    public void deleteOtherUserQuestionThrowCannotDeleteException() {
+        assertThatThrownBy(() -> {
+            Q1.delete(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class)
+                .hasMessage("질문을 삭제할 권한이 없습니다.");
+    }
+
+    @Test
+    public void deleteWithMyAnswer() throws CannotDeleteException {
+        Q1.addAnswer(AnswerTest.A1);
+        Q1.delete(UserTest.JAVAJIGI);
+        assertThat(Q1.isDeleted()).isTrue();
+        assertThat(AnswerTest.A1.isDeleted()).isTrue();
+    }
+
+    @Test
+    public void deleteWithOtherUserAnswerThrowCannotDeleteException() {
+        Q1.addAnswer(AnswerTest.A1);
+        Q1.addAnswer(AnswerTest.A2);
+        assertThatThrownBy(() -> {
+            Q1.delete(UserTest.JAVAJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
 }
