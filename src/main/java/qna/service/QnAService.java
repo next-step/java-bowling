7d69bff6eabb9e0f0e4ce43c2fca68e5896b4a71
kks <rudtjs9956@gmail.com>
2020-04-11 00:21:20 +0900

    refactor(QnAService): change deleteQuestion

--- src/main/java/qna/service/QnAService.java
+++ src/main/java/qna/service/QnAService.java
@@ -35,17 +35,13 @@ public class QnAService {
     @Transactional
     public void deleteQuestion(User loginUser, long questionId) throws CannotDeleteException {
         Question question = findQuestionById(questionId);
-        if (!question.isOwner(loginUser)) {
-            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
-        }
-
-        List<Answer> answers = question.getAnswers();
+        question.delete(loginUser);
 
         List<DeleteHistory> deleteHistories = new ArrayList<>();
-        question.setDeleted(true);
         deleteHistories.add(new DeleteHistory(ContentType.QUESTION, questionId, question.getWriter(), LocalDateTime.now()));
+
+        List<Answer> answers = question.getAnswers();
         for (Answer answer : answers) {
-            answer.delete(loginUser);
             deleteHistories.add(new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now()));
         }
         deleteHistoryService.saveAll(deleteHistories);
