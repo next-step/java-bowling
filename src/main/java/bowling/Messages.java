0fba82bec10e31a71d39f50a0ef00379056626c3
LeeSeungHee <palmseung@gmail.com>
2020-04-10 13:58:33 +0900

    Feat (Frame) : add feature to validate secondScore when Strike
    * Add Frame.validateSecondWhenFirstTen

--- src/main/java/bowling/Messages.java
+++ src/main/java/bowling/Messages.java
@@ -7,4 +7,5 @@ public class Messages {
     public static final String WARNING_PLAYERNAME_MUST_ENGLISH = "플레이어 이름은 영문이어야 합니다.";
     public static final String WARNING_SCANNERUTIL_NOT_ALLOWED_NULL_EMPTY = "공백 문자열은 입력할 수 없습니다.";
     public static final String WARNING_SCORE_NOT_ALLOWED_RANGE = "스코어는 0 이상 10 이하의 숫자로만 생성 가능합니다.";
+    public static final String WARNING_FRAME_NOT_ALLOWED_SECOND_WHEN_STRIKE = "첫 번째 타구가 10점이면, 두 번째는 0점이어야 합니다.";
 }
