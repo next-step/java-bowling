3269de47e0bb91830db3bf8665dce762dfc94645
LeeSeungHee <palmseung@gmail.com>
2020-04-10 15:32:09 +0900

    Feat (RandomGenerator) : add feature to get first point
    * Add RandomGenerator.getFirstPoint

--- src/main/java/bowling/domain/RandomGenerator.java
+++ src/main/java/bowling/domain/RandomGenerator.java
@@ -1,8 +1,12 @@
 package bowling.domain;
 
+import java.util.Random;
+
 public class RandomGenerator {
+    private static final Random RANDOM = new Random();
+    private static final int BOUND_FIRST_POINT = 11;
 
     public static int getFirstPoint() {
-        return 0;
+        return RANDOM.nextInt(BOUND_FIRST_POINT);
     }
 }
--- src/test/java/bowling/RandomGeneratorTest.java
+++ src/test/java/bowling/RandomGeneratorTest.java
@@ -4,12 +4,15 @@ import bowling.domain.RandomGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RandomGeneratorTest {
     @DisplayName("첫 번째 포인트는 0 이상 10 이하의 숫자로 발생된다.")
     @Test
     void generateFirstPoint() {
-        assertThat(RandomGenerator.getFirstPoint()).isLessThanOrEqualTo(10);
+        IntStream.range(0, 1000)
+                .forEach(it -> assertThat(RandomGenerator.getFirstPoint()).isLessThanOrEqualTo(10));
     }
 }
