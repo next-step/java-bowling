d00af3de8211fb6e9aaa7c87fe09f3b6eae0368e
LeeSeungHee <palmseung@gmail.com>
2020-04-10 12:14:10 +0900

    Test (PlayerName) : add test to validate name length
    * Add PlayerNameTest.throwExceptionWhenGreaterThanThree

--- src/test/java/bowling/PlayerNameTest.java
+++ src/test/java/bowling/PlayerNameTest.java
@@ -3,8 +3,11 @@ package bowling;
 import bowling.domain.PlayerName;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class PlayerNameTest {
     @DisplayName("플레이어 이름이 알파벳 세 글자이면 성공")
@@ -15,5 +18,14 @@ public class PlayerNameTest {
         assertThatCode(() -> {
             new PlayerName(name);
         }).doesNotThrowAnyException();
+    }
+
+    @DisplayName("플레이어 이름의 길이가 3이 아니면 예외 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"Soojin, Yuqi, Soyeon"})
+    void throwExceptionWhenGreaterThanThree(String name) {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new PlayerName(name);
+        });
     }
 }
