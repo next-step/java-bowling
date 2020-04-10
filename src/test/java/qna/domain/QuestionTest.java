39ea9b60a7b636161cfd6b7169ee0d8208c82f9a
ohtaeg <otk1090@naver.com>
2020-04-11 02:02:53 +0900

    test (DeleteHistory) : 삭제시간 확인 테스트 이동

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -32,17 +32,4 @@ public class QuestionTest {
 
         assertThat(actual).isTrue();
     }
-
-    @DisplayName("질문글을 삭제한 시간을 확인할 수 있다.")
-    @Test
-    void now() {
-        Question question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-        LocalDateTime expect = LocalDateTime.of(2019, 4, 11, 1, 52);
-        DeleteHistories deleteHistories = question.delete(UserTest.JAVAJIGI, expect);
-        DeleteHistory deleteHistory = deleteHistories.getDeleteHistories().get(0);
-
-        LocalDateTime actual = deleteHistory.getCreateDate();
-
-        assertThat(actual).isEqualTo(expect);
-    }
 }
