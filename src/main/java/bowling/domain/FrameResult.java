93f21b093b2df2eb6de3392bcc3f40dd44f9fc03
LeeSeungHee <palmseung@gmail.com>
2020-04-10 14:34:09 +0900

    Feat (FrameResult) : add feature to find result when Strike
    * Add FrameResult.findResult
    
    * Add FrameResult.isFirstTen
    
    * Add FrameResult.isSecondZero

--- src/main/java/bowling/domain/FrameResult.java
+++ src/main/java/bowling/domain/FrameResult.java
@@ -1,12 +1,39 @@
 package bowling.domain;
 
-public enum FrameResult {
-    STRIKE,
-    SPARE,
-    MISS,
-    GUTTER;
+import java.util.Arrays;
 
-    public static FrameResult findResult(int firstPoint, int secondPoint){
-        return null;
+public enum FrameResult {
+    STRIKE(true, true, true, false),
+    MISS;
+
+    private boolean isFirstTen;
+    private boolean isSecondZero;
+    private boolean isSumTen;
+    private boolean isAllZero;
+
+    FrameResult() {
+    }
+
+    FrameResult(boolean isFirstTen, boolean isSecondZero, boolean isSumTen, boolean isAllZero) {
+        this.isFirstTen = isFirstTen;
+        this.isSecondZero = isSecondZero;
+        this.isSumTen = isSumTen;
+        this.isAllZero = isAllZero;
+    }
+
+    public static FrameResult findResult(int firstPoint, int secondPoint) {
+        return Arrays.stream(values())
+                .filter(value -> value.isFirstTen == isFirstTen(firstPoint))
+                .filter(value -> value.isSecondZero == isSecondZero(secondPoint))
+                .findFirst()
+                .orElse(MISS);
+    }
+
+    private static boolean isFirstTen(int firstPoint) {
+        return firstPoint == 10;
+    }
+
+    private static boolean isSecondZero(int secondPoint) {
+        return secondPoint == 0;
     }
 }
