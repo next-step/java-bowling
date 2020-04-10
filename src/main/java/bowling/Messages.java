3c0d211b7e0eaaadfd2666df984fe5337d0f6d4a
LeeSeungHee <palmseung@gmail.com>
2020-04-10 12:24:03 +0900

    Feat (PlayerName) : add feature to validate name is English
    * Add PlayerName.validateNameInEnglish

--- src/main/java/bowling/Messages.java
+++ src/main/java/bowling/Messages.java
@@ -2,4 +2,5 @@ package bowling;
 
 public class Messages {
     public static final String WARNING_PLAYERNAME_NOT_ALLOWED_LENGTH = "플레이어 이름은 3글자여야 합니다.";
+    public static final String WARNING_PLAYERNAME_MUST_ENGLISH = "플레이어 이름은 영문이어야 합니다.";
 }
--- src/main/java/bowling/domain/PlayerName.java
+++ src/main/java/bowling/domain/PlayerName.java
@@ -1,5 +1,6 @@
 package bowling.domain;
 
+import static bowling.Messages.WARNING_PLAYERNAME_MUST_ENGLISH;
 import static bowling.Messages.WARNING_PLAYERNAME_NOT_ALLOWED_LENGTH;
 
 public class PlayerName {
@@ -9,12 +10,19 @@ public class PlayerName {
 
     public PlayerName(String name) {
         validateNameLength(name);
+        validateNameInEnglish(name);
         this.name = name;
     }
 
     private void validateNameLength(String name) {
         if (name.length() != CRITERIA_NAME_LENGTH) {
             throw new IllegalArgumentException(WARNING_PLAYERNAME_NOT_ALLOWED_LENGTH);
+        }
+    }
+
+    private void validateNameInEnglish(String name){
+        if(name.toLowerCase() == name.toUpperCase()){
+            throw new IllegalArgumentException(WARNING_PLAYERNAME_MUST_ENGLISH);
         }
     }
 }
