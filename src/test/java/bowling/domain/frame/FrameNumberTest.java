b91570ca2ff182befe879a79c2b71d7fd043fb20
ohtaeg <otk1090@naver.com>
2020-04-12 02:53:52 +0900

    test (FrameNumber) : 프레임번호 1 ~ 10 이외의 숫자일경우 커스텀 예외 발생 테스트

--- src/test/java/bowling/domain/frame/FrameNumberTest.java
+++ src/test/java/bowling/domain/frame/FrameNumberTest.java
@@ -1,11 +1,16 @@
 package bowling.domain.frame;
 
+import bowling.exception.FrameNumberOutOfRangeException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class FrameNumberTest {
+
     @DisplayName("프레임 번호를 생성할 수 있다.")
     @Test
     void create() {
@@ -17,4 +22,12 @@ class FrameNumberTest {
         assertThat(actual).isEqualTo(expect);
     }
 
+    @DisplayName("프레임 번호가 1 ~ 10 범위 내가 아니라면 커스텀 예외를 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 11})
+    void validRange(int number) {
+        assertThatExceptionOfType(FrameNumberOutOfRangeException.class).isThrownBy(
+                () -> new FrameNumber(number)
+        );
+    }
 }
