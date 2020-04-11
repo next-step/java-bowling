6b3369b816e042c4456fa35f060f0a252859e8c0
ohtaeg <otk1090@naver.com>
2020-04-11 23:31:34 +0900

    test (Pins) : 10개의 핀을 가진 Pins 일급 컬렉션 생성 테스트

---
+++ src/test/java/bowling/domain/PinsTest.java
@@ -0,0 +1,18 @@
+package bowling.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PinsTest {
+    @DisplayName("10개의 핀을 가진 Pins 객체를 생성할 수 있다.")
+    @Test
+    void create() {
+        Pins expect = Pins.of();
+
+        Pins actual = Pins.of();
+
+        assertThat(actual).isEqualTo(expect);
+    }
+}
