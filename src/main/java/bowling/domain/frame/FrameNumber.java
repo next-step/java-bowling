2eecf8874c82251cc48f28988e6245717e16ace0
ohtaeg <otk1090@naver.com>
2020-04-12 02:54:33 +0900

    refactor (FrameNumber) : 프레임번호 1 ~ 10 이외의 숫자인지 체크하는 메서드 추가

--- src/main/java/bowling/domain/frame/FrameNumber.java
+++ src/main/java/bowling/domain/frame/FrameNumber.java
@@ -1,12 +1,24 @@
 package bowling.domain.frame;
 
+import bowling.exception.FrameNumberOutOfRangeException;
+
 import java.util.Objects;
 
 public class FrameNumber {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 10;
+
     private final int frameNumber;
 
     public FrameNumber(final int frameNumber) {
+        validRange(frameNumber);
         this.frameNumber = frameNumber;
+    }
+
+    private void validRange(final int frameNumber) {
+        if (frameNumber < MIN_NUMBER || frameNumber > MAX_NUMBER) {
+            throw new FrameNumberOutOfRangeException(frameNumber);
+        }
     }
 
     @Override
