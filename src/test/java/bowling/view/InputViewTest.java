195557af8bd12b8298bd5a0fac10f758226e3173
ohtaeg <otk1090@naver.com>
2020-04-11 21:57:03 +0900

    test (InputView) : inputStream 의존성 주입받아 이름 입력받는 테스트로 변경

--- src/test/java/bowling/view/InputViewTest.java
+++ src/test/java/bowling/view/InputViewTest.java
@@ -1,8 +1,9 @@
 package bowling.view;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,19 +11,18 @@ class InputViewTest {
 
     private InputView inputView;
 
-    @BeforeEach
-    void setUp() {
-        inputView = new InputView();
-    }
-
     @DisplayName("이름을 입력받을 수 있다.")
     @Test
     void inputPlayerName() {
         String expect = "otk";
+        inputView = new InputView(createInputStream(expect));
 
         String actual = inputView.inputPlayerName();
 
         assertThat(actual).isEqualTo(expect);
     }
 
+    ByteArrayInputStream createInputStream(String name) {
+        return new ByteArrayInputStream(name.getBytes());
+    }
 }
