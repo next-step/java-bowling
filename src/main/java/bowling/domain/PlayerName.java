a93494b7c8e090965727958030d70a1a2b0c171e
ohtaeg <otk1090@naver.com>
2020-04-11 22:02:11 +0900

    feat (PlayerName) : 플레이어 이름 객체 구현

---
+++ src/main/java/bowling/domain/PlayerName.java
@@ -0,0 +1,24 @@
+package bowling.domain;
+
+import java.util.Objects;
+
+public class PlayerName {
+    private final String name;
+
+    public PlayerName(final String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final PlayerName that = (PlayerName) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
