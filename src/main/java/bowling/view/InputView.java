85bdae2a5010b49625697fb821159686d99a6685
ohtaeg <otk1090@naver.com>
2020-04-11 21:57:34 +0900

    refactor (InputView) : InputStream 의존성을 생성자를 통해 주입받도록 수정

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -1,10 +1,23 @@
 package bowling.view;
 
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class InputView {
     private static final String WHAT_IS_YOUR_NAME = "플레이어 이름은(3 english letters)?: ";
 
+    private Scanner scanner;
+
+    public InputView() {
+        this(System.in);
+    }
+
+    public InputView(final InputStream inputStream) {
+        this.scanner = new Scanner(inputStream);
+    }
+
     public String inputPlayerName() {
         System.out.print(WHAT_IS_YOUR_NAME);
-        return "otk";
+        return scanner.nextLine();
     }
 }
