84cd45e87c11893bda34e918e1c6891a8c62fe12
luvram <sahul0804@gmail.com>
2020-04-11 20:59:18 +0900

    feat(player): 참가자 이름을 저장하는 player 추가

---
+++ src/main/java/bowling/domain/Player.java
@@ -0,0 +1,22 @@
+package bowling.domain;
+
+import bowling.domain.exception.IncorrectNameArgumentException;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+public class Player {
+    private static final String INCORRECT_LENGTH_ERROR_MESSAGE =
+            "이름은 영문 세글자로 입력해 주세요.";
+    private static final Pattern namePattern = Pattern.compile("[A-Za-z]{3}");
+
+    private String name;
+
+    public Player(String name) {
+        if (Objects.isNull(name) || !namePattern.matcher(name).matches()) {
+            throw new IncorrectNameArgumentException(
+                    INCORRECT_LENGTH_ERROR_MESSAGE);
+        }
+        this.name = name;
+    }
+}
---
+++ src/test/java/bowling/domain/PlayerTest.java
@@ -0,0 +1,25 @@
+package bowling.domain;
+
+import bowling.domain.exception.IncorrectNameArgumentException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class PlayerTest {
+    @DisplayName("이름은 영문 세글자여야 한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"abc", "ABC", "Abc"})
+    void init(String name) {
+        new Player(name);
+    }
+
+    @DisplayName("이름이 영문 세글자가 아니면 에러가 난다")
+    @ParameterizedTest
+    @ValueSource(strings = {"ab", "abcd", "김보람"})
+    void nameError(String name) {
+        assertThatExceptionOfType(IncorrectNameArgumentException.class)
+                .isThrownBy(() -> new Player(name));
+    }
+}
