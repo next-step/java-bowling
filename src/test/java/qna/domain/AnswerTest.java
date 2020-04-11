4b94da0d994e3c4b7536dc793a190035f0551cd0
Yeoseon Yun <devyyskr@gmail.com>
2020-04-11 22:36:07 +0900

    test(AnswerTest): 다른 사람이 쓴 댓글인지 검증하는 테스트 코드 추가

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,12 +1,21 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
 
     @Test
-    void name() {
+    @DisplayName("다른 사람이 쓴 댓글을 삭제하려고 할 경우 Exceptiond을 반환해야 한다.")
+    void assertIsOtherUsersAnswer() {
+        assertThatThrownBy(() -> {
+            A1.assertUser(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class)
+                .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
     }
 }
