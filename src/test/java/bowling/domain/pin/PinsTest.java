aae19ba9a4e638cd2a8afd85239d4d5f64f5f1f6
ohtaeg <otk1090@naver.com>
2020-04-12 01:33:12 +0900

    test (Pins) : 넘어진 핀의 갯수를 구하는 테스트 추가

--- src/test/java/bowling/domain/pin/PinsTest.java
+++ src/test/java/bowling/domain/pin/PinsTest.java
@@ -1,19 +1,32 @@
 package bowling.domain.pin;
 
+import bowling.domain.DomainGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PinsTest {
+    private DomainGenerator<Pins> pinsGenerator;
+
     @DisplayName("10개의 핀을 가진 Pins 객체를 생성할 수 있다.")
     @Test
     void create() {
-        PinGenerator pinGenerator = new PinGenerator();
-        Pins expect = Pins.of(pinGenerator);
+        pinsGenerator = new PinsGenerator();
+        Pins expect = pinsGenerator.generate();
 
-        Pins actual = Pins.of(pinGenerator);
+        Pins actual = pinsGenerator.generate();
 
         assertThat(actual).isEqualTo(expect);
+    }
+
+    @DisplayName("넘어져있는 3개의 핀의 갯수를 구할 수 있다.")
+    @Test
+    void knockOverCount() {
+        pinsGenerator = new FakeThreePinsGenerator();
+
+        Pins expect = pinsGenerator.generate();
+
+        assertThat(expect.knockOverCount()).isEqualTo(3);
     }
 }
