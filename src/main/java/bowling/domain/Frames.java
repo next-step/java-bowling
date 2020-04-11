c6aecbb0a7cb6c26a73a2cdb737f738ae6e5d1d9
luvram <sahul0804@gmail.com>
2020-04-11 20:16:06 +0900

    refactor(frames): 사이즈를 입력받을 수 있다

--- src/main/java/bowling/domain/Frames.java
+++ src/main/java/bowling/domain/Frames.java
@@ -4,35 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Frames {
-    private static final int SIZE = 10;
     private List<Frame> frameList;
     private Frame currentFrame;
 
-    public Frames() {
+    public Frames(int size) {
         frameList = new ArrayList<>();
-        initFrameList();
+        initFrameList(size);
 
     }
 
-    private void initFrameList() {
+    private void initFrameList(int size) {
         NormalFrame firstFrame = new NormalFrame();
 
         frameList.add(firstFrame);
         currentFrame = firstFrame;
 
         NormalFrame frame = firstFrame;
-        for (int i = 0, sizeWithoutLast = SIZE - 2; i < sizeWithoutLast; i++) {
+        for (int i = 0, sizeWithoutLast = size - 2; i < sizeWithoutLast; i++) {
             frame = frame.createNext();
             frameList.add(frame);
         }
         frameList.add(frame.createFinal());
     }
 
-    public void add(int pinCount) {
+    public Frames add(int pinCount) {
+        if (!isAddable()) {
+            return this;
+        }
+
         currentFrame.addPinCount(pinCount);
-        if (currentFrame.isDone()) {
+        if (currentFrame.isDone() && !currentFrame.isLast()) {
             currentFrame = currentFrame.getNext();
         }
+
+        return this;
     }
 
     public int getCurrentFrameIndex() {
@@ -41,5 +46,9 @@ public class Frames {
 
     public boolean isLast() {
         return currentFrame.isLast();
+    }
+
+    public boolean isAddable() {
+        return !currentFrame.isLast() || !currentFrame.isDone();
     }
 }
--- src/test/java/bowling/domain/FramesTest.java
+++ src/test/java/bowling/domain/FramesTest.java
@@ -7,11 +7,13 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FramesTest {
+    public static final Frames FINISHED_FRAMES = (new Frames(1)).add(1).add(2);
+
     private Frames frames;
 
     @BeforeEach
     void setUp() {
-        frames = new Frames();
+        frames = new Frames(2);
     }
     @DisplayName("프레임에 쓰러트린 핀 갯수를 저장할 수 있다")
     @Test
@@ -23,16 +25,14 @@ public class FramesTest {
     @Test
     void nextFrame() {
         frames.add(10);
-        assertThat(frames.getCurrentFrame()).isEqualTo(1);
+        assertThat(frames.getCurrentFrameIndex()).isEqualTo(1);
     }
 
     @DisplayName("현재 프레임이 마지막인지 여부를 체크한다")
     @Test
     void lastFrame(){
         int dummyPinCount = 10;
-        for(int i = 0; i < 8; i++) {
-            frames.add(dummyPinCount);
-        }
+        frames.add(dummyPinCount);
         assertThat(frames.isLast()).isFalse();
         frames.add(dummyPinCount);
         assertThat(frames.isLast()).isTrue();
