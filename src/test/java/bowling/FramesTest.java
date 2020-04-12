1ab31a25468ba913305bc4490b7cfdbe31e86ceb
LeeSeungHee <palmseung@gmail.com>
2020-04-12 23:45:24 +0900

    Test (Frames) : add test to get frame score when Spare
    * Add FramesTest.returnFrameScoreWhenSpare

--- src/test/java/bowling/FramesTest.java
+++ src/test/java/bowling/FramesTest.java
@@ -25,4 +25,21 @@ public class FramesTest {
         //then
         assertThat(frameScore).isEqualTo(firstPoint + secondPoint);
     }
+
+    @DisplayName("프레임의 결과가 Spare일 때, 프레임의 점수는 다음 투구의 첫 번째 포인트를 합산해 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"9:1", "8:2", "0:10"}, delimiter = ':')
+    void returnFrameScoreWhenSpare(int firstPoint, int secondPoint) {
+        //given
+        Frame frame = new Frame(0, firstPoint, secondPoint);
+        Frame nextFrame = frame.createNextFrame();
+        Frames frames = new Frames(Arrays.asList(frame, nextFrame));
+
+        //when
+        int frameScore = frames.getFrameScore(frame.getFrameId());
+
+        //then
+        assertThat(frameScore)
+                .isEqualTo(firstPoint + secondPoint + nextFrame.getFirstPoint());
+    }
 }
