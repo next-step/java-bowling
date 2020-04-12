4cabfa697c0c867df49678c19607614cb37af46a
gale34 <gales34@gmail.com>
2020-04-12 14:08:31 +0900

    test: add BowlingGameTests

---
+++ src/test/java/bowling/BowlingGameTests.java
@@ -0,0 +1,16 @@
+package bowling;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+@DisplayName("볼링 게임 테스트")
+public class BowlingGameTests {
+
+    @DisplayName("생성 테스트")
+    @Test
+    public void generateTest() {
+        assertThatCode(() -> BowlingFrames.newInstance("paul")).doesNotThrowAnyException();
+    }
+}
