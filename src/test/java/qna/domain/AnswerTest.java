2c769bc9966726c88feb4bd773ecec9f414d0da1
jinwoo <jwlee@qualson.com>
2020-04-12 22:41:39 +0900

    test(Answer) : Add test case for an error that occurs when delete other's

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,21 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("작성자가 쓴 댓글이 아닌 경우 삭제 불가")
+    void errorOnDeleteAnswer() {
+        assertThatThrownBy(() -> {
+            A1.delete(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
+
 }
