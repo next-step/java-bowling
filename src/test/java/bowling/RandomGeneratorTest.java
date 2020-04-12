3622fd5538d9c945e9b51bf9c19fc2b1bdd74dcd
LeeSeungHee <palmseung@gmail.com>
2020-04-12 22:31:43 +0900

    Test (RandomGenerator) : add tests for 10th frame points
    * Add RandomGeneratorTest.generateThirdPoint
    
    * Add RandomGeneratorTest.generateThirdAndFourthPointWhenStrike

--- src/test/java/bowling/RandomGeneratorTest.java
+++ src/test/java/bowling/RandomGeneratorTest.java
@@ -29,4 +29,26 @@ public class RandomGeneratorTest {
                 .forEach(it -> assertThat(randomGenerator.getFirstPoint() + randomGenerator.getSecondPoint())
                         .isLessThanOrEqualTo(10));
     }
+
+    @DisplayName("10회에 발생한 스페어를 위한 세 번째 포인트는 0 이상 10 이하의 숫자로 발생된다.")
+    @Test
+    void generateThirdPoint() {
+        RandomGenerator randomGenerator = new RandomGenerator();
+
+        IntStream.range(0, 1000)
+                .forEach(it -> assertThat(randomGenerator.getThirdPoint())
+                        .isLessThanOrEqualTo(10)
+                        .isGreaterThanOrEqualTo(0));
+    }
+
+    @DisplayName("10회에 발생한 스트라이크를 위한 세 번 포인트와 두 번째 포인트의 합은 10이거나 10보다 작아야 한다.")
+    @Test
+    void generateThirdAndFourthPointWhenStrike() {
+        RandomGenerator randomGenerator = new RandomGenerator();
+
+        IntStream.range(0, 1000)
+                .forEach(it -> assertThat(
+                        randomGenerator.getThirdPointForStrike() + randomGenerator.getFourthPointForStrike())
+                        .isLessThanOrEqualTo(10));
+    }
 }
