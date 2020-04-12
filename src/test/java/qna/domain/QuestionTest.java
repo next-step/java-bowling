a780cab34881121678a2e5785cd35caf39bc7d90
jinwoo <jwlee@qualson.com>
2020-04-12 22:49:26 +0900

    test(QuestionTest) : Add test class for validating user

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,20 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    @DisplayName("본인이 작성한 글이 아닌 글을 삭제할 경우 에러 발생")
+    void errorOnDeleteOthersQuestion() {
+        assertThatThrownBy(() -> {
+            Q1.delete(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
 }
