a98777592c7ac417e0a9ebe80e5357c38d4441ae
LeeSeungHee <palmseung@gmail.com>
2020-04-12 22:35:13 +0900

    Test (RandomGenerator) : add notes to explain point system when Strike

--- src/test/java/bowling/RandomGeneratorTest.java
+++ src/test/java/bowling/RandomGeneratorTest.java
@@ -41,7 +41,12 @@ public class RandomGeneratorTest {
                         .isGreaterThanOrEqualTo(0));
     }
 
-    @DisplayName("10회에 발생한 스트라이크를 위한 세 번 포인트와 두 번째 포인트의 합은 10이거나 10보다 작아야 한다.")
+    /*
+    각 프레임의 첫 투구가 스트라이크면 두 번째 투구는 0으로 설정했기 때문에,
+    본 코드에서는 10프레임의 첫 투구가 스트라이크일 경우,
+    코드 상으로는 총 4회의 투구가 진행되며, 외관상으로는 3회의 투구가 진행되는 것처럼 보인다.
+     */
+    @DisplayName("10회에 발생한 스트라이크를 위한 세 번 포인트와 네 번째 포인트의 합은 10이거나 10보다 작아야 한다.")
     @Test
     void generateThirdAndFourthPointWhenStrike() {
         RandomGenerator randomGenerator = new RandomGenerator();
