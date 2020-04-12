ba5e9b500fdd7ae0ce459e7e96c67562b56fd526
ohtaeg <otk1090@naver.com>
2020-04-12 22:23:02 +0900

    chore (remove class) : 필요없는 클래스 삭제

--- src/main/java/bowling/exception/FrameBowlCountOutOfRangeException.java
+++
@@ -1,9 +0,0 @@
-package bowling.exception;
-
-public class FrameBowlCountOutOfRangeException extends IllegalArgumentException {
-    private static final String MESSAGE = "투구수는 0 ~ 10까지 입니다. : %d";
-
-    public FrameBowlCountOutOfRangeException(int causativeValue) {
-        super(String.format(MESSAGE, causativeValue));
-    }
-}
