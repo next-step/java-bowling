1f24ef8b5a12d2fa9d675079247ba0c258084974
gale34 <gales34@gmail.com>
2020-04-12 12:10:18 +0900

    refactor: edit PinTests

---
+++ src/main/java/bowling/Pins.java
@@ -0,0 +1,14 @@
+package bowling;
+
+public class Pins {
+    public boolean isRemain(int i) {
+        return false;
+    }
+
+    public Score drop(int i) {
+        return false;
+    }
+
+    public void reset() {
+    }
+}
--- src/test/java/bowling/PinsTests.java
+++ src/test/java/bowling/PinsTests.java
@@ -3,24 +3,16 @@ package bowling;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Pins 테스트")
 public class PinsTests {
-    /*
-     * Pins
-     * 볼링 프레임의 남은 볼링 핀 수를 관리한다.
-     * 초기값은 10이며, 최소값은 0이다.
-     * 쓰러트린 핀 수를 입력받으면 남은 볼링 핀수를 갱신하고 Score 값을 반환한다.
-     * 쓰러트린 핀 수가 남은 볼링 핀수보다 많으면 에러를 발생한다.
-     * 남은 볼링 핀 수를 리셋할 수 있다. (다시 10개로)
-     */
 
     @DisplayName("생성 테스트")
     @Test
     public void generatePinsTest() {
-        assertThatCode(() -> new Pins());
+        assertThatCode(Pins::new);
     }
 
     @DisplayName("초기값 테스트")
@@ -36,7 +28,7 @@ public class PinsTests {
     public void dropPinsTest() {
         Pins pins = new Pins();
 
-        assertThatCode(() -> pins.drop(5));
+        assertThat(pins.drop(5)).isEqualTo(Score.of(5));
     }
 
     @DisplayName("핀 쓰러트리기 에러 테스트 ")
