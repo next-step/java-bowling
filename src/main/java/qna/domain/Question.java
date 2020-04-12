b2f3817d63ef32f35415f1e16435a230166180d8
minho <5minhominho@gmail.com>
2020-04-12 09:16:21 +0900

    Step1 리뷰 부탁드립니다! (#98)
    * QnaService 에 있던 삭제 로직을 Question 과 Answer 로 이동
    
    * 일급컬렉션(Answers, DeleteHistories) 사용, Answer 단위 테스트 추가

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -1,8 +1,9 @@
 package qna.domain;
 
-import org.hibernate.annotations.Where;
+import qna.CannotDeleteException;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,10 +19,7 @@ public class Question extends AbstractEntity {
     @JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"))
     private User writer;
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
-    @Where(clause = "deleted = false")
-    @OrderBy("id ASC")
-    private List<Answer> answers = new ArrayList<>();
+    private Answers answers = new Answers();
 
     private boolean deleted = false;
 
@@ -39,24 +37,6 @@ public class Question extends AbstractEntity {
         this.contents = contents;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public Question setTitle(String title) {
-        this.title = title;
-        return this;
-    }
-
-    public String getContents() {
-        return contents;
-    }
-
-    public Question setContents(String contents) {
-        this.contents = contents;
-        return this;
-    }
-
     public User getWriter() {
         return writer;
     }
@@ -71,25 +51,30 @@ public class Question extends AbstractEntity {
         answers.add(answer);
     }
 
-    public boolean isOwner(User loginUser) {
+    private boolean isOwner(User loginUser) {
         return writer.equals(loginUser);
-    }
-
-    public Question setDeleted(boolean deleted) {
-        this.deleted = deleted;
-        return this;
     }
 
     public boolean isDeleted() {
         return deleted;
     }
 
-    public List<Answer> getAnswers() {
-        return answers;
-    }
-
     @Override
     public String toString() {
         return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
+    }
+
+    public DeleteHistories deleteBy(User user) throws CannotDeleteException {
+        if (!isOwner(user)) {
+            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+        }
+
+        this.deleted = true;
+
+        DeleteHistory deleteHistory = new DeleteHistory(ContentType.QUESTION, this.getId(), this.writer, LocalDateTime.now());
+        DeleteHistories deleteHistories = DeleteHistories.of(deleteHistory);
+        deleteHistories.add(answers.deleteBy(user));
+
+        return deleteHistories;
     }
 }
--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,20 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("다른 사람의 작성한 답변은 삭제할 수 없다.")
+    public void deleteAnswerByOther() {
+        assertThatThrownBy(() -> A1.deleteBy(UserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .withFailMessage("다른 사람이 쓴 답변은 삭제할 수 없습니다.");
+    }
 }
