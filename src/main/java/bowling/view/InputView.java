fd3019e522ea4bd9f0ad08beb5d5c2cea068a21c
dongchul <sah3122@naver.com>
2020-04-13 15:19:41 +0900

    feat(InputView): add InputView

---
+++ src/main/java/bowling/view/InputView.java
@@ -0,0 +1,17 @@
+package bowling.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String inputPlayer() {
+        System.out.println("플레이어 이름은(3 english letters)?: ");
+        return scanner.next();
+    }
+
+    public static int relaseBowling(int frameIndex) {
+        System.out.println(String.format("%d프레임 투구 :", frameIndex + 1));
+        return scanner.nextInt();
+    }
+}
