f5ceb75f5409b37c3b3c6206ddd2961d721c1e03
parkeeseul <eeseul.p@gmail.com>
2020-04-14 00:30:07 +0900

    refactor(BowlingException): 미사용 코드 제거

--- src/main/java/bowling/exception/BowlingException.java
+++ src/main/java/bowling/exception/BowlingException.java
@@ -1,11 +1,7 @@
 package bowling.exception;
 
 public class BowlingException extends RuntimeException {
-    private ExceptionType type;
-
     public BowlingException(ExceptionType type) {
         super(type.getErrorMessage());
-
-        this.type = type;
     }
 }
