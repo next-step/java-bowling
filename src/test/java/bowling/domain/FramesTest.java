25b5531df01e4e824521234127515c59bf51599c
luvram <sahul0804@gmail.com>
2020-04-14 00:32:25 +0900

    test(frames): 테스트가 없는 메서드의 테스트 추가

--- src/test/java/bowling/domain/FramesTest.java
+++ src/test/java/bowling/domain/FramesTest.java
@@ -16,11 +16,23 @@ public class FramesTest {
         frames = new Frames(2);
     }
 
+    @DisplayName("지정된 사이즈만큼 생성한다.")
+    @Test
+    void init() {
+        assertThat(frames.size()).isEqualTo(2);
+    }
 
     @DisplayName("프레임 갯수는 최소 2 이상이여야 한다")
     @Test
     void minError() {
         assertThatExceptionOfType(OutOfRangeArgumentException.class)
                 .isThrownBy(() -> new Frames(1));
+    }
+
+    @DisplayName("현재 프레임의 인덱스를 얻어온다.")
+    @Test
+    void index(){
+        Frame first = frames.getFirstFrame();
+        assertThat(frames.getCurrentFrameIndex(first.getNext())).isEqualTo(1);
     }
 }
