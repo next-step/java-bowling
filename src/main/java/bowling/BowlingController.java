5ea92588ae92fed4f594d5907d54581f6fe82fc5
crystal <crystal2840@gmail.com>
2020-04-14 00:43:32 +0900

    Controller에 점수판 초기화 추가

--- src/main/java/bowling/BowlingController.java
+++ src/main/java/bowling/BowlingController.java
@@ -1,13 +1,15 @@
 package bowling;
 
 import bowling.view.InputView;
+import bowling.view.ResultView;
 
 public class BowlingController {
     private static InputView inputView = InputView.getInputView();
+    private static ResultView resultView = ResultView.getResultView();
 
     public static void main(String[] args) {
         String userName = inputView.enterUserName();
-        System.out.println(userName);
+        System.out.println(resultView.initScoreBoard(userName));
 
     }
 }
