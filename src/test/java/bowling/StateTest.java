666e1d117cf1a70a94b8f0fa9f9e494528d9a6c5
Yeoseon Yun <devyyskr@gmail.com>
2020-04-14 22:11:13 +0900

    test(StateTest): Gutter 상태로부터의 play 테스트 코드 추가

--- src/test/java/bowling/StateTest.java
+++ src/test/java/bowling/StateTest.java
@@ -16,8 +16,16 @@ public class StateTest {
     }
 
     @Test
+    @DisplayName("Playing 상태로부터 play를 수행했을 때의 상태")
     void playFromPlaying() {
         assertThat(new Playing(5).play(5)).isInstanceOf(Spare.class);
         assertThat(new Playing(5).play(3)).isInstanceOf(Miss.class);
+    }
+
+    @Test
+    @DisplayName("첫 투구의 Gutter로부터 play를 수행했을 때의 상태")
+    void playFromGutter() {
+        assertThat(new Gutter().play(0)).isInstanceOf(Miss.class);
+        assertThat(new Gutter().play(10)).isInstanceOf(Spare.class);
     }
 }
