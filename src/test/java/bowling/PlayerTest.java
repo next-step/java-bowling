14317c65da62a43f485ca04b463a33095b8cc9c3
Yeoseon Yun <devyyskr@gmail.com>
2020-04-14 12:51:52 +0900

    test(PlayerTest): 플레이어 이름 검증 테스트 추가

---
+++ src/test/java/bowling/PlayerTest.java
@@ -0,0 +1,18 @@
+package bowling;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class PlayerTest {
+
+    @Test
+    @DisplayName("플레이어의 이름은 3글자여야 한다.")
+    void assertPlayerName() {
+        String name = "ye"
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Player(name);
+        }).withMessage("플레이어의 이름은 3글자여야 합니다.");
+    }
+}
