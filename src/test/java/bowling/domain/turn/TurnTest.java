3b91df08b351dfa851181d1d7275870d61c1ef05
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-14 20:27:23 +0900

    test : test 코드 이동

--- src/test/java/bowling/domain/turn/TurnTest.java
+++ src/test/java/bowling/domain/turn/TurnTest.java
@@ -7,15 +7,4 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class TurnTest {
 
-    @DisplayName("현재 상태의 다음 상태를 구한다")
-    @Test
-    public void getNextTurnState_success() throws Exception {
-        //given
-        TurnState first = TurnState.FIRST;
-        TurnState second = TurnState.SECOND;
-
-        //then
-        assertThat(first.getNextTurnState()).isEqualTo(TurnState.SECOND);
-        assertThat(second.getNextTurnState()).isEqualTo(TurnState.THIRD);
-    }
 }
