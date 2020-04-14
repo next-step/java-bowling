669cf488bdce8255fe31f904aa1a1ec2e33e9ffa
Yeoseon Yun <devyyskr@gmail.com>
2020-04-14 22:14:24 +0900

    feat(Gutter): play 메소드 구현

--- src/main/java/bowling/domain/state/Gutter.java
+++ src/main/java/bowling/domain/state/Gutter.java
@@ -1,9 +1,17 @@
 package bowling.domain.state;
 
+import static bowling.Constants.MAX_FELLED_PIN_COUNT;
+import static bowling.Constants.MIN_FELLED_PIN_COUNT;
+
 public class Gutter implements State {
 
     @Override
-    public State play(int felledPin) {
-        return null;
+    public State play(int newFelledPin) {
+
+        if(newFelledPin == MAX_FELLED_PIN_COUNT) {
+            return new Spare(MIN_FELLED_PIN_COUNT, newFelledPin);
+        }
+
+        return new Miss(MIN_FELLED_PIN_COUNT, newFelledPin);
     }
 }
