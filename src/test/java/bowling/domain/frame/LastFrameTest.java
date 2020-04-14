ae702a6398032b84e7bf0cfac65e0bacbac7e92a
dongchul <sah3122@naver.com>
2020-04-15 02:56:09 +0900

    refactor(LastFrame): add setup method

--- src/test/java/bowling/domain/frame/LastFrameTest.java
+++ src/test/java/bowling/domain/frame/LastFrameTest.java
@@ -1,5 +1,6 @@
 package bowling.domain.frame;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -15,6 +16,12 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class LastFrameTest {
+    private LastFrame lastFrame;
+    @BeforeEach
+    void setUp() {
+        lastFrame = new LastFrame(new ArrayList<>());
+    }
+
     @DisplayName("마지막 프레임 생성")
     @Test
     void create() {
@@ -25,7 +32,7 @@ class LastFrameTest {
     @ParameterizedTest
     @MethodSource("points")
     void addPoint(List<Integer> values) {
-        LastFrame lastFrame = new LastFrame(new ArrayList<>());
+        lastFrame = new LastFrame(new ArrayList<>());
 
         assertThatCode(
                 () -> {
@@ -48,7 +55,7 @@ class LastFrameTest {
     @ParameterizedTest
     @MethodSource("overPoints")
     void addPointFailByPointOver(List<Integer> values) {
-        LastFrame lastFrame = new LastFrame(new ArrayList<>());
+        lastFrame = new LastFrame(new ArrayList<>());
 
         assertThatIllegalArgumentException().isThrownBy(
                 () -> {
@@ -69,16 +76,17 @@ class LastFrameTest {
     @DisplayName("볼링공을 던질수 있는 상태")
     @Test
     void isPlayable() {
-        LastFrame lastFrame = new LastFrame(new ArrayList<>());
+        lastFrame = new LastFrame(new ArrayList<>());
         lastFrame.addScore(1);
+
         assertThat(lastFrame.isPlayable()).isTrue();
     }
 
-    @DisplayName("볼링공을 던질수 있는 상태 - 2구 이내 스트라이크 또는 스페어 인 경우")
+    @DisplayName("3구째 볼링공을 던질수 있는 상태 - 2구 이내 스트라이크 또는 스페어 인 경우")
     @ParameterizedTest
     @MethodSource("strikeOrSparePoints")
     void isPlayableByStrikteOrSpare(List<Integer> values) {
-        LastFrame lastFrame = new LastFrame(new ArrayList<>());
+        lastFrame = new LastFrame(new ArrayList<>());
         for (Integer value : values) {
             lastFrame.addScore(value);
         }
