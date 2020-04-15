efb565e40c6b1dbf454905f87ac2b728b23f3591
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-15 21:23:34 +0900

    refactor : State 인터페이스 사용하도록 리팩토링

--- src/main/java/bowling/domain/frame/NormalFrame.java
+++ src/main/java/bowling/domain/frame/NormalFrame.java
@@ -1,7 +1,7 @@
 package bowling.domain.frame;
 
-import bowling.domain.turn.TurnState;
-import bowling.domain.turn.Turns;
+import bowling.domain.frame.state.Ready;
+import bowling.domain.frame.state.State;
 import bowling.exception.BowlingException;
 
 public class NormalFrame implements Frame {
@@ -10,46 +10,24 @@ public class NormalFrame implements Frame {
     private static final String CAN_THROW_TWICE = "1~9번 프레임은 2회 던질수 있습니다.";
     private static final int NEXT_TURN_SIZE = 2;
     private static final int MIN_FRAME_NUMBER = 1;
-    private static final int MAX_FRAME_NUMBER = 10;
 
-    private final Turns turns;
     private final int frameNumber;
     private final Frame nextFrame;
-    private final TurnState turnState;
+    private final State state;
 
     public NormalFrame() {
-        this(new Turns(), MIN_FRAME_NUMBER, null, TurnState.READY);
+        this(MIN_FRAME_NUMBER);
     }
 
     public NormalFrame(final int frameNumber) {
-        this(new Turns(), frameNumber, null, TurnState.READY);
+        this(frameNumber, null, new Ready());
     }
 
-    public NormalFrame(final Turns turns, final int frameNumber, final Frame nextFrame,
-                       final TurnState turnState) {
+    public NormalFrame(final int frameNumber, final Frame nextFrame, final State state) {
         validateFrameNumber(frameNumber);
-        validateTurnCount(turns, frameNumber);
-        this.turns = turns;
         this.frameNumber = frameNumber;
         this.nextFrame = nextFrame;
-        this.turnState = turnState;
-    }
-
-    public static Frame from(final int frameNumber) {
-        if (frameNumber == MAX_FRAME_NUMBER) {
-            return new FinalFrame();
-        }
-
-        return new NormalFrame(frameNumber);
-    }
-
-    public Frame createNext() {
-        if (frameNumber == MAX_FRAME_NUMBER - 1) {
-            return new FinalFrame();
-        }
-
-        NormalFrame next = new NormalFrame(frameNumber + 1);
-        return new NormalFrame(this.turns, this.frameNumber, next, this.turnState);
+        this.state = state;
     }
 
     private void validateFrameNumber(final int frameNumber) {
@@ -58,32 +36,23 @@ public class NormalFrame implements Frame {
         }
     }
 
-    private void validateTurnCount(final Turns turns, final int frameNumber) {
-        if (frameNumber < MAX_FRAME_NUMBER && turns.isMoreTwice()) {
-            throw new BowlingException(CAN_THROW_TWICE);
+    public Frame createNext() {
+        if (frameNumber == MAX_FRAME_NUMBER - 1) {
+            return new FinalFrame();
         }
+
+        NormalFrame next = new NormalFrame(frameNumber + 1);
+        return new NormalFrame(frameNumber, next, state);
     }
 
     @Override
     public Frame bowl(final int pinCount) {
-        if (this.turns.isFinish()) {
-            new NormalFrame(new Turns(), MIN_FRAME_NUMBER,
-                    new NormalFrame(), TurnState.READY);
-        }
-
-        Turns turns = this.turns.bowl(pinCount);
-        TurnState next = turnState.getNextTurnState();
-        Frame frame = null;
-
-        if (turns.size() == NEXT_TURN_SIZE && frameNumber < MAX_FRAME_NUMBER) {
-            frame = NormalFrame.from(frameNumber + MIN_FRAME_NUMBER);
-        }
-
-        return new NormalFrame(turns, frameNumber, frame, next);
+        State bowl = state.bowl(pinCount);
+        return new NormalFrame(frameNumber, nextFrame, bowl);
     }
 
     @Override
-    public Turns getTurns() {
-        return turns;
+    public Frame getNext() {
+        return nextFrame;
     }
 }
--- src/test/java/bowling/domain/frame/NormalFrameTest.java
+++ src/test/java/bowling/domain/frame/NormalFrameTest.java
@@ -1,18 +1,11 @@
 package bowling.domain.frame;
 
-import bowling.domain.turn.Turn;
-import bowling.domain.turn.TurnState;
-import bowling.domain.turn.Turns;
 import bowling.exception.BowlingException;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.Arrays;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NormalFrameTest {
@@ -20,9 +13,9 @@ class NormalFrameTest {
     @DisplayName("1~10번 프레임 까지만 등록 가능 하다")
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
-    public void validate_success(int number) throws Exception {
+    public void validate_success_frameNumber(int number) throws Exception {
         //then
-        new NormalFrame(new Turns(Turn.from(3)), number, null, TurnState.READY);
+        new NormalFrame(number);
     }
 
     @DisplayName("1~10번 외의 프레임 까지만 등록시 exception")
@@ -31,33 +24,7 @@ class NormalFrameTest {
     public void validate_fail(int number) throws Exception {
         //then
         assertThatThrownBy(
-                () -> new NormalFrame(new Turns(Turn.from(3)), number, null,
-                        TurnState.READY)
+                () -> new NormalFrame(number)
         ).isInstanceOf(BowlingException.class);
-    }
-
-    @DisplayName("1~9번 프레임은 2회만 투구 가능하다")
-    @Test
-    public void validate_fail_thirdThrow() throws Exception {
-        //given
-        Turns more = new Turns(Arrays.asList(Turn.from(3), Turn.from(3), Turn.from(3)));
-
-        //then
-        assertThatThrownBy(
-                () -> new NormalFrame(more, 1, null, TurnState.READY)
-        ).isInstanceOf(BowlingException.class);
-    }
-
-    @DisplayName("프레임의 첫 투구 시")
-    @Test
-    public void bowl_success() throws Exception {
-        //given
-        NormalFrame frame = new NormalFrame();
-
-        //when
-        Frame next = frame.bowl(3);
-
-        //then
-        Assertions.assertThat(next.getTurns().size()).isEqualTo(1);
     }
 }
