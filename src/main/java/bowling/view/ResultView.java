5fc7cef566bc380468f6265eaaa6862f3304e9ce
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-15 13:05:02 +0900

    refactor : for -> stream 으로 수정

--- src/main/java/bowling/view/ResultView.java
+++ src/main/java/bowling/view/ResultView.java
@@ -1,5 +1,8 @@
 package bowling.view;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class ResultView {
 
     private static final String VERTICAL = "|";
@@ -7,15 +10,11 @@ public class ResultView {
     private static final String SCORE_BOARD_TOP;
 
     static {
-        StringBuffer buffer = new StringBuffer();
-        buffer.append(String.format("%-3s%s  ", VERTICAL, NAME));
+        String buffer = IntStream.range(1, 11)
+                .mapToObj(i -> String.format("%-3s%02d  ", VERTICAL, i))
+                .collect(Collectors.joining("", String.format("%-3s%s  ", VERTICAL, NAME), VERTICAL));
 
-        for (int i = 1; i < 11; i++) {
-            buffer.append(String.format("%-3s%02d  ", VERTICAL, i));
-        }
-        buffer.append(VERTICAL);
-
-        SCORE_BOARD_TOP = buffer.toString();
+        SCORE_BOARD_TOP = buffer;
     }
 
     public static void printScoreBoardTop() {
