a16161394e63d37f99e3dcd1f3eeaea031dea250
crystal <crystal2840@gmail.com>
2020-04-16 23:42:41 +0900

    parameterizedTest 로 변경

--- src/test/java/bowling/domain/PinTest.java
+++ src/test/java/bowling/domain/PinTest.java
@@ -6,18 +6,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PinTest {
 
     @DisplayName("쓰러 트린 핀에 올바른 값이 들어 왔는지 확인하고 아니라면 예외발생")
-    @Test
-    void validatePin() {
-        assertAll(() -> assertThatIllegalArgumentException().isThrownBy(() -> {
-                      new Pin(-1);
-                  }),
-                  () -> assertThatIllegalArgumentException().isThrownBy(() -> {
-                      new Pin(11);
-                  })
-        );
+    @ParameterizedTest
+    @ValueSource(ints = { -1, 11 })
+    void validatePin(int pin) {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Pin(pin);
+        });
+
     }
 }
