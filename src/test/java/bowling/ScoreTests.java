bac01d1cf9bbf47fd38c8a4a2979e03d8d27e4a0
gale34 <gales34@gmail.com>
2020-04-16 21:29:02 +0900

    test: add ScoreTests

--- src/test/java/bowling/ScoreTests.java
+++ src/test/java/bowling/ScoreTests.java
@@ -5,23 +5,45 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Score 테스트")
 public class ScoreTests {
 
-    @DisplayName("Score 투구 생성 테스트")
+    @DisplayName("Score 생성 테스트")
     @Test
-    public void generateFirstPitchingTest() {
+    public void generateTest() {
         assertThatCode(() -> Score.of(5));
     }
 
     @DisplayName("Score 투구 생성 오류 테스트")
     @ParameterizedTest
     @ValueSource(ints = {-1, 11})
-    public void generateFirstPitchingAbnormalTest(final int score) {
+    public void generateAbnormalTest(final int score) {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> Score.of(score));
+    }
+
+    @DisplayName("Score 단순 합계 테스트")
+    @Test
+    public void sumTest() {
+        assertThat(Score.sum(Arrays.asList(Score.of(6), Score.of(4)))).isEqualTo(10);
+    }
+
+    @DisplayName("Score 비교 테스트")
+    @Test
+    public void compareTest() {
+        Score score = Score.of(7);
+        assertTrue(score.isEqualsTo(7));
+    }
+
+    @DisplayName("Score 변환 테스트")
+    @Test
+    public void convertTest() {
+        Score score = Score.of(7);
+        assertThat(score.toTotalScore()).isEqualTo(TotalScore.of(7));
     }
 }
