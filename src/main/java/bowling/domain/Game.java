78dddefb84c160f655891424c541b216936a9732
crystal <crystal2840@gmail.com>
2020-04-18 06:40:49 +0900

    Game 클래스 및 Frames 클래스 작성

---
+++ src/main/java/bowling/domain/Game.java
@@ -0,0 +1,35 @@
+package bowling.domain;
+
+import java.util.List;
+
+public class Game {
+    private static final int FIRST_FRAME_NUMBER = 1;
+    private String userName;
+    private Frames frames = new Frames();
+
+    public Game(String userName) {
+        this.userName = userName;
+    }
+
+    public Game(String userName, Frames frames) {
+        this.userName = userName;
+        this.frames = frames;
+    }
+
+    public NormalFrame startGame() {
+        NormalFrame normalFrame = new NormalFrame(FIRST_FRAME_NUMBER);
+        return normalFrame;
+    }
+
+    public void addFrame(Frame frame) {
+        frames.addFrame(frame);
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public List<Frame> getFrames() {
+        return frames.getFrames();
+    }
+}
