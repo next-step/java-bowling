9dbfa2532f5937aff5247fab5b9d176f8ed06fdb
crystal <crystal2840@gmail.com>
2020-04-18 06:44:35 +0900

    실행부분 추가

--- src/main/java/bowling/BowlingController.java
+++ src/main/java/bowling/BowlingController.java
@@ -1,13 +1,21 @@
 package bowling;
 
+import bowling.domain.Game;
+import bowling.domain.NormalFrame;
+import bowling.domain.PlayBowling;
 import bowling.view.InputView;
+import bowling.view.ResultView;
 
 public class BowlingController {
     private static InputView inputView = InputView.getInputView();
+    private static ResultView resultView = ResultView.getResultView();
+    private static PlayBowling playBowling = PlayBowling.getPlayBowling();
 
     public static void main(String[] args) {
         String userName = inputView.enterUserName();
-
+        Game game = new Game(userName);
+        resultView.print(game);
+        NormalFrame frame = game.startGame();
+        playBowling.play(game, frame);
     }
-
 }
