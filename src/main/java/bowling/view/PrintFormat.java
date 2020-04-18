230a48ea127bea18b37cbc2ed2780f69043daaf2
LeeSeungHee <palmseung@gmail.com>
2020-04-18 17:46:57 +0900

    Refactor : refactor codes (3)

---
+++ src/main/java/bowling/view/PrintFormat.java
@@ -0,0 +1,11 @@
+package bowling.view;
+
+public class PrintFormat {
+    public static final String BOWLING_FRAME
+            = "|   NAME   |    01    |    02    |    03    |    04    |    05    |    06    |    07    |    08    |    09    |    10    |";
+
+    public static final String NAME = "|    %s   |";
+    public static final String SCORE = "    %s   |";
+    public static final String FIRST_SCORE = "     %s    |";
+
+}
