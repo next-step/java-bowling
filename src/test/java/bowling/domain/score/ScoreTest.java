73eb694f909828ed19009bacc4827ec159d0d251
Yeoseon Yun <devyyskr@gmail.com>
2020-04-18 20:46:30 +0900

    test(ScoreTest): 각 상태별 점수 계산 테스트 추가

--- src/test/java/bowling/domain/score/ScoreTest.java
+++ src/test/java/bowling/domain/score/ScoreTest.java
@@ -1,5 +1,6 @@
 package bowling.domain.score;
 
+import bowling.domain.frame.Frame;
 import bowling.domain.frame.Frames;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,5 +23,45 @@ public class ScoreTest {
 
         assertThat(scoreFirstFrame).isEqualTo(16);
         assertThat(scoreSecondFrame).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("첫 두 투구에서 Spare가 나왔을 경우, 다음 한 투구에 대한 점수를 추가로 더한다.")
+    void getScoreStrike() {
+        Frames frames = new Frames();
+
+        frames.play(pinCount5);
+        frames.play(pinCount5);
+        frames.play(pinCount1);
+
+        int scoreFirstFrame = frames.getScores().get(0);
+        int scoreSecondFrame = frames.getScores().get(1);
+
+        assertThat(scoreFirstFrame).isEqualTo(11);
+        assertThat(scoreSecondFrame).isEqualTo(1);
+    }
+
+    @Test
+    void getScoreMiss() {
+        Frames frames = new Frames();
+
+        frames.play(pinCount1);
+        frames.play(pinCount5);
+
+        int scoreFirstFrame = frames.getScores().get(0);
+
+        assertThat(scoreFirstFrame).isEqualTo(6);
+    }
+
+    @Test
+    void getScoreGutter() {
+        Frames frames = new Frames();
+
+        frames.play(pinCount0);
+        frames.play(pinCount0);
+
+        int scoreFirstFrame = frames.getScores().get(0);
+
+        assertThat(scoreFirstFrame).isEqualTo(0);
     }
 }
