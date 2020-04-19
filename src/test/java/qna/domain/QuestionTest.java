f53b3a71b091fa3e23f6bf8c02c3014f9ad6cf8e
etff <wish2rich@gmail.com>
2020-04-19 15:05:57 +0900

    feature(Answer): 본인체크 테스트 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,20 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @DisplayName("글 작성자과 사용자인지 테스트")
+    @Test
+    void testArticleOwn() throws CannotDeleteException {
+        assertThatThrownBy(() -> {
+            Q1.isOwner(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
 }
