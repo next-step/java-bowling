e92de2900bc947b7de76861dfa6bc299902a9a95
luvram <sahul0804@gmail.com>
2020-04-19 21:06:16 +0900

    test(frame pitch): 신규 메서드의 테스트 추가

--- src/test/java/bowling/domain/frame/FramePitchTest.java
+++ src/test/java/bowling/domain/frame/FramePitchTest.java
@@ -26,4 +26,42 @@ public class FramePitchTest {
         assertThat(firstScore.isPresent()).isTrue();
         assertThat(firstScore.get().getScore()).isEqualTo(1);
     }
+
+    @DisplayName("첫번째 투구가 스트라이크인지 체크한다.")
+    @Test
+    void checkFirst() {
+        FramePitch strikePitch = new FramePitch();
+        strikePitch.add(10);
+        assertThat(strikePitch.isFirstPitchStrike()).isTrue();
+
+        FramePitch normalPitch = new FramePitch();
+        normalPitch.add(9);
+        assertThat(normalPitch.isFirstPitchStrike()).isFalse();
+    }
+
+    @DisplayName("두번째 투구의 점수를 구할 수 있다.")
+    @Test
+    void getSecond() {
+        FramePitch framePitch = new FramePitch();
+        framePitch.add(1);
+        framePitch.add(2);
+        Optional<Score> secondScore = framePitch.getSecondPitchScore();
+        assertThat(secondScore.isPresent()).isTrue();
+        assertThat(secondScore.get().getScore()).isEqualTo(2);
+    }
+
+    @DisplayName("두번째 투구가 스페어인지 체크한다.")
+    @Test
+    void checkSecond() {
+        FramePitch strikePitch = new FramePitch();
+        strikePitch.add(1);
+        strikePitch.add(9);
+        assertThat(strikePitch.isSecondPitchSpare()).isTrue();
+
+        FramePitch normalPitch = new FramePitch();
+        normalPitch.add(1);
+        normalPitch.add(8);
+        assertThat(normalPitch.isSecondPitchSpare()).isFalse();
+    }
+
 }
