640d075263650692ff09cb24873ea557a0cc6e52
ohtaeg <otk1090@naver.com>
2020-04-20 00:24:02 +0900

    test (PlayerCount) : 플레이어 횟수를 입력받는 테스트 추가

--- src/test/java/bowling/view/InputViewTest.java
+++ src/test/java/bowling/view/InputViewTest.java
@@ -1,6 +1,9 @@
 package bowling.view;
 
+import bowling.domain.frame.FrameNumber;
+import bowling.domain.pin.BowlCount;
 import bowling.domain.player.Player;
+import bowling.domain.player.PlayerCount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +23,30 @@ class InputViewTest {
         inputView = new InputView(createInputStream(name));
 
         Player actual = inputView.inputPlayerName();
+
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    @DisplayName("투구수를 입력받을 수 있다.")
+    @Test
+    void inputBowlCount() {
+        String bowlCount = "5";
+        BowlCount expect = new BowlCount(bowlCount);
+        inputView = new InputView(createInputStream(bowlCount));
+
+        BowlCount actual = inputView.inputBowlCount(FrameNumber.ofFirst());
+
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    @DisplayName("플레이어 수를 입력받을 수 있다.")
+    @Test
+    void inputPlayerCount() {
+        String playerCount = "2";
+        PlayerCount expect = PlayerCount.valueOf(playerCount);
+        inputView = new InputView(createInputStream(playerCount));
+
+        PlayerCount actual = inputView.inputPlayerCount();
 
         assertThat(actual).isEqualTo(expect);
     }
