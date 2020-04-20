da4b265ae1bcba55b9c406eea254b95543ec1218
gale34 <dev.gales34@gmail.com>
2020-04-20 11:52:13 +0900

    test: add n player tests

--- src/test/java/bowling/game/BowlingGameTests.java
+++ src/test/java/bowling/game/BowlingGameTests.java
@@ -5,6 +5,7 @@ import bowling.Pin;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
@@ -13,29 +14,45 @@ import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 @DisplayName("볼링 게임 테스트")
 public class BowlingGameTests {
 
+    private BowlingGame bowlingGame = BowlingGame.newInstance(Arrays.asList("AAA", "BBB"));
+
     @DisplayName("생성 테스트")
     @Test
     public void generateTest() {
-        assertThatCode(() -> BowlingGame.newInstance("ABC")).doesNotThrowAnyException();
+        assertThatCode(() -> BowlingGame.newInstance(Arrays.asList("AAA", "BBB")).doesNotThrowAnyException();
     }
 
     @DisplayName("투구 테스트")
     @Test
     public void bowlTest() {
-        BowlingGame bowlingGame = BowlingGame.newInstance("ABC");
-        assertThatCode(() -> bowlingGame.bowl(Pin.ofMax())).doesNotThrowAnyException();
+        assertThatCode(() -> bowlingGame.bowl("AAA", Pin.ofMax())).doesNotThrowAnyException();
     }
 
 
     @DisplayName("게임 투구 테스트 - 에러")
     @Test
     public void bowlAbnormalTest() {
-        BowlingGame bowlingGame = BowlingGame.newInstance("ABC");
         IntStream.range(0, 11)
-                .forEach(i -> bowlingGame.bowl(Pin.ofMax()));
+                .forEach(i -> bowlingGame.bowl("AAA", Pin.ofMax()));
 
 
         assertThatIllegalStateException()
-                .isThrownBy(() -> bowlingGame.bowl(Pin.ofMax()));
+                .isThrownBy(() -> bowlingGame.bowl("AAA", Pin.ofMax()));
+    }
+
+    @DisplayName("사용자 턴 테스트")
+    @Test
+    public void getNextTurnTest() {
+        assertThat(bowlingGame.getNextTurn()).isEqualsTo("AAA");
+        bowlingGame.bowl("AAA", Pin.ofMax());
+        assertThat(bowlingGame.getNextTurn()).isEqualsTo("BBB");
+    }
+
+    @DisplayName("사용자 턴 테스트2")
+    @Test
+    public void getNextTurnTest2() {
+        assertThat(bowlingGame.getNextTurn()).isEqualsTo("AAA");
+        bowlingGame.bowl("AAA", Pin.of(7));
+        assertThat(bowlingGame.getNextTurn()).isEqualsTo("BBB");
     }
 }
