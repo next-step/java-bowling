e634a9bc7c8ff1834624368a0f47d4abfff2d36e
lucas <lucas@zoyi.co>
2020-04-22 12:30:53 +0900

    test: Add test for score feature

--- src/main/java/bowling/domain/Score.java
+++ src/main/java/bowling/domain/Score.java
@@ -1,5 +1,45 @@
 package bowling.domain;
 
+import java.util.Objects;
+
 public class Score {
 
+  private static final Score NULL = new Score();
+
+  private int score;
+
+  private Score() {}
+
+  private Score(int score) {
+    this.score = score;
+  }
+
+  public static Score ofNull() {
+    return NULL;
+  }
+
+  public static Score of(int score) {
+    return new Score(score);
+  }
+
+  public int getScore() {
+    return score;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Score score1 = (Score) o;
+    return score == score1.score;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(score);
+  }
 }
--- src/test/java/bowling/domain/NormalFrameTest.java
+++ src/test/java/bowling/domain/NormalFrameTest.java
@@ -4,52 +4,128 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import bowling.exception.CannotBowlException;
-import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class NormalFrameTest {
+  private static final String DELIMITER = ",";
 
   @ParameterizedTest
-  @CsvSource(value = {"0,6=-|6", "3,5=3|5", "3,0=3|-"}, delimiter = '=')
-  public void testMiss(String pins, String visualized) throws CannotBowlException {
+  @CsvSource(value = {"0,6", "3,5", "3,0"})
+  public void testMiss(String firstTrial, String secondTrial) throws CannotBowlException {
     NormalFrame normalFrame = NormalFrame.initialize();
-    String[] pinCounts = pins.split(",");
+    int first = Integer.parseInt(firstTrial);
+    int second = Integer.parseInt(secondTrial);
+    int score = first + second;
 
-    normalFrame.roll(Integer.parseInt(pinCounts[0]));
-    Frame newFrame = normalFrame.roll(Integer.parseInt(pinCounts[1]));
+    normalFrame.roll(first);
+    assertThat(normalFrame.calculateScore()).isEqualTo(Score.ofNull());
 
+    normalFrame.roll(second);
+    assertThat(normalFrame.calculateScore()).isEqualTo(Score.of(score));
     assertThatThrownBy(() -> normalFrame.roll(3))
         .isInstanceOf(CannotBowlException.class);
   }
 
   @ParameterizedTest
-  @CsvSource(value = {"0,10=-|/", "3,7=3|/"}, delimiter = '=')
-  public void testSpare(String pins, String visualized) throws CannotBowlException {
+  @CsvSource(value = {"0,10,3", "3,7,10"})
+  public void testSpare(String firstTrial, String secondTrial, String thirdTrial)
+      throws CannotBowlException
+  {
     NormalFrame normalFrame = NormalFrame.initialize();
-    String[] pinCounts = pins.split(",");
+    int first = Integer.parseInt(firstTrial);
+    int second = Integer.parseInt(secondTrial);
+    int third = Integer.parseInt(thirdTrial);
+    int score = first + second + third;
 
-    normalFrame.roll(Integer.parseInt(pinCounts[0]));
-    Frame newFrame = normalFrame.roll(Integer.parseInt(pinCounts[1]));
-
+    normalFrame.roll(first);
+    Frame newFrame = normalFrame.roll(second);
     assertThatThrownBy(() -> normalFrame.roll(3))
         .isInstanceOf(CannotBowlException.class);
+
+    newFrame.roll(third);
+    assertThat(normalFrame.calculateScore()).isEqualTo(Score.of(score));
   }
 
-  @Test
-  public void testStrike() throws CannotBowlException {
+  @ParameterizedTest
+  @CsvSource(value = {"0,10", "3,5", "6,4"})
+  public void testMAX_PIN(String firstTrial, String secondTrial) throws CannotBowlException {
     NormalFrame normalFrame = NormalFrame.initialize();
+    int first = Integer.parseInt(firstTrial);
+    int second = Integer.parseInt(secondTrial);
+    int score = first + second + PinCount.MAX_PIN;
+
+    Frame newFrame = normalFrame.roll(PinCount.MAX_PIN);
+    assertThat(normalFrame.calculateScore()).isEqualTo(Score.ofNull());
+    assertThatThrownBy(() -> normalFrame.roll(3))
+        .isInstanceOf(CannotBowlException.class);
+
+    newFrame.roll(first);
+    assertThat(normalFrame.calculateScore()).isEqualTo(Score.ofNull());
+
+    newFrame.roll(second);
+    assertThat(normalFrame.calculateScore()).isEqualTo(Score.of(score));
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {0, 3, 10})
+  public void testDouble(int pinCount) throws CannotBowlException {
+    NormalFrame normalFrame = NormalFrame.initialize();
+    int score = PinCount.MAX_PIN + PinCount.MAX_PIN + pinCount;
     Frame newFrame = normalFrame.roll(PinCount.MAX_PIN);
 
+    assertThat(normalFrame.calculateScore()).isEqualTo(Score.ofNull());
     assertThatThrownBy(() -> normalFrame.roll(3))
         .isInstanceOf(CannotBowlException.class);
+
+    newFrame = newFrame.roll(PinCount.MAX_PIN);
+    assertThat(normalFrame.calculateScore()).isEqualTo(Score.ofNull());
+
+    newFrame.roll(pinCount);
+    assertThat(normalFrame.calculateScore()).isEqualTo(Score.of(score));
   }
 
-  @Test
-  public void testFinalRound() throws CannotBowlException {
+  @ParameterizedTest
+  @CsvSource(value = {"0,10", "3,5", "6,4"})
+  public void testMAX_PINBeforeFinalRound(String firstTrial, String secondTrial)
+      throws CannotBowlException
+  {
     NormalFrame normalFrame = NormalFrame.of(Round.of(Round.FINAL_ROUND - Round.ROUND_UNIT));
-    Frame newFrame = normalFrame.roll(PinCount.MAX_PIN);
+    int first = Integer.parseInt(firstTrial);
+    int second = Integer.parseInt(secondTrial);
+    int score = first + second + PinCount.MAX_PIN;
 
+    Frame newFrame = normalFrame.roll(PinCount.MAX_PIN);
     assertThat(newFrame instanceof FinalFrame).isTrue();
+    assertThat(normalFrame.calculateScore()).isEqualTo(Score.ofNull());
+
+    newFrame.roll(first);
+    assertThat(normalFrame.calculateScore()).isEqualTo(Score.ofNull());
+
+    newFrame.roll(second);
+    assertThat(normalFrame.calculateScore()).isEqualTo(Score.of(score));
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = {"0,10,3", "3,7,10", "3,7,0", "6,4,5"})
+  public void testSpareBeforeFinalRound(String firstTrial, String secondTrial, String thirdTrial)
+      throws CannotBowlException
+  {
+    NormalFrame normalFrame = NormalFrame.of(Round.of(Round.FINAL_ROUND - Round.ROUND_UNIT));
+    int first = Integer.parseInt(firstTrial);
+    int second = Integer.parseInt(secondTrial);
+    int third = Integer.parseInt(thirdTrial);
+    int score = first + second + third;
+
+    normalFrame.roll(first);
+    assertThat(normalFrame.calculateScore()).isEqualTo(Score.ofNull());
+
+    Frame newFrame = normalFrame.roll(second);
+    assertThat(newFrame instanceof FinalFrame).isTrue();
+    assertThat(normalFrame.calculateScore()).isEqualTo(Score.ofNull());
+
+    newFrame.roll(third);
+    assertThat(normalFrame.calculateScore()).isEqualTo(Score.of(score));
   }
 }
