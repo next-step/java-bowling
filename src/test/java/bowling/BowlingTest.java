095bde34f9f80b50d447c6483e55bc792e7f3d2a
wholemann <wholeman.dev@gmail.com>
2020-04-24 15:37:11 +0900

    test(bowling): 볼을 굴린 결과로 스트라이크를 얻는 테스트를 추가한다

--- src/test/java/bowling/BowlingTest.java
+++ src/test/java/bowling/BowlingTest.java
@@ -1,9 +1,18 @@
 package bowling;
 
-/**
- * Created by wholeman on 21/04/2020
- * Github : http://github.com/wholemann
- * E-Mail : wholeman.dev@gmail.com
- */
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class BowlingTest {
+    @Test
+    @DisplayName("모든 핀이 쓰러지면 스트라이크(X)가 결과로 온다")
+    public void rollAndGetStrikeWhenAllPinsAreFalledFirstTime() {
+        Bowling bowling = new Bowling();
+        List<String> result = bowling.roll(10);
+        assertThat(result.get(0)).isEqualTo("X");
+    }
 }
