3ac99a4b28d024b4b1a264790e30aaa4821574ee
wholemann <wholeman.dev@gmail.com>
2020-04-24 15:43:54 +0900

    test(bowling): 스트라이크 이후 다음 프레임으로 넘어간다

--- src/test/java/bowling/BowlingTest.java
+++ src/test/java/bowling/BowlingTest.java
@@ -9,10 +9,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BowlingTest {
     @Test
-    @DisplayName("모든 핀이 쓰러지면 스트라이크(X)가 결과로 온다")
+    @DisplayName("모든 핀이 쓰러지면 스트라이크(X)가 결과로 오고 그 다음 프레임으로 넘어간다")
     public void rollAndGetStrikeWhenAllPinsAreFalledFirstTime() {
         Bowling bowling = new Bowling();
         List<String> result = bowling.roll(10);
         assertThat(result.get(0)).isEqualTo("X");
+
+        result = bowling.roll(7);
+        assertThat(result.get(1)).isEqualTo("7");
     }
 }
