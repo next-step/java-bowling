c04a1bc3114736afd78b0abfb6dd347aebe445dc
etff <wish2rich@gmail.com>
2020-04-26 17:25:00 +0900

    feature(Player): add player class

---
+++ src/test/java/bowling/domain/PlayerTest.java
@@ -0,0 +1,11 @@
+package bowling.domain;
+
+import org.junit.jupiter.api.Test;
+
+public class PlayerTest {
+
+    @Test
+    public void canCreate() {
+        Player player = new Player();
+    }
+}
