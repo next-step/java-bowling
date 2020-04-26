159bbf0def1c592d7b9bd5e106582533ab622cfc
etff <wish2rich@gmail.com>
2020-04-26 14:37:06 +0900

    feature(Game): add create test

---
+++ src/main/java/bowling/Game.java
@@ -0,0 +1,4 @@
+package bowling;
+
+public class Game {
+}
---
+++ src/test/java/bowling/GameTest.java
@@ -0,0 +1,11 @@
+package bowling;
+
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+
+    @Test
+    public void canCreate() {
+        Game game = new Game();
+    }
+}
