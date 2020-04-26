3b5d9fa30b1df4e9aa744de428a92a87637873b4
wholemann <wholeman.dev@gmail.com>
2020-04-26 16:37:29 +0900

    refactor(bowling): 각 프레임을 나타내는 리스트로 변환한다
    리스트는 프레임을 나타내며 최대 길이 2이다.
    추후에 Frame 클래스로 분리할 수 있다.
    이를 통해 createResult의 난해한 코드를 제거할 수 있다.

--- src/main/java/bowling/Bowling.java
+++ src/main/java/bowling/Bowling.java
@@ -4,38 +4,72 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bowling {
-    private List<Integer> falledPins = new ArrayList<>();
+    public static final int BLANK_FOR_STRIKE = 0;
+    private List<List<Integer>> frames;
 
-    public List<String> roll(int falledPinCount) {
-        falledPins.add(falledPinCount);
-
-        return createResult(falledPins);
+    public Bowling() {
+        this.frames = new ArrayList<>();
     }
 
-    public List<String> createResult(List<Integer> falledPins) {
-        List<String> result = new ArrayList<>();
-        int leftSelectCount = 1;
-        int falledPinsBefore = 0;
+    public List<String> roll(int fallenPinCount) {
+        List<Integer> currentFrame = frames
+                .stream()
+                .filter(frame -> frame.size() != 2)
+                .findFirst().orElse(new ArrayList<>());
 
-        for (int countOfPins : falledPins) {
-            if (countOfPins != 10) {
-                if (leftSelectCount == 0) {
-                    if (falledPinsBefore + countOfPins == 10) {
-                        result.add(falledPinsBefore + "|/");
-                    } else {
-                        result.add(falledPinsBefore + "|" + (countOfPins == 0 ? "-" : countOfPins));
-                    }
-                    leftSelectCount = 1;
+        frames.add(recordFrameResult(currentFrame, fallenPinCount));
+
+        return createResult();
+    }
+
+    private List<Integer> recordFrameResult(List<Integer> currentFrame, int fallenPinCount) {
+        if (isStrike(fallenPinCount)) {
+            currentFrame.add(fallenPinCount);
+            currentFrame.add(BLANK_FOR_STRIKE);
+            return currentFrame;
+        }
+        currentFrame.add(fallenPinCount);
+        return currentFrame;
+    }
+
+    public List<String> createResult() {
+        List<String> result = new ArrayList<>();
+        for (List<Integer> frame : frames) {
+            if (frame.size() == 2) {
+                if (isStrike(frame.get(0))) {
+                    result.add(status(frame.get(0)));
+                } else if (isSpare(frame.get(0), frame.get(1))) {
+                    result.add(status(frame.get(0)) + "|/");
                 } else {
-                    falledPinsBefore = countOfPins;
-                    leftSelectCount = 0;
+                    result.add(status(frame.get(0)) + "|" + status(frame.get(1)));
                 }
             }
-            if (countOfPins == 10) {
-                result.add("X");
+            if (frame.size() == 1) {
+                result.add(status(frame.get(0)));
             }
         }
 
         return result;
+    }
+
+    private String status(int countOfPins) {
+        if (isStrike(countOfPins)) {
+            return "X";
+        }
+        if (countOfPins == 0) {
+            return "-";
+        }
+        return countOfPins + "";
+    }
+
+    private boolean isStrike(int countOfPins) {
+        return countOfPins == 10;
+    }
+
+    private boolean isSpare(int first, int second) {
+        if (isStrike(first)) {
+            return false;
+        }
+        return first + second == 10;
     }
 }
--- src/test/java/bowling/BowlingTest.java
+++ src/test/java/bowling/BowlingTest.java
@@ -11,20 +11,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BowlingTest {
     @Test
     @DisplayName("모든 핀이 쓰러지면 스트라이크(X)가 결과로 오고 그 다음 프레임으로 넘어간다")
-    public void rollAndGetStrikeWhenAllPinsAreFalledFirstTime() {
+    public void rollAndGetStrikeWhenAllPinsAreFallenFirstTime() {
         Bowling bowling = new Bowling();
         List<String> result = bowling.roll(10);
         assertThat(result.get(0)).isEqualTo("X");
 
         result = bowling.roll(7);
         assertThat(result.get(1)).isEqualTo("7");
+
+        result = bowling.roll(3);
+        assertThat(result.get(1)).isEqualTo("7|/");
     }
 
     @Test
     @DisplayName("현재까지 진행된 프레임의 결과를 모두 가져온다")
-    void createResultByFalledPinsCounts() {
+    void createResultByFallenPinsCounts() {
         Bowling bowling = new Bowling();
-        List<String> result = bowling.createResult(Arrays.asList(10, 7, 3, 1, 8, 6, 0));
+        bowling.roll(10);
+        bowling.roll(7);
+        bowling.roll(3);
+        bowling.roll(1);
+        bowling.roll(8);
+        bowling.roll(6);
+        bowling.roll(0);
+
+        List<String> result = bowling.createResult();
 
         assertThat(result.get(0)).isEqualTo("X");
         assertThat(result.get(1)).isEqualTo("7|/");
