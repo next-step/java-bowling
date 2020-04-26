b2a4ad6a10451c881e9469d4150e4052bd58f305
etff <wish2rich@gmail.com>
2020-04-26 18:55:11 +0900

    feature(Pin): add pin test

---
+++ src/main/java/bowling/domain/Pin.java
@@ -0,0 +1,9 @@
+package bowling.domain;
+
+public class Pin {
+    private final int pin;
+
+    public Pin(final int pin) {
+        this.pin = pin;
+    }
+}
---
+++ src/test/java/bowling/domain/PinTest.java
@@ -0,0 +1,11 @@
+package bowling.domain;
+
+import org.junit.jupiter.api.Test;
+
+public class PinTest {
+
+    @Test
+    void canCreate() {
+        Pin pin = new Pin(5);
+    }
+}
