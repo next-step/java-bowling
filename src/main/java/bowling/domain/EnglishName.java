4549d444cc7947542573e8ef09e917faa7f27568
etff <wish2rich@gmail.com>
2020-04-26 18:45:43 +0900

    refactor(EnglishName): introduce constant

--- src/main/java/bowling/domain/EnglishName.java
+++ src/main/java/bowling/domain/EnglishName.java
@@ -3,6 +3,11 @@ package bowling.domain;
 import java.util.regex.Pattern;
 
 public class EnglishName {
+    private static final String ENGLISH_PATTERN = "^[a-zA-Z]*$";
+    private static final String ENGLISH_INPUT_ONLY = "영어만 입력이 입력이 가능합니다.";
+    private static final String ALLOW_THREE_WORD = "3글자까지 입력이 가능합니다.";
+    private static final int MAXIMUM_NAME_LENGHT = 3;
+
     private final String userName;
 
     public EnglishName(final String inputText) {
@@ -11,17 +16,20 @@ public class EnglishName {
 
     public String validateName(final String inputText) {
         if (isNotEnglish(inputText)) {
-            throw new IllegalArgumentException("영어만 입력이 입력이 가능합니다.");
+            throw new IllegalArgumentException(ENGLISH_INPUT_ONLY);
         }
 
-        if (inputText.length() > 3) {
-            throw new IllegalArgumentException("3글자까지 입력이 가능합니다.");
+        if (inputText.length() > MAXIMUM_NAME_LENGHT) {
+            throw new IllegalArgumentException(ALLOW_THREE_WORD);
         }
         return inputText;
     }
 
-    public static boolean isNotEnglish(String inputText) {
-        return !Pattern.matches("^[a-zA-Z]*$", inputText);
+    public static boolean isNotEnglish(final String inputText) {
+        return !Pattern.matches(ENGLISH_PATTERN, inputText);
     }
 
+    public String getUserName() {
+        return userName;
+    }
 }
