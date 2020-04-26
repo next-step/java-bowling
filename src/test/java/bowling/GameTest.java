aaa347867ca4574d0ce4be3862ca2fa5df4be4c4
etff <wish2rich@gmail.com>
2020-04-26 16:01:50 +0900

    feeature(Game): add one spare game

--- src/test/java/bowling/GameTest.java
+++ src/test/java/bowling/GameTest.java
@@ -1,6 +1,6 @@
 package bowling;
 
-import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -27,16 +27,30 @@ public class GameTest {
         assertThat(game.getScore()).isEqualTo(0);
     }
 
-    private void rollMany(int pins, int frames) {
-        for (int i = 0; i < frames; i++) {
-            game.roll(pins);
-        }
-    }
-
     @Test
     public void allOnes() {
         rollMany(1, 20);
         assertThat(game.getScore()).isEqualTo(20);
+    }
+
+    /**
+     * oneSpare game : 1게임 스페어, 2게임:3, 나머지 게임은 거터 게임
+     * (5 + 5 + 3) + 3 = 16
+     */
+    @Ignore
+    @Test
+    public void oneSpare() {
+        game.roll(5);
+        game.roll(5);   // spare
+        game.roll(3);
+        rollMany(17,0);
+        assertThat(game.getScore()).isEqualTo(16);
+    }
+
+    private void rollMany(int pins, int frames) {
+        for (int i = 0; i < frames; i++) {
+            game.roll(pins);
+        }
     }
 
 }
