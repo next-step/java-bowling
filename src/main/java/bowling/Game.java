855269dcd0af4847ef0503c18ee8e1881c9f6dcb
etff <wish2rich@gmail.com>
2020-04-26 15:48:40 +0900

    feature(Game): add gutter game, add all One game

--- src/main/java/bowling/Game.java
+++ src/main/java/bowling/Game.java
@@ -1,6 +1,15 @@
 package bowling;
 
 public class Game {
+    private int score = 0;
+    public Game() {
+    }
+
     public void roll(int pins) {
+        score += pins;
+    }
+
+    public int getScore() {
+        return score;
     }
 }
--- src/test/java/bowling/GameTest.java
+++ src/test/java/bowling/GameTest.java
@@ -1,20 +1,42 @@
 package bowling;
 
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class GameTest {
 
     private Game game;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         game = new Game();
     }
 
     @Test
     public void canRoll() {
-        game = new Game();
         game.roll(0);
     }
+
+    @Test
+    public void gutterGame() {
+        rollMany(0, 20);
+        assertThat(game.getScore()).isEqualTo(0);
+    }
+
+    private void rollMany(int pins, int frames) {
+        for (int i = 0; i < frames; i++) {
+            game.roll(pins);
+        }
+    }
+
+    @Test
+    public void allOnes() {
+        rollMany(1, 20);
+        assertThat(game.getScore()).isEqualTo(20);
+    }
+
 }
