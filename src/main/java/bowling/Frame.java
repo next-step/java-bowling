3adce251989571eef5a9e8156b3f641b45d0af77
wholemann <wholeman.dev@gmail.com>
2020-04-27 17:16:11 +0900

    feat(frame): 스트라이크가 나오면 다음 프레임으로 넘어간다

--- src/main/java/bowling/Frame.java
+++ src/main/java/bowling/Frame.java
@@ -1,11 +1,38 @@
 package bowling;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Frame {
-    public Frame bowl(int i) {
-        return new Frame();
+    private List<Integer> fallenPins = new ArrayList<>();
+    private int number;
+    private boolean isFinished = false;
+
+    public Frame(int number) {
+        this.number = number;
+    }
+
+    public Frame bowl(int fallenPinCount) {
+        fallenPins.add(fallenPinCount);
+        if (isStrike(fallenPinCount)) {
+            isFinished = true;
+        }
+        if (isFinish()) {
+            return new Frame(number + 1);
+        }
+
+        return this;
+    }
+
+    private boolean isStrike(int fallenPinCount) {
+        return fallenPinCount == 10;
+    }
+
+    private boolean isFinish() {
+        return this.isFinished;
     }
 
     public int getNumber() {
-        return 2;
+        return this.number;
     }
 }
--- src/test/java/bowling/FrameTest.java
+++ src/test/java/bowling/FrameTest.java
@@ -8,9 +8,11 @@ public class FrameTest {
 
     @Test
     public void bowlWhenStrike() {
-        Frame frame = new Frame();
+        Frame frame = new Frame(1);
         Frame nextFrame = frame.bowl(10);
-
         assertThat(nextFrame.getNumber()).isEqualTo(2);
+
+        nextFrame = nextFrame.bowl(10);
+        assertThat(nextFrame.getNumber()).isEqualTo(3);
     }
 }
