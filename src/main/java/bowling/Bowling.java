24baf7ee0ab842aa711214cebd000a31d9ffc1ce
wholemann <wholeman.dev@gmail.com>
2020-04-27 12:44:43 +0900

    feat(bowling): 각 프레임의 결과를 얻어올 수 있다

--- src/main/java/bowling/Bowling.java
+++ src/main/java/bowling/Bowling.java
@@ -2,6 +2,7 @@ package bowling;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Bowling {
     public static final int BLANK_FOR_STRIKE = 0;
@@ -12,12 +13,15 @@ public class Bowling {
     }
 
     public List<String> roll(int fallenPinCount) {
-        List<Integer> currentFrame = frames
-                .stream()
+        List<Integer> currentFrame = frames.stream()
                 .filter(frame -> frame.size() != 2)
                 .findFirst().orElse(new ArrayList<>());
 
-        frames.add(recordFrameResult(currentFrame, fallenPinCount));
+        if (currentFrame.isEmpty()) {
+            frames.add(recordFrameResult(currentFrame, fallenPinCount));
+        } else {
+            recordFrameResult(currentFrame, fallenPinCount);
+        }
 
         return createResult();
     }
@@ -29,37 +33,34 @@ public class Bowling {
             return currentFrame;
         }
         currentFrame.add(fallenPinCount);
+
         return currentFrame;
     }
 
     public List<String> createResult() {
         List<String> result = new ArrayList<>();
         for (List<Integer> frame : frames) {
-            if (frame.size() == 2) {
-                if (isStrike(frame.get(0))) {
-                    result.add(status(frame.get(0)));
-                } else if (isSpare(frame.get(0), frame.get(1))) {
-                    result.add(status(frame.get(0)) + "|/");
-                } else {
-                    result.add(status(frame.get(0)) + "|" + status(frame.get(1)));
-                }
-            }
-            if (frame.size() == 1) {
-                result.add(status(frame.get(0)));
-            }
+            result.add(status(frame));
         }
 
         return result;
     }
 
-    private String status(int countOfPins) {
-        if (isStrike(countOfPins)) {
+    private String status(List<Integer> frame) {
+        if (frame.size() != 2) {
+            return frame.get(0) + "";
+        }
+        if (isStrike(frame.get(0))) {
             return "X";
         }
-        if (countOfPins == 0) {
-            return "-";
+        if (isSpare(frame.get(0), frame.get(1))) {
+            return frame.get(0) + "|/";
         }
-        return countOfPins + "";
+        List<String> result = frame.stream()
+                .map(p -> p == 0 ? "-" : String.valueOf(p))
+                .collect(Collectors.toList());
+
+        return result.get(0) + "|" + result.get(1);
     }
 
     private boolean isStrike(int countOfPins) {
