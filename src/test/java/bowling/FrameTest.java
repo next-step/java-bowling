96fbbd59b56f7c35ebca2b58b4279419b879a8ec
wholemann <wholeman.dev@gmail.com>
2020-04-27 17:50:58 +0900

    test(frame): 스페어처리에 대한 테스트를 추가한다

--- src/test/java/bowling/FrameTest.java
+++ src/test/java/bowling/FrameTest.java
@@ -1,5 +1,6 @@
 package bowling;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,12 +8,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class FrameTest {
 
     @Test
-    public void bowlWhenStrike() {
+    @DisplayName("스트라이크가 나오면 다음 프레임으로 넘어간다")
+    public void bowlNextFrameWhenStrike() {
         Frame frame = new Frame(1);
         Frame nextFrame = frame.bowl(10);
         assertThat(nextFrame.getNumber()).isEqualTo(2);
 
         nextFrame = nextFrame.bowl(10);
         assertThat(nextFrame.getNumber()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("스페어 처리 후 다음 프레임으로 넘어간다")
+    void bowlNextFrameWhenSpare() {
+        Frame frame = new Frame(1);
+        Frame nextFrame = frame.bowl(9);
+        assertThat(nextFrame.getNumber()).isEqualTo(1);
+
+        nextFrame = frame.bowl(1);
+        assertThat(nextFrame.getNumber()).isEqualTo(2);
     }
 }
