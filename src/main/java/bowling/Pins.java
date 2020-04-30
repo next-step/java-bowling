8b3d7c2036b415a0fb7d597161a85ef9cc35a7c1
wholemann <wholeman.dev@gmail.com>
2020-05-01 01:17:37 +0900

    feat(pins): 첫번째 시도에 10개 핀이 쓰러지면 스트라이크이다

--- src/main/java/bowling/Pins.java
+++ src/main/java/bowling/Pins.java
@@ -1,12 +1,17 @@
 package bowling;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pins {
+    public static final int MAX_PIN_COUNT = 10;
+    private final List<Integer> fallenPins = new ArrayList<>();
 
-    public void bowl(int i) {
-
+    public void bowl(int count) {
+        fallenPins.add(count);
     }
 
     public boolean isStrike() {
-        return true;
+        return fallenPins.get(0) == MAX_PIN_COUNT;
     }
 }
