ee8283d955885b9e283929b9963ea9dcf671c5d1
wholemann <wholeman.dev@gmail.com>
2020-04-30 15:07:50 +0900

    test(frame): 미스가 나도 2번을 던지면 다음 프레임으로 넘어간다

--- src/test/java/bowling/FrameTest.java
+++ src/test/java/bowling/FrameTest.java
@@ -20,9 +20,20 @@ public class FrameTest {
 
     @Test
     @DisplayName("스페어 처리 후 다음 프레임으로 넘어간다")
-    void bowlNextFrameWhenSpare() {
+    public void bowlNextFrameWhenSpare() {
         Frame frame = new Frame(1);
         Frame nextFrame = frame.bowl(9);
+        assertThat(nextFrame.getNumber()).isEqualTo(1);
+
+        nextFrame = frame.bowl(1);
+        assertThat(nextFrame.getNumber()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("미스가 나도 2번을 던졌다면 다음 프레임으로 넘어간다")
+    public void bowlNextFrameWhenMiss() {
+        Frame frame = new Frame(1);
+        Frame nextFrame = frame.bowl(7);
         assertThat(nextFrame.getNumber()).isEqualTo(1);
 
         nextFrame = frame.bowl(1);
