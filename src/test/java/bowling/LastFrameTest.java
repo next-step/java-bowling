160434917b625acfc35fbec1f4e72141b8dc11b1
wholemann <wholeman.dev@gmail.com>
2020-05-05 02:59:47 +0900

    refactor(lastframe): BeforeEach로 객체 생성 중복을 제거한다

--- src/test/java/bowling/LastFrameTest.java
+++ src/test/java/bowling/LastFrameTest.java
@@ -1,15 +1,22 @@
 package bowling;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LastFrameTest {
+    private LastFrame lastFrame;
+
+    @BeforeEach
+    public void setup() {
+        lastFrame = new LastFrame();
+    }
+
     @Test
     @DisplayName("마지막 프레임에서 미스가 나면 게임이 종료된다")
     public void bowlGameFinishWhenMissed() {
-        LastFrame lastFrame = new LastFrame();
         lastFrame.bowl(8).bowl(1);
 
         assertThat(lastFrame.isGameEnd()).isTrue();
@@ -18,7 +25,6 @@ public class LastFrameTest {
     @Test
     @DisplayName("마지막 프레임에서 스트라이크이면 한 번을 더 투구할 수 있다")
     public void bowlOneMoreWhenStrike() {
-        LastFrame lastFrame = new LastFrame();
         lastFrame.bowl(10);
 
         assertThat(lastFrame.isGameEnd()).isFalse();
@@ -27,7 +33,6 @@ public class LastFrameTest {
     @Test
     @DisplayName("마지막 프레임에서 스페어이면 한 번을 더 투구할 수 있다")
     public void bowlOneMoreWhenSpare() {
-        LastFrame lastFrame = new LastFrame();
         lastFrame.bowl(9).bowl(1);
 
         assertThat(lastFrame.isGameEnd()).isFalse();
@@ -36,7 +41,6 @@ public class LastFrameTest {
     @Test
     @DisplayName("스페어가 나오고 한 번 더 투구하면 게임이 끝난다")
     public void bowlOneMoreGameEndWhenSpare() {
-        LastFrame lastFrame = new LastFrame();
         lastFrame.bowl(9).bowl(1).bowl(7);
 
         assertThat(lastFrame.isGameEnd()).isTrue();
