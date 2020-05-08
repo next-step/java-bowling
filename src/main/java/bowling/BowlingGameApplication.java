1255e68051ba5a3befe0935ee6c09b7d8ecdbc9e
wholemann <wholeman.dev@gmail.com>
2020-05-08 12:36:40 +0900

    feat(view): InputView와 ResultView로 분리하여 View를 구현한다

--- src/main/java/bowling/BowlingGameApplication.java
+++ src/main/java/bowling/BowlingGameApplication.java
@@ -1,24 +1,16 @@
 package bowling;
 
-import java.util.Scanner;
+import java.util.Collections;
 
 public class BowlingGameApplication {
     public static void main(String[] args) {
         Bowling bowling = new Bowling();
-
-        System.out.print("플레이어 이름(3 english letters)? : ");
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        System.out.println("| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |");
-        System.out.println(String.format(
-                "|  %s |      |      |      |      |      |      |      |      |      |      |",
-                name
-        ));
+        String name = InputView.inputPlayerName();
+        ResultView.printBoard(name, Collections.emptyList());
 
         while (!bowling.isGameEnd()) {
-            System.out.print(String.format("%d프레임 투구 : ", bowling.getCurrentFrameNumber()));
-            int fallenPins = scanner.nextInt();
-            System.out.println(bowling.roll(fallenPins));
+            int fallenPins = InputView.inputFallenPins(bowling.getCurrentFrameNumber());
+            ResultView.printBoard(name, bowling.roll(fallenPins));
         }
     }
 }
