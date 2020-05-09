aebe501ac95593a4569d78a99a5fcdd0a737e794
kiyeon <kyeoneee@gmail.com>
2020-05-10 01:26:32 +0900

    feat (NormalFrame) : 1 ~ 9 프레임의 투구 기능 추가

--- src/main/java/bowling/domain/frame/NormalFrame.java
+++ src/main/java/bowling/domain/frame/NormalFrame.java
@@ -1,10 +1,32 @@
 package bowling.domain.frame;
 
 public class NormalFrame {
+    private static final int KNOCKED_DOWN_PIN_COUNT_DEFAULT = -1;
+    private static final int KNOCKED_DOWN_PIN_COUNT_STRIKE = 10;
+    private static final int KNOCKED_DOWN_PIN_COUNT_GUTTER = 0;
+
+    private Integer firstRollingResult;
+    private Integer secondRollingResult;
+
+    public NormalFrame() {
+        this.firstRollingResult = KNOCKED_DOWN_PIN_COUNT_DEFAULT;
+        this.secondRollingResult = KNOCKED_DOWN_PIN_COUNT_DEFAULT;
+    }
+
     public void rollingBall(int pinCount) {
+        if (firstRollingResult == KNOCKED_DOWN_PIN_COUNT_DEFAULT) {
+            this.firstRollingResult = pinCount;
+            return;
+        }
+
+        this.secondRollingResult = pinCount;
     }
 
     public boolean isRollable() {
+        if (firstRollingResult == KNOCKED_DOWN_PIN_COUNT_STRIKE) {
+            return false;
+        }
+
         return true;
     }
 }
