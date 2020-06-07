5a79f72c00a538734c55c42f98e2bd1f74512d3e
kiyeon <kyeoneee@gmail.com>
2020-06-08 02:26:34 +0900

    feat (Score) : step3 - State 값에 따른 점수 계산 기능 추가

--- src/main/java/bowling/domain/frame/Score.java
+++ src/main/java/bowling/domain/frame/Score.java
@@ -3,10 +3,27 @@ package bowling.domain.frame;
 import bowling.domain.rolling.State;
 
 public class Score {
+    private static final int DEFAULT_SCORE_ADDABLE_COUNT = 0;
+
+    private int score;
+    private int scoreAddableCount;
+
     public Score(State state, int knockedDownPinCount) {
+        this.scoreAddableCount = DEFAULT_SCORE_ADDABLE_COUNT;
+
+        if (state == State.STRIKE) {
+            this.scoreAddableCount = 2;
+        }
+
+        if (state == State.SPARE) {
+            this.scoreAddableCount = 1;
+        }
+
+        this.score = knockedDownPinCount;
     }
 
     public boolean isCalculateEnd() {
-        return false;
+        return scoreAddableCount == DEFAULT_SCORE_ADDABLE_COUNT;
     }
 }
+
