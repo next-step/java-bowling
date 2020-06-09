eb0771972b2974879fbc9b2618d01284a100d300
junilhwang <tjsdlf4261@naver.com>
2020-06-09 17:25:20 +0900

    feat: [step2] PlayerName과 PlayerNameEmptyException 추가

---
+++ src/main/java/bowling/step2/domain/PlayerName.java
@@ -0,0 +1,38 @@
+/* Copyright (c) 2020 ZUM Internet, Inc.
+ * All right reserved.
+ * http://www.zum.com
+ * This software is the confidential and proprietary information of ZUM
+ * , Inc. You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with ZUM.
+ *
+ * Revision History
+ * Author                    Date                     Description
+ * ------------------       --------------            ------------------
+ *   junil                2020-06-09
+ */
+package bowling.step2.domain;
+
+import bowling.step2.exception.PlayerNameEmptyException;
+
+import java.util.HashMap;
+
+public class PlayerName {
+    private static final HashMap<String, PlayerName> FACTORY = new HashMap<>();
+    private final String value;
+
+    private PlayerName (String value) {
+        this.value = value;
+    }
+
+    public static PlayerName valueOf (String value) {
+        validateEmpty(value);
+        return FACTORY.computeIfAbsent(value, PlayerName::new);
+    }
+
+    public static void validateEmpty (String value) {
+        if (value == null || value.trim().equals("")) {
+            throw new PlayerNameEmptyException();
+        }
+    }
+}
