32cfc26b2b02b80fad55260a7b82649db8aa9b6c
junilhwang <tjsdlf4261@naver.com>
2020-06-09 15:22:44 +0900

    feat: [step1] Answers에 @Embeddable 적용

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -1,19 +1,20 @@
 package qna.domain;
 
+import org.hibernate.annotations.Where;
 import qna.CannotDeleteException;
 
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
+@Embeddable
 public class Answers {
-    private final List<Answer> answers;
 
-    private Answers(List<Answer> answers) {
-        this.answers = answers;
-    }
-
-    public static Answers of (List<Answer> answers) {
-        return new Answers(answers);
-    }
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    @Where(clause = "deleted = false")
+    @OrderBy("id ASC")
+    private final List<Answer> answers = new ArrayList<>();
 
     public void validateDelete (User loginUser) throws CannotDeleteException {
         boolean isMatch = answers.stream()
@@ -21,5 +22,13 @@ public class Answers {
         if (isMatch) {
             throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
         }
+    }
+
+    public void add (Answer answer) {
+        answers.add(answer);
+    }
+
+    public Stream<Answer> stream () {
+        return answers.stream();
     }
 }
