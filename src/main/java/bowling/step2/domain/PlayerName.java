aa63fa415a65b2d49145664568964ba23a8f931d
junilhwang <tjsdlf4261@naver.com>
2020-06-09 17:27:34 +0900

    feat: [step2] PlayerName의 최대 길이 검증 코드 추가

--- src/main/java/bowling/step2/domain/PlayerName.java
+++ src/main/java/bowling/step2/domain/PlayerName.java
@@ -14,6 +14,7 @@
 package bowling.step2.domain;
 
 import bowling.step2.exception.PlayerNameEmptyException;
+import bowling.step2.exception.PlayerNameMaximumException;
 
 import java.util.HashMap;
 
@@ -27,12 +28,19 @@ public class PlayerName {
 
     public static PlayerName valueOf (String value) {
         validateEmpty(value);
+        validateMaximumLength(value);
         return FACTORY.computeIfAbsent(value, PlayerName::new);
     }
 
-    public static void validateEmpty (String value) {
+    private static void validateEmpty (String value) {
         if (value == null || value.trim().equals("")) {
             throw new PlayerNameEmptyException();
+        }
+    }
+
+    private static void validateMaximumLength (String value) {
+        if (value.length() > 3) {
+            throw new PlayerNameMaximumException();
         }
     }
 }
