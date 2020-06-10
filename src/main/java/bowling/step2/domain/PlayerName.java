77c27f9f5e699a504c49d341f2fa2efc0d8f5990
junil-hwang <tjsdlf4261@naver.com>
2020-06-10 20:58:59 +0900

    refactor: [step2] Player -> PlayerName로 변경

---
+++ src/main/java/bowling/step2/domain/PlayerName.java
@@ -0,0 +1,34 @@
+package bowling.step2.domain;
+
+import bowling.step2.exception.PlayerNameEmptyException;
+import bowling.step2.exception.PlayerNameMaximumException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlayerName {
+    private static final Map<String, PlayerName> FACTORY = new HashMap<>();
+    private final String value;
+
+    private PlayerName(String value) {
+        this.value = value;
+    }
+
+    public static PlayerName valueOf (String value) {
+        validateEmpty(value);
+        validateMaximumLength(value);
+        return FACTORY.computeIfAbsent(value, PlayerName::new);
+    }
+
+    private static void validateEmpty (String value) {
+        if (value == null || value.trim().equals("")) {
+            throw new PlayerNameEmptyException();
+        }
+    }
+
+    private static void validateMaximumLength (String value) {
+        if (value.length() > 3) {
+            throw new PlayerNameMaximumException();
+        }
+    }
+}
