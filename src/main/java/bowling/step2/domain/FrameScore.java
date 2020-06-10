5fadb1249cb953c95f865a77a0568226f0a2a939
junilhwang <tjsdlf4261@naver.com>
2020-06-10 14:16:24 +0900

    doc: [step2] README.md 수정

--- src/main/java/bowling/step2/domain/FrameScore.java
+++ src/main/java/bowling/step2/domain/FrameScore.java
@@ -1,4 +1,15 @@
 package bowling.step2.domain;
 
 public class FrameScore {
+    private final Score score;
+    private final boolean spared;
+
+    private FrameScore (Score score, boolean spared) {
+        this.score = score;
+        this.spared = spared;
+    }
+
+    public static FrameScore of (Score score, boolean spared) {
+        return new FrameScore(score, spared);
+    }
 }
