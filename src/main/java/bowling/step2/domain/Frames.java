f78c155b0edb0bf8d6f8be8f40a8d950a65edab9
junilhwang <tjsdlf4261@naver.com>
2020-06-12 09:15:09 +0900

    feat: [step2] PlayerFrames 추가

--- src/main/java/bowling/step2/domain/Frames.java
+++ src/main/java/bowling/step2/domain/Frames.java
@@ -1,5 +1,7 @@
 package bowling.step2.domain;
 
+import bowling.step2.domain.frame.Frame;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
--- src/main/java/bowling/step2/domain/PlayerFrames.java
+++ src/main/java/bowling/step2/domain/PlayerFrames.java
@@ -1,11 +1,27 @@
 package bowling.step2.domain;
 
+import java.util.HashMap;
 import java.util.Map;
 
-public class PlayerFrames {
-    private final Map<PlayerName, Frames> playerFrames;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toMap;
 
-    private PlayerFrames (Map<PlayerName, Frames> playerFrames) {
+public class PlayerFrames {
+    private final Map<Player, Frames> playerFrames;
+
+    private PlayerFrames (Map<Player, Frames> playerFrames) {
         this.playerFrames = playerFrames;
+    }
+
+    public static PlayerFrames of (Map<Player, Frames> playerFrames) {
+        return new PlayerFrames(playerFrames);
+    }
+
+    public static PlayerFrames init (Players players) {
+        return players.stream()
+                      .collect(collectingAndThen(
+                          toMap(player -> player, player -> Frames.init()),
+                          PlayerFrames::of
+                      ));
     }
 }
