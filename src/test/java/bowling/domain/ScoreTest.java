abe28b05e57461e9540f6dfad3afc811123dae02
junilHwang <tjsdlf4261@naver.com>
2020-06-13 21:03:04 +0900

    test: [step2] ScoreTest - 재사용 테스트 및 스트라이크 확인 테스트 완료

--- src/test/java/bowling/domain/ScoreTest.java
+++ src/test/java/bowling/domain/ScoreTest.java
@@ -2,14 +2,12 @@ package bowling.domain;
 
 import bowling.step2.domain.Score;
 import bowling.step2.exception.ScoreRangeException;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ScoreTest {
 
@@ -19,5 +17,29 @@ public class ScoreTest {
     void 스코어_검증_테스트 (int score) {
         assertThatExceptionOfType(ScoreRangeException.class)
             .isThrownBy(() -> Score.valueOf(score));
+    }
+
+    @DisplayName("값 객체의 재사용 여부를 확인")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+    void 스코어_재사용_테스트 (int value) {
+        Score score = Score.valueOf(value);
+        assertEquals(Score.valueOf(value), score);
+    }
+
+    @DisplayName("스코어의 스트라이크 확인 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = { 10 })
+    void 스코어_스트라이크_일치_테스트 (int value) {
+        Score score = Score.valueOf(value);
+        assertEquals(Score.getStrike(), score);
+    }
+
+    @DisplayName("스코어의 스트라이크 불일치 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 })
+    void 스코어_스트라이크_불일치_테스트 (int value) {
+        Score score = Score.valueOf(value);
+        assertEquals(false, score == Score.getStrike());
     }
 }
