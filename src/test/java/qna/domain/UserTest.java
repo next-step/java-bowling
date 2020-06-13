b992e0300a96a697abb675439a7015c73497f615
kwonheejeong <gmlwjd9405@naver.com>
2020-06-14 01:15:36 +0900

    feat: userId 가 같으면 동일한 사용자가 되도록 equals 재정의

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,6 +1,20 @@
 package qna.domain;
 
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class UserTest {
+
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+
+    @DisplayName("userId 가 같으면 동일한 사용자")
+    @Test
+    public void equals() {
+        User other = new User(1L, "javajigi", "1111", "jj", "jj@gmail.com");
+
+        assertThat(JAVAJIGI).isEqualTo(other);
+    }
 }
