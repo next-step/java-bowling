fa4a4bf47fe34a5c4198d0bff6fbb4b6652caa62
junilHwang <tjsdlf4261@naver.com>
2020-06-13 20:42:39 +0900

    feat: [step2] Controller 작성 완료

--- src/main/java/bowling/step2/controller/BowlingController.java
+++ src/main/java/bowling/step2/controller/BowlingController.java
@@ -1,5 +1,6 @@
 package bowling.step2.controller;
 
+import bowling.step2.domain.frame.FinalFrame;
 import bowling.step2.domain.frame.Frame;
 import bowling.step2.domain.frame.NormalFrame;
 import bowling.step2.domain.scores.NormalScores;
@@ -18,18 +19,40 @@ public class BowlingController {
 
     private BowlingController () {}
 
-    public static void newPlayerFrame (Player player, Frame frame) {
+    public static Frame frameView (Frame frame, Player player) {
+        Score score = inputView.inputScore(frame.getValue());
+        Scores scores = frame.getScores().nextInit(score);
+        Frame nextFrame = frame.createNextFrame(scores);
+        resultView.printFrames(PlayerFrames.ofFrame(player, nextFrame));
+        return nextFrame;
+    }
 
+    public static Frame normalFrameView (Frame frame, Player player) {
+        Frame nextFrame = frameView(frame, player);
+        if (!nextFrame.getScores().isStrike()) {
+            nextFrame = frameView(nextFrame, player);
+        }
+        return nextFrame;
+    }
+
+    public static void finalFrameView (Frame frame, Player player) {
+        Frame nextFrame = frameView(frame, player);
+        nextFrame = frameView(nextFrame, player);
+        Scores scores = nextFrame.getScores();
+        if (scores.isStrike() || scores.isSpared()) {
+            frameView(nextFrame, player);
+        }
     }
 
     public static void main(String[] args) {
         // Player player = inputView.inputName();
         Player player = Player.valueOf("hji");
-        PlayerFrames playerFrames = PlayerFrames.init(player);
 
-        resultView.printFrames(playerFrames);
-
-
-
+        resultView.printFrames(PlayerFrames.init(player));
+        Frame temp = NormalFrame.of(1, NormalScores.init(), null);
+        while (temp instanceof NormalFrame) {
+            temp = normalFrameView(temp, player);
+        }
+        finalFrameView(temp, player);
     }
 }
