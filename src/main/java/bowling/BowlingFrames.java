25ac2f0b785c7d2b55d9bd7c2070472b3a15a495
ka0oll <jjyoung0216@naver.com>
2020-06-14 22:11:00 +0900

    feat(BowlingFrames) 구현

---
+++ src/main/java/bowling/BowlingFrames.java
@@ -0,0 +1,51 @@
+package bowling;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BowlingFrames {
+
+    private final List<Frame> frames;
+    private int currentPosition;
+
+    BowlingFrames(List<Frame> frames) {
+        if (frames.size() != 10) {
+            throw new IllegalArgumentException("invalid frames");
+        }
+
+        this.frames = new ArrayList<>(frames);
+    }
+
+    static BowlingFrames newInstance() {
+        List<Frame> frames = new ArrayList<>();
+
+        NormalFrame normalFrame = NormalFrame.first();
+        frames.add(normalFrame);
+
+        for (int i = 1; i < 9; i++) {
+            normalFrame = normalFrame.next();
+            frames.add(normalFrame);
+        }
+
+        frames.add(new FinalFrame());
+
+        return new BowlingFrames(frames);
+    }
+
+    public void play(int downPin) {
+        Frame frame = this.frames.get(this.currentPosition);
+        if (!frame.hasTurn()) {
+            frame = this.frames.get(++this.currentPosition);
+        }
+
+        frame.play(downPin);
+    }
+
+    public List<FrameResult> getResults(){
+        return this.frames.stream()
+            .map(Frame::getResult)
+            .limit(this.currentPosition + 1)
+            .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/bowling/BowlingFramesTest.java
@@ -0,0 +1,38 @@
+package bowling;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BowlingFramesTest {
+
+    @DisplayName("frame이 10개가 아니면 예외가 발생한다.")
+    @Test
+    void frame_size(){
+        List<Frame> frames = Arrays.asList(NormalFrame.first());
+
+        assertThatThrownBy(()->new BowlingFrames(frames))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("프레임들의 결과를 조회한다.")
+    @Test
+    void play(){
+        BowlingFrames bowlingFrames = BowlingFrames.newInstance();
+        bowlingFrames.play(10);
+        bowlingFrames.play(1);
+        bowlingFrames.play(2);
+
+        List<FrameResult> frameResults = bowlingFrames.getResults();
+
+        List<FrameResult> expect= Arrays.asList(
+            new FrameResult(Arrays.asList(10)),
+            new FrameResult(Arrays.asList(1,2))
+        );
+        assertThat(frameResults).isEqualTo(expect);
+    }
+}
