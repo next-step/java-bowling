4df922b73f4c376c06faa43a1e9b6df78102cc14
JuWanHan <baekseongsa@naver.com>
2020-06-15 00:00:51 +0900

    test(QuestionTest): 테스트 코드 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,24 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @DisplayName("삭제 히스토리를 생성할 수 있다.")
+    @Test
+    void canCreateDeleteHistory() {
+        assertThat(Q1.createDeleteHistory()).isInstanceOf(DeleteHistory.class);
+        assertThat(Q1.isDeleted()).isTrue();
+    }
+
+    @DisplayName("답변 리스트를 생성할 수 있다.")
+    @Test
+    void canCreateAnswers() {
+        assertThat(Q1.createAnswers()).isInstanceOf(Answers.class);
+    }
 }
