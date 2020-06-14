fef7dc315387657a6c9ef8b2fc700988e2c9ccdf
kwonheejeong <gmlwjd9405@naver.com>
2020-06-15 02:30:37 +0900

    feat: 질문자와 답변글의 모든 답변자가 같은 경우 모든 답변글 삭제 가능

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -1,7 +1,10 @@
 package qna.domain;
 
+import qna.CannotDeleteException;
+
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Answers {
 
@@ -13,6 +16,25 @@ public class Answers {
 
     public static Answers of(final List<Answer> answers) {
         return new Answers(answers);
+    }
+
+    public List<DeleteHistory> delete(final User user) {
+        verifyDeletable(user);
+
+        return answers.stream()
+                .map(Answer::delete)
+                .collect(Collectors.toList());
+    }
+
+    private void verifyDeletable(final User user) {
+        if (isExistOtherCommenter(user)) {
+            throw new CannotDeleteException("다른 사용자가 답변글을 작성했습니다.");
+        }
+    }
+
+    private boolean isExistOtherCommenter(final User user) {
+        return answers.stream()
+                .anyMatch(answer -> !answer.isOwner(user));
     }
 
     public List<Answer> getAnswers() {
--- src/test/java/qna/domain/AnswersTest.java
+++ src/test/java/qna/domain/AnswersTest.java
@@ -1,31 +1,41 @@
 package qna.domain;
 
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import qna.CannotDeleteException;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class AnswersTest {
 
-    private Answer answer;
-    private Answer otherAnswer;
-
-    @BeforeEach
-    void setUp() {
-        answer = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-        otherAnswer = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
-    }
+    public static final Answers answers = Answers.of(Arrays.asList(AnswerTest.A1, AnswerTest.A2));
 
     @DisplayName("답변 리스트를 반환")
     @Test
     public void getAnswers() {
-        List<Answer> answerList = Arrays.asList(answer, otherAnswer);
+        List<Answer> answerList = Arrays.asList(AnswerTest.A1, AnswerTest.A2);
 
         assertThat(Answers.of(answerList).getAnswers())
-                .containsExactly(answer, otherAnswer);
+                .containsExactly(AnswerTest.A1, AnswerTest.A2);
+    }
+
+    @DisplayName("질문자와 답변글의 모든 답변자가 같지 않은 경우 예외 발생")
+    @Test
+    public void isExistOtherCommenter() {
+        assertThatExceptionOfType(CannotDeleteException.class)
+                .isThrownBy(() -> answers.delete(UserTest.JAVAJIGI));
+    }
+
+    @DisplayName("질문자와 답변글의 모든 답변자가 같은 경우 모든 답변글 삭제 후 DeleteHistory 리스트 반환")
+    @Test
+    public void deleteSuccess() {
+        Answers answers = Answers.of(Arrays.asList(AnswerTest.A1, AnswerTest.A1));
+
+        assertThat(answers.delete(UserTest.JAVAJIGI).size())
+                .isEqualTo(answers.getAnswers().size());
     }
 }
