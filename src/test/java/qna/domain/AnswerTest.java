9bdc7c56f9e4e97c9ceb1d25aa0e6a908e7520c5
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-15 09:52:36 +0900

    test(AnswersTests): deleteTest
    - 테스트 케이스 세분화

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -3,4 +3,9 @@ package qna.domain;
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    public static final Answer A1J = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+    public static final Answer A2J = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents2");
+    public static final Answer A1S = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents1");
+    public static final Answer A2S = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
 }
--- src/test/java/qna/domain/AnswersTests.java
+++ src/test/java/qna/domain/AnswersTests.java
@@ -8,33 +8,35 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static qna.domain.AnswerTest.A1;
-import static qna.domain.AnswerTest.A2;
+import static qna.domain.AnswerTest.A1J;
+import static qna.domain.AnswerTest.A1S;
+import static qna.domain.AnswerTest.A2J;
 
 public class AnswersTests {
-    private List<Answer> twoAnswerList = Arrays.asList(A1, A2);
+    private List<Answer> twoAnswerListSameWithQuestion = Arrays.asList(A1J, A2J);
+    private List<Answer> twoAnswerListDifferentWithQuestion = Arrays.asList(A1J, A1S);
     private List<Answer> emptyAnswerList = new ArrayList<>();
 
     @DisplayName("Answer 콜렉션을 통해 객체를 생성할 수 있다.")
     @Test
     public void createTest() {
-        assertThat(twoAnswerList.size()).isEqualTo(2);
-        Answers answers = new Answers(twoAnswerList);
+        assertThat(twoAnswerListSameWithQuestion.size()).isEqualTo(2);
+        Answers answers = new Answers(twoAnswerListSameWithQuestion);
 
         assertThat(answers).isNotNull();
         assertThat(answers.size()).isEqualTo(2);
     }
 
-    @DisplayName("속한 질문들을 삭제 처리할 수 있다.")
+    @DisplayName("답변의 작성자가 모두 질문자인 경우 전체 삭제가 가능하다.")
     @Test
     public void deleteTest() {
-        assertThat(A1.isDeleted()).isFalse();
-        assertThat(A2.isDeleted()).isFalse();
+        assertThat(A1J.isDeleted()).isFalse();
+        assertThat(A2J.isDeleted()).isFalse();
 
-        Answers answers = new Answers(twoAnswerList);
+        Answers answers = new Answers(twoAnswerListSameWithQuestion);
         answers.delete();
 
-        assertThat(A1.isDeleted()).isTrue();
-        assertThat(A2.isDeleted()).isTrue();
+        assertThat(A1J.isDeleted()).isTrue();
+        assertThat(A2J.isDeleted()).isTrue();
     }
 }
