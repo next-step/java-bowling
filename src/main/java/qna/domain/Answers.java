4087f20e82af3dbe574b7ead21f562a3712b98b1
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-15 17:08:50 +0900

    refactor(Answers): delete
    - 삭제 정합성 확인 메서드 분리

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -17,10 +17,15 @@ public class Answers {
     }
 
     public void delete(User questionUser) throws CannotDeleteException {
+        validateAnswerUser(questionUser);
+        answers.forEach(Answer::delete);
+    }
+
+    private void validateAnswerUser(User questionUser) throws CannotDeleteException {
+        // TODO: 삭제 동작을 Answer에 이관하기
         long count = answers.stream().filter(answer -> !answer.getWriter().equalsNameAndEmail(questionUser)).count();
         if (count > 0) {
             throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
         }
-        answers.forEach(Answer::delete);
     }
 }
