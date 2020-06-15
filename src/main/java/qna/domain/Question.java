fe130e1eb0d27d2b9e0bfb6033c6e8ac7b5c60ad
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-15 09:09:47 +0900

    refactor(Question): delete
    - 메서드 시그니처 변경
    - 로그인한 사용자와 삭제할 질문글의 질문자가 다르면 예외 발생시키는 권한 기능 추가

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -1,6 +1,7 @@
 package qna.domain;
 
 import org.hibernate.annotations.Where;
+import qna.CannotDeleteException;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -39,7 +40,10 @@ public class Question extends AbstractEntity {
         this.contents = contents;
     }
 
-    public void delete() {
+    public void delete(User loginUser) throws CannotDeleteException {
+        if (!loginUser.equalsNameAndEmail(this.writer)) {
+            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+        }
         this.deleted = true;
     }
 
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -2,20 +2,29 @@ package qna.domain;
 
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import qna.CannotDeleteException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static qna.domain.UserTest.*;
 
 public class QuestionTest {
-    public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-    public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+    public static final Question Q1 = new Question("title1", "contents1").writeBy(JAVAJIGI);
+    public static final Question Q2 = new Question("title2", "contents2").writeBy(SANJIGI);
 
     @DisplayName("질문 삭제 시 삭제 상태를 true로 변경한다.")
     @Test
-    public void deleteQuestionTest() {
+    public void deleteQuestionTest() throws CannotDeleteException {
         assertThat(Q1.isDeleted()).isFalse();
 
-        Q1.delete();
-        
+        Q1.delete(JAVAJIGI);
+
         assertThat(Q1.isDeleted()).isTrue();
+    }
+
+    @DisplayName("로그인한 사용자와 질문자가 다른 상태에서 삭제 시도 시 예외 발생 - CannotDeleteException")
+    @Test
+    public void deleteQuestionsAuthTest() {
+        assertThatThrownBy(() -> Q1.delete(SANJIGI)).isInstanceOf(CannotDeleteException.class);
     }
 }
