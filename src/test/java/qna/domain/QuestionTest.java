72d60b9fa17e51aafd929f189dd6a4d22839e186
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-06-15 16:11:02 +0900

    test (QuestionTest) : Question 삭제 요청  주체와 작성자를 확인하는 테스트

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,29 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @DisplayName("Question 삭제요청 주체가 작성자와 동일하면 정상")
+    @Test
+    public void validateQuestionWriter_정상() {
+        assertThatCode(() -> {
+            Q1.validateDeleteRequestor(UserTest.JAVAJIGI);
+        }).doesNotThrowAnyException();
+    }
+
+    @DisplayName("Question 삭제요청 주체가 작성자와 동일하지 않으면 예외 발생")
+    @Test
+    public void validateQuestionWriter_에러() {
+        assertThatCode(() -> {
+            Q1.validateDeleteRequestor(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class)
+                .hasMessageContaining("질문을 삭제할 권한이 없습니다.");
+    }
 }
