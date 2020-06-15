057e91cf3dc0acd03637b4365907424d1de75399
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-06-15 17:21:46 +0900

    feat (DeleteHistoriesGroup) : 질문 삭제 이력 추가 기능 구현

--- src/main/java/qna/domain/DeleteHistoriesGroup.java
+++ src/main/java/qna/domain/DeleteHistoriesGroup.java
@@ -1,9 +1,21 @@
 package qna.domain;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class DeleteHistoriesGroup {
 
     private final List<DeleteHistory> deleteHistories = new ArrayList<>();
+
+    public List<DeleteHistory> getDeleteHistories() {
+        return Collections.unmodifiableList(deleteHistories);
+    }
+
+    public void addQuestionHistory(Question question, long questionId) {
+        DeleteHistory deleteHistory =
+                new DeleteHistory(ContentType.QUESTION, questionId, question.getWriter(), LocalDateTime.now());
+        deleteHistories.add(deleteHistory);
+    }
 }
--- src/test/java/qna/domain/DeleteHistoriesGroupTest.java
+++ src/test/java/qna/domain/DeleteHistoriesGroupTest.java
@@ -3,6 +3,9 @@ package qna.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 class DeleteHistoriesGroupTest {
@@ -13,5 +16,18 @@ class DeleteHistoriesGroupTest {
         assertThatCode(() -> {
             new DeleteHistoriesGroup();
         }).doesNotThrowAnyException();
+    }
+
+    @DisplayName("addQuestionHistory 작업 요청을 받응면 내부 리스트에 Question 삭제 이력을 추가함")
+    @Test
+    public void addDeleteHistory_정상() {
+        DeleteHistoriesGroup deleteHistoriesGroup = new DeleteHistoriesGroup();
+        List<DeleteHistory> deleteHistories = deleteHistoriesGroup.getDeleteHistories();
+
+        assertThat(deleteHistories.size()).isEqualTo(0);
+
+        deleteHistoriesGroup.addQuestionHistory(QuestionTest.Q1, 1);
+
+        assertThat(deleteHistories.size()).isEqualTo(1);
     }
 }
