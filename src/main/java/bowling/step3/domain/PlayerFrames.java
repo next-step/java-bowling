54d15957ae98f46fd75c8fa2e32e38e1e653ef55
junilhwang <tjsdlf4261@naver.com>
2020-06-15 16:33:30 +0900

    refactor: [step3] PlayerFrames 구조 개선

--- src/main/java/bowling/step3/domain/PlayerFrames.java
+++ src/main/java/bowling/step3/domain/PlayerFrames.java
@@ -6,27 +6,19 @@ import java.util.stream.Stream;
 
 public class PlayerFrames {
     private final Player player;
-    private final Frames frames;
+    private final Frame firstFrame;
 
-    private PlayerFrames(Player player, Frames frames) {
+    private PlayerFrames(Player player, Frame firstFrame) {
         this.player = player;
-        this.frames = frames;
+        this.firstFrame = firstFrame;
     }
 
-    public static PlayerFrames of(Player player, Frames frames) {
-        return new PlayerFrames(player, frames);
-    }
-
-    public static PlayerFrames ofFrame(Player player, Frame frame) {
-        return of(player, Frames.ofLastFrame(frame));
-    }
-
-    public static PlayerFrames init(Player player) {
-        return of(player, Frames.init());
+    public static PlayerFrames of(Player player, Frame firstFrame) {
+        return new PlayerFrames(player, firstFrame);
     }
 
     public Stream<Frame> getPreview() {
-        return frames.preview();
+        return Frames.preview(firstFrame);
     }
 
     public Player getPlayer() {
