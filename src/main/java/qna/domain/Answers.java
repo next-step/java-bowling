deb0b0e82a21d47357f9f1175662ec91ce8620da
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-15 17:20:55 +0900

    refactor(Answer): delete
    - 질문자와 답변자가 일치하지 않는 경우 해당 답변을 삭제할 수 없도록 기능 구현

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -1,7 +1,5 @@
 package qna.domain;
 
-import qna.CannotDeleteException;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,16 +14,7 @@ public class Answers {
         return this.answers.size();
     }
 
-    public void delete(User questionUser) throws CannotDeleteException {
-        validateAnswerUser(questionUser);
-        answers.forEach(Answer::delete);
-    }
-
-    private void validateAnswerUser(User questionUser) throws CannotDeleteException {
-        // TODO: 삭제 동작을 Answer에 이관하기
-        long count = answers.stream().filter(answer -> !answer.getWriter().equalsNameAndEmail(questionUser)).count();
-        if (count > 0) {
-            throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
-        }
+    public void delete(User questionUser) {
+        answers.forEach(answer -> answer.delete(questionUser));
     }
 }
--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,5 +1,11 @@
 package qna.domain;
 
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
@@ -8,4 +14,10 @@ public class AnswerTest {
     public static final Answer A2J = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents2");
     public static final Answer A3J = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A1S = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @DisplayName("질문자와 답변자가 다른 답변은 삭제할 수 없다.")
+    @Test
+    public void deleteValidationTest() {
+        assertThatThrownBy(() -> A1.delete(UserTest.SANJIGI)).isInstanceOf(CannotDeleteException.class);
+    }
 }
