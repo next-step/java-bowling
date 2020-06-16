f1ca5dfc4c03f5185be5985f11058f3c73a5a111
junilhwang <tjsdlf4261@naver.com>
2020-06-17 00:49:13 +0900

    test: [step3] NormalScoresTest - Two Strike Score 테스트 완료

--- src/test/java/bowling/step3/domain/NormalFrameTest.java
+++ src/test/java/bowling/step3/domain/NormalFrameTest.java
@@ -1,5 +1,6 @@
 package bowling.step3.domain;
 
+import bowling.step3.domain.frame.FinalFrame;
 import bowling.step3.domain.frame.Frame;
 import bowling.step3.domain.frame.NormalFrame;
 import bowling.step3.domain.scores.FinalScores;
@@ -102,12 +103,17 @@ public class NormalFrameTest {
             ),
             Arguments.of(
                 NormalFrame.of(1, NormalScores.of(Score.valueOf(4), Score.valueOf(6)),
-                    NormalFrame.of(2, FinalScores.of(Score.valueOf(7), null), null)),
+                    FinalFrame.of(2, FinalScores.of(Score.valueOf(7), null))),
                 17
             ),
             Arguments.of(
                 NormalFrame.of(1, NormalScores.of(Score.valueOf(4), Score.valueOf(6)),
-                    NormalFrame.of(2, FinalScores.of(null, null), null)),
+                    NormalFrame.of(2, NormalScores.init(), null)),
+                Frame.EMPTY_CALC
+            ),
+            Arguments.of(
+                NormalFrame.of(9, NormalScores.of(Score.valueOf(4), Score.valueOf(6)),
+                    FinalFrame.of(10, FinalScores.init())),
                 Frame.EMPTY_CALC
             )
         );
@@ -139,7 +145,73 @@ public class NormalFrameTest {
             ),
             Arguments.of(
                 NormalFrame.of(1, NormalScores.of(Score.getStrike(), null),
+                    NormalFrame.of(2, NormalScores.init(), null)),
+                Frame.EMPTY_CALC
+            ),
+            Arguments.of(
+                NormalFrame.of(1, NormalScores.of(Score.getStrike(), null),
                     NormalFrame.of(2, NormalScores.of(Score.valueOf(5), null), null)),
+                Frame.EMPTY_CALC
+            )
+        );
+    }
+
+    @DisplayName("Two Strike 점수 계산 테스트")
+    @ParameterizedTest
+    @MethodSource("provideFrameAndTwoStrikeScore")
+    public void Two_Strike_계산_테스트(NormalFrame frame, int expected) {
+        assertEquals(expected, frame.calculateScore());
+    }
+
+    private static Stream<Arguments> provideFrameAndTwoStrikeScore() {
+        return Stream.of(
+            Arguments.of(
+                NormalFrame.of(1, NormalScores.of(Score.getStrike(), null),
+                    NormalFrame.of(2, NormalScores.of(Score.getStrike(), null),
+                        NormalFrame.of(3, NormalScores.of(Score.getStrike(), null), null))),
+                30
+            ),
+            Arguments.of(
+                NormalFrame.of(1, NormalScores.of(Score.getStrike(), null),
+                    NormalFrame.of(2, NormalScores.of(Score.getStrike(), null),
+                        NormalFrame.of(3, NormalScores.of(Score.valueOf(0), null), null))),
+                20
+            ),
+            Arguments.of(
+                NormalFrame.of(1, NormalScores.of(Score.getStrike(), null),
+                    NormalFrame.of(2, NormalScores.of(Score.getStrike(), null),
+                        NormalFrame.of(3, NormalScores.of(Score.valueOf(5), null), null))),
+                25
+            ),
+            Arguments.of(
+                NormalFrame.of(9, NormalScores.of(Score.getStrike(), null),
+                    FinalFrame.of(10, FinalScores.of(Score.getStrike(), Score.valueOf(9)))),
+                29
+            ),
+            Arguments.of(
+                NormalFrame.of(9, NormalScores.of(Score.getStrike(), null),
+                    FinalFrame.of(10, FinalScores.of(Score.getStrike(), Score.getStrike()))),
+                30
+            ),
+            Arguments.of(
+                NormalFrame.of(1, NormalScores.of(Score.getStrike(), null),
+                    NormalFrame.of(2, NormalScores.init(), null)),
+                Frame.EMPTY_CALC
+            ),
+            Arguments.of(
+                NormalFrame.of(1, NormalScores.of(Score.getStrike(), null),
+                    NormalFrame.of(2, NormalScores.init(),
+                        NormalFrame.of(3, NormalScores.init(), null))),
+                Frame.EMPTY_CALC
+            ),
+            Arguments.of(
+                NormalFrame.of(9, NormalScores.of(Score.getStrike(), null),
+                    FinalFrame.of(10, FinalScores.init())),
+                Frame.EMPTY_CALC
+            ),
+            Arguments.of(
+                NormalFrame.of(9, NormalScores.of(Score.getStrike(), null),
+                    FinalFrame.of(10, FinalScores.of(Score.getStrike(), null))),
                 Frame.EMPTY_CALC
             )
         );
