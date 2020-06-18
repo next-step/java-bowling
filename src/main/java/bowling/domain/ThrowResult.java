233597138cbd761c99c0c396de501cae1179332d
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-18 17:33:59 +0900

    refactor(ThrowResults): validate
    - 전달된 ThrowResult의 총합을 구해서 10을 넘지 않는지 유효성 검증하는 기능 추가

--- src/main/java/bowling/domain/ThrowResult.java
+++ src/main/java/bowling/domain/ThrowResult.java
@@ -4,7 +4,7 @@ import bowling.domain.exceptions.InvalidNumberOfHitPinException;
 
 import java.util.Objects;
 
-public class ThrowResult {
+public class ThrowResult implements Comparable<ThrowResult> {
     private static final int MIN_NUMBER_OF_HIT_PIN = 0;
     private static final int MAX_NUMBER_OF_HIT_PIN = 10;
     private static final String ERROR_COMMENT = "한 번에 맞출수 있는 핀의 수는 0 ~ 10 사이여야 합니다.";
@@ -17,8 +17,8 @@ public class ThrowResult {
         this.numberOfHitPin = numberOfHitPin;
     }
 
-    public int plus(ThrowResult throwResult) {
-        return this.numberOfHitPin + throwResult.numberOfHitPin;
+    public ThrowResult plus(ThrowResult throwResult) {
+        return new ThrowResult(this.numberOfHitPin + throwResult.numberOfHitPin);
     }
 
     private void validate(int numberOfHitPin) {
@@ -38,5 +38,10 @@ public class ThrowResult {
     @Override
     public int hashCode() {
         return Objects.hash(numberOfHitPin);
+    }
+
+    @Override
+    public int compareTo(ThrowResult o) {
+        return Integer.compare(this.numberOfHitPin, o.numberOfHitPin);
     }
 }
--- src/main/java/bowling/domain/ThrowResults.java
+++ src/main/java/bowling/domain/ThrowResults.java
@@ -9,7 +9,7 @@ import java.util.Objects;
 
 public class ThrowResults {
     private static final int MAX_NUMBER_OF_THROW_RESULTS = 2;
-    private static final int MAX_NUMBER_OF_HIT_PINS = 10;
+    private static final ThrowResult MAX_NUMBER_OF_HIT_PINS = new ThrowResult(10);
 
     private final List<ThrowResult> throwResults;
 
@@ -25,7 +25,7 @@ public class ThrowResults {
     }
 
     private void validateNumberOfHitPins(List<ThrowResult> throwResults) {
-        if (throwResults.get(0).plus(throwResults.get(1)) > MAX_NUMBER_OF_HIT_PINS) {
+        if (sumThrowResults(throwResults).compareTo(MAX_NUMBER_OF_HIT_PINS) > 0) {
             throw new InvalidNumberOfHitPinException("쓰러뜨린 핀의 총합은 0 ~ 10을 벗어날 수 없습니다.");
         }
     }
@@ -34,6 +34,11 @@ public class ThrowResults {
         if (throwResults.size() > MAX_NUMBER_OF_THROW_RESULTS) {
             throw new TooManyThrowResultsException("투구 수는 2를 넘을 수 없습니다.");
         }
+    }
+
+    private ThrowResult sumThrowResults(List<ThrowResult> throwResults) {
+        return throwResults.stream()
+                .reduce(new ThrowResult(0), ThrowResult::plus);
     }
 
     @Override
