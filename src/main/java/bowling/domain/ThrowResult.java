3d957d519aa843a0636de7b4f1ef143ba31e4e3a
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-18 16:40:35 +0900

    refactor(ThrowResult): add const value
    - 상수로 관리해야 될 값들을 상수화

--- src/main/java/bowling/domain/ThrowResult.java
+++ src/main/java/bowling/domain/ThrowResult.java
@@ -3,13 +3,21 @@ package bowling.domain;
 import bowling.domain.exceptions.InvalidNumberOfHitPinException;
 
 public class ThrowResult {
+    private static final int MIN_NUMBER_OF_HIT_PIN = 0;
+    private static final int MAX_NUMBER_OF_HIT_PIN = 10;
+    private static final String ERROR_COMMENT = "한 번에 맞출수 있는 핀의 수는 0 ~ 10 사이여야 합니다.";
+
     private final int numberOfHitPin;
 
     public ThrowResult(int numberOfHitPin) {
-        if (numberOfHitPin < 0 || numberOfHitPin > 10) {
-            throw new InvalidNumberOfHitPinException("한 번에 맞출수 있는 핀의 수는 0 ~ 10 사이여야 합니다.");
-        }
+        validate(numberOfHitPin);
 
         this.numberOfHitPin = numberOfHitPin;
+    }
+
+    private void validate(int numberOfHitPin) {
+        if (numberOfHitPin < MIN_NUMBER_OF_HIT_PIN || numberOfHitPin > MAX_NUMBER_OF_HIT_PIN) {
+            throw new InvalidNumberOfHitPinException(ERROR_COMMENT);
+        }
     }
 }
