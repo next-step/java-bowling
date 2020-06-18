eab8e13cabadafe628dc7770d2acb0e39cb9dc8c
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-18 16:38:14 +0900

    tests(ThrowResultTests)
    - JUnit 의존성 수정 (vintage -> jupiter)

--- src/test/java/bowling/domain/ThrowResultTests.java
+++ src/test/java/bowling/domain/ThrowResultTests.java
@@ -1,8 +1,8 @@
 package bowling.domain;
 
 import bowling.domain.exceptions.InvalidNumberOfHitPinException;
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
