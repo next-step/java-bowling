ab936b224ed02c6ac6b5491f9fda4b146ccfa358
junilhwang <tjsdlf4261@naver.com>
2020-06-19 14:35:02 +0900

    test: [step4] PlayerCountTest - PlayersFrames 생성 테스트 완료

--- src/test/java/bowling/step4/domain/PlayerCountTest.java
+++ src/test/java/bowling/step4/domain/PlayerCountTest.java
@@ -1,11 +1,20 @@
 package bowling.step4.domain;
 
+import bowling.step4.domain.frame.NormalFrame;
 import bowling.step4.exception.PlayerMinimumCountException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlayerCountTest {
 
@@ -17,4 +26,28 @@ public class PlayerCountTest {
             .isThrownBy(() -> PlayerCount.of(playerCount));
     }
 
+    @DisplayName("플레이어 수 만큼의 PlayersFrames를 생성하는지 확인")
+    @ParameterizedTest
+    @MethodSource("providePlayersFramesByPlayerCount")
+    void PlayersFrames_생성_테스트(int playerCount, Function<Integer, PlayerFrames> initializer, String expected) {
+        PlayersFrames playersFrames = PlayerCount.of(playerCount).ofPlayersFrames(initializer);
+        assertEquals(
+            expected,
+            playersFrames.stream()
+                         .map(PlayerFrames::getPlayerName)
+                         .collect(joining(";"))
+        );
+    }
+
+    private static Stream<Arguments> providePlayersFramesByPlayerCount() {
+        Function<Integer, PlayerFrames> initializer = i -> {
+            Player player = Player.valueOf("p" + i);
+            return PlayerFrames.of(player, NormalFrame.start());
+        };
+        return Stream.of(
+            Arguments.of(2, initializer, "p1;p2"),
+            Arguments.of(3, initializer, "p1;p2;p3"),
+            Arguments.of(4, initializer, "p1;p2;p3;p4")
+        );
+    }
 }
