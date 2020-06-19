7e8516f7a49511ea419691cab1d48111df5da10d
junilhwang <tjsdlf4261@naver.com>
2020-06-19 14:39:18 +0900

    test: [step4] PlayerFramesTest - 마지막 프레임 가져오기 테스트 완료

--- src/test/java/bowling/step4/domain/PlayerFramesTest.java
+++ src/test/java/bowling/step4/domain/PlayerFramesTest.java
@@ -1,5 +1,6 @@
 package bowling.step4.domain;
 
+import bowling.step4.domain.frame.Frame;
 import bowling.step4.domain.frame.NormalFrame;
 import bowling.step4.domain.scores.NormalScores;
 import org.junit.Test;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlayerFramesTest {
+
     @DisplayName("Frame의 길이와 관계 없이 10개의 프레임을 출력하는지 확인하는 테스트")
     @Test
     public void Preview_테스트 () {
@@ -19,4 +21,15 @@ public class PlayerFramesTest {
             ).getPreview().count()
         );
     }
+
+    @DisplayName("firstFrame을 통해서 lastFrame을 가져오는 테스트")
+    @Test
+    public void lastFrame_가져오기_테스트() {
+        Frame frame3 = NormalFrame.of(3, NormalScores.init(), null);
+        Frame frame2 = NormalFrame.of(2, NormalScores.init(), frame3);
+        Frame firstFrame = NormalFrame.of(Frames.START_FRAME, NormalScores.init(), frame2);
+        PlayerFrames playerFrames = PlayerFrames.of(Player.valueOf("hji"), firstFrame);
+        assertEquals(frame3, playerFrames.getLastFrame());
+    }
+
 }
