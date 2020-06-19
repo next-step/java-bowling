d6c83f9f91cfce7ef6b61a57b545a14c7a6f9de3
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-19 10:11:20 +0900

    feat(NormalFrameResult): validation
    - 첫번째 투구수로 맞출수 있는 핀의 수에 정합성 확인 기능 추가

--- src/main/java/bowling/domain/NormalFrameResult.java
+++ src/main/java/bowling/domain/NormalFrameResult.java
@@ -1,5 +1,7 @@
 package bowling.domain;
 
+import bowling.domain.exceptions.InvalidNumberOfHitPinException;
+
 public class NormalFrameResult implements FrameResult {
     private int firstNumberOfHitPin;
 
@@ -8,6 +10,10 @@ public class NormalFrameResult implements FrameResult {
     }
 
     public static NormalFrameResult firstThrow(int numberOfHitPin) {
+        if (numberOfHitPin < 0 || numberOfHitPin > 9) {
+            throw new InvalidNumberOfHitPinException("맞춘 핀의 수가 0 ~ 9를 벗어났습니다.");
+        }
+
         return new NormalFrameResult(numberOfHitPin);
     }
 
--- src/test/java/bowling/domain/NormalFrameResultTests.java
+++ src/test/java/bowling/domain/NormalFrameResultTests.java
@@ -1,16 +1,28 @@
 package bowling.domain;
 
+import bowling.domain.exceptions.InvalidNumberOfHitPinException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class NormalFrameResultTests {
     @DisplayName("첫번째 투구로 맞춘 핀의 수를 입력해서 객체를 생성할 수 있다.")
     @Test
     void createTest() {
-        int numberOfHitPin = 5;
+        int numberOfHitPin = 9;
         NormalFrameResult normalFrameResult = NormalFrameResult.firstThrow(numberOfHitPin);
         assertThat(normalFrameResult.isStrike()).isFalse();
+    }
+
+    @DisplayName("첫번째 투구로 맞춘 핀의 수는 0 ~ 9의 범위를 벗어날 수 없다.")
+    @ParameterizedTest
+    @ValueSource(ints = { -1, 10 })
+    void createValidationTest(int invalidValue) {
+        assertThatThrownBy(() -> NormalFrameResult.firstThrow(invalidValue))
+                .isInstanceOf(InvalidNumberOfHitPinException.class);
     }
 }
