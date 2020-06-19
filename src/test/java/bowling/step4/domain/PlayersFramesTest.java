d3dd14e66af34b32f9faaae33748385fe9a36208
junilhwang <tjsdlf4261@naver.com>
2020-06-19 14:54:36 +0900

    test: [step4] PlayersFramesTest - 마지막 프레임 확인 테스트 완료

--- src/test/java/bowling/step4/domain/PlayersFramesTest.java
+++ src/test/java/bowling/step4/domain/PlayersFramesTest.java
@@ -1,10 +1,21 @@
 package bowling.step4.domain;
 
+import bowling.step4.domain.frame.FinalFrame;
+import bowling.step4.domain.frame.Frame;
+import bowling.step4.domain.frame.NormalFrame;
+import bowling.step4.domain.scores.FinalScores;
+import bowling.step4.domain.scores.NormalScores;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Collections;
+import java.util.stream.Stream;
+
 import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.joining;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -23,5 +34,15 @@ public class PlayersFramesTest {
             players.stream().count(),
             playersFrames.stream().count()
         );
+    }
+
+    @DisplayName("PlayersFrames에서 frames의 현재 상태가 마지막 프레임인지 확인")
+    @Test
+    void 마지막_프레임_확인_테스트() {
+        Frame finalFrame = FinalFrame.of(2, FinalScores.init());
+        Frame normalFrame = NormalFrame.of(1, NormalScores.init(), finalFrame);
+        PlayerFrames playerFrames = PlayerFrames.of(Player.valueOf("hji"), normalFrame);
+        PlayersFrames playersFrames = PlayersFrames.of(singletonList(playerFrames));
+        assertEquals(true, playersFrames.isLast());
     }
 }
