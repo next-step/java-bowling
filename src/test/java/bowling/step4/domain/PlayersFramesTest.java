b1d963935f4a1b63a69984e4457698fa803a1fe7
junilhwang <tjsdlf4261@naver.com>
2020-06-19 14:48:24 +0900

    test: [step4] PlayersFramesTest - 생성 테스트 완료

--- src/test/java/bowling/step4/domain/PlayersFramesTest.java
+++ src/test/java/bowling/step4/domain/PlayersFramesTest.java
@@ -1,4 +1,27 @@
 package bowling.step4.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.joining;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PlayersFramesTest {
+
+    @DisplayName("Players로 부터 PlayersFrames가 정상적으로 생성되는지 확인")
+    @Test
+    void playersFrames_생성_테스트 () {
+        Players players = Players.of(asList(Player.valueOf("p01"), Player.valueOf("p02")));
+        PlayersFrames playersFrames = PlayersFrames.init(players);
+        assertEquals(
+            players.stream().map(Player::toString).collect(joining(";")),
+            playersFrames.stream().map(PlayerFrames::getPlayerName).collect(joining(";"))
+        );
+        assertEquals(
+            players.stream().count(),
+            playersFrames.stream().count()
+        );
+    }
 }
