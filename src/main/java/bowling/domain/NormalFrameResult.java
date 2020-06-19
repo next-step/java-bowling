75d43e3ea39e7869b66fb170752a8b283a6b31f5
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-19 10:12:47 +0900

    refactor(NormaFrameResult): validate
    - 정합성 확인 기능 별도 메서드로 분리 및 상수화

--- src/main/java/bowling/domain/NormalFrameResult.java
+++ src/main/java/bowling/domain/NormalFrameResult.java
@@ -3,6 +3,9 @@ package bowling.domain;
 import bowling.domain.exceptions.InvalidNumberOfHitPinException;
 
 public class NormalFrameResult implements FrameResult {
+    private static final int MIN_NUMBER_OF_HIT_PIN = 0;
+    private static final int MAX_NUMBER_OF_HIT_PIN = 9;
+
     private int firstNumberOfHitPin;
 
     NormalFrameResult(int firstNumberOfHitPin) {
@@ -10,11 +13,15 @@ public class NormalFrameResult implements FrameResult {
     }
 
     public static NormalFrameResult firstThrow(int numberOfHitPin) {
-        if (numberOfHitPin < 0 || numberOfHitPin > 9) {
-            throw new InvalidNumberOfHitPinException("맞춘 핀의 수가 0 ~ 9를 벗어났습니다.");
-        }
+        validate(numberOfHitPin);
 
         return new NormalFrameResult(numberOfHitPin);
+    }
+
+    private static void validate(int numberOfHitPin) {
+        if (numberOfHitPin < MIN_NUMBER_OF_HIT_PIN || numberOfHitPin > MAX_NUMBER_OF_HIT_PIN) {
+            throw new InvalidNumberOfHitPinException("맞춘 핀의 수가 0 ~ 9를 벗어났습니다.");
+        }
     }
 
     @Override
