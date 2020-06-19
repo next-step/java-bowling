af36046f0a25489b231106f724c39be367398de2
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-19 11:10:25 +0900

    tests(FrameResultFactory): validation test
    - 맞춘 핀 수를 잘못 입력할 경우 정합성 확인 테스트

--- src/test/java/bowling/domain/FrameResultFactoryTests.java
+++ src/test/java/bowling/domain/FrameResultFactoryTests.java
@@ -1,13 +1,16 @@
 package bowling.domain;
 
+import bowling.domain.exceptions.InvalidNumberOfHitPinException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class FrameResultFactoryTests {
     @DisplayName("첫번째 투구로 맞춘 핀의 수에 따라 알맞는 FrameResult를 반환한다.")
@@ -25,5 +28,13 @@ class FrameResultFactoryTests {
                 Arguments.of(5, false, false),
                 Arguments.of(0, false, false)
         );
+    }
+    
+    @DisplayName("0 ~ 10을 벗어난 값으로 생성 시도 시 예외 발생")
+    @ParameterizedTest
+    @ValueSource(ints = { -1, 11 })
+    void createValidationTest(int invalidValue) {
+        assertThatThrownBy(() -> FrameResultFactory.create(invalidValue))
+                .isInstanceOf(InvalidNumberOfHitPinException.class);
     }
 }
