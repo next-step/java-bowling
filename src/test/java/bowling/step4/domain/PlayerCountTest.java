1cd0a3fd621b93be59382952e6756b07bb35f032
junilhwang <tjsdlf4261@naver.com>
2020-06-19 14:25:35 +0900

    test: [step4] PlayerCountTest - 플레이어수 검증 완료

--- src/test/java/bowling/step4/domain/PlayerCountTest.java
+++ src/test/java/bowling/step4/domain/PlayerCountTest.java
@@ -1,17 +1,20 @@
-/* Copyright (c) 2020 ZUM Internet, Inc.
- * All right reserved.
- * http://www.zum.com
- * This software is the confidential and proprietary information of ZUM
- * , Inc. You shall not disclose such Confidential Information and
- * shall use it only in accordance with the terms of the license agreement
- * you entered into with ZUM.
- *
- * Revision History
- * Author                    Date                     Description
- * ------------------       --------------            ------------------
- *   junil                2020-06-19
- */
 package bowling.step4.domain;
 
+import bowling.step4.exception.PlayerMinimumCountException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 public class PlayerCountTest {
+
+    @DisplayName("플레이어의 수가 1 미만일 경우 PlayerMinimumCountException 발생")
+    @ParameterizedTest
+    @ValueSource(ints = { -1, 0 })
+    void 플레이어_수_검증(int playerCount) {
+        assertThatExceptionOfType(PlayerMinimumCountException.class)
+            .isThrownBy(() -> PlayerCount.of(playerCount));
+    }
+
 }
