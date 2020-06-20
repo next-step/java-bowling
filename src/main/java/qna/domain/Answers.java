326916395af786649008772326c13abed60d2712
chulman <chlcjfals0122@gmail.com>
2020-06-20 20:23:31 +0900

    feat (answers) : feat answers

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -1,14 +1,35 @@
 package qna.domain;
 
+import org.hibernate.annotations.Where;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+@Embeddable
 public class Answers {
 
-    private final List<Answer> answers;
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    @Where(clause = "deleted = false")
+    @OrderBy("id ASC")
+    private final List<Answer> answers = new ArrayList<>();
 
-    public Answers(List<Answer> answers) {
-        this.answers = answers;
+    public void add(Answer answer) {
+        answers.add(answer);
+    }
+
+    public List<DeleteHistory> deleteAll(User user) {
+        return answers.stream()
+                .map(answer -> answer.delete(user))
+                .map(Optional::get)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
     }
 
     @Override
