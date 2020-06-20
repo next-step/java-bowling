7a17d19843177a8c5df11f7bce08da6cda1202b0
chulman <chlcjfals0122@gmail.com>
2020-06-20 21:38:01 +0900

    refactor (questionTest) : refactoring questionTest

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,7 @@
 package qna.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -15,7 +16,7 @@ public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
 
-    @MethodSource("provideQuestionByGuest")
+    @MethodSource("provideQuestionByGuestWithAnswers")
     @ParameterizedTest
     @DisplayName("질문 삭제 테스트 - 이미 삭제 된 경우")
     void isDeletedQuestionDelete(Question question) throws CannotDeleteException {
@@ -26,33 +27,27 @@ public class QuestionTest {
         assertThat(deleteHistoryList).hasSize(0);
     }
 
-    @MethodSource("provideQuestionByGuest")
-    @ParameterizedTest
+    @Test
     @DisplayName("질문 삭제 테스트 - 답변이 없는 경우")
-    void deleteWithNoAnswers(Question question) throws CannotDeleteException {
-
+    void deleteWithNoAnswers() throws CannotDeleteException {
+        Question question = new Question("test", "test2").writeBy(User.GUEST_USER);
         List<DeleteHistory> deleteHistoryList = question.delete(User.GUEST_USER);
-
         assertThat(deleteHistoryList).isNotEmpty();
         assertThat(deleteHistoryList).hasSize(1);
     }
 
-    @MethodSource("provideQuestionByGuest")
+    @MethodSource("provideQuestionByGuestWithAnswers")
     @ParameterizedTest
     @DisplayName("질문 삭제 테스트 - 삭제하려고하는 유저가 질문자가 아닌 경우")
     void deleteByAnotherUser(Question question) {
-        assertThatIllegalArgumentException()
+        assertThatExceptionOfType(CannotDeleteException.class)
                 .isThrownBy(() -> question.delete(UserTest.SANJIGI));
     }
 
-    @MethodSource("provideQuestionByGuest")
+    @MethodSource("provideQuestionByGuestWithAnswers")
     @ParameterizedTest
     @DisplayName("질문 삭제 테스트 - 답변의 모든 유저가 질문자와 같은 경우")
     void deleteWithEqualsAllAnswerUser(Question question) throws CannotDeleteException {
-
-        question.addAnswer(new Answer(User.GUEST_USER, question, "답변1"));
-        question.addAnswer(new Answer(User.GUEST_USER, question, "답변2"));
-        question.addAnswer(new Answer(User.GUEST_USER, question, "답변3"));
 
         List<DeleteHistory> deleteHistoryList = question.delete(User.GUEST_USER);
 
@@ -61,23 +56,25 @@ public class QuestionTest {
 
     }
 
-    @MethodSource("provideQuestionByGuest")
+    @MethodSource("provideQuestionByGuestWithAnswers")
     @ParameterizedTest
     @DisplayName("질문 삭제 테스트 - 답변의 모든 유저가 질문자와 같지 경우")
     void deleteWithEqualsAllAnswerUserException(Question question) {
-
-        question.addAnswer(new Answer(User.GUEST_USER, question, "답변1"));
-        question.addAnswer(new Answer(UserTest.JAVAJIGI, question, "답변2"));
-        question.addAnswer(new Answer(User.GUEST_USER, question, "답변3"));
+        question.addAnswer(new Answer(UserTest.JAVAJIGI, question, "답변4"));
 
         assertThatExceptionOfType(CannotDeleteException.class)
                 .isThrownBy(() -> question.delete(User.GUEST_USER));
 
     }
 
-    private static Stream<Arguments> provideQuestionByGuest() {
+    private static Stream<Arguments> provideQuestionByGuestWithAnswers() {
+        Question question = new Question("title2", "contents2").writeBy(User.GUEST_USER);
+        question.addAnswer(new Answer(User.GUEST_USER, question, "답변1"));
+        question.addAnswer(new Answer(User.GUEST_USER, question, "답변2"));
+        question.addAnswer(new Answer(User.GUEST_USER, question, "답변3"));
+
         return Stream.of(
-                Arguments.of(new Question("title2", "contents2").writeBy(User.GUEST_USER))
+                Arguments.of(question)
         );
     }
 }
