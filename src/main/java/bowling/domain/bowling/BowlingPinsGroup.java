0b7a8675cdb1fe16ee95c8327be209b71ed7ca9b
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-06-21 16:56:30 +0900

    test & feat (BowlingPinsGroup ) : hitCounts 예외 추가

--- src/main/java/bowling/domain/bowling/BowlingPinsGroup.java
+++ src/main/java/bowling/domain/bowling/BowlingPinsGroup.java
@@ -1,7 +1,10 @@
 package bowling.domain.bowling;
 
+import bowling.domain.exception.BowlingBuildingException;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class BowlingPinsGroup {
@@ -18,5 +21,28 @@ public class BowlingPinsGroup {
                 .forEach(i -> bowlingPins.add(BowlingPin.of(i)));
         bowlingPins.forEach(BowlingPin::initiate);
         return new BowlingPinsGroup(bowlingPins);
+    }
+
+    public BowlingPinsGroup next() {
+        List<BowlingPin> nextBowlingPins = bowlingPins.stream()
+                .filter(BowlingPin::isStanding)
+                .collect(Collectors.toList());
+        return new BowlingPinsGroup(nextBowlingPins);
+    }
+
+    public void hitByBall(int hitCounts) {
+        validateHitCounts(hitCounts);
+        IntStream.range(BowlingPin.FIRST_INDEX, hitCounts)
+                .forEach(i -> bowlingPins.get(i).hitByBall());
+    }
+
+    private void validateHitCounts(int hitCounts) {
+        if (hitCounts > bowlingPins.size()) {
+            throw new BowlingBuildingException(BowlingBuildingException.INVALID_PITCH);
+        }
+    }
+
+    public int getBowlingPinCounts() {
+        return bowlingPins.size();
     }
 }
--- src/test/java/bowling/domain/bowling/BowlingPinsGroupTest.java
+++ src/test/java/bowling/domain/bowling/BowlingPinsGroupTest.java
@@ -1,11 +1,18 @@
 package bowling.domain.bowling;
 
+import bowling.domain.exception.BowlingBuildingException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.*;
 
 class BowlingPinsGroupTest {
+
+    @AfterEach
+    public void resetBowlingPins() {
+        BowlingPinsGroup.initiate().hitByBall(10);
+    }
 
     @DisplayName("매 프레임의 첫 번째 투구 등 처음 칠 때 볼링 핀을 초기화 시켜 반환함")
     @Test
@@ -13,5 +20,30 @@ class BowlingPinsGroupTest {
         assertThatCode(() -> {
             BowlingPinsGroup.initiate();
         }).doesNotThrowAnyException();
+    }
+
+    @DisplayName("next 요청을 보내면, 현재 넘어지지 않은 볼링핀만을 모아 리턴함")
+    @Test
+    public void next_6개() {
+        BowlingPinsGroup bowlingPinsGroup = BowlingPinsGroup.initiate();
+        bowlingPinsGroup.hitByBall(4);
+
+        BowlingPinsGroup nextPitchBowlingPinsGroup = bowlingPinsGroup.next();
+
+        assertThat(nextPitchBowlingPinsGroup.getBowlingPinCounts())
+                .isEqualTo(6);
+    }
+
+    @DisplayName("hit 요청을 보낼 때 현재 서있는 볼링핀 개수보다 많은 수를 입력하면 예외 발생")
+    @Test
+    public void hitByBall_예외() {
+        BowlingPinsGroup bowlingPinsGroup = BowlingPinsGroup.initiate();
+        bowlingPinsGroup.hitByBall(4);
+        BowlingPinsGroup nextPitchBowlingPinsGroup = bowlingPinsGroup.next();
+
+        assertThatThrownBy(() -> {
+            nextPitchBowlingPinsGroup.hitByBall(7);
+        }).isInstanceOf(BowlingBuildingException.class)
+                .hasMessageContaining(BowlingBuildingException.INVALID_PITCH);
     }
 }
