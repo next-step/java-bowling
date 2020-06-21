065dc2b6e7dd40f9042ab5436b3db8fd0da34c07
kwonheejeong <gmlwjd9405@naver.com>
2020-06-22 05:46:55 +0900

    feat: String format 을 util 로 리팩토링

--- src/main/java/bowling/util/StringUtil.java
+++ src/main/java/bowling/util/StringUtil.java
@@ -2,10 +2,16 @@ package bowling.util;
 
 public class StringUtil {
 
+    public static final String EMPTY = "";
+
     private StringUtil() {
     }
 
     public static boolean isEmpty(final String str) {
         return (str == null || str.isEmpty());
+    }
+
+    public static String format(final String str, final String formatExp) {
+        return String.format(formatExp, str);
     }
 }
