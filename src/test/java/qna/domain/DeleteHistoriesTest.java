a5d5e432c95d9e5d56a1ab4168befb8b1f9a1edc
Lim Geun <shing100@naver.com>
2020-06-21 21:13:08 +0900

    test(DeleteHistories 테스트 추가)

---
+++ src/test/java/qna/domain/DeleteHistoriesTest.java
@@ -0,0 +1,43 @@
+package qna.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeleteHistoriesTest {
+
+    @Test
+    @DisplayName("DeleteHistories 추가 테스트")
+    void add() {
+        Long questionId = 1L;
+
+        User writer = new User();
+        DeleteHistories deleteHistories = new DeleteHistories();
+
+        DeleteHistory deleteHistory = new DeleteHistory(ContentType.QUESTION, questionId, writer, LocalDateTime.now());
+        deleteHistories.add(deleteHistory);
+
+        List<DeleteHistory> newDeleteHistories = deleteHistories.getDeleteHistories();
+        assertThat(newDeleteHistories.size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("DeleteHistories get 테스트")
+    void getDeleteHistories() {
+        Long questionId = 1L;
+
+        User writer = new User();
+        DeleteHistories deleteHistories = new DeleteHistories();
+
+        DeleteHistory deleteHistory = new DeleteHistory(ContentType.QUESTION, questionId, writer, LocalDateTime.now());
+        deleteHistories.add(deleteHistory);
+
+        List<DeleteHistory> newDeleteHistories = deleteHistories.getDeleteHistories();
+        assertThat(newDeleteHistories.get(0)).isEqualTo(new DeleteHistory(ContentType.QUESTION, questionId, writer, LocalDateTime.now()));
+    }
+}
