3246cd8459b0ee94f4015f1a774c905fd6608247
kys9261 <nser789@gmail.com>
2020-06-23 01:53:07 +0900

    Bowling - step1 - test(테스트 코드 작성)

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,54 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @DisplayName("삭제할 권한 확인 - 성공")
+    @Test
+    void checkOwner_성공() {
+        assertThatCode(() -> A1.checkOwner(UserTest.JAVAJIGI)).doesNotThrowAnyException();
+        assertThatCode(() -> A2.checkOwner(UserTest.SANJIGI)).doesNotThrowAnyException();
+    }
+
+    @DisplayName("삭제할 권한 확인 - 실패")
+    @Test
+    void checkOwner_실패() {
+        assertThatThrownBy(() -> {
+            A1.checkOwner(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class)
+                .hasMessageContaining("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+
+        assertThatThrownBy(() -> {
+            A2.checkOwner(UserTest.JAVAJIGI);
+        }).isInstanceOf(CannotDeleteException.class)
+                .hasMessageContaining("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
+
+    @DisplayName("삭제 히스토리 생성 체크")
+    @Test
+    void checkDeleteHistory() {
+        List<DeleteHistory> deleteHistories = new ArrayList<>();
+        A1.delete(deleteHistories);
+        assertThat(deleteHistories.size()).isEqualTo(1);
+        assertThat(deleteHistories.get(0)).isInstanceOf(DeleteHistory.class);
+    }
+
+    @DisplayName("삭제 상태 확인")
+    @Test
+    void checkDelete() {
+        List<DeleteHistory> deleteHistories = new ArrayList<>();
+        A1.delete(deleteHistories);
+        assertThat(A1.isDeleted()).isTrue();
+    }
+
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,54 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @DisplayName("삭제할 권한 확인 - 성공")
+    @Test
+    void checkOwner_성공() {
+        assertThatCode(() -> Q1.checkOwner(UserTest.JAVAJIGI)).doesNotThrowAnyException();
+        assertThatCode(() -> Q2.checkOwner(UserTest.SANJIGI)).doesNotThrowAnyException();
+    }
+
+    @DisplayName("삭제할 권한 확인 - 실패")
+    @Test
+    void checkOwner_실패() {
+        assertThatThrownBy(() -> {
+            Q1.checkOwner(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class)
+                .hasMessageContaining("질문을 삭제할 권한이 없습니다.");
+
+        assertThatThrownBy(() -> {
+            Q2.checkOwner(UserTest.JAVAJIGI);
+        }).isInstanceOf(CannotDeleteException.class)
+                .hasMessageContaining("질문을 삭제할 권한이 없습니다.");
+    }
+
+    @DisplayName("삭제 히스토리 생성 체크")
+    @Test
+    void checkDeleteHistory() {
+        List<DeleteHistory> deleteHistories = new ArrayList<>();
+        Q1.delete(deleteHistories);
+        assertThat(deleteHistories.size()).isEqualTo(1);
+        assertThat(deleteHistories.get(0)).isInstanceOf(DeleteHistory.class);
+    }
+
+    @DisplayName("삭제 상태 확인")
+    @Test
+    void checkDelete() {
+        List<DeleteHistory> deleteHistories = new ArrayList<>();
+        Q1.delete(deleteHistories);
+        assertThat(Q1.isDeleted()).isTrue();
+    }
 }
