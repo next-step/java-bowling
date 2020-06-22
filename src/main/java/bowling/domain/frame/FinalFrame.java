b28844fad1de0a9646908fec1f030c0dbccf54f7
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-22 17:11:29 +0900

    feat(FinalFrame): constructor
    - 이전 프레임과 초구로 맞춘 핀의 수를 주입받아서 객체를 생성할 수 있다.

--- src/main/java/bowling/domain/frame/FinalFrame.java
+++ src/main/java/bowling/domain/frame/FinalFrame.java
@@ -1,5 +1,38 @@
 package bowling.domain.frame;
 
-public class FinalFrame {
+import bowling.domain.frameStatus.FinalFrameStatus;
 
+import java.util.Objects;
+
+public class FinalFrame {
+    private static int TEN = 10;
+
+    private final int index;
+    private final FinalFrameStatus finalFrameStatus;
+    private final NormalFrame prevFrame;
+
+    FinalFrame(int index, FinalFrameStatus finalFrameStatus, NormalFrame ninthFrame) {
+        this.index = index;
+        this.finalFrameStatus = finalFrameStatus;
+        this.prevFrame = ninthFrame;
+    }
+
+    public static FinalFrame firstBowl(int numberOfHitPin, NormalFrame ninthFrame) {
+        return new FinalFrame(TEN, FinalFrameStatus.bowlFirst(numberOfHitPin), ninthFrame);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FinalFrame that = (FinalFrame) o;
+        return index == that.index &&
+                Objects.equals(finalFrameStatus, that.finalFrameStatus) &&
+                Objects.equals(prevFrame, that.prevFrame);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(index, finalFrameStatus, prevFrame);
+    }
 }
--- src/test/java/bowling/domain/frame/FinalFrameTests.java
+++ src/test/java/bowling/domain/frame/FinalFrameTests.java
@@ -1,5 +1,21 @@
 package bowling.domain.frame;
 
-class FinalFrameTests {
+import bowling.domain.frameStatus.FinalFrameStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FinalFrameTests {
+    private static final int TEN = 10;
+    private static final int FIVE = 5;
+
+    @DisplayName("초구로 맞춘 핀의 수와 아홉번째 프레임을 입력 받아서 객체를 생성할 수 있다.")
+    @Test
+    void createTest() {
+        NormalFrame ninthFrame = NormalFrame.start(FIVE).bowl(FIVE);
+        FinalFrame finalFrame = FinalFrame.firstBowl(FIVE, ninthFrame);
+
+        assertThat(finalFrame).isEqualTo(new FinalFrame(10, FinalFrameStatus.bowlFirst(FIVE), ninthFrame));
+    }
 }
