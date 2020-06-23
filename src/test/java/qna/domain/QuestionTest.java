8d2e3920217b0e62028b2780a7ffc0f49e6d4ba1
kys9261 <nser789@gmail.com>
2020-06-23 20:55:06 +0900

    Bowling - step1 - test (답변에 따른 질문 테스트코드 추가)

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -25,18 +25,19 @@ public class QuestionTest {
     @DisplayName("삭제할 권한 확인 - 실패")
     @Test
     void checkOwner_실패() {
-        assertThatThrownBy(() -> {
-            Q1.checkOwnerForDelete(UserTest.SANJIGI);
-        }).isInstanceOf(CannotDeleteException.class)
+        assertThatThrownBy(() -> Q1.checkOwnerForDelete(UserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class)
                 .hasMessageContaining("질문을 삭제할 권한이 없습니다.");
 
-        assertThatThrownBy(() -> {
-            Q2.checkOwnerForDelete(UserTest.JAVAJIGI);
-        }).isInstanceOf(CannotDeleteException.class)
+        assertThatThrownBy(() -> Q2.checkOwnerForDelete(UserTest.JAVAJIGI))
+                .isInstanceOf(CannotDeleteException.class)
                 .hasMessageContaining("질문을 삭제할 권한이 없습니다.");
     }
 
-    /* 질문과 답변 삭제 이력에 대한 정보를 DeleteHistory를 활용해 남긴다. */
+    /*
+    * 질문과 답변 삭제 이력에 대한 정보를 DeleteHistory를 활용해 남긴다.
+    * 답이 없는 경우 삭제가 가능하다.
+    * */
     @DisplayName("삭제 히스토리 생성 체크")
     @Test
     void checkDeleteHistory() throws CannotDeleteException {
@@ -49,6 +50,24 @@ public class QuestionTest {
     @Test
     void checkDelete() throws CannotDeleteException {
         List<DeleteHistory> deleteHistories = new ArrayList<>(Q1.delete(UserTest.JAVAJIGI));
+        assertThat(Q1.isDeleted()).isTrue();
+    }
+
+    @DisplayName("질문자와 다른 유저가 답변을 했을경우 질문삭제 불가")
+    @Test
+    void checkDeleteQuestion_AnswerOtherUser() {
+        Q1.addAnswer(AnswerTest.A2);
+        assertThatThrownBy(() -> Q1.delete(UserTest.JAVAJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessageContaining("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
+
+    @DisplayName("질문자와 같은유저가 답변을 달았으면 삭제 가능")
+    @Test
+    void checkDeleteQuestion_AnswerSameUser() throws CannotDeleteException {
+        Q1.addAnswer(AnswerTest.A1);
+        assertThat(Q1.delete(UserTest.JAVAJIGI).get(0)).isInstanceOf(DeleteHistory.class);
+        assertThat(Q1.delete(UserTest.JAVAJIGI).size()).isEqualTo(1);
         assertThat(Q1.isDeleted()).isTrue();
     }
 }
