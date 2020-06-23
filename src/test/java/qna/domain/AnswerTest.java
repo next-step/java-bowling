3269a3321b04952bbe3ef9c04b5601bf8bce6588
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-06-23 15:24:44 +0900

    test : Answer

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.util.stream.Stream;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -11,16 +12,86 @@ import qna.CannotDeleteException;
 
 public class AnswerTest {
 
-  public static final Answer A1 = new Answer(
+  public static Answer A1 = new Answer(
       UserTest.JAVAJIGI,
       QuestionTest.Q1,
       "Answers Contents1"
   );
-  public static final Answer A2 = new Answer(
+  public static Answer A2 = new Answer(
       UserTest.SANJIGI,
       QuestionTest.Q1,
       "Answers Contents2"
   );
+
+  @AfterEach
+  void tearDown() {
+    A1 = new Answer(
+        UserTest.JAVAJIGI,
+        QuestionTest.Q1,
+        "Answers Contents1"
+    );
+    A2 = new Answer(
+        UserTest.SANJIGI,
+        QuestionTest.Q1,
+        "Answers Contents2"
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideAnswerWithValidLoginUser")
+  void isOwner(Answer answer, User loginUser) {
+        assertThat(answer.isOwner(loginUser)).isTrue();
+  }
+  static Stream<Arguments> provideAnswerWithValidLoginUser() {
+    return Stream.of(
+        arguments(
+            A1,
+            UserTest.JAVAJIGI
+        ),
+        arguments(
+            A2,
+            UserTest.SANJIGI
+        )
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideAnswerWithQuestion")
+  void toQuestion(Answer answer, Question question, Answer expected) {
+    answer.toQuestion(question);
+
+    assertThat(answer).isEqualTo(expected);
+  }
+  static Stream<Arguments> provideAnswerWithQuestion() {
+    return Stream.of(
+        arguments(
+            new Answer(
+                UserTest.JAVAJIGI,
+                QuestionTest.Q1,
+                "Answers Contents1"
+            ),
+            QuestionTest.Q2,
+            new Answer(
+                UserTest.JAVAJIGI,
+                QuestionTest.Q2,
+                "Answers Contents1"
+            )
+        ),
+        arguments(
+            new Answer(
+                UserTest.JAVAJIGI,
+                QuestionTest.Q2,
+                "Answers Contents2"
+            ),
+            QuestionTest.Q1,
+            new Answer(
+                UserTest.JAVAJIGI,
+                QuestionTest.Q1,
+                "Answers Contents2"
+            )
+        )
+    );
+  }
 
   @ParameterizedTest
   @MethodSource("provideAnswerWithLoginUser")
