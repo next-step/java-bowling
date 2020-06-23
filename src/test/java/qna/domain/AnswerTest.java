b429220ac6c2e22f2d76d46accf9a18a9dd69c24
JuWanHan <baekseongsa@naver.com>
2020-06-23 23:33:52 +0900

    test(AnswerTest): 테스트 코드 작성

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,18 +1,34 @@
 package qna.domain;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
 
-    @DisplayName("삭제 히스토리를 생성할 수 있다.")
     @Test
-    void canCreateDeleteHistory() {
-        assertThat(A1.createDeleteHistory()).isInstanceOf(DeleteHistory.class);
+    void 질문자와_답변한_사람이_다를_경우_삭제할_수_없다() {
+        assertThatThrownBy(() -> A1.delete(UserTest.SANJIGI)).isInstanceOf(CannotDeleteException.class);
+        assertThatThrownBy(() -> A2.delete(UserTest.JAVAJIGI)).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    void 답변을_삭제하면_상태가_삭제상태로_변경된다() throws CannotDeleteException {
+        A1.delete(UserTest.JAVAJIGI);
         assertThat(A1.isDeleted()).isTrue();
+    }
+
+    @Test
+    void 답변_삭제_이력을_남긴다() throws CannotDeleteException {
+        DeleteHistory deleteHistory = A1.delete(UserTest.JAVAJIGI);
+        DeleteHistory otherDeleteHistory = new DeleteHistory(ContentType.ANSWER, A1.getId(), A1.getWriter(), LocalDateTime.now());
+
+        assertThat(deleteHistory).isEqualTo(otherDeleteHistory);;
     }
 }
