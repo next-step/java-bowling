c8a607323a04dbad3f296ec0e3eb0c0dd1997463
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-06-23 17:18:10 +0900

    test (NormalPitchesTest) : 두번째 투구에 대한 테스트 케이스 추가

--- src/test/java/bowling/pitch/NormalPitchesTest.java
+++ src/test/java/bowling/pitch/NormalPitchesTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class NormalPitchesTest {
 
-    @DisplayName("첫 투구면 Pitch를 initiate하여 내부 컬렉션에 추가함")
+    @DisplayName("첫 투구면 Pitch를 initiate를 통해 생성하여 내부 컬렉션에 추가함")
     @Test
     public void throwBall_첫_투구() {
         Score score = Score.valueOf(5);
@@ -23,5 +23,21 @@ class NormalPitchesTest {
 
         assertThat(currentPitch.getScore()).isEqualTo(5);
         assertThat(currentPitch.getScoreType()).isEqualTo(ScoreType.NORMAL);
+    }
+
+    @DisplayName("두 번째 투구면 Pitch를 next를 통해 생성하여 내부 컬렉션에 추가하며, 스페어임")
+    @Test
+    public void throwBall_두번째_투구() {
+        Score score = Score.valueOf(5);
+        NormalPitches normalPitches = new NormalPitches();
+        List<Pitch> normalPitchesList = normalPitches.getPitches();
+
+        normalPitches.throwBall(score);
+        normalPitches.throwBall(Score.valueOf(5));
+
+        Pitch currentPitch = normalPitchesList.get(1);
+
+        assertThat(currentPitch.getScore()).isEqualTo(5);
+        assertThat(currentPitch.getScoreType()).isEqualTo(ScoreType.SPARE);
     }
 }
