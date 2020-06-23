cc6e66df3b2626a1502cf3428419fb21a95365e2
JuWanHan <baekseongsa@naver.com>
2020-06-23 23:32:45 +0900

    refactor(QnAService): deleteQuestion 메서드 개선

--- src/main/java/qna/service/QnAService.java
+++ src/main/java/qna/service/QnAService.java
@@ -9,6 +9,7 @@ import qna.NotFoundException;
 import qna.domain.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service("qnaService")
 public class QnAService {
@@ -32,15 +33,7 @@ public class QnAService {
     @Transactional
     public void deleteQuestion(User loginUser, long questionId) throws CannotDeleteException {
         Question question = findQuestionById(questionId);
-        question.validateOwner(loginUser);
-
-        Answers answers = question.createAnswers();
-        answers.validateOwner(loginUser);
-
-        DeleteHistories deleteHistories = new DeleteHistories();
-        deleteHistories.addQuestion(question);
-        deleteHistories.addAnswers(answers);
-
-        deleteHistoryService.saveAll(deleteHistories.toList());
+        List<DeleteHistory> deleteHistories = question.delete(loginUser);
+        deleteHistoryService.saveAll(deleteHistories);
     }
 }
