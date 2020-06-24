b5d43a0b0588236ea7a70d71b0265dc64c0d3ead
Gobukgol <zxczc159@gmail.com>
2020-06-24 17:28:00 +0900

    feat(QuestionTest): 다른 사람의 답변이 존재할때 질문 제거하는 테스트 코드 구현

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -14,11 +14,18 @@ public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
 
+    private Question question;
+
+    @BeforeEach
+    void setUp() {
+        question = new Question("title", "contents").writeBy(UserTest.JAVAJIGI);
+    }
+
     @DisplayName("삭제에 성공하면 답변 삭제 히스토리를 포함한 질문 히스토리 리스트를 반환한다.")
     @Test
     void deleteQuestion() throws CannotDeleteException {
-        Q1.addAnswer(AnswerTest.A1);
-        List<DeleteHistory> deleteHistories = Q1.delete(UserTest.JAVAJIGI);
+        question.addAnswer(new Answer(UserTest.JAVAJIGI, question, "Answers Contents1"));
+        List<DeleteHistory> deleteHistories = question.delete(UserTest.JAVAJIGI);
 
         assertThat(deleteHistories.size()).isEqualTo(2);
     }
@@ -29,5 +36,14 @@ public class QuestionTest {
         assertThatThrownBy(() -> Q1.delete(UserTest.SANJIGI))
                 .isInstanceOf(CannotDeleteException.class)
                 .hasMessage("질문을 삭제할 권한이 없습니다.");
+    }
+
+    @DisplayName("답변중 본인의 답변이 아닌게 존재하면 CannotDeleteException Throw")
+    @Test
+    void deleteAnswerThrowException() {
+        question.addAnswer(new Answer(UserTest.SANJIGI, question, "Answers Contents2"));
+        assertThatThrownBy(() -> question.delete(UserTest.JAVAJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("다른 사람이 쓴 답변이 존재해서 삭제할 수 없습니다.");
     }
 }
