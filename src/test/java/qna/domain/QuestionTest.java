2318a4f007614d7818b6eb5bb7b028447f626743
gwangjunchoi <namsookill@gmail.com>
2020-06-24 13:08:46 +0900

    test QuestionTest 테스트 케이스 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,32 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class QuestionTest {
-    public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-    public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+    public static final Question Q1 = new Question(1, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
+    public static final Question Q2 = new Question(2, "title2", "contents2").writeBy(UserTest.SANJIGI);
+
+
+    @Test
+    void checkWriterTest() {
+        Questions questions = new Questions(Q2);
+        DeleteHistories deleteHistories = new DeleteHistories();
+        assertThatThrownBy(() -> {
+            questions.deleteQuestion(Q2.getId(), UserTest.JAVAJIGI, deleteHistories);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    void deleteQuestionTest() throws CannotDeleteException {
+        Questions questions = new Questions(Q1);
+        DeleteHistories deleteHistories = new DeleteHistories();
+        questions.deleteQuestion(Q1.getId(), UserTest.JAVAJIGI, deleteHistories);
+        assertThat(Q1.isDeleted()).isTrue();
+    }
 }
