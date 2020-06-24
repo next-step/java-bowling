eabfed216a0b9bc11270d80afdc79b37ccd65ac7
Sungjun <sungjunpizz@gmail.com>
2020-06-25 00:45:08 +0900

    feat: States 클래스 추가

---
+++ src/main/java/bowling/domain/States.java
@@ -0,0 +1,12 @@
+package bowling.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class States {
+    private final List<State> states;
+
+    public States() {
+        this.states = new ArrayList<>();
+    }
+}
