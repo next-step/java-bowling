0f604517439c3fd6ad7e780588579973cd0814af
김다혜 <dahye432111@gmail.com>
2020-06-25 01:26:33 +0900

    feat: 답변 데이터 삭제 시 상태 변경 메소드 추가

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,27 @@
 package qna.domain;
 
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    private User loginUser;
+
+    @BeforeClass
+    void setUp() {
+        loginUser = UserTest.JAVAJIGI;
+    }
+
+    @Test
+    @DisplayName("답변 데이터를 완전히 삭제하는 것이 아니라 데이터의 상태를 삭제 상태만 변경한다.")
+    void delete_성공() {
+        assertThat(A1.isDeleted()).isFalse();
+        A1.deleteBy(loginUser);
+        assertThat(A1.isDeleted()).isTrue();
+    }
 }
