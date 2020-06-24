0299087f8e91e29d74bea59f2de67519021a7b65
Sungjun <sungjunpizz@gmail.com>
2020-06-25 00:48:23 +0900

    test: States add 메소드 TC 추가

--- src/test/java/bowling/domain/StatesTest.java
+++ src/test/java/bowling/domain/StatesTest.java
@@ -3,6 +3,7 @@ package bowling.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 @DisplayName("States 클래스 테스트")
@@ -11,5 +12,14 @@ public class StatesTest {
     @Test
     void create() {
         assertThatCode(States::new).doesNotThrowAnyException();
+    }
+
+    @Test
+    void add() {
+        States states = new States();
+
+        states.add(State.ONE);
+
+        assertThat(states.getStates().get(0)).isEqualTo(State.ONE);
     }
 }
