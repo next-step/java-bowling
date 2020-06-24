388789cdbe7a854fbbaf29b8698cd2318c0eeb4c
Sungjun <sungjunpizz@gmail.com>
2020-06-24 23:53:50 +0900

    refactor: Pin 생성 예외 검증 추가

--- src/main/java/bowling/domain/Pin.java
+++ src/main/java/bowling/domain/Pin.java
@@ -3,10 +3,20 @@ package bowling.domain;
 import java.util.Objects;
 
 public class Pin {
+    private static final int MIN_PIN = 0;
+    private static final int MAX_PIN = 10;
+
     private final int fallenPin;
 
     public Pin(int fallenPin) {
+        validateFallenPin(fallenPin);
         this.fallenPin = fallenPin;
+    }
+
+    private void validateFallenPin(int fallenPin) {
+        if (fallenPin < MIN_PIN || fallenPin > MAX_PIN) {
+            throw new IncorrectPinException();
+        }
     }
 
     @Override
