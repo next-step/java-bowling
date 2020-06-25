7255f9aef0c3a1cde02d5c661bc62a9e31c6ff16
gwonsungjun <sungjunpizz@gmail.com>
2020-06-25 16:34:08 +0900

    test: NormalFrame idEndFrame TC 추가

--- src/test/java/bowling/domain/NormalFrameTest.java
+++ src/test/java/bowling/domain/NormalFrameTest.java
@@ -17,4 +17,16 @@ public class NormalFrameTest {
         assertThat(normalFrame.getStates().getStates().get(0)).isEqualTo(State.STRIKE);
         assertThat(normalFrame.getPin()).isEqualTo(new Pin(10));
     }
+
+
+    @DisplayName("현재 프레임 종료 여부를 반환할 수 있다.")
+    @Test
+    void isEndFrame() {
+        NormalFrame normalFrame = new NormalFrame();
+        normalFrame.bowl(new Pin(10));
+
+        boolean actual = normalFrame.isEndFrame();
+
+        assertThat(actual).isTrue();
+    }
 }
