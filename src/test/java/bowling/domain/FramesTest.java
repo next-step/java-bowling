a7790e0837b9c3c4e89a61954860a1a70eb0d136
Sungjun <sungjunpizz@gmail.com>
2020-06-25 21:39:03 +0900

    test: Frames getCurrentFrameNumber, isEndGame TC 추가

--- src/test/java/bowling/domain/FramesTest.java
+++ src/test/java/bowling/domain/FramesTest.java
@@ -1,26 +1,37 @@
 package bowling.domain;
 
-import aTest.NormalFrame;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DisplayName("Fames 클래스 테스트")
+@DisplayName("Frames 클래스 테스트")
 class FramesTest {
+    final Frames frames = new Frames(new NormalFrame());
 
     @Test
     void create() {
-        Frames frames = new Frames(new NormalFrame());
         assertThat(frames.getCurrentFrame()).isInstanceOf(NormalFrame.class);
     }
 
     @Test
     void add() {
-        Frames frames = new Frames(new NormalFrame());
-
         frames.add(new NormalFrame());
 
         assertThat(frames.getFrames()).hasSize(2);
+    }
+
+    @Test
+    void getCurrentFrameNumber() {
+        frames.add(new NormalFrame());
+
+        assertThat(frames.getCurrentFrameNumber()).isEqualTo(2);
+    }
+
+    @Test
+    void isEndGame() {
+        frames.add(new NormalFrame());
+
+        assertThat(frames.isEndGame()).isFalse();
     }
 }
