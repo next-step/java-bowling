87a434e5e580af29ad81351cef55fc6ac78aec7f
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-25 09:23:26 +0900

    feat(BowlingGameResult): constructor
    - FrameResults를 주입받아서 객체 생성 가능
    - 현재는 데이터 홀더 성격의 VO
    - 향후 Score까지 관리하게 될 예정

--- src/main/java/bowling/domain/BowlingGameResult.java
+++ src/main/java/bowling/domain/BowlingGameResult.java
@@ -1,4 +1,24 @@
 package bowling.domain;
 
+import java.util.Objects;
+
 public class BowlingGameResult {
+    private final FrameResults frameResults;
+
+    public BowlingGameResult(FrameResults frameResults) {
+        this.frameResults = frameResults;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BowlingGameResult that = (BowlingGameResult) o;
+        return Objects.equals(frameResults, that.frameResults);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(frameResults);
+    }
 }
