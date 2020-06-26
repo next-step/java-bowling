c24f3e51e1bc9e3b5426bc5135ece99ceec8bc46
kys9261 <nser789@gmail.com>
2020-06-27 02:50:53 +0900

    Bowling - step2 - feat (Score 구현)

---
+++ src/main/java/bowling/domain/Score.java
@@ -0,0 +1,13 @@
+package bowling.domain;
+
+public class Score {
+    private int score;
+
+    public Score(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
