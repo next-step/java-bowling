8e5633d5077e4792e3bdb4df41d7c73adff7171a
bgpark <bgpark82@gmail.com>
2020-06-27 20:45:54 +0900

    feat : validate 로직 Question으로 이동

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,17 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    void validate() {
+        assertThatThrownBy(() -> Q1.validate(UserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class);
+    }
 }
