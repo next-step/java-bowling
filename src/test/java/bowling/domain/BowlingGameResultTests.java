50257a15a16b2f590a87600c83819dc539a989a0
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-27 15:41:42 +0900

    refactor(BowlingGameResult): add property
    - 관리할 객체 추가(FrameScore)

--- src/test/java/bowling/domain/BowlingGameResultTests.java
+++ src/test/java/bowling/domain/BowlingGameResultTests.java
@@ -16,4 +16,14 @@ class BowlingGameResultTests {
 
         assertThat(bowlingGameResult).isEqualTo(new BowlingGameResult(frameResults));
     }
+
+    @DisplayName("FrameResults와 FrameScore를 주입받아서 객체를 생성 할 수 있다.")
+    @Test
+    void createRefactorTest() {
+        FrameResults frameResults = new FrameResults(Collections.singletonList(FrameResult.STRIKE));
+        FrameScore frameScore = new FrameScore(FrameScoreStatus.COMPLETE, 10);
+        BowlingGameResult bowlingGameResult = new BowlingGameResult(frameResults, frameScore);
+
+        assertThat(bowlingGameResult).isEqualTo(new BowlingGameResult(frameResults, frameScore));
+    }
 }
