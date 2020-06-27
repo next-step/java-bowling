fe4ea6738163a217db28db6124876bdf03b65149
최홍희 <vvshinevv@gmail.com>
2020-06-27 16:20:03 +0900

    [feat] 히스토리 저장 추가

--- src/main/java/qna/service/DeleteHistoryService.java
+++ src/main/java/qna/service/DeleteHistoryService.java
@@ -3,6 +3,7 @@ package qna.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import qna.domain.DeleteHistories;
 import qna.domain.DeleteHistory;
 import qna.domain.DeleteHistoryRepository;
 
@@ -20,7 +21,7 @@ public class DeleteHistoryService {
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void save(DeleteHistory deleteHistory) {
-        deleteHistoryRepository.save(deleteHistory);
+    public void saveAll(DeleteHistories deleteHistories) {
+        this.saveAll(deleteHistories.getDeleteHistories());
     }
 }
