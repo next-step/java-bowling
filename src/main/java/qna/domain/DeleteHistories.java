312ad93792d3a3279afd3600ab3b9c419884710f
최홍희 <vvshinevv@gmail.com>
2020-06-27 16:19:32 +0900

    [feat] deleteHistories 일급 객체로 추출

--- src/main/java/qna/domain/DeleteHistories.java
+++ src/main/java/qna/domain/DeleteHistories.java
@@ -1,4 +1,34 @@
 package qna.domain;
 
-public class DeleteHistorys {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DeleteHistories {
+
+    private List<DeleteHistory> deleteHistories;
+
+    private DeleteHistories() {
+        this.deleteHistories = new ArrayList<>();
+    }
+
+    public static DeleteHistories of() {
+        return new DeleteHistories();
+    }
+
+    public void addToDeleteHistories(Question question) {
+        DeleteHistory questionHistory = question.generateDeleteHistoryForQuestion();
+        this.deleteHistories.add(questionHistory);
+
+        addToDeleteHistoriesForAnswers(question.getAnswers());
+    }
+
+    public List<DeleteHistory> getDeleteHistories() {
+        return Collections.unmodifiableList(this.deleteHistories);
+    }
+
+    private void addToDeleteHistoriesForAnswers(Answers answers) {
+        List<DeleteHistory> deleteHistories = answers.generateDeleteHistories();
+        this.deleteHistories.addAll(deleteHistories);
+    }
 }
