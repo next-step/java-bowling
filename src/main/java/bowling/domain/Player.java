a565d3fa82a7ec0fad0dc67d44e95bb5795a86e2
gwangjunchoi <namsookill@gmail.com>
2020-06-28 17:19:50 +0900

    feat player 클래스 추가

---
+++ src/main/java/bowling/domain/Player.java
@@ -0,0 +1,14 @@
+package bowling.domain;
+
+public class Player {
+
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
