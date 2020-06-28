7cfab02fb3fcaa96da23c9da5b940faa5b75e61c
Sungjun <sungjunpizz@gmail.com>
2020-06-28 16:26:40 +0900

    feat: Frames getScores 메소드 추가

--- src/main/java/bowling/domain/Frames.java
+++ src/main/java/bowling/domain/Frames.java
@@ -31,4 +31,23 @@ public class Frames {
     public List<Frame> getFrames() {
         return frames;
     }
+
+    public List<Integer> getScores() {
+        List<Integer> scores = new ArrayList<>();
+
+        frames.forEach(frame -> sumScore(scores, frame));
+
+        return scores;
+    }
+
+    private void sumScore(List<Integer> scores, Frame frame) {
+        if (!frame.isCalculateScore()) {
+            return;
+        }
+
+        int score = frame.getScore();
+
+        score = scores.isEmpty() ? score : scores.get(scores.size() - 1) + score;
+        scores.add(score);
+    }
 }
