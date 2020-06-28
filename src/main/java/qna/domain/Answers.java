99fbce5ffe724095be47d3c33098ec6bf323942f
bbbnam <bbbnam@naver.com>
2020-06-28 23:27:22 +0900

    feat : answer 일급 콜렉션 클래스 생성, delete 메소드 구현

---
+++ src/main/java/qna/domain/Answers.java
@@ -0,0 +1,44 @@
+package qna.domain;
+
+import qna.CannotDeleteException;
+import qna.ExistTheOthersAnswerException;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Answers {
+
+    private final List<Answer> answers;
+
+    public Answers(List<Answer> answers) {
+        this.answers = answers;
+    }
+
+    public static Answers of(List<Answer> answers){
+        return new Answers(answers);
+    }
+
+    public int countAnswers(){
+        return answers.size();
+    }
+
+    public void delete(User loginUser, List<DeleteHistory> deleteHistories) throws CannotDeleteException {
+        checkAnswers(loginUser);
+        for (Answer answer : answers) {
+            answer.setDeleted(true);
+            deleteHistories.add(new DeleteHistory(ContentType.ANSWER,
+                    answer.getId(), answer.getWriter(), LocalDateTime.now()));
+        }
+    }
+
+    private void checkAnswers(User loginUser) throws CannotDeleteException {
+        if (isExistOthersAnswer(loginUser)) {
+            throw new ExistTheOthersAnswerException();
+        }
+    }
+
+    private boolean isExistOthersAnswer(User loginUser) {
+        return answers.stream()
+                      .anyMatch(answer -> !answer.isOwner(loginUser));
+    }
+}
