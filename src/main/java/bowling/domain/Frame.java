a8ddf7edc15fae2834375a810b7a719ce9de21cc
Sungjun <sungjunpizz@gmail.com>
2020-06-28 16:35:53 +0900

    refactor: Frame 인터페이스 추상 클래스로 수정

--- src/main/java/bowling/domain/Frame.java
+++ src/main/java/bowling/domain/Frame.java
@@ -1,9 +1,38 @@
 package bowling.domain;
 
-public interface Frame {
-    void bowl(Pin pin);
-    boolean isEndFrame();
-    boolean isEndGame();
-    Frame getNextFrame(int frameNumber);
-    States getStates();
+import java.util.Objects;
+
+public abstract class Frame {
+    public static final int WAITING_CALCULATION = -1;
+
+    Pin pin;
+    States states;
+    Frame nextFrame;
+
+    abstract void bowl(Pin pin);
+    abstract boolean isEndFrame();
+    abstract boolean isEndGame();
+    abstract Frame getNextFrame(int frameNumber);
+    abstract int getScore();
+    public abstract States getStates();
+
+    int calculateAdditionalScore(Score score) {
+        for (Pin pin : states.getPins()) {
+            score = score.bowl(pin.getFallenPin());
+
+            if (score.canCalculateScore()) {
+                return score.getScore();
+            }
+        }
+
+        if (Objects.isNull(nextFrame)) {
+            return WAITING_CALCULATION;
+        }
+
+        return nextFrame.calculateAdditionalScore(score);
+    }
+
+    boolean isCalculateScore() {
+        return getScore() != WAITING_CALCULATION;
+    }
 }
