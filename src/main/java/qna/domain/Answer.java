19e236d1f3b56f6ab4c36a005b6d48e6d0d94610
Kim-yongjin <yongjin@sixshop.com>
2020-06-28 18:58:22 +0900

    <refactor> [step1] Exception 메시지 문구 수정

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -77,7 +77,7 @@ public class Answer extends AbstractEntity {
 
     public DeleteHistory deleteByUser(User loginUser) throws CannotDeleteException {
         if (!isOwner(loginUser)) {
-            throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
         }
 
         this.deleted = true;
