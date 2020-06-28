2a58ce6203f0353c5fa3612c8d2c03d3087efd4b
김다혜 <dahye432111@gmail.com>
2020-06-29 01:21:00 +0900

    feat: FrameScore 객체에 strike를 위한 팩토리 메소드 추가

--- src/main/java/bowling/domain/score/FrameScore.java
+++ src/main/java/bowling/domain/score/FrameScore.java
@@ -1,10 +1,21 @@
 package bowling.domain.score;
 
 public class FrameScore {
-    private static final int MAX_SCORE_PER_FRAME = 10;
+    public static final int MAX_SCORE_PER_FRAME = 10;
 
     private final Score firstScore;
     private final Score secondScore;
+
+    private FrameScore(Score firstScore) {
+        this(firstScore, Score.of(0));
+        validateStrike(firstScore);
+    }
+
+    private void validateStrike(Score firstScore) {
+        if (!firstScore.equals(Score.of(MAX_SCORE_PER_FRAME))) {
+            throw new IllegalArgumentException("스트라이크의 점수는 반드시 " + MAX_SCORE_PER_FRAME + " 점 입니다.");
+        }
+    }
 
     private FrameScore(Score firstScore, Score secondScore) {
         validateSum(firstScore, secondScore);
@@ -20,5 +31,9 @@ public class FrameScore {
 
     public static FrameScore of(Score firstScore, Score secondScore) {
         return new FrameScore(firstScore, secondScore);
+    }
+
+    public static FrameScore strike(Score firstScore) {
+        return new FrameScore(firstScore);
     }
 }
--- src/main/java/bowling/domain/score/Score.java
+++ src/main/java/bowling/domain/score/Score.java
@@ -1,5 +1,7 @@
 package bowling.domain.score;
 
+import java.util.Objects;
+
 public class Score {
     private static final int MIN_SCORE = 0;
     private static final int MAX_SCORE = 10;
@@ -36,5 +38,18 @@ public class Score {
         }
 
         return Integer.toString(score);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Score score1 = (Score) o;
+        return score == score1.score;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score);
     }
 }
