7de3393d5ba7cb8537d7d160bd2b403a7281b993
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-29 09:15:35 +0900

    feat: delete answer method

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,26 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import qna.CannotDeleteException;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @DisplayName("답변을 삭제하고 해당 답변을 반환한다.")
+    @Test
+    void 답변을_삭제하고_이를_반환한다() throws CannotDeleteException {
+        Answer deletedTheFirst = A1.delete();
+        Answer deletedTheSecond = A2.delete();
+
+        assertAll(
+            () -> assertThat(deletedTheFirst).isEqualTo(A1),
+            () -> assertThat(deletedTheSecond).isEqualTo(A2)
+        );
+    }
 }
