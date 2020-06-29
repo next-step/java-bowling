f2c70ea74333a789b70c44270bd43d69ef6505ac
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-06-29 16:26:18 +0900

    refacotr (FrameScoresTest) : 불필요한 테스트 삭제

--- src/test/java/bowling/domain/score/FrameScoresTest.java
+++ src/test/java/bowling/domain/score/FrameScoresTest.java
@@ -1,58 +1,36 @@
 package bowling.domain.score;
 
-import bowling.domain.frame.Frames;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FrameScoresTest {
 
-    @DisplayName("값이 존재하는 FrameScore만 내부 컬렉션에 추가함")
-    @Test
-    public void makeFrameScores_크기_1() {
-        Frames frames = Frames.initiate();
-        frames.bowl(PitchScore.valueOf(5));
-        frames.bowl(PitchScore.valueOf(3));
-        frames.moveToNextFrame();
-        frames.bowl(PitchScore.valueOf(10));
-
-        List<Integer> list = frames.getFrameScores();
-
-        assertThat(list).containsExactly(8);
-    }
-
-    @DisplayName("각 프레임의 점수를 누적 계산하여 리턴함")
+    @DisplayName("점수 리턴을 요청하면 내부 컬렉션의 점수들을 누계하여 리스트로 반환함")
     @Test
     public void getFrameScores_8점_25점_32점() {
-        Frames frames = Frames.initiate();
-        frames.bowl(PitchScore.valueOf(5));
-        frames.bowl(PitchScore.valueOf(3));
-        frames.moveToNextFrame();
-        frames.bowl(PitchScore.valueOf(10));
-        frames.moveToNextFrame();
-        frames.bowl(PitchScore.valueOf(3));
-        frames.bowl(PitchScore.valueOf(4));
+        List<FrameScore> frameScoreList =
+                Arrays.asList(FrameScore.ofMiss(8), FrameScore.ofMiss(10), FrameScore.ofMiss(7));
+        FrameScores frameScores = FrameScores.of(frameScoreList);
 
-        List<Integer> frameScores = frames.getFrameScores();
-
-        assertThat(frameScores).containsExactly(8, 25, 32);
+        assertThat(frameScores.getFrameScores()).containsExactly(8, 25, 32);
     }
 
-    @DisplayName("12번 스트라이크를 치면 300점")
+    @DisplayName("12번 스트라이크를 치면 300점이 최종 누계됨")
     @Test
     public void getFrameScores_300점() {
-        Frames frames = Frames.initiate();
-        for (int i = 0; i < 11; i++) {
-            frames.bowl(PitchScore.valueOf(10));
-            frames.moveToNextFrame();
+        List<FrameScore> frameScoreList = new ArrayList<>();
+        for (int i = 0; i < 12; i++) {
+            frameScoreList.add(FrameScore.ofMiss(10));
         }
-        frames.bowl(PitchScore.valueOf(10));
 
-        List<Integer> cumulativeFrameScores = frames.getFrameScores();
+        List<Integer> frameScores = FrameScores.of(frameScoreList).getFrameScores();
 
-        assertThat(cumulativeFrameScores.get(cumulativeFrameScores.size() - 1)).isEqualTo(300);
+        assertThat(frameScores.get(frameScores.size() - 1)).isEqualTo(300);
     }
 }
