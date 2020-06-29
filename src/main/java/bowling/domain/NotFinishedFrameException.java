f36af8ad3e2156a073145bd43a1f559ca73d1077
하재권 <hazxz@naver.com>
2020-06-29 10:46:56 +0900

    chore: 사용하지 않게된 NotFinishedFrameException 클래스 삭제

--- src/main/java/bowling/domain/NotFinishedFrameException.java
+++
@@ -1,7 +0,0 @@
-package bowling.domain;
-
-public class NotFinishedFrameException extends RuntimeException {
-    public NotFinishedFrameException(String message) {
-        super(message);
-    }
-}
