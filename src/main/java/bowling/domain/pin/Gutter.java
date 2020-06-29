2d6b79ead56b1c4cd5c4ea3782e81e0670ce7619
김다혜 <dahye432111@gmail.com>
2020-06-30 00:25:08 +0900

    feat: Gutter 객체 구현

---
+++ src/main/java/bowling/domain/pin/Gutter.java
@@ -0,0 +1,11 @@
+package bowling.domain.pin;
+
+public class Gutter extends FramePins {
+    public Gutter(Pins firstPins, Pins secondPins) {
+        super(firstPins, secondPins);
+    }
+
+    public static Gutter of() {
+        return new Gutter(Pins.of(0), Pins.of(0));
+    }
+}
