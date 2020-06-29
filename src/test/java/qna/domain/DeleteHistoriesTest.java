2b27886d45b89e881ecf1cfe15ad7cc6cb5dc202
최홍희 <vvshinevv@gmail.com>
2020-06-29 13:32:06 +0900

    [test]테스트생성자 작업 추가

--- src/test/java/qna/domain/DeleteHistoriesTest.java
+++ src/test/java/qna/domain/DeleteHistoriesTest.java
@@ -1,8 +1,30 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class DeleteHistoriesTest {
 
     public static final DeleteHistories HISTORIES = DeleteHistories.of(Arrays.asList(DeleteHistoryTest.D1_Q, DeleteHistoryTest.D2_A));
+
+    @DisplayName("Histories 생성자 테스트")
+    @ParameterizedTest
+    @MethodSource("provideHistories")
+    void historyConstructorTest(final DeleteHistory d1, final DeleteHistory d2, final DeleteHistories expected) {
+        DeleteHistories deleteHistories = DeleteHistories.of(Arrays.asList(d1, d2));
+        assertThat(deleteHistories).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideHistories() {
+        return Stream.of(
+                Arguments.of(DeleteHistoryTest.D1_Q, DeleteHistoryTest.D2_A, HISTORIES)
+        );
+    }
 }
--- src/test/java/qna/domain/DeleteHistoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryTest.java
@@ -1,10 +1,32 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 import java.time.LocalDateTime;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class DeleteHistoryTest {
 
     public static final DeleteHistory D1_Q = DeleteHistory.of(ContentType.QUESTION, 1L, UserTest.SANJIGI, LocalDateTime.now());
     public static final DeleteHistory D2_A = DeleteHistory.of(ContentType.ANSWER, 2L, UserTest.JAVAJIGI, LocalDateTime.now());
 
+    @DisplayName("History 생성자 테스트")
+    @ParameterizedTest
+    @MethodSource("provideHistory")
+    void historyConstructorTest(final ContentType contentType, final long no, final User user, final DeleteHistory expected) {
+        DeleteHistory deleteHistory = DeleteHistory.of(contentType, no, user, LocalDateTime.now());
+        assertThat(deleteHistory).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideHistory() {
+        return Stream.of(
+                Arguments.of(ContentType.QUESTION, 1L, UserTest.JAVAJIGI, DeleteHistory.of(ContentType.QUESTION, 1L, UserTest.JAVAJIGI, LocalDateTime.now())),
+                Arguments.of(ContentType.ANSWER, 2L, UserTest.SANJIGI, DeleteHistory.of(ContentType.ANSWER, 2L, UserTest.SANJIGI, LocalDateTime.now()))
+        );
+    }
 }
