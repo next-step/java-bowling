42d19e6ddc3319d230597fe9d7c3e464b1ee9ccc
neojjc <neo.jjc@gmail.com>
2020-06-29 14:30:39 +0900

    [fix#16, fix#18] 리뷰 사항 반영

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -10,24 +10,46 @@ public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
 
+    @DisplayName("답변 작성자와 일치하는지 확인")
+    @Test
+    public void ownerTest() {
+        // given
+        Answer answer = A1;
+        User user = UserTest.JAVAJIGI;
+
+        // when
+        boolean result = answer.isOwner(user);
+
+        // then
+        assertThat(result).isTrue();
+
+    }
+
     @DisplayName("삭제가 잘 되는지 확인")
     @Test
     public void deleteTest() {
+        // given
+        Answer answer = A1;
+
         // when
-        A1.delete();
+        answer.delete();
+
         // then
-        assertThat(A1.isDeleted()).isTrue();
+        assertThat(answer.isDeleted()).isTrue();
     }
 
     @DisplayName("삭제 시 History가 잘 반환되는지 확인")
     @Test
     public void deleteHistorytest() {
+        // given
+        Answer answer = A1;
+
         // when
-        DeleteHistory history = A1.delete();
+        DeleteHistory history = answer.delete();
 
         // then
         assertAll(
-                () -> assertThat(A1.isDeleted()).isTrue(),
+                () -> assertThat(answer.isDeleted()).isTrue(),
                 () -> assertThat(history.isContentType(ContentType.ANSWER)).isTrue(),
                 () -> assertThat(history.isOwner(UserTest.JAVAJIGI)).isTrue()
         );
