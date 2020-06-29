07f42de586b52d1d46c77532e6004a86692441a8
gwangjunchoi <namsookill@gmail.com>
2020-06-30 01:58:20 +0900

    refactor Score 유효성체크 추가

--- src/main/java/bowling/domain/Score.java
+++ src/main/java/bowling/domain/Score.java
@@ -4,9 +4,17 @@ public class Score {
 
     private int score;
     public static final int MAX_SCORE = 10;
+    public static final int MIN_SCORE = 0;
 
     public Score(int score) {
+        validateInputScore(score);
         this.score = score;
+    }
+
+    private void validateInputScore(int score) {
+        if (MIN_SCORE > score || MAX_SCORE < score) {
+            throw new IllegalArgumentException("0 ~ 10 점수 입력");
+        }
     }
 
     public boolean validateMaxScore() {
