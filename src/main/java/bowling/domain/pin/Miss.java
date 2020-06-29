5c7336d2a5f5da5bef30558726b43bc5d78fee79
김다혜 <dahye432111@gmail.com>
2020-06-30 00:22:28 +0900

    feat: Miss 객체 구현

---
+++ src/main/java/bowling/domain/pin/Miss.java
@@ -0,0 +1,11 @@
+package bowling.domain.pin;
+
+public class Miss extends FramePins {
+    public Miss(Pins firstPins, Pins secondPins) {
+        super(firstPins, secondPins);
+    }
+
+    public static Miss of(Pins firstPins, Pins secondPins) {
+        return new Miss(firstPins, secondPins);
+    }
+}
