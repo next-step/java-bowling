a967de28478379dd233438815e4c9daea83255de
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-01 00:55:19 +0900

    feat: add framesize class for managing static constants

---
+++ src/main/java/bowling/util/FrameSize.java
@@ -0,0 +1,7 @@
+package bowling.util;
+
+public class FrameSize {
+	public static final int STARTING_INDEX = 0;
+	public static final int FRAME_SIZE = 10;
+	public static final int NORMAL_FRAME_SIZE = FRAME_SIZE - 1;
+}
