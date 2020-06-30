c3aff316138b22478fa1bb0c586a36d9186dea07
하재권 <hazxz@naver.com>
2020-07-01 02:33:03 +0900

    feat(Frame): tobe 패키지 하위에 FinalFrame 클래스 개발

--- src/main/java/bowling/tobe/FinalFrame.java
+++ src/main/java/bowling/tobe/FinalFrame.java
@@ -1,5 +1,6 @@
 package bowling.tobe;
 
+import bowling.common.IntegerUtils;
 import bowling.domain.Pin;
 import bowling.domain.Pitch;
 import bowling.domain.Shot;
@@ -8,16 +9,39 @@ import bowling.domain.State;
 import java.util.List;
 
 public class FinalFrame extends Frame {
+    private boolean isBonusPitch;
+    private Pitch bonusPitch;
 
     public FinalFrame() {
         this.frameNo = FINAL_FRAME;
         this.pitch = new Pitch();
+        this.isBonusPitch = false;
     }
 
     @Override
     public State bowling(Pin pin) {
-        // TODO
-        return null;
+        if (isBonusPitch) {
+            return bowlingBonus(pin);
+        }
+
+        if (pitch.add(pin)) {
+            return isNextBonusPitch();
+        }
+        return State.NotFinish;
+    }
+
+    private State bowlingBonus(Pin pin) {
+        bonusPitch = new Pitch();
+        bonusPitch.add(pin);
+        return State.Finish;
+    }
+
+    private State isNextBonusPitch() {
+        if (pitch.getRemain() == IntegerUtils.ZERO) {
+            isBonusPitch = true;
+            return State.Bonus;
+        }
+        return State.Finish;
     }
 
     @Override
