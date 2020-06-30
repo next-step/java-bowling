0837b44f49e118b4453b7cb7075d0a3ba8e64c66
Sungjun <sungjunpizz@gmail.com>
2020-06-30 22:27:19 +0900

    refactor: InputView 수정

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -3,15 +3,22 @@ package bowling.view;
 import java.util.Scanner;
 
 public class InputView {
-    public static String inputPlayerName() {
-        System.out.print("플레이어 이름은(3 english letters)?: ");
+    public static String inputPlayerName(int number) {
+        System.out.print("플레이어 " + (number + 1) + "의 이름은(3 english letters)?: ");
 
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 
-    public static int inputFramePitches(int frameNumber) {
-        System.out.print(frameNumber + "프레임 투구 : ");
+    public static int inputFramePitches(String playerName) {
+        System.out.print(playerName + "'s turn : ");
+
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public static int inputNumberOfPeople() {
+        System.out.print("How many people? ");
 
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
