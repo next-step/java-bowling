410011c11e2c6e73d4f31c1e19523f3928ec6daa
하재권 <hazxz@naver.com>
2020-06-30 20:56:30 +0900

    feat(Player): Player 클래스에서 Frames 클래스 제거를 준비, 테스트 코드 수정

--- src/main/java/bowling/domain/Player.java
+++ src/main/java/bowling/domain/Player.java
@@ -3,8 +3,21 @@ package bowling.domain;
 import java.util.List;
 
 public class Player {
+    private static final int NAME_LENGTH = 3;
     private final String name;
     private final Frames frames;
+
+    public Player(String name) {
+        validateName(name);
+        this.name = name;
+        this.frames = null;
+    }
+
+    private void validateName(String name) {
+        if (name.length() != NAME_LENGTH) {
+            throw new IllegalArgumentException("name length is must " + NAME_LENGTH);
+        }
+    }
 
     public Player(String name, int finalRound) {
         this.name = name;
--- src/test/java/bowling/domain/PlayerTest.java
+++ src/test/java/bowling/domain/PlayerTest.java
@@ -1,30 +1,39 @@
 package bowling.domain;
 
-import bowling.BowlingGame;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@DisplayName("플레이어 로직 테스트")
+@DisplayName("Player: 플레이어 로직 테스트")
 class PlayerTest {
 
-    @DisplayName("플레이어 생성 성공")
+    @DisplayName("이름의 길이를 입력 받아 생성")
     @ParameterizedTest
-    @MethodSource("source_create_player_shouldSuccess")
-    public void create_player_shouldSuccess(String playerName) {
-        Player player = new Player(playerName, BowlingGame.FINAL_FRAME);
-        assertThat(player.getName()).isEqualTo(playerName);
+    @MethodSource("source_getName_ReturnName")
+    public void getName_ReturnName(String name) {
+        Player player = new Player(name);
+        assertThat(player.getName()).isEqualTo(name);
     }
 
-    public static Stream<Arguments> source_create_player_shouldSuccess() {
+    public static Stream<Arguments> source_getName_ReturnName() {
         return Stream.of(
-                Arguments.of("jaekwon"),
-                Arguments.of("jk")
-        );
+                Arguments.of("PJS"),
+                Arguments.of("JAE"));
+    }
+
+    @DisplayName("이름의 길이가 3자가 아니면 예외 발생")
+    @ParameterizedTest
+    @CsvSource(value = {"po", "honu", "crong", "jk"}, delimiter = ',')
+    public void create_WithNameLengthNot3_ExceptionThrown(String name) {
+        assertThatThrownBy(() -> {
+            new Player(name);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
