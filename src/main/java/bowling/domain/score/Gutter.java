fe328df286bd8abffc8a7423cdaec4ada943c51b
iljun <sumusb@naver.com>
2020-07-01 18:35:56 +0900

    [refactor] edit magic number

--- src/main/java/bowling/domain/score/Gutter.java
+++ src/main/java/bowling/domain/score/Gutter.java
@@ -5,18 +5,20 @@ import bowling.domain.point.Point;
 public class Gutter implements Score {
     private static final Point POINT = Point.MIN;
     private static final ScoreType SCORE_TYPE = ScoreType.GUTTER;
+    private static final int STRIKE_SCORE = 10;
+    private static final int GUTTER_SCORE = 0;
 
     @Override
     public Score nextScore(int point) {
-        if (point > 10) {
+        if (point > STRIKE_SCORE) {
             throw new IllegalArgumentException("total Score less than 10");
         }
 
-        if (point == 10) {
+        if (point == STRIKE_SCORE) {
             return new Spare(point);
         }
 
-        if (point == 0) {
+        if (point == GUTTER_SCORE) {
             return new Gutter();
         }
 
