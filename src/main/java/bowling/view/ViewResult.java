a021112827e46cbdc21b81cee61726ce5e82fea8
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-03 22:29:32 +0900

    refactor: remove verbose optional wrapping

--- src/main/java/bowling/view/ViewResult.java
+++ src/main/java/bowling/view/ViewResult.java
@@ -1,31 +1,17 @@
 package bowling.view;
 
-import java.util.Optional;
-
 import bowling.domain.result.Result;
 import bowling.domain.score.Score;
 import bowling.domain.score.Scores;
 
 public class ViewResult {
 
-	private final Result result;
-
-	ViewResult(Result result) {
-		this.result = result;
-	}
-
 	public static String printScores(Scores scores) {
-		Optional<Score> first = Optional.ofNullable(scores.getFirst()).orElseThrow(() ->
-			new IllegalArgumentException("not found"));
-		Optional<Score> second = Optional.ofNullable(scores.getSecond()).orElseThrow(() ->
-			new IllegalArgumentException("not found"));
+		Score first = scores.getFirst().orElseThrow(() -> new IllegalArgumentException("not found"));
+		if (first.getScore() == 10 && scores.isSecondScoreNull()) {
+			return String.format("   %s    ", first.getScore());
+		}
 
-		if (! first.isPresent()) {
-			return "      ";
-		}
-		if (! (first.map(Score::getScore).get() == 10) && ! second.isPresent()) {
-			return String.format("   %s    ", first.get().getScore());
-		}
 		Result result = scores.checkResult();
 		return result.renderString(scores);
 	}
