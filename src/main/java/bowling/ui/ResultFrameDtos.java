f3f05fc84395c935e6974f0a0d7a819a4e926df3
하재권 <hazxz@naver.com>
2020-07-06 08:31:46 +0900

    refactor(UI): UI 레이어가 도메인 레이어에 의존적이지 않도록 수정

--- src/main/java/bowling/ui/ResultFrameDtos.java
+++ src/main/java/bowling/ui/ResultFrameDtos.java
@@ -7,13 +7,25 @@ import java.util.stream.Collectors;
 
 public class ResultFrameDtos {
 
+    private final int firstFrameNo;
+    private final int finalFrameNo;
     private final List<ResultFrameDto> resultFrameDto;
 
-    public ResultFrameDtos(MultiUserFrames multiUserFrames) {
+    public ResultFrameDtos(int firstFrame, int finalFrameNo, MultiUserFrames multiUserFrames) {
+        this.firstFrameNo = firstFrame;
+        this.finalFrameNo = finalFrameNo;
         this.resultFrameDto = multiUserFrames
                 .forEachFrames()
                 .map(ResultFrameDto::new)
                 .collect(Collectors.toList());
+    }
+
+    public int getFirstFrameNo() {
+        return firstFrameNo;
+    }
+
+    public int getFinalFrameNo() {
+        return finalFrameNo;
     }
 
     public List<ResultFrameDto> getResultFrameDto() {
