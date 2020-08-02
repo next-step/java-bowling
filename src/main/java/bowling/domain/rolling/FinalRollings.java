9b343e6778332ae9dbabec91dd130855f1aaf66f
kiyeoneee <kyeoneee@gmail.com>
2020-08-02 19:35:23 +0900

    refactor (FinalRollings) : 보너스 투구 자료형 변경 * 보너스 투구는 존재하지 않을 수 있으므로 Optional로 변경

--- src/main/java/bowling/domain/rolling/FinalRollings.java
+++ src/main/java/bowling/domain/rolling/FinalRollings.java
@@ -4,14 +4,16 @@ import bowling.domain.frame.Score;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public class FinalRollings implements Rollings {
 
     private final NormalRollings normalRollings;
-    private Rolling bonusRolling;
+    private Optional<Rolling> bonusRolling;
 
     private FinalRollings() {
         this.normalRollings = NormalRollings.init();
+        this.bonusRolling = Optional.empty();
     }
 
     public static FinalRollings init() {
@@ -32,53 +34,50 @@ public class FinalRollings implements Rollings {
 
     private void setBonusRolling(int pinCount) {
         State state = State.valueOf(pinCount);
-        bonusRolling = new Rolling(state, pinCount);
+        bonusRolling = Optional.of(new Rolling(state, pinCount));
     }
 
     @Override
     public boolean isState(State state) {
-        if (bonusRolling != null) {
-            return bonusRolling.getState() == state;
-        }
-
-        return normalRollings.isState(state);
+        return bonusRolling.map(rolling -> rolling.isState(state))
+                .orElseGet(() -> normalRollings.isState(state));
     }
 
     @Override
     public boolean isRollingPossible() {
-        if (bonusRolling != null) {
+        if (bonusRolling.isPresent()) {
             return false;
         }
 
-        return normalRollings.isRollingPossible() || normalRollings.isStrikeOrSpare();
+        return normalRollings.isRollingPossible() ||
+                normalRollings.isStrikeOrSpare();
     }
 
     @Override
     public List<String> getStates() {
         List<String> states = normalRollings.getStates();
 
-        if (bonusRolling != null) {
-            states.add(bonusRolling.getStateFormat());
-        }
+        bonusRolling.ifPresent(
+                rolling -> states.add(rolling.getStateFormat())
+        );
 
         return states;
     }
 
     @Override
     public int calculateScore() {
-        if (bonusRolling != null) {
-            return normalRollings.calculateScore() + bonusRolling.getKnockedDownPinCount();
-        }
+        return bonusRolling.map(
+                rolling -> normalRollings.calculateScore() + rolling.getKnockedDownPinCount())
+                .orElseGet(normalRollings::calculateScore);
 
-        return normalRollings.calculateScore();
     }
 
     @Override
     public void calculateAdditionalScore(Score score) {
         normalRollings.calculateAdditionalScore(score);
 
-        if (!score.isCalculateDone() && bonusRolling != null) {
-            score.calculateAdditional(bonusRolling.getKnockedDownPinCount());
+        if (!score.isCalculateDone() && bonusRolling.isPresent()) {
+            score.calculateAdditional(bonusRolling.get().getKnockedDownPinCount());
         }
     }
 }
