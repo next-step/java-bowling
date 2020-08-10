d64caf9188f438a00b269568dc561d3704123e03
kiyeon <kyeoneee@gmail.com>
2020-08-10 13:51:38 +0900

    docs (README) : 새로 개발하기 위한 기능 목록 정리

--- src/main/java/bowling/BowlingApplication.java
+++ src/main/java/bowling/BowlingApplication.java
@@ -1,19 +1,7 @@
 package bowling;
 
-import bowling.domain.frame.Frames;
-import bowling.domain.player.Player;
-import bowling.view.InputView;
-import bowling.view.ResultView;
 
 public class BowlingApplication {
     public static void main(String[] args) {
-        Player player = InputView.getPlayer();
-        Frames frames = Frames.init();
-
-        ResultView.printFrame(player, frames);
-        while (frames.canPlay()) {
-            frames.play(InputView.getRollingCount(frames.getFrameNumber()));
-            ResultView.printFrame(player, frames);
-        }
     }
 }
