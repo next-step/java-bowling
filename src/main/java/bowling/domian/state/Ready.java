ae59fed9146b91c2640c12d5b552ae17ed995190
kiyeoneee <kyeoneee@gmail.com>
2020-08-12 00:03:53 +0900

    feat (Ready) : step3 - 프레임의 초기 상태 객체 기능 구현

--- src/main/java/bowling/domian/state/Ready.java
+++ src/main/java/bowling/domian/state/Ready.java
@@ -1,11 +1,17 @@
 package bowling.domian.state;
 
 public class Ready implements State {
-    public State bowl(int i) {
-        return new Ready();
+    public State bowl(int falledPinsCount) {
+        Pins pins = Pins.bowl(falledPinsCount);
+
+        if (pins.isStrike()) {
+            return new Strike();
+        }
+
+        return new FirstBowl();
     }
 
     public boolean isBowlPossible() {
-        return false;
+        return true;
     }
 }
