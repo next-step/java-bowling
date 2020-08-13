fa265eb989827916fbb2efd6abff1dd6fd0964b0
kiyeoneee <kyeoneee@gmail.com>
2020-08-14 01:26:30 +0900

    feat (Miss) : step3 - miss 상태 기능 구현

--- src/main/java/bowling/domian/state/Miss.java
+++ src/main/java/bowling/domian/state/Miss.java
@@ -1,12 +1,15 @@
 package bowling.domian.state;
 
+import bowling.domian.state.exception.BowlFinishedException;
+
 public class Miss implements State {
+    private static final String BOWL_FINISHED_EXCEPTION_MSG = "두 번의 투구가 모두 끝났습니다!";
     @Override
     public State bowl(int falledPinsCount) {
-        return null;
+        throw new BowlFinishedException(BOWL_FINISHED_EXCEPTION_MSG);
     }
 
     public boolean isFinished() {
-        return false;
+        return true;
     }
 }
