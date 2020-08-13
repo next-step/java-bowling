5e0bfa0bfb4fb3c615d1fb0123d707c7a42a3659
kiyeoneee <kyeoneee@gmail.com>
2020-08-14 01:27:40 +0900

    refactor (State) : step3 - exception 메세지 수정

--- src/main/java/bowling/domian/state/Spare.java
+++ src/main/java/bowling/domian/state/Spare.java
@@ -3,7 +3,7 @@ package bowling.domian.state;
 import bowling.domian.state.exception.BowlFinishedException;
 
 public class Spare implements State {
-    private static final String BOWL_FINISHED_EXCEPTION_MSG = "Spare 후에는 투구 불가능!";
+    private static final String BOWL_FINISHED_EXCEPTION_MSG = "Spare 후에는 투구가 불가능 합니다!";
 
     @Override
     public State bowl(int falledPinsCount) {
--- src/main/java/bowling/domian/state/Strike.java
+++ src/main/java/bowling/domian/state/Strike.java
@@ -3,7 +3,7 @@ package bowling.domian.state;
 import bowling.domian.state.exception.BowlFinishedException;
 
 public class Strike implements State {
-    private static final String BOWL_FINISHED_EXCEPTION_MSG = "스트라이크 후에는 투구 불가능!";
+    private static final String BOWL_FINISHED_EXCEPTION_MSG = "Strike 후에는 투구가 불가능 합니다!";
 
     @Override
     public State bowl(int falledPinsCount) {
