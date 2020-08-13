f42ca314c4c637ddfe7758f071660a74ed6e843d
kiyeoneee <kyeoneee@gmail.com>
2020-08-14 01:07:59 +0900

    feat (Spare) : step3 - spare 상태 기능 구현

--- src/main/java/bowling/domian/state/Spare.java
+++ src/main/java/bowling/domian/state/Spare.java
@@ -1,12 +1,16 @@
 package bowling.domian.state;
 
+import bowling.domian.state.exception.BowlFinishedException;
+
 public class Spare implements State {
+    private static final String BOWL_FINISHED_EXCEPTION_MSG = "Spare 후에는 투구 불가능!";
+
     @Override
     public State bowl(int falledPinsCount) {
-        return null;
+        throw new BowlFinishedException(BOWL_FINISHED_EXCEPTION_MSG);
     }
 
     public boolean isFinished() {
-        return false;
+        return true;
     }
 }
