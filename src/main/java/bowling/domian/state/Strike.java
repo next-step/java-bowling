d57fc858cc935fca348ef01051b13a0d61638ec3
kiyeoneee <kyeoneee@gmail.com>
2020-08-14 01:04:47 +0900

    feat (Strike) : step3 - stike 상태 기능 구현

--- src/main/java/bowling/domian/state/Strike.java
+++ src/main/java/bowling/domian/state/Strike.java
@@ -1,8 +1,16 @@
 package bowling.domian.state;
 
+import bowling.domian.state.exception.BowlFinishedException;
+
 public class Strike implements State {
+    private static final String BOWL_FINISHED_EXCEPTION_MSG = "스트라이크 후에는 투구 불가능!";
+
     @Override
     public State bowl(int falledPinsCount) {
-        return null;
+        throw new BowlFinishedException(BOWL_FINISHED_EXCEPTION_MSG);
+    }
+
+    public boolean isFinished() {
+        return true;
     }
 }
--- src/main/java/bowling/domian/state/exception/BowlFinishedException.java
+++ src/main/java/bowling/domian/state/exception/BowlFinishedException.java
@@ -1,4 +1,10 @@
 package bowling.domian.state.exception;
 
-public class BowlFinishedException {
+public class BowlFinishedException extends RuntimeException {
+    public BowlFinishedException() {
+    }
+
+    public BowlFinishedException(String message) {
+        super(message);
+    }
 }
