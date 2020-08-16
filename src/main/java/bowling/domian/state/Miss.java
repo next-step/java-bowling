aca3cb0455ddbbd56ad4626e4a9b2ccfb548ba12
kiyeon <kyeoneee@gmail.com>
2020-08-16 16:12:12 +0900

    feat (MissTest) : step3 - Miss 객체 점수 계산 테스트 추가

--- src/main/java/bowling/domian/state/Miss.java
+++ src/main/java/bowling/domian/state/Miss.java
@@ -1,9 +1,18 @@
 package bowling.domian.state;
 
+import bowling.domian.frame.Score;
 import bowling.domian.state.exception.BowlFinishedException;
 
 public class Miss implements State {
     private static final String BOWL_FINISHED_EXCEPTION_MSG = "두 번의 투구가 모두 끝났습니다!";
+
+    private Pins first;
+    private Pins second;
+
+    public Miss(Pins first, Pins second) {
+        this.first = first;
+        this.second = second;
+    }
 
     @Override
     public State bowl(int falledPinsCount) {
@@ -12,5 +21,13 @@ public class Miss implements State {
 
     public boolean isFinished() {
         return true;
+    }
+
+    public Score getScore() {
+        return Score.miss(3);
+    }
+
+    public Score calculateAdditional(Score score) {
+        return score;
     }
 }
--- src/test/java/bowling/domain/state/MissTest.java
+++ src/test/java/bowling/domain/state/MissTest.java
@@ -1,29 +1,50 @@
 package bowling.domain.state;
 
+import bowling.domian.frame.Score;
 import bowling.domian.state.Miss;
+import bowling.domian.state.Pins;
 import bowling.domian.state.exception.BowlFinishedException;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtensionContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class MissTest {
+    private Miss miss;
+
+    @Before
+    public void setUp() {
+        Miss miss = new Miss(Pins.bowl(1), Pins.bowl(3));
+    }
+
     @Test
     @DisplayName("투구 불가능 확인")
     public void isBowlImpossible() {
-        Miss miss = new Miss();
-
         assertThat(miss.isFinished()).isTrue();
     }
 
     @Test
     @DisplayName("투구 시도 시 exception 발생")
     public void bowlThrowException() {
-        Miss miss = new Miss();
-
         assertThatExceptionOfType(BowlFinishedException.class).isThrownBy(
                 () -> miss.bowl(4)
         );
+    }
+
+    @Test
+    @DisplayName("점수 계산 테스트")
+    public void getScore() {
+        assertThat(miss.getScore().getScore()).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("점수 추가 계산 테스트")
+    public void calculateAdditionalScore() {
+        Score score = Score.strike();
+
+        assertThat(miss.calculateAdditional(score).getScore()).isEqualTo(14);
     }
 }
