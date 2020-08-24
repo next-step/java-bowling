56e7756c1b3bc9dfbbbd8e46d670e30f579a20c4
kiyeoneee <kyeoneee@gmail.com>
2020-08-25 02:30:31 +0900

    feat (BowlingApplication) : step3 - 게임 컨트롤러 구현

--- src/main/java/bowling/BowlingApplication.java
+++ src/main/java/bowling/BowlingApplication.java
@@ -1,12 +1,21 @@
 package bowling;
 
 
+import bowling.domian.frame.Frames;
 import bowling.domian.player.Player;
 import bowling.view.InputView;
+import bowling.view.ResultView;
 
 public class BowlingApplication {
     public static void main(String[] args) {
         Player player = InputView.getPlayer();
+        Frames frames = Frames.of();
 
+        ResultView.printFrames(player, frames.getBoard());
+        while(!frames.isGameEnd()) {
+            int falledPinsCount = InputView.getFalledPinsCount(frames.getCurrentFrameNumber());
+            frames.bowl(falledPinsCount);
+            ResultView.printFrames(player, frames.getBoard());
+        }
     }
 }
