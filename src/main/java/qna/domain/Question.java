0f581b1e55f1b06c414d133a7f094919906540d8
newton <antksk@gmail.com>
2020-08-27 08:54:25 +0900

    [refactor][볼링-step1] 질문을 생성한 Owner 여부를 확인하는 코드 move(QnaService > Question)

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -6,6 +6,8 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import qna.CannotDeleteException;
+
 @Entity
 public class Question extends AbstractEntity {
     @Column(length = 100, nullable = false)
@@ -91,5 +93,11 @@ public class Question extends AbstractEntity {
     @Override
     public String toString() {
         return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
+    }
+
+    public void verifyOwner(User loginUser) throws CannotDeleteException {
+        if (!isOwner(loginUser)) {
+            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+        }
     }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,21 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @DisplayName("질문을 생성한 Owner 여부를 확인함")
+    @Test
+    void verifyOwner() {
+        assertThatExceptionOfType(CannotDeleteException.class)
+            .isThrownBy(()->Q1.verifyOwner(UserTest.SANJIGI))
+        ;
+    }
 }
