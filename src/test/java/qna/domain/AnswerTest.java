bdbf73db5c543a51ebad775b2e5e88e1aad70074
neojjc2 <neo.jjc+2@gmail.com>
2020-08-28 22:01:00 +0900

    [test][Step1] Question, Answer 테스트 코드 작성 완료

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,4 +1,67 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+import qna.fixture.QuestionFixture;
+import qna.fixture.UserFixture;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 public class AnswerTest {
+    @DisplayName("답변이 잘 생성되는지 확인")
+    @Test
+    public void answerTest() {
+        // given
+        String answerContent = "Contents of Answer";
+        User owner = UserFixture.make(1L, "userId", "Password", "Name", "Email");
+        Question question = QuestionFixture.make(1L, "Title", "Content", owner);
+
+        // when
+        Answer answer = new Answer(1L, owner, question, answerContent);
+
+        // then
+        assertAll(
+                () -> assertThat(answer.isOwner(owner)).isTrue(),
+                () -> assertThat(answer.getContents()).isEqualTo(answerContent),
+                () -> assertThat(answer.isDeleted()).isFalse(),
+                () -> assertThat(answer.getId()).isEqualTo(1L)
+        );
+    }
+
+    @DisplayName("작성자와 답변이 다를 경우 삭제 할 때 예외 발생")
+    @Test
+    public void makeDeleteThrownTest() {
+        // given
+        String answerContent = "Contents of Answer";
+        User owner = UserFixture.make(1L, "userId", "Password", "Name", "Email");
+        User other = UserFixture.make(2L, "userId2", "Password2", "Name2", "Email2");
+        Question question = QuestionFixture.make(1L, "Title", "Content", owner);
+
+        // when
+        Answer answer = new Answer(1L, owner, question, answerContent);
+
+        // then
+        assertThatThrownBy(() -> answer.makeDeleted(other))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessageContaining("답변을 삭제할 권한이 없습니다");
+    }
+
+    @DisplayName("작성자와 답변이 같을 경우 삭제")
+    @Test
+    public void makeDeleteTest() {
+        // given
+        String answerContent = "Contents of Answer";
+        User owner = UserFixture.make(1L, "userId", "Password", "Name", "Email");
+        Question question = QuestionFixture.make(1L, "Title", "Content", owner);
+
+        // when
+        Answer answer = new Answer(1L, owner, question, answerContent);
+        answer.makeDeleted(owner);
+
+        // then
+        assertThat(answer.isDeleted()).isTrue();
+    }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,4 +1,71 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+import qna.fixture.UserFixture;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 public class QuestionTest {
+    @DisplayName("질문이 잘 생성 되는지 확인")
+    @Test
+    public void questionTest() {
+        // given
+        Long questionId = 1L;
+        String title = "Title";
+        String contents = "Contents";
+        User writer = UserFixture.make(1L, "userId", "Password", "Name", "Email");
+
+        // when
+        Question question = new Question(questionId, title, contents).writeBy(writer);
+
+        // then
+        assertAll(
+                () -> assertThat(question.getId()).isEqualTo(questionId),
+                () -> assertThat(question.getTitle()).isEqualTo(title),
+                () -> assertThat(question.getContents()).isEqualTo(contents),
+                () -> assertThat(question.isOwner(writer)).isTrue(),
+                () -> assertThat(question.isDeleted()).isFalse()
+        );
+    }
+
+    @DisplayName("질문자와 삭제하려는 사용자가 같을 경우 삭제")
+    @Test
+    public void makeDeletedTest() {
+        // given
+        Long questionId = 1L;
+        String title = "Title";
+        String contents = "Contents";
+        User writer = UserFixture.make(1L, "userId", "Password", "Name", "Email");
+
+        // when
+        Question question = new Question(questionId, title, contents).writeBy(writer);
+
+        // then
+        assertDoesNotThrow(() -> question.makeDeleted(writer));
+        assertThat(question.isDeleted()).isTrue();
+    }
+
+    @DisplayName("질문자와 삭제하려는 사용자가 다를 경우 예외 발생")
+    @Test
+    public void makeDeletedThrownTest() {
+        // given
+        Long questionId = 1L;
+        String title = "Title";
+        String contents = "Contents";
+        User writer = UserFixture.make(1L, "userId", "Password", "Name", "Email");
+        User other = UserFixture.make(2L, "userId", "Password", "Other name", "Other email");
+
+        // when
+        Question question = new Question(questionId, title, contents).writeBy(writer);
+
+        // then
+        assertThatThrownBy(() -> question.makeDeleted(other))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessageContaining("질문을 삭제할 권한이 없습니다");
+    }
 }
