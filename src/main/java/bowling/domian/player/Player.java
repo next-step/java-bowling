c4e47b60a9d01889d3001818d12289e257f85f97
kiyeoneee <kyeoneee@gmail.com>
2020-08-30 19:44:21 +0900

    feat (Player) : step4 - 사용자 별 프레임 할당

--- src/main/java/bowling/domian/player/Player.java
+++ src/main/java/bowling/domian/player/Player.java
@@ -1,12 +1,16 @@
 package bowling.domian.player;
 
+import bowling.domian.frame.Frames;
+
 import java.util.List;
 
 public class Player {
     private final Name name;
+    private final Frames frames;
 
     private Player(Name name) {
         this.name = name;
+        this.frames = Frames.of();
     }
 
     public static Player get(String name) {
@@ -17,18 +21,20 @@ public class Player {
         return name.getName();
     }
 
+    // TODO: 2020/08/30 리팩토링
     public List<String> getDescs() {
-        return null;
+        return frames.getBoard().getDescs();
     }
 
     public List<Integer> getTotalScores() {
-        return null;
+        return frames.getBoard().getTotalScores();
     }
 
     public boolean isFrameNumber(int frameNumber) {
-        return false;
+        return frames.isFrameNumber(frameNumber);
     }
 
     public void bowl(int falledPinsCount) {
+        frames.bowl(falledPinsCount);
     }
 }
