671802694ca5b48ef41e839a95edc1319a4865e3
kiyeoneee <kyeoneee@gmail.com>
2020-08-30 21:19:34 +0900

    feat (Players) : step4 - 투구해야 하는 사용자만 반환 구현

--- src/main/java/bowling/domian/player/Players.java
+++ src/main/java/bowling/domian/player/Players.java
@@ -2,13 +2,17 @@ package bowling.domian.player;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Players {
+    private static final int INITIAL_FRAME_NUMBER = 1;
 
     private final List<Player> players;
+    private int currentFrameNumber;
 
     private Players(List<Player> players) {
          this.players = players;
+         this.currentFrameNumber = INITIAL_FRAME_NUMBER;
     }
 
     public static Players of(List<Player> players) {
@@ -16,11 +20,23 @@ public class Players {
     }
 
     public List<Player> getCurrentPlayers() {
-        return null;
+        checkPlayersFrameNumber();
+
+        return players.stream()
+                .filter(player -> player.isFrameNumber(currentFrameNumber) && !player.isGameEnd())
+                .collect(Collectors.toList());
     }
 
     public List<Player> getPlayers() {
         return Collections.unmodifiableList(players);
+    }
+
+    private void checkPlayersFrameNumber() {
+        boolean needNextFrameNumber = players.stream().noneMatch(player -> player.isFrameNumber(currentFrameNumber));
+
+        if (needNextFrameNumber) {
+            this.currentFrameNumber += 1;
+        }
     }
 
     public boolean isGameEnd() {
