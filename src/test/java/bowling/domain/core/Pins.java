d33aff392e83fb3a8a868fec69f6cd25ff1bdcaf
newton <antksk@gmail.com>
2020-09-01 08:45:09 +0900

    [fix][볼링-step2] 넘어지는 핀(pins.getFallenPinCount()) 기준으로 스트라이크, 스페어, 미스, 거터 테스트 코드 작성

---
+++ src/test/java/bowling/domain/core/Pins.java
@@ -0,0 +1,75 @@
+package bowling.domain.core;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+final class Pins {
+    private static final int MIN_FALLEN_PIN_COUNT = 0;
+    private static final int MAX_FALLEN_PIN_COUNT = 10;
+    private static final List<Pins> cachedPins;
+    static final String ERROR_MESSAGE = "지정된 넘어진 핀 갯수가 잘못되었습니다.";
+    private final int fallenPinCount;
+
+    static {
+        final List<Pins> pins = IntStream.rangeClosed(MIN_FALLEN_PIN_COUNT, MAX_FALLEN_PIN_COUNT)
+                                         .mapToObj(Pins::new)
+                                         .collect(toList());
+        cachedPins = Collections.unmodifiableList(pins);
+    }
+
+    private Pins(int fallenPinCount) {
+        this.fallenPinCount = fallenPinCount;
+    }
+
+    public static Pins of(int fallenPinCount) {
+        verifyPins(fallenPinCount);
+        return cachedPins.get(fallenPinCount);
+    }
+
+    private static void verifyPins(int fallenPinCount) {
+        if (MIN_FALLEN_PIN_COUNT > fallenPinCount || MAX_FALLEN_PIN_COUNT < fallenPinCount) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+    }
+
+    int getFallenPinCount() {
+        return fallenPinCount;
+    }
+
+    boolean isStrike() {
+        return MAX_FALLEN_PIN_COUNT == getFallenPinCount();
+    }
+
+    boolean isSpare(Pins secondRoll) {
+        return MAX_FALLEN_PIN_COUNT == (getFallenPinCount() + secondRoll.getFallenPinCount());
+    }
+
+    boolean isMiss(Pins secondRoll) {
+        return !isSpare(secondRoll);
+    }
+
+    boolean isGutter() {
+        return MIN_FALLEN_PIN_COUNT == getFallenPinCount();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pins pins = (Pins) o;
+        return getFallenPinCount() == pins.getFallenPinCount();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getFallenPinCount());
+    }
+}
---
+++ src/test/java/bowling/domain/core/PinsTest.java
@@ -0,0 +1,95 @@
+package bowling.domain.core;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class PinsTest {
+    @DisplayName("단일객체만 생성하는 지 확인")
+    @Test
+    void singleton() {
+        Pins pins = Pins.of(0);
+        Pins pins2= Pins.of(0);
+        assertThat(pins == pins2).isTrue();
+        assertThat(pins.equals(pins2)).isTrue();
+    }
+
+    @DisplayName("잘못 설정된 넘어진 핀 개수를 설정하면 오류 발생 유무 확인")
+    @ParameterizedTest
+    @ValueSource(ints = {-1,11})
+    void wrongFellenPinsCount(final int wrongPins) {
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> Pins.of(wrongPins))
+            .withMessage(Pins.ERROR_MESSAGE);
+    }
+
+    @DisplayName("strike 유무 확인")
+    @Test
+    void isStrike() {
+        assertThat(Pins.of(0).isStrike()).isFalse();
+        assertThat(Pins.of(10).isStrike()).isTrue();
+    }
+
+    static Stream<Arguments> provideSpareTestCase(){
+        return Stream.of(
+            Arguments.of(1, 9, true)
+            , Arguments.of(2, 8, true)
+            , Arguments.of(5, 5, true)
+            , Arguments.of(9, 1, true)
+            , Arguments.of(4, 6, true)
+            , Arguments.of(5, 0, false)
+            , Arguments.of(2, 0, false)
+            , Arguments.of(3, 2, false)
+            , Arguments.of(1, 7, false)
+            , Arguments.of(0, 7, false)
+        );
+    }
+
+    @DisplayName("spare 유무 확인")
+    @ParameterizedTest
+    @MethodSource("provideSpareTestCase")
+    void isSpare(int first, int second, boolean result) {
+        Pins firstRoll = Pins.of(first);
+        Pins secondRoll = Pins.of(second);
+        assertThat(firstRoll.isSpare(secondRoll)).isEqualTo(result);
+    }
+
+    static Stream<Arguments> provideMissTestCase(){
+        return Stream.of(
+            Arguments.of(1, 9, false)
+            , Arguments.of(2, 8, false)
+            , Arguments.of(5, 5, false)
+            , Arguments.of(9, 1, false)
+            , Arguments.of(4, 6, false)
+            , Arguments.of(5, 0, true)
+            , Arguments.of(2, 0, true)
+            , Arguments.of(3, 2, true)
+            , Arguments.of(1, 7, true)
+            , Arguments.of(0, 7, true)
+        );
+    }
+
+    @DisplayName("miss 유무 확인")
+    @ParameterizedTest
+    @MethodSource("provideMissTestCase")
+    void isMiss(int first, int second, boolean result) {
+        Pins firstRoll = Pins.of(first);
+        Pins secondRoll = Pins.of(second);
+        assertThat(firstRoll.isMiss(secondRoll)).isEqualTo(result);
+    }
+
+    @DisplayName("gutter 유무 확인")
+    @Test
+    void isGutter() {
+        assertThat(Pins.of(0).isGutter()).isTrue();
+        assertThat(Pins.of(10).isGutter()).isFalse();
+    }
+}
