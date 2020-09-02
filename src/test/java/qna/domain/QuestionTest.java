8622ee979a85144fd686f699bc22932b3a0edb92
yongdae <7942yongdae@gmail.com>
2020-09-03 02:12:06 +0900

    refactor: Question 클래스 delete 함수 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,53 @@
 package qna.domain;
 
+import org.junit.Before;
+import org.junit.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    private Question question;
+    private Answer answer;
+
+    @Before
+    public void setUp() throws Exception {
+        question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        answer = new Answer(11L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+        question.addAnswer(answer);
+    }
+
+    @Test
+    public void delete_성공() throws Exception {
+        assertThat(question.isDeleted()).isFalse();
+        question.delete(UserTest.JAVAJIGI);
+
+        assertThat(question.isDeleted()).isTrue();
+    }
+
+    @Test
+    public void delete_다른_사람이_쓴_글() throws Exception {
+        assertThatThrownBy(() -> {
+            question.delete(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    public void delete_성공_질문자_답변자_같음() throws Exception {
+        question.delete(UserTest.JAVAJIGI);
+
+        assertThat(question.isDeleted()).isTrue();
+        assertThat(answer.isDeleted()).isTrue();
+    }
+
+    @Test
+    public void delete_답변_중_다른_사람이_쓴_글() throws Exception {
+        assertThatThrownBy(() -> {
+            question.delete(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
 }
