2ba500a9dd3e510b265ab51b2afa792b4bf1fede
hJJANG <dacapolife87@gmail.com>
2020-09-09 01:03:44 +0900

    feat : List<DeleteHistory>의 일급컬렉션 추가

---
+++ src/main/java/qna/domain/DeleteHistories.java
@@ -0,0 +1,24 @@
+package qna.domain;
+
+import java.util.List;
+
+public class DeleteHistories {
+
+    private List<DeleteHistory> deleteHistories;
+
+    private DeleteHistories(List deleteHistories) {
+        this.deleteHistories = deleteHistories;
+    }
+
+    public static DeleteHistories create(List deleteHistories){
+        return new DeleteHistories(deleteHistories);
+    }
+
+    public void addHistory(DeleteHistory deleteHistory){
+        deleteHistories.add(deleteHistory);
+    }
+
+    public List<DeleteHistory> getDeleteHistories() {
+        return deleteHistories;
+    }
+}
