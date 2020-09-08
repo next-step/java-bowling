27afb98acc00fc6ad53fe6cf1a76c266e0801c01
hJJANG <dacapolife87@gmail.com>
2020-09-09 01:04:33 +0900

    test : Answer클래스의 삭제 기능 테스트케이스 추가

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,36 @@
 package qna.domain;
 
+import org.junit.Test;
+import qna.CannotDeleteException;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static qna.domain.QuestionTest.Q1;
+
 public class AnswerTest {
-    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, Q1, "Answers Contents1");
+    public static final Answer A2 = new Answer(UserTest.SANJIGI, Q1, "Answers Contents2");
+    public static final Answer A3 = new Answer(UserTest.JAVAJIGI, Q1, "Answers Contents3");
+
+    @Test
+    public void delete_정상적으로_삭제처리되어_상태가_변하는지_확인() throws CannotDeleteException {
+        A1.delete(UserTest.JAVAJIGI, DeleteHistories.create(new ArrayList()));
+        assertTrue(A1.isDeleted());
+    }
+
+    @Test
+    public void delete_작성자와_요청자가_다를때_확인() {
+        assertThatThrownBy(() -> {
+            A1.delete(UserTest.SANJIGI, DeleteHistories.create(new ArrayList()));
+        }).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    public void delete_삭제이력_추가_확인() throws CannotDeleteException {
+        DeleteHistories deleteHistories = A1.delete(UserTest.JAVAJIGI, DeleteHistories.create(new ArrayList()));
+        assertThat(deleteHistories.getDeleteHistories()).hasSize(1);
+    }
 }
