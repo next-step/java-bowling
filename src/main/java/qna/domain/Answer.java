14c661016596246c291bbe9db482e1161f984e92
LimHanGyeol <dlagksruf19@naver.com>
2020-09-10 14:53:12 +0900

    refactor : List<Answer>를 Answers 클래스로 래핑

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -5,6 +5,7 @@ import qna.global.exception.UnAuthorizedException;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 public class Answer extends AbstractEntity {
@@ -67,6 +68,23 @@ public class Answer extends AbstractEntity {
 
     public void toQuestion(Question question) {
         this.question = question;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Answer answer = (Answer) o;
+        return deleted == answer.deleted &&
+                Objects.equals(writer, answer.writer) &&
+                Objects.equals(question, answer.question) &&
+                Objects.equals(contents, answer.contents);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), writer, question, contents, deleted);
     }
 
     @Override
--- src/test/java/qna/service/QnaServiceTest.java
+++ src/test/java/qna/service/QnaServiceTest.java
@@ -32,12 +32,14 @@ public class QnaServiceTest {
 
     private Question question;
     private Answer answer;
+    private Answers answers;
 
     @Before
     public void setUp() throws Exception {
         question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
         answer = new Answer(11L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-        question.addAnswer(answer);
+        answers = question.getAnswers();
+        answers.addAnswer(answer, question);
     }
 
     @Test
