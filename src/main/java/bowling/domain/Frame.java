6037fbdbd0b69faf2c62e390b073243fcbeaba3b
yongdae <7942yongdae@gmail.com>
2020-09-11 02:30:07 +0900

    refactor: 기존 코드 삭제

--- src/main/java/bowling/domain/Frame.java
+++ src/main/java/bowling/domain/Frame.java
@@ -2,81 +2,18 @@ package bowling.domain;
 
 public class Frame {
 
-    public static final int BEGIN_STAGE = 1;
-    public static final int END_STAGE = 10;
-    public static final int BONUS_STAGE = END_STAGE;
+    public static final int PIN_COUNT = 10;
+    private int pinCount;
 
-    public static final int FIRST_STEP = 1;
-    public static final int FINAL_STEP = 2;
-    public static final int BONUS_STEP = FINAL_STEP + 1;
-
-    private int stage;
-    private int step;
-    private Pins pins;
-    private Pins bonusPins;
-    private GameResults results;
-
-    private Frame(int stage, Pins pins, Pins bonusPins, GameResults gameResults) {
-        this.stage = stage;
-        this.step = FIRST_STEP;
-        this.pins = pins;
-        this.bonusPins = bonusPins;
-        this.results = gameResults;
+    private Frame(int pinCount) {
+        this.pinCount = pinCount;
     }
 
-    public static Frame from(int stage) {
-        if (stage < BEGIN_STAGE) {
-            throw new IllegalArgumentException(String.format("%s 보다 작은 값은 설정될수 없습니다. [%s]", BEGIN_STAGE, stage));
-        }
-
-        if (stage > END_STAGE) {
-            throw new IllegalArgumentException(String.format("%s 보다 큰 값은 설정될수 없습니다. [%s]", END_STAGE, stage));
-        }
-
-        return new Frame(stage, Pins.create(), Pins.create(), GameResults.create());
+    public static Frame create() {
+        return new Frame(PIN_COUNT);
     }
 
-    public boolean hasNextTurn() {
-        if (hasBonusStep()) {
-            return true;
-        }
-
-        if (step > FINAL_STEP) {
-            return false;
-        }
-
-        return !pins.isClear();
-    }
-
-    private boolean hasBonusStep() {
-        return stage == BONUS_STAGE && pins.isClear() && step <= BONUS_STEP;
-    }
-
-    public void record(int hitCount) {
-        if (hasBonusStep()) {
-            bonusRecord(hitCount);
-            return;
-        }
-
-        hitting(pins, hitCount);
-    }
-
-    private void bonusRecord(int hitCount) {
-        step = BONUS_STEP;
-        hitting(bonusPins, hitCount);
-    }
-
-    private void hitting(Pins pins, int hitCount) {
-        pins.hitting(hitCount);
-        results.record(GameResult.toResult(stage, step, hitCount, pins.isClear()));
-        step++;
-    }
-
-    public int getStage() {
-        return stage;
-    }
-
-    public GameResults getResults() {
-        return results;
+    public int getPinCount() {
+        return pinCount;
     }
 }
--- src/test/java/bowling/domain/FrameTest.java
+++ src/test/java/bowling/domain/FrameTest.java
@@ -3,77 +3,13 @@ package bowling.domain;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class FrameTest {
-
-    public static final int STRIKE = 10;
-    public static final int GUTTER = 0;
-
+public class FrameTest {
     @Test
-    void from() {
-        assertThat(Frame.from(Frame.BEGIN_STAGE)).isNotNull();
+    void constructor() {
+        Frame frame = Frame.create();
+        assertThat(frame.getPinCount()).isEqualTo(Frame.PIN_COUNT);
     }
 
-    @Test
-    void fromThrowException() {
-        assertThatThrownBy(() -> Frame.from(Frame.BEGIN_STAGE - 1))
-                .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> Frame.from(Frame.END_STAGE + 1))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
 
-    @Test
-    void hasNextTurn() {
-        Frame frame = Frame.from(Frame.BEGIN_STAGE);
-        int count = 0;
-
-        while (frame.hasNextTurn()) {
-            frame.record(GUTTER);
-            count++;
-        }
-
-        assertThat(count).isEqualTo(2);
-    }
-
-    @Test
-    void hasNextTurnWithBonusStep() {
-        Frame frame = Frame.from(Frame.END_STAGE);
-        int count = 0;
-
-        while (frame.hasNextTurn()) {
-            frame.record(count == 0 ? GUTTER : STRIKE);
-            count++;
-        }
-
-        assertThat(count).isEqualTo(3);
-    }
-
-    @Test
-    void record() {
-        Frame frame = Frame.from(Frame.END_STAGE);
-        frame.record(GUTTER);
-        assertThat(frame.getResults().getHistory().size()).isEqualTo(1);
-        frame.record(1);
-        assertThat(frame.getResults().getHistory().size()).isEqualTo(2);
-    }
-
-    @Test
-    void getStage() {
-        Frame frame = Frame.from(Frame.BEGIN_STAGE);
-
-        assertThat(frame.getStage()).isEqualTo(1);
-        frame.record(GUTTER);
-        assertThat(frame.getStage()).isEqualTo(1);
-    }
-
-    @Test
-    void getResults() {
-        Frame frame = Frame.from(Frame.BEGIN_STAGE);
-
-        frame.record(GUTTER);
-        frame.record(STRIKE);
-
-        assertThat(frame.getResults().getHistory().size()).isEqualTo(2);
-    }
 }
