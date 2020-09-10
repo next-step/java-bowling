e7075068de5b6ed9852a5092124ea763856532aa
yongdae <7942yongdae@gmail.com>
2020-09-10 20:44:07 +0900

    refactor: https://github.com/next-step/java-bowling/pull/262#discussion_r485779015 수정 반영

--- src/main/java/bowling/view/Printer.java
+++ src/main/java/bowling/view/Printer.java
@@ -7,6 +7,12 @@ import bowling.domain.GameResults;
 import java.util.stream.Collectors;
 
 public class Printer {
+    private static final String STAGE_PREFIX_STRING = "| NAME |";
+    private static final String STAGE_FORMAT = "  %02d  |";
+    private static final String BOWLER_NAME_FORMAT = "| %4s |";
+    private static final String GAME_RESULT_FORMAT = "  %-3s |";
+    private static final String GAME_RESULT_DELIMITER = "|";
+
     public static void showResult(Bowler bowler) {
         showHead(bowler);
         showBody(bowler);
@@ -14,23 +20,23 @@ public class Printer {
     }
 
     private static void showHead(Bowler bowler) {
-        System.out.print("| NAME |");
+        System.out.print(STAGE_PREFIX_STRING);
         for (Frame frame : bowler.getFrames()) {
-            System.out.print(String.format("  %02d  |", frame.getStage()));
+            System.out.print(String.format(STAGE_FORMAT, frame.getStage()));
         }
         System.out.println();
     }
 
     private static void showBody(Bowler bowler) {
-        System.out.print(String.format("| %4s |", bowler.toString()));
+        System.out.print(String.format(BOWLER_NAME_FORMAT, bowler.toString()));
 
         for (Frame frame : bowler.getFrames()) {
             GameResults results = frame.getResults();
             System.out.printf(
-                    String.format("  %-3s |",
+                    String.format(GAME_RESULT_FORMAT,
                             results.getHistory().stream()
                                     .map(String::valueOf)
-                                    .collect(Collectors.joining("|"))
+                                    .collect(Collectors.joining(GAME_RESULT_DELIMITER))
                     )
             );
         }
