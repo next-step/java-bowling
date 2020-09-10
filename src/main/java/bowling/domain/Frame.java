04f0d03f4b1a5457cce3422061e33f84294761d6
yongdae <7942yongdae@gmail.com>
2020-09-11 03:06:53 +0900

    feat: 프레임 역할 클래스 추가

--- src/main/java/bowling/domain/Frame.java
+++ src/main/java/bowling/domain/Frame.java
@@ -1,19 +1,54 @@
 package bowling.domain;
 
 public class Frame {
-
     public static final int PIN_COUNT = 10;
+    public static final int BEGIN_NUMBER = 1;
+    public static final int END_NUMBER = 10;
+
+    private int number;
+    private boolean next;
     private int pinCount;
 
-    private Frame(int pinCount) {
+    private Frame(int number, int pinCount, boolean next) {
+        this.number = number;
         this.pinCount = pinCount;
+        this.next = next;
     }
 
-    public static Frame create() {
-        return new Frame(PIN_COUNT);
+    private static Frame from(int number) {
+        if (number < BEGIN_NUMBER) {
+            throw new IllegalArgumentException(String.format("%s 보다 작은 값은 설정할 수 없습니다. [%s]", BEGIN_NUMBER, number));
+        }
+
+        if (number > END_NUMBER) {
+            throw new IllegalArgumentException(String.format("%s 보다 큰 값은 설정할 수 없습니다. [%s]", END_NUMBER, number));
+        }
+
+        return new Frame(number, PIN_COUNT, number < END_NUMBER);
     }
 
-    public int getPinCount() {
+    public static Frame first() {
+        return from(BEGIN_NUMBER);
+    }
+
+    public Frame next() {
+        if (!next) {
+            throw new RuntimeException("남아있는 프레임이 없습니다.");
+        }
+
+        return from(number + 1);
+    }
+
+    public boolean isFinish() {
+        return number == END_NUMBER;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int hit(int count) {
+        pinCount -= count;
         return pinCount;
     }
 }
--- src/test/java/bowling/domain/FrameTest.java
+++ src/test/java/bowling/domain/FrameTest.java
@@ -6,10 +6,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class FrameTest {
     @Test
-    void constructor() {
-        Frame frame = Frame.create();
-        assertThat(frame.getPinCount()).isEqualTo(Frame.PIN_COUNT);
+    void first() {
+        assertThat(Frame.first()).isNotNull();
     }
 
+    @Test
+    void next() {
+        assertThat(Frame.first().next()).isNotNull();
+    }
 
+    @Test
+    void nextToFinish() {
+        Frame frame = Frame.first();
+        int count = 1;
+
+        while (!frame.isFinish()) {
+            frame = frame.next();
+            count++;
+        }
+
+        assertThat(count).isEqualTo(Frame.END_NUMBER);
+    }
+
+    @Test
+    void getNumber() {
+        assertThat(Frame.first().getNumber()).isEqualTo(Frame.BEGIN_NUMBER);
+    }
+
+    @Test
+    void hit() {
+        int NO_COUNT = 0;
+        assertThat(Frame.first().hit(NO_COUNT)).isEqualTo(Frame.PIN_COUNT);
+        assertThat(Frame.first().hit(Frame.PIN_COUNT)).isEqualTo(NO_COUNT);
+    }
 }
