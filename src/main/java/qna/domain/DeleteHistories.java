ce04e7441fb3f7c0e978f419428093413dba0ab8
John <bass0223@naver.com>
2020-09-11 23:34:11 +0900

    List<DeleteHistory>에 대한 일급 컬렉션인 DeleteHistories 생성

---
+++ src/main/java/qna/domain/DeleteHistories.java
@@ -0,0 +1,34 @@
+package qna.domain;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class DeleteHistories {
+	private final List<DeleteHistory> deleteHistories;
+
+	private DeleteHistories(List<DeleteHistory> deleteHistories) {
+		this.deleteHistories = deleteHistories;
+	}
+
+	public static DeleteHistories of(DeleteHistory deleteHistory) {
+		return new DeleteHistories(Arrays.asList(deleteHistory));
+	}
+
+	public static DeleteHistories of(List<DeleteHistory> deleteHistories) {
+		return new DeleteHistories(deleteHistories);
+	}
+
+	public DeleteHistories merge(DeleteHistories other) {
+		return of(Stream.of(this.deleteHistories, other.deleteHistories)
+						.flatMap(Collection::stream)
+						.collect(Collectors.toList()));
+	}
+
+	public List<DeleteHistory> getDeleteHistories() {
+		return deleteHistories.stream()
+							  .collect(Collectors.toList());
+	}
+}
