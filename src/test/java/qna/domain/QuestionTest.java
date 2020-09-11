bfa4858dc3a69989b678fafa521c04ffc93a1e03
John <bass0223@naver.com>
2020-09-11 23:40:30 +0900

    각각 테스트 케이스 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,32 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+import qna.CannotDeleteException;
+
 public class QuestionTest {
-    public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-    public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+	public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+	public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+	@Test
+	public void otherUserDeleteException() {
+		assertThatThrownBy(() -> Q2.delete(UserTest.JAVAJIGI))
+				.isInstanceOf(CannotDeleteException.class)
+				.hasMessage("질문을 삭제할 권한이 없습니다.");
+	}
+
+	@Test
+	public void deleteSuccessTest() throws CannotDeleteException {
+		Q2.delete(UserTest.SANJIGI);
+		assertThat(Q2.isDeleted()).isEqualTo(Boolean.TRUE);
+	}
+
+	@Test
+	public void addDeleteHistoryTest() throws CannotDeleteException {
+		DeleteHistories deleteHistories = Q1.delete(UserTest.JAVAJIGI);
+		assertThat(deleteHistories.getDeleteHistories()).isNotNull();
+	}
 }
--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,6 +1,20 @@
 package qna.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
 public class UserTest {
-    public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
-    public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+	public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
+	public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+
+	@Test
+	public void sameUserTest() {
+		assertThat(JAVAJIGI).isEqualTo(new User(1L, "javajigi", "password", "name", "javajigi@slipp.net"));
+	}
+
+	@Test
+	public void otherUserTest() {
+		assertThat(SANJIGI).isNotEqualTo(new User(3L, "sanjigi", "password", "name", "sanjigi@slipp.net"));
+	}
 }
