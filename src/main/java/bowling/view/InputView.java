2e26c726540cf5558f586d2155ce10b960d0365d
LimHanGyeol <dlagksruf19@naver.com>
2020-09-11 22:56:31 +0900

    feat : 투구 입력 기능 구현

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String PRINT_INPUT_PLAYER = "플레이어 이름은? (3 english letters) : ";
+    private static final String PRINT_INPUT_FRAME_NUMBER_PITCH = "프레임 투구 : ";
 
     private static final Scanner scanner = new Scanner(System.in);
 
@@ -16,5 +17,9 @@ public class InputView {
         return scanner.nextLine();
     }
 
+    public static String inputPitch(int frameNumber) {
+        System.out.print(frameNumber + PRINT_INPUT_FRAME_NUMBER_PITCH);
+        return scanner.nextLine();
+    }
 
 }
