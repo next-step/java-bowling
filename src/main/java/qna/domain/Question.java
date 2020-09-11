c6d7494a25379cc64fc52f0b26b5ef29e66f9eb2
John <bass0223@naver.com>
2020-09-11 23:37:39 +0900

    삭제의 핵심 기능을 Question에서 구현

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -1,95 +1,120 @@
 package qna.domain;
 
-import org.hibernate.annotations.Where;
-
-import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+
+import org.hibernate.annotations.Where;
+
+import qna.CannotDeleteException;
+
 @Entity
 public class Question extends AbstractEntity {
-    @Column(length = 100, nullable = false)
-    private String title;
+	@Column(length = 100, nullable = false)
+	private String title;
 
-    @Lob
-    private String contents;
+	@Lob
+	private String contents;
 
-    @ManyToOne
-    @JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"))
-    private User writer;
+	@ManyToOne
+	@JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"))
+	private User writer;
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
-    @Where(clause = "deleted = false")
-    @OrderBy("id ASC")
-    private List<Answer> answers = new ArrayList<>();
+	@OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+	@Where(clause = "deleted = false")
+	@OrderBy("id ASC")
+	private List<Answer> answers = new ArrayList<>();
 
-    private boolean deleted = false;
+	private boolean deleted = false;
 
-    public Question() {
-    }
+	public Question() {
+	}
 
-    public Question(String title, String contents) {
-        this.title = title;
-        this.contents = contents;
-    }
+	public Question(String title, String contents) {
+		this.title = title;
+		this.contents = contents;
+	}
 
-    public Question(long id, String title, String contents) {
-        super(id);
-        this.title = title;
-        this.contents = contents;
-    }
+	public Question(long id, String title, String contents) {
+		super(id);
+		this.title = title;
+		this.contents = contents;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public Question setTitle(String title) {
-        this.title = title;
-        return this;
-    }
+	public Question setTitle(String title) {
+		this.title = title;
+		return this;
+	}
 
-    public String getContents() {
-        return contents;
-    }
+	public String getContents() {
+		return contents;
+	}
 
-    public Question setContents(String contents) {
-        this.contents = contents;
-        return this;
-    }
+	public Question setContents(String contents) {
+		this.contents = contents;
+		return this;
+	}
 
-    public User getWriter() {
-        return writer;
-    }
+	public User getWriter() {
+		return writer;
+	}
 
-    public Question writeBy(User loginUser) {
-        this.writer = loginUser;
-        return this;
-    }
+	public Question writeBy(User loginUser) {
+		this.writer = loginUser;
+		return this;
+	}
 
-    public void addAnswer(Answer answer) {
-        answer.toQuestion(this);
-        answers.add(answer);
-    }
+	public void addAnswer(Answer answer) {
+		answer.toQuestion(this);
+		answers.add(answer);
+	}
 
-    public boolean isOwner(User loginUser) {
-        return writer.equals(loginUser);
-    }
+	private boolean isOwner(User loginUser) {
+		return writer.equals(loginUser);
+	}
 
-    public Question setDeleted(boolean deleted) {
-        this.deleted = deleted;
-        return this;
-    }
+	public boolean isDeleted() {
+		return deleted;
+	}
 
-    public boolean isDeleted() {
-        return deleted;
-    }
+	public Answers getAnswers() {
+		return Answers.of(answers);
+	}
 
-    public List<Answer> getAnswers() {
-        return answers;
-    }
+	@Override
+	public String toString() {
+		return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
+	}
 
-    @Override
-    public String toString() {
-        return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
-    }
+	public DeleteHistories delete(User loginUser) throws CannotDeleteException {
+		this.validateEqualsWithLoginUser(loginUser);
+		this.deleted = true;
+		DeleteHistories questionDeleteHistories = DeleteHistories.of(new DeleteHistory(ContentType.QUESTION,
+																					   getId(),
+																					   writer,
+																					   LocalDateTime.now()));
+		DeleteHistories answerDeleteHistories = getAnswers().deleteAnswers(loginUser);
+
+		return questionDeleteHistories.merge(answerDeleteHistories);
+	}
+
+	private void validateEqualsWithLoginUser(User loginUser) throws CannotDeleteException {
+		if (!this.isOwner(loginUser)) {
+			throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+		}
+	}
 }
