26e5949841fb79d88276d35c1cf97950805eccab
skid901 <tlrudejr1993@naver.com>
2020-09-14 00:07:10 +0900

    test: test Question class & Answer class
    1. test Question.validateOwnerWhenDeleting() method
    2. test Question.validateAnswersWhenDeleting() method
    3. test Question.delete() method
    4. test Answer.validateOwnerWhenDeleting() method
    5. test Answer.delete() method

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,28 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.BDDAssertions.then;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("답변이 로그인 된 사용자에 의해 작성 됐는지 검증")
+    void validateOwnerWhenDeleting() {
+        assertThatThrownBy(() -> A1.validateOwnerWhenDeleting(UserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("답변을 삭제하는 메소드 검증")
+    void delete() {
+        A1.delete();
+        then(A1.isDeleted()).isTrue();
+    }
 }
