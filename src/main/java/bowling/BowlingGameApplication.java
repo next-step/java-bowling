4af8843c656ef6d2173f6f0597ec54986796aada
John <bass0223@naver.com>
2020-09-14 21:50:55 +0900

    도메인 로직들 변경에 따른 Controller 수정

--- src/main/java/bowling/BowlingGameApplication.java
+++ src/main/java/bowling/BowlingGameApplication.java
@@ -1,22 +1,27 @@
 package bowling;
 
+import java.util.List;
+
 import bowling.frame.Frames;
 import bowling.pin.Pins;
-import bowling.user.Player;
+import bowling.user.Players;
 import bowling.view.InputView;
 import bowling.view.OutputView;
 
 public class BowlingGameApplication {
 
 	public static void main(String[] args) {
-		Player player = Player.of(InputView.inputPlayerName());
-		Frames frames = Frames.newInstance();
-		OutputView.viewInit(player);
+		//Player player = Player.of(InputView.inputPlayerName());
+		int countOfPlayer = InputView.inputCountOfPlayer();
+		Players players = Players.of(InputView.inputPlayerNames(countOfPlayer));
+		BowlingGame bowlingGame = BowlingGame.init(players);
+		OutputView.viewInit(players);
 
-		while (!frames.isEnd()) {
-			int knockingDownPins = InputView.inputPitchingOf(frames.getCurrentFrameNo());
-			frames = frames.reflect(Pins.of(knockingDownPins));
-			OutputView.viewPitchingResult(player, frames);
+		while (!bowlingGame.isEndGame()) {
+			int knockingDownPins = InputView.inputPitchingOf(bowlingGame.getCurrentPlayer());
+			List<Frames> framesList = bowlingGame.pitchBall(Pins.of(knockingDownPins));
+			OutputView.viewPitchingResults(players, framesList);
+			//OutputView.viewPitchingResult(bowlingGame.getPlayer(), frames);
 		}
 
 	}
