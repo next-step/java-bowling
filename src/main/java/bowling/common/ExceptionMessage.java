d0575d45c820b38471d5bd0123559e9408612b60
hJJANG <dacapolife87@gmail.com>
2020-09-15 22:58:33 +0900

    feat : 공통상수 및 에러 메시지 추가

---
+++ src/main/java/bowling/common/ExceptionMessage.java
@@ -0,0 +1,11 @@
+package bowling.common;
+
+public class ExceptionMessage {
+
+    public static final String NORMAL_FRAME_RANGE = "NormalFrame의 범위는 1~9 사이입니다.";
+    public static final String FINAL_FRAME_RANGE = "FinalFrame의 인덱스는 10이여야합니다.";
+
+    public static final String PIN_BOWL_RANGE = "볼링 한 구당 쓰러트릴수 있는 핀의 갯수는 0 ~ 10 사이입니다.";
+
+    public static final String PLAYER_NAME_LENGTH = "플레이어 이름은 최대 영문3 글자 입니다.";
+}
