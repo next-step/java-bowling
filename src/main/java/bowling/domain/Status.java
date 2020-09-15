a4ab22cf6a9757f71f52153eb1616eacace82f97
hJJANG <dacapolife87@gmail.com>
2020-09-15 22:44:41 +0900

    style : code Formatting 수정

--- src/main/java/bowling/domain/Status.java
+++ src/main/java/bowling/domain/Status.java
@@ -3,10 +3,10 @@ package bowling.domain;
 import java.util.Arrays;
 
 public enum Status {
-    STRIKE(true,1),
-    SPARE(true,2),
-    MISS(false,2),
-    ING(false,1);
+    STRIKE(true, 1),
+    SPARE(true, 2),
+    MISS(false, 2),
+    ING(false, 1);
 
     private final boolean allFallenPins;
     private final int tryCount;
@@ -18,7 +18,7 @@ public enum Status {
 
     public static Status of(boolean allFallenPins, int tryCount) {
         return Arrays.stream(values())
-                .filter( status -> status.allFallenPins == allFallenPins && status.tryCount == tryCount)
+                .filter(status -> status.allFallenPins == allFallenPins && status.tryCount == tryCount)
                 .findFirst()
                 .orElse(ING);
     }
