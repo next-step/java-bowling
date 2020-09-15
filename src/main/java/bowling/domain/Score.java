699382b23e0b6701d135bc71ce78879e66fc4d3f
yongdae <7942yongdae@gmail.com>
2020-09-15 17:38:56 +0900

    feat: 점수 역할 클래스 추가

---
+++ src/main/java/bowling/domain/Score.java
@@ -0,0 +1,17 @@
+package bowling.domain;
+
+public class Score {
+    private int count;
+
+    public Score(int count) {
+        this.count = count;
+    }
+
+    public Score hit(int count) {
+        return new Score(this.count + count);
+    }
+
+    public int getScore() {
+        return count;
+    }
+}
---
+++ src/test/java/bowling/domain/ScoreTest.java
@@ -0,0 +1,14 @@
+package bowling.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ScoreTest {
+
+    @Test
+    public void getScore() {
+        Score score = new Score(10);
+        assertThat(score.hit(9).hit(1).getScore()).isEqualTo(20);
+    }
+}
