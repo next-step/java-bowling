69f9615f4903042ce73e74af5a504b85b77ad0de
sefide <sefide@naver.com>
2020-09-16 02:29:49 +0900

    test: Score 테스트코드 추가

---
+++ src/test/java/bowling/score/ScoreTest.java
@@ -0,0 +1,33 @@
+package bowling.score;
+
+import bowling.exception.ExceptionMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class ScoreTest {
+
+    @DisplayName("생성 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"3", "7", "21"})
+    void test(String scoreValue) {
+
+        assertThatCode(() -> Score.of(scoreValue))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("투구값 범위 넘겼을 시, 생성 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "-20"})
+    void of_invalid_score(String scoreValue) {
+        String exceptionMessage = ExceptionMessage.INVALID_SCORE_VALUE;
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> Score.of(scoreValue))
+                .withMessage(exceptionMessage);
+
+    }
+}
