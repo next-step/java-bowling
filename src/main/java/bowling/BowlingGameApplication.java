f8e8ee6f0bdab188c38a78cabff73b1bef606625
John <bass0223@naver.com>
2020-09-15 10:03:08 +0900

    BowlingGame 삭제

--- src/main/java/bowling/BowlingGameApplication.java
+++ src/main/java/bowling/BowlingGameApplication.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import bowling.frame.Frames;
 import bowling.pin.Pins;
+import bowling.score.ScoringBoards;
 import bowling.user.Players;
 import bowling.view.InputView;
 import bowling.view.OutputView;
@@ -11,17 +12,15 @@ import bowling.view.OutputView;
 public class BowlingGameApplication {
 
 	public static void main(String[] args) {
-		//Player player = Player.of(InputView.inputPlayerName());
 		int countOfPlayer = InputView.inputCountOfPlayer();
 		Players players = Players.of(InputView.inputPlayerNames(countOfPlayer));
-		BowlingGame bowlingGame = BowlingGame.init(players);
+		ScoringBoards scoringBoards = ScoringBoards.of(players);
 		OutputView.viewInit(players);
 
-		while (!bowlingGame.isEndGame()) {
-			int knockingDownPins = InputView.inputPitchingOf(bowlingGame.getCurrentPlayer());
-			List<Frames> framesList = bowlingGame.pitchBall(Pins.of(knockingDownPins));
+		while (!scoringBoards.isEnd()) {
+			int knockingDownPins = InputView.inputPitchingOf(scoringBoards.getCurrentPlayer());
+			List<Frames> framesList = scoringBoards.reflect(Pins.of(knockingDownPins));
 			OutputView.viewPitchingResults(players, framesList);
-			//OutputView.viewPitchingResult(bowlingGame.getPlayer(), frames);
 		}
 
 	}
