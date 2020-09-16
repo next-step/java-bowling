24f8cc92141602146ea23f390ee4a6537e70f86f
ap-jylee <e200uk@gmail.com>
2020-09-16 19:52:52 +0900

    feat: NumberOfPins 객체, 테스트 생성

---
+++ src/test/java/bowling/domain/NumberOfPinsTest.java
@@ -0,0 +1,27 @@
+package bowling.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+@DisplayName("핀갯수 객체 테스트")
+public class NumberOfPinsTest {
+
+    @DisplayName("생성 테스트")
+    @Test
+    public void of() {
+        assertThat(new NumberOfPins(5)).isEqualTo(new NumberOfPins(5));
+    }
+
+    @DisplayName("유효성 테스트: 음수, 10 보다 큰 수 입력")
+    @ParameterizedTest
+    @ValueSource(ints = { -5, 12 })
+    public void validate(int numberOfPins) {
+        assertThatIllegalArgumentException().isThrownBy(() -> new NumberOfPins(numberOfPins));
+    }
+
+}
