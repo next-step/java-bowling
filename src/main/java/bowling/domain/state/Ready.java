8e184c25aef29ad43044d8a9a8cded2ada6d5e87
slamdunk7575 <slamdunk7575@gmail.com>
2020-09-17 23:26:11 +0900

    (feat) 초기 준비(Ready) 상태 객체 생성

---
+++ src/main/java/bowling/domain/state/Ready.java
@@ -0,0 +1,31 @@
+package bowling.domain.state;
+
+import bowling.domain.pin.Pin;
+
+public class Ready implements State {
+
+    private Ready() {
+    }
+
+    public static Ready of() {
+        return new Ready();
+    }
+
+    @Override
+    public State bowl(Pin felledPin) {
+        if (felledPin.isAllFelled()) {
+            return Strike.of();
+        }
+        return Continue.of(felledPin);
+    }
+
+    @Override
+    public boolean isEnd() {
+        return false;
+    }
+
+    @Override
+    public String view() {
+        return "";
+    }
+}
---
+++ src/test/java/bowling/domain/state/ReadyTest.java
@@ -0,0 +1,31 @@
+package bowling.domain.state;
+
+import bowling.domain.pin.Pin;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class ReadyTest {
+
+    @Test
+    @DisplayName("Ready에서 10개 미만의 핀을 쓰러뜨리면 Continue 상태가 되는 테스트")
+    void ready_to_continue_test() {
+        assertThat(Ready.of().bowl(Pin.of(0)))
+                .isInstanceOf(Continue.class);
+    }
+
+    @Test
+    @DisplayName("Ready에서 모든 핀을 쓰러뜨리면 Strike 상태가 되는 테스트")
+    void ready_to_strike_test() {
+        assertThat(Ready.of().bowl(Pin.of(10)))
+                .isInstanceOf(Strike.class);
+    }
+
+    @Test
+    @DisplayName("Ready는 투구 가능(False) 상태를 가지는 테스트")
+    void ready_isEnd_test() {
+        assertThat(Ready.of().isEnd()).isFalse();
+    }
+}
