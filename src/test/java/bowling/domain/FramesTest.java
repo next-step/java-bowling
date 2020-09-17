6292a5c702b8260b7058833fa67b62805917ebd6
hJJANG <dacapolife87@gmail.com>
2020-09-17 22:01:30 +0900

    test : Player변수 제거에 따른 테스트케이스 수정

--- src/test/java/bowling/domain/FramesTest.java
+++ src/test/java/bowling/domain/FramesTest.java
@@ -1,6 +1,5 @@
 package bowling.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -8,41 +7,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FramesTest {
 
-    Player player;
-    @BeforeEach
-    void makePlayer() {
-        player = new Player("JHJ");
-    }
-
     @Test
     void create() {
-        Frames frames = new Frames(player);
-
-        assertThat(frames.currentFrame()).isEqualTo(String.valueOf(Frame.MIN_FRAME_INDEX));
+        Frames frames = new Frames();
+        assertThat(frames.currentFrameIndex()).isEqualTo(String.valueOf(Frame.MIN_FRAME_INDEX));
     }
-
-    @Test
-    void playerTest() {
-        String playerName = player.name();
-        Frames frames = new Frames(player);
-
-        assertThat(frames.getPlayerName()).isEqualTo(playerName);
-    }
-
 
     @Test
     void frameSizeTest() {
-
-        Frames frames = new Frames(player);
-
+        Frames frames = new Frames();
         assertThat(frames.getFrames()).hasSize(1);
     }
 
     @Test
     void frameFinishTest() {
-        Frames frames = new Frames(player);
+        Frames frames = new Frames();
         for(int i = 0; i < 12; i++) {
-            frames.play(10);
+            frames.play(new Pin(10));
         }
         assertTrue(frames.allFrameEnd());
     }
