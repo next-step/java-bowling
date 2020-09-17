512262fef120369e97124a55d42c603934a47f3e
ap-jylee <e200uk@gmail.com>
2020-09-17 20:53:36 +0900

    feat: normal, final 에서 모두 사용할만한 것들 합침

---
+++ src/main/java/bowling/domain/AbstractFrame.java
@@ -0,0 +1,35 @@
+package bowling.domain;
+
+import java.util.Objects;
+
+public abstract class AbstractFrame implements Frame {
+
+    public static final int FIRST_FRAME_NUMBER = 1;
+    public static final int LAST_FRAME_NUMBER = 10;
+
+    protected final int frameNumber;
+    protected final FrameBowl frameBowl = new FrameBowl();
+
+    public AbstractFrame(int frameNumber) {
+        this.frameNumber = frameNumber;
+    }
+
+    @Override
+    public int getFrameNumber() {
+        return frameNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NormalFrame that = (NormalFrame) o;
+        return frameNumber == that.frameNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(frameNumber);
+    }
+
+}
