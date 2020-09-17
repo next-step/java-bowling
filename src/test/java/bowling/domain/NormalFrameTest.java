b0b530716e7751f5c22f0d9b35a408fa93e0f632
hJJANG <dacapolife87@gmail.com>
2020-09-17 21:57:32 +0900

    test : 경우별 프레임 종료 테스트케이스

--- src/test/java/bowling/domain/NormalFrameTest.java
+++ src/test/java/bowling/domain/NormalFrameTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class NormalFrameTest {
@@ -21,9 +22,41 @@ class NormalFrameTest {
         int frameIndex = 5;
 
         NormalFrame normalFrame = new NormalFrame(frameIndex);
-        normalFrame.bowl(10);
+        normalFrame.bowl(new Pin(2));
+        normalFrame.bowl(new Pin(5));
 
         assertTrue(normalFrame.rollingEnd());
+    }
+
+    @Test
+    void rollingEndByStrikeTest() {
+        int frameIndex = 5;
+
+        NormalFrame normalFrame = new NormalFrame(frameIndex);
+        normalFrame.bowl(new Pin(10));
+
+        assertTrue(normalFrame.rollingEnd());
+    }
+
+    @Test
+    void rollingEndBySpareTest() {
+        int frameIndex = 5;
+
+        NormalFrame normalFrame = new NormalFrame(frameIndex);
+        normalFrame.bowl(new Pin(2));
+        normalFrame.bowl(new Pin(8));
+
+        assertTrue(normalFrame.rollingEnd());
+    }
+
+    @Test
+    void rollingNotEndTest() {
+        int frameIndex = 5;
+
+        NormalFrame normalFrame = new NormalFrame(frameIndex);
+        normalFrame.bowl(new Pin(5));
+
+        assertFalse(normalFrame.rollingEnd());
     }
 
     @Test
