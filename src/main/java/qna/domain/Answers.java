79be5821673bb80b75d53f92cd482d5a249ad5e1
Dami <fdfdsa123@hanmail.net>
2020-09-17 20:52:56 +0900

    refactor($step1): Answers 일급 컬렉션 클래스 추가

---
+++ src/main/java/qna/domain/Answers.java
@@ -0,0 +1,39 @@
+package qna.domain;
+
+import org.hibernate.annotations.Where;
+import qna.CannotDeleteException;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import java.util.ArrayList;
+import java.util.List;
+
+@Embeddable
+public class Answers {
+	@OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+	@Where(clause = "deleted = false")
+	@OrderBy("id ASC")
+	private final List<Answer> answers;
+
+	public Answers() {
+		this.answers = new ArrayList<>();
+	}
+
+	public void addAnswer(Answer answer) {
+		answers.add(answer);
+	}
+
+	public void validAnswersCanDelete(User loginUser) throws CannotDeleteException {
+		for (Answer answer : answers) {
+			answer.validAnswerCanDelete(loginUser);
+		}
+	}
+
+	public void delete(List<DeleteHistory> deleteHistories) {
+		for (Answer answer : answers) {
+			deleteHistories.add(answer.setDeleted());
+		}
+	}
+}
