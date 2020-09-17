4d00cbb0060d1b14f81ba207910751079afa9fd0
ap-jylee <e200uk@gmail.com>
2020-09-17 20:54:32 +0900

    refactor: 기타 출력 관련 수정

--- src/main/java/bowling/app/BowlingExecutor.java
+++ src/main/java/bowling/app/BowlingExecutor.java
@@ -1,26 +1,28 @@
 package bowling.app;
 
+import bowling.domain.Frame;
 import bowling.domain.NormalFrame;
 import bowling.domain.Player;
 import bowling.view.InputView;
 import bowling.view.OutputView;
 
+import static bowling.domain.AbstractFrame.FIRST_FRAME_NUMBER;
+
 public class BowlingExecutor {
 
-    public static final int INIT_FRAME_NUMBER = 1;
-    public static final NormalFrame INIT_NORMAL_FRAME = new NormalFrame(INIT_FRAME_NUMBER);
+    public static final Frame FIRST_FRAME = new NormalFrame(FIRST_FRAME_NUMBER);
 
     private BowlingExecutor() {
 
     }
 
     public static void execute(Player player) {
-        NormalFrame normalFrame = INIT_NORMAL_FRAME;
-        OutputView.printBoard(player, INIT_NORMAL_FRAME);
-        while (normalFrame != null) {
+        Frame frame = FIRST_FRAME;
+        OutputView.printBoard(player, FIRST_FRAME);
+        while (frame != null) {
             int numberOfPins = InputView.getNumberOfPins();
-            normalFrame = normalFrame.bowl(numberOfPins);
-            OutputView.printBoard(player, INIT_NORMAL_FRAME);
+            frame = frame.bowl(numberOfPins);
+            OutputView.printBoard(player, FIRST_FRAME);
         }
     }
 
--- src/main/java/bowling/view/OutputView.java
+++ src/main/java/bowling/view/OutputView.java
@@ -1,24 +1,34 @@
 package bowling.view;
 
-import bowling.domain.NormalFrame;
+import bowling.domain.Frame;
 import bowling.domain.Player;
 import bowling.util.StringUtils;
 
 import java.text.MessageFormat;
 import java.util.stream.IntStream;
 
-import static bowling.domain.NormalFrame.LAST_FRAME_NUMBER;
+import static bowling.domain.AbstractFrame.LAST_FRAME_NUMBER;
 
 public class OutputView {
 
-    public static void printBoard(Player player, NormalFrame normalFrame) {
+    public static void printBoard(Player player, Frame firstFrame) {
         System.out.println("| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |");
-        System.out.print(MessageFormat.format("| {0} | {1} |", StringUtils.leftPad(player.getName(), 4), StringUtils.leftPad(normalFrame.toString(), 4)));
-        while (normalFrame.hasNext()) {
-            normalFrame = normalFrame.next();
-            System.out.print(StringUtils.leftPad(normalFrame.toString(), 5) + " |");
+        System.out.print(MessageFormat.format("| {0} |", StringUtils.leftPad(player.getName(), 4)));
+        Frame lastFrame = printFrame(firstFrame);
+        printEmptyBoard(lastFrame);
+    }
+
+    private static Frame printFrame(Frame firstFrame) {
+        Frame lastFrame = firstFrame;
+        for (Frame nextFrame: firstFrame) {
+            System.out.print(StringUtils.leftPad(nextFrame.toString(), 5) + " |");
+            lastFrame = nextFrame;
         }
-        IntStream.range(normalFrame.getFrameNumber(), LAST_FRAME_NUMBER)
+        return lastFrame;
+    }
+
+    private static void printEmptyBoard(Frame lastFrame) {
+        IntStream.range(lastFrame.getFrameNumber(), LAST_FRAME_NUMBER)
                 .forEach(i -> System.out.print(StringUtils.leftPad("", 5) + " |"));
     }
 
