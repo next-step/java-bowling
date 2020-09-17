e09cfbcc91ebb58089f8fee7edef5fbd5bc189b5
hJJANG <dacapolife87@gmail.com>
2020-09-17 22:04:13 +0900

    refactor : Player가 Frames관리하도록변경

--- src/main/java/bowling/domain/Player.java
+++ src/main/java/bowling/domain/Player.java
@@ -1,18 +1,40 @@
 package bowling.domain;
 
+import java.util.List;
 import java.util.Objects;
-
-import static bowling.common.ExceptionMessage.PLAYER_NAME_LENGTH;
+import java.util.function.Function;
 
 public class Player {
+
+    private final String PLAYER_NAME_LENGTH = "플레이어 이름은 최대 영문3 글자 입니다.";
+
     private String name;
+
+    private Frames frames;
 
     public Player(String name) {
         this.name = name;
-
+        this.frames = new Frames();
         if (name.length() > 3) {
             throw new IllegalArgumentException(PLAYER_NAME_LENGTH);
         }
+    }
+
+    public boolean isEndGame() {
+        return frames.allFrameEnd();
+    }
+
+    public void bowl(Function<String, String> rollingBall) {
+        int countOfPins = Integer.parseInt(rollingBall.apply(frames.currentFrameIndex()));
+        frames.play(new Pin(countOfPins));
+    }
+
+    public List<Frame> frames() {
+        return frames.getFrames();
+    }
+
+    public int frameSize() {
+        return frames.size();
     }
 
     @Override
