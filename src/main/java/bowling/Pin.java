d7d324ae181f525af7c16fc063d8ecf37d650531
skid901 <tlrudejr1993@naver.com>
2020-09-18 18:32:34 +0900

    feat: implement & test Pin class
    1. implement Pin class
    2. test Pin class methods

--- src/main/java/bowling/Pin.java
+++ src/main/java/bowling/Pin.java
@@ -1,10 +1,20 @@
 package bowling;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
 public class Pin {
 
-    private final static int MAX_PINS = 10;
-    private final static int MIN_PINS = 0;
+    public final static int MAX_PINS = 10;
+    public final static int MIN_PINS = 0;
+    private final static Map<Integer, Pin> instances = new HashMap<>();
     private final int pin;
+
+    static {
+        IntStream.rangeClosed(MIN_PINS, MAX_PINS)
+                .forEach(pin -> instances.put(pin, new Pin(pin)));
+    }
 
     private Pin(int pin) {
         this.pin = pin;
@@ -16,12 +26,38 @@ public class Pin {
 
     private static void validate(int pin) throws IllegalArgumentException {
         if (pin < MIN_PINS || pin > MAX_PINS) {
-            throw new IllegalArgumentException("입력된 핀 갯수가 범위를 벗어났습니다.");
+            throw new IllegalArgumentException("입력된 핀 갯수가 범위를 초과했습니다.");
         }
+    }
+
+    public static Pin ofMin() {
+        return instances.get(MIN_PINS);
+    }
+
+    public static Pin ofMax() {
+        return instances.get(MAX_PINS);
     }
 
     public static Pin of(int pin) {
         validate(pin);
-        return new Pin(pin);
+        return instances.get(pin);
+    }
+
+    public static Pin of(Pin pin) {
+        validate(pin.pin);
+        return instances.get(pin.pin);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pin pin1 = (Pin) o;
+        return pin == pin1.pin;
+    }
+
+    @Override
+    public int hashCode() {
+        return pin;
     }
 }
