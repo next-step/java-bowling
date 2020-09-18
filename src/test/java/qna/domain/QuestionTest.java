717589d3fb022d9ace89fb53a00a2323d489578c
An_Teatreeessential <wdefrg1324@gmail.com>
2020-09-19 02:39:14 +0900

    test : 답변, 질문 테스트 코드 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,28 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+
+    @Test
+    public void 본인의_답변일_경우_삭제() throws Exception {
+        Q1.delete(UserTest.JAVAJIGI);
+        assertTrue(Q1.isDeleted());
+
+        Q2.delete(UserTest.SANJIGI);
+        assertTrue(Q2.isDeleted());;
+    }
+
+    @Test
+    public void 타인의_답변일_경우_삭제() throws Exception {
+        assertThrows(CannotDeleteException.class, () -> Q1.delete(UserTest.SANJIGI));
+        assertThrows(CannotDeleteException.class, () -> Q2.delete(UserTest.JAVAJIGI));
+    }
 }
