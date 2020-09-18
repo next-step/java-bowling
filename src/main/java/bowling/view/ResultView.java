4a5c27e5c46c083bbd6fd162036010667dcbdd32
misudev <jungmisu801@gmail.com>
2020-09-18 12:56:12 +0900

    feat : 출력하는 ResultView 구현

---
+++ src/main/java/bowling/view/ResultView.java
@@ -0,0 +1,71 @@
+package bowling.view;
+
+import bowling.model.State;
+import bowling.model.delivery.Delivery;
+import bowling.model.frame.Frame;
+import bowling.model.frame.Frames;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ResultView {
+    private static final String FRAME_BOARD = "|  NAME  |   01   |   02   |   03   |   04   |   05   |   06   |" +
+                                              "   07   |   08   |   09   |   10   |";
+    private static final String BOARD_FORMAT = " %-6s |";
+    private static final String VERTICAL_LINE = "|";
+    private static final String BLANK = " ";
+    private static final Map<State, String> stateStringMap;
+
+    static {
+        stateStringMap = new HashMap<>();
+        stateStringMap.put(State.GUTTER, "-");
+        stateStringMap.put(State.STRIKE, "X");
+        stateStringMap.put(State.SPARE, "/");
+    }
+
+    public static void printScoreBoard(int frameNo, String userName, Stream<Frame> frames) {
+        System.out.println(FRAME_BOARD);
+        int countOfBlank = Frames.TOTAL_FRAME_COUNT - frameNo;
+
+        printUserName(userName);
+        printScores(frames);
+        printBlank(countOfBlank);
+        System.out.println();
+    }
+
+    private static void printUserName(String userName) {
+        System.out.print(VERTICAL_LINE);
+        System.out.printf(BOARD_FORMAT, userName);
+    }
+
+    private static void printScores(Stream<Frame> frames) {
+        frames.forEach(frame -> {
+            printScore(frame.getDeliveries());
+        });
+    }
+
+    private static void printScore(Stream<Delivery> deliveries) {
+        String score = deliveries.map(ResultView::getScoreString)
+                                 .collect(Collectors.joining ("|"));
+
+        System.out.printf(BOARD_FORMAT, BLANK + score);
+    }
+
+    private static String getScoreString(Delivery delivery) {
+        if (stateStringMap.containsKey(delivery.getState())) {
+            return stateStringMap.get(delivery.getState());
+        }
+
+        return String.valueOf(delivery.getFallenPins());
+    }
+
+    private static void printBlank(int countOfBlank) {
+        while (countOfBlank > 0) {
+            System.out.printf(BOARD_FORMAT, "");
+            countOfBlank--;
+        }
+    }
+
+}
