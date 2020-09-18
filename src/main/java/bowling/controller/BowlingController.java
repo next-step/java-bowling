aec690880dfe1d4d3e12bd3080929a2cd741fbb1
misudev <jungmisu801@gmail.com>
2020-09-18 12:56:28 +0900

    feat: controller 구현

---
+++ src/main/java/bowling/controller/BowlingController.java
@@ -0,0 +1,22 @@
+package bowling.controller;
+
+import bowling.model.BowlingGame;
+import bowling.model.User;
+import bowling.view.InputView;
+import bowling.view.ResultView;
+
+public class BowlingController {
+
+    public static void main(String[] args) {
+        BowlingGame bowlingGame = new BowlingGame();
+        User user = new User(InputView.getUserName());
+
+        while (!bowlingGame.isEnd()) {
+            int frameNo = bowlingGame.getPlayFrameNo();
+            int countOfFallenPins = InputView.getFallenPins(frameNo);
+            bowlingGame.bowling(countOfFallenPins);
+
+            ResultView.printScoreBoard(frameNo, user.getName(), bowlingGame.getFrames());
+        }
+    }
+}
