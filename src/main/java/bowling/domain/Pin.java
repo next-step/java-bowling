41e599426279a5f1064fbf3c5c865e66bcb32146
hJJANG <dacapolife87@gmail.com>
2020-09-19 00:29:54 +0900

    refactor : 볼링핀의 투구 결과 기능 위임

--- src/main/java/bowling/domain/Pin.java
+++ src/main/java/bowling/domain/Pin.java
@@ -6,8 +6,12 @@ public class Pin {
 
     private final String PIN_BOWL_RANGE = "볼링 한 구당 쓰러트릴수 있는 핀의 갯수는 0 ~ 10 사이입니다.";
 
-    private final int MAXIMUM_PIN_COUNT = 10;
-    private final int MINIMUM_PIN_COUNT = 0;
+    private static final String BOWLING_STATUS_STRIKE = "X";
+    private static final String BOWLING_STATUS_SPARE = "/";
+    private static final String BOWLING_STATUS_GUTTER = "-";
+
+    public static final int MAXIMUM_PIN_COUNT = 10;
+    private static final int MINIMUM_PIN_COUNT = 0;
 
     private int pins;
 
@@ -26,6 +30,41 @@ public class Pin {
         }
     }
 
+    public boolean isStrike() {
+        return pins == MAXIMUM_PIN_COUNT;
+    }
+
+    public boolean isSpare(Pin secondPins) {
+        if (isStrike()) {
+            return false;
+        }
+        return totalFallenPins(secondPins) == MAXIMUM_PIN_COUNT;
+    }
+
+    private int totalFallenPins(Pin secondPins) {
+        return pins + secondPins.count();
+    }
+
+    private String ifCountOfPinsZeroTransGutter() {
+        if (pins == MINIMUM_PIN_COUNT) {
+            return BOWLING_STATUS_GUTTER;
+        }
+        return String.valueOf(pins);
+    }
+
+    public String record() {
+        if (isStrike()) {
+            return BOWLING_STATUS_STRIKE;
+        }
+        return ifCountOfPinsZeroTransGutter();
+    }
+
+    public String record(Pin secondPin) {
+        if (isSpare(secondPin)) {
+            return BOWLING_STATUS_SPARE;
+        }
+        return String.valueOf(secondPin.count());
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -39,4 +78,5 @@ public class Pin {
     public int hashCode() {
         return Objects.hash(pins);
     }
+
 }
