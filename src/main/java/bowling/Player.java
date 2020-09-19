815d764b5b0b71efbef0f94c368b0964bbe71794
skid901 <tlrudejr1993@naver.com>
2020-09-19 18:26:44 +0900

    refactor: add equals & hashcode methods in Player class

--- src/main/java/bowling/Player.java
+++ src/main/java/bowling/Player.java
@@ -1,5 +1,6 @@
 package bowling;
 
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class Player {
@@ -19,6 +20,21 @@ public class Player {
         if (!Pattern.matches(NAMING_REGEX, name)) {
             throw new IllegalArgumentException("이름은 영문 3 글자만 허용 됩니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Player player = (Player) o;
+
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
     }
 
     public static Player of(String name) {
