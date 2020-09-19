748fea07aab6066d0071c01c504da919b6d45b4d
ap-jylee <e200uk@gmail.com>
2020-09-19 15:26:33 +0900

    feat: 점수 출력

--- src/main/java/bowling/view/OutputView.java
+++ src/main/java/bowling/view/OutputView.java
@@ -11,21 +11,38 @@ import static bowling.domain.frame.AbstractFrame.LAST_FRAME_NUMBER;
 
 public class OutputView {
 
+    public static final String EMPTY = "";
+
     public static void printBoard(Player player, Frame firstFrame) {
         System.out.println("| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |");
-        System.out.print(MessageFormat.format("| {0} |", StringUtils.leftPad(player.getName(), 4)));
-        Frame lastFrame = printFrame(firstFrame);
-        printEmptyBoard(lastFrame);
+        printPins(player, firstFrame);
+        System.out.println();
+        printScore(firstFrame);
         System.out.println();
     }
 
-    private static Frame printFrame(Frame firstFrame) {
+    private static void printPins(Player player, Frame firstFrame) {
+        System.out.print(MessageFormat.format("| {0} |", StringUtils.leftPad(player.getName(), 4)));
         Frame lastFrame = firstFrame;
         for (Frame nextFrame: firstFrame) {
             System.out.print(StringUtils.leftPad(nextFrame.toString(), 5) + " |");
             lastFrame = nextFrame;
         }
-        return lastFrame;
+        printEmptyBoard(lastFrame);
+    }
+
+    private static void printScore(Frame firstFrame) {
+        System.out.print("|      |");
+        Frame lastFrame = firstFrame;
+        for (Frame nextFrame: firstFrame) {
+            System.out.print(StringUtils.leftPad(convertScore(nextFrame.getScore()), 5) + " |");
+            lastFrame = nextFrame;
+        }
+        printEmptyBoard(lastFrame);
+    }
+
+    private static String convertScore(int score) {
+        return score == -1 ? EMPTY : String.valueOf(score);
     }
 
     private static void printEmptyBoard(Frame lastFrame) {
