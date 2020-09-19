f27b96c96113563ea931ecf63fa649409dd4a4e3
slamdunk7575 <slamdunk7575@gmail.com>
2020-09-19 17:44:36 +0900

    (refactor) EndFrameStates를 제거하고 EndFrame으로 이동

--- src/main/java/bowling/domain/frame/EndFrame.java
+++ src/main/java/bowling/domain/frame/EndFrame.java
@@ -1,11 +1,15 @@
 package bowling.domain.frame;
 
 import bowling.domain.pin.Pin;
-import bowling.domain.state.EndFrameStates;
+import bowling.domain.state.*;
+
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class EndFrame implements Frame {
 
-    private final EndFrameStates states = new EndFrameStates();
+    private final LinkedList<State> states = new LinkedList<>(Arrays.asList(Ready.of()));
+    private final EndFrameCount count = EndFrameCount.of();
 
     public static Frame of() {
         return new EndFrame();
@@ -13,16 +17,30 @@ public class EndFrame implements Frame {
 
     @Override
     public void bowl(Pin felledPin) {
-        this.states.bowl(felledPin);
+        count.increment();
+        bowlAndSet(felledPin);
+        if (states.getLast().isEnd()) {
+            states.add(Ready.of());
+        }
+    }
+
+    private void bowlAndSet(Pin felledPin) {
+        State current = states.getLast();
+        states.set(states.indexOf(current), current.bowl(felledPin));
     }
 
     @Override
     public boolean isEnd() {
-        return states.isEnd();
+        return isClear() || count.isMax();
     }
 
-    public EndFrameStates getStates() {
+    @Override
+    public boolean isClear() {
+        State first = states.getFirst();
+        return first instanceof Miss || first instanceof Gutter;
+    }
+
+    public LinkedList<State> getStates() {
         return states;
     }
-
 }
