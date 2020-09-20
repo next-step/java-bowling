7476ce87398efa53463e4727c4e32a960469ab97
rftengineer <nightpw1@naver.com>
2020-09-20 22:35:17 +0900

    feat(step1) : Answer 일급 콜렉션에 delete 추가

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -1,11 +1,15 @@
 package qna.domain;
 
+import qna.CannotDeleteException;
+
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Answers {
     private List<Answer> answers;
 
-    private Answers(List<Answer> answer){
+    private Answers(List<Answer> answer) {
         this.answers = answer;
     }
 
@@ -13,7 +17,20 @@ public class Answers {
         return new Answers(answer);
     }
 
-    public void delete() {
+    public List<DeleteHistory> delete(User loginUser) throws CannotDeleteException {
+        validateDeletion(loginUser);
+        return answers.stream()
+                .map(it -> new DeleteHistory(ContentType.ANSWER, it.getId(), it.getWriter(), LocalDateTime.now()))
+                .collect(Collectors.toList())
+                ;
 
+    }
+
+    private void validateDeletion(User loginUser) throws CannotDeleteException {
+        if (answers.stream()
+                .anyMatch(it -> !it.isOwner(loginUser))
+        ) {
+            throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+        }
     }
 }
