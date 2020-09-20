c3ac5e94a24f9a55fe64e9eb113d252b2620e5fe
mandykk1212 <mandykk1212@gmail.com>
2020-09-20 14:39:08 +0900

    test: Question delete_성공

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,15 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    void delete_성공() throws CannotDeleteException {
+        Question question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        question.delete(UserTest.JAVAJIGI);
+    }
 }
