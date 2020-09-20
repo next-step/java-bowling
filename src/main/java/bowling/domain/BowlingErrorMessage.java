8aa7b661408244e92bff6b873503b6996f36b6cb
Sang Uk Ahn <suahnn@gmail.com>
2020-09-20 17:57:25 +0900

    feat: 프레임 기본 속성 프로퍼티, 메시지 분리

---
+++ src/main/java/bowling/domain/BowlingErrorMessage.java
@@ -0,0 +1,12 @@
+package bowling.domain;
+
+public class BowlingErrorMessage {
+
+    public static final String PLAYER_NAME_BLANK = "The name cannot be null or empty";
+
+    public static final String PLAYER_NAME_TOO_LONG = "The name can only be %s characters or less";
+
+    public static final String INVALID_PIN_COUNT_RANGE = "Invalid pin count range";
+
+    public static final String INVALID_FRAME_NUMBER = "Invalid frame number";
+}
