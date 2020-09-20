4a922c9e298ec3c3b430c6aa13e8fa8794c6eafe
ap-jylee <e200uk@gmail.com>
2020-09-21 00:23:43 +0900

    refactor: Output 수정

--- src/main/java/bowling/view/OutputView.java
+++ src/main/java/bowling/view/OutputView.java
@@ -12,7 +12,7 @@ import static bowling.domain.frame.AbstractFrame.LAST_FRAME_NUMBER;
 
 public class OutputView {
 
-    public static final String EMPTY = "";
+    public static final String EMPTY = "      |";
 
     public static void printBoard(Player player, Frame firstFrame) {
         System.out.println("| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |");
@@ -34,17 +34,26 @@ public class OutputView {
 
     private static void printScore(Frame firstFrame) {
         System.out.print("|      |");
+        int totalScore = 0;
         Frame lastFrame = firstFrame;
         for (Frame nextFrame: firstFrame) {
-            System.out.print(StringUtils.leftPad(convertScore(nextFrame.getScore()), 5) + " |");
+            totalScore = printScore(totalScore, nextFrame);
             lastFrame = nextFrame;
         }
         printEmptyBoard(lastFrame);
     }
 
-    private static String convertScore(Score score) {
-        return score.isValid() ? String.valueOf(score.getScore()) : EMPTY;
+    private static int printScore(int totalScore, Frame nextFrame) {
+        Score score = nextFrame.getScore();
+        if (!score.isValid()) {
+            System.out.print(EMPTY);
+            return totalScore;
+        }
+        totalScore += score.getScore();
+        System.out.print(StringUtils.leftPad(String.valueOf(totalScore), 5) + " |");
+        return totalScore;
     }
+
 
     private static void printEmptyBoard(Frame lastFrame) {
         IntStream.range(lastFrame.getFrameNumber(), LAST_FRAME_NUMBER)
