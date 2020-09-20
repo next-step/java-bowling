92fa4af1de3fb266890e642f628253ed60fef557
ted.kang <ted.kang@kakaocorp.com>
2020-09-21 02:31:21 +0900

    [feature] 3. Frame에서 이전 Frame을 멤버 변수로 갖게 수정 후 공을 굴릴 때 이전 프레임의 Score에 더해주게 변경

--- src/main/java/bowling/domain/frame/Frame.java
+++ src/main/java/bowling/domain/frame/Frame.java
@@ -1,6 +1,7 @@
 package bowling.domain.frame;
 
 import bowling.domain.DownedPinCount;
+import bowling.domain.score.Score;
 import bowling.domain.state.InitState;
 import bowling.domain.state.State;
 
@@ -10,14 +11,27 @@ public class Frame {
 
 	private static final int INCREMENTAL = 1;
 	private static final int FINAL_INDEX = 10;
+	private static final Frame GROUND_FRAME = new Frame();
 
 	private final int index;
+	protected State state;
+	protected Frame previous;
 
-	private State state;
+	private Frame() {
+		this.index = 0;
+		this.state = InitState.getInstance();
+	}
 
 	public Frame(int index) {
 		this.index = index;
 		this.state = InitState.getInstance();
+		this.previous = GROUND_FRAME;
+	}
+
+	public Frame(int index, Frame previous) {
+		this.index = index;
+		this.state = InitState.getInstance();
+		this.previous = previous;
 	}
 
 	public boolean isFrameFinished() {
@@ -25,7 +39,8 @@ public class Frame {
 	}
 
 	public Frame roll(DownedPinCount downedPinCount) {
-		state = state.roll(downedPinCount);
+		addScoreToPrevious(downedPinCount);
+		state = state.roll(downedPinCount, previous.getScore());
 		if(isFrameFinished()) {
 			return getNextFrame();
 		}
@@ -40,12 +55,27 @@ public class Frame {
 		return state;
 	}
 
+	public Score getScore() {
+		return state.getScore();
+	}
+
+	protected void addScoreToPrevious(DownedPinCount pinCount) {
+		if(previous != GROUND_FRAME) {
+			previous.addScore(pinCount);
+			previous.addScoreToPrevious(pinCount);
+		}
+	}
+
+	private void addScore(DownedPinCount pinCount) {
+		state.addPreviousCount(pinCount);
+	}
+
 	private Frame getNextFrame() {
 		int nextIndex = index + INCREMENTAL;
 		if(isNextFinal(nextIndex)) {
-			return new FinalFrame(nextIndex);
+			return new FinalFrame(nextIndex, this);
 		}
-		return new Frame(nextIndex);
+		return new Frame(nextIndex, this);
 	}
 
 	private boolean isNextFinal(int nextIndex) {
