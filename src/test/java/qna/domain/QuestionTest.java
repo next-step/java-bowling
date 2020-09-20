e73bb447771a8e7e983aded703afed9565ded15b
rftengineer <nightpw1@naver.com>
2020-09-20 23:26:01 +0900

    feat(step1) : Question에 대해서 Test 보강

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,42 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import qna.CannotDeleteException;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    private static Stream<Arguments> parameterForQuestionTest() {
+        return Stream.of(
+                Arguments.of(
+                        Q1
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameterForQuestionTest")
+    @DisplayName("Question 생성 및 삭제기능 Test-DeleteHistory가 정상적으로 반환?")
+    void initialize_Question(Question value) throws CannotDeleteException {
+        assertThat(value.delete(new User(1L, "javajigi", "password", "name", "javajigi@slipp.net"))).isNotNull();
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameterForQuestionTest")
+    @DisplayName("Question 생성, 삭제에서 Exception 체크 - User의 생성자를 조금 다르게 이용")
+    void initialize_Question_And_Exception(Question value) {
+        assertThatExceptionOfType(CannotDeleteException.class)
+                .isThrownBy(
+                        () -> value.delete(new User( "javajigi", "password", "name", "javajigi@slipp.net"))
+                ).withMessageMatching("질문을 삭제할 권한이 없습니다.");
+    }
 }
