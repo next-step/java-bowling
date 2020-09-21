c6443aeb43085a12beaac7ce10da19fbff59c363
Kim Daeyong <dev.kimdaeyong@gmail.com>
2020-09-22 01:20:02 +0900

    feat(scoreboard): use ScoreBoard to multi play

--- src/main/java/bowling/controller/BowlingGame.java
+++ src/main/java/bowling/controller/BowlingGame.java
@@ -1,11 +1,16 @@
 package bowling.controller;
 
+import bowling.model.scoreboard.ScoreBoard;
+import bowling.model.frame.Frame;
 import bowling.model.frame.Frames;
 import bowling.model.Player;
+import bowling.util.ValidationUtils;
 import bowling.view.InputView;
 import bowling.view.ResultView;
 
-import static bowling.util.ValidationUtils.tryUntilSuccess;
+import java.util.Collection;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
 public class BowlingGame {
     InputView inputView;
@@ -17,25 +22,48 @@ public class BowlingGame {
     }
 
     public void start() {
-        Player player = tryUntilSuccess(this::getPlayer, resultView::printError);
-        Frames frames = new Frames();
-        while(!frames.isEnded()) {
-            tryUntilSuccess(() -> addResult(frames), resultView::printError);
-            resultView.printHeader();
-            resultView.printFrames(player, frames.getDto());
-            resultView.printScores(frames.getScores());
+        int playerCount = tryUntilSuccess(this::getPlayerCount);
+        ScoreBoard scoreBoard = new ScoreBoard();
+        IntStream.range(0, playerCount)
+                .forEach(i -> tryUntilSuccess(() -> addPlayer(scoreBoard, i + 1)));
+
+        Collection<Player> players = scoreBoard.getPlayers();
+        while(!scoreBoard.isEnded()) {
+            players.forEach(player -> playTurn(player, scoreBoard));
         }
     }
 
-    private Frames addResult(Frames frames) {
-        int currentIndex = frames.getCurrentFrame().getIndex();
-        int count = inputView.inputFramePinCount(currentIndex);
-        frames.addResult(count);
-        return frames;
+    private void playTurn(Player player, ScoreBoard scoreBoard) {
+        Frames frames = scoreBoard.get(player);
+        Frame currentFrame = frames.getCurrentFrame();
+        while (!currentFrame.isEnded()) {
+            tryUntilSuccess(() -> addResult(player, currentFrame));
+            resultView.printHeader();
+            resultView.printScoreBoard(scoreBoard.toDto());
+        }
     }
 
-    private Player getPlayer() {
-        String name = inputView.inputPlayerName();
-        return new Player(name);
+    private Frame addResult(Player player, Frame frame) {
+        int count = inputView.inputFramePinCount(player);
+        frame.addResult(count);
+        return frame;
+    }
+
+    private ScoreBoard addPlayer(ScoreBoard scoreBoard, int index) {
+        Player player = new Player(inputView.inputPlayerName(index));
+        scoreBoard.add(player);
+        return scoreBoard;
+    }
+
+    private int getPlayerCount() {
+        int playerCount = inputView.inputPlayerCount();
+        if (playerCount <= 0) {
+            throw new IllegalArgumentException("0보다 큰 수를 입력해주세요.");
+        }
+        return playerCount;
+    }
+
+    private <T> T tryUntilSuccess(Supplier<T> supplier) {
+        return ValidationUtils.tryUntilSuccess(supplier, resultView::printError);
     }
 }
