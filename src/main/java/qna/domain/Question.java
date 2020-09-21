ca683e7fbee361d514f47a031778c106d26fa43e
rftengineer <nightpw1@naver.com>
2020-09-21 22:53:52 +0900

    Step1 review 요청 (#311)

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -1,10 +1,15 @@
 package qna.domain;
 
 import org.hibernate.annotations.Where;
+import qna.CannotDeleteException;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Entity
 public class Question extends AbstractEntity {
@@ -91,5 +96,23 @@ public class Question extends AbstractEntity {
     @Override
     public String toString() {
         return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
+    }
+
+    public List<DeleteHistory> delete(User loginUser, LocalDateTime now) throws CannotDeleteException {
+        validateQuestion(loginUser);
+        Answers answers = Answers.of(getAnswers());
+        setDeleted(true);
+        return Stream.concat(
+                Stream.<DeleteHistory>builder()
+                        .add(new DeleteHistory(ContentType.QUESTION,getId(),loginUser,now))
+                        .build(), answers.delete(loginUser,now).stream()
+                )
+                .collect(Collectors.toList());
+    }
+
+    private void validateQuestion(User loginUser) throws CannotDeleteException {
+        if (!isOwner(loginUser)){
+            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+        }
     }
 }
---
+++ src/test/java/qna/domain/AnswersTest.java
@@ -0,0 +1,52 @@
+package qna.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import qna.CannotDeleteException;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class AnswersTest {
+    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    private static Stream<Arguments> parameterForAnswersTest() {
+        return Stream.of(
+                Arguments.of(
+                        Collections.singletonList(
+                                new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1")
+                                //new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2")
+                        )
+
+                )
+        )
+                ;
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameterForAnswersTest")
+    @DisplayName("기능이 정상적으로 작동했는지(Return이 Null이 아닌지) 확인")
+    void initialize_Answers_And_Deletion(List<Answer> answerList) throws CannotDeleteException {
+        Answers answers = Answers.of(answerList);
+        assertThat(answers.delete(new User(1L,"javajigi", "password", "name", "javajigi@slipp.net"), LocalDateTime.of(2020,5,21,14,23,11))).isNotNull();
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameterForAnswersTest")
+    @DisplayName("두 번째 생성자를 이용해 id가 없을 때, 서로 다른유저임을 판별해 Exception 나는 것을 확인")
+    void initialize_Answers_And_Exception(List<Answer> answerList) {
+        Answers answers = Answers.of(answerList);
+        assertThatExceptionOfType(CannotDeleteException.class)
+                .isThrownBy(
+                        ()->answers.delete(new User("javajigi", "password", "name", "javajigi@slipp.net"), LocalDateTime.of(2020,5,21,14,23,11))
+                );
+    }
+}
