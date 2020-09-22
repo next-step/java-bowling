df5be94bef365259512178821fda6facc0986332
skid901 <tlrudejr1993@naver.com>
2020-09-22 19:51:46 +0900

    feat(inputView): add methods for input
    1. add inputPersonnel() method
    2. add inputNames() method

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,10 +1,14 @@
 package view;
 
+import dto.NamesDTO;
 import dto.PinDTO;
 import dto.NameDTO;
 
+import java.util.List;
 import java.util.Scanner;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class InputView {
 
@@ -22,9 +26,9 @@ public class InputView {
         }
     }
 
-    public NameDTO inputName() {
+    public NameDTO inputName(int index) {
         return tryCatch(() -> {
-            System.out.print("플레이어 이름은? ( 3 english letters ) : ");
+            System.out.printf("플레이어 %2d의 이름은? ( 3 english letters ) : ", index);
             String name = scanner.nextLine();
             return NameDTO.of(name);
         });
@@ -32,9 +36,23 @@ public class InputView {
 
     public PinDTO inputPin(NameDTO nameDTO) {
         return tryCatch(() -> {
-            System.out.printf("\n%s' turn : ", nameDTO.getName());
+            System.out.printf("\n%s's turn : ", nameDTO.getName());
             int pin = Integer.parseInt(scanner.nextLine());
             return PinDTO.of(pin);
         });
+    }
+
+    public int inputPersonnel() {
+        return tryCatch(() -> {
+            System.out.print("How many people? ");
+            return Integer.parseInt(scanner.nextLine());
+        });
+    }
+
+    public NamesDTO inputNames(int personnel) {
+        List<NameDTO> nameDTOs = tryCatch(() -> IntStream.rangeClosed(1, personnel)
+                .mapToObj(this::inputName)
+                .collect(Collectors.toList()));
+        return NamesDTO.of(nameDTOs);
     }
 }
