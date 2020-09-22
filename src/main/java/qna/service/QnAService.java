62012caa0bfce7215b4e4cdb421b970ea63436c1
Jay <beginin15@gmail.com>
2020-09-23 00:05:37 +0900

    refactor: QnAService 리팩토링

--- src/main/java/qna/service/QnAService.java
+++ src/main/java/qna/service/QnAService.java
@@ -35,14 +35,7 @@ public class QnAService {
     @Transactional
     public void deleteQuestion(User loginUser, long questionId) throws CannotDeleteException {
         Question question = findQuestionById(questionId);
-        question.deleteQuestion(loginUser);
-
-        List<Answer> answers = question.getAnswers();
-        List<DeleteHistory> deleteHistories = new ArrayList<>();
-        deleteHistories.add(new DeleteHistory(ContentType.QUESTION, questionId, question.getWriter(), LocalDateTime.now()));
-        for (Answer answer : answers) {
-            deleteHistories.add(new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now()));
-        }
-        deleteHistoryService.saveAll(deleteHistories);
+        DeleteHistories deleteHistories = question.deleteQuestion(loginUser);
+        deleteHistoryService.saveAll(deleteHistories.getDeleteHistories());
     }
 }
