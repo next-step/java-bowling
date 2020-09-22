edc99a4af248aa85066bae361326bd555a2c9283
Jay <beginin15@gmail.com>
2020-09-23 00:07:37 +0900

    style: 코드 컨벤션

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -32,7 +32,9 @@ public class Answers {
     }
 
     public void deleteAnswers(User loginUser) {
-        Optional<Answer> maybeAnswer = answers.stream().filter(answer -> !answer.isOwner(loginUser)).findAny();
+        Optional<Answer> maybeAnswer = answers.stream()
+                .filter(answer -> !answer.isOwner(loginUser))
+                .findAny();
         if (maybeAnswer.isPresent()) {
             throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
         }
