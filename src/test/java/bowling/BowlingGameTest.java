5bb9c830c102e62f57b14fee73c9b9cf9558f3ae
skid901 <tlrudejr1993@naver.com>
2020-09-22 20:42:37 +0900

    test(BowlingGameTest): test added codes

--- src/test/java/bowling/BowlingGameTest.java
+++ src/test/java/bowling/BowlingGameTest.java
@@ -7,30 +7,56 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.stream.IntStream;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.BDDAssertions.then;
 
 class BowlingGameTest {
 
     @Test
     @DisplayName("참가자 명 반환 검증")
-    void getPlayer() {
+    void getPlayerName() {
         String expected = "abc";
-        BowlingGame bowlingGame = BowlingGame.of(2, expected);
+        BowlingGame bowlingGame = BowlingGame.of(2, expected, 0);
 
-        then(bowlingGame.getPlayer()).isEqualTo(Player.of(expected));
+        then(bowlingGame.getPlayerName()).isEqualTo(expected);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"2", "3", "4"})
     @DisplayName("경기 종료 여부 검증")
     void isFinished(int totalFrames) {
-        BowlingGame bowlingGame = BowlingGame.of(totalFrames, "abc");
+        BowlingGame bowlingGame = BowlingGame.of(totalFrames, "abc", 0);
 
         IntStream.range(0, totalFrames + 1).forEach(index -> {
             then(bowlingGame.isFinished()).isFalse();
             bowlingGame.bowl(Pin.ofMax());
+            if (bowlingGame.hasNextFrameAndIsCurrentFrameFinished()) {
+                bowlingGame.shiftCurrentFrame();
+            }
         });
 
         then(bowlingGame.isFinished()).isTrue();
+    }
+
+    @Test
+    void hasNextFrameAndIsCurrentFrameFinished() {
+        BowlingGame bowlingGame = BowlingGame.of(2, "abc", 0);
+
+        then(bowlingGame.hasNextFrameAndIsCurrentFrameFinished()).isFalse();
+
+        bowlingGame.bowl(Pin.ofMax());
+        then(bowlingGame.hasNextFrameAndIsCurrentFrameFinished()).isTrue();
+    }
+
+    @Test
+    void shiftCurrentFrame() {
+        BowlingGame bowlingGame = BowlingGame.of(2, "abc", 0);
+
+        bowlingGame.bowl(Pin.ofMax());
+        bowlingGame.shiftCurrentFrame();
+
+        assertThatThrownBy(bowlingGame::shiftCurrentFrame)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("다음 프레임이 없습니다.");
     }
 }
