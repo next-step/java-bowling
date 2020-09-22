105dbb3e77194d3748f6aeb60596fc3f5d737ded
slamdunk7575 <slamdunk7575@gmail.com>
2020-09-22 09:11:07 +0900

    (refactor) 스트라이크 getScore() calculate() 테스트 추가

--- src/test/java/bowling/domain/state/StrikeTest.java
+++ src/test/java/bowling/domain/state/StrikeTest.java
@@ -1,5 +1,7 @@
 package bowling.domain.state;
 
+import bowling.domain.frame.Remaining;
+import bowling.domain.frame.Score;
 import bowling.domain.pin.Pin;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,5 +24,33 @@ class StrikeTest {
         assertThat(Strike.of().isEnd()).isTrue();
     }
 
+    @Test
+    @DisplayName("STRIKE 상태는 점수 10점, 추가 투구 2회를 가짐")
+    void strike_getScore_test() {
+
+        // given
+        State strike = Strike.of();
+
+        // when
+        Score score = strike.getScore();
+
+        // then
+        assertThat(score).isEqualTo(Score.of(10, Remaining.of(2)));
+    }
+
+    @Test
+    @DisplayName("STRIKE 상태는 기존 점수에 10점이 더해짐")
+    void strike_calculate_test() {
+
+        // given
+        Score baseScore = Score.of(10, Remaining.of(2));
+
+        // when
+        Score calculateScore = Strike.of().calculate(baseScore);
+
+        // then
+        assertThat(calculateScore).isEqualTo(Score.of(20, Remaining.of(1)));
+        assertThat(calculateScore.isPending()).isEqualTo(Boolean.TRUE);
+    }
 
 }
