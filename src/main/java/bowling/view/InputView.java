15310ce459af40e4c3ec3e84f0f81b61e923bac2
ted.kang <ted.kang@kakaocorp.com>
2020-09-23 23:27:22 +0900

    [feature] 2. 여러 사용자 입력 및 출력이 가능하게 InputView, OutputView 리팩토링

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -1,25 +1,43 @@
 package bowling.view;
 
 import bowling.domain.DownedPinCount;
+import bowling.domain.game.Games;
 import bowling.domain.player.Player;
 
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class InputView {
 
 	private static final String INPUT_SHOULD_INTEGER = "자연수로 입력 해 주세요.";
 	private static final String PLEASE_INPUT_PLAYER_NAME = "플레이어 이름은(3 english letters)?:";
-	private static final String PLEASE_INPUT_N_FRAME_PITCH_RESULT = "%d프레임 투구 :";
+	private static final String PLEASE_INPUT_N_FRAME_PITCH_RESULT = "%s's turn :";
+	private static final String PLEASE_INPUT_PLAYER_COUNT = "How many people?";
 	private static final Scanner SCANNER = new Scanner(System.in);
+
+	public static Games prepareGames() {
+		int playerCount = getPlayerCount();
+		List<Player> players = IntStream.rangeClosed(1, playerCount)
+				.mapToObj(count -> getPlayer())
+				.collect(Collectors.toList());
+		return new Games(players);
+	}
+
+	public static int getPlayerCount() {
+		System.out.println(PLEASE_INPUT_PLAYER_COUNT);
+		return getIntValue();
+	}
 
 	public static Player getPlayer() {
 		System.out.println(PLEASE_INPUT_PLAYER_NAME);
 		return new Player(getStringValue());
 	}
 
-	public static DownedPinCount getCurrentFramePitch(int currentFrameSequence) {
-		System.out.println(String.format(PLEASE_INPUT_N_FRAME_PITCH_RESULT, currentFrameSequence));
+	public static DownedPinCount getCurrentFramePitch(String playerName) {
+		System.out.println(String.format(PLEASE_INPUT_N_FRAME_PITCH_RESULT, playerName));
 		return DownedPinCount.fromDownCount(getIntValue());
 	}
 
