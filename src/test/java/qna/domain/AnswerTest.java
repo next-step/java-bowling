59d87450890df6f5e4ddbdb21d1fd6c7ca9620ef
Jay <beginin15@gmail.com>
2020-09-23 15:02:46 +0900

    feat: Answer의 삭제 메소드 정의
    - 기존 Answers에서 수행하던 개별 Answer 삭제 책임을 Answer가 맡도록 변경함

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,30 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.Assert.assertTrue;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("삭제")
+    void deleteAnswer() {
+        // when
+        A1.deleteAnswer(UserTest.JAVAJIGI);
+        assertTrue(A1.isDeleted());
+    }
+
+    @Test
+    @DisplayName("삭제 실패")
+    void deleteAnswer_validate() {
+        // when
+        assertThatThrownBy(() ->
+                A1.deleteAnswer(UserTest.SANJIGI)
+        ).isInstanceOf(CannotDeleteException.class);
+    }
 }
