0facaafb0a4c315766eb7d91f877796e1d4608ce
yohan <good_1411@naver.com>
2020-11-01 14:16:01 +0900

    feat: Player 추가

---
+++ src/main/java/bowling/domain/Player.java
@@ -0,0 +1,17 @@
+package bowling.domain;
+
+public class Player {
+    private static final int NAME_LENGTH = 3;
+    private final String name;
+
+    public Player(final String name) {
+        if (name.length() != NAME_LENGTH) {
+            throw new IllegalArgumentException("플레이어 이름은 3자리 영문이여야 합니다.");
+        }
+        this.name = name;
+    }
+
+    public String print() {
+        return name;
+    }
+}
