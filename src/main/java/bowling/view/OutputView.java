ef76396878f46c91b81dac7c0704dd24501b6e17
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-11-07 11:03:03 +0900

    feat: create InputView and OutputView

---
+++ src/main/java/bowling/view/OutputView.java
@@ -0,0 +1,8 @@
+package bowling.view;
+
+public class OutputView {
+
+	public static void printScoreView() {
+		System.out.println("| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |");
+	}
+}
