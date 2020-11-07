d9db304bb1db1287c1677ce127119b86ffec245e
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-11-07 11:19:25 +0900

    feat: create Pin object - set the minimum/maximum threshold of Pin number

---
+++ src/main/java/bowling/domain/Pin.java
@@ -0,0 +1,46 @@
+package bowling.domain;
+
+import java.util.Objects;
+
+public class Pin {
+	public static final int MAX_PIN_COUNT = 10;
+	private final int count;
+
+	public Pin(int count){
+		this.validate(count);
+		this.count = count;
+	}
+
+	private void validate(int count) {
+		if (count < 0) {
+			throw new IllegalArgumentException("0보다 작을 수 없습니다.");
+		}
+
+		if (count > MAX_PIN_COUNT) {
+			throw new IllegalArgumentException("10보다 클 수 없습니다.");
+		}
+	}
+
+	public Pin next(int nextCount) {
+		if (this.count + nextCount > MAX_PIN_COUNT) {
+			throw new IllegalArgumentException("10보다 클 수 없습니다.");
+		}
+
+		return new Pin(nextCount);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Pin pin = (Pin)o;
+		return count == pin.count;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(count);
+	}
+}
---
+++ src/test/java/bowling/domain/PinTest.java
@@ -0,0 +1,38 @@
+package bowling.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class PinTest {
+
+	@ParameterizedTest
+	@ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+	void 핀을_가져온다(int input) {
+		Pin pin = new Pin(input);
+		assertThat(pin).isEqualTo(new Pin(input));
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {-1, 11})
+	void 잘못된_숫자의_핀을_가져오면_오류를_반환한다(int input) {
+		assertThatThrownBy(() -> new Pin(input))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	void 다음_핀을_가져온다() {
+		Pin pin = new Pin(9);
+		Pin nextPin = pin.next(1);
+		assertThat(nextPin).isEqualTo(new Pin(1));
+	}
+
+	@Test
+	void 잘못된_숫자의_다음_핀을_가져오면_오류를_반환한다() {
+		Pin pin = new Pin(9);
+		assertThatThrownBy(() -> pin.next(2))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+}
