a5379de93eab5d9697b8c0442226740d826e9b5f
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-11-08 20:46:46 +0900

    feat: BowlingGameTest to error throw wrong first, second number

--- src/test/java/bowling/domain/BowlingGameTest.java
+++ src/test/java/bowling/domain/BowlingGameTest.java
@@ -22,12 +22,20 @@ public class BowlingGameTest {
 	}
 
 	@ParameterizedTest
-	@CsvSource(value = {"10, 1, 9, 10, 3", "1, 1, 1, 1, 2", "10, 10, 10, 10, 4"})
+	@CsvSource(value = {"10, 5, 5, 10, 3", "1, 1, 1, 1, 2", "10, 10, 10, 10, 4"})
 	void 볼링게임의_프레임개수를_알려준다(int first, int second, int third, int fourth, int expect) {
 		bowlingGame.pitch(first);
 		bowlingGame.pitch(second);
 		bowlingGame.pitch(third);
 		bowlingGame.pitch(fourth);
 		assertThat(bowlingGame.getFrames()).hasSize(expect);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"5, 6", "1, -1", "10, 12"})
+	void 잘못된_피칭을_하면_오류를_반환한다(int first, int second) {
+		bowlingGame.pitch(first);
+		assertThatThrownBy(() -> bowlingGame.pitch(second))
+			.isInstanceOf(IllegalArgumentException.class);
 	}
 }
