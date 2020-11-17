c9ef51d0c4168c0c4d4481e2d0e78d50708a3862
etff <wish2rich@gmail.com>
2020-11-17 18:33:37 +0900

    Feature(frames): add convert point

--- src/main/java/bowling/domain/frame/Frames2.java
+++ src/main/java/bowling/domain/frame/Frames2.java
@@ -3,22 +3,29 @@ package bowling.domain.frame;
 import bowling.domain.point.Point;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class Frames2 {
     private static final int MIN_FRAME_INDEX = 0;
     private static final int MAX_FRAME_INDEX = 10;
     private static final String FRAME_MIN_ERROR = "프레임은 0보다 커야합니다";
     private static final String FRAME_MAX_ERROR = "프레임은 10보다 클 수 없습니다";
+    private static final String STRIKE_SIGN = "X";
+    private static final String SPARE_SIGN = "/";
+    private static final String NONE = "";
     private static final int BASE_INDEX = 0;
+    private static final int ZERO = 0;
     private static final int LAST_NORMAL_FRAME_INDEX = 8;
 
-    private final LinkedList<Frame2> frames = new LinkedList<>();
+    private final List<Frame2> frames = new LinkedList<>();
+    private final List<String> points = new LinkedList<>();
     private int frameIndex;
 
-    public Frames2() {
+    private Frames2() {
         validate(frameIndex);
         this.frameIndex = BASE_INDEX;
         frames.add(NormalFrame2.of());
+        points.add(convertPoint());
     }
 
     public static Frames2 generate() {
@@ -28,6 +35,7 @@ public class Frames2 {
     public void nextFrame() {
         if (frames.get(frameIndex).isLastRoll()) {
             frames.add(generateNextFrame());
+            points.add(NONE);
             frameIndex++;
         }
     }
@@ -48,6 +56,23 @@ public class Frames2 {
             return FinalFrame2.of();
         }
         return NormalFrame2.of();
+    }
+
+    private String convertPoint() {
+        Frame2 currentFrame = frames.get(frameIndex);
+        if (currentFrame.getScores().equals(STRIKE_SIGN) && currentFrame.isLastRoll()) {
+            return NONE;
+        }
+        if (currentFrame.getScores().equals(SPARE_SIGN) && currentFrame.isLastRoll()) {
+            return NONE;
+        }
+        if (currentFrame.getScores().equals(ZERO) && currentFrame.isLastRoll()) {
+            return String.valueOf(currentFrame.getPoint());
+        }
+        if (currentFrame.isLastRoll()) {
+            return String.valueOf(currentFrame.getPoint());
+        }
+        return NONE;
     }
 
     public boolean isGameOver() {
