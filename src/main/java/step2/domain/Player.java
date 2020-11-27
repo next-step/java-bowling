6f4e39fe4e63c458302758ea515553e5a80f4f5a
catsbi <killfine1988@gmail.com>
2020-11-27 20:12:26 +0900

    [refactor] 문자열 상수화

--- src/main/java/step2/domain/Player.java
+++ src/main/java/step2/domain/Player.java
@@ -4,6 +4,8 @@ import java.util.regex.Pattern;
 
 public class Player {
     public static final Pattern pattern = Pattern.compile("[^a-zA-Z]");
+    public static final String ERROR_INVALID_NAME = "이름이 유효하지 않습니다.";
+
     private final String name;
 
     public Player(String name) {
@@ -15,7 +17,7 @@ public class Player {
     public static void isValidName(String name) {
         int length = name.length();
         if ((length > 3 || length <= 0) || pattern.matcher(name).find()) {
-            throw new IllegalArgumentException("이름이 유효하지 않습니다.");
+            throw new IllegalArgumentException(ERROR_INVALID_NAME);
         }
     }
 
