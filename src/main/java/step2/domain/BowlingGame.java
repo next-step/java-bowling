e3f677a68ccdc3674b86862cc5b8fb43cbecdb72
catsbi <hansolsolsol@mosition.co.kr>
2020-11-30 18:17:38 +0900

    [refactor] BowlingGame 1회 투구 메소드 추가

--- src/main/java/step2/domain/BowlingGame.java
+++ src/main/java/step2/domain/BowlingGame.java
@@ -1,42 +1,28 @@
 package step2.domain;
 
-import step2.domain.dto.PlayerDTO;
-import step2.strategy.BowlingPitchesStrategy;
+import step2.exception.InvalidPitchesException;
+
+import java.util.List;
 
 public class BowlingGame {
 
     public static final int FRAME_FIRST_NO = 0;
     public static final int FRAME_LAST_NO = 8;
     public static final int FRAME_SIZE = 10;
-    public static final int PLUS_NUMBER = 1;
 
     public static Frames build() {
         return Frames.Builder()
                 .size(FRAME_SIZE)
-                .head(NormalFrame.of(FRAME_FIRST_NO))
+                .head(new NormalFrame(FRAME_FIRST_NO))
                 .build();
     }
 
-    public static GameHistories start(PlayerDTO playerDTO, BowlingPitchesStrategy strategy) {
-        boolean loop = true;
-        GameHistories gameHistories = new GameHistories();
-        Frames frames = playerDTO.getFrames();
-        Frame frame = frames.getHead();
+    public static List<String> pitches(Frames frames, int pitchesCount) throws InvalidPitchesException {
+        Frame headFrame = frames.getHead();
+        Frame frame = frames.getLastCompletedFrame(headFrame);
+        frame.pitches(pitchesCount);
 
-        while (loop) {
-            int pitches = frame.pitches(strategy);
-            Frame lastFrame = frames.getLastCompletedFrame(frame);
+        return frames.getMarks(headFrame);
 
-            gameHistories.addHistory(GameHistory.Builder()
-                    .pitchesPoint(pitches)
-                    .player(playerDTO.getPlayer())
-                    .currentFrameNo(lastFrame.getFrameNo() + PLUS_NUMBER)
-                    .marks(frames.getMarks(frame))
-                    .points(frames.getScores(frame))
-                    .build());
-
-            loop = !lastFrame.isFinished();
-        }
-        return gameHistories;
     }
 }
