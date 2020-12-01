622e96f07e6ee3bdf3e3b8156c2980b1e0de451b
catsbi <hansolsolsol@mosition.co.kr>
2020-12-01 18:27:47 +0900

    [refactor] hash and equals 추가

--- src/main/java/step2/domain/Player.java
+++ src/main/java/step2/domain/Player.java
@@ -1,5 +1,6 @@
 package step2.domain;
 
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class Player {
@@ -23,5 +24,18 @@ public class Player {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
