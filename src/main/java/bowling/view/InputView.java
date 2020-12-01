b5d4600d417dd7f609a3e27899619df8fc942dbb
genos.lee <genos.lee@kakaocorp.com>
2020-12-01 22:02:14 +0900

    feature: 전체 플레이어 수와 각 플레이어 명을 입력받음

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private final static String MEMBER_NAME_PHRASE = "플레이어 이름은(3 english letters)?: ";
-    private final static String SCORE_PHRASE = "%d프레임 투구 : ";
+    private static final String MEMBER_COUNT_PHRASE = "How many people? ";
+    private static final String MEMBER_NAME_PHRASE = "플레이어 %d의 이름은(3 english letters)?: ";
+    private static final String SCORE_PHRASE = "%s's turn : ";
     private final Scanner scanner;
     private final PrintWriter output;
 
@@ -15,13 +16,18 @@ public class InputView {
         this.output = output;
     }
 
-    public String enterMemberName() {
-        output.println(MEMBER_NAME_PHRASE);
+    public int enterMemberCount() {
+        output.println(MEMBER_COUNT_PHRASE);
+        return Integer.parseInt(scanner.nextLine().trim());
+    }
+
+    public String enterMemberName(int memberNumber) {
+        output.println(String.format(MEMBER_NAME_PHRASE, memberNumber));
         return scanner.nextLine().trim();
     }
 
-    public int enterScore(int frameNumber) {
-        output.println(String.format(SCORE_PHRASE, frameNumber));
+    public int enterScore(String memberName) {
+        output.println(String.format(SCORE_PHRASE, memberName));
         return Integer.parseInt(scanner.nextLine().trim());
     }
 }
--- src/test/java/bowling/view/InputViewTest.java
+++ src/test/java/bowling/view/InputViewTest.java
@@ -11,12 +11,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("입력 테스트")
 public class InputViewTest {
+
+    @DisplayName("참여자 명수 입력")
+    @Test
+    public void enterMemberCount() {
+        InputView inputView = new InputView(new Scanner("2"), new PrintWriter(new StringWriter()));
+
+        assertThat(inputView.enterMemberCount()).isEqualTo(2);
+    }
+
+    @DisplayName("참여자 명수 입력시 문구")
+    @Test
+    public void enterMemberCountPhrase() {
+        StringWriter output = new StringWriter();
+        InputView inputView = new InputView(new Scanner("2"), new PrintWriter(output));
+
+        inputView.enterMemberCount();
+
+        assertThat(output.toString()).isEqualTo("How many people? \n");
+    }
+
     @DisplayName("참여자 이름 입력")
     @Test
     public void enterMemberName() {
         InputView inputView = new InputView(new Scanner("ABC"), new PrintWriter(new StringWriter()));
 
-        assertThat(inputView.enterMemberName()).isEqualTo("ABC");
+        assertThat(inputView.enterMemberName(1)).isEqualTo("ABC");
     }
 
     @DisplayName("참여자 이름 입력시 문구")
@@ -25,9 +45,9 @@ public class InputViewTest {
         StringWriter output = new StringWriter();
         InputView inputView = new InputView(new Scanner("ABC"), new PrintWriter(output));
 
-        inputView.enterMemberName();
+        inputView.enterMemberName(1);
 
-        assertThat(output.toString()).isEqualTo("플레이어 이름은(3 english letters)?: \n");
+        assertThat(output.toString()).isEqualTo("플레이어 1의 이름은(3 english letters)?: \n");
     }
 
     @DisplayName("참여자 투구 점수 입력")
@@ -35,7 +55,7 @@ public class InputViewTest {
     public void enterScore() {
         InputView inputView = new InputView(new Scanner("3"), new PrintWriter(new StringWriter()));
 
-        assertThat(inputView.enterScore(1)).isEqualTo(3);
+        assertThat(inputView.enterScore("PJS")).isEqualTo(3);
     }
 
     @DisplayName("참여자 투구 점수 입력시 문구")
@@ -44,8 +64,8 @@ public class InputViewTest {
         StringWriter output = new StringWriter();
         InputView inputView = new InputView(new Scanner("3"), new PrintWriter(output));
 
-        inputView.enterScore(1);
+        inputView.enterScore("PJS");
 
-        assertThat(output.toString()).isEqualTo("1프레임 투구 : \n");
+        assertThat(output.toString()).isEqualTo("PJS's turn : \n");
     }
 }
