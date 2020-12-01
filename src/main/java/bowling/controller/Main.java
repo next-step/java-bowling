2911bfa375a9c6163073fb007e2b360922fc6f40
고정완 (Can) <jw.gho@ecubelabs.com>
2020-12-02 08:18:44 +0900

    [refactor]: ModelMapper 제거

--- src/main/java/bowling/controller/Main.java
+++ src/main/java/bowling/controller/Main.java
@@ -1,12 +1,41 @@
 package bowling.controller;
 
 import bowling.domain.Game;
+import bowling.domain.Player;
+import bowling.domain.Players;
+import bowling.domain.Roll;
+import bowling.view.View;
 
 public class Main {
     private Main() {}
 
     public static void main(String[] args) {
-        Game game = ModelMapper.getGame();
+        Game game = getGame();
         game.play();
+    }
+
+    private static Game getGame() {
+        Game game = new Game();
+        getPlayers().addToGame(game, Main::getRoll);
+        game.registerGamePrinter(View::printGame);
+        return game;
+    }
+
+    private static Players getPlayers() {
+        return Players.of(
+                1, // View.askSizeOfPlayers().getSize(),
+                Main::getPlayer
+        );
+    }
+
+    private static Player getPlayer() {
+        return new Player(View.askPlayer()
+                .getName()
+        );
+    }
+
+    private static Roll getRoll(String prefix) {
+        return Roll.of(View.askRoll(prefix)
+                .getCountOfPins());
     }
 }
