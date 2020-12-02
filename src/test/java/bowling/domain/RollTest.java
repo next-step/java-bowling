2789a4ef89b06319ca99ef0864bd1c9fdfeaa1a8
고정완 (Can) <jw.gho@ecubelabs.com>
2020-12-02 13:50:21 +0900

    [test]: getCountOfPins 테스트 수정

--- src/test/java/bowling/domain/RollTest.java
+++ src/test/java/bowling/domain/RollTest.java
@@ -2,14 +2,9 @@ package bowling.domain;
 
 import bowling.exception.RollException;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
@@ -39,14 +34,11 @@ class RollTest {
                 .isEqualTo(count);
     }
 
-    @Test
-    @DisplayName("sum 테스트")
-    void sum() {
-        List<Integer> counts = Arrays.asList(3, 5, 7);
-
-        List<Roll> rolls = counts.stream().map(Roll::of).collect(toList());
-        assertThat(Roll.sum(rolls))
-                .isEqualTo(counts.stream()
-                        .reduce(0, Integer::sum));
+    @ParameterizedTest
+    @DisplayName("getCountOfPins 테스트")
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+    void getCountOfPins(int count) {
+        assertThat(Roll.of(count).getCountOfPins())
+                .isEqualTo(count);
     }
 }
