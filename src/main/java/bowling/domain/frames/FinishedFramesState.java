b223e33ba852c1a8c18d9c0ececd3e0e90d12cfd
고정완 (Can) <jw.gho@ecubelabs.com>
2020-12-03 11:34:54 +0900

    [refactor] FramesState 에서 if 문 제거

--- src/main/java/bowling/domain/frames/FinishedFramesState.java
+++ src/main/java/bowling/domain/frames/FinishedFramesState.java
@@ -3,8 +3,6 @@ package bowling.domain.frames;
 import bowling.domain.Frame;
 import bowling.domain.Rolls;
 
-import static bowling.asset.Const.MAX_FRAME_NO;
-
 public class FinishedFramesState implements FramesState {
     private FinishedFramesState() {}
 
@@ -14,10 +12,6 @@ public class FinishedFramesState implements FramesState {
 
     @Override
     public void update(FramesContext context, Rolls rolls) {
-        if (context.size() >= MAX_FRAME_NO) {
-            context.setState(GameOverFramesState.getInstance());
-            return;
-        }
         Frame frame = Frame.of(rolls);
         context.addFrame(frame);
         FramesMediator.notify(context, frame);
--- src/main/java/bowling/domain/frames/FramesMediator.java
+++ src/main/java/bowling/domain/frames/FramesMediator.java
@@ -3,17 +3,24 @@ package bowling.domain.frames;
 import bowling.domain.Frame;
 import bowling.domain.FrameEnum;
 
+import static bowling.asset.Const.MAX_FRAME_NO;
+
 class FramesMediator {
     private FramesMediator() {}
 
     static void notify(FramesContext context, Frame frame) {
         FramesState nextState = frame.getFrameEnum() == FrameEnum.UNFINISHED
                 ? UnfinishedFramesState.getInstance()
-                : FinishedFramesState.getInstance();
+                : context.size() < MAX_FRAME_NO
+                ? FinishedFramesState.getInstance()
+                : GameOverFramesState.getInstance();
         context.setState(nextState);
     }
 
     static void notify(FramesContext context) {
-        context.setState(FinishedFramesState.getInstance());
+        FramesState nextState = context.size() < MAX_FRAME_NO
+                ? FinishedFramesState.getInstance()
+                : GameOverFramesState.getInstance();
+        context.setState(nextState);
     }
 }
