2d0f69948c3403ceca7da8572a8b19cd6bd8c1cd
고정완 (Can) <jw.gho@ecubelabs.com>
2020-12-03 12:15:53 +0900

    [chore]: Mediator 에 관한 주석 보완

--- src/main/java/bowling/domain/play/PlayMediator.java
+++ src/main/java/bowling/domain/play/PlayMediator.java
@@ -8,6 +8,8 @@ import static bowling.asset.Const.PIN_NUM;
 /**
  * NOTE: Mediator 에는 Context 의 State 를 바꾸는 로직만 있어야 한다.
  * 다른 상태나 객체값을 변화시키지 않도록 조심하자.
+ * Mediator 의 목적은 한 State 가 다른 State 들에게 의존하지 않도록 중재인 역할을 하는데 목적이 있다.
+ * 따라서, State 는 Colleague 로써 다른 State 에 의존성을 가져서는 안된다.
  */
 class PlayMediator {
     private PlayMediator() {}
