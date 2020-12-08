060e6d63b8c3b91ab6d41601c64b37086404e12d
LimHanGyeol <dlagksruf19@naver.com>
2020-12-08 16:27:37 +0900

    feat : InputView 게임 참여자 수 및 참여자 이름 입력 구현

--- src/main/java/bowling/global/utils/ExceptionMessage.java
+++ src/main/java/bowling/global/utils/ExceptionMessage.java
@@ -6,7 +6,7 @@ public class ExceptionMessage {
     public static final String INVALID_PLAYER_NAME_LENGTH = "이름의 길이는 영어 3자 이하입니다.";
     public static final String INVALID_PITCH_BALL_IS_NULL = "투구 값을 입력해주세요.";
     public static final String INVALID_PITCH_RANGE = "투구 범위는 0 ~ 10 입니다.";
-    public static final String INVALID_LARGE_THAN_REMAINING_PINS = "%d이(가) 넘는값을 입력할 수 없습니다.";
+    public static final String INVALID_INPUT_PLAYER_NUMBER = "게임 참여 인원은 숫자만 입력 가능합니다.";
 
     private ExceptionMessage() {
     }
