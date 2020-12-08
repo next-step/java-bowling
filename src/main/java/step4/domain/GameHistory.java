770f5c0d69524c56fc9b66ff01dedbd2ab61b557
catsbi <killfine1988@gmail.com>
2020-12-08 15:35:19 +0900

    [feat] GameHistory 빌더패턴 추가및 플레이어 이름 필드 추가

--- src/main/java/step4/domain/GameHistory.java
+++ src/main/java/step4/domain/GameHistory.java
@@ -4,12 +4,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class GameHistory {
+    private final String playerName;
     private final List<String> marks;
     private final List<String> points;
 
-    public GameHistory(List<String> marks, List<String> points) {
-        this.marks = Collections.unmodifiableList(marks);
-        this.points = Collections.unmodifiableList(points);
+    public GameHistory(Builder builder) {
+        this.marks = builder.marks;
+        this.points = builder.points;
+        this.playerName = builder.playerName;
+    }
+
+    public static Builder Builder() {
+        return new Builder();
     }
 
     public List<String> getMarks() {
@@ -19,4 +25,34 @@ public class GameHistory {
     public List<String> getPoints() {
         return points;
     }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public static class Builder {
+        private String playerName;
+        private List<String> marks;
+        private List<String> points;
+
+        public Builder playerName(String playerName) {
+            this.playerName = playerName;
+            return this;
+        }
+
+        public Builder marks(List<String> marks) {
+            this.marks = Collections.unmodifiableList(marks);
+            return this;
+        }
+
+        public Builder points(List<String> points) {
+            this.points = Collections.unmodifiableList(points);
+            return this;
+        }
+
+        public GameHistory build() {
+            return new GameHistory(this);
+        }
+    }
+
 }
