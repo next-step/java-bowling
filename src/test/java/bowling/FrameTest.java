864855d54972399108e1b4bb78fe9bb43a729400
siyoon <siyoon210@gmail.com>
2020-12-09 07:17:54 +0900

    test: 한 프레임의 두번째 투구에서 10개의 핀을 모두 쓰러트린 경우 스페어(/)

--- src/test/java/bowling/FrameTest.java
+++ src/test/java/bowling/FrameTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class FrameTest {
     @Test
-    @DisplayName("첫번째 투구에서 10개의 핀을 모두 쓰러트리면 스트라이크")
+    @DisplayName("첫번째 투구에서 10개의 핀을 모두 쓰러트리면 스트라이크(X)")
     public void strikeTest() {
         Frame frame = new Frame();
         frame.setKnockDownPins(10);
@@ -22,5 +22,14 @@ public class FrameTest {
         frame.setKnockDownPins(3);
         frame.setKnockDownPins(5);
         assertThat(frame.getStatus()).isEqualTo("3|5");
+    }
+
+    @Test
+    @DisplayName("한 프레임의 두번째 투구에서 10개의 핀을 모두 쓰러트린 경우 스페어(/)")
+    public void spareTest() {
+        Frame frame = new Frame();
+        frame.setKnockDownPins(3);
+        frame.setKnockDownPins(7);
+        assertThat(frame.getStatus()).isEqualTo("3|/");
     }
 }
