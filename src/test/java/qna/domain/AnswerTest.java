77fc6788104c9db523854d6c8063b0910cea8011
wani <oen142@naver.com>
2020-12-08 11:35:23 +0900

    feat : 각각 테스트 클래스 정의

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,31 @@
 package qna.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("삭제를 성공한다.")
+    void deleteSuccess() {
+
+    }
+
+    @Test
+    @DisplayName("다른 사람이 쓴 글은 삭제할수 없다.")
+    void throwAnotherWrite() {
+    }
+
+    @Test
+    @DisplayName("질문자와 답변자가 같을경우 삭제가 가능하다.")
+    void deleteSuccessSameUser() {
+    }
+
+
+
 }
