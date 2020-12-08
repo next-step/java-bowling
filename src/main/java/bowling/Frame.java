7b4e3691f2ab241a5e15c021549329c34614c658
siyoon <siyoon210@gmail.com>
2020-12-09 07:14:24 +0900

    feat: 한 프레임의 모든 투구에서 10개의 핀을 모두 쓰러트리지 못한 경우 점수만 표기

--- src/main/java/bowling/Frame.java
+++ src/main/java/bowling/Frame.java
@@ -2,16 +2,23 @@ package bowling;
 
 public class Frame {
     private int score;
+    private Integer firstPitching;
+    private Integer secondPitching;
 
     public void setKnockDownPins(int knockDownPins) {
-        score = knockDownPins;
+        if (firstPitching == null) {
+            firstPitching = knockDownPins;
+            return;
+        }
+
+        secondPitching = knockDownPins;
     }
 
     public String getStatus() {
-        if (score == 10) {
+        if (firstPitching == 10) {
             return "X";
         }
 
-        return String.valueOf(score);
+        return firstPitching + "|" + secondPitching;
     }
 }
