48bf6315b0cb17de7050862c3efe69132f0a904a
siyoon <siyoon210@gmail.com>
2020-12-09 09:16:34 +0900

    feat: 프레임 출력

--- src/main/java/bowling/BowlingGame.java
+++ src/main/java/bowling/BowlingGame.java
@@ -27,9 +27,27 @@ public class BowlingGame {
 
             int knockDownPins = Integer.parseInt(scanner.nextLine());
             frame.setKnockDownPins(knockDownPins);
-
-            System.out.println("frame" + (frameIndex + 1));
-            System.out.println(frame.getStatus());
+            print(frames);
         }
+    }
+
+    private static void print(List<Frame> frames) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("| NAME |");
+        for (int i = 1; i <= frames.size(); i++) {
+            sb.append("  ").append(String.format("%02d", i)).append("  |");
+        }
+        sb.append(System.lineSeparator());
+
+        sb.append("|  JSY |");
+        for (Frame frame : frames) {
+            sb.append(centerString(6, frame.getStatus())).append("|");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    public static String centerString (int width, String s) {
+        return String.format("%-" + width  + "s", String.format("%" + (s.length() + ((width - s.length()) / 2) + 1) + "s", s));
     }
 }
