943c6e1d2c40a3f7637f8254e4c9eb3c6861dbdb
Kaeum <oror1024@gmail.com>
2020-12-10 15:29:46 +0900

    style: 익셉션 패키지 생성 및 이동

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -1,7 +1,7 @@
 package qna.domain;
 
-import qna.NotFoundException;
-import qna.UnAuthorizedException;
+import qna.exception.NotFoundException;
+import qna.exception.UnAuthorizedException;
 
 import javax.persistence.*;
 
