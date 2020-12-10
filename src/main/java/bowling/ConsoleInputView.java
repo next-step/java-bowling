c0f62f92a2ec928e33d5017aeb4a808a8c6952f1
siyoon <siyoon210@gmail.com>
2020-12-11 07:10:06 +0900

    feat: 콘솔로 입력값 받는 ConsoleInputView 구현

---
+++ src/main/java/bowling/ConsoleInputView.java
@@ -0,0 +1,21 @@
+package bowling;
+
+import java.util.Scanner;
+
+public class ConsoleInputView {
+    private final Scanner scanner;
+
+    public ConsoleInputView() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String getPlayerName() {
+        System.out.print("플레이어 이름은(3 english letters)?:");
+        return scanner.nextLine();
+    }
+
+    public int getKnockDownPins(int frameIndex) {
+        System.out.print(frameIndex +"프레임 투구 : ");
+        return Integer.parseInt(scanner.nextLine());
+    }
+}
