054c4f5a5d3b3ca07e5b632bed7880e8f9b48402
oen142 <oen142@naver.com>
2020-12-10 22:02:08 +0900

    test : player의 이름이 3글자를 초과하면 익셉션을 발생한다.

--- src/test/java/bowling/domain/PlayerTest.java
+++ src/test/java/bowling/domain/PlayerTest.java
@@ -1,5 +1,6 @@
 package bowling.domain;
 
+import bowling.exception.PlayerNameLengthException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,12 +14,17 @@ class PlayerTest {
 
     @ParameterizedTest
     @DisplayName("플레이어가 잘 생성되는지 확인한다.")
-    @ValueSource(strings = "pobi,wani")
+    @ValueSource(strings = "POB,WAN")
     void create(String name) {
         Player player = Player.of(name);
         assertThat(player).isEqualTo(Player.of(name));
     }
 
 
-
+    @Test
+    @DisplayName("플레이어의 이름이 3글자를 초과하면 익셉션이 발생한다.")
+    void throwNameLength() {
+        Assertions.assertThatThrownBy(() -> Player.of("POBI"))
+                .isInstanceOf(PlayerNameLengthException.class);
+    }
 }
