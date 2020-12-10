2abeaf535600986e1dfaac87b3be2271657e3d1b
홍성민 <honggasm@gmail.com>
2020-12-11 00:43:06 +0900

    test: 첫 번째 두 번쨰 투구의 결과 합이 10을 넘는 경우 테스트

--- src/test/java/bowling/domain/NormalFrameTest.java
+++ src/test/java/bowling/domain/NormalFrameTest.java
@@ -1,9 +1,12 @@
 package bowling.domain;
 
+import bowling.InvalidFrameIndexException;
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 public class NormalFrameTest {
     @DisplayName("10개 미만의 핀이 쓰러진 경우 다음 투구 가능")
@@ -25,13 +28,30 @@ public class NormalFrameTest {
     void should_return_pitchable_false_when_first_fallen_pin_10() {
         // given
         final Frame normalFrame = NormalFrame.first();
-        final Pins pins = Pins.MAX;
 
         // when
-        normalFrame.pitch(pins);
+        normalFrame.pitch(Pins.MAX);
 
         // then
         assertThat(normalFrame.isPitchable()).isFalse();
+    }
+
+    @DisplayName("첫 번째, 두 번째 투구의 쓰러진 볼링 핀 합이 10개가 넘는 경우")
+    @Test
+    void should_throw_exception_when_first_second_fallen_pin_sum_is_grater_than_10() {
+        // given
+        final Frame normalFrame = NormalFrame.first();
+        final Pins first = Pins.of(5);
+        final Pins second = Pins.of(6);
+
+        // when
+        normalFrame.pitch(first);
+        
+        // when 
+        final Throwable thrown = catchThrowable(() -> normalFrame.pitch(second));
+        
+        // then
+        AssertionsForClassTypes.assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
     }
 
 }
