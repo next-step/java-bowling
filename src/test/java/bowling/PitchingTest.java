48666c210ee077dffefebd94b2aba98fcbe92b98
siyoon <siyoon210@gmail.com>
2020-12-10 20:11:32 +0900

    test: 테스트 케이스 추가

--- src/test/java/bowling/PitchingTest.java
+++ src/test/java/bowling/PitchingTest.java
@@ -1,13 +1,26 @@
 package bowling;
 
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class PitchingTest {
-    @Test
-    public void getPitchingTest() {
-        int knockDownPins = 10;
-        assertThat(Pitching.getPitching(knockDownPins)).isEqualTo(Pitching.STRIKE);
+    @ParameterizedTest
+    @CsvSource(value = {
+            "0:GUTTER",
+            "1:ONE_PIN",
+            "2:TWO_PINS",
+            "3:THREE_PINS",
+            "4:FOUR_PINS",
+            "5:FIVE_PINS",
+            "6:SIX_PINS",
+            "7:SEVEN_PINS",
+            "8:EIGHT_PINS",
+            "9:NINE_PINS",
+            "10:STRIKE"},
+            delimiter = ':')
+    public void getPitchingTest(int knockDownPins, Pitching pitching) {
+        assertThat(Pitching.getPitching(knockDownPins)).isEqualTo(pitching);
     }
 }
