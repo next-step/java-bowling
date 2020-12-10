267e7b386cbd3265c86174a31eea163d077e591b
siyoon <siyoon210@gmail.com>
2020-12-11 08:47:20 +0900

    test: PlayerName 생성 실패 테스트 케이스 추가

--- src/test/java/bowling/domain/PlayerNameTest.java
+++ src/test/java/bowling/domain/PlayerNameTest.java
@@ -1,8 +1,10 @@
 package bowling.domain;
 
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 
 public class PlayerNameTest {
@@ -10,5 +12,21 @@ public class PlayerNameTest {
     public void createTest() {
         assertThatCode(() -> PlayerName.valueOf("JSY"))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("플레이어 이름은 3글자로 제한한다.")
+    public void invalidLengthTest() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> PlayerName.valueOf("ABCD"))
+                .withMessage(PlayerName.INVALID_PLAYER_NAME_ERR_MSG);
+    }
+
+    @Test
+    @DisplayName("플레이어 이름은 알파벳으로 제한한다.")
+    public void invalidCharTest() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> PlayerName.valueOf("123"))
+                .withMessage(PlayerName.INVALID_PLAYER_NAME_ERR_MSG);
     }
 }
