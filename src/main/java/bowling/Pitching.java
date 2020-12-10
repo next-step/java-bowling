10e25cd44399711813661ffeb686b41e0640ffe4
siyoon <siyoon210@gmail.com>
2020-12-10 19:49:21 +0900

    feat: Pitching 상태를 나타내는 enum 생성

---
+++ src/main/java/bowling/Pitching.java
@@ -0,0 +1,16 @@
+package bowling;
+
+public enum Pitching {
+    GUTTER,
+    ONE_PIN,
+    TWO_PINS,
+    THREE_PINS,
+    FOUR_PINS,
+    FIVE_PINS,
+    SIX_PINS,
+    SEVEN_PINS,
+    EIGHT_PINS,
+    NINE_PINS,
+    STRIKE,
+    SPARE;
+}
