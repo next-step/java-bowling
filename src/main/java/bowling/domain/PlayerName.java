5e7a2089f7a035820be0a36fb023b3ac91ce0195
siyoon <siyoon210@gmail.com>
2020-12-11 08:53:04 +0900

    refactor: 글자수 알파벳 검사 모두 정규표현식으로 변경

--- src/main/java/bowling/domain/PlayerName.java
+++ src/main/java/bowling/domain/PlayerName.java
@@ -2,18 +2,18 @@ package bowling.domain;
 
 public class PlayerName {
     public static final String INVALID_PLAYER_NAME_ERR_MSG = "플레이어의 이름은 영문 3글자로 입력해주세요.";
+    public static final String VALID_INPUT_REGEX = "^[a-zA-Z]{3}";
     private final String value;
 
     private PlayerName(String value) {
-        if (value.length() != 3) {
-            throw new IllegalArgumentException(INVALID_PLAYER_NAME_ERR_MSG);
-        }
-
-        if (!value.matches("^[a-zA-Z]*${3}")) {
-            throw new IllegalArgumentException(INVALID_PLAYER_NAME_ERR_MSG);
-        }
-
+        validateInputValue(value);
         this.value = value;
+    }
+
+    private void validateInputValue(String value) {
+        if (!value.matches(VALID_INPUT_REGEX)) {
+            throw new IllegalArgumentException(INVALID_PLAYER_NAME_ERR_MSG);
+        }
     }
 
     public static PlayerName valueOf(String value) {
--- src/test/java/bowling/domain/PlayerNameTest.java
+++ src/test/java/bowling/domain/PlayerNameTest.java
@@ -2,9 +2,11 @@ package bowling.domain;
 
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 
 public class PlayerNameTest {
@@ -14,7 +16,8 @@ public class PlayerNameTest {
                 .doesNotThrowAnyException();
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"", "AB", "ABCD"})
     @DisplayName("플레이어 이름은 3글자로 제한한다.")
     public void invalidLengthTest() {
         assertThatExceptionOfType(IllegalArgumentException.class)
@@ -22,11 +25,12 @@ public class PlayerNameTest {
                 .withMessage(PlayerName.INVALID_PLAYER_NAME_ERR_MSG);
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"123",  "12A", "!@#", "ABㄱ"})
     @DisplayName("플레이어 이름은 알파벳으로 제한한다.")
-    public void invalidCharTest() {
+    public void invalidCharTest(String invalidInput) {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> PlayerName.valueOf("123"))
+                .isThrownBy(() -> PlayerName.valueOf(invalidInput))
                 .withMessage(PlayerName.INVALID_PLAYER_NAME_ERR_MSG);
     }
 }
