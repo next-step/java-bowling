26efd14206e81a9140c394a1f279f09431b7d7a6
siyoon <siyoon210@gmail.com>
2020-12-10 20:07:01 +0900

    feat: knockDownPins으로 pitching 반환받기

--- src/main/java/bowling/Pitching.java
+++ src/main/java/bowling/Pitching.java
@@ -1,16 +1,44 @@
 package bowling;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Pitching {
-    GUTTER,
-    ONE_PIN,
-    TWO_PINS,
-    THREE_PINS,
-    FOUR_PINS,
-    FIVE_PINS,
-    SIX_PINS,
-    SEVEN_PINS,
-    EIGHT_PINS,
-    NINE_PINS,
-    STRIKE,
-    SPARE;
+    GUTTER(0),
+    ONE_PIN(1),
+    TWO_PINS(2),
+    THREE_PINS(3),
+    FOUR_PINS(4),
+    FIVE_PINS(5),
+    SIX_PINS(6),
+    SEVEN_PINS(7),
+    EIGHT_PINS(8),
+    NINE_PINS(9),
+    STRIKE(10),
+    SPARE(null);
+
+    private final Integer score;
+
+    Pitching(Integer score) {
+        this.score = score;
+    }
+
+    public static final Map<Integer, Pitching> pitchingByScore = new HashMap<>();
+    static {
+        for (Pitching pitching : Pitching.values()) {
+            pitchingByScore.put(pitching.score, pitching);
+        }
+    }
+
+    public static Pitching getPitching(int knockDownPins) {
+        return pitchingByScore.get(knockDownPins);
+    }
+
+    public static Pitching getPitching(int knockDownPins, Pitching previousPitching) {
+        if (knockDownPins + previousPitching.score == STRIKE.score) {
+            return SPARE;
+        }
+
+        return getPitching(knockDownPins);
+    }
 }
