6df73264e39266acadb05b53b26ce53ad39db115
oen142 <oen142@naver.com>
2020-12-11 00:24:39 +0900

    feat : 플레이어의 이름은 3글자 이상 넘을수 없다.

--- src/test/java/bowling/domain/PlayerTest.java
+++ src/test/java/bowling/domain/PlayerTest.java
@@ -5,10 +5,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class PlayerTest {
 
