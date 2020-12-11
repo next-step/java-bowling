50dd39ed9a34963590006fcd6913132b1bdd15a7
kit(김광수) <kit.t@kakaocorp.com>
2020-12-11 21:59:47 +0900

    refactor: 중복 테스트 삭제
    행위 검증 테스트만 남기고 상태 검증 테스트는 도메인에서 진행하므로 삭제한다

--- src/test/java/qna/service/QnaServiceTest.java
+++ src/test/java/qna/service/QnaServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import qna.CannotDeleteException;
 import qna.domain.*;
 
 import java.time.LocalDateTime;
@@ -15,7 +14,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -51,14 +49,6 @@ public class QnaServiceTest {
         verifyDeleteHistories();
     }
 
-    @Test
-    public void delete_다른_사람이_쓴_글() throws Exception {
-        when(questionRepository.findByIdAndDeletedFalse(question.getId())).thenReturn(Optional.of(question));
-
-        assertThatThrownBy(() -> {
-            qnAService.deleteQuestion(UserTest.SANJIGI, question.getId());
-        }).isInstanceOf(CannotDeleteException.class);
-    }
 
     @Test
     public void delete_성공_질문자_답변자_같음() throws Exception {
@@ -69,19 +59,6 @@ public class QnaServiceTest {
         assertThat(question.isDeleted()).isTrue();
         assertThat(answer.isDeleted()).isTrue();
         verifyDeleteHistories();
-    }
-
-    @Test
-    public void delete_답변_중_다른_사람이_쓴_글() throws Exception {
-        question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
-        answer = new Answer(11L, UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents1");
-        question.addAnswer(answer);
-
-        when(questionRepository.findByIdAndDeletedFalse(question.getId())).thenReturn(Optional.of(question));
-
-        assertThatThrownBy(() -> {
-            qnAService.deleteQuestion(UserTest.JAVAJIGI, question.getId());
-        }).isInstanceOf(CannotDeleteException.class);
     }
 
     private void verifyDeleteHistories() {
