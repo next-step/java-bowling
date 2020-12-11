d00b0771588f2468078b8f02d8c38587bc485936
catsbi <killfine1988@gmail.com>
2020-12-11 10:53:32 +0900

    [refactor] 테스트케이스 수정

--- src/test/java/step4/FramePointTest.java
+++ src/test/java/step4/FramePointTest.java
@@ -1,11 +1,14 @@
 package step4;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import step4.domain.BowlingGame;
+import step4.controller.BowlingController;
 import step4.domain.Frames;
+import step4.domain.NormalFrame;
+import step4.domain.Player;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,6 +17,12 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FramePointTest {
+    private Player player;
+
+    @BeforeEach
+    void setup() {
+        player = Player.of("tst");
+    }
 
     @DisplayName("올 스트라이크 점수 테스트")
     @ParameterizedTest
@@ -26,15 +35,18 @@ public class FramePointTest {
     }
 
     private static Stream<Arguments> provideAllStrikeFrames() {
-        Frames frames = BowlingGame.build();
-        while (!frames.isFinished()) {
-            BowlingGame.pitches(frames, 10);
+        Player player = new Player("Tst", Frames.Builder()
+                .size(Player.FRAME_SIZE)
+                .head(new NormalFrame(Player.FRAME_FIRST_NO))
+                .build());
+        while (!player.isFinished()) {
+            player.pitches(10);
         }
 
         List<String> strings = Arrays.asList("30", "60", "90", "120", "150", "180", "210", "240", "270", "300");
 
         return Stream.of(
-                Arguments.of(frames, 300, strings)
+                Arguments.of(player.getFrames(), 300, strings)
         );
     }
 
@@ -48,13 +60,13 @@ public class FramePointTest {
     }
 
     private static Stream<Arguments> provideBowlingScoreInfo() {
-        Frames frames = BowlingGame.build();
+        Player player = Player.of("tst");
         List<Integer> pitchesCounts = Arrays.asList(1, 2, 3, 4, 5, 4, 5, 4, 3, 7, 10, 6, 4, 2, 8, 0, 2, 10, 5, 4);
-        pitchesCounts.forEach(pitchesCount -> BowlingGame.pitches(frames, pitchesCount));
+        pitchesCounts.forEach(player::pitches);
         List<String> pitchesCountStrings = Arrays.asList("3", "10", "19", "28", "48", "68", "80", "90", "92", "111");
 
         return Stream.of(
-                Arguments.of(frames, 111, pitchesCountStrings)
+                Arguments.of(player.getFrames(), 111, pitchesCountStrings)
         );
     }
 
--- src/test/java/step4/FramesTest.java
+++ src/test/java/step4/FramesTest.java
@@ -1,5 +1,6 @@
 package step4;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,18 +10,25 @@ import step4.domain.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FramesTest {
+    private Player player;
+
+    @BeforeEach
+    void setup() {
+        player = Player.of("tst");
+    }
+
 
     @DisplayName("10개의 프레임 생성 테스트")
     @Test
     void makeFrame() {
-        Frames frames = BowlingGame.build();
+        Frames frames = player.getFrames();
         assertThat(frames.size()).isEqualTo(10);
     }
 
     @DisplayName("첫번째 프레임과 마지막 프레임 클래스 타입 테스트")
     @Test
     void checkFirstAndLastFrame() {
-        Frames frames = BowlingGame.build();
+        Frames frames = player.getFrames();
         assertThat(frames.getHead()).isInstanceOf(NormalFrame.class);
     }
 
@@ -28,7 +36,7 @@ public class FramesTest {
     @ParameterizedTest
     @ValueSource(ints = {0,1,2,3,4,5,6,7,8,9})
     void getFrame(int frameNo) {
-        Frames frames = BowlingGame.build();
+        Frames frames = player.getFrames();
 
         assertThat(frames.getFrame(frameNo).getFrameNo()).isEqualTo(frameNo);
         assertThat(frames.getFrame(8).next()).isInstanceOf(FinalFrame.class);
@@ -37,16 +45,13 @@ public class FramesTest {
     @DisplayName("투구 테스트")
     @Test
     void pitches() {
-        Frames frames = BowlingGame.build();
-        Frame head = frames.getHead();
+        player.pitches(10);
+        player.pitches(10);
+        player.pitches(10);
+        player.pitches(5);
 
-        head.pitches(10);
-        head.pitches(10);
-        head.pitches(10);
-        head.pitches(5);
-
-        assertThat(head.getScore()).isEqualTo(30);
-        assertThat(frames.getFrame(3).getScore()).isEqualTo(5);
+        assertThat(player.getFrames().getTotalScores()).isEqualTo(75);
+        assertThat(player.getFrames().getFrame(3).getScore()).isEqualTo(5);
     }
 
 }
