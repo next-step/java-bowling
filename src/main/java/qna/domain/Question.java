5fc668b72eec208af0be807e18383275f353422f
kit(김광수) <kit.t@kakaocorp.com>
2020-12-11 21:01:55 +0900

    refactor: 답변 삭제 검증 이동

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -81,6 +81,12 @@ public class Question extends AbstractEntity {
             throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
         }
 
+        for (Answer answer : answers) {
+            if (!answer.isOwner(loginUser)) {
+                throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+            }
+        }
+
     }
 
     public Question setDeleted(boolean deleted) {
