6bd9733d7b2605ea3d13f243bd5cc01f2e4e9aff
kit(김광수) <kit.t@kakaocorp.com>
2020-12-11 21:11:41 +0900

    refactor: delete history 생성로직 이동

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -4,6 +4,7 @@ import org.hibernate.annotations.Where;
 import qna.CannotDeleteException;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -76,7 +77,7 @@ public class Question extends AbstractEntity {
         return writer.equals(loginUser);
     }
 
-    public void delete(User loginUser) throws CannotDeleteException {
+    public List<DeleteHistory> delete(User loginUser) throws CannotDeleteException {
         if (!isOwner(loginUser)) {
             throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
         }
@@ -86,11 +87,20 @@ public class Question extends AbstractEntity {
                 throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
             }
         }
+
         setDeleted(true);
         for (Answer answer : answers) {
             answer.setDeleted(true);
         }
 
+        List<DeleteHistory> deleteHistories = new ArrayList<>();
+
+        deleteHistories.add(new DeleteHistory(ContentType.QUESTION, getId(), getWriter(), LocalDateTime.now()));
+        for (Answer answer : answers) {
+            deleteHistories.add(new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now()));
+        }
+
+        return deleteHistories;
     }
 
     public Question setDeleted(boolean deleted) {
