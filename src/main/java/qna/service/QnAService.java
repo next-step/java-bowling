729ac06d824c1eccdb1e0c182990758e5ec5d983
kit(김광수) <kit.t@kakaocorp.com>
2020-12-11 21:41:39 +0900

    refactor: 코드정리

--- src/main/java/qna/service/QnAService.java
+++ src/main/java/qna/service/QnAService.java
@@ -32,8 +32,7 @@ public class QnAService {
 
     @Transactional
     public void deleteQuestion(User loginUser, long questionId) throws CannotDeleteException {
-        Question question = findQuestionById(questionId);
-        List<DeleteHistory> deleteHistories = question.delete(loginUser);
+        List<DeleteHistory> deleteHistories = findQuestionById(questionId).delete(loginUser);
         deleteHistoryService.saveAll(deleteHistories);
     }
 }
