8e90c485477a01b9ec34101dafc8b1ea442402d8
홍성민 <honggasm@gmail.com>
2020-12-12 11:31:28 +0900

    refactor: 불필요한 테스트용 정적 리스트 제거

--- src/test/java/bowling/domain/FrameFactoryTest.java
+++ src/test/java/bowling/domain/FrameFactoryTest.java
@@ -2,24 +2,17 @@ package bowling.domain;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FrameFactoryTest {
     public static final Frame FINAL;
-    public static final List<Frame> FRAME_LIST;
 
     static {
-        FRAME_LIST = new ArrayList<>();
         Frame frame = Frame.createFirst();
-        FRAME_LIST.add(frame);
         for (int i = 1; i < 10; i++) {
             frame = frame.createNext();
-            FRAME_LIST.add(frame);
         }
-        FINAL = FRAME_LIST.get(FRAME_LIST.size() - 1);
+        FINAL = frame;
     }
 
     @Test
