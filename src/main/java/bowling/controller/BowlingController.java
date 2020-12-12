a69b067a5da15ff4fcdce79fdbeaa86f0ad0a4b1
oen142 <oen142@naver.com>
2020-12-12 23:48:11 +0900

    feat : view controller 정의

--- src/main/java/bowling/controller/BowlingController.java
+++ src/main/java/bowling/controller/BowlingController.java
@@ -1,4 +1,30 @@
 package bowling.controller;
 
+import bowling.domain.Frame;
+import bowling.domain.Frames;
+import bowling.domain.Player;
+import bowling.domain.Type;
+import bowling.view.InputView;
+import bowling.view.ResultView;
+
 public class BowlingController {
+
+    public static void runBowlingGame() {
+        String playerName = InputView.inputPlayerName();
+
+        Player player = Player.of(playerName);
+
+        Frames frames = Frames.of();
+        Frame frame;
+        for (int i = 1; i <= 10; i++) {
+            frame = Frame.of();
+            Type firstType = frame.first(InputView.inputPitchBowl(i));
+            if (firstType == Type.STRIKE) {
+                continue;
+            }
+            frame.second(InputView.inputPitchBowl(i));
+            frames.inputFrame(frame);
+            ResultView.printBowlResult(frames);
+        }
+    }
 }
--- src/main/java/bowling/view/ResultView.java
+++ src/main/java/bowling/view/ResultView.java
@@ -1,6 +1,18 @@
 package bowling.view;
 
+import bowling.domain.Frames;
+
 public class ResultView {
+    private static final String FRAME = "|";
+    private static final String NAME = "NAME";
+    public static final String title = "| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |";
 
 
+    public static void printTitle() {
+        System.out.println(title);
+    }
+
+    public static void printBowlResult(Frames frames) {
+        frames.getFrames();
+    }
 }
