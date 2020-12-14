1dd979e49e6401f59e978498ec861ca4e83cc217
mand2 <nayeon.kim1123@gmail.com>
2020-12-14 20:37:33 +0900

    feat: 테스트 케이스 작성

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,15 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("답변자가 같은 경우 삭제가 가능하다")
+    void isAnswerWriterAndQuestionOwnerSame() {
+
+    }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,55 @@
 package qna.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+
+    @ParameterizedTest
+    @MethodSource("providedDeletedState")
+    @DisplayName("질문 삭제 요청이 오면 질문을 '삭제상태'로 변경한다.")
+    void changeQuestionInDeleteStateWhenRequestForDeleteSubmitted(Question question,
+                                                                  boolean requestState,
+                                                                  boolean expectedState) {
+        assertEquals(expectedState, question.setDeleted(requestState).isDeleted());
+    }
+
+    private static Stream<Arguments> providedDeletedState() {
+        return Stream.of(
+                arguments(Q1, true, true),
+                arguments(Q2, true, true)
+        );
+    }
+
+    @Test
+    @DisplayName("로그인 사용자와 질문한 사람이 같은 경우 삭제 가능")
+    void isLoginUserAndQuestionWriterSame() {
+
+    }
+
+    @Test
+    @DisplayName("답변이 없는 경우 삭제가 가능하다")
+    void isAnswerListNull() {
+
+    }
+
+    @Test
+    @DisplayName("답변자가 같은 경우 삭제가 가능하다")
+    void isAnswerWriterAndQuestionOwnerSame() {
+
+    }
+
 }
