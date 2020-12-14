1163f6dbd738eeee030477ef9db8ab61d067d389
mand2 <nayeon.kim1123@gmail.com>
2020-12-14 22:13:11 +0900

    refactor : Question 서비스 리팩토링

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -1,13 +1,20 @@
 package qna.domain;
 
-import org.hibernate.annotations.Where;
+import qna.CannotDeleteException;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Entity
 public class Question extends AbstractEntity {
+
+    public static final String ERROR_DELETE_QUESTION_AUTHORITY_MISMATCH = "질문을 삭제할 권한이 없습니다.";
+    public static final String ERROR_DELETE_ANSWERS_AUTHORITY_MISMATCH = "다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.";
+
     @Column(length = 100, nullable = false)
     private String title;
 
@@ -18,15 +25,12 @@ public class Question extends AbstractEntity {
     @JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"))
     private User writer;
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
-    @Where(clause = "deleted = false")
-    @OrderBy("id ASC")
-    private List<Answer> answers = new ArrayList<>();
+    @Embedded
+    private Answers answers; // nullable
 
     private boolean deleted = false;
 
-    public Question() {
-    }
+    public Question() {}
 
     public Question(String title, String contents) {
         this.title = title;
@@ -68,7 +72,7 @@ public class Question extends AbstractEntity {
 
     public void addAnswer(Answer answer) {
         answer.toQuestion(this);
-        answers.add(answer);
+        answers.addAnswer(answer);
     }
 
     public boolean isOwner(User loginUser) {
@@ -84,12 +88,42 @@ public class Question extends AbstractEntity {
         return deleted;
     }
 
-    public List<Answer> getAnswers() {
-        return answers;
+    public List<DeleteHistory> delete(User loginUser) throws CannotDeleteException {
+        // validate question and answers
+        validateWriter(loginUser);
+        validateAnswersWriter(loginUser);
+
+        return Stream
+                .concat(Stream.of(deleteQuestion()),
+                        deleteAnswers().stream())
+                .collect(Collectors.toList());
     }
 
-    @Override
+    private DeleteHistory deleteQuestion() {
+        this.deleted = true;
+        return new DeleteHistory(ContentType.QUESTION, getId(), writer, LocalDateTime.now());
+    }
+
+    private List<DeleteHistory> deleteAnswers() {
+        return this.answers.delete();
+    }
+
+    private void validateWriter(User loginUser) throws CannotDeleteException {
+        if (!isOwner(loginUser)) {
+            throw new CannotDeleteException(ERROR_DELETE_QUESTION_AUTHORITY_MISMATCH);
+        }
+    }
+
+    private void validateAnswersWriter(User loginUser) throws CannotDeleteException {
+        if (answers.isDifferentWriter(loginUser)) {
+            throw new CannotDeleteException(ERROR_DELETE_ANSWERS_AUTHORITY_MISMATCH);
+        }
+    }
+
+
+    @Override //FIXME 수정?
     public String toString() {
         return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
     }
+
 }
