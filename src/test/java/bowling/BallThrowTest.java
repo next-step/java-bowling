0fa9db2742f98bac9903dad1dbff437b0e08dcc1
kit(김광수) <kit.t@kakaocorp.com>
2020-12-15 20:50:22 +0900

    feat: make it pass

--- src/test/java/bowling/BallThrowTest.java
+++ src/test/java/bowling/BallThrowTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 /**
  * #### 투구
  * [x] 한 투구는 0..10의 쓰러뜨린 핀 수를 가진다
- * * 한 프레임내의 두번째 투구는 첫번재 투구의 핀수를 더해서 10을 초과할 수 없다
+ * [x] 한 프레임내의 두번째 투구는 첫번재 투구의 핀수를 더해서 10을 초과할 수 없다
  * * 마지막 프레임의 두번째 투구는 첫번째 투구가 10개의 핀수를 가진다면 또다시 10개의 핀수를 가질 수 있다
  * * 마지막 프레임의 두번째 투구는 첫번째 투구가 10개 미만의 핀수를 가진다면 첫번째 투구의 핀수를 더해서 10을 초과할 수 없다
  * * 마지막 프레임의 세번째 투구는 첫번째 투구의 핀수가 10인경우에만 존재한다
@@ -45,9 +45,17 @@ public class BallThrowTest {
     private static class BallThrow {
         public static final int MAX_PINS = 10;
         public static final int MIN_FINS = 0;
+        private final int fallingPins;
 
         public BallThrow(int fallingPins) {
+            this.fallingPins = fallingPins;
             if (fallingPins > MAX_PINS || fallingPins < MIN_FINS) {
+                throw new IllegalFallingPinsException();
+            }
+        }
+
+        public void throwSecond(int secondFallingPins) {
+            if (this.fallingPins + secondFallingPins > MAX_PINS) {
                 throw new IllegalFallingPinsException();
             }
         }
