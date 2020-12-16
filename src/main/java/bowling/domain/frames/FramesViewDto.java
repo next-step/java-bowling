7cd957731eff89e857f315b62380e6513485fefd
siyoon <siyoon210@gmail.com>
2020-12-16 10:19:37 +0900

    refactor: View에서 Frames에 대한 의존 제거

--- src/main/java/bowling/domain/frames/FramesViewDto.java
+++ src/main/java/bowling/domain/frames/FramesViewDto.java
@@ -3,7 +3,7 @@ package bowling.domain.frames;
 import java.util.stream.Stream;
 
 public interface FramesViewDto {
-    public static final int MAX_FRAME_SIZE = 10;
+    int MAX_FRAME_SIZE = 10;
 
     Stream<FrameViewDto> viewDtoStream();
 }
--- src/main/java/bowling/view/ConsoleResultView.java
+++ src/main/java/bowling/view/ConsoleResultView.java
@@ -2,7 +2,7 @@ package bowling.view;
 
 import bowling.domain.Pitching;
 import bowling.domain.bowlinggame.BowlingGameViewDto;
-import bowling.domain.frames.Frames;
+import bowling.domain.frames.FramesViewDto;
 
 import java.util.EnumMap;
 import java.util.Map;
@@ -52,7 +52,7 @@ public class ConsoleResultView implements ResultView {
     }
 
     private void appendFrameIndex(StringBuilder resultBuilder) {
-        IntStream.rangeClosed(1, Frames.MAX_FRAME_SIZE)
+        IntStream.rangeClosed(1, FramesViewDto.MAX_FRAME_SIZE)
                 .forEach(index -> {
                     String formattedFrameIndex = centerString(String.format(FRAME_INDEX_FORMAT, index));
                     resultBuilder.append(formattedFrameIndex).append(DELIMITER);
