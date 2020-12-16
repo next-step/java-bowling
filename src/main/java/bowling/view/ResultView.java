abfebd5fa4ee2f4d07e3f96eaacf48037b0610cc
oen142 <oen142@naver.com>
2020-12-16 23:37:33 +0900

    feat : 화면 결과를 출력하는 ResultView 생성

--- src/main/java/bowling/view/ResultView.java
+++ src/main/java/bowling/view/ResultView.java
@@ -4,13 +4,9 @@ import bowling.domain.Frames;
 
 public class ResultView {
     private static final String FRAME = "|";
-    private static final String NAME = "NAME";
-    public static final String title = "| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |";
-
-
-    public static void printTitle() {
-        System.out.println(title);
-    }
+    private static final String FRAME_TITLE = "NAME | %S ";
+    private static final String FRAME_BODY = "%s | ";
+    private static final String SEPARATOR = System.lineSeparator();
 
     public static void printBowlResult(Frames frames) {
         frames.getFrames();
