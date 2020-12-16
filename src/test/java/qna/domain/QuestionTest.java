300dd64f5ae430f7d5b4e57ddf5608cb95a7b930
SungHyeob Baek <baek9652@naver.com>
2020-12-16 11:15:41 +0900

    [test] 본인이 작성하지 않은 답변글이 포함되어 있을 때의 테스트 삭제

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -12,9 +12,20 @@ public class QuestionTest {
 
     @Test
     @DisplayName("login 유저가 본인 소유가 아닌 질문을 삭제하려 할 때의 예외 처리")
-    void test() {
+    void testQuestionsOwnerValidation() {
         assertThatThrownBy(
                 () -> Q1.delete(UserTest.SANJIGI)
+        ).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    @DisplayName("질문글에 자신의 소유가 아닌 답변글이 포함되어 있을 시 예외 처리")
+    void testQuestionsAnswerValidation() {
+        Q1.addAnswer(AnswerTest.A1);
+        Q1.addAnswer(AnswerTest.A2);
+
+        assertThatThrownBy(
+                () -> Q1.delete(UserTest.JAVAJIGI)
         ).isInstanceOf(CannotDeleteException.class);
     }
 }
