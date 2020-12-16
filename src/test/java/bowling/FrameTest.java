07dfa5c221e64597f303a8a72e7d46c14bc1632b
kit(김광수) <kit.t@kakaocorp.com>
2020-12-16 22:02:48 +0900

    feat: make it pass

--- src/test/java/bowling/FrameTest.java
+++ src/test/java/bowling/FrameTest.java
@@ -8,15 +8,15 @@ import java.util.Optional;
 
 import static bowling.BallThrow.MAX_PINS;
 import static bowling.BallThrow.MIN_PINS;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static bowling.FrameTest.Scoring.*;
+import static org.assertj.core.api.Assertions.*;
 
 /**
  * #### 프레임
  * [x] 1~9 프레임은 1..2 개의 투구를 가진다
  * * 마지막 프래임은 1..3 개의 투구를 가진다
  * * 각 프레임은 1부터 시작하는 자신의 프레임 넘버를 가진다
- * * 각 프레임은 프래임내 투구 결과에 따라 스트라이크/스페어/미스/거터의 상태를 가진다
+ * [x] 각 프레임은 프래임내 투구 결과에 따라 스트라이크/스페어/미스/거터의 상태를 가진다
  * * 1~9 프레임은 첫번째 투구의 쓰러뜨린 핀 수가 10인 경우 다음 프레임으로 전환한다
  * * 마지막 프레임은 아홉번째 프레임이 스트라이크 인 경우 한번 더 투구 할 수 있다
  * * 마지막 프레임은 첫번째 투구가 스트라이크 인 경우 두번 더 투구할 수 있다.
@@ -51,7 +51,7 @@ public class FrameTest {
     @Test
     void strike() {
         frame.throwBall(10);
-        assertThat(frame.getScoring()).isEqualTo(Scoring.STRIKE);
+        assertThat(frame.getScoring()).isEqualTo(Optional.of(STRIKE));
     }
 
     @DisplayName("9/1 개를 쓰러뜨리면 스페어")
@@ -59,7 +59,7 @@ public class FrameTest {
     void spare() {
         frame.throwBall(9);
         frame.throwBall(1);
-        assertThat(frame.getScoring()).isEqualTo(Scoring.SPARE);
+        assertThat(frame.getScoring()).isEqualTo(Optional.of(SPARE));
     }
 
     @DisplayName("9/0 개를 쓰러뜨리면 미스")
@@ -67,7 +67,7 @@ public class FrameTest {
     void miss() {
         frame.throwBall(9);
         frame.throwBall(0);
-        assertThat(frame.getScoring()).isEqualTo(Scoring.MISS);
+        assertThat(frame.getScoring()).isEqualTo(Optional.of(MISS));
     }
 
     @DisplayName("아무것도 쓰러뜨리지 못하면 거터")
@@ -75,12 +75,13 @@ public class FrameTest {
     void gutter() {
         frame.throwBall(0);
         frame.throwBall(0);
-        assertThat(frame.getScoring()).isEqualTo(Scoring.GUTTER);
+        assertThat(frame.getScoring()).isEqualTo(Optional.of(GUTTER));
     }
 
     @DisplayName("아직 프레임이 끝나지 않은 경우")
     @Test
     void incomplete() {
+        assertThat(frame.getScoring()).isEqualTo(Optional.empty());
         frame.throwBall(0);
         assertThat(frame.getScoring()).isEqualTo(Optional.empty());
     }
@@ -97,17 +98,24 @@ public class FrameTest {
             secondThrow = firstThrow.throwSecond(fallingPins);
         }
 
-        public Scoring getScoring() {
+        public Optional<Scoring> getScoring() {
+            if (isIncomplete()) {
+                return Optional.empty();
+            }
             if (firstThrow.isStrike()) {
-                return Scoring.STRIKE;
+                return Optional.of(STRIKE);
             }
             if (sumOfFallingPins() == MAX_PINS) {
-                return Scoring.SPARE;
+                return Optional.of(SPARE);
             }
             if (sumOfFallingPins() == MIN_PINS) {
-                return Scoring.GUTTER;
+                return Optional.of(GUTTER);
             }
-            return Scoring.MISS;
+            return Optional.of(MISS);
+        }
+
+        private boolean isIncomplete() {
+            return firstThrow == null || !firstThrow.isStrike() && secondThrow == null;
         }
 
         public int sumOfFallingPins() {
@@ -117,7 +125,7 @@ public class FrameTest {
         }
     }
 
-    private enum Scoring {
+    protected enum Scoring {
         STRIKE, MISS, GUTTER, SPARE
     }
 }
