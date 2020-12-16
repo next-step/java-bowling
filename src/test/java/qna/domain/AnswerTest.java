1e64788d8d07d5dc941c9700d3ef84e0fd6b9b6c
SungHyeob Baek <baek9652@naver.com>
2020-12-16 19:20:37 +0900

    [test] 삭제된 답변글 삭제 기능 관련 테스트 추가

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,23 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("삭제 테스트")
+    void testDelete() {
+        Answer sample = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "ABCD");
+
+        assertThat(sample.isDeleted()).isFalse();
+
+        sample.delete();
+
+        assertThat(sample.isDeleted()).isTrue();
+    }
 }
