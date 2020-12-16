34f53f4b5baf0a8c59717b83470eb804e1b7e31e
oen142 <oen142@naver.com>
2020-12-16 23:00:13 +0900

    feat : 포인트를 가지는 Score객체 생성

--- src/main/java/bowling/domain/Score.java
+++ src/main/java/bowling/domain/Score.java
@@ -1,23 +1,31 @@
 package bowling.domain;
 
+import bowling.exception.ScorePointRangeOutBoundException;
+
 public class Score {
-    private int first;
-    private int second;
+
+    private static final int MIN_SCORE_COUNT = 0;
+    private static final int MAX_SCORE_COUNT = 10;
 
 
-    private Score() {
+    private final int point;
+
+    private Score(int point) {
+        this.point = point;
     }
 
-    public Type first(int point) {
-        if (point > 10 || point < 0) {
-            throw new IllegalArgumentException("");
-        }
-        this.first = point;
-        if (point == 10) {
-            return Type.STRIKE;
-        }
-        return Type.NONE;
+    public static Score stringOf(String point) {
+        return valueOf(Integer.parseInt(point));
     }
 
+    private static Score valueOf(int point) {
+        validPoint(point);
+        return new Score(point);
+    }
 
+    private static void validPoint(int point) {
+        if (point < MIN_SCORE_COUNT || point > MAX_SCORE_COUNT) {
+            throw new ScorePointRangeOutBoundException();
+        }
+    }
 }
