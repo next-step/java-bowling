c0693926eece1295645899242f962d70ee57eca3
mimimjin <babysodapop21@hotmail.com>
2020-12-18 00:11:07 +0900

    refactor: ParameterizedTest - MethodSource를 사용하는 테스트로 변경

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,8 +1,13 @@
 package qna.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import qna.CannotDeleteException;
+
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -12,17 +17,33 @@ public class AnswerTest {
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
 
     @DisplayName("답변 삭제 성공 테스트")
-    @Test
-    public void deleteSuccessTest() throws CannotDeleteException {
-        A1.delete(UserTest.JAVAJIGI);
-        assertThat(A1.isDeleted()).isTrue();
+    @ParameterizedTest
+    @MethodSource("provideAnswersWithSameWriters")
+    public void deleteSuccessTest(Answer answer, User answerWriter) throws CannotDeleteException {
+        assertThat(answer.delete(answerWriter))
+                .isEqualTo(new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now()));
+    }
+
+    private static Stream<Arguments> provideAnswersWithSameWriters(){
+        return Stream.of(
+                Arguments.arguments(A1, UserTest.JAVAJIGI),
+                Arguments.arguments(A2, UserTest.SANJIGI)
+        );
     }
 
     @DisplayName("삭제할 답변이 다른 사람의 답변일때 Exception 테스트")
-    @Test
-    public void delete_others_answer_throw_exception(){
+    @ParameterizedTest
+    @MethodSource("provideAnswersWithOtherWriters")
+    public void delete_others_answer_throw_exception(Answer answer, User answerWriter){
         assertThatThrownBy(() -> {
-            A2.delete(UserTest.JAVAJIGI);
+            answer.delete(answerWriter);
         }).isInstanceOf(CannotDeleteException.class);
+    }
+
+    private static Stream<Arguments> provideAnswersWithOtherWriters(){
+        return Stream.of(
+                Arguments.arguments(A1, UserTest.SANJIGI),
+                Arguments.arguments(A2, UserTest.JAVAJIGI)
+        );
     }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -2,7 +2,13 @@ package qna.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import qna.CannotDeleteException;
+
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -13,18 +19,34 @@ public class QuestionTest {
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
 
     @DisplayName("질문 삭제 성공 테스트")
-    @Test
-    public void deleteSuccessTest() throws CannotDeleteException {
-        Q1.delete(UserTest.JAVAJIGI);
-        assertThat(Q1.isDeleted()).isTrue();
+    @ParameterizedTest
+    @MethodSource("provideQuestionsWithSameWriters")
+    public void deleteSuccessTest(Question question, User questionWriter) throws CannotDeleteException {
+        assertThat(question.delete(questionWriter).getDeleteHistories())
+                .containsExactly(new DeleteHistory(ContentType.QUESTION, question.getId(), question.getWriter(), LocalDateTime.now()));
+    }
+
+    private static Stream<Arguments> provideQuestionsWithSameWriters(){
+        return Stream.of(
+                Arguments.arguments(Q1, UserTest.JAVAJIGI),
+                Arguments.arguments(Q2, UserTest.SANJIGI)
+        );
     }
 
     @DisplayName("삭제할 질문이 다른 사람의 질문일때 Exception 테스트")
-    @Test
-    public void delete_others_question_throw_exception(){
+    @ParameterizedTest
+    @MethodSource("provideQuestionsWithOtherWriters")
+    public void delete_others_question_throw_exception(Question question, User questionWriter){
         assertThatThrownBy(() -> {
-            Q1.delete(UserTest.SANJIGI);
+            question.delete(questionWriter);
         }).isInstanceOf(CannotDeleteException.class);
+    }
+
+    private static Stream<Arguments> provideQuestionsWithOtherWriters(){
+        return Stream.of(
+                Arguments.arguments(Q1, UserTest.SANJIGI),
+                Arguments.arguments(Q2, UserTest.JAVAJIGI)
+        );
     }
 
 }
