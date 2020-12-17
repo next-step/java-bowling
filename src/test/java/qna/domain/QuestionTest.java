99675734b9abe08c62f70270415ed54a3d81ebbd
dbeod2 <darung.0211@gmail.com>
2020-12-17 23:01:29 +0900

    test : question 테스트 코드 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,29 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.Assert.assertTrue;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    @DisplayName("답변이 없는 경우 삭제 가능")
+    public void 답변_없는_경우() {
+        Question question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        assertTrue(question.hasAnswer());
+    }
+
+    @Test
+    @DisplayName("질문을 삭제할 권한이 없는경우")
+    public void 질문삭제_권한_없는_경우() {
+        Question question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        assertThatThrownBy(() -> {
+            question.isOwnerMatch(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
 }
