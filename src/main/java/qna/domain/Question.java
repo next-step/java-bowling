effa833c080776400c52f099760c5b4cc8737954
SungHyeob Baek <baek9652@naver.com>
2020-12-17 10:32:53 +0900

    [refactor] DeleteHistory 삭제 책임 Question 내부로 이동

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -4,6 +4,7 @@ import org.hibernate.annotations.Where;
 import qna.CannotDeleteException;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -94,7 +95,7 @@ public class Question extends AbstractEntity {
         return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
     }
 
-    public void delete(User loginUser) throws CannotDeleteException {
+    public List<DeleteHistory> delete(User loginUser) throws CannotDeleteException {
         if (!isOwner(loginUser)) {
             throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
         }
@@ -105,9 +106,16 @@ public class Question extends AbstractEntity {
             }
         }
 
+        List<DeleteHistory> deleteHistories = new ArrayList<>();
+
         deleted = true;
+        deleteHistories.add(new DeleteHistory(ContentType.QUESTION, getId(), writer, LocalDateTime.now()));
+
         for (Answer answer : answers) {
             answer.delete();
+            deleteHistories.add(new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now()));
         }
+
+        return deleteHistories;
     }
 }
