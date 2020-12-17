25f0b27a817a424a6f8642008761c4bdff13114d
oen142 <oen142@naver.com>
2020-12-17 23:42:14 +0900

    feat : 볼링 타입 정의

--- src/main/java/bowling/domain/BowlType.java
+++ src/main/java/bowling/domain/BowlType.java
@@ -1,15 +1,24 @@
 package bowling.domain;
 
-public enum Type {
+public enum BowlType {
 
     STRIKE("X"),
     SPARE("/"),
     GUTTER("-"),
+    ONE("1"),
+    TWO("2"),
+    THREE("3"),
+    FORE("4"),
+    FIVE("5"),
+    SIX("6"),
+    SEVEN("7"),
+    EIGHT("8"),
+    NINE("9"),
     NONE("");
 
     private final String type;
 
-    Type(String type) {
+    BowlType(String type) {
         this.type = type;
     }
 
