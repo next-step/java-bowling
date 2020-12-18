3000a679a5f6df96d01ecfa00d6f70a25b18ffb0
devyoon91 <kimbyungyoun91@gmail.com>
2020-12-18 17:48:56 +0900

    1단계 - 질문 삭제하기 기능 리팩토링 (#378)
    * [refactor] : Answer setDeleted 메소드 리팩토링, 테스트 케이스 추가
    
    * [feat] : Answer 일급 컬렉션 추가
    
    * [feat] : DeleteHistory 일급 컬렉션 추가
    
    * [refactor] : Question setDeleted 리팩토링, 테스트 케이스 추가

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,36 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class AnswerTest {
-    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    private Answer actual;
+
+    @BeforeEach
+    void setUp() {
+        this.actual = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+    }
+
+    @Test
+    @DisplayName("Answer delete() 성공 후 DeleteHistory 객체 비교")
+    void answer_deleteSuccess_isEqualTo() throws CannotDeleteException {
+        assertThat(actual.delete(UserTest.JAVAJIGI))
+                .isEqualTo(new DeleteHistory(ContentType.ANSWER, actual.getId(), actual.getWriter(), LocalDateTime.now()));
+    }
+
+    @Test
+    @DisplayName("Answer delete() 다른 사람의 답변일 경우 CannotDeleteException 발생")
+    void answer_deleteFailed_throwException() {
+        assertThatThrownBy(() -> actual.delete(UserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessageContaining("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
 }
