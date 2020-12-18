8303f2723c629fff1269f0e8892185a31c1316b1
seo <asda00400@gmail.com>
2020-12-18 17:52:44 +0900

    step2 (fix) 뷰 수정

--- src/main/java/bowling/view/ResultView.java
+++ src/main/java/bowling/view/ResultView.java
@@ -10,7 +10,7 @@ import bowling.domain.User;
  */
 public class ResultView {
 
-    public static final String DELIMITER = " |  ";
+    public static final String FORMAT_SPACE = "  %-3s |";
 
     public static void print(User user, Frames frames) {
         init(frames, user);
@@ -19,42 +19,54 @@ public class ResultView {
 
     private static void init(Frames frames, User user) {
         header(frames);
-        System.out.printf("|  %-3s |", user.getName());
-        frames.getFrames().stream().forEach(frame -> System.out.printf("  %-3s |", ""));
+        scores(frames, user);
     }
 
     private static void header(Frames frames) {
-        System.out.print("| NAME |");
-        frames.getFrames().stream().forEach(frame -> System.out.printf("  %s  |", frame.getFrameNoString()));
-        System.out.println();
+        printf("| NAME |", "");
+        frames.getFrames().forEach(frame -> printf("  %s  |", frame.getFrameNoString()));
+        space();
+    }
+
+    private static void scores(Frames frames, User user) {
+        printf("|" + FORMAT_SPACE, user.getName());
+        frames.getFrames().forEach(frame -> printf(FORMAT_SPACE, ""));
     }
 
     private static void eachFrame(Frames frames, User user) {
         for (int i = 0; i < frames.size(); i++) {
             space();
-            Frame frame = frames.get(i);
-            frameSum(i, frame);
+            space();
+            sumFrameScore(i, frames.get(i));
             header(frames);
             untilFrame(i, frames, user);
         }
     }
 
-    private static void frameSum(int i, Frame frame) {
-        System.out.println((i + 1) + "프레임 투구 : " + frame.getScore().get());
+    private static void sumFrameScore(int i, Frame frame) {
+        printf((i + 1) + "프레임 투구 : " + frame.getScore().get(), "");
+        space();
     }
 
     private static void untilFrame(int i, Frames frames, User user) {
-        System.out.printf("|  %-3s |", user.getName());
+        printf("|" + FORMAT_SPACE, user.getName());
         for (int j = 0; j < i + 1; j++) {
-            System.out.printf("  %-3s |", frames.get(j).getScore().getSymbol(j));
+            printf(FORMAT_SPACE, frames.get(j).getScore().getSymbol(j));
         }
+        remainFrames(i);
+    }
+
+    private static void remainFrames(int i) {
         for (int j = i + 1; j < 10; j++) {
-            System.out.printf("  %-3s |", "");
+            printf(FORMAT_SPACE, "");
         }
     }
 
+    private static void printf(String format, String args) {
+        System.out.printf(format, args);
+    }
+
     private static void space() {
-        System.out.println();
         System.out.println();
     }
 
