b3bfc1bdce3c80df0e6e8249ce52f5fac42ec71e
changgunyee <minyoung403@naver.com>
2020-12-19 23:39:59 +0900

    feat: add showFrameGuide

---
+++ src/main/java/bowling/view/OutputView.java
@@ -0,0 +1,28 @@
+package bowling.view;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.PrintStream;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class OutputView {
+    public static final String NAME = "NAME";
+    public static final String INFIX = "|";
+    public static final int FRAME_LENGTH = 6;
+    private static final PrintStream out = System.out;
+
+    private static String getNameGuideString() {
+        return StringUtils.wrap(StringUtils.center(NAME, FRAME_LENGTH), INFIX);
+    }
+
+    private static String getFrameGuideString() {
+        return IntStream.range(0, 10)
+            .mapToObj(frame -> StringUtils.center(String.format("%02d", frame + 1), FRAME_LENGTH))
+            .collect(Collectors.joining(INFIX));
+    }
+
+    public static void showFramesGuide() {
+        out.println(getNameGuideString() + getFrameGuideString() + INFIX);
+    }
+}
