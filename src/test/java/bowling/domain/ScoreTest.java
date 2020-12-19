1b58101163769b91df5dfc190389aaa3f074dc10
kit(김광수) <kit.t@kakaocorp.com>
2020-12-19 23:31:02 +0900

    feat: 인스턴스 변수 두개 이하로

--- src/test/java/bowling/domain/ScoreTest.java
+++ src/test/java/bowling/domain/ScoreTest.java
@@ -1,7 +1,13 @@
 package bowling.domain;
 
+import bowling.util.Lists;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ScoreTest {
@@ -25,41 +31,51 @@ public class ScoreTest {
     }
 
     private static class Score {
-        private final Integer first;
-        private final Integer second;
-        private final Integer third;
+        private final List<Integer> values;
 
-        public Score(Integer first, Integer second, Integer third) {
-            this.first = first;
-            this.second = second;
-            this.third = third;
+        public Score(Integer... fallingPins) {
+            values = Arrays.stream(fallingPins)
+                    .filter(Objects::nonNull)
+                    .collect(toList());
         }
 
         public Integer toInt() {
-            if (first == null) {
+            if (first() == null) {
                 return null;
             }
 
-            if (first == 10) {
+            if (first() == 10) {
                 return sumAll();
             }
 
-            if (second == null) {
-                return first;
+            if (second() == null) {
+                return first();
             }
 
-            if (first + second == 10) {
+            if (first() + second() == 10) {
                 return sumAll();
             }
-            return first + second;
+            return first() + second();
 
         }
 
+        private Integer first() {
+            return Lists.getOrNull(values, 0);
+        }
+
+        private Integer second() {
+            return Lists.getOrNull(values, 1);
+        }
+
+        private Integer third() {
+            return Lists.getOrNull(values, 2);
+        }
+
         private Integer sumAll() {
-            if (third == null) {
+            if (third() == null) {
                 return null;
             }
-            return first + second + third;
+            return values.stream().mapToInt(value -> value).sum();
         }
     }
 }
