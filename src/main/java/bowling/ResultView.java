53daed94167eaaaf06b75fe40e4d978e0c17ff52
kit(김광수) <kit.t@kakaocorp.com>
2020-12-19 14:59:53 +0900

    feat: ResultView 스케치

--- src/main/java/bowling/ResultView.java
+++ src/main/java/bowling/ResultView.java
@@ -1,7 +1,19 @@
 package bowling;
 
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.joining;
+
 public class ResultView {
     public static void printResult(GameStatus status) {
+        System.out.println(getFrameTitle());
+        System.out.println(status.toString());
+    }
 
+    private static String getFrameTitle() {
+        String frameTitle = IntStream.rangeClosed(0, 10)
+                .mapToObj(i -> String.format("  %d  ", i))
+                .collect(joining("|"));
+        return "| NAME |" + frameTitle;
     }
 }
