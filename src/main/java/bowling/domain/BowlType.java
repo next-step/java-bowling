2bccac2ed0c29f7107bb9f2f7d0a69324f979f1d
oen142 <oen142@naver.com>
2020-12-19 23:47:13 +0900

    feat : 볼링 타입을 가진다.

--- src/main/java/bowling/domain/BowlType.java
+++ src/main/java/bowling/domain/BowlType.java
@@ -5,8 +5,8 @@ public enum BowlType {
     STRIKE(2, "X"),
     SPARED(1, "/"),
     MISS(0, ""),
-    READY(0, ""),
-    NORMAL(0, "");
+    NONE(0, ""),
+    END(0, "");
 
     private int bonusCount;
     private String mark;
