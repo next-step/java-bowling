be9c44c8ab4c0f097f38afac704c5099e1a06025
sjh <sjh@revhat.com>
2020-12-19 17:22:12 +0900

    test code 에러 수정

--- src/main/java/qna/domain/DeleteHistories.java
+++ src/main/java/qna/domain/DeleteHistories.java
@@ -2,6 +2,7 @@ package qna.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class DeleteHistories {
     private long questionId;
@@ -17,5 +18,19 @@ public class DeleteHistories {
 
     public List<DeleteHistory> getDeleteHistories() {
         return deleteHistories;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DeleteHistories that = (DeleteHistories) o;
+        return questionId == that.questionId &&
+                Objects.equals(deleteHistories, that.deleteHistories);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(questionId, deleteHistories);
     }
 }
