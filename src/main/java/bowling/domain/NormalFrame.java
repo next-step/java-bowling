c24b8ede00ff3e885ea9f509480d298844146392
Sunghyeob.Baek <baek9652@naver.com>
2020-12-20 05:14:03 +0900

    [feat] NormalFrame 에 Spare, Strike 조건 추가

--- src/main/java/bowling/domain/NormalFrame.java
+++ src/main/java/bowling/domain/NormalFrame.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class NormalFrame {
 
     private static final int FIRST_TURN_INDEX = 0;
+    private static final int SECOND_TURN_INDEX = 1;
     private static final int MAXIMUM_TRIES_PER_NORMAL_FRAME = 2;
 
     private final List<DownedPin> tries;
@@ -25,5 +26,14 @@ public class NormalFrame {
 
     public boolean isEnd() {
         return (tries.get(FIRST_TURN_INDEX).isStrike() || tries.size() == MAXIMUM_TRIES_PER_NORMAL_FRAME);
+    }
+
+    public boolean isStrike() {
+        return tries.size() == 1 && tries.get(0).isStrike();
+    }
+
+    public boolean isSpare() {
+        return tries.size() == MAXIMUM_TRIES_PER_NORMAL_FRAME
+                && tries.get(FIRST_TURN_INDEX).isSpare(tries.get(SECOND_TURN_INDEX));
     }
 }
--- src/test/java/bowling/domain/NormalFrameTest.java
+++ src/test/java/bowling/domain/NormalFrameTest.java
@@ -22,12 +22,47 @@ public class NormalFrameTest {
 
     @Test
     @DisplayName("첫 투구에 10개가 입력되었을 때 프레임 종료조건 테스트")
-    void testStrike() {
+    void endedWhenStrike() {
         NormalFrame frame = new NormalFrame();
         DownedPin firstTry = DownedPin.fromNumber(10);
 
         frame.record(firstTry);
 
         assertThat(frame.isEnd()).isTrue();
+    }
+
+    @Test
+    @DisplayName("스트라이크 컨디션 테스트(true)")
+    void isStrike() {
+        NormalFrame frame = new NormalFrame();
+        DownedPin firstTry = DownedPin.fromNumber(10);
+
+        frame.record(firstTry);
+
+        assertThat(firstTry.isStrike()).isTrue();
+    }
+
+    @Test
+    @DisplayName("스트라이크 컨디션 테스트(false)")
+    void isNotStrike() {
+        NormalFrame frame = new NormalFrame();
+        DownedPin firstTry = DownedPin.fromNumber(7);
+
+        frame.record(firstTry);
+
+        assertThat(firstTry.isStrike()).isFalse();
+    }
+
+    @Test
+    @DisplayName("스페어 컨디션 테스트(true)")
+    void isSpare() {
+        NormalFrame frame = new NormalFrame();
+        DownedPin firstTry = DownedPin.fromNumber(3);
+        DownedPin secondTry = firstTry.fromSubordinateTry(DownedPin.fromNumber(7));
+
+        frame.record(firstTry);
+        frame.record(secondTry);
+
+        assertThat(frame.isSpare()).isTrue();
     }
 }
