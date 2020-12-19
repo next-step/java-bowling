0e877e4409985ced147e8694060a52335f1a8968
kyeongmin <kyeongmin.jake.lee@gmail.com>
2020-12-19 18:56:43 +0900

    feat - 플레이어 이름이 3글자가 아닌 경우 예외 처리

--- src/main/java/bowling/domain/Player.java
+++ src/main/java/bowling/domain/Player.java
@@ -2,13 +2,35 @@ package bowling.domain;
 
 public class Player {
 
+    public static final int NAME_LENGTH = 3;
+    public static final String LENGTH_LIMIT = "이름은 3글자여야 합니다.";
+    public static final String NAME_EMPTY = "이름을 입력해주세요.";
+
     private final String name;
 
     public Player(String name) {
+        validateName(name);
         this.name = name;
     }
 
     public static Player from(String name) {
         return new Player(name);
+    }
+
+    private void validateName(String name) {
+        validateLength(name);
+        validateEmpty(name);
+    }
+
+    private void validateLength(String name) {
+        if(name.length() != NAME_LENGTH) {
+            throw new IllegalArgumentException(LENGTH_LIMIT);
+        }
+    }
+
+    private void validateEmpty(String name) {
+        if(name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException(NAME_EMPTY);
+        }
     }
 }
--- src/test/java/bowling/domain/PlayerTest.java
+++ src/test/java/bowling/domain/PlayerTest.java
@@ -1,5 +1,6 @@
 package bowling.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +13,22 @@ class PlayerTest {
     void createPlayer() {
         String name = "LKM";
         assertThat(Player.from(name)).isInstanceOf(Player.class);
+    }
+
+    @Test
+    @DisplayName("이름이 3글자가 아닐 경우 예외 처리")
+    void exceptNameLengthThree() {
+        String name = "abcd";
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> Player.from(name));
+    }
+
+    @Test
+    @DisplayName("이름이 비어있는 경우 예외 처리")
+    void exceptNameLengthEmpty() {
+        String name = "";
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> Player.from(name));
     }
 
 }
