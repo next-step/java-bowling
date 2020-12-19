5b0f60e1833050e50bf08f0eae08c2eccb812a61
changgunyee <minyoung403@naver.com>
2020-12-19 23:39:31 +0900

    feat: add Player

---
+++ src/main/java/bowling/domain/Player.java
@@ -0,0 +1,15 @@
+package bowling.domain;
+
+public class Player {
+
+    public static final int NAME_LENGTH = 3;
+    private final String name;
+
+    public Player(String name) {
+        if (name.length() != NAME_LENGTH) {
+            throw new IllegalArgumentException("Name size must be 3");
+        }
+
+        this.name = name;
+    }
+}
