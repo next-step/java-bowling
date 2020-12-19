78348a6183acf9404cb8738953b5103f8826b51e
oen142 <oen142@naver.com>
2020-12-19 13:32:28 +0900

    feat : 인풋 뷰 다시 정의

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -1,19 +1,24 @@
 package bowling.view;
 
+import bowling.domain.Player;
+import bowling.domain.point.Point;
+
 import java.util.Scanner;
 
 public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
+    public static final String PLAYER_NAME = "플레이어 이름은(3 english letters)?:";
+    public static final String FRAME_PITCH = "프레임 투구 : ";
 
-    public static String inputPlayerName() {
-        System.out.println("플레이어 이름은(3 english letters)?:");
-        return scanner.next();
+    public static Player inputPlayerName() {
+        System.out.println(PLAYER_NAME);
+        return Player.of(scanner.nextLine());
     }
 
-    public static int inputPitchBowl(int stage) {
-        System.out.print(stage + "프레임 투구 : ");
-        return scanner.nextInt();
+    public static Point inputPitchBowl(int stage) {
+        System.out.print((stage + 1) + FRAME_PITCH);
+        return Point.valueOfString(scanner.nextLine());
     }
 
 
