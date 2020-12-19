f26868d1bd3c7188da0d92b97ecfda19b2fb448d
joshua(박근희) <joshua.park321@kakaocorp.com>
2020-12-20 01:16:02 +0900

    feat: 단위 테스트 먼저 구현
    AnswerTest 및 QuestionTest 에 deleteBy를 구현한다 치고 테스트 작성
    DeleteHistory 는 QnaServiceTest 에 검증이 구현되어있어서 따로 작성 안함

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,35 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+
+    @Test
+    public void deleted_by_writer() {
+        A1.deleteBy(UserTest.JAVAJIGI);
+
+        assertThat(A1.isDeleted()).isTrue();
+    }
+
+    @Test
+    public void delete_by_other_Exception() {
+        assertThatThrownBy(A1.deleteBy(UserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    public void delete_cascaded_from_question() {
+        QuestionTest.Q1.addAnswer(A1);
+        QuestionTest.Q1.deleteBy(UserTest.JAVAJIGI);
+
+        assertThat(A1.isDeleted()).isTrue();
+    }
+
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,41 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    public void delete_by_writer() {
+        Q1.deleteBy(UserTest.JAVAJIGI);
+
+        assertThat(Q1.isDeleted()).isTrue();
+    }
+
+    @Test
+    public void delete_by_other_Exception() {
+        assertThatThrownBy(Q1.deleteBy(UserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    public void delete_with_answers_by_writer() {
+        Q1.addAnswer(AnswerTest.A1);
+
+        Q1.deleteBy(UserTest.JAVAJIGI);
+
+        assertThat(Q1.isDeleted()).isTrue();
+    }
+
+    @Test
+    public void delete_with_answers_by_other_Exception() {
+        Q1.addAnswer(AnswerTest.A2);
+        assertThatThrownBy(Q1.deleteBy(UserTest.JAVAJIGI))
+                .isInstanceOf(CannotDeleteException.class);
+    }
 }
