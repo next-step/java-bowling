88cabc2c9fd148c40c7b724181bcf24bb16b1f10
oen142 <oen142@naver.com>
2020-12-20 00:03:33 +0900

    test : 포인트는 범위를 넘을수가 없다.

--- src/test/java/bowling/domain/point/PointTest.java
+++ src/test/java/bowling/domain/point/PointTest.java
@@ -1,7 +1,26 @@
 package bowling.domain.point;
 
+import bowling.exception.ScorePointRangeOutBoundException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PointTest {
+
+    @Test
+    @DisplayName("포인트는 마이너스를 가질수는 없다.")
+    void throwNegativeException() {
+        assertThatThrownBy(() -> Point.valueOf(-1))
+                .isInstanceOf(ScorePointRangeOutBoundException.class);
+    }
+
+    @Test
+    @DisplayName("포인트는 10을 넘을수는 없다.")
+    void throwOverException() {
+        assertThatThrownBy(() -> Point.valueOf(11))
+                .isInstanceOf(ScorePointRangeOutBoundException.class);
+    }
 
 }
