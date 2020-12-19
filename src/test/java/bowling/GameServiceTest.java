1ab99912499974b99b7fc1dd947199880a721e19
kit(김광수) <kit.t@kakaocorp.com>
2020-12-19 15:20:04 +0900

    test: isFinish

--- src/test/java/bowling/GameServiceTest.java
+++ src/test/java/bowling/GameServiceTest.java
@@ -3,10 +3,24 @@ package bowling;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class GameServiceTest {
     @Test
     void illegalPlayerName() {
         Assertions.assertThatThrownBy(() -> new GameService("ABCD"))
                 .isInstanceOf(IllegalPlayerNameException.class);
+    }
+
+    @Test
+    void isFinish() {
+        GameService gameService = new GameService("ABC");
+        gameService.start();
+        for (int i = 1; i <= 12; i++) {
+            assertThat(gameService.isFinish()).isFalse();
+            gameService.throwBall(10);
+        }
+
+        assertThat(gameService.isFinish()).isTrue();
     }
 }
