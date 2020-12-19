64f89df2736c882d69649a49929aa523883e2457
kit(김광수) <kit.t@kakaocorp.com>
2020-12-19 11:12:18 +0900

    feat: make it pass

--- src/test/java/bowling/LastFrameTest.java
+++ src/test/java/bowling/LastFrameTest.java
@@ -58,7 +58,7 @@ public class LastFrameTest {
 
     @DisplayName("마지막 프레임의 미스, 스페어, 거터")
     @ParameterizedTest
-    @CsvSource({"8,1,MISS", "8,2,SPARE", "0.0,GUTTER"})
+    @CsvSource({"8,1,MISS", "8,2,SPARE", "0,0,GUTTER"})
     void lastFrameMiss(int firstThrow, int secondThrow, String scoringName) {
         Frame lastFrame = new NormalFrame(9).throwBall(10);
         assertThat(lastFrame.throwBall(firstThrow).throwBall(secondThrow).getScoring()).isEqualTo(Scoring.valueOf(scoringName).asOptional());
@@ -90,6 +90,10 @@ public class LastFrameTest {
 
             if (sumOfFallingPins() == MAX_PINS) {
                 return SPARE.asOptional();
+            }
+
+            if (sumOfFallingPins() == MIN_PINS) {
+                return GUTTER.asOptional();
             }
 
             return Optional.empty();
