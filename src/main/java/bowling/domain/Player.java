5d3d57f0c46843ba498371cd1117226f95ea00b7
oen142 <oen142@naver.com>
2020-12-19 13:40:57 +0900

    feat : 게임 플레이어 정의

--- src/main/java/bowling/domain/Player.java
+++ src/main/java/bowling/domain/Player.java
@@ -23,6 +23,10 @@ public class Player {
         }
     }
 
+    public String getName() {
+        return name;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
