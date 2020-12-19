b489ae77f1e17b977c91065e660f31770ef56bd7
oen142 <oen142@naver.com>
2020-12-19 22:27:03 +0900

    feat : 컨트롤러 다시 작성

--- src/main/java/bowling/controller/BowlingController.java
+++ src/main/java/bowling/controller/BowlingController.java
@@ -1,14 +1,11 @@
 package bowling.controller;
 
 import bowling.domain.Player;
-import bowling.domain.frame.FrameResultDto;
 import bowling.domain.frame.Frames;
 import bowling.domain.game.Bowling;
 import bowling.domain.point.Point;
 import bowling.view.InputView;
 import bowling.view.OutputView;
-
-import java.util.List;
 
 public class BowlingController {
 
@@ -22,14 +19,6 @@ public class BowlingController {
             Point pointPitch = InputView.inputPitchBowl(frameNumber);
 
             bowling.pitch(pointPitch);
-            List<FrameResultDto> result = bowling.getResult();
-            result.forEach(frameResultDto -> {
-                List<Point> points = frameResultDto.getPoints();
-                points.forEach(point ->
-                        System.out.println("포인트 : " + point.getPoint()));
-                System.out.println("스코어 : " + frameResultDto.getScoreDto().getClass().getName());
-                System.out.println("스코어타입 : " + frameResultDto.getScoreType());
-            });
             OutputView.printResult(bowling.getPlayerName(), bowling.getResult());
         }
     }
