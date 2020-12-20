499d41faf65f7799c9a8e3e0d729f2fea7fd82e9
dbeod2 <darung.0211@gmail.com>
2020-12-20 23:54:58 +0900

    feat : 볼링 실행 클래스 생성

---
+++ src/main/java/bowling/BowlingApplication.java
@@ -0,0 +1,9 @@
+package bowling;
+
+import bowling.controller.BowlingGameController;
+
+public class BowlingApplication {
+    public static void main(String[] args) {
+        BowlingGameController.run();
+    }
+}
