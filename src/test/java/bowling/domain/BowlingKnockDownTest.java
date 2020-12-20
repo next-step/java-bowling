6919c4502d85472dd15fe3761060fa12050eb2e0
dbeod2 <darung.0211@gmail.com>
2020-12-20 23:52:11 +0900

    test : 팩토리 메서드 사용으로 인한 테스트 코드 수정

--- src/test/java/bowling/domain/BowlingKnockDownTest.java
+++ src/test/java/bowling/domain/BowlingKnockDownTest.java
@@ -13,7 +13,7 @@ public class BowlingKnockDownTest {
     @DisplayName("쓰러뜨린 볼링핀 수 확인")
     @ValueSource(ints = {10, 9, 7, 0})
     public void 볼링핀_쓰러뜨린수(int countOfKnockDown) {
-        BowlingKnockDown bowlingKnockDown = new BowlingKnockDown(countOfKnockDown);
+        BowlingKnockDown bowlingKnockDown = BowlingKnockDown.of(countOfKnockDown);
         assertEquals(bowlingKnockDown.getCount(), countOfKnockDown);
     }
 
@@ -21,7 +21,7 @@ public class BowlingKnockDownTest {
     @DisplayName("쓰러뜨린 볼링 투구 값 10이상인 경우 예외")
     @ValueSource(ints = {11, 15, 21})
     public void 볼링핀_경계값_테스트(int countOfKnockDown) {
-        assertThatThrownBy(() ->  new BowlingKnockDown(countOfKnockDown))
+        assertThatThrownBy(() ->  BowlingKnockDown.of(countOfKnockDown))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("볼링 핀은 최대 10개입니다.");
     }
