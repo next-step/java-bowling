facbb83fbadb7f2e81665c0ce35e5010eea5234c
dbeod2 <darung.0211@gmail.com>
2020-12-21 14:56:08 +0900

    refactor :  볼링 핀 최대 수 상수명 수정으로 인한 관련 코드 수정

--- src/main/java/bowling/exception/BowlingMaxCountException.java
+++ src/main/java/bowling/exception/BowlingMaxCountException.java
@@ -1,9 +1,9 @@
 package bowling.exception;
 
-import static bowling.domain.BowlingKnockDown.MAX_COUNT;
+import static bowling.domain.BowlingKnockDown.MAX_NUMBER;
 
 public class BowlingMaxCountException extends IllegalArgumentException {
     public BowlingMaxCountException() {
-        super(String.format("볼링 핀은 최대 %d개입니다.", MAX_COUNT));
+        super(String.format("볼링 핀은 최대 %d개입니다.", MAX_NUMBER));
     }
 }
