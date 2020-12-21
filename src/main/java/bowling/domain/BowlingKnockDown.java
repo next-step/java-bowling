e9a6efc59aa381df0e69ccc720a18af816ccb463
dbeod2 <darung.0211@gmail.com>
2020-12-21 17:10:34 +0900

    refactor : 미사용 클래스삭제 및 bowlingKnockDown클래스에 입력받은 투구에 대한 처리

--- src/main/java/bowling/domain/BowlingKnockDown.java
+++ src/main/java/bowling/domain/BowlingKnockDown.java
@@ -2,42 +2,60 @@ package bowling.domain;
 
 import bowling.exception.BowlingMaxCountException;
 
-import java.util.Objects;
-
 public class BowlingKnockDown {
-    public static final int MAX_COUNT = 10;
-    private final int count;
+    private static final int MIN_NUMBER = 0;
+    public static final int MAX_NUMBER = 10;
+    private static final String STRIKE = "X";
+    private static final String SPARE = "/";
+    private static final String GUTTER = "-";
+    private static final String LINE = " | ";
+    private String knockDownExpression;
 
-    private BowlingKnockDown(int count) {
-        this.count = count;
-    }
-
-    public static BowlingKnockDown of(int count) {
+    public BowlingKnockDown(int count) {
         validate(count);
-        return new BowlingKnockDown(count);
+        this.knockDownExpression = knockDownExpress(count);
     }
 
-    public int getCount() {
-        return count;
+    public BowlingKnockDown(String currentBowlingKnockDown, int count) {
+        validate(count);
+        this.knockDownExpression = knockDownExpress(currentBowlingKnockDown, count);
     }
 
-    private static void validate(int count){
-        if (count > MAX_COUNT) {
+    public String knockDownExpress(int count) {
+        if (count == MIN_NUMBER) {
+            return String.format(" %s ", GUTTER);
+        }
+
+        if (count == MAX_NUMBER) {
+            return String.format("   %s  ", STRIKE);
+        }
+
+        return String.format("  %d   ", count);
+    }
+
+    public String knockDownExpress(String currentBowlingKnockDown, int count) {
+        if (currentBowlingKnockDown.equals(GUTTER)) {
+            currentBowlingKnockDown = "0";
+        }
+
+        if ((Integer.parseInt(currentBowlingKnockDown) + count) == MAX_NUMBER) {
+            return String.format( "%s%s%s ", currentBowlingKnockDown, LINE, SPARE);
+        }
+
+        if (count == MIN_NUMBER) {
+            return String.format(" %s%s%s ", currentBowlingKnockDown.equals("0") ? GUTTER : currentBowlingKnockDown, LINE, GUTTER);
+        }
+
+        return String.format(" %s%s%d ", currentBowlingKnockDown, LINE, count);
+    }
+
+    public String getKnockDownExpression() {
+        return knockDownExpression;
+    }
+
+    private static void validate(int count) {
+        if (count > MAX_NUMBER) {
             throw new BowlingMaxCountException();
         }
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        BowlingKnockDown that = (BowlingKnockDown) o;
-        return count == that.count;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(count);
-    }
-
 }
