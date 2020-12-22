f46d14a31db55fe20c1f55aa12b34113234eec81
kriery <jongchan.lee@lmep.kr>
2020-12-23 00:18:53 +0900

    refactor: `Question` 삭제를 `Question.canDeleteBy` 및 `Question.delete`로 위임

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -1,8 +1,10 @@
 package qna.domain;
 
 import org.hibernate.annotations.Where;
+import qna.CannotDeleteException;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -75,17 +77,30 @@ public class Question extends AbstractEntity {
         return writer.equals(loginUser);
     }
 
-    public Question setDeleted(boolean deleted) {
-        this.deleted = deleted;
-        return this;
+    public void canDeleteBy(User loginUser) throws CannotDeleteException {
+        if (!isOwner(loginUser)) {
+            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+        }
+        for (Answer answer : answers) {
+            if (!answer.isOwner(loginUser)) {
+                throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+            }
+        }
+    }
+
+    public List<DeleteHistory> delete() {
+        List<DeleteHistory> deleteHistories = new ArrayList<>();
+        deleted = true;
+        deleteHistories.add(new DeleteHistory(ContentType.QUESTION, getId(), getWriter(), LocalDateTime.now()));
+        for (Answer answer : answers) {
+            answer.setDeleted(true);
+            deleteHistories.add(new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now()));
+        }
+        return deleteHistories;
     }
 
     public boolean isDeleted() {
         return deleted;
-    }
-
-    public List<Answer> getAnswers() {
-        return answers;
     }
 
     @Override
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,63 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    private Question question;
+
+    @BeforeEach
+    void initQuestion() {
+        question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+    }
+
+    @Test
+    public void delete_성공() {
+        assertThatCode(() -> {
+            question.canDeleteBy(UserTest.JAVAJIGI);
+            question.delete();
+        }).doesNotThrowAnyException();
+
+        assertThat(question.isDeleted()).isEqualTo(true);
+    }
+
+    @Test
+    public void delete_다른_사람이_쓴_글() {
+        assertThatExceptionOfType(CannotDeleteException.class).isThrownBy(() -> {
+            question.canDeleteBy(UserTest.SANJIGI);
+            question.delete();
+        }).withMessage("질문을 삭제할 권한이 없습니다.");
+
+        assertThat(question.isDeleted()).isEqualTo(false);
+    }
+
+    @Test
+    public void delete_성공_질문자_답변자_같음() {
+        question.addAnswer(new Answer(UserTest.JAVAJIGI, question, "answer1"));
+
+        assertThatCode(() -> {
+            question.canDeleteBy(UserTest.JAVAJIGI);
+            question.delete();
+        }).doesNotThrowAnyException();
+
+        assertThat(question.isDeleted()).isEqualTo(true);
+    }
+
+    @Test
+    public void delete_답변_중_다른_사람이_쓴_글() {
+        question.addAnswer(new Answer(UserTest.SANJIGI, question, "answer1"));
+
+        assertThatExceptionOfType(CannotDeleteException.class).isThrownBy(() -> {
+            question.canDeleteBy(UserTest.JAVAJIGI);
+            question.delete();
+        }).withMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+
+        assertThat(question.isDeleted()).isEqualTo(false);
+    }
 }
