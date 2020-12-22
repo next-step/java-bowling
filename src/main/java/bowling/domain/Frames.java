aa71d13d3723aabe98754b4f62608fe0f7ae8985
dbeod2 <darung.0211@gmail.com>
2020-12-22 23:26:27 +0900

    refactor : bowlingKnockdown 제거

--- src/main/java/bowling/domain/Frames.java
+++ src/main/java/bowling/domain/Frames.java
@@ -5,37 +5,21 @@ import java.util.Map;
 
 public class Frames {
 
-    private final Map<Integer, BowlingKnockDown> bowlingKnockDownMap = new LinkedHashMap<>();
+    private Map<Integer, BowlingKnockDown> bowlingKnockDownMap = new LinkedHashMap<>();
 
     public Frames() {
 
-    }
-
-    public BowlingKnockDown initFirst(int number, int countOfKnockDown) {
-        BowlingKnockDown bowlingKnockDown = new BowlingKnockDown(countOfKnockDown);
-        bowlingKnockDownMap.put(number, bowlingKnockDown);
-        return bowlingKnockDown;
-    }
-
-    public BowlingKnockDown initNext(int number, BowlingKnockDown bowlingKnockDown, int nextCountOfBowlingKnockDown) {
-        String currentKnockDownExpression = bowlingKnockDown.getKnockDownExpression().trim();
-        int countOfBowlingKnockDown = bowlingKnockDown.getCountOfBowlingKnockDown();
-
-        bowlingKnockDown =
-                new BowlingKnockDown(currentKnockDownExpression, countOfBowlingKnockDown, nextCountOfBowlingKnockDown);
-        bowlingKnockDownMap.put(number, bowlingKnockDown);
-        return bowlingKnockDown;
     }
 
     public Map<Integer, BowlingKnockDown> getBowlingKnockDownMap() {
         return bowlingKnockDownMap;
     }
 
-    public String bowlingKnockDownExpression(int key) {
-        return this.bowlingKnockDownMap.get(key).getKnockDownExpression();
-    }
-
     public int size() {
         return this.bowlingKnockDownMap.size();
+    }
+
+    public void put(int number, BowlingKnockDown bowlingKnockDown) {
+        this.bowlingKnockDownMap.put(number, bowlingKnockDown);
     }
 }
