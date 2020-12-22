11b08ff4c9d4604facc14f805b8fbd6cd1597e29
SungHyeob Baek <baek9652@naver.com>
2020-12-23 08:25:58 +0900

    [feat] SPARE 및 MISS의 상태 추가

--- src/main/java/bowling/domain/FrameStatus.java
+++ src/main/java/bowling/domain/FrameStatus.java
@@ -2,13 +2,16 @@ package bowling.domain;
 
 public enum FrameStatus {
     STRIKE,
-    GUTTER,
     SPARE,
     MISS;
 
     public static FrameStatus getStatus(DownedPin previousPitch, DownedPin currentPitch) {
         if (previousPitch.isStrike()) {
             return STRIKE;
+        }
+
+        if (previousPitch.isSpare(currentPitch)) {
+            return SPARE;
         }
 
         return MISS;
--- src/test/java/bowling/domain/FrameStatusTest.java
+++ src/test/java/bowling/domain/FrameStatusTest.java
@@ -2,6 +2,10 @@ package bowling.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,5 +16,27 @@ public class FrameStatusTest {
     void testIsStrike() {
         assertThat(FrameStatus.getStatus(DownedPin.fromNumber(10), null))
                 .isEqualTo(FrameStatus.STRIKE);
+    }
+
+    private static Stream<Arguments> provideMissStatus() {
+        return Stream.of(
+                Arguments.of(DownedPin.fromNumber(4), DownedPin.fromNumber(3)),
+                Arguments.of(DownedPin.fromNumber(0), DownedPin.fromNumber(6)),
+                Arguments.of(DownedPin.fromNumber(5), null)
+        );
+    }
+
+    @Test
+    @DisplayName("Spare 상태 확인")
+    void testIsSpare() {
+        assertThat(FrameStatus.getStatus(DownedPin.fromNumber(4), DownedPin.fromNumber(6)))
+                .isEqualTo(FrameStatus.SPARE);
+    }
+
+    @MethodSource("provideMissStatus")
+    @DisplayName("Miss 상태")
+    void testMiss(DownedPin previousTurn, DownedPin currentTurn) {
+        assertThat(FrameStatus.getStatus(previousTurn, currentTurn))
+                .isEqualTo(FrameStatus.MISS);
     }
 }
