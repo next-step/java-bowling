c39b5cb9dffb5d083ae4c80772fc9519327c67d9
nio.d <nio.d@kakaocorp.com>
2020-12-24 10:11:04 +0900

    refactor: DeleteHistories.getHistories() return 을 불변객체로 지정

--- src/main/java/qna/domain/DeleteHistories.java
+++ src/main/java/qna/domain/DeleteHistories.java
@@ -1,17 +1,18 @@
 package qna.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class DeleteHistories {
 
-    private List<DeleteHistory> histories = new ArrayList<>();
+    private final List<DeleteHistory> histories = new ArrayList<>();
 
     public void add(DeleteHistory deleteHistory){
         histories.add(deleteHistory);
     }
 
     public List<DeleteHistory> getHistories() {
-        return histories;
+        return Collections.unmodifiableList(histories);
     }
 }
