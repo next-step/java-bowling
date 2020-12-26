a6a4c44783acbb6c5f6de2a3b00aca90ba5796ca
dbeod2 <darung.0211@gmail.com>
2020-12-26 21:59:12 +0900

    refactor : frame에서 공통으로 사용하는 상수 추상클래스로 이동

--- src/main/java/bowling_step3/domain/Frame/Frame.java
+++ src/main/java/bowling_step3/domain/Frame/Frame.java
@@ -4,6 +4,12 @@ import bowling_step3.domain.Score;
 
 public abstract class Frame {
 
+    protected static final int DEFAULT = 0;
+
+    protected static final int MIN_PITCH_COUNT = 2;
+
+    protected static final int MAX_PITCH_COUNT = 3;
+
     protected Score score;
 
     abstract void pitch(int countOfKnockDown);
