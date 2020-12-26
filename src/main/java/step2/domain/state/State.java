14ea1e33f019024f4ff10735bf1a27c58a9ab52b
kyeongmin <kyeongmin.jake.lee@gmail.com>
2020-12-26 22:15:13 +0900

    feat - State 인터페이스 정의

---
+++ src/main/java/step2/domain/state/State.java
@@ -0,0 +1,17 @@
+package step2.domain.state;
+
+import step2.domain.Score;
+
+public interface State {
+
+    State bowl(int fallingPins);
+
+    boolean isFinish();
+
+    Score getScore();
+
+    Score calculateAdditionalScore(Score score);
+
+    String toString();
+
+}
