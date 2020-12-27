490aeee0a98adbca01e7a3fb9773dcb469ec862a
dbeod2 <darung.0211@gmail.com>
2020-12-27 22:32:33 +0900

    test : finalFrame 테스트코드 수정

--- src/test/java/bowling_step3/FinalFrameTest.java
+++ src/test/java/bowling_step3/FinalFrameTest.java
@@ -13,26 +13,25 @@ public class FinalFrameTest {
 
     @ParameterizedTest
     @DisplayName("마지막 프레임 투구 & 점수")
-    @CsvSource(value = {"4,4, 4|4, 8", "1,2, 1|2, 3", "4,2, 4|2, 6"})
-    public void 마지막_프레임_투구(int firstOfKnockDown, int secondOfKnockDown, String expression, int score) {
+    @CsvSource(value = {"4,4, 8", "1,2, 3", "4,2, 6"})
+    public void 마지막_프레임_투구(int firstOfKnockDown, int secondOfKnockDown, int score) {
         FinalFrame frame = new FinalFrame();
         frame.pitch(firstOfKnockDown);
         frame.pitch(secondOfKnockDown);
 
-        assertThat(frame.getKnockDownExpression()).isEqualTo(expression);
         assertThat(frame.getScore()).isEqualTo(score);
     }
 
     @ParameterizedTest
-    @DisplayName("마지막 프레임 스트라이크_스페어인 경우 3번 투구")
-    @CsvSource(value = {"10,10,10, X|X|X, 30", "10,1,2, X|1|2", "9,1,10, 9|/|X"})
-    public void 마지막_프레임_스트라이크_스페어인경우_3번투구(int firstOfKnockDown, int secondOfKnockDown, int thirdOfKnockDown, String expression) {
+    @DisplayName("마지막 프레임 스트라이크_스페어인 경우 3번 투구에 대한 점수")
+    @CsvSource(value = {"10,10,10, 30", "10,1,2, 13", "9,1,10, 20"})
+    public void 마지막_프레임_스트라이크_스페어인경우_3번투구(int firstOfKnockDown, int secondOfKnockDown, int thirdOfKnockDown, int score) {
         FinalFrame frame = new FinalFrame();
         frame.pitch(firstOfKnockDown);
         frame.pitch(secondOfKnockDown);
         frame.pitch(thirdOfKnockDown);
 
-        assertThat(frame.getKnockDownExpression()).isEqualTo(expression);
+        assertThat(frame.getScore()).isEqualTo(score);
     }
 
     @Test
