30b18e127e5d29ad7173cc4ab7fe1263f4b48c50
kyeongmin <kyeongmin.jake.lee@gmail.com>
2020-12-28 21:26:29 +0900

    test - 투구 점수 입력 테스트 코드 수정 및 0점, 10점 테스트 추가

--- src/test/java/step2/domain/PitchTest.java
+++ src/test/java/step2/domain/PitchTest.java
@@ -2,16 +2,33 @@ package step2.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PitchTest {
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(
+            value = {"1:1", "2:2", "3:3", "4:4", "5:5", "6:6", "7:7", "8:8", "9:9"},
+            delimiter = ':')
     @DisplayName("투구 점수 입력")
-    void createPitch() {
-        assertThat(Pitch.from(10).toString()).isEqualTo("10");
+    void createPitch(int score, String value) {
+        assertThat(Pitch.from(score).toString()).isEqualTo(value);
+    }
+
+    @Test
+    @DisplayName("0점 입력")
+    void zeroPitch() {
+        assertThat(Pitch.from(0).toString()).isEqualTo("-");
+    }
+
+    @Test
+    @DisplayName("10점 입력")
+    void tenPitch() {
+        assertThat(Pitch.from(10).toString()).isEqualTo("X");
     }
 
     @Test
