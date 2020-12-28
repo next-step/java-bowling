3875d138010d4f9b43add798224bd1d17fad5365
Sunghyeob.Baek <baek9652@naver.com>
2020-12-29 00:47:12 +0900

    최소 구현을 제외한 나머지 부분 삭제

--- src/main/java/bowling/bowlingexception/InvalidProgressException.java
+++
@@ -1,7 +0,0 @@
-package bowling.bowlingexception;
-
-public class InvalidProgressException extends IllegalArgumentException {
-    public InvalidProgressException() {
-        super("허용되지 않은 프레임 진행상태 입니다.");
-    }
-}
