2c69e5b9c83ac1a6ce2e03b1c41496468fe4fc05
Kaeum <oror1024@gmail.com>
2021-01-02 23:45:37 +0900

    feat: N명이 플레이 가능한 구조로 어플리케이션 변경

--- src/main/java/bowling/ui/InputView.java
+++ src/main/java/bowling/ui/InputView.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String INSERT_PLAYER_NAME = "플레이어 이름은(3 english letters)?: ";
-    private static final String NTH_FRAME_TRY_IS = "%d프레임 투구 : ";
+    private static final String WHOSE_TURN = "%s's turn : ";
+    private static final String HOW_MANY_PEOPLE = "how many people? ";
 
     private InputView() {}
 
@@ -14,8 +15,13 @@ public class InputView {
         return SCANNER.nextLine();
     }
 
-    public static int askNumberOfPins(int frameNo) {
-        System.out.printf(NTH_FRAME_TRY_IS, frameNo);
+    public static int askNumberOfPins(String name) {
+        System.out.printf(WHOSE_TURN, name);
+        return Integer.parseInt(SCANNER.nextLine());
+    }
+
+    public static int askNumberOfPeople() {
+        System.out.print(HOW_MANY_PEOPLE);
         return Integer.parseInt(SCANNER.nextLine());
     }
 }
