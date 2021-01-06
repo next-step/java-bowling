74fb949fdaeae8286b8f43abd32632ee5f29d7c3
rlawls1991 <rlawls1991@gmail.com>
2021-01-06 23:19:58 +0900

    test - answer 테스트 코드작성

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,53 @@
 package qna.domain;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import qna.NotFoundException;
+import qna.UnAuthorizedException;
+import qna.service.DeleteHistoryService;
+import qna.service.QnAService;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+@RunWith(MockitoJUnitRunner.class)
 public class AnswerTest {
-    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+    @Mock
+    private QuestionRepository questionRepository;
+
+    @Mock
+    private DeleteHistoryService deleteHistoryService;
+
+    @InjectMocks
+    private QnAService qnAService;
+
+    private Question question;
+    private Answer answer;
+
+    @Before
+    public void setUp() throws Exception {
+        question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        answer = new Answer(11L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+        question.addAnswer(answer);
+    }
+
+    @Test
+    public void check_null_작성자(){
+        assertThatThrownBy(() -> {
+            Answer answerTest = new Answer(1L, null, question, "test");
+        }).isInstanceOf(UnAuthorizedException.class);
+    }
+
+    @Test
+    public void check_null_question(){
+        assertThatThrownBy(() -> {
+            Answer answerTest = new Answer(1L,  UserTest.JAVAJIGI, null, "test");
+        }).isInstanceOf(NotFoundException.class);
+    }
 }
