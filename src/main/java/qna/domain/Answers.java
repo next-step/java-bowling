1b462eb9d6dbb14b906501650888647787ee46d4
rlawls1991 <rlawls1991@gmail.com>
2021-01-06 23:18:50 +0900

    refactor - delete, 다른사람글답변확인, add, null 확인 기능 추가

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -1,6 +1,7 @@
 package qna.domain;
 
 import org.hibernate.annotations.Where;
+import qna.CannotDeleteException;
 
 import javax.persistence.CascadeType;
 import javax.persistence.OneToMany;
@@ -14,11 +15,43 @@ public class Answers {
     @OrderBy("id ASC")
     private List<Answer> answers = new ArrayList<>();
 
+    public List<DeleteHistory> deleteAnswer() {
+        List<DeleteHistory> deleteHistories = new ArrayList<>();
+
+        for (Answer answer : answers) {
+            answer.setDeleted(true);
+            deleteHistories.add(new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter()));
+        }
+        return deleteHistories;
+    }
+
+    public void isOtherAnswer(User loginUser) throws CannotDeleteException {
+        checkNullUser(loginUser);
+        for (Answer answer : answers) {
+            if (!answer.isOwner(loginUser)) {
+                throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+            }
+        }
+    }
+
     public void addAnswer(Answer answer) {
+        checkNullAnswer(answer);
         answers.add(answer);
     }
 
     public List<Answer> getAnswers() {
         return answers;
+    }
+
+    private void checkNullUser(User loginUser) {
+        if (loginUser == null) {
+            throw new NullPointerException("사용자 정보는 null이 존재할 수 없습니다.");
+        }
+    }
+
+    private void checkNullAnswer(Answer answer) {
+        if (answer == null) {
+            throw new NullPointerException("답변정보는 null이 존재할 수 없습니다.");
+        }
     }
 }
