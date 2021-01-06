978fbbd2fcd599b0bc94007f900d3a992df84cee
rlawls1991 <rlawls1991@gmail.com>
2021-01-06 23:20:15 +0900

    test - AnswerTestData 추가

---
+++ src/test/java/qna/domain/AnswerTestData.java
@@ -0,0 +1,7 @@
+package qna.domain;
+
+public class AnswerTestData {
+
+    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+}
