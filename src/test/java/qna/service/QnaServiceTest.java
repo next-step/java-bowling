46e3625ccc08c077499522c3bd5c1c4e1763e107
rlawls1991 <rlawls1991@gmail.com>
2021-01-06 23:20:35 +0900

    test

--- src/test/java/qna/service/QnaServiceTest.java
+++ src/test/java/qna/service/QnaServiceTest.java
@@ -9,7 +9,6 @@ import org.mockito.junit.MockitoJUnitRunner;
 import qna.CannotDeleteException;
 import qna.domain.*;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -33,6 +32,7 @@ public class QnaServiceTest {
     private Question question;
     private Answer answer;
 
+
     @Before
     public void setUp() throws Exception {
         question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
@@ -52,20 +52,11 @@ public class QnaServiceTest {
     }
 
     @Test
-    public void delete_질문자와_로그인자가_다른경우() throws CannotDeleteException {
-        when(questionRepository.findByIdAndDeletedFalse(question.getId())).thenReturn(Optional.of(question));
-
-        assertThatThrownBy(() -> {
-            question.isOwner(UserTest.SANJIGI);
-        }).isInstanceOf(CannotDeleteException.class);
-    }
-
-    @Test
     public void delete_다른_사람이_쓴_글() throws Exception {
         when(questionRepository.findByIdAndDeletedFalse(question.getId())).thenReturn(Optional.of(question));
 
         assertThatThrownBy(() -> {
-            question.isOwner(UserTest.SANJIGI);
+            qnAService.deleteQuestion(UserTest.SANJIGI, question.getId());
         }).isInstanceOf(CannotDeleteException.class);
     }
 
@@ -78,6 +69,15 @@ public class QnaServiceTest {
         assertThat(question.isDeleted()).isTrue();
         assertThat(answer.isDeleted()).isTrue();
         verifyDeleteHistories();
+    }
+
+    @Test
+    public void delete_답변_중_다른_사람이_쓴_글() throws Exception {
+        when(questionRepository.findByIdAndDeletedFalse(question.getId())).thenReturn(Optional.of(question));
+
+        assertThatThrownBy(() -> {
+            qnAService.deleteQuestion(UserTest.SANJIGI, question.getId());
+        }).isInstanceOf(CannotDeleteException.class);
     }
 
     private void verifyDeleteHistories() {
