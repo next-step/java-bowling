497db06a30dd1ae908efe574a54d813dcd54e623
SungHyeob Baek <baek9652@naver.com>
2021-01-11 09:11:49 +0900

    [test] 프레임에 2회 이상 기록을 시도했을 시에 예외처리 관련 테스트 추가

--- src/test/java/bowling/domain/frame/NormalFrameTest.java
+++ src/test/java/bowling/domain/frame/NormalFrameTest.java
@@ -1,9 +1,11 @@
 package bowling.domain.frame;
 
+import bowling.bowlingexception.IllegalFrameRecordException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NormalFrameTest {
 
@@ -24,6 +26,19 @@ class NormalFrameTest {
         frame.record(4);
         assertThat(frame.isEnd()).isFalse();
         frame.record(5);
-        assertThat(frame.isEnd()).isFalse();
+        assertThat(frame.isEnd()).isTrue();
+    }
+
+    @Test
+    @DisplayName("프레임이 종료 이후에도 record를 시도할 시에 예외 처리")
+    void exceptionWhenAfterTwoRecord() {
+        NormalFrame frame = new NormalFrame();
+
+        frame.record(4);
+        frame.record(5);
+
+        assertThatThrownBy(
+                () -> frame.record(3)
+        ).isInstanceOf(IllegalFrameRecordException.class);
     }
 }
