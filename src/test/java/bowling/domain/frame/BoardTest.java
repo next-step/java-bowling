5a859934a720a648e9576e5cbbdb806438d8a7f8
SungHyeob Baek <baek9652@naver.com>
2021-01-18 11:56:11 +0900

    [test] 게임 시나리오 추가

--- src/test/java/bowling/domain/frame/BoardTest.java
+++ src/test/java/bowling/domain/frame/BoardTest.java
@@ -2,6 +2,11 @@ package bowling.domain.frame;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -37,22 +42,40 @@ public class BoardTest {
         assertThat(board.getCurrentFrameNumber()).isEqualTo(2);
     }
 
-    @Test
+    private static Stream<Arguments> gameScenario() {
+        return Stream.of(
+                Arguments.of(new int[]{
+                        10,
+                        9, 1,
+                        8, 2,
+                        10,
+                        10,
+                        4, 5,
+                        10,
+                        10,
+                        10,
+                        10, 10, 10
+                }),
+                Arguments.of(new int[]{
+                        5, 5,
+                        6, 4,
+                        10,
+                        3, 4,
+                        4, 5,
+                        10,
+                        10,
+                        10,
+                        8, 1,
+                        6, 3
+                })
+        );
+    }
+
+    @MethodSource("gameScenario")
+    @ParameterizedTest
     @DisplayName("게임 종료조건 테스트")
-    void isEnd() {
+    void isEnd(int[] scenario) {
         Board board = new Board();
-        int[] scenario = {
-                10,
-                9, 1,
-                8, 2,
-                10,
-                10,
-                4, 5,
-                10,
-                10,
-                10,
-                10, 10, 10
-        };
 
         for (int downedPin : scenario) {
             assertThat(board.isEnd()).isFalse();
