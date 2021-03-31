6c18019da1bb85dba4fbc81d12e0714d0c480563
apdkeb <apdkeb@naver.com>
2021-03-31 16:41:49 +0900

    test : Answer @DisplayName 변경
    2021. 03. 31.
    - 댓글 -> 답변으로 변경
    
    Resolves : loop-study
    See also : wheejuni

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -7,7 +7,7 @@ import qna.UnAuthorizedException;
 
 import static org.assertj.core.api.Assertions.*;
 
-@DisplayName("댓글")
+@DisplayName("답변")
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
