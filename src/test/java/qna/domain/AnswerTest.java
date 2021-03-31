e9b1a8d49884b0032eee95b5ca08bea521ddcd98
apdkeb <apdkeb@naver.com>
2021-03-31 16:04:04 +0900

    test : Answer 테스트 추가
    2021. 03. 31.
    - 작성자 확인 테스트 코드 추가
    - 작성자 null 에러 테스트 코드 추가
    - 게시글 null 에러 테스트 코드 추가
    
    Resolves : loop-study
    See also : wheejuni

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,52 @@
 package qna.domain;
 
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import qna.NotFoundException;
+import qna.UnAuthorizedException;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("댓글")
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("작성자 확인")
+    public void isAnswer() throws Exception {
+        //given
+        User javajigi = UserTest.JAVAJIGI;
+        User sanjigi = UserTest.SANJIGI;
+
+        //when
+
+        //then
+        assertThat(A1.isOwner(javajigi)).isTrue();
+        assertThat(A2.isOwner(sanjigi)).isTrue();
+    }
+
+    @Test
+    @DisplayName("작성자 null 에러 확인")
+    public void writerIsNullException() throws Exception {
+        //given
+        //when
+        assertThatThrownBy(() -> {
+                new Answer(null, QuestionTest.Q1, "Answers Contents1");
+        }).isInstanceOf(UnAuthorizedException.class);
+        //then
+
+    }
+
+    @Test
+    @DisplayName("게시글 null 에러 확인")
+    public void questionIsNullException() throws Exception {
+        //given
+
+        //when
+        assertThatThrownBy(() -> {
+            new Answer(UserTest.JAVAJIGI, null, "Answers Contents1");
+        }).isInstanceOf(NotFoundException.class);
+        //then
+    }
 }
