c6816713f36085c0c7f1d07cc14aa78bf03389a2
apdkeb <apdkeb@naver.com>
2021-04-02 20:18:57 +0900

    feat(User) : User 추가
    2021. 04. 02.
    - User 추가
    
    Resolves : loop-study
    See also : wheejuni

---
+++ src/main/java/bowling/domain/User.java
@@ -0,0 +1,18 @@
+package bowling.domain;
+
+public class User {
+    private static final String MAX_LENGHT_OVER = "이름은 최대 3글자입니다.";
+    private static final int MAX_LENGHT = 3;
+    private final String name;
+
+    public User(String name) {
+        validMaxLength(name);
+        this.name = name;
+    }
+
+    private void validMaxLength(String name) {
+        if (name.length() > MAX_LENGHT) {
+            throw new IllegalArgumentException(MAX_LENGHT_OVER);
+        }
+    }
+}
--- src/test/java/bowling/domain/UserTest.java
+++ src/test/java/bowling/domain/UserTest.java
@@ -1,5 +1,17 @@
 package bowling.domain;
 
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class UserTest {
 
+    @Test
+    @DisplayName("참여자 이름 3글자 초과 에러 확인")
+    public void userNameMaxLength3OverException() throws Exception {
+        assertThatThrownBy(() -> {
+            new User("povi");
+        });
+    }
 }
