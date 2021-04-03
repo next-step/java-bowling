fdecdd8ab9e4c238a74c957290904abc16676f79
apdkeb <apdkeb@naver.com>
2021-04-03 15:21:26 +0900

    refector : Frame 개선
    2021. 04. 03.
    Frame 개선
    
    Resolves : loop-study
    See also : wheejuni

--- src/main/java/bowling/domain/Frame.java
+++ src/main/java/bowling/domain/Frame.java
@@ -2,24 +2,33 @@ package bowling.domain;
 
 public class Frame {
     private static final int END_TURN = 2;
-    private final Pin pin;
-    private int turn = 0;
+    private static final int FIRST_TURN = 1;
 
-    private Frame(Pin pin) {
-        turn++;
+    private final Pin pin;
+    private final int turn;
+
+    private Frame(Pin pin, int turn) {
+        this.turn = turn;
         this.pin = pin;
     }
 
     public static Frame first(int countOfDownPin) {
-        return new Frame(Pin.first(countOfDownPin));
+        return new Frame(Pin.first(countOfDownPin), FIRST_TURN);
     }
 
     public Frame second(int countOfDownPin) {
-        turn++;
-        return new Frame(pin.next(countOfDownPin));
+        return new Frame(pin.next(countOfDownPin), END_TURN);
     }
-    
+
     public boolean isEnd() {
         return turn >= END_TURN;
+    }
+
+    public int firstDownPin() {
+        return pin.firstBall();
+    }
+
+    public int secondDownPin() {
+        return pin.secondBall();
     }
 }
