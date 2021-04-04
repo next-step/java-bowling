f450c8abf53f666651e2fb81e834853dafba6e06
apdkeb <apdkeb@naver.com>
2021-04-04 12:06:15 +0900

    refector(Pin) : 재설계
    2021. 04. 04.
    이전 설계은 마지막 프레임에 대한 심각한 오류로 Pin 부터 다시 재설계 진행중.
    
    Resolves : loop-study
    See also : wheejuni

--- src/main/java/bowling/domain/Pin.java
+++ src/main/java/bowling/domain/Pin.java
@@ -1,58 +1,45 @@
 package bowling.domain;
 
-import java.util.function.BiPredicate;
-
 public class Pin {
     private static final String MAX_OVER_PINS = "넘어뜨리는 볼링핀은 10개가 최대입니다.";
     private static final String MIN_UNDER_PINS = "넘어뜨리는 볼링핀은 0개 미만이 안됩니다.";
     private static final int MAX_PINS = 10;
     private static final int MIN_PINS = 0;
 
-    private int firstBall = 0;
-    private int secondBall = 0;
+    private final int pin;
 
-    private Pin(int firstBall) {
-        this(firstBall, 0);
+    private Pin(int pin) {
+        validMaxPins(pin);
+        validMinPins(pin);
+        this.pin = pin;
     }
 
-    private Pin(int firstBall, int secondBall) {
-        validMaxPin(firstBall, secondBall);
-        validMinPin(firstBall, secondBall);
-        this.firstBall = firstBall;
-        this.secondBall = secondBall;
+    public static Pin of(int pin) {
+        return new Pin(pin);
     }
 
-    public static Pin first(int firstBall) {
-        return new Pin(firstBall);
-    }
-
-    public Pin next(int secondBall) {
-        return new Pin(this.firstBall, secondBall);
-    }
-
-    private void validMinPin(int firstBall, int secondBall) {
-        if (firstBall < MIN_PINS
-                || secondBall < MIN_PINS) {
+    private void validMinPins(int pin) {
+        if (pin < MIN_PINS) {
             throw new IllegalArgumentException(MIN_UNDER_PINS);
         }
     }
 
-    private void validMaxPin(int firstBall, int secondBall) {
-        if ((firstBall + secondBall) > MAX_PINS) {
+    private void validMaxPins(int pin) {
+        if (pin > MAX_PINS) {
             throw new IllegalArgumentException(MAX_OVER_PINS);
         }
     }
 
-    public boolean checkOfDownPin(BiPredicate<Integer, Integer> knockOverCount) {
-        return knockOverCount.test(firstBall, secondBall);
+    public boolean isStrike(boolean isFirst) {
+        return ScoreRule.STRIKE == ScoreRule.of(pin, isFirst);
     }
 
-    public int firstBall() {
-        return firstBall;
+    public int accumulated(int downPins) {
+        return pin + downPins;
     }
 
-    public int secondBall() {
-        return secondBall;
+    public int pin() {
+        return pin;
     }
 }
 
--- src/test/java/bowling/domain/PinTest.java
+++ src/test/java/bowling/domain/PinTest.java
@@ -15,54 +15,33 @@ public class PinTest {
     @DisplayName("스트라이크 확인")
     public void strike() throws Exception {
         //given
-        Pin pin = Pin.first(10);
-
+        ScoreRule scoreEnum = ScoreRule.of(10, true);
         //when
-        Rule scoreEnum = Rule.of(pin, true);
 
         //then
-        assertThat(scoreEnum).isEqualTo(Rule.STRIKE);
+        assertThat(scoreEnum).isEqualTo(ScoreRule.STRIKE);
     }
 
-    @ParameterizedTest
-    @CsvSource(value = {"9,1", "5,5", "1,9"})
+    @Test
     @DisplayName("스페어 확인")
-    public void spare(int first, int second) throws Exception {
+    public void spare() throws Exception {
         //given
-        Pin pin = Pin.first(first).next(second);
-
         //when
-        Rule scoreEnum = Rule.of(pin, false);
+        ScoreRule scoreEnum = ScoreRule.of(10, false);
 
         //then
-        assertThat(scoreEnum).isEqualTo(Rule.SPARE);
+        assertThat(scoreEnum).isEqualTo(ScoreRule.SPARE);
     }
     
     @Test
     @DisplayName("첫구 거터 확인")
     public void firstBallGutter() throws Exception {
         //given
-        Pin pin = Pin.first(0);
-
         //when
-        Rule scoreEnum = Rule.of(pin, true);
+        ScoreRule scoreEnum = ScoreRule.of(0, true);
 
         //then
-        assertThat(scoreEnum).isEqualTo(Rule.FIRST_GUTTER);
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"0,0", "1,0", "9,0"})
-    @DisplayName("2구 거터 확인")
-    public void secondBallGutter(int first, int second) throws Exception {
-        //given
-        Pin pin = Pin.first(first).next(second);
-
-        //when
-        Rule scoreEnum = Rule.of(pin, false);
-
-        //then
-        assertThat(scoreEnum).isEqualTo(Rule.SECOND_GUTTER);
+        assertThat(scoreEnum).isEqualTo(ScoreRule.GUTTER);
     }
 
     @ParameterizedTest
@@ -70,55 +49,24 @@ public class PinTest {
     @DisplayName("첫구 MISS 확인")
     public void firstBallMiss(int first) throws Exception {
         //given
-        Pin pin = Pin.first(first);
-
         //when
-        Rule scoreEnum = Rule.of(pin, true);
+        ScoreRule scoreEnum = ScoreRule.of(first, true);
 
         //then
-        assertThat(scoreEnum).isEqualTo(Rule.MISS);
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"0,1", "1,1", "1,8"})
-    @DisplayName("2구 MISS 확인")
-    public void secondBallMiss(int first, int second) throws Exception {
-        //given
-        Pin pin = Pin.first(first).next(second);
-
-        //when
-        Rule scoreEnum = Rule.of(pin, false);
-
-        //then
-        assertThat(scoreEnum).isEqualTo(Rule.MISS);
+        assertThat(scoreEnum).isEqualTo(ScoreRule.MISS);
     }
 
     @Test
     @DisplayName("첫구 잘못된 핀개수 초과 에러")
     public void downPin10OverException() {
-        assertThatThrownBy(() -> Pin.first(11))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"1,10", "9,2", "10,1"})
-    @DisplayName("첫구,2구 총개수 초과 에러")
-    public void totalDownPin10OverException(int first, int second) {
-        assertThatThrownBy(() -> Pin.first(first).next(second))
+        assertThatThrownBy(() -> Pin.of(11))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("첫구 핀 0개 미만 에러")
     public void firstPin0UnderException() {
-        assertThatThrownBy(() -> Pin.first(-1))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    @DisplayName("2구 0개 미만 에러")
-    public void secondPin0UnderException() {
-        assertThatThrownBy(() -> Pin.first(0).next(-1))
+        assertThatThrownBy(() -> Pin.of(-1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
