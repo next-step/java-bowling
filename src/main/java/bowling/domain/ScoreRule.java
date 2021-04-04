728819f33abbc1f697eb919e168060aceb9dd3c0
apdkeb <apdkeb@naver.com>
2021-04-05 00:48:02 +0900

    refector(ScoreRule) : 피드백 반영
    2021. 04. 04.
    - Predicate 제거 -> 단순 int 로 변경
    
    Resolves : loop-study
    See also : wheejuni

--- src/main/java/bowling/domain/ScoreRule.java
+++ src/main/java/bowling/domain/ScoreRule.java
@@ -1,25 +1,24 @@
 package bowling.domain;
 
 import java.util.Arrays;
-import java.util.function.Predicate;
 
 public enum ScoreRule {
-    STRIKE(a -> a == 10, "x"),
-    SPARE(a -> a == 10, "/"),
-    GUTTER(a -> a == 0, "-"),
-    MISS(a -> true, "");
+    STRIKE(10, "x"),
+    SPARE(10, "/"),
+    GUTTER(0, "-"),
+    MISS(-1, "");
 
-    private Predicate<Integer> knockOverCount;
+    private int count;
     public String symbol;
 
-    ScoreRule(Predicate<Integer> knockOverCount, String symbol) {
-        this.knockOverCount = knockOverCount;
+    ScoreRule(int count, String symbol) {
+        this.count = count;
         this.symbol = symbol;
     }
 
     public static ScoreRule of(int pin, boolean isFirst) {
         ScoreRule result = Arrays.stream(values())
-                .filter(rule -> rule.knockOverCount.test(pin))
+                .filter(rule -> rule.count == pin)
                 .findFirst()
                 .orElse(MISS);
 
