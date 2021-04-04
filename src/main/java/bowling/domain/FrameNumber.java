cf6099516d4875cc2ef16d2417e2910afbcc1485
JuJinPark <wnwls1216@gmail.com>
2021-04-04 15:35:10 +0900

    [refactor] FrameNumber 마지막 번호 제거 및 메서드 추가

--- src/main/java/bowling/domain/FrameNumber.java
+++ src/main/java/bowling/domain/FrameNumber.java
@@ -2,17 +2,24 @@ package bowling.domain;
 
 public class FrameNumber {
 
-    private static final int MIN_FRAME_NUMBER = 1;
-
-    private static final int MAX_FRAME_NUMBER = 10;
+    private static final int MIN = 1;
 
     private final int number;
 
     public FrameNumber(int number) {
-        if (number < MIN_FRAME_NUMBER || number > MAX_FRAME_NUMBER) {
+        if (number < MIN ) {
             throw new IllegalArgumentException("프레임 번호는 1-10사이어야 합니다.");
         }
         this.number = number;
+    }
+
+    public static FrameNumber first() {
+        return new FrameNumber(MIN);
+    }
+
+
+    public FrameNumber next() {
+        return new FrameNumber(number + 1);
     }
 
     public int number() {
@@ -33,4 +40,5 @@ public class FrameNumber {
     public int hashCode() {
         return number;
     }
+
 }
