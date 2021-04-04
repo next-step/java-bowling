53926f4c0260083f47c859e4491a7a217452f9a8
JuJinPark <wnwls1216@gmail.com>
2021-04-04 15:32:07 +0900

    [test] PinCountsTest 추가

--- src/test/java/bowling/domain/PinCountsTest.java
+++ src/test/java/bowling/domain/PinCountsTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PinCountsTest {
+
     @Test
     void add() {
         int pinCountInInt = 5;
@@ -13,4 +14,25 @@ public class PinCountsTest {
 
         assertThat(pinCounts.counts()).containsExactly(new PinCount(pinCountInInt));
     }
+
+    @Test
+    void total_count() {
+        int pinCountInIntAtFirstTry = 7;
+        int pinCountInIntAtSecondTry = 2;
+        PinCounts pinCounts = new PinCounts();
+        pinCounts.add(pinCountInIntAtFirstTry);
+        pinCounts.add(pinCountInIntAtSecondTry);
+
+        assertThat(pinCounts.totalCount()).isEqualTo(pinCountInIntAtFirstTry + pinCountInIntAtSecondTry);
+    }
+
+    @Test
+    void size() {
+        int pinCountInInt = 4;
+        PinCounts pinCounts = new PinCounts();
+        pinCounts.add(pinCountInInt);
+
+        assertThat(pinCounts.size()).isEqualTo(1);
+    }
+
 }
