f652c7341143637ab91dd86f8059690ecaccf5d1
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-04-07 00:14:13 +0900

    feat : 일급컬렉션 객체 추가

---
+++ src/main/java/qna/domain/Answers.java
@@ -0,0 +1,10 @@
+package qna.domain;
+
+public class Answers {
+
+    private final Answers answers;
+
+    public Answers(Answers answers) {
+        this.answers = answers;
+    }
+}
---
+++ src/main/java/qna/domain/DeleteHistorys.java
@@ -0,0 +1,12 @@
+package qna.domain;
+
+import java.util.List;
+
+public class DeleteHistorys {
+
+    private final List<DeleteHistory> deleteHistoryList;
+
+    public DeleteHistorys(List<DeleteHistory> deleteHistoryList) {
+        this.deleteHistoryList = deleteHistoryList;
+    }
+}
