c005ff16ea302e762fe5b1d7dcc4be38e70ce637
jeonyeonbin <qtrgor@gmail.com>
2021-04-07 19:08:57 +0900

    feat: 나머지 기능들 구현

--- src/main/java/bowling/controller/BowlingGameController.java
+++ src/main/java/bowling/controller/BowlingGameController.java
@@ -1,6 +1,11 @@
 package bowling.controller;
 
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 import bowling.entity.Player;
+import bowling.entity.Players;
 import bowling.views.InputView;
 import bowling.views.OutputView;
 
@@ -14,23 +19,48 @@ public class BowlingGameController {
 	}
 
 	public void run() {
-		Player player = makePlayer();
+		int count = makePeopleCount();
 
-		while(player.isKeepGoing()) {
-			player.addPlayerFrameScore(inputView.getFrameScore(player.currentPlayerFrameIndex()));
-			showGame(player);
+		Players players = makePlayers(count);
+		while (players.isKeepGoing()) {
+			int turn = players.turn();
+			players.getPlayers().forEach(playerRunFunc.apply(inputView).apply(turn, players));
 		}
+
 	}
+
+	private Function<InputView, BiFunction<Integer, Players, Consumer<Player>>> playerRunFunc = (inputView) -> (turn, players) -> (player) -> {
+		while (player.isKeepGoing(turn)) {
+			player.addPlayerFrameScore(inputView.getFrameScore(player));
+			showGame(players);
+		}
+	};
 
 	private Player makePlayer() {
 		String name = inputView.getUserName();
 		return new Player(name);
 	}
 
+	private Players makePlayers(int count) {
+		return new Players(inputView.getPlayers(count));
+	}
+
+	private int makePeopleCount() {
+		return inputView.getPeopleCount();
+	}
+
 	private void showGame(Player player) {
 		outputView.showFrames();
 		outputView.showPlayerFrames(player);
 		outputView.showPlayerFrameScore(player);
+	}
+
+	private void showGame(Players players) {
+		outputView.showFrames();
+		players.getPlayers().forEach(player -> {
+			outputView.showPlayerFrames(player);
+			outputView.showPlayerFrameScore(player);
+		});
 	}
 
 }
