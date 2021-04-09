bce72e566d94146742dddd6b93436870d6446b8a
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-04-09 15:59:55 +0900

    feat : 질문 삭제 기능 , 답변자와 질문자가 같은지 판별 코드 추가

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -1,10 +1,60 @@
 package qna.domain;
 
+import org.hibernate.annotations.Where;
+import qna.CannotDeleteException;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
+@Embeddable
 public class Answers {
 
-    private final Answers answers;
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    @Where(clause = "deleted = false")
+    @OrderBy("id ASC")
+    private final List<Answer> answers;
 
-    public Answers(Answers answers) {
+    private Answers(List<Answer> answers) {
         this.answers = answers;
+    }
+
+    protected Answers() {
+        this.answers = new ArrayList<>();
+    }
+
+    public static Answers of(List<Answer> answers){
+        return new Answers(answers);
+    }
+
+    public void add(Answer answer) {
+        answers.add(answer);
+    }
+
+    public boolean isOwner(User user) {
+        if(!(answers.size() == writeCount(user))){
+            throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+        }
+        return true;
+    }
+
+    private int writeCount(User user) {
+        return (int) answers.stream()
+                .filter(answer -> answer.isOwner(user))
+                .count();
+    }
+
+    public DeleteHistorys delete() {
+         return  answers.stream()
+                    .map(Answer::delete)
+                    .map(answer -> new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now()))
+                    .collect(collectingAndThen(toList(),DeleteHistorys::of));
     }
 }
