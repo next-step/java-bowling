ad759a03f90f74185dd6ea5e72e42d6238f43e7d
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-04-09 15:21:07 +0900

    refactoring(AnswersTest): 예외케이스, 삭제 시 히스토리 저장 케이스 추가

--- src/test/java/qna/domain/AnswersTest.java
+++ src/test/java/qna/domain/AnswersTest.java
@@ -7,10 +7,11 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AnswersTest {
-
 
     private Answers answers;
 
@@ -23,17 +24,34 @@ class AnswersTest {
         answers = new Answers(Arrays.asList(A1,A2));
     }
 
-    @DisplayName(" 질문자와 답변글의 모든 답변자가 같은 경우 삭제를 할 수 없다.")
+    @DisplayName("질문자와 답변글이 같은지 판별한다.")
     @Test
     void testCase1() {
         User user = UserTest.JAVAJIGI;
-        Answers answers = createAnswers(UserTest.JAVAJIGI, UserTest.SANJIGI);
-        Answers deletedAnswers = answers.delete(user);
+        Answers answers = createAnswers(UserTest.JAVAJIGI, UserTest.JAVAJIGI);
+        boolean owner = answers.isOwner(user);
 
-        Assertions.assertThat(deletedAnswers.deletedAnswersSize()).isEqualTo(2);
+        assertThat(owner).isTrue();
     }
 
+    @DisplayName("로그인 사용자와 답변한 사람이 같지 않으면 예외가 발생한다.")
+    @Test
+    void testCase2() {
+        User user = UserTest.JAVAJIGI;
+        Answers answers = createAnswers(UserTest.JAVAJIGI, UserTest.SANJIGI);
+        assertThatThrownBy(() ->{
+            answers.isOwner(user);
+        }).isInstanceOf(RuntimeException.class);
+    }
 
+    @DisplayName("답변들이 삭제되면 답변들을 삭제 히스토리에 저장한다.")
+    @Test
+    void testCase4() {
+        Answers answers = createAnswers(UserTest.JAVAJIGI, UserTest.SANJIGI);
+        DeleteHistorys delete = answers.delete(DeleteHistorys.of());
+
+        assertThat(delete.size()).isEqualTo(2);
+    }
 
     Answers createAnswers(User user, User user2) {
         Question Q1 = new Question("title1", "contents1").writeBy(user);
