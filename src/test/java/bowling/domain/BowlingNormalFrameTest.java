0d58508e7f6f048b1c705ef16edb8f264e610c36
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-04-10 21:55:02 +0900

    test: 볼링 프레임 테스트 코드 추가

---
+++ src/test/java/bowling/domain/BowlingNormalFrameTest.java
@@ -0,0 +1,27 @@
+package bowling.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BowlingNormalFrameTest {
+
+    @DisplayName("볼링 프레임은 0부터 시작한다.")
+    @Test
+    void case1() {
+        BowlingNormalFrame bowlingNormalFrame = BowlingNormalFrame.first();
+
+        assertThat(bowlingNormalFrame.position()).isEqualTo((Position.of(0)));
+    }
+
+    @DisplayName("한 게임이 끝나면 새로운 프레임이 생성된다.")
+    @Test
+    void case2() {
+        BowlingNormalFrame bowlingNormalFrame = BowlingNormalFrame.first();
+        BowlingFrame newBowlingNormalFrame = bowlingNormalFrame.play();
+
+        assertThat(newBowlingNormalFrame.position()).isEqualTo(Position.of(1));
+    }
+    
+}
