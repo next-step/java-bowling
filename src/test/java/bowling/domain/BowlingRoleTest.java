0b43020e13d57059f1cee19347c2bc75353cdc8f
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-04-10 16:11:38 +0900

    refactoring : 룰 추가에 따른 테스트 코드 수정

--- src/test/java/bowling/domain/BowlingRoleTest.java
+++ src/test/java/bowling/domain/BowlingRoleTest.java
@@ -11,21 +11,21 @@ class BowlingRoleTest {
     @Test
     void case1() {
         BowlingRole bowlingRole = BowlingRole.valueOf(10, 0);
-        assertThat(bowlingRole).isEqualTo(bowlingRole.STRIKE);
+        assertThat(bowlingRole).isEqualTo(BowlingRole.STRIKE);
     }
 
     @DisplayName("두개의 포인트 합이 10이면 스페어다.")
     @Test
     void case2() {
         BowlingRole bowlingRole = BowlingRole.valueOf(10, 0);
-        assertThat(bowlingRole).isEqualTo(bowlingRole.STRIKE);
+        assertThat(bowlingRole).isEqualTo(BowlingRole.STRIKE);
     }
 
     @DisplayName("두개의 포인트 합이 10이 아니면 미스다.")
     @Test
     void case3() {
         BowlingRole bowlingRole = BowlingRole.valueOf(5, 4);
-        assertThat(bowlingRole).isEqualTo(bowlingRole.STRIKE);
+        assertThat(bowlingRole).isEqualTo(BowlingRole.STRIKE);
     }
 
 }
