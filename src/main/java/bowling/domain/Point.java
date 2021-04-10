d0136adb0c288bea10bd4b60332320340fb13062
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-04-10 15:20:07 +0900

    feat : Point 생성 코드 추가

---
+++ src/main/java/bowling/domain/Point.java
@@ -0,0 +1,21 @@
+package bowling.domain;
+
+public class Point {
+
+    private final int point;
+
+    private Point(int point) {
+        valid(point);
+        this.point = point;
+    }
+
+    private void valid(int point) {
+        if (point < 0 || point > 10) {
+            throw new IllegalArgumentException("포인트는 0에서 10 사이여야 합니다.");
+        }
+    }
+
+    public static Point of(int point) {
+        return new Point(point);
+    }
+}
