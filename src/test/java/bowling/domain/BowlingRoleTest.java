9accf6fb354fa3bd2e47c4e7e1c9b6cfe447905a
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-04-10 16:10:10 +0900

    test : Role 테스트 코드 추가

--- src/test/java/bowling/domain/BowlingRoleTest.java
+++ src/test/java/bowling/domain/BowlingRoleTest.java
@@ -1,5 +1,31 @@
 package bowling.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class BowlingRoleTest {
+
+    @DisplayName("첫번쨰 포인트가 10이면 STRIKE이다.")
+    @Test
+    void case1() {
+        BowlingRole bowlingRole = BowlingRole.valueOf(10, 0);
+        assertThat(bowlingRole).isEqualTo(bowlingRole.STRIKE);
+    }
+
+    @DisplayName("두개의 포인트 합이 10이면 스페어다.")
+    @Test
+    void case2() {
+        BowlingRole bowlingRole = BowlingRole.valueOf(10, 0);
+        assertThat(bowlingRole).isEqualTo(bowlingRole.STRIKE);
+    }
+
+    @DisplayName("두개의 포인트 합이 10이 아니면 미스다.")
+    @Test
+    void case3() {
+        BowlingRole bowlingRole = BowlingRole.valueOf(5, 4);
+        assertThat(bowlingRole).isEqualTo(bowlingRole.STRIKE);
+    }
 
 }
