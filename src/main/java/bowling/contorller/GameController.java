65e0619e3294c4858e08c030fae5e33b6cb8cc1d
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-04-11 20:06:57 +0900

    feat(GameController) : 볼링 게임 로직 구현

--- src/main/java/bowling/contorller/GameController.java
+++ src/main/java/bowling/contorller/GameController.java
@@ -1,6 +1,8 @@
 package bowling.contorller;
 
+import bowling.domain.BowlingFrame;
 import bowling.view.InputView;
+import bowling.view.OutputView;
 
 public class GameController {
 
@@ -13,9 +15,33 @@ public class GameController {
         this.bowlingGame = BowlingGame.of();
     }
 
+    public BowlingFrame play() {
+        int point = InputView.inputPoint(bowlingGame.round());
+        BowlingFrame bowlingFrame = bowlingGame.firstPitching(point);
+        OutputView.firstResultView(BowlingBoardDto.of(player, bowlingFrame));
+        boolean isStrike = bowlingFrame.isStrike();
+        if (!isStrike) {
+            return next();
+        }
+        return bowlingFrame;
+    }
+
+    public BowlingFrame next() {
+        int point = InputView.inputPoint(bowlingGame.round());
+        BowlingFrame bowlingFrame = bowlingGame.secondPitching(point);
+        OutputView.secondResultView(BowlingBoardDto.of(player, bowlingFrame));
+        return bowlingFrame;
+    }
 
     public static void main(String[] args) {
         String playerName = InputView.inputPlayer();
         GameController gameController = new GameController(playerName);
+        boolean isEnd = true;
+        do {
+            BowlingFrame play = gameController.play();
+            isEnd = play.isEnd();
+        } while (isEnd);
     }
+
+
 }
