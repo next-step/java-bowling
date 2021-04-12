1dd2f9a3e0d7b95d4a4efa3b8bfd0c34e66849b1
meme2367 <meme91322367@gmail.com>
2021-04-12 14:47:03 +0900

    feat : Answer 테스트 코드 작성

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,26 @@
 package qna.domain;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("삭제 확인")
+    public void isDeleted() {
+      A1.delete();
+      assertThat(A1.isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("답변자인지 확인")
+    public void isOwner() {
+        assertThat(A1.isOwner(UserTest.JAVAJIGI)).isTrue();
+    }
+
 }
