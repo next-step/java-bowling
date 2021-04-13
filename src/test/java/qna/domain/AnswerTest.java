e191d27af2b448a43c033e88c39f63823b0f912e
seokrae <kslbsh@gmail.com>
2021-04-13 21:42:24 +0900

    test: answer 생성 테스트

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,20 @@
 package qna.domain;
 
-public class AnswerTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @DisplayName("")
+    @Test
+    void created_생성테스트() {
+        // when
+        Answer expected = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+        // then
+        assertThat(A1).isEqualTo(expected);
+    }
 }
