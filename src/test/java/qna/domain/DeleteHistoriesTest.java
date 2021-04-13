661f2be5f66c0e67edd5b27c7166ed6b2e8414fd
623nana <6230nana@gmail.com>
2021-04-13 23:28:42 +0900

    test: DeleteHistories 테스트

---
+++ src/test/java/qna/domain/DeleteHistoriesTest.java
@@ -0,0 +1,39 @@
+package qna.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static qna.domain.QuestionTest.Q1;
+import static qna.domain.QuestionTest.Q2;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DeleteHistoriesTest {
+
+  private DeleteHistories deleteHistories;
+
+  @BeforeEach
+  void setUp() {
+    deleteHistories = new DeleteHistories();
+  }
+
+  @Test
+  void add_history_test() {
+    deleteHistories.add(new DeleteHistory(Q1, LocalDateTime.now()));
+
+    int size = deleteHistories.history().size();
+
+    assertThat(size).isEqualTo(1);
+  }
+
+  @Test
+  void addAll_history_test() {
+    deleteHistories.addAll(Arrays.asList(new DeleteHistory(Q1, LocalDateTime.now()), new DeleteHistory(Q2,LocalDateTime.now())));
+
+    int size = deleteHistories.history().size();
+
+    assertThat(size).isEqualTo(2);
+  }
+
+}
