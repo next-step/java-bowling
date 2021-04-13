63f3d51b528fe6fb6a4ddcd783be9e1a2fa94e46
623nana <6230nana@gmail.com>
2021-04-13 22:29:03 +0900

    refactor: Question 삭제 로직 도메인에서 실행되도록 수정

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -1,5 +1,7 @@
 package qna.domain;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import javax.persistence.*;
 import qna.CannotDeleteException;
 
@@ -80,10 +82,19 @@ public class Question extends AbstractEntity {
     return deleted;
   }
 
+  public List<Answer> answers() {
+    return answers.answers();
+  }
+
   public void hasDeleteOwner(User loginUser) throws CannotDeleteException {
     if (!isOwner(loginUser)) {
       throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
     }
+  }
+
+  public DeleteHistory deleteQuestion(User loginUser) throws CannotDeleteException {
+    hasDeleteOwner(loginUser);
+    return new DeleteHistory(setDeleted(true), LocalDateTime.now());
   }
 
   @Override
--- src/main/java/qna/service/QnAService.java
+++ src/main/java/qna/service/QnAService.java
@@ -10,49 +10,46 @@ import qna.domain.*;
 
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service("qnaService")
 public class QnAService {
-    private static final Logger log = LoggerFactory.getLogger(QnAService.class);
 
-    @Resource(name = "questionRepository")
-    private QuestionRepository questionRepository;
+  private static final Logger log = LoggerFactory.getLogger(QnAService.class);
 
-    @Resource(name = "answerRepository")
-    private AnswerRepository answerRepository;
+  @Resource(name = "questionRepository")
+  private QuestionRepository questionRepository;
 
-    @Resource(name = "deleteHistoryService")
-    private DeleteHistoryService deleteHistoryService;
+  @Resource(name = "answerRepository")
+  private AnswerRepository answerRepository;
 
-    @Transactional(readOnly = true)
-    public Question findQuestionById(Long id) {
-        return questionRepository.findByIdAndDeletedFalse(id)
-                .orElseThrow(NotFoundException::new);
+  @Resource(name = "deleteHistoryService")
+  private DeleteHistoryService deleteHistoryService;
+
+  @Transactional(readOnly = true)
+  public Question findQuestionById(Long id) {
+    return questionRepository.findByIdAndDeletedFalse(id)
+        .orElseThrow(NotFoundException::new);
+  }
+
+  @Transactional
+  public void deleteQuestion(User loginUser, long questionId) throws CannotDeleteException {
+
+    Question question = findQuestionById(questionId);
+
+    DeleteHistories deleteHistories = new DeleteHistories();
+    deleteHistories.add(question.deleteQuestion(loginUser));
+
+    List<Answer> answers = question.answers();
+    for (Answer answer : answers) {
+      answer.hasOthers(loginUser);
     }
 
-    @Transactional
-    public void deleteQuestion(User loginUser, long questionId) throws CannotDeleteException {
-        Question question = findQuestionById(questionId);
-        if (!question.isOwner(loginUser)) {
-            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
-        }
-
-        List<Answer> answers = question.getAnswers();
-        for (Answer answer : answers) {
-            if (!answer.isOwner(loginUser)) {
-                throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
-            }
-        }
-
-        List<DeleteHistory> deleteHistories = new ArrayList<>();
-        question.setDeleted(true);
-        deleteHistories.add(new DeleteHistory(ContentType.QUESTION, questionId, question.getWriter(), LocalDateTime.now()));
-        for (Answer answer : answers) {
-            answer.setDeleted(true);
-            deleteHistories.add(new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now()));
-        }
-        deleteHistoryService.saveAll(deleteHistories);
+    for (Answer answer : answers) {
+      answer.setDeleted(true);
+      deleteHistories.add(new DeleteHistory(answer, LocalDateTime.now()));
     }
+
+    deleteHistoryService.saveAll(deleteHistories.history());
+  }
 }
