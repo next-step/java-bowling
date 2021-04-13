e4fcc71e765bd8018117e6bf92ebab47dd26d855
JuJinPark <wnwls1216@gmail.com>
2021-04-13 18:36:31 +0900

    [feat] score를 추라한 FrameResult객체 구현중

--- src/main/java/bowling/dto/FrameResult.java
+++ src/main/java/bowling/dto/FrameResult.java
@@ -1,5 +1,6 @@
 package bowling.dto;
 
+import bowling.domain.Score;
 import bowling.domain.frame.Frame;
 
 public class FrameResult {
@@ -8,9 +9,16 @@ public class FrameResult {
 
     private String state;
 
+    private int fullyCalculatedScore;
+
     public FrameResult(Frame frame) {
         this.frameNumber = frame.number().number();
         this.state = frame.currentState().stateInString();
+        Score score = frame.score();
+        if(score.isUnFinished() || !score.isDoneCalculating()){
+            this.fullyCalculatedScore =
+        }
+        this.fullyCalculatedScore = score.scoreInInt();
     }
 
     public int frameNumber() {
