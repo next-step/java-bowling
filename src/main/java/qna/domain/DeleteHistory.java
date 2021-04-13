6a3e49185206c1f21537e0c16aaf4c4e4681b036
623nana <6230nana@gmail.com>
2021-04-13 22:15:13 +0900

    refactor: 생성자 파라미터 각각의 객체로 받도록 수정

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -6,50 +6,70 @@ import java.util.Objects;
 
 @Entity
 public class DeleteHistory {
-    @Id
-    @GeneratedValue
-    private Long id;
 
-    @Enumerated(EnumType.STRING)
-    private ContentType contentType;
+  @Id
+  @GeneratedValue
+  private Long id;
 
-    private Long contentId;
+  @Enumerated(EnumType.STRING)
+  private ContentType contentType;
 
-    @ManyToOne
-    @JoinColumn(foreignKey = @ForeignKey(name = "fk_deletehistory_to_user"))
-    private User deletedBy;
+  private Long contentId;
 
-    private LocalDateTime createDate = LocalDateTime.now();
+  @ManyToOne
+  @JoinColumn(foreignKey = @ForeignKey(name = "fk_deletehistory_to_user"))
+  private User deletedBy;
 
-    public DeleteHistory() {
+  private LocalDateTime createDate = LocalDateTime.now();
+
+  public DeleteHistory() {
+  }
+
+  public DeleteHistory(ContentType contentType, Long contentId, User deletedBy, LocalDateTime createDate) {
+    this.contentType = contentType;
+    this.contentId = contentId;
+    this.deletedBy = deletedBy;
+    this.createDate = createDate;
+  }
+
+  public DeleteHistory(Question question, LocalDateTime createDate) {
+    this.contentType = ContentType.QUESTION;
+    this.contentId = question.getId();
+    this.deletedBy = question.getWriter();
+    this.createDate = createDate;
+  }
+
+  public DeleteHistory(Answer answer, LocalDateTime createDate) {
+    this.contentType = ContentType.ANSWER;
+    this.contentId = answer.getId();
+    this.deletedBy = answer.getWriter();
+    this.createDate = createDate;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
-
-    public DeleteHistory(ContentType contentType, Long contentId, User deletedBy, LocalDateTime createDate) {
-        this.contentType = contentType;
-        this.contentId = contentId;
-        this.deletedBy = deletedBy;
-        this.createDate = createDate;
+    if (o == null || getClass() != o.getClass()) {
+      return false;
     }
+    DeleteHistory that = (DeleteHistory) o;
+    return Objects.equals(id, that.id) &&
+        contentType == that.contentType &&
+        Objects.equals(contentId, that.contentId) &&
+        Objects.equals(deletedBy, that.deletedBy);
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        DeleteHistory that = (DeleteHistory) o;
-        return Objects.equals(id, that.id) &&
-                contentType == that.contentType &&
-                Objects.equals(contentId, that.contentId) &&
-                Objects.equals(deletedBy, that.deletedBy);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, contentType, contentId, deletedBy);
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, contentType, contentId, deletedBy);
-    }
-
-    @Override
-    public String toString() {
-        return "DeleteHistory [id=" + id + ", contentType=" + contentType + ", contentId=" + contentId + ", deletedBy="
-                + deletedBy + ", createDate=" + createDate + "]";
-    }
+  @Override
+  public String toString() {
+    return "DeleteHistory [id=" + id + ", contentType=" + contentType + ", contentId=" + contentId
+        + ", deletedBy="
+        + deletedBy + ", createDate=" + createDate + "]";
+  }
 }
