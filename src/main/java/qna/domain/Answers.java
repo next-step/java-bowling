dbc71109ab39f2d38b54b9a58819e2927d50fc8f
seokrae <kslbsh@gmail.com>
2021-04-14 13:31:29 +0900

    refactor: answers 답변 게시물 삭제여부 확인 및 삭제 처리 테스트

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -1,13 +1,16 @@
 package qna.domain;
 
 import org.hibernate.annotations.Where;
+import qna.CannotDeleteException;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
 import javax.persistence.OrderBy;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Embeddable
 public class Answers {
@@ -15,11 +18,29 @@ public class Answers {
     @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
     @Where(clause = "deleted = false")
     @OrderBy("id ASC")
-    List<Answer> answers = new ArrayList<>();
+    private final List<Answer> answers = new ArrayList<>();
 
     public Answers() { }
 
     public void add(Answer answer) {
         answers.add(answer);
+    }
+
+    public void deleteAnswers(User loginUser) {
+        for (Answer answer : answers) {
+            if (!answer.isOwner(loginUser)) {
+                throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+            }
+        }
+    }
+
+    public List<DeleteHistory> deleteAll() {
+        return answers.stream()
+                .map(Answer::deleteAnswers)
+                .collect(Collectors.toList());
+    }
+
+    public List<Answer> getAnswers() {
+        return Collections.unmodifiableList(answers);
     }
 }
---
+++ src/test/java/qna/domain/AnswersTest.java
@@ -0,0 +1,41 @@
+package qna.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static qna.domain.AnswerTest.A1;
+import static qna.domain.AnswerTest.A3;
+
+class AnswersTest {
+
+    @DisplayName("답변 게시물 삭제 권한 테스트")
+    @Test
+    void deleteAnswers_응답_삭제_여부테스트() {
+        // given
+        Answers answers = new Answers();
+        // when
+        answers.add(A1);
+        // then
+        assertThatExceptionOfType(CannotDeleteException.class)
+                .isThrownBy(() -> answers.deleteAnswers(UserTest.SANJIGI));
+    }
+
+    @DisplayName("답변 게시물 삭제처리 리스트 확인 테스트")
+    @Test
+    void deleteAll_답변_게시물_삭제테스트() {
+        Answers answers = new Answers();
+
+        // when
+        answers.add(A1);
+        answers.add(A3);
+        List<DeleteHistory> deleteHistories = answers.deleteAll();
+        // then
+        assertThat(deleteHistories.size()).isEqualTo(2);
+    }
+
+}
