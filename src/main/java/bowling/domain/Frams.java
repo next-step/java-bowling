12deca752e1a23d3426befe8cbc3f1a979cfee39
meme2367 <meme91322367@gmail.com>
2021-04-14 15:58:18 +0900

    feat : Frame들을 담을 Frams 생성

---
+++ src/main/java/bowling/domain/Frams.java
@@ -0,0 +1,10 @@
+package bowling.domain;
+
+import bowling.domain.frame.Frame;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Frams {
+  List<Frame> frams = new ArrayList<>();
+
+}
