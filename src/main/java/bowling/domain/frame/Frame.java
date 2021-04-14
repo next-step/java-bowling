c0774914cb6db10e579cf37c38ffc08b955d9c85
meme2367 <meme91322367@gmail.com>
2021-04-14 17:42:12 +0900

    feat : 게임 진행 횟수 확인

--- src/main/java/bowling/domain/frame/Frame.java
+++ src/main/java/bowling/domain/frame/Frame.java
@@ -6,10 +6,19 @@ public abstract class Frame {
 
   private int playCount;
   private State state;
+  private static final String END_GAME = "더이상 게임 진행할 수 없습니다.";
+  private static final String WRONG_PIN_COUNT = "잘못된 수를 입력했습니다.";
 
   public Frame(int playCount, State state) {
+    validatePlayCount(playCount);
     this.playCount = playCount;
     this.state = state;
+  }
+
+  private void validatePlayCount(int playCount) {
+    if (playCount < 1 || playCount > 11) {
+      throw new IllegalArgumentException(END_GAME);
+    }
   }
 
   public abstract Frame next();
@@ -21,7 +30,7 @@ public abstract class Frame {
 
   private void validatePinCount(int pinCount) {
     if (pinCount < 0 || pinCount > 10) {
-      throw new IllegalArgumentException();
+      throw new IllegalArgumentException(WRONG_PIN_COUNT);
     }
   }
 
--- src/test/java/bowling/NormalFrameTest.java
+++ src/test/java/bowling/NormalFrameTest.java
@@ -5,11 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import bowling.domain.frame.Frame;
 import bowling.domain.frame.NormalFrame;
-import bowling.domain.state.FirstGutter;
-import bowling.domain.state.Hit;
 import bowling.domain.state.Ready;
-import bowling.domain.state.Spare;
-import bowling.domain.state.Strike;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 
@@ -21,6 +17,14 @@ public class NormalFrameTest {
     assertThatThrownBy(() -> {
       NormalFrame normalFrame = NormalFrame.createFirst();
       normalFrame.play(11);
+    }).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  @DisplayName("게임 진행 횟수 확인")
+  public void validatePlayCount() {
+    assertThatThrownBy(() -> {
+      NormalFrame normalFrame = new NormalFrame(12, new Ready());
     }).isInstanceOf(IllegalArgumentException.class);
   }
 
