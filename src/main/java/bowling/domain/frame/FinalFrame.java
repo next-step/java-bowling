a73cf311661a7e6610d234cd2065414c649a2206
meme2367 <meme91322367@gmail.com>
2021-04-14 17:56:52 +0900

    fix : finalFrame의 next 메서드 수정

--- src/main/java/bowling/domain/frame/FinalFrame.java
+++ src/main/java/bowling/domain/frame/FinalFrame.java
@@ -5,13 +5,16 @@ import bowling.domain.state.State;
 
 public class FinalFrame extends Frame {
 
+  private static boolean end = false;
+
   public FinalFrame(int playCount, State state) {
     super(playCount, state);
   }
 
   @Override
   public Frame next() {
-    if (getState().isBonus()) {
+    if (getState().isBonus() && !end) {
+      end = true;
       return new FinalFrame(10, new Ready());
     }
     return this;
--- src/main/java/bowling/domain/state/Strike.java
+++ src/main/java/bowling/domain/state/Strike.java
@@ -1,10 +1,11 @@
 package bowling.domain.state;
 
 public class Strike implements  State {
+  private static final String END = "더이상 진행할 수 없습니다.";
 
   @Override
   public State play(int pinCount) {
-    return null;
+    throw new IllegalArgumentException(END);
   }
 
   @Override
