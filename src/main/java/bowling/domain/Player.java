651220b8f42b284985b9d29a7d65501ad503bd91
meme2367 <meme91322367@gmail.com>
2021-04-14 13:03:13 +0900

    Player 테스트 코드 및 Player 클래스 작성

---
+++ src/main/java/bowling/domain/Player.java
@@ -0,0 +1,23 @@
+package bowling.domain;
+
+import java.util.regex.Pattern;
+
+public class Player {
+
+  private final String name;
+  private final static String INVALID_PATTERN_REGEX = "^[a-zA-Z]*$";
+  private final static Pattern pattern = Pattern.compile(INVALID_PATTERN_REGEX);
+  private final static String INVALID_NAME = "잘못된 이름입니다.";
+
+  public Player(String name) {
+    validateName(name);
+    this.name = name;
+  }
+
+  private void validateName(String name) {
+    if (name.length() > 3 || !pattern.matcher(name).matches()) {
+      throw new IllegalArgumentException(INVALID_NAME);
+    }
+  }
+
+}
