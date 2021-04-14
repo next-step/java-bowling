46f3e608db12f7126cf43b105156fa262fb9577a
623nana <6230nana@gmail.com>
2021-04-14 21:43:56 +0900

    refactor: DeleteHistories 테스트 수정

--- src/test/java/qna/domain/DeleteHistoriesTest.java
+++ src/test/java/qna/domain/DeleteHistoriesTest.java
@@ -1,39 +1,23 @@
-//package qna.domain;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static qna.domain.QuestionTest.Q1;
-//import static qna.domain.QuestionTest.Q2;
-//
-//import java.time.LocalDateTime;
-//import java.util.Arrays;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//public class DeleteHistoriesTest {
-//
-//  private DeleteHistories deleteHistories;
-//
-//  @BeforeEach
-//  void setUp() {
-//    deleteHistories = new DeleteHistories();
-//  }
-//
-//  @Test
-//  void add_history_test() {
-//    deleteHistories.add(new DeleteHistory(Q1, LocalDateTime.now()));
-//
-//    int size = deleteHistories.history().size();
-//
-//    assertThat(size).isEqualTo(1);
-//  }
-//
-//  @Test
-//  void addAll_history_test() {
-//    deleteHistories.addAll(Arrays.asList(new DeleteHistory(Q1, LocalDateTime.now()), new DeleteHistory(Q2,LocalDateTime.now())));
-//
-//    int size = deleteHistories.history().size();
-//
-//    assertThat(size).isEqualTo(2);
-//  }
-//
-//}
+package qna.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static qna.domain.QuestionTest.Q1;
+import static qna.domain.QuestionTest.Q2;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DeleteHistoriesTest {
+
+  @Test
+  @DisplayName("[DeleteHistories] DeleteHistories 생성 테스트")
+  void create_deleteHistories_test() {
+    DeleteHistories deleteHistories = new DeleteHistories(Arrays.asList(new DeleteHistory(Q1, LocalDateTime.now()), new DeleteHistory(Q2,LocalDateTime.now())));
+
+    int size = deleteHistories.histories().size();
+
+    assertThat(size).isEqualTo(2);
+  }
+}
