bdf5c60f5839af6f52e79d606e3c3daae74c0f90
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-04-15 23:10:07 +0900

    fix(PlayerTest) : 테스트 클래스 명 변경

---
+++ src/test/java/bowling/domain/PlayerTest.java
@@ -0,0 +1,16 @@
+package bowling.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+    @DisplayName("플레이어 이름은 3자리여야 한다.")
+    @Test
+    void case1() {
+        Assertions.assertThatThrownBy(() -> {
+            Player.of("4444");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}
