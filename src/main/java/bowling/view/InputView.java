e4caaa9de91f28f80b893417310d4d6cb2bbb63e
623nana <6230nana@gmail.com>
2021-04-15 22:24:23 +0900

    feat: 참가자의 이름을 입력 받는 기능

---
+++ src/main/java/bowling/view/InputView.java
@@ -0,0 +1,11 @@
+package bowling.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+  public static String inputOfPlayerName() {
+    System.out.println("플레이어 이름은(3 english letters)?:");
+    return new Scanner(System.in).nextLine();
+  }
+}
