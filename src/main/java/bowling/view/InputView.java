148d0c7730538330b936a6ee7c3af9f81e8c5e2d
623nana <6230nana@gmail.com>
2021-04-15 22:25:43 +0900

    fix: 안내 문구 줄바꿈 삭제

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InputView {
 
   public static String inputOfPlayerName() {
-    System.out.println("플레이어 이름은(3 english letters)?:");
+    System.out.print("플레이어 이름은(3 english letters)?: ");
     return new Scanner(System.in).nextLine();
   }
 }
