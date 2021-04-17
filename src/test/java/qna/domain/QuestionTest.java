ff43d312532a98d6be7513b9f279b6e00bbb013f
Sooncheol <ryxhdrnrmf@gmail.com>
2021-04-17 22:42:05 +0900

    refacotr: QuestionTest가 통과하도록 Question.delete() 구현

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,5 +1,7 @@
 package qna.domain;
 
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import qna.CannotDeleteException;
@@ -11,30 +13,38 @@ public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
 
-    @Test
-    @DisplayName("삭제하기 전의 질문 삭제 상태 확인 테스트")
-    public void 삭제전_상태_테스트() {
-        assertThat(Q1.isDeleted()).isEqualTo(false);
+
+    @BeforeEach
+    public void init() {
+        Q1.addAnswer(new Answer(UserTest.JAVAJIGI,Q1,"contents1"));
+        Q2.addAnswer(new Answer(UserTest.JAVAJIGI,Q2,"contents2"));
     }
 
     @Test
-    @DisplayName("삭제하기 전의 질문 삭제 상태 확인 테스트")
+    @DisplayName("질문 작성자가 아닐 때의 삭제 불가 테스트")
     public void 질문_작성자_테스트() {
         assertThatThrownBy(() -> Q1.delete(UserTest.SANJIGI))
-                .isInstanceOf(CannotDeleteException)
+                .isInstanceOf(CannotDeleteException.class)
                 .hasMessage("질문을 삭제할 권한이 없습니다.");
     }
 
     @Test
+    @DisplayName("답변 작성자 중에 질문 작성자가 아닌 사람이 있을 때의 삭제 불가 테스트")
     public void 답변_작성자_테스트() {
-        assertThatThrownBy(() -> Q1.delete(UserTest.SANJIGI))
-                .isInstanceOf(CannotDeleteException)
+        assertThatThrownBy(() -> Q2.delete(UserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class)
                 .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
     }
 
     @Test
-    public void 삭제후_상태_테스트() {
-        Q1.delete(UserTest.JAVAJIGI);
-        assertThat(Q1.isDeleted()).isEqualTo(true);
+    @DisplayName("삭제 전후의 질문 삭제 상태 확인 테스트")
+    public void 삭제전후_상태_테스트() {
+        assertThat(Q1.isDeleted()).isEqualTo(false);
+        try {
+            Q1.delete(UserTest.JAVAJIGI);
+            assertThat(Q1.isDeleted()).isEqualTo(true);
+        } catch (CannotDeleteException e) {
+            e.printStackTrace();
+        }
     }
 }
