0872c1287b92b897f7a653c58a0bca8648fd48d8
hwangjeonghyeon1 <deert202@naver.com>
2021-04-18 00:37:36 +0900

    [feat] 10라운드 프레임 클래스

--- src/main/java/bowling/domain/frame/FinalFrame.java
+++ src/main/java/bowling/domain/frame/FinalFrame.java
@@ -1,6 +1,6 @@
-package bowling.domain;
+package bowling.domain.frame;
 
-import static bowling.domain.NormalFrame.MAX_PITCH;
+import static bowling.domain.frame.NormalFrame.MAX_PITCH;
 
 public class FinalFrame extends Frame {
 
@@ -26,12 +26,12 @@ public class FinalFrame extends Frame {
     }
 
     private void validatePinCount(int pinCount) {
-        if ((pinCountSum() + pinCount) > Pin.MAX_PIN_COUNT) {
-            throw new IllegalArgumentException("핀의 개수가 10개를 넘을 수 없습니다.");
+        if ((pinCountSum() + pinCount) > MAX_FINAL_PIN_COUNT) {
+            throw new IllegalArgumentException("핀의 개수가 30개를 넘을 수 없습니다.");
         }
 
-        if ((getPinsSize() + 1) > MAX_PITCH) {
-            throw new IllegalArgumentException("최대 2회 투구 가능합니다.");
+        if ((getPinsSize() + 1) > BONUS_PITCH) {
+            throw new IllegalArgumentException("최대 3회 투구 가능합니다.");
         }
     }
 
@@ -42,12 +42,16 @@ public class FinalFrame extends Frame {
 
     @Override
     boolean isLast() {
+        return !isNextPitch();
+    }
+
+    private boolean isNextPitch() {
         if (getPinsSize() == FIRST_PITCH) {
-            return false;
+            return true;
         }
 
         if (getPinsSize() == SECOND_PITCH) {
-            return pinCountSum() < Pin.MAX_PIN_COUNT;
+            return pinCountSum() >= Pin.MAX_PIN_COUNT;
         }
 
         return getPinsSize() != BONUS_PITCH;
