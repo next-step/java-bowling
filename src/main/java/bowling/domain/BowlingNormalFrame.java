77bfad6572cd48c6bb19e1192660a9eca9ef199c
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-04-18 02:20:53 +0900

    feat : 프레임 현재 점수 계산 코드 추가

--- src/main/java/bowling/domain/BowlingNormalFrame.java
+++ src/main/java/bowling/domain/BowlingNormalFrame.java
@@ -4,10 +4,15 @@ import bowling.dto.ScoreDto;
 
 public class BowlingNormalFrame extends BowlingFrame {
 
-    private final Score score;
+    private Score score;
 
-    public BowlingNormalFrame(Round round, Score score) {
+    private BowlingNormalFrame(Round round, Score score) {
         super(round);
+        this.score = score;
+    }
+
+    private BowlingNormalFrame(Round round, Score score, BowlingFrame nextFrame) {
+        super(round, nextFrame);
         this.score = score;
     }
 
@@ -23,22 +28,27 @@ public class BowlingNormalFrame extends BowlingFrame {
         return new BowlingNormalFrame(round, Score.of(Point.of(0), Point.of(0)));
     }
 
+    public static BowlingNormalFrame of(Round round, Score score) {
+        return new BowlingNormalFrame(round, score);
+    }
 
     @Override
     public BowlingFrame secondPitching(Point point) {
-        return new BowlingNormalFrame(round(), score.next(point));
+        score = score.next(point);
+        return this;
     }
 
     @Override
     public BowlingFrame firstPitching(Point point) {
-        return new BowlingNormalFrame(round(), Score.first(point));
+        score = Score.first(point);
+        return this;
     }
 
     @Override
     public BowlingFrame bonusPitching(Point point) {
         return new BowlingNormalFrame(round(), score.next(point));
     }
-    
+
     @Override
     public BowlingRole isType() {
         return score.type();
@@ -47,6 +57,24 @@ public class BowlingNormalFrame extends BowlingFrame {
     @Override
     public ScoreDto toDto() {
         return ScoreDto.of(score);
+    }
+
+    @Override
+    public int calculateOfScore() {
+        if (score.type() == BowlingRole.SPARE) {
+            Score nextFrameScore = getNextFrame().score();
+            return this.score.total() + nextFrameScore.firstPoint();
+        }
+        if (score.type() == BowlingRole.STRIKE) {
+            Score nextFrameScore = getNextFrame().score();
+            return score.total() + nextFrameScore.total();
+        }
+        return score.total();
+    }
+
+    @Override
+    public Score score() {
+        return score;
     }
 
 
