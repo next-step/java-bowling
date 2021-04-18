0da87d4df8e628a215f50de5fe1b78f887e3c3df
meme2367 <meme91322367@gmail.com>
2021-04-18 17:47:50 +0900

    refactor : Pin 테스트 코드 수정

---
+++ src/test/java/bowling/domain/PinTest.java
@@ -0,0 +1,33 @@
+package bowling.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+public class PinTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 15})
+    @DisplayName("친 핀의 개수는 0개 이상 10개 이하여야 한다.")
+    public void validatePinCount(int number) {
+        assertThatThrownBy(() -> {
+            new Pin(number);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("게임 종료된 핀 확인")
+    void isEnd() {
+        Pin pin = new Pin(10);
+        assertThat(pin.isEnd()).isTrue();
+    }
+}
