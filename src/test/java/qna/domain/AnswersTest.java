f6590bef4331f9584340d88dff0094cae0678467
Dion <ksundong@gmail.com>
2021-04-18 19:00:25 +0900

    test: 기본 생성자와 빈 List로 생성한 Answer가 같음을 검증하는 테스트 작성

--- src/test/java/qna/domain/AnswersTest.java
+++ src/test/java/qna/domain/AnswersTest.java
@@ -3,6 +3,8 @@ package qna.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AnswersTest {
@@ -11,5 +13,11 @@ class AnswersTest {
     @DisplayName("기본생성자로 생성된 Answers는 서로 같은 상태다.")
     void create() {
         assertThat(new Answers()).isEqualTo(new Answers());
+    }
+
+    @Test
+    @DisplayName("기본생성자로 생성된 Answers와 빈 List로 생성한 Answers는 서로 같은 상태다.")
+    void create_with_emptyList() {
+        assertThat(new Answers(new ArrayList<>())).isEqualTo(new Answers());
     }
 }
