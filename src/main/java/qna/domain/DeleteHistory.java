d0d64bbde0faf93745db49cdb871e3f7d45f833f
Dion <ksundong@gmail.com>
2021-04-18 17:16:38 +0900

    refactor: DeleteHistory 기본 생성자 protected로 변경

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -21,8 +21,7 @@ public class DeleteHistory {
 
     private LocalDateTime createDate = LocalDateTime.now();
 
-    public DeleteHistory() {
-    }
+    protected DeleteHistory() {}
 
     public DeleteHistory(ContentType contentType, Long contentId, User deletedBy, LocalDateTime createDate) {
         this.contentType = contentType;
