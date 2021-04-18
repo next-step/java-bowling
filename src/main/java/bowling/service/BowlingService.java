2d355d79e82533287936a0a99976ebfe8b0fdf0e
seokrae <kslbsh@gmail.com>
2021-04-18 17:21:18 +0900

    feat(BowlingService): 10 프레임에 대한 값을 확인 할 수 있는 메서드 생성

--- src/main/java/bowling/service/BowlingService.java
+++ src/main/java/bowling/service/BowlingService.java
@@ -2,11 +2,17 @@ package bowling.service;
 
 public class BowlingService {
 
+    private int frameNumber = 0;
+
     public boolean isLast() {
-        return false;
+        return frameNumber == 10;
     }
 
     public void bowl(String pins) {
 
+    }
+
+    public int tryingCount() {
+        return 0;
     }
 }
