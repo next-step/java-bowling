d967c5ccff630e05cb0503d8b36867740b5d6a61
Dion <ksundong@gmail.com>
2021-04-18 18:57:46 +0900

    feat: 기본 생성자로 생성시 동일함을 보장하도록 구현

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -7,6 +7,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.OrderBy;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Answers {
 
@@ -21,5 +22,18 @@ public class Answers {
 
     public Answers(List<Answer> answers) {
         this.answers = answers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Answers answers1 = (Answers) o;
+        return Objects.equals(answers, answers1.answers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(answers);
     }
 }
