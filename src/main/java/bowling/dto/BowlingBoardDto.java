1306d3b5a0834c34d77e5d8fe953b1c6940ace4a
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-04-18 15:49:41 +0900

    refactoring: 점수 List 추가

--- src/main/java/bowling/dto/BowlingBoardDto.java
+++ src/main/java/bowling/dto/BowlingBoardDto.java
@@ -9,18 +9,20 @@ import java.util.List;
 public class BowlingBoardDto {
     private final List<ScoreDto> scoreDtoList;
     private final ThrowsState throwsState;
+    private final List<Integer> framePoints;
 
-    private BowlingBoardDto(List<ScoreDto> scoreDtoList, ThrowsState throwsState) {
+    private BowlingBoardDto(List<ScoreDto> scoreDtoList, ThrowsState throwsState, List<Integer> framePoints) {
         this.scoreDtoList = scoreDtoList;
         this.throwsState = throwsState;
+        this.framePoints = framePoints;
     }
 
     public static BowlingBoardDto of(BowlingBoard bowlingBoard) {
-        return new BowlingBoardDto(bowlingBoard.toScoreDto(), bowlingBoard.state());
+        return new BowlingBoardDto(bowlingBoard.toScoreDto(), bowlingBoard.state(), bowlingBoard.calculate());
     }
 
     public static BowlingBoardDto of() {
-        return new BowlingBoardDto(new ArrayList<>(), ThrowsState.FIRST_THROWS);
+        return new BowlingBoardDto(new ArrayList<>(), ThrowsState.FIRST_THROWS, new ArrayList<>());
     }
 
     public List<ScoreDto> getScoreDtoList() {
@@ -29,5 +31,9 @@ public class BowlingBoardDto {
 
     public ThrowsState getThrowsState() {
         return throwsState;
+    }
+
+    public List<Integer> getFramePoint() {
+        return framePoints;
     }
 }
