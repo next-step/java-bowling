5f1a03446d1b658fb7475fa3ed9936be66d9d571
juniq <juniqlim@gmail.com>
2021-04-19 22:34:20 +0900

    test: 테스트 케이스 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -3,9 +3,12 @@ package qna.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static qna.domain.AnswerTest.A1;
+import static qna.domain.AnswerTest.A2;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Collections;
 import org.junit.jupiter.api.Test;
 import qna.CannotDeleteException;
 
@@ -19,15 +22,40 @@ public class QuestionTest {
     }
 
     @Test
+    void delete_내가_쓴_답변() {
+        Question question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        question.addAnswer(A1);
+        assertThatCode(() -> question.delete(UserTest.JAVAJIGI)).doesNotThrowAnyException();
+    }
+
+    @Test
     void delete_다른_사람이_쓴_글() {
         assertThatThrownBy(() -> Q1.delete(UserTest.SANJIGI)).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    void delete_내가_쓴_글에_다른_사람이_답변() {
+        Question question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        question.addAnswer(A2);
+        assertThatThrownBy(() -> question.delete(UserTest.SANJIGI)).isInstanceOf(CannotDeleteException.class);
     }
 
     @Test
     void delete_히스토리_생성() throws CannotDeleteException {
         Q1.delete(UserTest.JAVAJIGI);
         assertThat(Q1.deleteHistorys()).usingElementComparatorIgnoringFields("createDate")
-            .isEqualTo(Arrays.asList(
+            .isEqualTo(Collections.singletonList(
                 new DeleteHistory(ContentType.QUESTION, Q1.getId(), Q1.getWriter(), LocalDateTime.now())));
+    }
+
+    @Test
+    void delete_답변까지_히스토리_생성() throws CannotDeleteException {
+        Question question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        question.addAnswer(A1);
+        question.delete(UserTest.JAVAJIGI);
+        assertThat(question.deleteHistorys()).usingElementComparatorIgnoringFields("createDate")
+            .isEqualTo(Arrays.asList(
+                new DeleteHistory(ContentType.QUESTION, Q1.getId(), Q1.getWriter(), LocalDateTime.now()),
+                new DeleteHistory(ContentType.ANSWER, A1.getId(), A1.getWriter(), LocalDateTime.now())));
     }
 }
