bee5b5704c99192208eb46060193345beb63fd55
juniq <juniqlim@gmail.com>
2021-04-19 16:53:43 +0900

    test: 다른 사람이 쓴 질문 삭제

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,8 +1,10 @@
 package qna.domain;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
 
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
@@ -11,5 +13,10 @@ public class QuestionTest {
     @Test
     void delete() {
         assertThatCode(() -> Q1.delete(UserTest.JAVAJIGI)).doesNotThrowAnyException();
+    }
+
+    @Test
+    void delete_다른_사람이_쓴_글() {
+        assertThatThrownBy(() -> Q1.delete(UserTest.SANJIGI)).isInstanceOf(CannotDeleteException.class);
     }
 }
