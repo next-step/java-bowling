b9e625506e8c219e1e7484d2f2b6c59c7bc79c04
623nana <6230nana@gmail.com>
2021-04-20 13:27:55 +0900

    feat: 쓰러트린 핀 갯수 입력 받는 기능

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -1,5 +1,6 @@
 package bowling.view;
 
+import bowling.domain.Frames;
 import java.util.Scanner;
 
 public class InputView {
@@ -7,5 +8,10 @@ public class InputView {
   public static String inputOfPlayerName() {
     System.out.print("플레이어 이름은(3 english letters)?: ");
     return new Scanner(System.in).nextLine();
+  }
+
+  public static int inputCountOfHitPin(Frames frames) {
+    System.out.printf("%d프레임 투구 : ", frames.round());
+    return new Scanner(System.in).nextInt();
   }
 }
