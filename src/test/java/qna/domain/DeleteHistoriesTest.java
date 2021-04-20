777b0a43575efe0446f1337dc3700932758f141c
Dion <ksundong@gmail.com>
2021-04-20 15:53:39 +0900

    test: 기본생성자 빈 List 생성 비교하는 테스트 추가

--- src/test/java/qna/domain/DeleteHistoriesTest.java
+++ src/test/java/qna/domain/DeleteHistoriesTest.java
@@ -3,6 +3,8 @@ package qna.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DeleteHistoriesTest {
@@ -11,5 +13,11 @@ class DeleteHistoriesTest {
     @DisplayName("기본생성자로 생성된 DeleteHistories는 서로 같은 상태다.")
     void create() {
         assertThat(new DeleteHistories()).isEqualTo(new DeleteHistories());
+    }
+
+    @Test
+    @DisplayName("기본생성자로 생성된 DeleteHistories와 빈 List로 생성된 것은 서로 같은 상태다.")
+    void createWithEmptyList() {
+        assertThat(new DeleteHistories()).isEqualTo(new DeleteHistories(new ArrayList<>()));
     }
 }
