e82b7be7cce5fb604231eb2ceedd596d6bf7e46f
Dion <ksundong@gmail.com>
2021-04-20 15:52:19 +0900

    feat: DeleteHistories 생성 기능 구현

--- src/main/java/qna/domain/DeleteHistories.java
+++ src/main/java/qna/domain/DeleteHistories.java
@@ -2,6 +2,7 @@ package qna.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public final class DeleteHistories {
 
@@ -13,5 +14,18 @@ public final class DeleteHistories {
 
     public DeleteHistories(List<DeleteHistory> deleteHistories) {
         this.deleteHistories = deleteHistories;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DeleteHistories that = (DeleteHistories) o;
+        return Objects.equals(deleteHistories, that.deleteHistories);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(deleteHistories);
     }
 }
