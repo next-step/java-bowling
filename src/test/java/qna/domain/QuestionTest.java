eb50cc4303a92dbf950262d7fe8a981000d57e61
Dion <ksundong@gmail.com>
2021-04-20 15:38:13 +0900

    test: 질문 삭제시 답변도 삭제되는 상황 실패하는 테스트 작성

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -8,6 +8,7 @@ import qna.exception.CannotDeleteException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class QuestionTest {
 
@@ -59,5 +60,36 @@ class QuestionTest {
 
         // then
         assertThat(question.isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("질문자와 답변글의 모든 답변자가 같은 경우 삭제가 가능하다.")
+    void deleteWithSameOwner() {
+        // given
+        question = TestFixture.Q2;
+        Answer answer = new Answer(TestFixture.SANJIGI, question, "Answers Contents");
+        question.addAnswer(answer);
+
+        // when
+        question.delete(TestFixture.SANJIGI);
+
+        // then
+        assertAll(
+                () -> assertThat(question.isDeleted()).isTrue(),
+                () -> assertThat(answer.isDeleted()).isTrue()
+        );
+    }
+
+    @Test
+    @DisplayName("답변자가 다른 경우 삭제가 불가능하다.")
+    void deleteNotSameOwner() {
+        // given
+        question.addAnswer(TestFixture.A1);
+        question.addAnswer(TestFixture.A2);
+
+        // when
+        assertThatThrownBy(() -> question.delete(TestFixture.JAVAJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage(CannotDeleteException.DIFFERENT_USERS_ANSWER_CONTAINED);
     }
 }
