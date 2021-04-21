5724952a32730f24d7837199176cc24fb19fc771
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-21 17:32:00 +0900

    refactor : 질문 삭제로직 QnAService 클래스에서 Question 도메인 클래스로 이동

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,17 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    @DisplayName("질문을 삭제할 권한은 작성자만 가짐")
+    void deletePermission() {
+        Assertions.assertThrows(CannotDeleteException.class, () -> Q1.delete(UserTest.SANJIGI), "질문을 삭제할 권한이 없습니다.");
+    }
 }
