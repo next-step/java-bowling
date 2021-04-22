fe38c21f2a3a9adb54526bc5724db87889399d17
Donghui-Lee <ehdgml3206@naver.com>
2021-04-22 19:32:23 +0900

    test: 테스트 코드 추가

--- src/test/java/bowling/domain/state/BowlingPinTest.java
+++ src/test/java/bowling/domain/state/BowlingPinTest.java
@@ -1,9 +1,16 @@
 package bowling.domain.state;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BowlingPinTest {
+
+    @Test
+    void 생성_테스트() {
+        assertThat(BowlingPin.of(5)).isEqualTo(BowlingPin.of(5));
+    }
 
     @Test
     void 유효성_테스트() {
