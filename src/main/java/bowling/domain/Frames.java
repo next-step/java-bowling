b1065ae472c247e1908452d5e269b092db125258
Beenie93 <seongbeen93@naver.com>
2021-04-23 01:15:55 +0900

    feat: implement Frames Object

---
+++ src/main/java/bowling/domain/Frames.java
@@ -0,0 +1,52 @@
+package bowling.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Frames {
+    private static final int NORMAL_FRAME_INDEX_MAX = 8;
+    private static final int FIRST_FRAME_INDEX = 0;
+
+    private final List<Frame> frames;
+
+    public Frames() {
+        this(init());
+    }
+
+    private static List<Frame> init() {
+        List<Frame> frames = IntStream.range(0, NORMAL_FRAME_INDEX_MAX + 1)
+                .mapToObj(NormalFrame::new)
+                .collect(Collectors.toList());
+
+        frames.add(new FinalFrame());
+
+        return frames;
+    }
+
+    public Frames(List<Frame> frames) {
+        this.frames = frames;
+    }
+
+    public Frame first() {
+        return get(FIRST_FRAME_INDEX);
+    }
+
+    public boolean isAllFinished() {
+        return frames.stream()
+                .allMatch(Frame::isFinished);
+    }
+
+    public Frame get(int index) {
+        return frames.get(index);
+    }
+
+    public void update(Frame frame) {
+        frames.set(frame.index(), frame);
+    }
+
+    public List<Frame> list() {
+        return Collections.unmodifiableList(frames);
+    }
+}
