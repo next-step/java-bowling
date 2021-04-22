0d5ec90587d04d8e95fb4d48e6d4461991a84cde
Donghui-Lee <ehdgml3206@naver.com>
2021-04-23 01:58:40 +0900

    refactor: 테스트 성공을 위한 코드 수정

--- src/main/java/bowling/domain/Player.java
+++ src/main/java/bowling/domain/Player.java
@@ -1,5 +1,7 @@
 package bowling.domain;
 
+import java.util.Objects;
+
 public class Player {
     private final String name;
 
@@ -8,6 +10,28 @@ public class Player {
     }
 
     Player(String name) {
-        this.name = name;
+        this.name = validate(name.trim());
+    }
+
+    private String validate(String name) {
+        if (name.length() > 3 || name.isEmpty()) {
+            throw new IllegalArgumentException("Player 이름은 3글자 이하여야 합니다.");
+        }
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Player player = (Player)o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
