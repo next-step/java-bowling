c5cae6f53839ba79c3730929b3cf761d0626e5f9
Donghui-Lee <ehdgml3206@naver.com>
2021-04-22 10:30:53 +0900

    refactor: 테스트 성공을 위한 코드 수정

--- src/main/java/bowling/domain/state/BowlingPin.java
+++ src/main/java/bowling/domain/state/BowlingPin.java
@@ -1,5 +1,7 @@
 package bowling.domain.state;
 
+import java.util.Objects;
+
 public class BowlingPin {
     private static final int MAX_PIN_COUNT = 10;
     private static final int MIN_PIN_COUNT = 0;
@@ -7,6 +9,17 @@ public class BowlingPin {
     private final int pin;
 
     private BowlingPin(int pin) {
+        validate(pin);
         this.pin = pin;
+    }
+
+    public static BowlingPin of(int pin) {
+        return new BowlingPin(pin);
+    }
+
+    private void validate(int pin) {
+        if (pin < MIN_PIN_COUNT || pin > MAX_PIN_COUNT) {
+            throw new IllegalArgumentException("유효하지 않은 볼링핀 입니다.");
+        }
     }
 }
