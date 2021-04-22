30cf0cc21bd705f83453d10845029cd4efb30873
Jake <cocahack@naver.com>
2021-04-23 00:34:39 +0900

    feat: 프레임 생성 클래스 추가

---
+++ src/main/java/bowling/domain/engine/FrameCreator.java
@@ -0,0 +1,16 @@
+package bowling.domain.engine;
+
+import bowling.domain.concrete.FinalFrame;
+import bowling.domain.concrete.NormalFrame;
+
+public class FrameCreator {
+
+    private static final int MAX_FRAMES = 10;
+
+    private int count = 1;
+
+    public Frame create() {
+        return count++ < MAX_FRAMES ? new NormalFrame() : new FinalFrame();
+    }
+
+}
