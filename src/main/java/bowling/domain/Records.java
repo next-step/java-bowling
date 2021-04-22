e825aa62d5e01e9f721002b3d09b9f87b8d65511
Jake <cocahack@naver.com>
2021-04-22 23:45:56 +0900

    feat: records 클래스 구현

---
+++ src/main/java/bowling/domain/Records.java
@@ -0,0 +1,30 @@
+package bowling.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Records {
+
+    private final List<PitchResult> results = new ArrayList<>();
+
+    public void add(PitchResult pitchResult) {
+        results.add(pitchResult);
+    }
+
+    public boolean isStrike() {
+        return !results.isEmpty() && results.get(0).getValue() == Pins.MAXIMUM_PINS;
+    }
+
+    public boolean isSpare() {
+        return results.size() == 2 && results.stream().mapToInt(PitchResult::getValue).sum() == Pins.MAXIMUM_PINS;
+    }
+
+    public boolean isMissed() {
+        return results.size() == 2 && results.stream().mapToInt(PitchResult::getValue).sum() < Pins.MAXIMUM_PINS;
+    }
+
+    public int throwCounts() {
+        return results.size();
+    }
+
+}
---
+++ src/test/java/bowling/domain/RecordsTest.java
@@ -0,0 +1,51 @@
+package bowling.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class RecordsTest {
+
+    @Test
+    @DisplayName("저장된 기록이 스트라이크인지 확인한다.")
+    void existsStrikeInRecords() {
+        Records records = new Records();
+        records.add(new PitchResult(10));
+
+        assertAll(
+            () -> assertThat(records.isStrike()).isTrue(),
+            () -> assertThat(records.isSpare()).isFalse(),
+            () -> assertThat(records.isMissed()).isFalse()
+        );
+    }
+
+    @Test
+    @DisplayName("두 번의 시도로 10 개의 핀이 쓰러졌다면 스페어 처리가 된 것이다.")
+    void checkIfSpare() {
+        Records records = new Records();
+        records.add(new PitchResult(8));
+        records.add(new PitchResult(2));
+
+        assertAll(
+            () -> assertThat(records.isStrike()).isFalse(),
+            () -> assertThat(records.isSpare()).isTrue(),
+            () -> assertThat(records.isMissed()).isFalse()
+        );
+    }
+
+    @Test
+    @DisplayName("두 번의 시도를 다 쓰고도 핀이 남았다면 미스 처리가 된 것이다.")
+    void checkIfMissed() {
+        Records records = new Records();
+        records.add(new PitchResult(8));
+        records.add(new PitchResult(1));
+
+        assertAll(
+            () -> assertThat(records.isSpare()).isFalse(),
+            () -> assertThat(records.isMissed()).isTrue()
+        );
+    }
+
+}
