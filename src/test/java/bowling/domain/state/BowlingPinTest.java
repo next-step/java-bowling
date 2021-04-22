206f8dec23f63afcf4f337edf7f45bfb736f1d84
Donghui-Lee <ehdgml3206@naver.com>
2021-04-23 05:55:32 +0900

    feat: 상태에 따른 볼링핀 출력 기능 추가

--- src/test/java/bowling/domain/state/BowlingPinTest.java
+++ src/test/java/bowling/domain/state/BowlingPinTest.java
@@ -17,4 +17,28 @@ public class BowlingPinTest {
         Assertions.assertThrows(IllegalArgumentException.class, () -> BowlingPin.of(-1));
         Assertions.assertThrows(IllegalArgumentException.class, () -> BowlingPin.of(11));
     }
+
+    @Test
+    void 최대_볼링핀_테스트() {
+        assertThat(BowlingPin.of(10).isMax()).isTrue();
+        assertThat(BowlingPin.of(9).isMax()).isFalse();
+    }
+
+    @Test
+    void 합계_테스트() {
+        assertThat(BowlingPin.of(3).sum(BowlingPin.of(7))).isEqualTo(BowlingPin.of(10));
+        assertThat(BowlingPin.of(2).sum(BowlingPin.of(7))).isEqualTo(BowlingPin.of(9));
+    }
+
+    @Test
+    void 합계_유효성_테스트() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> BowlingPin.of(3).sum(BowlingPin.of(8)));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> BowlingPin.of(5).sum(BowlingPin.of(8)));
+    }
+
+    @Test
+    void 볼링핀_출력_테스트() {
+        assertThat(BowlingPin.of(7).score()).isEqualTo("7");
+        assertThat(BowlingPin.of(0).score()).isEqualTo("-");
+    }
 }
--- src/test/java/bowling/domain/state/StateTest.java
+++ src/test/java/bowling/domain/state/StateTest.java
@@ -24,4 +24,22 @@ public class StateTest {
     void 스트라이크_생성_테스트() {
         assertThat(State.newState(BowlingPin.of(10))).isInstanceOf(Strike.class);
     }
+
+    @Test
+    void 미스_출력_테스트() {
+        assertThat(State.newState(BowlingPin.of(3)).score()).isEqualTo("3");
+        assertThat(State.newState(BowlingPin.of(0), BowlingPin.of(9)).totalScore()).isEqualTo("-|9");
+    }
+
+    @Test
+    void 스페어_출력_테스트() {
+        assertThat(State.newState(BowlingPin.of(0), BowlingPin.of(10)).totalScore()).isEqualTo("-|/");
+        assertThat(State.newState(BowlingPin.of(3), BowlingPin.of(7)).totalScore()).isEqualTo("3|/");
+    }
+
+    @Test
+    void 스트라이크_출력_테스트() {
+        assertThat(State.newState(BowlingPin.of(10)).score()).isEqualTo("X");
+        assertThat(State.newState(BowlingPin.of(10)).totalScore()).isEqualTo("X");
+    }
 }
