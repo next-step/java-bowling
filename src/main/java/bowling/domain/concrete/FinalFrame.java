cb5ae847383ec722ad47f8e0326f01335bb2ec95
Jake <cocahack@naver.com>
2021-04-23 00:18:20 +0900

    refactor: 프레임 인터페이스의 구현체로 변경

--- src/main/java/bowling/domain/concrete/FinalFrame.java
+++ src/main/java/bowling/domain/concrete/FinalFrame.java
@@ -1,16 +1,18 @@
 package bowling.domain.concrete;
 
+import bowling.domain.engine.Frame;
 import bowling.domain.engine.Pins;
 import bowling.domain.engine.PitchResult;
 import bowling.domain.engine.Records;
 
-public class FinalFrame {
+public class FinalFrame implements Frame {
 
     private static final int MAXIMUM_THROW_CHANCES = 3;
 
     private final Records records = new Records();
     private Pins pins = new Pins();
 
+    @Override
     public void throwBall(PitchResult pitchResult) {
         if (isEnded()) {
             throw new IllegalStateException("이미 프레임이 종료된 상태입니다.");
@@ -24,6 +26,7 @@ public class FinalFrame {
         }
     }
 
+    @Override
     public boolean isEnded() {
         return records.isMissed() || records.throwCounts() == MAXIMUM_THROW_CHANCES;
     }
--- src/main/java/bowling/domain/concrete/NormalFrame.java
+++ src/main/java/bowling/domain/concrete/NormalFrame.java
@@ -1,14 +1,16 @@
 package bowling.domain.concrete;
 
+import bowling.domain.engine.Frame;
 import bowling.domain.engine.Pins;
 import bowling.domain.engine.PitchResult;
 import bowling.domain.engine.Records;
 
-public class NormalFrame {
+public class NormalFrame implements Frame {
 
     private final Pins pins = new Pins();
     private final Records records = new Records();
 
+    @Override
     public void throwBall(PitchResult pitchResult) {
         if (isEnded()) {
             throw new IllegalStateException("이미 프레임이 종료된 상태입니다.");
@@ -18,6 +20,7 @@ public class NormalFrame {
         pins.knockDown(pitchResult);
     }
 
+    @Override
     public boolean isEnded() {
         return records.isStrike() || records.isMissed() || records.isSpare();
     }
