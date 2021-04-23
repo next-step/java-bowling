8f5ae2deab7085a1bb8ec53b8523a4718e3a1a7d
jaystar13 <astarisk2@gmail.com>
2021-04-23 15:00:21 +0900

    refactor: 권한체크 타입을 void로 변경

--- src/main/java/qna/domain/AnswerDeleteCheck.java
+++ src/main/java/qna/domain/AnswerDeleteCheck.java
@@ -13,10 +13,9 @@ public class AnswerDeleteCheck {
         return new AnswerDeleteCheck(answer);
     }
 
-    public boolean authCheck(User loginUser) throws CannotDeleteException {
+    public void authCheck(User loginUser) throws CannotDeleteException {
         if (!answer.isOwner(loginUser)) {
             throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
         }
-        return true;
     }
 }
--- src/main/java/qna/domain/QuestionDeleteCheck.java
+++ src/main/java/qna/domain/QuestionDeleteCheck.java
@@ -13,10 +13,9 @@ public class QuestionDeleteCheck {
         return new QuestionDeleteCheck(question);
     }
 
-    public boolean authCheck(User loginUser) throws CannotDeleteException {
+    public void authCheck(User loginUser) throws CannotDeleteException {
         if (!question.isOwner(loginUser)) {
             throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
         }
-        return true;
     }
 }
