8987c15f42cc120810d0fb4ad3a3245d643dbd34
Beenie93 <seongbeen93@naver.com>
2021-04-24 18:28:07 +0900

    refactor: refactor Mark

--- src/main/java/bowling/domain/Mark.java
+++ src/main/java/bowling/domain/Mark.java
@@ -1,9 +1,13 @@
 package bowling.domain;
 
+import java.util.List;
+
 public enum Mark {
     STRIKE ("X"),
     SPARE ("/"),
-    GUTTER ("-");
+    GUTTER ("-"),
+    DELIMETER("|"),
+    EMPTY ("");
 
     public static final int PIN_COUNT_MAX = 10;
 
@@ -13,7 +17,23 @@ public enum Mark {
         this.Mark = Mark;
     }
 
-    public static String firstMark(PinCount pinCount) {
+    public static String convert(List<PinCount> pinCounts) {
+        if (pinCounts.isEmpty()) {
+            return EMPTY.Mark;
+        }
+
+        if (pinCounts.size() == 1) {
+            return firstMark(pinCounts.get(0));
+        }
+
+        if (pinCounts.size() == 2) {
+            return secondMark(pinCounts.get(0), pinCounts.get(1));
+        }
+
+        return thirdMark(pinCounts.get(0), pinCounts.get(1), pinCounts.get(2));
+    }
+
+    protected static String firstMark(PinCount pinCount) {
         if (pinCount.isStrike()) {
             return STRIKE.Mark;
         }
@@ -25,38 +45,43 @@ public enum Mark {
         return String.valueOf(pinCount.value());
     }
 
-    public static String secondMark(PinCount firstPinCount, PinCount secondPinCount) {
+    protected static String secondMark(PinCount firstPinCount, PinCount secondPinCount) {
+        String firstMark = firstMark(firstPinCount);
+
         if (firstPinCount.isStrike() && secondPinCount.isStrike()) {
-            return STRIKE.Mark;
+            return firstMark + DELIMETER.Mark + STRIKE.Mark;
         }
 
         if (!firstPinCount.isStrike() && firstPinCount.plus(secondPinCount) == PIN_COUNT_MAX) {
-            return SPARE.Mark;
+            return firstMark + DELIMETER.Mark + SPARE.Mark;
         }
 
         if (secondPinCount.isGutter()) {
-            return GUTTER.Mark;
+            return firstMark + DELIMETER.Mark + GUTTER.Mark;
         }
 
-        return String.valueOf(secondPinCount.value());
+        return firstMark + DELIMETER.Mark + secondPinCount.value();
     }
 
-    public static String thirdMark(PinCount firstPinCount, PinCount secondPinCount, PinCount thirdPinCount) {
+    protected static String thirdMark(PinCount firstPinCount, PinCount secondPinCount, PinCount thirdPinCount) {
+        String secondMark = secondMark(firstPinCount, secondPinCount);
+
         if (isAllStrike(firstPinCount, secondPinCount, thirdPinCount)
                 || isOnlyThirdStrike(firstPinCount, secondPinCount, thirdPinCount)) {
-            return STRIKE.Mark;
+            return secondMark + DELIMETER.Mark + STRIKE.Mark;
         }
 
         if (isThirdSpare(firstPinCount, secondPinCount, thirdPinCount)) {
-            return SPARE.Mark;
+            return secondMark + DELIMETER.Mark + SPARE.Mark;
         }
 
         if (isThirdGutterWithFirstStrike(firstPinCount, secondPinCount, thirdPinCount)
-                || isThirdGutterWithSecondSpare(firstPinCount, secondPinCount, thirdPinCount)) {
-            return GUTTER.Mark;
+                || isThirdGutterWithSecondSpare(firstPinCount, secondPinCount, thirdPinCount)
+                || isThirdGutterWithFirstAndSecondStrike(firstPinCount, secondPinCount, thirdPinCount)){
+            return secondMark + DELIMETER.Mark + GUTTER.Mark;
         }
 
-        return String.valueOf(thirdPinCount.value());
+        return secondMark + DELIMETER.Mark + thirdPinCount.value();
     }
 
     private static boolean isAllStrike(PinCount firstPinCount, PinCount secondPinCount, PinCount thirdPinCount) {
@@ -77,5 +102,9 @@ public enum Mark {
 
     private static boolean isThirdGutterWithSecondSpare(PinCount firstPinCount, PinCount secondPinCount, PinCount thirdPinCount) {
         return !firstPinCount.isStrike() && firstPinCount.plus(secondPinCount) == PIN_COUNT_MAX && thirdPinCount.isGutter();
+    }
+
+    private static boolean isThirdGutterWithFirstAndSecondStrike(PinCount firstPinCount, PinCount secondPinCount, PinCount thirdPinCount) {
+        return firstPinCount.isStrike() && secondPinCount.isStrike() && thirdPinCount.isGutter();
     }
 }
