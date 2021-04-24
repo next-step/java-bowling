2ec3dcafaa699249bf253898d15c27da1d26ebf0
623nana <6230nana@gmail.com>
2021-04-25 00:03:56 +0900

    refactor: 기본 생성자 자동으로 생성되므로 삭제

--- src/main/java/bowling/domain/NormalFrame.java
+++ src/main/java/bowling/domain/NormalFrame.java
@@ -5,9 +5,6 @@ public class NormalFrame extends Frame {
   private static final int NORMAL_FRAME_MAX_SIZE = 2;
   private static final int MAX_PIN_COUNT = 10;
 
-  public NormalFrame() {
-  }
-
   protected void validateHitPin(int countOfHitPin) {
     if (pins.totalHitPin() + countOfHitPin > MAX_PIN_COUNT) {
       throw new IllegalArgumentException("핀의 갯수는 " + MAX_PIN_COUNT + "개를 넘을 수 없습니다.");
