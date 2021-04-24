a6a32097467fdc49242651b4e3f448a9fca65425
623nana <6230nana@gmail.com>
2021-04-24 13:48:43 +0900

    test: States 테스트

---
+++ src/test/java/bowling/domain/StatesTest.java
@@ -0,0 +1,20 @@
+package bowling.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StatesTest {
+
+  @Test
+  @DisplayName("[States] 핀의 상태로 현재 해당 프레임의 상태 및 점수를 추가")
+  void states_add_test() {
+    States states = new States();
+    states.add(ScoreSymbol.STRIKE.mark());
+
+    int size = states.states().size();
+
+    assertThat(size).isEqualTo(1);
+  }
+}
