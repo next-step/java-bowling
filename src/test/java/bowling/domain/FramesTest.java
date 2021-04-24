ed89597dafcf2703a7814eac56c40261dc4fdcf3
623nana <6230nana@gmail.com>
2021-04-24 23:33:03 +0900

    test: 구조 변경에 따른 테스트 수정

--- src/test/java/bowling/domain/FramesTest.java
+++ src/test/java/bowling/domain/FramesTest.java
@@ -11,20 +11,7 @@ public class FramesTest {
   @DisplayName("[Frames] Frames init 테스트")
   void init_frames_test() {
     Frames frames = Frames.init();
-
     int frameSize = frames.frames().size();
-
-    assertThat(frameSize).isEqualTo(1);
-  }
-
-  @Test
-  @DisplayName("[Frames] 공을 던진 후 다음 프레임이 만들어지는지 테스트")
-  void add_frame_test() {
-    Frames frames = Frames.init();
-    frames.throwBall(10);
-
-    int frameSize = frames.frames().size();
-
-    assertThat(frameSize).isEqualTo(2);
+    assertThat(frameSize).isEqualTo(10);
   }
 }
