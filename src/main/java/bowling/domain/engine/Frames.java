189835111b567a3182aee445ecb26f0941c4befd
Jake <cocahack@naver.com>
2021-04-24 14:21:47 +0900

    feat: Frame 일급 컬렉션 추가

---
+++ src/main/java/bowling/domain/engine/Frames.java
@@ -0,0 +1,49 @@
+package bowling.domain.engine;
+
+import bowling.dto.FramesDto;
+import bowling.util.ListUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
+public class Frames {
+
+    private static final int TOTAL_FRAMES = 10;
+
+    private final List<Frame> frames = new ArrayList<>();
+
+    private Frames() {}
+
+    public static Frames initialize(Frame firstFrame) {
+        Frames frames = new Frames();
+        frames.add(firstFrame);
+
+        return frames;
+    }
+
+    public void add(Frame frame) {
+        frames.add(frame);
+    }
+
+    public Frame getLast() {
+        return ListUtils.getLastElement(frames);
+    }
+
+    public int getNextFrameNumber() {
+        int nextFrameNumber = frames.size() + (getLast().isEnded() ? 1 : 0);
+
+        return nextFrameNumber > TOTAL_FRAMES ? -1 : nextFrameNumber;
+    }
+
+    public boolean isAllFrameEnded() {
+        return frames.size() == TOTAL_FRAMES && getLast().isEnded();
+    }
+
+    public FramesDto export() {
+        return frames.stream().map(Frame::export).collect(collectingAndThen(toList(), FramesDto::new));
+    }
+
+}
---
+++ src/test/java/bowling/domain/engine/FramesTest.java
@@ -0,0 +1,61 @@
+package bowling.domain.engine;
+
+import bowling.domain.concrete.FinalFrame;
+import bowling.domain.concrete.NormalFrame;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FramesTest {
+
+    static Frames allFinishedFrames() {
+        Frames frames = Frames.initialize(new NormalFrame());
+        frames.getLast().throwBall(PitchResult.wrap(10));
+
+        for (int i = 0; i < 8; i++){
+            frames.add(new NormalFrame());
+            frames.getLast().throwBall(PitchResult.wrap(10));
+        }
+
+        frames.add(new FinalFrame());
+        frames.getLast().throwBall(PitchResult.wrap(7));
+        frames.getLast().throwBall(PitchResult.wrap(2));
+
+
+        return frames;
+    }
+
+    @Test
+    @DisplayName("다음 프레임의 번호를 가져온다.")
+    void getNextFrameNumber() {
+        Frames frames = Frames.initialize(new NormalFrame());
+        assertThat(frames.getNextFrameNumber()).isEqualTo(1);
+
+        frames.getLast().throwBall(PitchResult.wrap(10));
+
+        assertThat(frames.getNextFrameNumber()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("볼링 게임이 종료된 상태에서 다음 번호를 요청하면 -1 을 반환한다.")
+    void returnMinusIfGameIsEnded() {
+        assertThat(allFinishedFrames().getNextFrameNumber()).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("마지막 프레임을 가져온다.")
+    void getLastFrame() {
+        Frame firstFrame = new NormalFrame();
+        Frames frames = Frames.initialize(firstFrame);
+
+        assertThat(frames.getLast()).isSameAs(firstFrame);
+    }
+
+    @Test
+    @DisplayName("모든 프레임이 종료되면 true 를 반환한다.")
+    void returnTrueIfAllFrameIsEnded() {
+        assertThat(allFinishedFrames().isAllFrameEnded()).isTrue();
+    }
+    
+}
