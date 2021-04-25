d2d5a21d873af35264f5d9af9433644f7cd1379a
Dion <ksundong@gmail.com>
2021-04-25 14:18:21 +0900

    test: 사용자의 이름 유효한 문자 테스트 작성

---
+++ src/main/java/bowling/exception/NotAllowedPlayerNameCharacterException.java
@@ -0,0 +1,11 @@
+package bowling.exception;
+
+public final class NotAllowedPlayerNameCharacterException extends BowlingException {
+
+    public static final String NOT_ALLOWED_PLAYER_NAME_CHARACTER = "사용자의 이름은 영문 대소문자만 입력할 수 있습니다. 입력된 이름: ";
+
+    public NotAllowedPlayerNameCharacterException(String name) {
+        super(NOT_ALLOWED_PLAYER_NAME_CHARACTER + name);
+    }
+
+}
--- src/test/java/bowling/domain/PlayerNameTest.java
+++ src/test/java/bowling/domain/PlayerNameTest.java
@@ -1,8 +1,8 @@
 package bowling.domain;
 
+import bowling.exception.NotAllowedPlayerNameCharacterException;
 import bowling.exception.PlayerNameLengthException;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -11,12 +11,11 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PlayerNameTest {
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"KSD", "ksd"})
     @DisplayName("사용자의 이름을 입력받아 생성할 수 있다.")
-    void create() {
+    void create(final String name) {
         // given
-        final String name = "KSD";
-
         // when
         final PlayerName playerName = new PlayerName(name);
 
@@ -27,9 +26,18 @@ class PlayerNameTest {
     @ParameterizedTest
     @ValueSource(strings = {"", "ABCD"})
     @DisplayName("사용자의 이름은 1~3자여야 한다.")
-    void playerNameLength(final String name) {
-        assertThatThrownBy(() -> new PlayerName(name))
+    void playerNameLength(final String invalidName) {
+        assertThatThrownBy(() -> new PlayerName(invalidName))
                 .isInstanceOf(PlayerNameLengthException.class)
-                .hasMessage(PlayerNameLengthException.PLAYER_NAME_RANGE_MESSAGE + name);
+                .hasMessage(PlayerNameLengthException.PLAYER_NAME_RANGE_MESSAGE + invalidName);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"김선동", "123"})
+    @DisplayName("사용자의 이름으로 사용될 수 있는 문자는 영문 대소문자만 허용된다.")
+    void playerNameCharacter(final String invalidName) {
+        assertThatThrownBy(() -> new PlayerName(invalidName))
+                .isInstanceOf(NotAllowedPlayerNameCharacterException.class)
+                .hasMessage(NotAllowedPlayerNameCharacterException.NOT_ALLOWED_PLAYER_NAME_CHARACTER + invalidName);
     }
 }
