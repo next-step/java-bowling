58815b37e0b420070d7d5536eee893afdfbfafd7
Dion <ksundong@gmail.com>
2021-04-25 14:52:14 +0900

    feat: 핀의 개수를 입력받아 생성하는 기능 구현

--- src/main/java/bowling/domain/frame/Pin.java
+++ src/main/java/bowling/domain/frame/Pin.java
@@ -1,8 +1,25 @@
 package bowling.domain.frame;
 
+import java.util.Objects;
+
 public final class Pin {
 
-    public Pin(int pinCount) {
+    private final int pin;
 
+    public Pin(final int pin) {
+        this.pin = pin;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pin pin1 = (Pin) o;
+        return pin == pin1.pin;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pin);
     }
 }
