9491b0341d70d51d8dbabb34abd0f5ef5bff6166
Dion <ksundong@gmail.com>
2021-04-26 02:15:13 +0900

    refactor: 다시 개발하기 위해서 FrameScore 제거

--- src/main/java/bowling/domain/frame/FrameScore.java
+++ src/main/java/bowling/domain/frame/FrameScore.java
@@ -1,33 +1,4 @@
 package bowling.domain.frame;
 
-import bowling.domain.pin.Pin;
-
-import java.util.Objects;
-
 public final class FrameScore {
-
-    private final Pin pin;
-    private final FrameStatus frameStatus;
-
-    public FrameScore() {
-        this(new Pin(), FrameStatus.NONE);
-    }
-
-    public FrameScore(Pin pin, FrameStatus frameStatus) {
-        this.pin = pin;
-        this.frameStatus = frameStatus;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        FrameScore that = (FrameScore) o;
-        return Objects.equals(pin, that.pin) && frameStatus == that.frameStatus;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(pin, frameStatus);
-    }
 }
