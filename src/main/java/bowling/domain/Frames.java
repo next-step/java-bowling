6b1449c59489824f95d99dd2c0146d3148edadc4
PandaHun <gnsgnsek@gmail.com>
2021-04-25 23:21:40 +0900

    feat: Create Frames feature

--- src/main/java/bowling/domain/Frames.java
+++ src/main/java/bowling/domain/Frames.java
@@ -1,15 +1,34 @@
 package bowling.domain;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Frames {
 
-    public Frames() {
+    private static final int LAST_NORMAL_FRAME_SIZE = 10;
+
+    private final List<Frame> frames;
+    private Round round;
+
+    private Frames(List<Frame> frames) {
+        this.frames = frames;
+        this.round = Round.firstRound();
     }
 
     public static Frames init() {
-        return new Frames();
+        List<Frame> frames = generateNormalFrame();
+        frames.add(FinalFrame.from(Round.finalRound()));
+        return new Frames(frames);
+    }
+
+    private static List<Frame> generateNormalFrame() {
+        return IntStream.range(1, LAST_NORMAL_FRAME_SIZE)
+                .mapToObj(i -> NormalFrame.from(Round.from(i)))
+                .collect(Collectors.toList());
     }
 
     public int size() {
-        return -1;
+        return frames.size();
     }
 }
