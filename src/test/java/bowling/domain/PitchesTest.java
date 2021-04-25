3026afebc8b31e6147a1c12360f58c563a3116ff
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-25 18:08:32 +0900

    test : Pitches 클래스 add 메서드 테스트코드 작성

--- src/test/java/bowling/domain/PitchesTest.java
+++ src/test/java/bowling/domain/PitchesTest.java
@@ -3,7 +3,8 @@ package bowling.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 
 class PitchesTest {
 
@@ -69,5 +70,63 @@ class PitchesTest {
 
         // then
         assertThat(3).isEqualTo(pitches.spare());
+    }
+
+    @Test
+    @DisplayName("투구 실행 - null 입력")
+    void pitch_null() {
+        // given
+        Pitches pitches = new Pitches();
+
+        // when then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> pitches.add(null))
+                .withMessageMatching("투구 정보를 입력해 주세요.");
+    }
+
+    @Test
+    @DisplayName("핀 처리 횟수가 남은 횟수를 초과할 때")
+    void pitch_greaterThanSpareCount() {
+        // given
+        Pitches pitches = new Pitches();
+
+        // when
+        pitches.add(new Pitch(3));
+
+        // then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> pitches.add(new Pitch(8)))
+                .withMessageMatching("핀 처리횟수가 남은 핀수를 초과합니다. 투구정보를 확인해 주세요.");
+    }
+
+    @Test
+    @DisplayName("종료된 프레임 - 스트라이크로 인한 종료")
+    void pitch_finishedByStrike() {
+        // given
+        Pitches pitches = new Pitches();
+
+        // when
+        pitches.add(new Pitch(10));
+
+        // then
+        assertThatIllegalStateException()
+                .isThrownBy(() -> pitches.add(new Pitch(8)))
+                .withMessageMatching("종료된 프레임입니다.");
+    }
+
+    @Test
+    @DisplayName("종료된 프레임 - 스페어처리로 인한 종료")
+    void pitch_finishedBySpare() {
+        // given
+        Pitches pitches = new Pitches();
+
+        // when
+        pitches.add(new Pitch(7));
+        pitches.add(new Pitch(3));
+
+        // then
+        assertThatIllegalStateException()
+                .isThrownBy(() -> pitches.add(new Pitch(8)))
+                .withMessageMatching("종료된 프레임입니다.");
     }
 }
