b86844ee865b00e860ed1e8ea5796deec230f4b6
김우재 <kwj1270@naver.com>
2021-04-25 23:16:32 +0900

    test:삭제가 되었을 때 상태값이 true 로 변경되었는지 테스트작성

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import qna.CannotDeleteException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
@@ -12,7 +15,7 @@ public class AnswerTest {
 
     @DisplayName("작성자가 아닌 다른 사람이 삭제 요청을 했을시 예외처리 발생 여부 테스트")
     @Test
-    void 예외_테스트() {
+    void 예외_작성자() {
         assertThatThrownBy(() -> A1.delete(UserTest.SANJIGI))
                 .isInstanceOf(CannotDeleteException.class)
                 .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
@@ -20,6 +23,18 @@ public class AnswerTest {
         assertThatThrownBy(() -> A2.delete(UserTest.JAVAJIGI))
                 .isInstanceOf(CannotDeleteException.class)
                 .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
+
+    @DisplayName("삭제가 되었을 때 상태값이 true 로 변경되었는지 테스트")
+    @Test
+    void 변환_삭제상태() throws CannotDeleteException {
+        A1.delete(UserTest.JAVAJIGI);
+        A2.delete(UserTest.SANJIGI);
+
+        assertAll(
+                () -> assertThat(A1.isDeleted()).isTrue(),
+                () -> assertThat(A2.isDeleted()).isTrue()
+        );
     }
 
 }
