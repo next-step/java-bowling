b84fa12ffd6915bd1798d98d2145679044d64823
ttokey <ttokey.lee@gmail.com>
2021-04-25 18:03:18 +0900

    feat : DeleteHistories 객체 생성

---
+++ src/main/java/qna/domain/DeleteHistories.java
@@ -0,0 +1,11 @@
+package qna.domain;
+
+import java.util.List;
+
+public class DeleteHistories {
+    private final List<DeleteHistory> deleteHistories;
+
+    public DeleteHistories(List<DeleteHistory> deleteHistories) {
+        this.deleteHistories = deleteHistories;
+    }
+}
