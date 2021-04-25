b82b735f36416e45a67585db36a99521aac2ee3a
Dion <ksundong@gmail.com>
2021-04-25 17:00:44 +0900

    feat: Frame 생성 기능 추가

--- src/main/java/bowling/domain/frame/Frame.java
+++ src/main/java/bowling/domain/frame/Frame.java
@@ -1,8 +1,27 @@
 package bowling.domain.frame;
 
+import java.util.Objects;
+
 public final class Frame {
 
-    public Frame(RoundNumber roundNumber, FrameScore frameScore) {
+    private final RoundNumber roundNumber;
+    private final FrameScore frameScore;
 
+    public Frame(RoundNumber roundNumber, FrameScore frameScore) {
+        this.roundNumber = roundNumber;
+        this.frameScore = frameScore;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Frame frame = (Frame) o;
+        return Objects.equals(roundNumber, frame.roundNumber) && Objects.equals(frameScore, frame.frameScore);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roundNumber, frameScore);
     }
 }
