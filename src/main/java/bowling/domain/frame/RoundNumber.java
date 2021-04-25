615c848cf0ac55dfae85f3382edf4dd4b2bad874
Dion <ksundong@gmail.com>
2021-04-25 14:37:39 +0900

    feat: 라운드 번호 생성 기능 구현

--- src/main/java/bowling/domain/frame/RoundNumber.java
+++ src/main/java/bowling/domain/frame/RoundNumber.java
@@ -1,8 +1,25 @@
 package bowling.domain.frame;
 
+import java.util.Objects;
+
 public final class RoundNumber {
 
-    public RoundNumber(int roundNumberSource) {
+    private final int roundNumber;
 
+    public RoundNumber(int roundNumber) {
+        this.roundNumber = roundNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoundNumber that = (RoundNumber) o;
+        return roundNumber == that.roundNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roundNumber);
     }
 }
