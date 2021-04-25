c9dfb6d3611cd0f28856e066485853ca5da91e3a
김우재 <kwj1270@naver.com>
2021-04-26 00:01:35 +0900

    feat:DeleteHistories 인스턴스 리스트로 생성 기능 구현

--- src/main/java/qna/domain/DeleteHistories.java
+++ src/main/java/qna/domain/DeleteHistories.java
@@ -5,5 +5,13 @@ import java.util.List;
 
 public class DeleteHistories {
 
+    private List<DeleteHistory> deleteHistories;
 
+    public DeleteHistories() {
+        this(new ArrayList<>());
+    }
+
+    public DeleteHistories(List<DeleteHistory> deleteHistories) {
+        this.deleteHistories = deleteHistories;
+    }
 }
