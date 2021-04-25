57ee07ef828ed7eead528ecfe509b4618ecb01a9
Dion <ksundong@gmail.com>
2021-04-25 17:48:04 +0900

    feat: 첫번째 라운드 번호 생성 기능 추가

--- src/main/java/bowling/domain/frame/RoundNumber.java
+++ src/main/java/bowling/domain/frame/RoundNumber.java
@@ -8,6 +8,7 @@ public final class RoundNumber {
 
     private static final int MIN = 1;
     private static final int MAX = 10;
+    private static final int FIRST_ROUND_NUMBER = 1;
 
     private final int roundNumber;
 
@@ -17,7 +18,7 @@ public final class RoundNumber {
     }
 
     public static RoundNumber firstRoundNumber() {
-        return null;
+        return new RoundNumber(FIRST_ROUND_NUMBER);
     }
 
     private void validateRoundNumber(int roundNumber) {
