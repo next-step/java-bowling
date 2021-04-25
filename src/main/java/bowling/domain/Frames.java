788cfc77e30a60f6d01662309bf8d42be9695691
PandaHun <gnsgnsek@gmail.com>
2021-04-25 23:09:25 +0900

    test: Create Frames test

---
+++ src/main/java/bowling/domain/Frames.java
@@ -0,0 +1,15 @@
+package bowling.domain;
+
+public class Frames {
+
+    public Frames() {
+    }
+
+    public static Frames init() {
+        return new Frames();
+    }
+
+    public int size() {
+        return -1;
+    }
+}
