b16e7dd866fbde84d87551d7dfa6c72841882c27
PandaHun <gnsgnsek@gmail.com>
2021-04-25 14:20:30 +0900

    feat: Create Player init feature

--- src/main/java/bowling/domain/Player.java
+++ src/main/java/bowling/domain/Player.java
@@ -2,11 +2,31 @@ package bowling.domain;
 
 public class Player {
 
-    public static Player from(String bao) {
-        return new Player();
+    private static final int NAME_MAX_LENGTH = 3;
+    private static final String NAME_LENGTH_EXCEPTION_MESSAGE = "이름은 3글자 이하 입니다";
+    private static final String NAME_EMPTY_EXCEPTION_MESSAGE = "이름은 1글자 이상 입니다.";
+
+    private final String name;
+
+    private Player(final String name) {
+        this.name = name;
+    }
+
+    public static Player from(final String name) {
+        validateName(name);
+        return new Player(name);
+    }
+
+    private static void validateName(String name) {
+        if (name.length() > NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException(NAME_LENGTH_EXCEPTION_MESSAGE);
+        }
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException(NAME_EMPTY_EXCEPTION_MESSAGE);
+        }
     }
 
     public String name() {
-        return "";
+        return this.name;
     }
 }
