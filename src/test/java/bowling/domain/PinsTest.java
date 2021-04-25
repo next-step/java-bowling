c7763f1c7dd56bc904c39c92dbcd29515992d9f4
PandaHun <gnsgnsek@gmail.com>
2021-04-25 21:54:50 +0900

    test: Modify Pins test add try count

--- src/test/java/bowling/domain/PinsTest.java
+++ src/test/java/bowling/domain/PinsTest.java
@@ -1,5 +1,6 @@
 package bowling.domain;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,19 +11,42 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PinsTest {
 
+    private static List<Pin> pinList;
+    private static Pins pins;
+
+    @BeforeAll
+    static void init() {
+        pinList = Arrays.asList(new Pin(5), new Pin(5), new Pin(2));
+        pins = new Pins(pinList);
+    }
+
     @DisplayName("투구로 쓰러트린 핀의 갯수를 얻는다")
     @Test
     void pinsCountTest() {
-        List<Pin> pinList = Arrays.asList(new Pin(5), new Pin(5), new Pin(2));
-        Pins pins = new Pins(pinList);
         assertThat(pins.totalCount()).isEqualTo(12);
     }
 
     @DisplayName("두번째 투구 까지의 핀의 갯수를 얻는다")
     @Test
     void pinsToSecondTest() {
-        List<Pin> pinList = Arrays.asList(new Pin(5), new Pin(5), new Pin(2));
-        Pins pins = new Pins(pinList);
         assertThat(pins.toSecondCount()).isEqualTo(10);
+    }
+
+    @DisplayName("첫 시도의 갯수를 얻는다")
+    @Test
+    void pinsFirstTryTest() {
+        assertThat(pins.firstTryCount()).isEqualTo(5);
+    }
+
+    @DisplayName("두번째 시도의 갯수를 얻는다")
+    @Test
+    void pinsSecondTryTest() {
+        assertThat(pins.secondTryCount()).isEqualTo(5);
+    }
+
+    @DisplayName("보너스 시도의 갯수를 얻는다")
+    @Test
+    void pinsBonusTryTest() {
+        assertThat(pins.bonusTryCount()).isEqualTo(2);
     }
 }
