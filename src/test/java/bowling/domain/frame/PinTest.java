05d4160f9c9d8044e8c279c42a46d6ff3f99ebee
Dion <ksundong@gmail.com>
2021-04-25 15:00:18 +0900

    test: 핀의 개수 유효성 검증 테스트 작성

--- src/test/java/bowling/domain/frame/PinTest.java
+++ src/test/java/bowling/domain/frame/PinTest.java
@@ -1,16 +1,18 @@
 package bowling.domain.frame;
 
+import bowling.exception.InvalidPinCountException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PinTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
     @DisplayName("핀의 개수를 입력받아 핀을 생성한다.")
     void create(final int pinCount) {
         // given
@@ -32,5 +34,14 @@ class PinTest {
 
         // then
         assertThat(pin).isEqualTo(new Pin(defaultPinCount));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 11})
+    @DisplayName("핀의 수는 0~10 이어야 한다.")
+    void validate(final int invalidPinCount) {
+        assertThatThrownBy(() -> new Pin(invalidPinCount))
+                .isInstanceOf(InvalidPinCountException.class)
+                .hasMessage(InvalidPinCountException.INVALID_PIN_COUNT);
     }
 }
