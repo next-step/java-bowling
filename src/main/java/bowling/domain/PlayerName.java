0a162349ca0415207652b409107d9c6654689544
Dion <ksundong@gmail.com>
2021-04-25 14:32:02 +0900

    feat: 사용자 이름 허용문자 validation 구현

--- src/main/java/bowling/domain/PlayerName.java
+++ src/main/java/bowling/domain/PlayerName.java
@@ -1,5 +1,6 @@
 package bowling.domain;
 
+import bowling.exception.NotAllowedPlayerNameCharacterException;
 import bowling.exception.PlayerNameLengthException;
 
 import java.util.Objects;
@@ -8,18 +9,30 @@ public final class PlayerName {
 
     private static final int MIN_LENGTH = 1;
     private static final int MAX_LENGTH = 3;
+    private static final String VALID_NAME_REGEX = "^[a-zA-Z+]*$";
 
     private final String name;
 
     public PlayerName(String name) {
-        validateNameLength(name);
+        validatePlayerName(name);
         this.name = name;
+    }
+
+    private void validatePlayerName(String name) {
+        validateNameLength(name);
+        validateNameCharacter(name);
     }
 
     private void validateNameLength(String name) {
         final int length = name.length();
         if (length < MIN_LENGTH || length > MAX_LENGTH) {
             throw new PlayerNameLengthException(name);
+        }
+    }
+
+    private void validateNameCharacter(String name) {
+        if (!name.matches(VALID_NAME_REGEX)) {
+            throw new NotAllowedPlayerNameCharacterException(name);
         }
     }
 
