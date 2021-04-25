b78f720fc4774376d6475c5358895ed256cfa829
Dion <ksundong@gmail.com>
2021-04-25 18:50:42 +0900

    test: FinalFrame의 다음 프레임 생성 테스트 작성

--- src/test/java/bowling/domain/frame/FinalFrameTest.java
+++ src/test/java/bowling/domain/frame/FinalFrameTest.java
@@ -1,9 +1,11 @@
 package bowling.domain.frame;
 
+import bowling.exception.NoNextFrameException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class FinalFrameTest {
 
@@ -18,5 +20,19 @@ class FinalFrameTest {
 
         // then
         assertThat(finalFrame).isEqualTo(FinalFrame.from(frameScore));
+    }
+
+    @Test
+    @DisplayName("FinalFrame의 다음 프레임은 생성되지 않는다.")
+    void nextFrame() {
+        // given
+        final FrameScore frameScore = new FrameScore();
+        final FinalFrame finalFrame = FinalFrame.from(frameScore);
+
+        // when
+        // then
+        assertThatThrownBy(() -> finalFrame.createNextFrame())
+                .isInstanceOf(NoNextFrameException.class)
+                .hasMessage(NoNextFrameException.FINAL_FRAME_CANNOT_CREATE_NEXT_FRAME);
     }
 }
