7c5eb22bba0df11fdeb2a525338fd2fa54f0f913
Dion <ksundong@gmail.com>
2021-04-25 16:33:23 +0900

    feat: FrameScore 생성 기능 구현

--- src/main/java/bowling/domain/frame/FrameScore.java
+++ src/main/java/bowling/domain/frame/FrameScore.java
@@ -1,8 +1,27 @@
 package bowling.domain.frame;
 
+import java.util.Objects;
+
 public final class FrameScore {
 
-    public FrameScore(Pin pin, FrameStatus frameStatus) {
+    private final Pin pin;
+    private final FrameStatus frameStatus;
 
+    public FrameScore(Pin pin, FrameStatus frameStatus) {
+        this.pin = pin;
+        this.frameStatus = frameStatus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FrameScore that = (FrameScore) o;
+        return Objects.equals(pin, that.pin) && frameStatus == that.frameStatus;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pin, frameStatus);
     }
 }
