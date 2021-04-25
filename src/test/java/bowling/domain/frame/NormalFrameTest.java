db7d48bdeba7e73cb533df3ee0eef9f29ea1f8d5
Dion <ksundong@gmail.com>
2021-04-25 17:56:32 +0900

    test: NormalFrame의 다음 Frame을 생성하는 기능 추가

--- src/test/java/bowling/domain/frame/NormalFrameTest.java
+++ src/test/java/bowling/domain/frame/NormalFrameTest.java
@@ -2,6 +2,8 @@ package bowling.domain.frame;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,5 +36,22 @@ class NormalFrameTest {
 
         // then
         assertThat(frame).isEqualTo(new NormalFrame(roundNumber, frameScore));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8})
+    @DisplayName("다음 프레임을 생성한다.")
+    void nextFrame(final int roundNumberSource) {
+        // given
+        final RoundNumber roundNumber = new RoundNumber(roundNumberSource);
+        final RoundNumber nextRoundNumber = new RoundNumber(roundNumberSource + 1);
+        final FrameScore frameScore = new FrameScore();
+        final Frame frame = new NormalFrame(roundNumber, frameScore);
+
+        // when
+        final Frame nextFrame = frame.createNextFrame();
+
+        // then
+        assertThat(nextFrame).isEqualTo(new NormalFrame(nextRoundNumber, frameScore));
     }
 }
