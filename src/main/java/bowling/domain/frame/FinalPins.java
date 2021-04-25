6b2908ba86d86d1baa1285a0888dd3ff5a39e13f
Dion <ksundong@gmail.com>
2021-04-25 22:55:38 +0900

    test: FinalPins 생성 테스트 작성

---
+++ src/main/java/bowling/domain/frame/FinalPins.java
@@ -0,0 +1,22 @@
+package bowling.domain.frame;
+
+public final class FinalPins implements Pins {
+
+    public FinalPins(Pin firstPin, Pin secondPin, Pin thirdPin) {
+
+    }
+
+    @Override
+    public Pin firstPin() {
+        return null;
+    }
+
+    @Override
+    public Pin secondPin() {
+        return null;
+    }
+
+    public Pin thirdPin() {
+        return null;
+    }
+}
---
+++ src/test/java/bowling/domain/frame/FinalPinsTest.java
@@ -0,0 +1,30 @@
+package bowling.domain.frame;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class FinalPinsTest {
+
+    @Test
+    @DisplayName("FinalPins는 Pin을 세 개 가지고 생성된다.")
+    void create() {
+        // given
+        final Pin firstPin = new Pin();
+        final Pin secondPin = new Pin();
+        final Pin thirdPin = new Pin();
+
+        // when
+        final FinalPins pins = new FinalPins(firstPin, secondPin, thirdPin);
+
+        // then
+        assertAll(
+                () -> assertThat(pins).isEqualTo(new FinalPins(firstPin, secondPin, thirdPin)),
+                () -> assertThat(pins.firstPin()).isEqualTo(firstPin),
+                () -> assertThat(pins.secondPin()).isEqualTo(secondPin),
+                () -> assertThat(pins.thirdPin()).isEqualTo(thirdPin)
+        );
+    }
+}
