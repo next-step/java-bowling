a8bfff9f0a026cf2d037d04085da5f8f8224d6a4
623nana <6230nana@gmail.com>
2021-04-26 11:22:29 +0900

    test: Players 테스트

---
+++ src/test/java/bowling/domain/PlayersTest.java
@@ -0,0 +1,22 @@
+package bowling.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PlayersTest {
+
+  @Test
+  @DisplayName("[Players] Players 생성 테스트")
+  void create_players_test() {
+    List<Player> playerList = Arrays.asList(new Player("nhj"), new Player("nn"));
+    Players players = new Players(playerList);
+
+    int size = players.players().size();
+
+    assertThat(size).isEqualTo(2);
+  }
+}
