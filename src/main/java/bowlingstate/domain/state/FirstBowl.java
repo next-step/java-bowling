3a5e879977f6ae449a86ce62cd7cf5a12cda1862
623nana <6230nana@gmail.com>
2021-04-26 22:51:07 +0900

    refactor: 테스트에 따른 생성자 추가 및 Spare 핀 카운트 상수화

--- src/main/java/bowlingstate/domain/state/FirstBowl.java
+++ src/main/java/bowlingstate/domain/state/FirstBowl.java
@@ -8,16 +8,17 @@ public class FirstBowl implements State {
 
 
   private static final String GUTTER_SYMBOL = "-";
+  private static final int SPARE_PIN_COUNT = 10;
   private static final int GUTTER_PIN_COUNT = 0;
   private final int countOfHitPin;
 
-  FirstBowl(int countOfHitPin) {
+  public FirstBowl(int countOfHitPin) {
     this.countOfHitPin = countOfHitPin;
   }
 
   @Override
   public State bowl(int countOfHitPin) {
-    if (this.countOfHitPin + countOfHitPin == 10) {
+    if (this.countOfHitPin + countOfHitPin == SPARE_PIN_COUNT) {
       return new Spare(this.countOfHitPin, countOfHitPin);
     }
     return new Miss(this.countOfHitPin, countOfHitPin);
