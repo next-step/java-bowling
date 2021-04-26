3d81a6ec4e52151fc2905863f1c01519ec681e99
Dongjoon Park <djoon@users.noreply.github.com>
2021-04-26 17:53:53 +0900

    refactor: List<Answer> 일급 컬렉션으로 변경

---
+++ src/main/java/qna/domain/Answers.java
@@ -0,0 +1,30 @@
+package qna.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import org.hibernate.annotations.Where;
+import qna.CannotDeleteException;
+
+@Embeddable
+public class Answers {
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    @Where(clause = "deleted = false")
+    @OrderBy("id ASC")
+    private List<Answer> answers = new ArrayList<>();
+
+    public boolean add(Answer answer) {
+        return answers.add(answer);
+    }
+
+    public DeleteHistories deleteAll(User loginUser) throws CannotDeleteException {
+        DeleteHistories deleteHistories = new DeleteHistories();
+        for (Answer answer : answers) {
+            deleteHistories.add(answer.delete(loginUser));
+        }
+        return deleteHistories;
+    }
+}
