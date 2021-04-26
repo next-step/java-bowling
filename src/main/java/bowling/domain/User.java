69dcbdc766de2430e642eb93803b300b969f888f
ttokey <ttokey.lee@gmail.com>
2021-04-26 19:07:52 +0900

    test : User 객체 테스트 코드 작성

--- src/main/java/bowling/domain/User.java
+++ src/main/java/bowling/domain/User.java
@@ -1,4 +1,13 @@
 package bowling.domain;
 
 public class User {
+    private final String user;
+
+    public User(String user) {
+        if (user.length() > 3) {
+            throw new IllegalArgumentException("Max 3 english letters Only");
+        }
+        this.user = user;
+    }
+
 }
--- src/test/java/bowling/UserTest.java
+++ src/test/java/bowling/UserTest.java
@@ -1,2 +1,21 @@
-package bowling;public class UserTest {
+package bowling;
+
+import bowling.domain.User;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class UserTest {
+    @Test
+    public void lengthTest() {
+        // given
+        final String user = "ttokey";
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> {
+            new User(user);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
 }
