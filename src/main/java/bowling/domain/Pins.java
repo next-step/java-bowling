6741549cb91744a29b19299f163f8c19c86b3f83
cyr9210 <jipsyer@naver.com>
2021-04-27 01:23:48 +0900

    feat : pins 생성 및 테스트 - 서있던 핀 및 쓰러뜨린 핀정보 를 가진 pins

---
+++ src/main/java/bowling/domain/Pins.java
@@ -0,0 +1,42 @@
+package bowling.domain;
+
+public class Pins {
+
+  private static final int MIN = 0;
+  public static final int MAX = 10;
+
+  private int totalPins;
+  private int hitPins;
+
+  public Pins(int totalPins, int hitPins) {
+    this.totalPins = totalPins;
+    this.hitPins = hitPins;
+    if (hitPins < MIN || hitPins > totalPins) {
+      throw new IllegalArgumentException("쓰러진핀의 갯수는 0이상 남은 핀이하 여야한다.");
+    }
+  }
+
+  public int getLeftCount() {
+    return totalPins - hitPins;
+  }
+
+  public boolean isClear() {
+    return getLeftCount() == 0;
+  }
+
+  public Pins(int hitPins) {
+    this(MAX, hitPins);
+  }
+
+  public int getHitPins() {
+    return hitPins;
+  }
+
+  @Override
+  public String toString() {
+    return "Pins{" +
+        "totalPins=" + totalPins +
+        ", hitPins=" + hitPins +
+        '}';
+  }
+}
---
+++ src/test/java/bowling/domain/PinsTest.java
@@ -0,0 +1,39 @@
+package bowling.domain;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class PinsTest {
+
+  private static final int MAX_PINS = 10;
+
+  @ParameterizedTest
+  @ValueSource(ints = {0, 10})
+  @DisplayName("0이상 10이하의 쓰러진 볼링핀의 갯수를 생성한다.")
+  void hit(int count) {
+    Pins pins = new Pins(count);
+    assertEquals(pins.getLeftCount(), MAX_PINS - count);
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {11, -1})
+  @DisplayName("0미난 10초과 쓰러진 볼링핀을 생성할 때, exception")
+  void hit_fail(int count) {
+    assertThatIllegalArgumentException()
+        .isThrownBy(() -> new Pins(count))
+        .withMessage("쓰러진핀의 갯수는 0이상 남은 핀이하 여야한다.");
+  }
+
+  @Test
+  @DisplayName("남은 핀 수보다 쓰러뜨린 핀의 수가 크면, exception")
+  void hit_fail_secondBall() {
+    assertThatIllegalArgumentException()
+        .isThrownBy(() -> new Pins(10, 11))
+        .withMessage("쓰러진핀의 갯수는 0이상 남은 핀이하 여야한다.");
+  }
+}
