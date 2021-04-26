1c38e656563ae4b00c1c84b8211fb88964abb213
cyr9210 <jipsyer@naver.com>
2021-04-27 01:31:07 +0900

    feat : ResultView 제외 BowlingGame 구현

--- src/main/java/bowling/BowlingGame.java
+++ src/main/java/bowling/BowlingGame.java
@@ -1,8 +1,26 @@
 package bowling;
 
+import bowling.domain.Bowling;
+import bowling.view.InputView;
+
 public class BowlingGame {
 
+  public static final int MAX_FRAME = 10;
+
   public static void main(String[] args) {
+    String name = InputView.getName();
+    Bowling bowling = new Bowling(name);
+
+    System.out.println(bowling.getCurrent());
+    while (bowling.getCurrent() <= MAX_FRAME) {
+      int hitCount = InputView.getHitPinsCount(bowling.getCurrent());
+      bowling.play(hitCount);
+      System.out.println(bowling);
+    }
+
+    if (bowling.hasBonusPins()) {
+      int hitCount = InputView.getHitPinsCount(bowling.getCurrent());
+    }
 
   }
 
