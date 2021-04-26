02bb9f27f86fdacbbd600e793e0e8fa427bd3865
623nana <6230nana@gmail.com>
2021-04-26 15:42:19 +0900

    feat: validatePlayers 로직 추가

--- src/main/java/bowling/domain/player/Players.java
+++ src/main/java/bowling/domain/player/Players.java
@@ -8,10 +8,17 @@ public class Players {
   private final List<Player> players;
 
   public Players(List<Player> players) {
+    validatePlayers(players);
     this.players = players;
   }
 
   public List<Player> players() {
     return Collections.unmodifiableList(players);
+  }
+
+  private void validatePlayers(List<Player> players) {
+    if (players.isEmpty()) {
+      throw new IllegalArgumentException("최소 한명의 참가자가 있어야 실행할 수 있습니다.");
+    }
   }
 }
