fc9a980580eb014d551efa528efbdfd87de19cf8
Dion <ksundong@gmail.com>
2021-04-26 21:51:19 +0900

    feat: Player가 이름과 Frame 목록으로 생성되는 기능 구현

--- src/main/java/bowling/domain/player/Player.java
+++ src/main/java/bowling/domain/player/Player.java
@@ -2,9 +2,28 @@ package bowling.domain.player;
 
 import bowling.domain.frame.Frames;
 
+import java.util.Objects;
+
 public final class Player {
 
-    public Player(PlayerName playerName, Frames frames) {
+    private final PlayerName playerName;
+    private final Frames frames;
 
+    public Player(PlayerName playerName, Frames frames) {
+        this.playerName = playerName;
+        this.frames = frames;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(playerName, player.playerName) && Objects.equals(frames, player.frames);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(playerName, frames);
     }
 }
