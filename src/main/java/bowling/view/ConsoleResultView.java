dda0f0d949e99f5c52ff47f7550a3f269f1244b4
Jake <cocahack@naver.com>
2021-04-26 23:31:57 +0900

    refactor: 출력 방식 개선

--- src/main/java/bowling/view/ConsoleResultView.java
+++ src/main/java/bowling/view/ConsoleResultView.java
@@ -9,6 +9,7 @@ public class ConsoleResultView implements ResultView {
     private static final String DELIMITER = "|";
     private static final String WHITE_SPACE = " ";
     private static final int ELEMENT_LENGTH = 6;
+    private static final int FINAL_ELEMENT_LENGTH = 8;
     private static final int TOTAL_FRAMES = 10;
 
     @Override
@@ -19,42 +20,50 @@ public class ConsoleResultView implements ResultView {
 
     private String buildHeader() {
         StringBuilder stringBuilder = new StringBuilder(DELIMITER);
-        stringBuilder.append(alignMiddle("NAME")).append(DELIMITER);
+        stringBuilder.append(alignCenter("NAME", ELEMENT_LENGTH)).append(DELIMITER);
 
-        for (int i = 1; i <= TOTAL_FRAMES; i++){
-            stringBuilder.append(alignMiddle(frameNumber(i))).append(DELIMITER);
+        for (int i = 1; i < TOTAL_FRAMES; i++){
+            stringBuilder.append(alignCenter(frameNumber(i), ELEMENT_LENGTH)).append(DELIMITER);
         }
+
+        stringBuilder.append(alignCenter(frameNumber(10), FINAL_ELEMENT_LENGTH)).append(DELIMITER);
 
         return stringBuilder.toString();
     }
 
     private String frameNumber(int i) {
-        return i < TOTAL_FRAMES ? "0" + i : String.valueOf(i);
+        if (i == TOTAL_FRAMES) {
+            return "10";
+        }
+
+        return "0" + i;
     }
 
     private String buildBody(PlayerDto playerDto) {
         StringBuilder stringBuilder = new StringBuilder(DELIMITER);
-        stringBuilder.append(alignMiddle(playerDto.getPlayerName())).append(DELIMITER);
+        stringBuilder.append(alignCenter(playerDto.getPlayerName(), ELEMENT_LENGTH)).append(DELIMITER);
 
         List<String> frameRecords = playerDto.getFramesDto().getFrames();
 
         frameRecords.forEach(frameRecord -> {
-            String centeredRecord = alignMiddle(String.join(DELIMITER, frameRecord));
+            String centeredRecord = alignCenter(String.join(DELIMITER, frameRecord), ELEMENT_LENGTH);
             stringBuilder.append(centeredRecord).append(DELIMITER);
         });
 
-        for (int i = 0; i < TOTAL_FRAMES - frameRecords.size(); i++){
-            stringBuilder.append(alignMiddle("")).append(DELIMITER);
+        for (int i = 0; i < TOTAL_FRAMES - frameRecords.size() - 1; i++){
+            stringBuilder.append(alignCenter("", ELEMENT_LENGTH)).append(DELIMITER);
         }
+
+        stringBuilder.append(alignCenter("", FINAL_ELEMENT_LENGTH)).append(DELIMITER);
 
         return stringBuilder.toString();
     }
 
-    private String alignMiddle(String string) {
+    private String alignCenter(String string, int totalLength) {
         int stringLength = string.length();
 
-        int leftPadding = (ELEMENT_LENGTH - stringLength) / 2;
-        int rightPadding = ELEMENT_LENGTH - stringLength - leftPadding;
+        int rightPadding = (totalLength - stringLength) / 2;
+        int leftPadding = totalLength - stringLength - rightPadding;
 
         StringBuilder stringBuilder = new StringBuilder();
 
