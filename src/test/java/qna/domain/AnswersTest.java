21f3d3610ceca2db419cbc1dbfc655353b7e0fb2
김우재 <kwj1270@naver.com>
2021-04-26 09:38:29 +0900

    test:삭제가 되었을 때 반환 값으로 삭제 내역들을 기록한 객체를 반환하는지 테스트작성

--- src/test/java/qna/domain/AnswersTest.java
+++ src/test/java/qna/domain/AnswersTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class AnswersTest {
 
@@ -28,5 +30,18 @@ public class AnswersTest {
         Answers answers = new Answers(answerList);
 
         assertThat(answers).isNotNull();
+    }
+
+    @DisplayName("삭제가 되었을 때 반환 값으로 삭제 내역들을 기록한 객체를 반환하는지 테스트")
+    @Test
+    void 반환_삭제기록들() {
+        // given
+        List<Answer> answerList = new ArrayList<>(Arrays.asList(AnswerTest.A1, AnswerTest.A2));
+
+        Answers answers = new Answers(answerList);
+        assertAll(
+                () -> assertThat(answers.delete(UserTest.JAVAJIGI)).isNotNull(),
+                () -> assertThat(answers.delete(UserTest.JAVAJIGI)).isInstanceOf(DeleteHistories.class)
+        );
     }
 }
