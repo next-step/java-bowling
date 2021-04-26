a5e7528f11f4259a543c57266bfdbbe4aaa6a330
Jake <cocahack@naver.com>
2021-04-26 23:32:17 +0900

    refactor: 삼항 연산자 제거

--- src/main/java/bowling/domain/engine/frame/FrameCreator.java
+++ src/main/java/bowling/domain/engine/frame/FrameCreator.java
@@ -10,7 +10,14 @@ public class FrameCreator {
     private int count = 1;
 
     public Frame create() {
-        return count++ < MAX_FRAMES ? NormalFrame.init() : FinalFrame.init();
+        if (count > MAX_FRAMES) {
+            throw new IllegalStateException("더 이상 프레임을 생성할 수 없습니다.");
+        }
+
+        if (count++ == MAX_FRAMES) {
+            return FinalFrame.init();
+        }
+        return NormalFrame.init();
     }
 
 }
