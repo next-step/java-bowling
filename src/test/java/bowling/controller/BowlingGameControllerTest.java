485f71444914baae5b9c3457eaa0c2cf341e4dbd
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-26 19:32:03 +0900

    feat : BowlingGameController 클래스 신규작성

---
+++ src/test/java/bowling/controller/BowlingGameControllerTest.java
@@ -0,0 +1,85 @@
+package bowling.controller;
+
+import bowling.controller.dto.BowlingGameRequest;
+import bowling.controller.dto.BowlingGameResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BowlingGameControllerTest {
+
+    private BowlingGameController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new BowlingGameController();
+    }
+
+    @Test
+    @DisplayName("볼링게임 시작 테스트")
+    void startGame() {
+        // given
+        BowlingGameRequest request = new BowlingGameRequest("LDS");
+
+        // when
+        BowlingGameResponse bowlingGameResponse = controller.startGame(request);
+
+        // then
+        assertThat("LDS").isEqualTo(bowlingGameResponse.getParticipantName());
+        assertThat(1).isEqualTo(bowlingGameResponse.getNextFrameNumber());
+    }
+
+    @Test
+    @DisplayName("투구 처리")
+    void pitchBall() {
+        // given
+        BowlingGameRequest request = new BowlingGameRequest("LDS");
+        BowlingGameRequest request10 = new BowlingGameRequest("LDS", 10);
+        BowlingGameRequest request9 = new BowlingGameRequest("LDS", 9);
+        BowlingGameRequest request1 = new BowlingGameRequest("LDS", 1);
+
+        // when
+        controller.startGame(request);
+        controller.pitchBall(request10);
+        controller.pitchBall(request9);
+        BowlingGameResponse response = controller.pitchBall(request1);
+
+        // then
+        assertThat("LDS").isEqualTo(response.getParticipantName());
+        assertThat(3).isEqualTo(response.getNextFrameNumber());
+        assertThat(response.isFinished()).isFalse();
+        assertThat(Arrays.asList("X", "9|/")).isEqualTo(response.getPinDownResults());
+    }
+
+    @Test
+    @DisplayName("게임 종료")
+    void finishGame() {
+        // given
+        BowlingGameRequest request = new BowlingGameRequest("LDS");
+        BowlingGameRequest request10 = new BowlingGameRequest("LDS", 10);
+
+        // when
+        BowlingGameResponse response = null;
+        controller.startGame(request);
+        controller.pitchBall(new BowlingGameRequest("LDS", 3));
+        controller.pitchBall(new BowlingGameRequest("LDS", 0));
+        controller.pitchBall(new BowlingGameRequest("LDS", 7));
+        controller.pitchBall(new BowlingGameRequest("LDS", 3));
+        controller.pitchBall(new BowlingGameRequest("LDS", 0));
+        controller.pitchBall(new BowlingGameRequest("LDS", 0));
+        for (int i = 3; i < 11; i++) {
+            response = controller.pitchBall(request10);
+        }
+
+        // then
+        assertThat("LDS").isEqualTo(response.getParticipantName());
+        assertThat(10).isEqualTo(response.getNextFrameNumber());
+        assertThat(response.isFinished()).isTrue();
+        assertThat(Arrays.asList("3|-", "7|/", "-|-", "X", "X", "X", "X", "X", "X", "X|X")).isEqualTo(response.getPinDownResults());
+    }
+
+}
