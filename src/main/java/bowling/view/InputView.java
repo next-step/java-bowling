920773b726cf5bb641a3bcf568bf6f45e556c326
623nana <6230nana@gmail.com>
2021-04-26 14:18:57 +0900

    feat: 참가자 수 입력 받고 각각 참가자 핀 입력 받는 기능

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -1,17 +1,22 @@
 package bowling.view;
 
-import bowling.domain.frame.Frames;
+import bowling.domain.Name;
 import java.util.Scanner;
 
 public class InputView {
 
-  public static String inputOfPlayerName() {
-    System.out.print("플레이어 이름은(3 english letters)?: ");
+  public static int inputCountOfPlayer() {
+    System.out.print("How many people? ");
+    return new Scanner(System.in).nextInt();
+  }
+
+  public static String inputOfPlayerName(int playerCount) {
+    System.out.printf("플레이어 %d의 이름은(3 english letters)?: ", playerCount + 1);
     return new Scanner(System.in).nextLine();
   }
 
-  public static int inputCountOfHitPin(Frames frames) {
-    System.out.printf("%d프레임 투구 : ", frames.round());
+  public static int inputCountOfHitPin(Name name) {
+    System.out.printf("%s's turn : ", name.name());
     return new Scanner(System.in).nextInt();
   }
 }
