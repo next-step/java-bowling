15a137dc3932da37a8a6c92afbdda91eefdd8ac6
Beenie93 <seongbeen93@naver.com>
2021-04-27 00:25:30 +0900

    test: implement test code for score

---
+++ src/test/java/bowling/domain/ScoreTest.java
@@ -0,0 +1,51 @@
+package bowling.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ScoreTest {
+
+    @Test
+    @DisplayName("카운트 할 수 없는 점수를 생성한다.")
+    void unCountableScore() {
+        Score score = Score.unCountableScore();
+        assertThat(score.value()).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("점수를 2번 더할 수 있는 기회를 가지는 점수를 생성한다.")
+    void strike() {
+        Score strike = Score.Strike(10);
+        assertThat(strike.leftOpportunity()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("점수를 1번 더할 수 있는 기회를 가지는 점수를 생성한다.")
+    void spare() {
+        Score spare = Score.Spare(10);
+        assertThat(spare.leftOpportunity()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("점수를 더할 수 있는 기회가 없는 점수를 생성한다.")
+    void miss() {
+        Score miss = Score.Miss(10);
+        assertThat(miss.leftOpportunity()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("점수를 더할 수 있는 기회가 남아있을 경우, 참을 반환한다.")
+    void isOpportunityLeft() {
+        Score strike = Score.Strike(10);
+        Score spare = Score.Spare(10);
+        Score miss = Score.Miss(10);
+        Score unCountableScore = Score.unCountableScore();
+
+        assertThat(strike.isOpportunityLeft()).isTrue();
+        assertThat(spare.isOpportunityLeft()).isTrue();
+        assertThat(miss.isOpportunityLeft()).isFalse();
+        assertThat(unCountableScore.isOpportunityLeft()).isFalse();
+    }
+}
