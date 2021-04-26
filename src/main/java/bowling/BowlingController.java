c374cb395784b01327c6b33538ad84a3aa35aad0
Dion <ksundong@gmail.com>
2021-04-26 22:25:43 +0900

    feat: 사용자의 이름을 입력받는 기능 구현

--- src/main/java/bowling/BowlingController.java
+++ src/main/java/bowling/BowlingController.java
@@ -1,10 +1,22 @@
 package bowling;
 
+import bowling.domain.player.PlayerName;
+import bowling.exception.PlayerNameValidationException;
+
 public final class BowlingController {
 
     private BowlingController() {}
 
     public static void run() {
+        final PlayerName playerName = playerName(InputView.inputPlayerName());
+    }
 
+    private static PlayerName playerName(final String playerName) {
+        try {
+            return PlayerName.valueOf(playerName);
+        } catch (PlayerNameValidationException e) {
+            System.err.println(e.getMessage());
+            return playerName(InputView.inputPlayerName());
+        }
     }
 }
--- src/main/java/bowling/InputView.java
+++ src/main/java/bowling/InputView.java
@@ -1,6 +1,15 @@
 package bowling;
 
+import java.util.Scanner;
+
 public final class InputView {
 
+    private static final Scanner scanner = new Scanner(System.in);
+
     private InputView() {}
+
+    public static String inputPlayerName() {
+        System.out.println("플레이어의 이름을 입력해주세요.(영문 대소문자 3자이내): ");
+        return scanner.nextLine();
+    }
 }
