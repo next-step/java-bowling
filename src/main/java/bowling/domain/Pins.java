0a1ebfe574fe52bc426f69f14556ca98bf63c6d3
김우재 <kwj1270@naver.com>
2021-04-27 17:18:12 +0900

    feat:Pins 인스턴스 생성 기능 구현

--- src/main/java/bowling/domain/Pins.java
+++ src/main/java/bowling/domain/Pins.java
@@ -1,4 +1,16 @@
 package bowling.domain;
 
-public class Pins {
+public final class Pins {
+
+    private static final int MAXIMUM_COUNT = 10;
+    private static final int MINIMUM_COUNT = 0;
+
+    private int pins = MAXIMUM_COUNT;
+
+    private Pins() { }
+
+    public static final Pins init() {
+        return new Pins();
+    }
+
 }
