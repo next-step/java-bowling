eb0ea77e1a83b701819db660b32ba6aca04395c6
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-04-27 20:37:16 +0900

    refactoring: n명으로 플레이어가 늘어남에 따른 입력 기능 수정

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -7,14 +7,20 @@ public class InputView {
     private static Scanner scanner = new Scanner(System.in);
 
     //플레이어 이름은(3 english letters)?: PJS
-    public static String inputPlayer() {
-        System.out.print("플레이어 이름은(3 english letters)?:");
+    public static String inputPlayer(int playerPosition) {
+        System.out.print("플레이어 " + playerPosition + "의 이름은(3 english letters)?:");
         return scanner.nextLine();
     }
 
     //1프레임 투구 : 10
-    public static int inputPoint(int round) {
-        System.out.print(round + "프레임 투구 : ");
+    public static int inputPoint(String playerName) {
+        System.out.print(playerName + "'s turn : ");
+        return scanner.nextInt();
+    }
+
+    //How many people? 2
+    public static int playerCount() {
+        System.out.println("How many people? ");
         return scanner.nextInt();
     }
 }
