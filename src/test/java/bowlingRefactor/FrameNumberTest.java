ca02f368f278f9a4b0910f0e9c908aff454dc65b
hwangjeonghyeon1 <deert202@naver.com>
2021-04-28 03:16:11 +0900

    [refactor] test 추가

--- src/test/java/bowlingRefactor/FrameNumberTest.java
+++ src/test/java/bowlingRefactor/FrameNumberTest.java
@@ -1,2 +1,26 @@
-package bowlingRefactor;public class FrameNumberTest {
+package bowlingRefactor;
+
+import bowlingRefactor.domain.FrameNumber;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class FrameNumberTest {
+
+    @Test
+    @DisplayName("프레임 범위 에러 테스트")
+    void exception() {
+        assertThatThrownBy(() -> {
+            FrameNumber.of(0);
+            FrameNumber.of(11);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("마지막프레임 테스트")
+    void isLast() {
+        assertThat(FrameNumber.of(10).isFinalFrame()).isTrue();
+    }
 }
--- src/test/java/bowlingRefactor/ScoreTest.java
+++ src/test/java/bowlingRefactor/ScoreTest.java
@@ -1,2 +1,30 @@
-package bowlingRefactor;public class ScoreTest {
+package bowlingRefactor;
+
+import bowlingRefactor.domain.Pin;
+import bowlingRefactor.domain.Score;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ScoreTest {
+
+    @Test
+    @DisplayName("남은 투구가 남았는지")
+    void leftPitchTest() {
+        Score score = Score.ofDefault();
+        assertThat(score.isLeft()).isFalse();
+    }
+
+    @Test
+    @DisplayName("점수 플러스, 보너스 횟수 마이너스")
+    void scoreTest() {
+        Score strike = Score.ofStrike(0);
+        Score spare = Score.ofSpare(0);
+        Score miss = Score.of(0, 1);
+
+        assertThat(strike.addBonus(Pin.of(10))).isEqualTo(new Score(1, 20));
+        assertThat(spare.addBonus(Pin.of(10))).isEqualTo(new Score(0, 20));
+        assertThat(miss.addBonus(Pin.of(10))).isEqualTo(new Score(0, 1));
+    }
 }
