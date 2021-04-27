dc8e91acd0d00602eed0159d6420422fb89f6433
hwangjeonghyeon1 <deert202@naver.com>
2021-04-28 03:13:47 +0900

    [refactor] Score 타입 enum 추가

--- src/main/java/bowlingRefactor/domain/ScoreType.java
+++ src/main/java/bowlingRefactor/domain/ScoreType.java
@@ -1,2 +1,23 @@
-package bowlingRefactor.domain;public enum ScoreType {
+package bowlingRefactor.domain;
+
+import java.util.stream.Stream;
+
+public enum ScoreType {
+
+    STRIKE(10, "X"),
+    SPARE(10, "/"),
+    MISS(-1, ""),
+    GUTTER(0, "-");
+
+    private int count;
+    private String type;
+
+    ScoreType(int count, String type) {
+        this.count = count;
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
