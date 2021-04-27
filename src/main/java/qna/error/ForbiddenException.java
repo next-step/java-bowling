752dd3405d61a2bb82275844a56ec2513766acbb
povia <47127607+povia@users.noreply.github.com>
2021-04-28 02:08:10 +0900

    fix: Exception error 패키지 내로 위치 이동

--- src/main/java/qna/error/ForbiddenException.java
+++ src/main/java/qna/error/ForbiddenException.java
@@ -1,4 +1,4 @@
-package qna;
+package qna.error;
 
 public class ForbiddenException extends RuntimeException{
     public ForbiddenException() {
--- src/main/java/qna/error/UnAuthenticationException.java
+++ src/main/java/qna/error/UnAuthenticationException.java
@@ -1,4 +1,4 @@
-package qna;
+package qna.error;
 
 public class UnAuthenticationException extends Exception {
     private static final long serialVersionUID = 1L;
