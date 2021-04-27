c06336c927d2b66b9ef694b6eb5368024643c8e0
lee.jaesung <lee.jaesung@joins.com>
2021-04-28 07:46:19 +0900

    refactor: Frames.pitch 의 프레임 생성 방법 변경 및 컬렉션 read-only 처리

--- src/main/java/bowling/domain/Frames.java
+++ src/main/java/bowling/domain/Frames.java
@@ -1,7 +1,9 @@
 package bowling.domain;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class Frames {
     private final Map<Integer, Frame> frames;
@@ -15,14 +17,13 @@ public class Frames {
     }
 
     public Map<Integer, Frame> frames() {
-        return frames;
+        return Collections.unmodifiableMap(frames);
     }
 
     public void pitch(int frameNo, int point) {
-        Frame frame = new Frame(frameNo);
-        if (frames.containsKey(frameNo)) {
-            frame = frames.get(frameNo);
-        }
+        Frame frame = Optional.ofNullable(frames.get(frameNo))
+                .orElse(new Frame(frameNo));
+
         frames.put(frameNo, frame.pitch(point));
     }
 
