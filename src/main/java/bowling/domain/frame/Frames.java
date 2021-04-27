014663b7b3882cda6638d47900b4d7b08f189f00
ttokey <ttokey.lee@gmail.com>
2021-04-28 06:40:29 +0900

    feat : Frames 객체 메소드 구현하였으나, 테스트 코드 수정이 필요

--- src/main/java/bowling/domain/frame/Frames.java
+++ src/main/java/bowling/domain/frame/Frames.java
@@ -2,20 +2,83 @@ package bowling.domain.frame;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
 
 public class Frames {
-    List<Frame> frames;
+    private final List<Frame> frames;
+
+    public Frames() {
+        this(generateFrames());
+    }
 
     public Frames(List<Frame> frames) {
         this.frames = frames;
     }
 
+    public boolean isFinished() {
+        if (nowFrameIndex() > 9) {
+            return true;
+        }
+        return false;
+    }
+
     public int nextFrame() {
-        return 0;
+        if (frames.isEmpty()) {
+            return 0;
+        }
+        if (frames.get(nowFrameIndex() - 1).isFinished()) {
+            return nowFrameIndex() + 1;
+        }
+        return nowFrameIndex();
     }
 
-    public Frames addScore(int score) {
-        return new Frames(new ArrayList<>());
+    public Frames addScore(int score) throws Exception {
+        if (nowFrameIndex() == nextFrame()) {
+            frames.get(nowFrameIndex()).addScore(score);
+        }
+        if (nextFrame() == 9) {
+            frames.get(nextFrame()).addScore(score);
+        }
+        return new Frames(this.frames);
     }
 
+    private int nowFrameIndex() {
+        return IntStream.range(0, 10)
+                .filter(i -> !frames.get(i).isFinished())
+                .findFirst()
+                .orElse(10);
+    }
+
+    private static List<Frame> generateFrames() {
+        List<Frame> result = new ArrayList<>();
+        IntStream.range(0, 9)
+                .forEach(i -> result.add(new NormalFrame()));
+        result.add(new FinalFrame());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Frames frames1 = (Frames) o;
+        return Objects.equals(frames, frames1.frames);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(frames);
+    }
+
+    @Override
+    public String toString() {
+        return "Frames{" +
+                "frames=" + frames +
+                '}';
+    }
 }
--- src/test/java/bowling/FramesTest.java
+++ src/test/java/bowling/FramesTest.java
@@ -4,7 +4,6 @@ import bowling.domain.frame.FinalFrame;
 import bowling.domain.frame.Frame;
 import bowling.domain.frame.Frames;
 import bowling.domain.frame.NormalFrame;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -12,21 +11,24 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FramesTest {
 
+
     @Test
-    public void nextFrame_10번째_프레임() {
+    public void nextFrame_4번째_프레임() {
         // given
-        Frame normalFrame = NormalFrame.valueOf(Arrays.asList(3, 4));
-        List<Frame> frameList = new ArrayList<>();
+        List<Integer> scores = Arrays.asList(1, 5, 3, 4, 2, 3);
+
         IntStream.range(0, 9)
                 .forEach(i -> frameList.add(normalFrame));
-        Frames frames = new Frames(frameList);
-        int expectNextFrame = 10;
+        Frames frames = new Frames();
+        int expectNextFrame = 9;
         // when
 
         // then
-        Assertions.assertThat(frames.nextFrame()).isEqualTo(expectNextFrame);
+        assertThat(frames.nextFrame()).isEqualTo(expectNextFrame);
     }
 
     @Test
@@ -38,15 +40,16 @@ public class FramesTest {
                 .forEach(i -> frameList.add(normalFrame));
         frameList.add(NormalFrame.valueOf(3));
         Frames frames = new Frames(frameList);
-        int expectNextFrame = 6;
+        int expectNextFrame = 5;
         // when
 
         // then
-        Assertions.assertThat(frames.nextFrame()).isEqualTo(expectNextFrame);
+        assertThat(frames.nextFrame()).isEqualTo(expectNextFrame);
     }
 
+
     @Test
-    public void addFrame_10번째_프레임() {
+    public void addScore_10번째_프레임() throws Exception {
         // given
         int ADD_SCORE = 3;
         Frame normalFrame = NormalFrame.valueOf(Arrays.asList(3, 4));
@@ -60,11 +63,11 @@ public class FramesTest {
         Frames resultFrames = frames.addScore(ADD_SCORE);
 
         // then
-        Assertions.assertThat(resultFrames).isEqualTo(expectFrames);
+        assertThat(resultFrames).isEqualTo(expectFrames);
     }
 
     @Test
-    public void addFrame_6번째_프레임() throws Exception {
+    public void addScore_6번째_프레임() throws Exception {
         // given
         int ADD_SCORE = 3;
         Frame normalFrame = NormalFrame.valueOf(Arrays.asList(3, 4));
@@ -78,9 +81,28 @@ public class FramesTest {
 
         // when
         Frames resultFrames = frames.addScore(ADD_SCORE);
-
+        System.out.println(resultFrames);
+        System.out.println(expectFrames);
         // then
-        Assertions.assertThat(resultFrames).isEqualTo(expectFrames);
+        assertThat(resultFrames).isEqualTo(expectFrames);
     }
+
+
+    @Test
+    public void addScore_5번째_프레임() throws Exception {
+        // given
+        int ADD_SCORE = 10;
+        Frames frames = new Frames();
+        Frames expectFrames = new Frames();
+
+        // when
+        Frames resultFrames = frames.addScore(ADD_SCORE);
+
+        System.out.println(resultFrames);
+        System.out.println(expectFrames);
+        // then
+        assertThat(resultFrames).isEqualTo(expectFrames);
+    }
+
 
 }
