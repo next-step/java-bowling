9e4c0f2f7aca5eb5bd8c79a36b825a0d05301a3f
jhLim97 <wnsgur2749@naver.com>
2021-04-27 21:37:34 +0900

    feat: 점수 관련 테스트 코드 구현

--- src/test/java/bowling/FinalFrameTest.java
+++ src/test/java/bowling/FinalFrameTest.java
@@ -1,6 +1,7 @@
 package bowling;
 
 import bowling.domain.FinalFrame;
+import bowling.domain.NormalFrame;
 import bowling.domain.PinNumber;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -26,5 +27,16 @@ public class FinalFrameTest {
         finalFrame.play(new PinNumber(3));
 
         Assertions.assertThat(finalFrame.hasThird()).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("현재 프레임의 점수를 알 수 있다.")
+    void scoreInFrame() {
+        FinalFrame finalFrame = new FinalFrame();
+        finalFrame.play(new PinNumber(3));
+        finalFrame.play(new PinNumber(7));
+        finalFrame.play(new PinNumber(8));
+
+        Assertions.assertThat(finalFrame.score()).isEqualTo(18);
     }
 }
--- src/test/java/bowling/NormalFrameTest.java
+++ src/test/java/bowling/NormalFrameTest.java
@@ -1,5 +1,6 @@
 package bowling;
 
+import bowling.domain.FrameStrategy;
 import bowling.domain.NormalFrame;
 import bowling.domain.PinNumber;
 import org.assertj.core.api.Assertions;
@@ -15,5 +16,61 @@ public class NormalFrameTest {
         normalFrame.play(new PinNumber(10));
 
         Assertions.assertThat(normalFrame.hasSecond()).isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("현재 프레임이 스트라이크나 스페어가 아닌 경우 현재 프레임 내에서 점수가 결정된다.")
+    void scoreInFrameWhenIsStrikeNorSpare() {
+        NormalFrame normalFrame = new NormalFrame();
+        normalFrame.play(new PinNumber(3));
+        normalFrame.play(new PinNumber(5));
+
+        Assertions.assertThat(normalFrame.score()).isEqualTo(8);
+    }
+
+    @Test
+    @DisplayName("현재 프레임이 스페어인 경우 다음 프레임의 첫 번째 투구 점수까지 반영된다.")
+    void scoreInFrameWhenIsSpare() {
+        NormalFrame normalFrame = new NormalFrame();
+        normalFrame.play(new PinNumber(3));
+        normalFrame.play(new PinNumber(7));
+
+        int frameNumber = 1;
+        FrameStrategy nextFrame = normalFrame.nextFrame(frameNumber);
+        nextFrame.play(new PinNumber(8));
+        nextFrame.play(new PinNumber(2));
+
+        Assertions.assertThat(normalFrame.score()).isEqualTo(18);
+    }
+
+    @Test
+    @DisplayName("현재 프레임이 스트라이크인 경우 다음 프레임의 두 번째 투구 점수까지 반영된다.")
+    void scoreInFrameWhenIsStrike() {
+        NormalFrame normalFrame = new NormalFrame();
+        normalFrame.play(new PinNumber(10));
+
+        int frameNumber = 1;
+        FrameStrategy nextFrame = normalFrame.nextFrame(frameNumber);
+        nextFrame.play(new PinNumber(8));
+        nextFrame.play(new PinNumber(2));
+
+        Assertions.assertThat(normalFrame.score()).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("스트라이크가 연속 두 번 나오면 세 번째 프레임의 첫 번째 투구 점수까지 반영된다.")
+    void scoreInFrameWhenIsTwoStrike() {
+        NormalFrame normalFrame = new NormalFrame();
+        normalFrame.play(new PinNumber(10));
+        int frameNumber = 1;
+        FrameStrategy nextFrame = normalFrame.nextFrame(frameNumber);
+        nextFrame.play(new PinNumber(10));
+
+        frameNumber = 2;
+        nextFrame = nextFrame.nextFrame(frameNumber);
+        nextFrame.play(new PinNumber(5));
+        nextFrame.play(new PinNumber(5));
+
+        Assertions.assertThat(normalFrame.score()).isEqualTo(25);
     }
 }
