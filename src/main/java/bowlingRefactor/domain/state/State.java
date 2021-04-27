e73b67b31b74979d3ac3ee36e2d638329bf8c5da
hwangjeonghyeon1 <deert202@naver.com>
2021-04-28 03:14:13 +0900

    [refactor] state 값 분리

--- src/main/java/bowlingRefactor/domain/state/State.java
+++ src/main/java/bowlingRefactor/domain/state/State.java
@@ -1,2 +1,23 @@
-package bowlingRefactor.domain.state;public interface State {
+package bowlingRefactor.domain.state;
+
+import bowlingRefactor.domain.Pin;
+import bowlingRefactor.domain.Score;
+
+import java.util.List;
+
+public interface State {
+
+    State bowl(int countOfPin, boolean isLastFrame);
+
+    boolean isSpare();
+
+    int sum();
+
+    List<Pin> getPins();
+
+    Score addBonus(Score score);
+
+    Score getScore(int totalStore);
+
+    boolean isEnd();
 }
