e9b56fc58cbce0440ebe8c0cfbf4647d1c780beb
hwangjeonghyeon1 <deert202@naver.com>
2021-04-28 03:14:58 +0900

    [refactor] state 중 완료된 state

--- src/main/java/bowlingRefactor/domain/state/completeState/Last.java
+++ src/main/java/bowlingRefactor/domain/state/completeState/Last.java
@@ -1,2 +1,22 @@
-package bowlingRefactor.domain.state.completeState;public class Last {
+package bowlingRefactor.domain.state.completeState;
+
+import bowlingRefactor.domain.Pin;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Last extends Complete {
+
+    public Last(int first, int second, int third) {
+        this(Arrays.asList(Pin.of(first), Pin.of(second), Pin.of(third)));
+    }
+
+    public Last(List<Pin> pins) {
+        this.pins = pins;
+    }
+
+    @Override
+    public boolean isSpare() {
+        return false;
+    }
 }
--- src/main/java/bowlingRefactor/domain/state/completeState/Miss.java
+++ src/main/java/bowlingRefactor/domain/state/completeState/Miss.java
@@ -1,2 +1,22 @@
-package bowlingRefactor.domain.state.completeState;public class Miss {
+package bowlingRefactor.domain.state.completeState;
+
+import bowlingRefactor.domain.Pin;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Miss extends Complete {
+
+    public Miss(int first, int second) {
+        this.pins = Arrays.asList(Pin.of(first), Pin.of(second));
+    }
+
+    public Miss(List<Pin> pins) {
+        this.pins = pins;
+    }
+
+    @Override
+    public boolean isSpare() {
+        return false;
+    }
 }
