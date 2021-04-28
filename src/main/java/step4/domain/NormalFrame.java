c489b44378ceca5e27f92227204ec4cab9092eef
Beenie93 <seongbeen93@naver.com>
2021-04-29 05:43:24 +0900

    refactor: refactor NormalFrame

--- src/main/java/step4/domain/NormalFrame.java
+++ src/main/java/step4/domain/NormalFrame.java
@@ -1,98 +1,54 @@
-package step4.domain.;
+package step4.domain;
 
-import java.util.Objects;
+import step4.domain.state.Ready;
+import step4.domain.state.State;
 
-import static bowling.domain.PinCounts.*;
+import java.util.Collections;
+import java.util.List;
 
 public class NormalFrame implements Frame {
     private static final int NORMAL_FRAME_INDEX_MAX = 8;
-    private static final int NORMAL_PIN_COUNTS_SIZE_MAX = 2;
-    private static final String CANNOT_THROW_MORE_THAN_TWO = "2번을 초과하여 던질 수 없습니다.";
 
     private final int index;
-    private final PinCounts pinCounts;
+    private State state;
 
     public NormalFrame(int index) {
-        this(index, new NormalPinCounts());
+        this(index, new Ready());
     }
 
-    public NormalFrame(int index, PinCounts FrameBowls) {
+    public NormalFrame(int index, State state) {
         this.index = index;
-        this.pinCounts = FrameBowls;
+        this.state = state;
     }
 
     @Override
     public Frame throwBowl(String pinCount) {
-        if (size() == NORMAL_PIN_COUNTS_SIZE_MAX) {
-            throw new IllegalArgumentException(CANNOT_THROW_MORE_THAN_TWO);
+        state = state.bowl(Integer.parseInt(pinCount));
+
+        if (isFinished()) {
+            return next();
         }
 
-        return addBowl(pinCount);
-    }
-
-    private Frame addBowl(int pinCount) {
-        pinCounts.knockDown(pinCount);
-        return new NormalFrame(index, pinCounts);
-    }
-
-    private Frame addBowl(String pinCount) {
-        return addBowl(Integer.parseInt(pinCount));
-    }
-
-    @Override
-    public Frame next() {
-        if (!isFinished()) {
-            return this;
-        }
-
-        if (index < NORMAL_FRAME_INDEX_MAX) {
-            return new NormalFrame(index + 1, new NormalPinCounts());
-        }
-
-        return new FinalFrame();
-    }
-
-    @Override
-    public Score score() {
-        if (!isFinished()) {
-            return Score.unCountableScore();
-        }
-
-        if (pinCounts.isFirstPinCountStrike()) {
-            return Score.Strike(pinCounts.totalPinCount());
-        }
-
-        if (pinCounts.isSecondPinCountSpare()) {
-            return Score.Spare(pinCounts.totalPinCount());
-        }
-
-        return Score.Miss(pinCounts.totalPinCount());
-    }
-
-    @Override
-    public Score add(Score previousScore) {
-        int previousScoreChance = previousScore.leftOpportunity();
-        int previousScoreValue = previousScore.value();
-
-        if (size() == PIN_COUNTS_EMPTY
-                || (previousScoreChance == 2 && size() == PIN_COUNTS_SINGLE_SIZE && !pinCounts().isFirstPinCountStrike())) {
-            return Score.unCountableScore();
-        }
-
-        if (previousScoreChance == 1) {
-            return Score.Miss(pinCounts.firstPinCount().value() + previousScoreValue);
-        }
-
-        if (pinCounts().isFirstPinCountStrike()) {
-            return Score.Spare(pinCounts.totalPinCount() + previousScoreValue);
-        }
-
-        return Score.Miss(pinCounts.totalPinCount() + previousScoreValue);
+        return this;
     }
 
     @Override
     public boolean isFinished() {
-        return pinCounts.isFinished();
+        return state.isFinished();
+    }
+
+    @Override
+    public List<String> states() {
+        return Collections.singletonList(state.marks());
+    }
+
+    @Override
+    public Frame next() {
+        if (index < NORMAL_FRAME_INDEX_MAX) {
+            return new NormalFrame(index + 1);
+        }
+
+        return new FinalFrame();
     }
 
     @Override
@@ -101,21 +57,16 @@ public class NormalFrame implements Frame {
     }
 
     @Override
-    public PinCounts pinCounts() {
-        return pinCounts;
+    public Score score() {
+        return state.score();
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        NormalFrame that = (NormalFrame) o;
-        return index == that.index &&
-                Objects.equals(pinCounts, that.pinCounts);
-    }
+    public Score add(Score previousScore) {
+        if (state instanceof Ready) {
+            return Score.unCountableScore();
+        }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(index, pinCounts);
+        return state.addScore(previousScore);
     }
 }
