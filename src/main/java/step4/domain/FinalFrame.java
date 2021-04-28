8b245ad3aa03ac23e96d7a955725df9b82ca21a1
Beenie93 <seongbeen93@naver.com>
2021-04-29 05:43:44 +0900

    refactor: refactor FinalFrame

--- src/main/java/step4/domain/FinalFrame.java
+++ src/main/java/step4/domain/FinalFrame.java
@@ -1,65 +1,65 @@
-package step4.domain.;
+package step4.domain;
 
-import static bowling.domain.PinCounts.*;
+import step4.domain.state.Ready;
+import step4.domain.state.Spare;
+import step4.domain.state.State;
+import step4.domain.state.Strike;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class FinalFrame implements Frame {
     private static final int FINAL_INDEX = 9;
-    private static final String CANNOT_THROW_THIRD_BOWL = "3구를 던질수 없습니다.";
+    private static final int START = 0;
+    private static final int TRY_NUMBER_MAX = 3;
 
-    private final PinCounts pinCounts;
+    private LinkedList<State> states = new LinkedList<>();
+    private int tryNumber;
 
     public FinalFrame() {
-        this(new FinalPinCounts());
-    }
-
-    private FinalFrame(PinCounts pinCounts) {
-        this.pinCounts = pinCounts;
+        states.add(new Ready());
+        tryNumber = START;
     }
 
     @Override
     public Frame throwBowl(String pinCount) {
-        if (size() == PIN_COUNTS_EMPTY) {
-            return addBowl(pinCount);
+        tryNumber += 1;
+        int knockedDownPinCount = Integer.parseInt(pinCount);
+
+        State currentState = states.getLast();
+
+        if (currentState.isFinished()) {
+            states.add(new Ready().bowl(knockedDownPinCount));
+            return this;
         }
 
-        if (size() == PIN_COUNTS_SINGLE_SIZE) {
-            return addBowl(pinCount);
-        }
-
-        if (isThirdThrowPossible()) {
-            return addBowl(pinCount);
-        }
-
-        throw new IllegalArgumentException(CANNOT_THROW_THIRD_BOWL);
-    }
-
-    private boolean isThirdThrowPossible() {
-        return pinCounts.isFirstPinCountStrike()
-                || (!pinCounts.isFirstPinCountStrike() && pinCounts.totalPinCount() == TOTAL_PIN_COUNT_MAX);
-    }
-
-    private Frame addBowl(int pinPinCount) {
-        pinCounts.knockDown(pinPinCount);
-        return new FinalFrame(pinCounts);
-    }
-
-    private Frame addBowl(String firstPinCount) {
-        return addBowl(Integer.parseInt(firstPinCount));
+        states.removeLast();
+        states.add(currentState.bowl(knockedDownPinCount));
+        return this;
     }
 
     @Override
     public boolean isFinished() {
-        return pinCounts.isFinished();
+        return tryNumber == TRY_NUMBER_MAX
+                || (tryNumber == TRY_NUMBER_MAX - 1 && !isThirdBowlAvailable());
+    }
+
+    private boolean isThirdBowlAvailable() {
+        return states.stream()
+                .anyMatch(this::isStrikeOrSpare);
+    }
+
+    private boolean isStrikeOrSpare(State state) {
+        return state instanceof Strike
+                || state instanceof Spare;
     }
 
     @Override
-    public int index() {
-        return FINAL_INDEX;
-    }
-
-    @Override
-    public PinCounts pinCounts() {
-        return pinCounts;
+    public List<String> states() {
+        return states.stream()
+                .map(State::marks)
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -68,30 +68,36 @@ public class FinalFrame implements Frame {
     }
 
     @Override
+    public int index() {
+        return FINAL_INDEX;
+    }
+
+    @Override
     public Score score() {
         if (!isFinished()) {
             return Score.unCountableScore();
         }
 
-        return Score.Miss(pinCounts.totalPinCount());
+        Score score = states.getFirst().score();
+
+        for (int i = 1; i < states.size(); i++) {
+            State state = states.get(i);
+            score = state.addScore(score);
+        }
+
+        return score;
     }
 
     @Override
-    public Score add(Score previousScore) {
-        int previousScoreChance = previousScore.leftOpportunity();
-        int previousScoreValue = previousScore.value();
-
-        if (size() == PIN_COUNTS_EMPTY
-                || (previousScoreChance == 2 && size() <= PIN_COUNTS_SINGLE_SIZE)) {
+    public Score add(Score score) {
+        if ((states.getFirst() instanceof Ready)) {
             return Score.unCountableScore();
         }
 
-        if (previousScoreChance == 1) {
-            return Score.Miss(pinCounts.firstPinCount().value() + previousScoreValue);
+        for (int i = 0; i < score.leftOpportunity(); i++) {
+            score = states.get(i).addScore(score);
         }
 
-        return Score.Miss(pinCounts.firstPinCount().value()
-                + pinCounts.secondPinCount().value()
-                + previousScoreValue);
+        return score;
     }
 }
