acbd702bbd7c9af98808ecbbebab9b8b62910ff5
ttokey <ttokey.lee@gmail.com>
2021-04-28 19:13:43 +0900

    feat : InputView 클래스 구현

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -1,4 +1,23 @@
 package bowling.view;
 
+import java.util.Scanner;
+
 public class InputView {
+    private final static String GET_PLAYER_NAME = "플레이어 이름은(3 english letters)? : ";
+    private final static String GET_FRAME_SCORE = "프레임 투구 : ";
+
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static String getPlayerName() {
+        System.out.println(GET_PLAYER_NAME);
+        return scanner.nextLine();
+    }
+
+    public static int getScore(int nowFrame) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(nowFrame)
+                .append(GET_FRAME_SCORE);
+        System.out.println(sb);
+        return Integer.parseInt(scanner.nextLine());
+    }
 }
