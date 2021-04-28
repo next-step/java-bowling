442812a75034b75c085351921d8aa71ca3d59a68
wkdehdlr <wkdehdlr@gmail.com>
2021-04-28 21:30:24 +0900

    [feat]Answer 삭제 테스트

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,29 @@
 package qna.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    void delete() {
+        A1.delete();
+
+        assertTrue(A1.isDeleted());
+    }
+
+    @Test
+    void writeHistory() {
+        List<DeleteHistory> deleteHistories = new ArrayList<>();
+
+        A1.writeHistory(deleteHistories);
+
+        assertEquals(A1.getId(), deleteHistories.get(0).getContentId());
+    }
 }
