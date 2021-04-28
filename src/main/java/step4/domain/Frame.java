24eea2d5511857ba396fe31732355012a88f8266
Beenie93 <seongbeen93@naver.com>
2021-04-29 05:42:23 +0900

    refactor: refactor Frame interface

--- src/main/java/step4/domain/Frame.java
+++ src/main/java/step4/domain/Frame.java
@@ -1,80 +1,19 @@
-package step4.domain.;
+package step4.domain;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-public class Frames {
-    private static final int FINAL_FRAME_INDEX = 9;
-    private static final int FIRST_INDEX = 0;
+public interface Frame {
+    Frame throwBowl(String pinCount);
 
-    private final List<Frame> frames;
-    private int currentIndex;
+    int index();
 
-    public Frames() {
-        this(init());
-    }
+    Frame next();
 
-    public Frames(List<Frame> frames) {
-        this.frames = frames;
-        this.currentIndex = FIRST_INDEX;
-    }
+    Score score();
 
-    private static List<Frame> init() {
-        List<Frame> frames = IntStream.range(0, FINAL_FRAME_INDEX)
-                .mapToObj(NormalFrame::new)
-                .collect(Collectors.toList());
+    Score add(Score score);
 
-        frames.add(new FinalFrame());
+    boolean isFinished();
 
-        return frames;
-    }
-
-    public int currentIndex() {
-        return currentIndex;
-    }
-
-    public void throwBowl(String pinCount) {
-        Frame Frame = get(currentIndex);
-        Frame = Frame.throwBowl(pinCount);
-        update(Frame);
-        Frame = Frame.next();
-        currentIndex = Frame.index();
-    }
-
-    public boolean isAllFinished() {
-        return frames.stream()
-                .allMatch(Frame::isFinished);
-    }
-
-    public Frame get(int index) {
-        return frames.get(index);
-    }
-
-    public void update(Frame frame) {
-        frames.set(frame.index(), frame);
-    }
-
-    public Marks marks() {
-        return new Marks(frames.stream()
-                .map(Mark::new)
-                .collect(Collectors.toList()));
-    }
-
-    public Scores scores() {
-        return new Scores(frames.stream()
-                .map(this::score)
-                .collect(Collectors.toList()));
-    }
-
-    private Score score(Frame frame) {
-        Score score = frame.score();
-
-        while (score.isOpportunityLeft()) {
-            frame = frames.get(frame.next().index());
-            score = frame.add(score);
-        }
-
-        return score;
-    }
+    List<String> states();
 }
