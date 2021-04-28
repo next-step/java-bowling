cfeecf207ed4f78ec1418686248e796e3724bb56
jhLim97 <wnsgur2749@naver.com>
2021-04-28 12:17:10 +0900

    feat: 최대 점수 300점에 대한 테스트 코드 및 로직 구현

--- src/main/java/bowling/domain/Score.java
+++ src/main/java/bowling/domain/Score.java
@@ -1,9 +1,15 @@
 package bowling.domain;
 
 public class Score {
+
+    private static final int MAX_SCORE = 300;
+
     private final int score;
 
     public Score(int score) {
+        if (score > MAX_SCORE) {
+            throw new IllegalArgumentException("가능한 최대 점수는 300점입니다.");
+        }
         this.score = score;
     }
 
