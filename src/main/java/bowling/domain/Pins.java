cab1fbecb0988f292413244cce3d0fde588265b6
punsoo <ryxhdrnrmf@gmail.com>
2021-04-28 14:39:46 +0900

    refactor: Pins 캐싱 구현

--- src/main/java/bowling/domain/Pins.java
+++ src/main/java/bowling/domain/Pins.java
@@ -3,34 +3,39 @@ package bowling.domain;
 import bowling.domain.exception.PinsCountException;
 
 import java.util.Objects;
+import java.util.stream.IntStream;
 
 public class Pins {
     private static final int MAX_PINS = 10;
     private static final int MIN_PINS = 0;
+    private static final Pins[] CACHE = IntStream.rangeClosed(MIN_PINS, MAX_PINS)
+            .mapToObj(Pins::new)
+            .toArray(Pins[]::new);
 
     private final int pins;
 
     private Pins(int pitch) {
-        validatePitch(pitch);
         this.pins = pitch;
     }
 
-    private void validatePitch(int pitch) {
+    private static void validatePitch(int pitch) {
         if (pitch < MIN_PINS || pitch > MAX_PINS) {
             throw new PinsCountException();
         }
     }
 
     public static Pins ofFirstPitch(int firstPitch) {
-        return new Pins(firstPitch);
+        validatePitch(firstPitch);
+        return CACHE[firstPitch];
     }
 
     public Pins ofSecondPitch(int secondPitch) {
         validatePitch(this.pins + secondPitch);
-        return new Pins(secondPitch);
+        validatePitch(secondPitch);
+        return CACHE[secondPitch];
     }
 
-    public int pins(){
+    public int pins() {
         return this.pins;
     }
 
@@ -38,8 +43,8 @@ public class Pins {
         return pins == MAX_PINS;
     }
 
-    public boolean isSpare(Pins secondPins){
-        if(pins() + secondPins.pins() == MAX_PINS){
+    public boolean isSpare(Pins secondPins) {
+        if (pins() + secondPins.pins() == MAX_PINS) {
             return true;
         }
         return false;
