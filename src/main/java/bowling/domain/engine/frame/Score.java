d9bbd18e2abc6e50ad9e2ec84d3daca47ab385d2
Jake <cocahack@naver.com>
2021-04-29 20:54:06 +0900

    feat: 사용 불가능한 점수를 추상화한 클래스 구현

--- src/main/java/bowling/domain/engine/frame/Score.java
+++ src/main/java/bowling/domain/engine/frame/Score.java
@@ -47,4 +47,30 @@ public class Score {
         return injectionCount == 0;
     }
 
+    public static final class UnavailableScore extends Score {
+
+        protected UnavailableScore(int value, int injectionCount) {
+            super(value, injectionCount);
+        }
+
+        public static UnavailableScore init() {
+            return new UnavailableScore(-1, -1);
+        }
+
+        @Override
+        public Score inject(RollResult rollResult) {
+            throw new IllegalStateException("점수 계산이 허용되지 않습니다.");
+        }
+
+        @Override
+        public int getValue() {
+            throw new IllegalStateException("점수 산정을 허용하지 않습니다.");
+        }
+
+        @Override
+        public boolean isCalculationCompleted() {
+            return false;
+        }
+    }
+
 }
--- src/test/java/bowling/domain/engine/frame/ScoreTest.java
+++ src/test/java/bowling/domain/engine/frame/ScoreTest.java
@@ -1,6 +1,7 @@
 package bowling.domain.engine.frame;
 
 import bowling.domain.RollResult;
+import bowling.domain.engine.frame.Score.UnavailableScore;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -61,5 +62,22 @@ class ScoreTest {
 
         assertThat(spareStrike.getValue()).isEqualTo(15);
     }
+
+    @Test
+    @DisplayName("UnavailableScore 는 점수 계산 관련 인터페이스를 사용할 수 없다.")
+    void cannotUseInterfacesOfUnavailableScore() {
+        UnavailableScore score = UnavailableScore.init();
+        assertAll(
+            () -> assertThatThrownBy(() -> score.inject(RollResult.of(10))).isInstanceOf(IllegalStateException.class),
+            () -> assertThatThrownBy(score::getValue).isInstanceOf(IllegalStateException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("UnavailableScore 는 항상 점수 계산이 완료되지 않은 상태로 남아있다.")
+    void alwaysUnableCompleteScore() {
+        assertThat(UnavailableScore.init().isCalculationCompleted()).isFalse();
+    }
+
 
 }
