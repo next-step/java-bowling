bea7660ec17a32416b50713a6c2bb6249f7b0385
김우재 <kwj1270@naver.com>
2021-04-29 20:12:38 +0900

    feat:Pins 인스턴스가 캐싱된 객체를 반환하는 테스트작성

--- src/main/java/bowling/domain/Pins.java
+++ src/main/java/bowling/domain/Pins.java
@@ -2,30 +2,38 @@ package bowling.domain;
 
 import bowling.exception.OverHitCountException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.IntStream;
 
 public final class Pins {
 
     private static final int MINIMUM_COUNT = 0;
     private static final int MAXIMUM_COUNT = 10;
 
+    private static final List<Pins> CACHE;
+
     private int remain;
 
-    public static final Pins init() {
-        return new Pins();
-    }
-
-    private Pins() {
-        this(MAXIMUM_COUNT);
+    static {
+        CACHE = new ArrayList<>();
+        for (int i = MINIMUM_COUNT; i <= MAXIMUM_COUNT; i++) {
+            CACHE.add(new Pins(i));
+        }
     }
 
     private Pins(final int remain) {
         this.remain = remain;
     }
 
+    public static final Pins init() {
+        return CACHE.get(MAXIMUM_COUNT);
+    }
+
     public final Pins hit(final int hitCount) {
         validateOverCount(hitCount);
-        return new Pins(Math.subtractExact(remain, hitCount));
+        return CACHE.get(Math.subtractExact(remain, hitCount));
     }
 
     private final void validateOverCount(final int hitCount) {
