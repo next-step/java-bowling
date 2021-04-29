ac9d468be0d36400a716bc482660e6b6a3bbdafb
punsoo <ryxhdrnrmf@gmail.com>
2021-04-29 20:44:01 +0900

    feat: InputView 구현

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -4,18 +4,24 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
-    private static final String REQUEST_PLAYER_NAME = "플레이어 이름은(3 english letters)?: ";
-    private static final String FRAME_PITCH = "%d프레임 투구 : ";
+    private static final String REQUEST_NUMBER_OF_PEOPLE = "How many people? ";
+    private static final String REQUEST_PLAYER_NAME = "플레이어 %d의 이름은(3 english letters)?: ";
+    private static final String WHOSE_TURN = "%s's turn : ";
 
     private InputView() {}
+
+    public static String requestNumberOfPeople() {
+        System.out.print(REQUEST_NUMBER_OF_PEOPLE);
+        return SCANNER.nextLine();
+    }
 
     public static String requestPlayerName() {
         System.out.print(REQUEST_PLAYER_NAME);
         return SCANNER.nextLine();
     }
 
-    public static int requestFallingPins(int frameNo) {
-        System.out.printf(FRAME_PITCH, frameNo);
+    public static int requestFallingPins(String playerName) {
+        System.out.printf(WHOSE_TURN, playerName);
         return Integer.parseInt(SCANNER.nextLine());
     }
 }
