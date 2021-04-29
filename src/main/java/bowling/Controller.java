337e315892c5bc722821688635e890357ed9af1e
Dongjoon Park <djoon@users.noreply.github.com>
2021-04-30 01:13:54 +0900

    feat: Controller와 사용자 입력 받는 객체 추가

---
+++ src/main/java/bowling/Controller.java
@@ -0,0 +1,11 @@
+package bowling;
+
+import bowling.ui.Placeholder;
+
+public class Controller {
+    public static void main(String[] args) {
+        Placeholder placeholder = new Placeholder();
+        placeholder.printNamingMessage();
+        placeholder.inputFromUser();
+    }
+}
---
+++ src/main/java/bowling/ui/Placeholder.java
@@ -0,0 +1,19 @@
+package bowling.ui;
+
+import java.util.Scanner;
+
+public class Placeholder {
+    private final Scanner scanner;
+
+    public Placeholder() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String inputFromUser() {
+        return scanner.nextLine();
+    }
+
+    public void printNamingMessage() {
+        System.out.print("플레이어 이름은(3 english letters)?: ");
+    }
+}
