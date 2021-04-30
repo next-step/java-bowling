19393e8d6bcc8364b67e2e66a75e12989315a6bf
jaystar13 <astarisk2@gmail.com>
2021-04-30 15:01:26 +0900

    test: 테스트 케이스 추가

--- src/test/java/bowling/domain/FinalFrameTypeTest.java
+++ src/test/java/bowling/domain/FinalFrameTypeTest.java
@@ -3,6 +3,7 @@ package bowling.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class FinalFrameTypeTest {
@@ -16,5 +17,54 @@ public class FinalFrameTypeTest {
             frameType.pitch(10);
             frameType.pitch(11);
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("스트라이크의 경우 프레임을 진행한다.")
+    @Test
+    void isContinue_strike() {
+        FrameType frameType = new FinalFrameType();
+        frameType.pitch(10);
+
+        assertThat(frameType.isContinue()).isTrue();
+    }
+
+    @DisplayName("스페어의 경우 프레임을 진행한다.")
+    @Test
+    void isContinue_spare() {
+        FrameType frameType = new FinalFrameType();
+        frameType.pitch(8);
+        frameType.pitch(2);
+
+        assertThat(frameType.isContinue()).isTrue();
+    }
+
+    @DisplayName("세번째 투구까지 완료한 경우 프레임을 종료한다.")
+    @Test
+    void isContinue_last() {
+        FrameType frameType = new FinalFrameType();
+        frameType.pitch(7);
+        frameType.pitch(3);
+        frameType.pitch(10);
+
+        assertThat(frameType.isContinue()).isFalse();
+    }
+
+    @DisplayName("첫번째 투구한 경우 프레임을 진행한다.")
+    @Test
+    void isContinue_firstPitch() {
+        FrameType frameType = new FinalFrameType();
+        frameType.pitch(8);
+
+        assertThat(frameType.isContinue()).isTrue();
+    }
+
+    @DisplayName("두번째 투구가 오픈의 경우 프레임을 종료한다.")
+    @Test
+    void isContinue_open() {
+        FrameType frameType = new FinalFrameType();
+        frameType.pitch(1);
+        frameType.pitch(3);
+
+        assertThat(frameType.isContinue()).isFalse();
     }
 }
--- src/test/java/bowling/domain/NormalFrameTypeTest.java
+++ src/test/java/bowling/domain/NormalFrameTypeTest.java
@@ -3,6 +3,7 @@ package bowling.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class NormalFrameTypeTest {
@@ -16,5 +17,43 @@ public class NormalFrameTypeTest {
             frameType.pitch(8);
             frameType.pitch(3);
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("스트라이크의 경우 프레임을 종료한다.")
+    @Test
+    void isContinue_strike() {
+        FrameType frameType = new NormalFrameType();
+        frameType.pitch(10);
+
+        assertThat(frameType.isContinue()).isFalse();
+    }
+
+    @DisplayName("스페어의 경우 프레임을 종료한다.")
+    @Test
+    void isContinue_spare() {
+        FrameType frameType = new NormalFrameType();
+        frameType.pitch(8);
+        frameType.pitch(2);
+
+        assertThat(frameType.isContinue()).isFalse();
+    }
+
+    @DisplayName("두번째 투구까지 완료한 경우 프레임을 종료한다.")
+    @Test
+    void isContinue_open() {
+        FrameType frameType = new NormalFrameType();
+        frameType.pitch(7);
+        frameType.pitch(2);
+
+        assertThat(frameType.isContinue()).isFalse();
+    }
+
+    @DisplayName("첫번째 투구한 경우 프레임을 진행한다.")
+    @Test
+    void isContinue_firstPitch() {
+        FrameType frameType = new NormalFrameType();
+        frameType.pitch(8);
+
+        assertThat(frameType.isContinue()).isTrue();
     }
 }
