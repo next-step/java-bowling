16f7757c36ea218dd8e6b24fac1e156b11b87fc6
jaystar13 <astarisk2@gmail.com>
2021-04-30 14:59:01 +0900

    feat: 투구 상태 판단 처리를 위한 메서드 추가

--- src/main/java/bowling/domain/Pitch.java
+++ src/main/java/bowling/domain/Pitch.java
@@ -1,12 +1,47 @@
 package bowling.domain;
 
+import bowling.domain.status.*;
+
 import java.util.Objects;
 
 public class Pitch {
     private final int fallenPins;
+    private Status status;
 
     public Pitch(int fallenPins) {
         this.fallenPins = fallenPins;
+    }
+
+    public Pitch(int fallenPins, Status status) {
+        this.fallenPins = fallenPins;
+        this.status = status;
+    }
+
+    public Pitch pitch(int fallenPins, int pitchIndex) {
+        if ((fallenPins == 10 && pitchIndex == 0)
+                || (this.fallenPins + fallenPins == 20)
+                || (fallenPins == 10 && pitchIndex == 2)) {
+            return new Pitch(fallenPins, new Strike());
+        }
+        if (this.fallenPins + fallenPins == 10) {
+            return new Pitch(fallenPins, new Spare());
+        }
+        if (this.fallenPins + fallenPins < 10 && pitchIndex == 1) {
+            return new Pitch(fallenPins, new Open());
+        }
+        return new Pitch(fallenPins, new Default());
+    }
+
+    public boolean isStrike() {
+        return status.isStrike();
+    }
+
+    public boolean isSpare() {
+        return status.isSpare();
+    }
+
+    public boolean isOpen() {
+        return status.isOpen();
     }
 
     public static Pitch sum(Pitch previous, Pitch current) {
@@ -15,6 +50,10 @@ public class Pitch {
 
     public int intValue() {
         return fallenPins;
+    }
+
+    public Status status() {
+        return status;
     }
 
     @Override
