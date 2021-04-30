05eb46e37c9a5f5584a8452004767f2dc371ba94
Younghyun <y.jo@navercorp.com>
2021-05-01 06:59:17 +0900

    feat(step2):FrameResult, PointSymbols에 equals, hahsCode  추가

--- src/main/java/bowling/domain/FrameResult.java
+++ src/main/java/bowling/domain/FrameResult.java
@@ -1,7 +1,13 @@
 package bowling.domain;
 
+import java.util.Objects;
+
 public class FrameResult {
     private final PointSymbols pointSymbols;
+
+    public FrameResult() {
+        pointSymbols = new PointSymbols();
+    }
 
     public FrameResult(PointSymbols pointSymbols) {
         this.pointSymbols = pointSymbols;
@@ -9,5 +15,18 @@ public class FrameResult {
 
     public PointSymbols pointSymbols() {
         return pointSymbols;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FrameResult that = (FrameResult) o;
+        return Objects.equals(pointSymbols, that.pointSymbols);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pointSymbols);
     }
 }
--- src/main/java/bowling/domain/PointSymbols.java
+++ src/main/java/bowling/domain/PointSymbols.java
@@ -4,6 +4,7 @@ import bowling.domain.Pinfall;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class PointSymbols {
     private final List<PointSymbol> symbols;
@@ -12,6 +13,12 @@ public class PointSymbols {
         this();
         symbols.add(symbol);
     }
+    public PointSymbols(PointSymbol first, PointSymbol second) {
+        this();
+        symbols.add(first);
+        symbols.add(second);
+    }
+
 
     public PointSymbols() {
         symbols = new ArrayList<>();
@@ -23,5 +30,18 @@ public class PointSymbols {
 
     public List<PointSymbol> symbols() {
         return symbols;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PointSymbols that = (PointSymbols) o;
+        return Objects.equals(symbols, that.symbols);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(symbols);
     }
 }
