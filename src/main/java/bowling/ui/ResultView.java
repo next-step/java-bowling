c1b79da667374b43fb86c7be2f4b9ff909775fda
Donghui-Lee <ehdgml3206@naver.com>
2021-04-30 16:30:33 +0900

    refactor: 상태 출력 변환에 따른 ResultView 수정

--- src/main/java/bowling/ui/ResultView.java
+++ src/main/java/bowling/ui/ResultView.java
@@ -1,15 +1,16 @@
 package bowling.ui;
 
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import bowling.domain.BowlingTurn;
+import bowling.domain.frame.Frame;
 
 public class ResultView {
     private static final int LAST_FRAME = 10;
-    private static final int FIRST_FRAME = 1;
     private static final String PARTITION = "|";
+    private static final String FRAME_TITLE = "| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |";
+
+
 
     public static void printInitBowlingBoard(BowlingTurn bowlingTurn) {
         printTitleFrames();
@@ -19,22 +20,12 @@ public class ResultView {
 
     public static void printBowlingBoard(BowlingTurn bowlingTurn) {
         printTitleFrames();
-        printScoreFrames(bowlingTurn);
-        printEmptyFrames(bowlingTurn.frames().size());
-    }
-
-    private static void printScoreFrames(BowlingTurn bowlingTurn) {
-        System.out.print(PARTITION + formatting(bowlingTurn.player()));
-        bowlingTurn.frames().stream()
-            .map(frame -> formatting(frame.scoreResult()))
-            .forEach(System.out::print);
+        printFrameStates(bowlingTurn);
+        printEmptyFrames(bowlingTurn.currentFrameSize());
     }
 
     private static void printTitleFrames() {
-        String name = "| NAME |";
-        String frames = IntStream.range(FIRST_FRAME, LAST_FRAME).
-            mapToObj(i -> "  0" + i + "  ").collect(Collectors.joining(PARTITION));
-        System.out.println(name + frames + "|  10  |");
+        System.out.println(FRAME_TITLE);
     }
 
     private static void printEmptyFrames(int size) {
@@ -42,6 +33,15 @@ public class ResultView {
             .limit(LAST_FRAME - size)
             .forEach(System.out::print);
         System.out.println("\n");
+    }
+
+    private static void printFrameStates(BowlingTurn bowlingTurn) {
+        System.out.print(PARTITION + formatting(bowlingTurn.player()));
+
+        bowlingTurn.frames().stream()
+            .map(Frame::frameState)
+            .map(ResultView::formatting)
+            .forEach(System.out::print);
     }
 
     private static String formatting(String input) {
