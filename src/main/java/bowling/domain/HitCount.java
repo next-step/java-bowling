e6abc997d193bfd41489369e727f7aa161f87914
김우재 <kwj1270@naver.com>
2021-04-30 20:50:21 +0900

    feat:HitCount 인스턴스에 범위를 벗어난 값 입력시 예외처리 기능 구현

--- src/main/java/bowling/domain/HitCount.java
+++ src/main/java/bowling/domain/HitCount.java
@@ -1,14 +1,28 @@
 package bowling.domain;
 
+import bowling.exception.InputNumberOutOfBoundsException;
+
 public final class HitCount {
 
-    private final int count;
+    private static final int MINIMUM_COUNT = 0;
+    private static final int MAXIMUM_COUNT = 10;
 
-    private HitCount(final int count) {
-        this.count = count;
-    }
+    private final int count;
 
     public static final HitCount valueOf(final int count) {
         return new HitCount(count);
     }
+
+    private HitCount(final int count) {
+        validateBounds(count);
+        this.count = count;
+    }
+
+    private final void validateBounds(final int count) {
+        if (count < MINIMUM_COUNT || count > MAXIMUM_COUNT) {
+            throw new InputNumberOutOfBoundsException(count);
+        }
+    }
+
+
 }
