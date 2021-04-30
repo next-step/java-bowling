23a66cddfa48588ae456c48e4a9aa1a48d8c883a
Dongjoon Park <djoon@users.noreply.github.com>
2021-04-30 17:18:48 +0900

    test: ScoreTest 추가, NormalFrameTest 삭제

--- src/test/java/bowling/domain/ScoreTest.java
+++ src/test/java/bowling/domain/ScoreTest.java
@@ -6,65 +6,65 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class NormalFrameTest {
+public class ScoreTest {
     @Test
     @DisplayName("1 프레임에 점수 입력은 2번만 가능하다.")
     void inputScoreLimitTest() {
-        NormalFrame normalFrame = new NormalFrame();
+        Score score = new Score(10, 2);
 
-        normalFrame.inputScore("3");
-        normalFrame.inputScore("4");
+        score.updateScore(3);
+        score.updateScore(4);
 
-        assertThatThrownBy(() -> normalFrame.inputScore("3"))
+        assertThatThrownBy(() -> score.updateScore(3))
                 .isInstanceOf(IllegalStateException.class);
     }
 
     @Test
     @DisplayName("남아있는 볼링핀이 없는 경우, 점수 입력이 불가능하다.")
     void cannotInputScoreIfNoPins() {
-        NormalFrame normalFrame = new NormalFrame();
+        Score score = new Score(10, 2);
 
-        normalFrame.inputScore("10");
+        score.updateScore(10);
 
-        assertThatThrownBy(() -> normalFrame.inputScore("3"))
+        assertThatThrownBy(() -> score.updateScore(3))
                 .isInstanceOf(IllegalStateException.class);
     }
 
     @Test
     @DisplayName("0을 입력한 경우, 거터,미스인 '-'를 반환한다.")
     void inputStrikeValueTest() {
-        NormalFrame normalFrame = new NormalFrame();
+        Score score = new Score(10, 2);
 
-        assertThat(normalFrame.inputScore("0")).isEqualTo("-");
+        assertThat(score.updateScore(0)).isEqualTo("-");
     }
 
     @Test
     @DisplayName("점수를 2번 입력 후, 그대로 출력되는지 확인한다.")
     void inputTwoTimes() {
-        NormalFrame normalFrame = new NormalFrame();
+        Score score = new Score(10, 2);
 
-        assertThat(normalFrame.inputScore("7")).isEqualTo("7");
-        assertThat(normalFrame.inputScore("2")).isEqualTo("2");
+        assertThat(score.updateScore(7)).isEqualTo("7");
+        assertThat(score.updateScore(2)).isEqualTo("2");
     }
 
     @Test
     @DisplayName("점수를 2번 입력 후, 스페어면 /가 출력되는지 확인한다.")
     void inputSpareTest() {
-        NormalFrame normalFrame = new NormalFrame();
+        Score score = new Score(10, 2);
 
-        normalFrame.inputScore("8");
+        score.updateScore(8);
 
-        assertThat(normalFrame.inputScore("2")).isEqualTo("/");
+        assertThat(score.updateScore(2)).isEqualTo("/");
     }
 
     @Test
     @DisplayName("점수가 10을 넘을 경우, 예외를 발생시킨다.")
     void inputOverScoreTest() {
-        NormalFrame normalFrame = new NormalFrame();
+        Score score = new Score(10, 2);
 
-        normalFrame.inputScore("9");
+        score.updateScore(9);
 
-        assertThatThrownBy(() -> normalFrame.inputScore("2"))
+        assertThatThrownBy(() -> score.updateScore(2))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
