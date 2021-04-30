81a9a29095eb4e190aea81a3edfadd5d0cc74744
jaystar13 <astarisk2@gmail.com>
2021-04-30 17:44:59 +0900

    test: Pitch 테스트 케이스 추가

--- src/test/java/bowling/domain/PitchTest.java
+++ src/test/java/bowling/domain/PitchTest.java
@@ -4,12 +4,47 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PitchTest {
-    @DisplayName("쓰러진 핀을 인자로 받아 인스턴스가 정상적으로 생성된다.")
+    @DisplayName("0개 이하의 핀을 쓰러트리는 경우 예외를 반환한다.")
     @Test
-    void create() {
-        Pitch pitch = new Pitch(8);
-        assertThat(pitch).isEqualTo(new Pitch(8));
+    void underZero() {
+        assertThatThrownBy(() -> new Pitch(-1)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("0개 이하의 핀을 쓰러트리는 경우 예외를 반환한다.")
+    @Test
+    void upTen() {
+        assertThatThrownBy(() -> new Pitch(11)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("스트라이크 투구")
+    @Test
+    void strike() {
+        Pitch initPitch = new Pitch(0);
+        Pitch firstPitch = initPitch.pitch(10, 0);
+
+        assertThat(firstPitch.isStrike()).isTrue();
+    }
+
+    @DisplayName("스페어 투구")
+    @Test
+    void spare() {
+        Pitch initPitch = new Pitch(0);
+        Pitch firstPitch = initPitch.pitch(8, 0);
+        Pitch secondPitch = firstPitch.pitch(2, 1);
+
+        assertThat(secondPitch.isSpare()).isTrue();
+    }
+
+    @DisplayName("오픈 투구")
+    @Test
+    void open() {
+        Pitch initPitch = new Pitch(0);
+        Pitch firstPitch = initPitch.pitch(0, 0);
+        Pitch secondPitch = firstPitch.pitch(0, 1);
+
+        assertThat(secondPitch.isOpen()).isTrue();
     }
 }
