dd0c3460d4983dce45328caa5deddd10fc24e540
김우재 <kwj1270@naver.com>
2021-04-30 21:22:45 +0900

    test:State 인터페이스 생성 여부 테스트작성

---
+++ src/test/java/bowling/domain/state/StateTest.java
@@ -0,0 +1,31 @@
+package bowling.domain.state;
+
+import bowling.domain.HitCount;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StateTest {
+
+    @DisplayName("State 인터페이스 생성 여부 테스트")
+    @Test
+    void 생성() {
+        State state = new State() {
+            @Override
+            public boolean isFinish() {
+                return true;
+            }
+
+            @Override
+            public State bowl(HitCount hitCount) {
+                return null;
+            }
+        };
+
+        assertThat(state).isNotNull();
+
+    }
+
+}
