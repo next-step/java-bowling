f718645411d7caef1f80663ac21a4c072fabd600
Jake <cocahack@naver.com>
2021-04-30 23:39:59 +0900

    feat: 플레이어의 특정 프레임 플레이 여부 확인 기능 추가

---
+++ src/test/java/bowling/domain/PlayerTest.java
@@ -0,0 +1,28 @@
+package bowling.domain;
+
+import bowling.domain.engine.frame.FrameNumber;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PlayerTest {
+
+    private Player player;
+
+    @BeforeEach
+    void setUp() {
+        player = Player.initialize("test");
+    }
+
+    @Test
+    @DisplayName("프레임 번호를 입력 받으면 그 프레임에서 투구를 종료했는지 여부를 반환한다.")
+    void returnFrameFinishedGivenFrameNumber() {
+        assertThat(player.isPlayedFrameOf(FrameNumber.firstFrame())).isFalse();
+
+        player.roll(RollResult.of(10));
+
+        assertThat(player.isPlayedFrameOf(FrameNumber.firstFrame())).isTrue();
+    }
+}
