1538a05dfeff261f7ef87d430ded8aa449c514aa
povia <47127607+povia@users.noreply.github.com>
2021-04-30 19:25:54 +0900

    feat: 사용자 생성

---
+++ src/main/java/bowling/domain/Player.java
@@ -0,0 +1,24 @@
+package bowling.domain;
+
+import bowling.error.NameInvalidLengthException;
+
+public class Player {
+  private static final int MAX_LENGTH = 3;
+
+  private final String name;
+
+  public Player(String name){
+    checkValidatedNameLength(name);
+    this.name = name;
+  }
+
+  public void checkValidatedNameLength(String name){
+    if(name.isEmpty() || name.length() != MAX_LENGTH){
+      throw new NameInvalidLengthException("");
+    }
+  }
+
+  public String name() {
+    return name;
+  }
+}
