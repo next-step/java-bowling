2ea9b3903638a38812555e6df9bd9edd4564cb61
Dongjoon Park <djoon@users.noreply.github.com>
2021-04-30 17:14:07 +0900

    refactor: NormalFrame에서 점수 객체 Score 분리

--- src/main/java/bowling/domain/NormalFrame.java
+++ src/main/java/bowling/domain/NormalFrame.java
@@ -1,37 +1,13 @@
 package bowling.domain;
 
 public class NormalFrame {
-    private int pins;
-    private int available;
+    private Score score;
 
     public NormalFrame() {
-        pins = 10;
-        available = 2;
+        this.score = new Score(10, 2);
     }
 
-    public String inputScore(String score) {
-        if (available <= 0) {
-            throw new IllegalStateException("점수를 더 이상 입력할 수 없습니다.");
-        }
-        available = available - 1;
-        return scoreFormat(Integer.parseInt(score));
-    }
-
-    private String scoreFormat(int score) {
-        if (score == 0) {
-            return "-";
-        }
-        pins = pins - score;
-        if (pins < 0) {
-            throw new IllegalArgumentException("남아있는 볼링핀 개수보다 입력한 점수가 더 많습니다.");
-        }
-        if (available == 1 && pins == 0) {
-            available = 0;
-            return "X";
-        }
-        if (available == 0 && pins == 0) {
-            return "/";
-        }
-        return String.valueOf(score);
+    public String inputScore(String input) {
+        return score.updateScore(Integer.parseInt(input));
     }
 }
---
+++ src/main/java/bowling/domain/Score.java
@@ -0,0 +1,53 @@
+package bowling.domain;
+
+public class Score {
+    private int pins;
+    private int availability;
+
+    public Score(int pins, int availability) {
+        this.pins = pins;
+        this.availability = availability;
+    }
+
+    public String updateScore(int score) {
+        useAvailability();
+        return getFormattedScore(updateNumberOfPins(score));
+    }
+
+    private void useAvailability() {
+        if (isAvailable()) {
+            --availability;
+            return;
+        }
+        throw new IllegalStateException("점수를 더 이상 입력할 수 없습니다.");
+    }
+
+    private int updateNumberOfPins(int score) {
+        if (pins < score) {
+            throw new IllegalArgumentException("남아있는 볼링핀 개수보다 입력한 점수가 더 많습니다.");
+        }
+        pins = pins - score;
+        return score;
+    }
+
+    public boolean isAvailable() {
+        if (availability <= 0) {
+            return false;
+        }
+        return true;
+    }
+
+    private String getFormattedScore(int score) {
+        if (score == 0) {
+            return "-";
+        }
+        if (availability == 1 && pins == 0) {
+            availability = 0;
+            return "X";
+        }
+        if (availability == 0 && pins == 0) {
+            return "/";
+        }
+        return String.valueOf(score);
+    }
+}
