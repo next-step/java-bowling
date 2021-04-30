3343487acd7a3cd229e71b24d5b0d1f11b16d7ad
Dion <ksundong@gmail.com>
2021-05-01 02:36:41 +0900

    test: FinalFrame 종료 여부 확인 테스트

--- src/test/java/bowling/domain/frame/FinalFrameTest.java
+++ src/test/java/bowling/domain/frame/FinalFrameTest.java
@@ -5,9 +5,17 @@ import bowling.domain.pin.Pins;
 import bowling.exception.NoNextFrameException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
+import static bowling.domain.TestFixture.GUTTER_PIN;
+import static bowling.domain.TestFixture.STRIKE_PIN;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class FinalFrameTest {
 
@@ -64,5 +72,30 @@ class FinalFrameTest {
         assertThat(finalFrame.isEnded()).isFalse();
     }
 
-    // TODO: 마지막 프레임 종료 테스트
+    static Stream<Arguments> endedFinalFrameSource() {
+        return Stream.of(
+                arguments(FinalFrame.from(Pins.create()), false),
+                arguments(FinalFrame.from(Pins.of(STRIKE_PIN)), false),
+                arguments(FinalFrame.from(Pins.of(GUTTER_PIN)), false),
+                arguments(FinalFrame.from(Pins.of(new Pin(5))), false),
+                arguments(FinalFrame.from(Pins.of(new Pin(5), new Pin(5))), false),
+                arguments(FinalFrame.from(Pins.of(new Pin(5), GUTTER_PIN)), true),
+                arguments(FinalFrame.from(Pins.of(GUTTER_PIN, GUTTER_PIN)), true),
+                arguments(FinalFrame.from(Pins.of(STRIKE_PIN, STRIKE_PIN)), false),
+                arguments(FinalFrame.from(Pins.of(new Pin(5), new Pin(5), GUTTER_PIN)), true),
+                arguments(FinalFrame.from(Pins.of(STRIKE_PIN, STRIKE_PIN, STRIKE_PIN)), true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("endedFinalFrameSource")
+    @DisplayName("마지막 프레임의 상태별로 종료되었는지 여부를 반환한다.")
+    void isEnded(FinalFrame finalFrame, boolean expected) {
+        // given
+        // when
+        final boolean ended = finalFrame.isEnded();
+
+        // then
+        assertThat(ended).isEqualTo(expected);
+    }
 }
