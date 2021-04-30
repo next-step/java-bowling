1baf17b4f408cf43d5a4234d2081566296dfa53f
김우재 <kwj1270@naver.com>
2021-04-30 20:30:56 +0900

    test:Player 인스턴스에 3글자 초과의 글자 입력 여부 테스트작성

--- src/test/java/bowling/domain/PlayerTest.java
+++ src/test/java/bowling/domain/PlayerTest.java
@@ -1,9 +1,11 @@
 package bowling.domain;
 
+import bowling.exception.NameSizeMissMatchException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
@@ -19,6 +21,18 @@ class PlayerTest {
 
         // then
         assertThat(player).isNotNull();
+    }
+
+    @DisplayName("Player 인스턴스에 3글자 초과의 글자 입력 여부 테스트")
+    @Test
+    void 검증_3글자() {
+        // given
+        String name = "pobi";
+
+        // when and then
+        assertThatThrownBy(() -> Player.from(name))
+                .isInstanceOf(NameSizeMissMatchException.class)
+                .hasMessage("( pobi )은 영문 3글자 또는 그 이하가 아닙니다.");
     }
 
 }
