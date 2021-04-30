3b701ef89b8badee5caa7b4bde9a8f19b68ef068
김우재 <kwj1270@naver.com>
2021-04-30 22:06:13 +0900

    test:Pins 인스턴스가 맞은 갯수만큼 감소하는지에 대한 테스트작성

--- src/test/java/bowling/domain/state/PinsTest.java
+++ src/test/java/bowling/domain/state/PinsTest.java
@@ -1,5 +1,6 @@
 package bowling.domain.state;
 
+import bowling.domain.HitCount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +17,19 @@ class PinsTest {
 
         // then
         assertThat(pins).isNotNull();
+    }
+
+    @DisplayName("Pins 인스턴스가 맞은 갯수만큼 감소하는지에 대한 테스트")
+    @Test
+    void 감소() {
+        // when
+        Pins pins = Pins.initialize();
+
+        // then
+        assertAll(
+                () -> assertThat(pins.hit(HitCount.valueOf(10))).isNotNull(),
+                () -> assertThat(pins.hit(HitCount.valueOf(0))).isNotNull()
+        );
+
     }
 }
