e1e27a96797f5ee010ea2991388099f784dbe25c
김우재 <kwj1270@naver.com>
2021-05-01 12:05:02 +0900

    test:Miss 인스턴스에 알맞은 값이 주입되었는지 테스트작성

--- src/test/java/bowling/domain/state/MissTest.java
+++ src/test/java/bowling/domain/state/MissTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MissTest {
@@ -12,9 +13,19 @@ class MissTest {
     @Test
     void 생성() {
         // when
-        Miss miss = Miss.from(0,9);
+        Miss miss = Miss.from(0, 9);
 
         // then
         assertThat(miss).isNotNull();
+    }
+
+    @DisplayName("Miss 인스턴스에 알맞은 값이 주입되었는지 테스트")
+    @Test
+    void 검증() {
+        // when
+        assertThatThrownBy(()->Miss.from(0, 10))
+                .isInstanceOf(InsufficientMissException.class);
+                .hasMessage("( %s )와 ( %s )의 합인 ( %s )는, 9이하의 값이 아닙니다.");
+
     }
 }
