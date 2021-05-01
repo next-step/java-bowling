c91c1f9b7d693bb1c5bd25ae82cd1be0d8285b79
Donghui-Lee <ehdgml3206@naver.com>
2021-05-02 02:50:11 +0900

    refactor: ResultView 출력 수정

--- src/main/java/bowling/ui/ResultView.java
+++ src/main/java/bowling/ui/ResultView.java
@@ -1,27 +1,29 @@
 package bowling.ui;
 
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import bowling.domain.BowlingTurn;
-import bowling.domain.frame.Frame;
+import bowling.domain.player.Player;
+import bowling.domain.state.State;
+import bowling.domain.state.States;
 
 public class ResultView {
     private static final int LAST_FRAME = 10;
     private static final String PARTITION = "|";
     private static final String FRAME_TITLE = "| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |";
+    private static final String EMPTY_STRING = "";
 
 
-
-    public static void printInitBowlingBoard(BowlingTurn bowlingTurn) {
+    public static void printInitBowlingBoard(Player player) {
         printTitleFrames();
-        System.out.print(PARTITION + formatting(bowlingTurn.player()));
-        printEmptyFrames(0);
+        System.out.print(PARTITION + formatting(player.name()));
+        printEmptyFrames(player.currentFrameNumber() - 1);
     }
 
-    public static void printBowlingBoard(BowlingTurn bowlingTurn) {
+    public static void printBowlingBoard(Player player) {
         printTitleFrames();
-        printFrameStates(bowlingTurn);
-        printEmptyFrames(bowlingTurn.currentFrameSize());
+        printFrameStates(player);
+        printEmptyFrames(player.currentFrameNumber());
     }
 
     private static void printTitleFrames() {
@@ -29,19 +31,33 @@ public class ResultView {
     }
 
     private static void printEmptyFrames(int size) {
-        Stream.generate(() -> formatting(""))
+        Stream.generate(() -> formatting(EMPTY_STRING))
             .limit(LAST_FRAME - size)
             .forEach(System.out::print);
         System.out.println("\n");
     }
 
-    private static void printFrameStates(BowlingTurn bowlingTurn) {
-        System.out.print(PARTITION + formatting(bowlingTurn.player()));
+    private static void printFrameStates(Player player) {
+        System.out.print(PARTITION + formatting(player.name()));
 
-        bowlingTurn.frames().stream()
-            .map(Frame::frameState)
-            .map(ResultView::formatting)
+        player.states().stream()
+            .map(ResultView::printStates)
             .forEach(System.out::print);
+    }
+
+    private static String printStates(States states) {
+        String expression = states.states()
+            .stream()
+            .map(ResultView::replaceNull)
+            .collect(Collectors.joining(PARTITION));
+        return formatting(expression);
+    }
+
+    private static String replaceNull(final State state) {
+        if (state == null) {
+            return EMPTY_STRING;
+        }
+        return state.toSymbol();
     }
 
     private static String formatting(String input) {
