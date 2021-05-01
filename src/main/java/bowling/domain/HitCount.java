7d7125ad01ad4bad50032eac0cc0c34c92f28b8d
김우재 <kwj1270@naver.com>
2021-05-01 14:42:07 +0900

    feat:HitCount 인스턴스가 값이 동일할 경우 같은 인스턴스 반환 기능 구현

--- src/main/java/bowling/domain/HitCount.java
+++ src/main/java/bowling/domain/HitCount.java
@@ -2,14 +2,27 @@ package bowling.domain;
 
 import bowling.exception.InputNumberOutOfBoundsException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static bowling.util.BowlingFixture.*;
 
 public final class HitCount {
 
+    private static final List<HitCount> CACHE;
+
     private final int count;
 
+    static {
+        CACHE = new ArrayList<>();
+        for (int count = MINIMUM_COUNT; count <= MAXIMUM_COUNT; count++) {
+            CACHE.add(new HitCount(count));
+        }
+    }
+
     public static final HitCount valueOf(final int count) {
-        return new HitCount(count);
+        validateBounds(count);
+        return CACHE.get(count);
     }
 
     private HitCount(final int count) {
@@ -17,7 +30,7 @@ public final class HitCount {
         this.count = count;
     }
 
-    private final void validateBounds(final int count) {
+    private static final void validateBounds(final int count) {
         if (count < MINIMUM_COUNT || count > MAXIMUM_COUNT) {
             throw new InputNumberOutOfBoundsException(count);
         }
--- src/main/java/bowling/domain/state/Pins.java
+++ src/main/java/bowling/domain/state/Pins.java
@@ -1,6 +1,7 @@
 package bowling.domain.state;
 
 import bowling.domain.HitCount;
+import bowling.exception.InputNumberOutOfBoundsException;
 import bowling.exception.InputOverHitCountException;
 
 import java.util.ArrayList;
@@ -24,24 +25,32 @@ public final class Pins {
         }
     }
 
-    private Pins(final int pinCount) {
-        this.pinCount = pinCount;
-    }
-
     public static final Pins initialize() {
         return CACHE.get(MAXIMUM_COUNT);
     }
 
+    private Pins(final int pinCount) {
+        validateBounds(pinCount);
+        this.pinCount = pinCount;
+    }
+
+    private final void validateBounds(final int count) {
+        if (count < MINIMUM_COUNT || count > MAXIMUM_COUNT) {
+            throw new InputNumberOutOfBoundsException(count);
+        }
+    }
+
     public final Pins hit(final HitCount hitCount) {
-        validateCount(hitCount);
+        validateAvailableCount(hitCount);
         return CACHE.get(Math.subtractExact(pinCount, hitCount.count()));
     }
 
-    private final void validateCount(final HitCount hitCount) {
+    private final void validateAvailableCount(final HitCount hitCount) {
         if (hitCount.count() > pinCount) {
             throw new InputOverHitCountException(hitCount.count(), pinCount);
         }
     }
+
 
     public final boolean isEmpty() {
         if (pinCount <= MINIMUM_COUNT) {
