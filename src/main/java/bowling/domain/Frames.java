6666bc5826e61529bcda3959b4b34b68b2db0c55
PandaHun <gnsgnsek@gmail.com>
2021-05-01 20:51:07 +0900

    feat: Modify Round, Frames  add Score feat

--- src/main/java/bowling/domain/Frames.java
+++ src/main/java/bowling/domain/Frames.java
@@ -34,19 +34,68 @@ public class Frames {
     }
 
     public boolean isContinue() {
-        return !round.isFinalRound();
+        return !(round.isFinalRound() && frames.get(round.now()).roundEnded());
     }
 
     public List<Frame> frames() {
         return Collections.unmodifiableList(frames);
     }
 
+    public Frame nFrame(int n) {
+        if (n >= 0) {
+            return frames.get(n);
+        }
+        return nFrame(0);
+    }
+
     public void throwBall(int topplePin) {
-        Frame nowFrame = frames.get(round.round() - 1);
+        Frame nowFrame = nFrame(round.now());
         nowFrame.throwBall(topplePin);
+        beforeFrameAddBonus(topplePin);
+        if (nowFrame.isLastRound() && nowFrame.isLastRound()) {
+            nowFrame.createScore();
+        }
         if (nowFrame.roundEnded()) {
+            nowFrame.createScore();
             round = round.nextRound();
         }
+    }
+
+    private void beforeFrameAddBonus(int score) {
+        if (hasLeftSecondFrame() && !leftSecondFrameCanCalculate()) {
+            leftSecondFrameAddBonus(score);
+        }
+        if (hasLeftFrame() && !leftFrameCanCalculate()) {
+            leftFrameAddBonus(score);
+        }
+    }
+
+    private void leftFrameAddBonus(int score) {
+        Frame leftFrame = nFrame(round.before());
+        leftFrame.additionalScore(score);
+    }
+
+    private void leftSecondFrameAddBonus(int score) {
+        Frame leftSecondFrame = nFrame(round.beforeSecond());
+        leftSecondFrame.additionalScore(score);
+    }
+
+    private boolean leftSecondFrameCanCalculate() {
+        Frame leftSecondFrame = nFrame(round.beforeSecond());
+        return leftSecondFrame.canCalculate();
+    }
+
+    private boolean hasLeftSecondFrame() {
+        return round.hasBeforeSecond();
+    }
+
+    private boolean leftFrameCanCalculate() {
+        Frame leftFrame = nFrame(round.before());
+        return leftFrame.canCalculate();
+    }
+
+    private boolean hasLeftFrame() {
+        return round.hasBefore();
     }
 
     public int round() {
--- src/main/java/bowling/domain/Round.java
+++ src/main/java/bowling/domain/Round.java
@@ -8,6 +8,9 @@ public class Round {
 
     private static final int FIRST_ROUND = 1;
     private static final int FINAL_ROUND = 10;
+    private static final int BEFORE_DELIMITER = 2;
+    private static final int BEFORE_SECOND_DELIMITER = 3;
+    private static final int ROUND_MINIMUM_BOUND = 0;
     private static final String ROUND_RANGE_EXCEPTION_MESSAGE = "라운드는 1 이상 10 이하 입니다";
 
     private final int round;
@@ -36,7 +39,43 @@ public class Round {
     }
 
     public Round nextRound() {
-        return from(this.round + 1);
+        if (this.round < FINAL_ROUND) {
+            return from(this.round + 1);
+        }
+        return this;
+    }
+
+    public boolean isFinalRound() {
+        return round == FINAL_ROUND;
+    }
+
+    public int round() {
+        return round;
+    }
+
+    public int before() {
+        return round - BEFORE_DELIMITER;
+    }
+
+    public int beforeSecond() {
+        return round - BEFORE_SECOND_DELIMITER;
+    }
+
+    public int now() {
+        return round - 1;
+    }
+
+    public boolean hasBefore() {
+        return round - BEFORE_DELIMITER >= ROUND_MINIMUM_BOUND;
+    }
+
+    public boolean hasBeforeSecond() {
+        return round - BEFORE_SECOND_DELIMITER >= ROUND_MINIMUM_BOUND;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(round);
     }
 
     @Override
@@ -49,18 +88,5 @@ public class Round {
         }
         Round round1 = (Round) o;
         return round == round1.round;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(round);
-    }
-
-    public boolean isFinalRound() {
-        return round == FINAL_ROUND;
-    }
-
-    public int round() {
-        return round;
     }
 }
