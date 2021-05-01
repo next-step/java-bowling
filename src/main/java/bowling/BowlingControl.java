510003fda1399f24fa4eb355d49ec6e7cfc20a38
Younghyun <y.jo@navercorp.com>
2021-05-01 13:21:54 +0900

    feat(step2):Control 추가

---
+++ src/main/java/bowling/BowlingControl.java
@@ -0,0 +1,26 @@
+package bowling;
+
+import bowling.domain.Bowling;
+import bowling.domain.Pinfall;
+import bowling.domain.Player;
+import bowling.view.View;
+
+public class BowlingControl {
+    public void play() {
+        String playerName = View.playerName();
+        Player player = new Player(playerName);
+
+        Bowling bowling = new Bowling();
+        while (!bowling.isDone()) {
+            View.printScoreBoardHeader();
+            View.printPlayer(player);
+            View.printBowlingResult(bowling.result());
+            Pinfall pinfall = new Pinfall(View.pinfall(bowling.frameNumber()));
+            bowling.roll(pinfall);
+        }
+
+        View.printScoreBoardHeader();
+        View.printPlayer(player);
+        View.printBowlingResult(bowling.result());
+    }
+}
