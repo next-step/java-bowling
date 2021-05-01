33eac32d502815af5ec3d5d90ad4f0b247a61ddf
김우재 <kwj1270@naver.com>
2021-05-01 11:50:31 +0900

    feat:Miss 인스턴스 생성 기능 구현

--- src/main/java/bowling/domain/state/Miss.java
+++ src/main/java/bowling/domain/state/Miss.java
@@ -1,4 +1,16 @@
 package bowling.domain.state;
 
-public class Miss {
+public final class Miss {
+
+    private final int firstCount;
+    private final int secondCount;
+
+    private Miss(final int firstCount, final int secondCount) {
+        this.firstCount = firstCount;
+        this.secondCount = secondCount;
+    }
+
+    public static final Miss from(final int firstCount, final int secondCount) {
+        return new Miss(firstCount, secondCount);
+    }
 }
