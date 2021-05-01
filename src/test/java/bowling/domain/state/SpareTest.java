46cc2964b5f599034203df03513a1e1af9fba0be
김우재 <kwj1270@naver.com>
2021-05-01 11:05:54 +0900

    test:Spare 인스턴스 생성시 들어오는 값들의 합이 10이 되는지 테스트작성

--- src/test/java/bowling/domain/state/SpareTest.java
+++ src/test/java/bowling/domain/state/SpareTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SpareTest {
@@ -24,5 +25,19 @@ class SpareTest {
                 () -> assertThat(spare).isNotNull(),
                 () -> assertThat(spare).isInstanceOf(Spare.class)
         );
+    }
+
+    @DisplayName("Spare 인스턴스 생성시 들어오는 값들의 합이 10이 되는지 테스트")
+    @Test
+    void 검증() {
+
+        // given
+        int firstCount = 9;
+        int secondCount = 0;
+
+        // when
+        assertThatThrownBy(()-> Spare.of(firstCount, secondCount))
+                .isInstanceOf(InsufficientForMaxCountException.class)
+                .hasMessage("( 9 )와 ( 0 )의 합인 ( 9 )는, 10을 충족하지 않습니다.");
     }
 }
