7ce47618625d5395fe80a577ac45c243d626f751
Dion <ksundong@gmail.com>
2021-05-02 14:53:17 +0900

    feat: 일반 Score 생성 기능 구현

--- src/main/java/bowling/domain/score/Score.java
+++ src/main/java/bowling/domain/score/Score.java
@@ -1,9 +1,21 @@
 package bowling.domain.score;
 
+import java.util.Objects;
+
 public final class Score {
 
+    private static final int CALCULABLE_COUNT = 0;
+
+    private final int score;
+    private final int leftCount;
+
+    public Score(int score, int leftCount) {
+        this.score = score;
+        this.leftCount = leftCount;
+    }
+
     public static Score of(int score, int leftCount) {
-        return null;
+        return new Score(score, leftCount);
     }
 
     public static Score strike() {
@@ -15,10 +27,23 @@ public final class Score {
     }
 
     public boolean canCalculate() {
-        return false;
+        return leftCount == CALCULABLE_COUNT;
     }
 
     public int calculate() {
-        return 0;
+        return score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Score score1 = (Score) o;
+        return score == score1.score && leftCount == score1.leftCount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score, leftCount);
     }
 }
