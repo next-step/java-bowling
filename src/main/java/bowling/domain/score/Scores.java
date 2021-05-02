c92301566119a5f59b16e13f7a1e29ce41d86174
Donghui-Lee <ehdgml3206@naver.com>
2021-05-02 19:57:35 +0900

    feat: 점수 리스트 Scores Class 추가

---
+++ src/main/java/bowling/domain/score/Scores.java
@@ -0,0 +1,26 @@
+package bowling.domain.score;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scores {
+    private final List<Score> scores;
+
+    public Scores() {
+        scores = new ArrayList<>();
+    }
+
+    public void accumulate(Score score) {
+        if (scores.isEmpty()) {
+            scores.add(score);
+            return;
+        }
+
+        Score nextScore = scores.get(scores.size() - 1).add(score);
+        scores.add(nextScore);
+    }
+
+    public List<Score> toList() {
+        return scores;
+    }
+}
