0eff1f54f831010954b92c6568389f0dcca77748
Dion <ksundong@gmail.com>
2021-05-02 15:18:22 +0900

    feat: leftCount 생성 기능 구현

--- src/main/java/bowling/domain/score/LeftCount.java
+++ src/main/java/bowling/domain/score/LeftCount.java
@@ -1,8 +1,29 @@
 package bowling.domain.score;
 
+import java.util.Objects;
+
 public final class LeftCount {
 
+    private final int leftCount;
+
+    public LeftCount(int leftCount) {
+        this.leftCount = leftCount;
+    }
+
     public static LeftCount from(int leftCount) {
-        return null;
+        return new LeftCount(leftCount);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LeftCount leftCount1 = (LeftCount) o;
+        return leftCount == leftCount1.leftCount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(leftCount);
     }
 }
