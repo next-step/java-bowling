a95d775e1632018727ff1f6d8c59cce323550be8
Donghui-Lee <ehdgml3206@naver.com>
2021-05-02 19:57:04 +0900

    refactor: 테스트 성공을 위한 코드 수정

--- src/main/java/bowling/domain/score/Score.java
+++ src/main/java/bowling/domain/score/Score.java
@@ -3,7 +3,10 @@ package bowling.domain.score;
 import java.util.Objects;
 
 public class Score {
+    private static final int MAX_PIN_COUNT = 10;
+    private static final int MIN_PIN_COUNT = 0;
     private static final int MIN_LEFT_COUNT = 0;
+    private static final int PROGRESS_VALUE = -1;
 
     private final int score;
     private final int left;
@@ -19,6 +22,22 @@ public class Score {
 
     public static Score of(int score, int left) {
         return new Score(score, left);
+    }
+
+    public static Score ofStrike() {
+        return Score.of(MAX_PIN_COUNT, 2);
+    }
+
+    public static Score ofSpare() {
+        return Score.of(MAX_PIN_COUNT, 1);
+    }
+
+    public static Score init() {
+        return Score.of(MIN_PIN_COUNT, MIN_LEFT_COUNT);
+    }
+
+    public static Score ofProgress() {
+        return Score.of(PROGRESS_VALUE, PROGRESS_VALUE);
     }
 
     public Score add(Score score) {
--- src/test/java/bowling/domain/score/ScoreTest.java
+++ src/test/java/bowling/domain/score/ScoreTest.java
@@ -10,23 +10,35 @@ public class ScoreTest {
     void 생성_테스트() {
         // given
         Score score = Score.of(5);
+        Score score2 = Score.ofSpare();
+        Score score3 = Score.ofStrike();
         // when & then
         assertThat(score).isEqualTo(Score.of(5, 0));
+        assertThat(score2).isEqualTo(Score.of(10, 1));
+        assertThat(score3).isEqualTo(Score.of(10, 2));
     }
 
     @Test
     void 더하기_테스트() {
         // given
         Score score = Score.of(5);
+        Score score2 = Score.of(5);
+        Score score3 = Score.of(5);
         // when & then
         assertThat(score.add(Score.of(5))).isEqualTo(Score.of(10, 0));
+        assertThat(score2.add(Score.ofSpare())).isEqualTo(Score.of(15, 1));
+        assertThat(score3.add(Score.ofStrike())).isEqualTo(Score.of(15, 2));
     }
 
     @Test
     void 누적_테스트() {
         // given
         Score score = Score.of(5);
+        Score score2 = Score.ofSpare();
+        Score score3 = Score.ofStrike();
         // when & then
         assertThat(score.accumulate(5)).isEqualTo(Score.of(10, -1));
+        assertThat(score2.accumulate(5)).isEqualTo(Score.of(15, 0));
+        assertThat(score3.accumulate(5)).isEqualTo(Score.of(15, 1));
     }
 }
