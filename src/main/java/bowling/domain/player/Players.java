3572e5a3f4129ba4fd3dc190b5d7309dd9c613df
Dion <ksundong@gmail.com>
2021-05-03 23:07:08 +0900

    test: Players value 테스트 작성

--- src/main/java/bowling/domain/player/Players.java
+++ src/main/java/bowling/domain/player/Players.java
@@ -28,6 +28,10 @@ public final class Players {
         players.add(player);
     }
 
+    public List<Player> value() {
+        return null;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/test/java/bowling/domain/player/PlayersTest.java
+++ src/test/java/bowling/domain/player/PlayersTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,5 +28,19 @@ class PlayersTest {
 
         // then
         assertThat(players).isEqualTo(Players.from(Collections.singletonList(player)));
+    }
+
+    @Test
+    @DisplayName("value를 사용하면 player List를 반환한다.")
+    void value() {
+        // given
+        final List<Player> playerList = Collections.singletonList(Player.from(PlayerName.valueOf("ksd")));
+        final Players players = Players.from(playerList);
+
+        // when
+        final List<Player> actualPlayerList = players.value();
+
+        // then
+        assertThat(actualPlayerList).isEqualTo(playerList);
     }
 }
