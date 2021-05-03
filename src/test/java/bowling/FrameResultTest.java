b8a54bd9321d1f8871294ee0840a750110bc4693
Younghyun <younghyunjo99@gmail.com>
2021-05-03 22:37:49 +0900

    feat(step4):DisplayName 추가

--- src/test/java/bowling/FrameResultTest.java
+++ src/test/java/bowling/FrameResultTest.java
@@ -4,17 +4,20 @@ import bowling.domain.FrameResult;
 import bowling.domain.PointSymbol;
 import bowling.domain.PointSymbols;
 import bowling.domain.Score;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class FrameResultTest {
     @Test
+    @DisplayName("생성자 테스트")
     void When_New_Then_Created() {
         assertThat(new FrameResult()).isEqualTo(new FrameResult());
     }
 
     @Test
+    @DisplayName("입력한 Score를 잘 가지고 있는지 테스트")
     void Given_AggregatedScore_When_aggregatedScore_Then_GivenScore() {
         Score aggregatedScore = Score.create(10);
 
@@ -23,6 +26,7 @@ public class FrameResultTest {
     }
 
     @Test
+    @DisplayName("입력한 PointSymbols를 잘 가지고 있는지 테스트")
     void Given_PointSymbols_When_PointSymbols_Then_GivenPointSymbols() {
         PointSymbols pointSymbols = new PointSymbols(PointSymbol.ONE, PointSymbol.TWO);
 
