4e43d9d94d875ac0fab42f249aa711ee2953e872
PandaHun <gnsgnsek@gmail.com>
2021-05-03 21:52:57 +0900

    feat: Create Players feat

--- src/main/java/bowling/domain/Players.java
+++ src/main/java/bowling/domain/Players.java
@@ -1,13 +1,25 @@
 package bowling.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Players {
+
+    private final List<Player> players;
+
+    public Players(List<Player> players) {
+        this.players = players;
+    }
+
     public static Players from(List<Player> players) {
-        return new Players();
+        return new Players(players);
     }
 
     public int howManyPlayers() {
-        return -1;
+        return players.size();
+    }
+
+    public List<Player> players() {
+        return Collections.unmodifiableList(players);
     }
 }
