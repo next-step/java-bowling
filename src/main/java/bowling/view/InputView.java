60037b19eb53d093b5937a1474be3b657ff25b15
PandaHun <gnsgnsek@gmail.com>
2021-05-03 23:10:06 +0900

    refactor: Modify InputView apply requirements

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -1,12 +1,14 @@
 package bowling.view;
 
+import bowling.domain.Player;
+
 import java.util.Scanner;
 
 public class InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String GET_PLAYER_NAME_MESSAGE = "플레이어 %d의 이름은(3 english letters)?: ";
-    private static final String GET_TOPPLE_PIN_MESSAGE = "%d프레임 투구: ";
+    private static final String GET_TOPPLE_PIN_MESSAGE = "%s's turn: ";
     private static final String GET_PLAYER_COUNT_MESSAGE = "How many people? ";
 
     private InputView() {
@@ -17,13 +19,13 @@ public class InputView {
         return SCANNER.nextLine();
     }
 
-    public static int getTopplePin(int round) {
-        System.out.printf(GET_TOPPLE_PIN_MESSAGE, round);
+    public static int getTopplePin(Player player) {
+        System.out.printf(GET_TOPPLE_PIN_MESSAGE, player.name());
         return SCANNER.nextInt();
     }
 
     public static int getPlayerCount() {
         System.out.print(GET_PLAYER_COUNT_MESSAGE);
-        return SCANNER.nextInt();
+        return Integer.parseInt(SCANNER.nextLine());
     }
 }
