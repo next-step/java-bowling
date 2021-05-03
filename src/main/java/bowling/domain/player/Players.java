45be2f75d1b0cbd4b815aba26592bfe175114787
Donghui-Lee <ehdgml3206@naver.com>
2021-05-04 00:57:46 +0900

    feat: 여러명의 플레이어 관리 Players Class 추가

---
+++ src/main/java/bowling/domain/player/Players.java
@@ -0,0 +1,19 @@
+package bowling.domain.player;
+
+import java.util.List;
+
+public class Players {
+    private final List<Player> players;
+
+    public Players(List<Player> players) {
+        this.players = players;
+    }
+
+    public List<Player> toList() {
+        return players;
+    }
+
+    public boolean isAllDone() {
+        return false;
+    }
+}
---
+++ src/test/java/bowling/domain/player/PlayersTest.java
@@ -0,0 +1,39 @@
+package bowling.domain.player;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+
+public class PlayersTest {
+
+    @Test
+    void 생성_테스트() {
+        // given
+        Players players = new Players(Arrays.asList(Player.of("ldh"), Player.of("kkk")));
+        // when & then
+        assertThat(players.toList().size()).isEqualTo(2);
+    }
+
+    @Test
+    void 모든_플레이어_종료_테스트() {
+        // given
+        Player player = Player.of("ldh");
+        Player player2 = Player.of("kkk");
+
+        // when
+        IntStream.range(0, 10).forEach(i -> {
+            player.bowl(3);
+            player.bowl(3);
+
+            player2.bowl(3);
+            player2.bowl(3);
+        });
+
+        Players players = new Players(Arrays.asList(player, player2));
+        // then
+        assertThat(players.isAllDone()).isTrue();
+    }
+}
