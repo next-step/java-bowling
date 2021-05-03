410439dc5f15f1f5c10643c68c98f443d151fa7f
Donghui-Lee <ehdgml3206@naver.com>
2021-05-04 01:05:55 +0900

    refactor: Main Class 수정

--- src/main/java/bowling/BowlingGame.java
+++ src/main/java/bowling/BowlingGame.java
@@ -1,17 +1,10 @@
 package bowling;
 
-import bowling.domain.player.Player;
-import bowling.ui.InputView;
-import bowling.ui.ResultView;
+import bowling.controller.BowlingGameController;
 
 public class BowlingGame {
     public static void main(String[] args) {
-        Player player = Player.of(InputView.inputPlayerName());
-        ResultView.printInitBowlingBoard(player);
-
-        while(!player.isDone()) {
-            player.bowl(InputView.inputBowlingPin(player.currentFrameNumber()));
-            ResultView.printBowlingBoard(player);
-        }
+        BowlingGameController controller = new BowlingGameController();
+        controller.run();
     }
 }
