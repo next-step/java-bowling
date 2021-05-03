4a818100463e04082be8c5efe952e5bf8e278719
PandaHun <gnsgnsek@gmail.com>
2021-05-03 22:33:53 +0900

    feat: Modify InputView add get PlayerCount, Modify Players add frames

--- src/main/java/bowling/domain/Players.java
+++ src/main/java/bowling/domain/Players.java
@@ -1,21 +1,27 @@
 package bowling.domain;
 
+import bowling.domain.frame.Frames;
 import bowling.exception.DuplicatePlayerException;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Players {
 
     private static final String DUPLICATE_EXCEPTION_MESSAGE = "동일한 이름의 플레이어는 추가할 수 없습니다";
 
     private final List<Player> players;
+    private final List<Frames> framesList;
 
     private Players(List<Player> players) {
         this.players = players;
         validatePlayers();
+        this.framesList = Stream.generate(Frames::init)
+                .limit(players.size())
+                .collect(Collectors.toList());
     }
 
     private void validatePlayers() {
@@ -31,9 +37,5 @@ public class Players {
 
     public int howManyPlayers() {
         return players.size();
-    }
-
-    public List<Player> players() {
-        return Collections.unmodifiableList(players);
     }
 }
--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -5,19 +5,25 @@ import java.util.Scanner;
 public class InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
-    private static final String GET_PLAYER_NAME_MESSAGE = "플레이어 이름은(3 english letters)?: ";
+    private static final String GET_PLAYER_NAME_MESSAGE = "플레이어 %d의 이름은(3 english letters)?: ";
     private static final String GET_TOPPLE_PIN_MESSAGE = "%d프레임 투구: ";
+    private static final String GET_PLAYER_COUNT_MESSAGE = "How many people? ";
 
     private InputView() {
     }
 
-    public static String getPlayerName() {
-        System.out.print(GET_PLAYER_NAME_MESSAGE);
+    public static String getPlayerName(int count) {
+        System.out.printf(GET_PLAYER_NAME_MESSAGE, count);
         return SCANNER.nextLine();
     }
 
     public static int getTopplePin(int round) {
         System.out.printf(GET_TOPPLE_PIN_MESSAGE, round);
+        return SCANNER.nextInt();
+    }
+
+    public static int getPlayerCount() {
+        System.out.print(GET_PLAYER_COUNT_MESSAGE);
         return SCANNER.nextInt();
     }
 }
