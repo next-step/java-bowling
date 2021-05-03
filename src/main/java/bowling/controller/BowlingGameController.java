6450dd2f39b658282605fd5565b405ae70599083
Donghui-Lee <ehdgml3206@naver.com>
2021-05-04 01:05:29 +0900

    feat: 조작 편의를 위해 controller 추가

---
+++ src/main/java/bowling/controller/BowlingGameController.java
@@ -0,0 +1,47 @@
+package bowling.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bowling.domain.player.Player;
+import bowling.domain.player.PlayerCount;
+import bowling.domain.player.Players;
+import bowling.ui.InputView;
+import bowling.ui.ResultView;
+
+public class BowlingGameController {
+    public BowlingGameController() {
+    }
+
+    public void run() {
+        Players players = init();
+        ResultView.printBowlingBoard(players);
+
+        while (!players.isAllDone()) {
+            play(players);
+        }
+    }
+
+    private Players init() {
+        List<Player> players = new ArrayList<>();
+        PlayerCount playerCount = PlayerCount.of(InputView.inputPlayerCount());
+        for (int i = 1; i < playerCount.toInteger() + 1; i++) {
+            players.add(Player.of(InputView.inputPlayerName(i)));
+        }
+        return new Players(players);
+    }
+
+    private void play(Players players) {
+        for (Player player : players.toList()) {
+            turn(players, player);
+            player.changeNextFrame();
+        }
+    }
+
+    private void turn(Players players, Player player) {
+        while(!player.isDone()) {
+            player.bowl(InputView.inputBowlingPin(player.name()));
+            ResultView.printBowlingBoard(players);
+        }
+    }
+}
