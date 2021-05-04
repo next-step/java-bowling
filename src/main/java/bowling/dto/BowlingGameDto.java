e0ef0b27df6a0e934e340fb30ace6f10cc73fb5c
ttokey <79037443+ttokey@users.noreply.github.com>
2021-05-04 18:08:06 +0900

    feat : BowlingGameDto 구현

--- src/main/java/bowling/dto/BowlingGameDto.java
+++ src/main/java/bowling/dto/BowlingGameDto.java
@@ -2,6 +2,7 @@ package bowling.dto;
 
 import bowling.domain.BowlingGame;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BowlingGameDto {
@@ -12,8 +13,14 @@ public class BowlingGameDto {
     }
 
     public static BowlingGameDto valueOf(BowlingGame bowlingGame) {
-        
+        List<BowlingDto> result = new ArrayList<>();
+        bowlingGame.getBowlingGame()
+                .forEach(bowling -> result.add(BowlingDto.valueOf(bowling)));
+        return new BowlingGameDto(result);
     }
 
+    public List<BowlingDto> getBowlingDto() {
+        return this.bowlingGameDto;
+    }
 
 }
