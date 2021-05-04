10f482caf86d51691188c99622799dfce34a6f25
DESKTOP-T6O4081\lee <lsecret@naver.com>
2021-05-04 22:17:39 +0900

    feat: Score 테스트 추카
    테스트 케이스 추가

--- src/test/java/bowling/score/ScoreTest.java
+++ src/test/java/bowling/score/ScoreTest.java
@@ -18,4 +18,25 @@ public class ScoreTest {
         assertThat(score.equals(new Score(totalScore))).isTrue();
     }
 
+    @Test
+    @DisplayName("스트라이크 스코어 계산")
+    public void calculateStrikeScore(){
+        Score score = new Score(10, 2);
+        assertThat(score.calculate(10).calculate(10).score() == 30).isTrue();
+    }
+
+    @Test
+    @DisplayName("스페어 스코어 계산")
+    public void calculateSpareScore(){
+        Score score = new Score(10, 1);
+        assertThat(score.calculate(10).score() == 20).isTrue();
+    }
+
+    @Test
+    @DisplayName("일반 스코어 계산")
+    public void calculateScore(){
+        Score score = new Score(9, 0);
+        assertThat(score.score() == 9).isTrue();
+    }
+
 }
