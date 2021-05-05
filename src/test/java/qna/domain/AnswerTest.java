8a5b0200383b10a95c2fe313f10c871d2445d4b8
w-beom <w_bum@naver.com>
2021-05-05 23:24:24 +0900

    test : 테스트 코드 작성

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,19 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("Answer 정보를 DeleteHistory로 반환")
+    public void infoTest() {
+        assertThat(A1.info()).isEqualTo(new DeleteHistory(ContentType.ANSWER, A1.getId(), UserTest.JAVAJIGI, LocalDateTime.now()));
+    }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,20 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    @DisplayName("본인이 작성한 게시물이 아니면 오류 발생")
+    void checkDeleteQuestionPermission() {
+        assertThatExceptionOfType(CannotDeleteException.class)
+                .isThrownBy(() -> Q1.checkDeleteQuestionPermission(UserTest.SANJIGI))
+                .withMessage("질문을 삭제할 권한이 없습니다.");
+    }
 }
