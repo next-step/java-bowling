7c863ffe232bb9402bcfec2f3d0c9b1704e67d05
김우재 <kwj1270@naver.com>
2021-05-09 22:00:06 +0900

    feat:Miss 인스턴스 생성 여부 테스트작성

--- src/main/java/bowling/domain/state/Miss.java
+++ src/main/java/bowling/domain/state/Miss.java
@@ -1,4 +1,33 @@
 package bowling.domain.state;
 
+import bowling.domain.score.Score;
+
 public final class Miss extends Finish {
+
+    private final Pins firstPins;
+    private final Pins secondPins;
+
+    public static final State from(final Pins firstPins, final Pins secondPins) {
+        return new Miss(firstPins, secondPins);
+    }
+
+    private Miss(final Pins firstPins, final Pins secondPins) {
+        this.firstPins = firstPins;
+        this.secondPins = secondPins;
+    }
+
+    @Override
+    public Score score() {
+        return null;
+    }
+
+    @Override
+    public Score calculateAdditionalScore(Score beforeScore) {
+        return null;
+    }
+
+    @Override
+    public String description() {
+        return null;
+    }
 }
