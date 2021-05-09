610145fb98d316b223a853347d554df3bfeaae1a
ad7549 <ad7549@gmail.com>
2021-05-10 04:03:08 +0900

    feat: Add BowlingMain

---
+++ src/main/java/bowling/BowlingMain.java
@@ -0,0 +1,9 @@
+package bowling;
+
+import bowling.controllers.BowlingController;
+
+public class BowlingMain {
+    public static void main(String[] args) {
+        BowlingController.run();
+    }
+}
