c53394c05abd46741c03c875b51df3a34f7a29cf
김우재 <kwj1270@naver.com>
2021-05-09 21:28:17 +0900

    feat:Spare 인스턴스 생성시 null 입력 시 예외처리 기능 구현

--- src/main/java/bowling/domain/state/Spare.java
+++ src/main/java/bowling/domain/state/Spare.java
@@ -1,19 +1,29 @@
 package bowling.domain.state;
 
 import bowling.domain.score.Score;
+import bowling.exception.PinsNullPointerException;
+
+import java.util.Objects;
 
 public class Spare extends Finish {
 
     private final Pins firstPins;
     private final Pins secondPins;
 
+    public static final State of(final Pins firstPins, final Pins secondPins) {
+        return new Spare(firstPins, secondPins);
+    }
+
     private Spare(final Pins firstPins, final Pins secondPins) {
+        validateNull(firstPins, secondPins);
         this.firstPins = firstPins;
         this.secondPins = secondPins;
     }
 
-    public static final State of(final Pins firstPins, final Pins secondPins) {
-        return new Spare(firstPins, secondPins);
+    private final void validateNull(Pins firstPins, Pins secondPins) {
+        if (Objects.isNull(firstPins) || Objects.isNull(secondPins)) {
+            throw new PinsNullPointerException();
+        }
     }
 
     @Override
