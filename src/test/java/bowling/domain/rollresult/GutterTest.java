8d12394018a3c693e27a9ae4e9e793c64adeded7
HJ-Woo <hjwoou@gmail.com>
2021-05-09 13:25:49 +0900

    test: Gutter test 추가

--- src/test/java/bowling/domain/rollresult/GutterTest.java
+++ src/test/java/bowling/domain/rollresult/GutterTest.java
@@ -1,5 +1,6 @@
 package bowling.domain.rollresult;
 
+import bowling.domain.Score;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,9 +10,25 @@ public class GutterTest {
 
     @Test
     void 거터생성() {
+        assertThat(GUTTER).isEqualTo(Gutter.of());
         assertThat(GUTTER.isSpare()).isFalse();
         assertThat(GUTTER.isStrike()).isFalse();
-        assertThat(GUTTER.eval().compareTo(0)).isEqualTo(0);
-        assertThat(GUTTER).isEqualTo(Gutter.of());
+        assertThat(GUTTER.eval().isGutter()).isTrue();
+        assertThat(GUTTER.hasNext()).isTrue();
+        assertThat(GUTTER.canAccumulate()).isFalse();
+    }
+
+    @Test
+    void 거터다음_10개치면_스페어() {
+        RollResultType spare = GUTTER.next(10);
+        assertThat(spare.isSpare()).isTrue();
+        assertThat(spare.eval().isClear()).isTrue();
+    }
+
+    @Test
+    void 거터다음_10개미만은_Miss() {
+        RollResultType miss = GUTTER.next(5);
+        assertThat(miss.isSpare()).isFalse();
+        assertThat(miss.eval()).isEqualTo(Score.of(5));
     }
 }
