dcfaf82591336719f5b9be736e9472698c8ccd64
김우재 <kwj1270@naver.com>
2021-05-09 22:52:02 +0900

    test:Strike의 calculateAdditionalScore 에 대한 테스트 작성

--- src/test/java/bowling/domain/state/StrikeTest.java
+++ src/test/java/bowling/domain/state/StrikeTest.java
@@ -33,4 +33,54 @@ class StrikeTest {
         );
     }
 
+    @DisplayName("이전이 Miss 일 때, calculateAdditionalScore 의 결과로 알맞는 Score 인스턴스를 반한하는지 테스트")
+    @Test
+    void 반환_calculateAdditionalScore_Miss_일_경우() {
+        // given
+        State strike = Strike.initialize();
+
+        // when
+        Score beforeScore = Score.miss(Pins.valueOf(0));
+        Score actual = strike.calculateAdditionalScore(beforeScore);
+
+        // then
+        assertAll(
+                () -> assertThat(actual.isFinish()).isTrue(),
+                () -> assertThat(actual.score()).isEqualTo(beforeScore.score())
+        );
+    }
+
+    @DisplayName("이전이 Spare 일 때, calculateAdditionalScore 의 결과로 알맞는 Score 인스턴스를 반한하는지 테스트")
+    @Test
+    void 반환_calculateAdditionalScore_Spare_일_경우() {
+        // given
+        State strike = Strike.initialize();
+
+        // when
+        Score beforeScore = Score.spare();
+        Score actual = strike.calculateAdditionalScore(beforeScore);
+
+        // then
+        assertAll(
+                () -> assertThat(actual.isFinish()).isTrue(),
+                () -> assertThat(actual.score()).isEqualTo(beforeScore.score() + strike.score().score())
+        );
+    }
+
+    @DisplayName("이전이 Strike 일 때, calculateAdditionalScore 의 결과로 알맞는 Score 인스턴스를 반한하는지 테스트")
+    @Test
+    void 반환_calculateAdditionalScore_Strike_일_경우() {
+        // given
+        State strike = Strike.initialize();
+
+        // when
+        Score beforeScore = Score.strike();
+        Score actual = strike.calculateAdditionalScore(beforeScore);
+
+        // then
+        assertAll(
+                () -> assertThat(actual.isFinish()).isTrue(),
+                () -> assertThat(actual.score()).isEqualTo(beforeScore.score() + strike.score().score())
+        );
+    }
 }
