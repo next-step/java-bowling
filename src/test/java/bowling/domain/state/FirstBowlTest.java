45fe387fea840c4373a2da80aa832674d49d8184
김우재 <kwj1270@naver.com>
2021-05-09 20:53:25 +0900

    test:FirstBowl 인스턴스 생성 여부 테스트작성

---
+++ src/test/java/bowling/domain/state/FirstBowlTest.java
@@ -0,0 +1,27 @@
+package bowling.domain.state;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FirstBowlTest {
+
+    @DisplayName("FirstBowl 인스턴스 생성 여부 테스트")
+    @Test
+    void 생성() {
+        // given
+        Pins pins = Pins.valueOf(9);
+
+        // when
+        State firstBowl = FirstBowl.from(pins);
+
+        // then
+        assertAll(
+                () -> assertThat(pins).isNotNull(),
+                () -> assertThat(pins).isInstanceOf(FirstBowl.class),
+        );
+    }
+
+}
