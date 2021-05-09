299e1904e10d00929540012073ba6cf2abdf0fc8
김우재 <kwj1270@naver.com>
2021-05-09 16:01:07 +0900

    refactor:BonusCount 인스턴스가 캐시를 사용하도록 리팩터링

--- src/main/java/bowling/domain/BonusCount.java
+++ src/main/java/bowling/domain/BonusCount.java
@@ -2,6 +2,9 @@ package bowling.domain;
 
 import bowling.exception.InvalidBonusCountSizeException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static java.lang.Math.subtractExact;
 
 public final class BonusCount {
@@ -10,10 +13,20 @@ public final class BonusCount {
     private static final int MAXIMUM_BONUS_COUNT = 2;
     private static final int OPERATION_UNIT = 1;
 
+    private static final List<BonusCount> CACHE;
+
     private final int remain;
 
+    static {
+        CACHE = new ArrayList<>();
+        for (int remain = MINIMUM_BONUS_COUNT; remain <= MAXIMUM_BONUS_COUNT; remain++) {
+            CACHE.add(new BonusCount(remain));
+        }
+    }
+
     public static final BonusCount valueOf(final int remain) {
-        return new BonusCount(remain);
+        validateSize(remain);
+        return CACHE.get(remain);
     }
 
     private BonusCount(final int remain) {
@@ -21,7 +34,7 @@ public final class BonusCount {
         this.remain = remain;
     }
 
-    private void validateSize(final int remain) {
+    private static void validateSize(final int remain) {
         if (remain < MINIMUM_BONUS_COUNT || remain > MAXIMUM_BONUS_COUNT) {
             throw new InvalidBonusCountSizeException();
         }
@@ -32,6 +45,6 @@ public final class BonusCount {
     }
 
     public final BonusCount decrease() {
-        return new BonusCount(subtractExact(remain, OPERATION_UNIT));
+        return CACHE.get(subtractExact(remain, OPERATION_UNIT));
     }
 }
