fb9693f12fd6b42d22630a6f519b128964d2217a
김우재 <kwj1270@naver.com>
2021-05-09 22:45:54 +0900

    test:FirstBowl의 calculateAdditionalScore 에 대한 테스트 작성

--- src/test/java/bowling/domain/state/FirstBowlTest.java
+++ src/test/java/bowling/domain/state/FirstBowlTest.java
@@ -1,5 +1,6 @@
 package bowling.domain.state;
 
+import bowling.domain.score.Score;
 import bowling.exception.InvalidFirstBowlSizeException;
 import bowling.exception.InvalidPinsSizeException;
 import bowling.exception.PinsNullPointerException;
@@ -83,6 +84,60 @@ class FirstBowlTest {
         assertAll(
                 () -> assertThat(actual).isNotNull(),
                 () -> assertThat(actual).isInstanceOf(Miss.class)
+        );
+    }
+
+    @DisplayName("이전이 Miss 일 때, calculateAdditionalScore 의 결과로 알맞는 Score 인스턴스를 반한하는지 테스트")
+    @Test
+    void 반환_calculateAdditionalScore_Miss_일_경우() {
+        // given
+        Pins pins = Pins.valueOf(9);
+
+        // when
+        State firstBowl = FirstBowl.from(pins);
+        Score beforeScore = Score.miss(Pins.valueOf(0));
+        Score actual = firstBowl.calculateAdditionalScore(beforeScore);
+
+        // then
+        assertAll(
+                () -> assertThat(actual.isFinish()).isTrue(),
+                () -> assertThat(actual.score()).isEqualTo(beforeScore.score())
+        );
+    }
+
+    @DisplayName("이전이 Spare 일 때, calculateAdditionalScore 의 결과로 알맞는 Score 인스턴스를 반한하는지 테스트")
+    @Test
+    void 반환_calculateAdditionalScore_Spare_일_경우() {
+        // given
+        Pins pins = Pins.valueOf(9);
+
+        // when
+        State firstBowl = FirstBowl.from(pins);
+        Score beforeScore = Score.spare();
+        Score actual = firstBowl.calculateAdditionalScore(beforeScore);
+
+        // then
+        assertAll(
+                () -> assertThat(actual.isFinish()).isTrue(),
+                () -> assertThat(actual.score()).isEqualTo(beforeScore.score() + pins.count())
+        );
+    }
+
+    @DisplayName("이전이 Strike 일 때, calculateAdditionalScore 의 결과로 알맞는 Score 인스턴스를 반한하는지 테스트")
+    @Test
+    void 반환_calculateAdditionalScore_Strike_일_경우() {
+        // given
+        Pins pins = Pins.valueOf(9);
+
+        // when
+        State firstBowl = FirstBowl.from(pins);
+        Score beforeScore = Score.strike();
+        Score actual = firstBowl.calculateAdditionalScore(beforeScore);
+
+        // then
+        assertAll(
+                () -> assertThat(actual.isFinish()).isTrue(),
+                () -> assertThat(actual.score()).isEqualTo(beforeScore.score() + pins.count())
         );
     }
 
