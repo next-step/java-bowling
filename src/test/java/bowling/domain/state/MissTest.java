f054e261a760bf124a10a8a86930e4f6089c8408
김우재 <kwj1270@naver.com>
2021-05-09 22:28:38 +0900

    test:Miss 인스턴스 생성시 입력되는 값의 합이 10 이상일시 예외처리 테스트작성

--- src/test/java/bowling/domain/state/MissTest.java
+++ src/test/java/bowling/domain/state/MissTest.java
@@ -1,5 +1,6 @@
 package bowling.domain.state;
 
+import bowling.exception.InvalidFirstBowlSizeException;
 import bowling.exception.PinsNullPointerException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -36,5 +37,18 @@ class MissTest {
         assertThatThrownBy(() -> Miss.of(firstPins, secondPins))
                 .isInstanceOf(PinsNullPointerException.class)
                 .hasMessage("Pins 인스턴스가 null 입니다.");
+    }
+
+    @DisplayName("Miss 인스턴스 생성시 입력되는 값의 합이 10 이상일시 예외처리 테스트")
+    @Test
+    void 검증_값의_합이_10미만() {
+        // given
+        Pins firstPins = Pins.valueOf(9);
+        Pins secondPins = Pins.valueOf(1);
+
+        // when and then
+        assertThatThrownBy(() -> Miss.of(firstPins, secondPins))
+                .isInstanceOf(InvalidMissSizeException.class)
+                .hasMessage("Miss 에 대해 알맞지 않은 크기가 입력 되었습니다.");
     }
 }
