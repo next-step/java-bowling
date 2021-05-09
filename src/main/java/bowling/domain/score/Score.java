d05dcdd0575043bb5633a16f68911388a84e3ed4
김우재 <kwj1270@naver.com>
2021-05-09 16:24:16 +0900

    feat:Score 인스턴스 셍성 기능 구현

--- src/main/java/bowling/domain/score/Score.java
+++ src/main/java/bowling/domain/score/Score.java
@@ -1,4 +1,16 @@
 package bowling.domain.score;
 
-public class Score {
+public final class Score {
+
+    private final int score;
+    private final BonusCount bonusCount;
+
+    private Score(final int score, final BonusCount bonusCount) {
+        this.score = score;
+        this.bonusCount = bonusCount;
+    }
+
+    public static Score of(int score, BonusCount bonusCount) {
+        return new Score(score, bonusCount);
+    }
 }
