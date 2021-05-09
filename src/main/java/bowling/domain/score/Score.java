06a0236d4d6381fae6e294fa0b2787ebf1639fe0
김우재 <kwj1270@naver.com>
2021-05-09 16:30:30 +0900

    feat:Score 인스턴스에 들어오는 BonusCount 가 null이면 예외처리 기능 구현

--- src/main/java/bowling/domain/score/Score.java
+++ src/main/java/bowling/domain/score/Score.java
@@ -1,16 +1,27 @@
 package bowling.domain.score;
 
+import bowling.exception.BonusCountNullPointerException;
+
+import java.util.Objects;
+
 public final class Score {
 
     private final int score;
     private final BonusCount bonusCount;
 
     private Score(final int score, final BonusCount bonusCount) {
+        validateNull(bonusCount);
         this.score = score;
         this.bonusCount = bonusCount;
     }
 
-    public static Score of(int score, BonusCount bonusCount) {
+    private final void validateNull(final BonusCount bonusCount) {
+        if (Objects.isNull(bonusCount)) {
+            throw new BonusCountNullPointerException();
+        }
+    }
+
+    public static Score of(final int score, final BonusCount bonusCount) {
         return new Score(score, bonusCount);
     }
 }
--- src/test/java/bowling/domain/score/ScoreTest.java
+++ src/test/java/bowling/domain/score/ScoreTest.java
@@ -1,5 +1,6 @@
 package bowling.domain.score;
 
+import bowling.exception.BonusCountNullPointerException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
