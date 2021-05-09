734081e4c58b355283d79471158ba5f05ab7fef2
김우재 <kwj1270@naver.com>
2021-05-09 15:17:40 +0900

    test:Name 인스턴스가 null 인지  테스트작성

--- src/test/java/bowling/domain/NameTest.java
+++ src/test/java/bowling/domain/NameTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NameTest {
@@ -11,10 +12,26 @@ class NameTest {
     @DisplayName("Name 인스턴스 생성에 관한 테스트")
     @Test
     void 생성() {
+        // given
         String stringName = "kwj";
 
+        // when
         Name name = Name.valueOf(stringName);
 
+        // then
         assertThat(name).isNotNull();
+    }
+
+    @DisplayName("Name 인스턴스가 null 인지  테스트")
+    @Test
+    void 검증_null() {
+        // given
+        String stringName = null;
+
+        // when and then
+        assertThatThrownBy(() -> Name.valueOf(stringName))
+                .isInstanceOf(StringNullPointerException.class)
+                .hasMessage("String 인스턴스가 null 입니다.");
+
     }
 }
