b9996435fc948a07e67e6fea03848a630fcd3558
김우재 <kwj1270@naver.com>
2021-05-09 15:24:21 +0900

    test:Name 인스턴스가 다른 언어를 사용하는 경우 예외처리 테스트작성

--- src/test/java/bowling/domain/NameTest.java
+++ src/test/java/bowling/domain/NameTest.java
@@ -23,7 +23,7 @@ class NameTest {
         assertThat(name).isNotNull();
     }
 
-    @DisplayName("Name 인스턴스가 null 인지  테스트")
+    @DisplayName("Name 인스턴스가 null 인 경우, 예외 처리 구현 테스트")
     @Test
     void 검증_null() {
         // given
@@ -35,5 +35,16 @@ class NameTest {
                 .hasMessage("String 인스턴스가 null 입니다.");
     }
 
+    @DisplayName("Name 인스턴스가 다른 언어를 사용하는 경우 예외처리 테스트")
+    @Test
+    void 검증_영어_외_다른_언어_사용() {
+        // given
+        String stringName = "김우재";
+
+        // when and then
+        assertThatThrownBy(() -> Name.valueOf(stringName))
+                .isInstanceOf(NameIncludeVariableLanguage.class)
+                .hasMessage("이름에 영어 외 다른 언어들이 섞여있습니다.");
+    }
 
 }
