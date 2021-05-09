3c58377eba3a6ccb879f6f88f8f5c10312752145
김우재 <kwj1270@naver.com>
2021-05-09 23:46:37 +0900

    feat:States 인스턴스 생성 여부 테스트작성

--- src/main/java/bowling/domain/state/States.java
+++ src/main/java/bowling/domain/state/States.java
@@ -1,5 +1,19 @@
 package bowling.domain.state;
 
-public class States {
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public final class States {
+
+    private final LinkedList<State> states;
+
+    private States() {
+        this.states = new LinkedList<>(Arrays.asList(Ready.initialize()));
+    }
+
+    public static final States initialize() {
+        return new States();
+    }
+
 
 }
