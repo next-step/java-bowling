6464c465eedaba16ce1c1caeab46861715ef5f2a
김우재 <kwj1270@naver.com>
2021-05-09 20:39:33 +0900

    test:Strike 인스턴스의 Score 반환 여부 테스트작성

--- src/test/java/bowling/domain/state/StrikeTest.java
+++ src/test/java/bowling/domain/state/StrikeTest.java
@@ -1,5 +1,6 @@
 package bowling.domain.state;
 
+import bowling.domain.score.Score;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,6 +17,18 @@ class StrikeTest {
         assertAll(
                 () -> assertThat(strike).isNotNull(),
                 () -> assertThat(strike).isInstanceOf(Strike.class)
+        );
+    }
+
+    @DisplayName("Strike 인스턴스의 Score 반환 여부 테스트")
+    @Test
+    void 반환_Score() {
+        State strike = Strike.initialize();
+
+        assertAll(
+                () -> assertThat(strike.score()).isNotNull(),
+                () -> assertThat(strike.score()).isInstanceOf(Score.class),
+                () -> assertThat(strike.score().score()).isEqualTo(10)
         );
     }
 }
