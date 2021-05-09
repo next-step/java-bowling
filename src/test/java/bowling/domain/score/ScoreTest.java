eadd5e4024b03ef0c5ad7a03e6de5bf5aa698300
김우재 <kwj1270@naver.com>
2021-05-09 16:26:13 +0900

    test:Score 인스턴스에 들어오는 BonusCount 가 null 인지 테스트작성

--- src/test/java/bowling/domain/score/ScoreTest.java
+++ src/test/java/bowling/domain/score/ScoreTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScoreTest {
@@ -20,5 +21,19 @@ class ScoreTest {
 
         // then
         assertThat(score).isNotNull();
+    }
+
+    @DisplayName("Score 인스턴스에 들어오는 BonusCount 가 null 인지 테스트")
+    @Test
+    void 검증_null() {
+        // given
+        int scorePoint = 10;
+        BonusCount bonusCount = null;
+
+        // when and then
+        assertThatThrownBy(() -> Score.of(scorePoint, bonusCount))
+                .isInstanceOf(BonusCountNullPointerException.class)
+                .hasMessage("BonusCount 인스턴스가 null 입니다.");
+
     }
 }
