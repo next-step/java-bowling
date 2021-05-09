ac650bf6a00b00c558a5c58d74be28c51318dc4d
김우재 <kwj1270@naver.com>
2021-05-09 19:07:53 +0900

    feat:Pins 인스턴스 생성 기능 구현

--- src/main/java/bowling/domain/Pins.java
+++ src/main/java/bowling/domain/Pins.java
@@ -1,4 +1,28 @@
 package bowling.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pins {
+
+    private static final int EMPTY = 0;
+    private static final int FULL = 10;
+    private static final List<Pins> CACHE;
+
+    private final int pinCount;
+
+    static {
+        CACHE = new ArrayList<>();
+        for (int count = EMPTY; count <= FULL; count++) {
+            CACHE.add(new Pins(count));
+        }
+    }
+
+    public Pins(final int pinCount) {
+        this.pinCount = pinCount;
+    }
+
+    public static Pins valueOf(final int pinCount) {
+        return CACHE.get(pinCount);
+    }
 }
