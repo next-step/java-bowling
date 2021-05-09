46c181df526913755b62e1efa5e8ed845e20d273
김우재 <kwj1270@naver.com>
2021-05-09 21:50:00 +0900

    feat:Spare 인스턴스가 Miss, Spare, Strike 일때, 알맞는 보너스 값 반환하도록 구현

--- src/test/java/bowling/domain/state/SpareTest.java
+++ src/test/java/bowling/domain/state/SpareTest.java
@@ -73,9 +73,28 @@ class SpareTest {
         );
     }
 
-    @DisplayName("Spare 인스턴스가 calculateAdditionalScore 의 결과로 알맞는 Score 인스턴스를 반한하는지 테스트")
+    @DisplayName("이전이 Miss 일 때, calculateAdditionalScore 의 결과로 알맞는 Score 인스턴스를 반한하는지 테스트")
     @Test
-    void 반환_calculateAdditionalScore_spare_일_경우() {
+    void 반환_calculateAdditionalScore_Miss_일_경우() {
+        // given
+        Pins firstPins = Pins.valueOf(1);
+        Pins secondPins = Pins.valueOf(9);
+
+        // when
+        State spare = Spare.of(firstPins, secondPins);
+        Score beforeScore = Score.miss(Pins.valueOf(0));
+        Score actual = spare.calculateAdditionalScore(beforeScore);
+
+        // then
+        assertAll(
+                () -> assertThat(actual.isFinish()).isTrue(),
+                () -> assertThat(actual.score()).isEqualTo(beforeScore.score())
+        );
+    }
+
+    @DisplayName("이전이 Spare 일 때, calculateAdditionalScore 의 결과로 알맞는 Score 인스턴스를 반한하는지 테스트")
+    @Test
+    void 반환_calculateAdditionalScore_Spare_일_경우() {
         // given
         Pins firstPins = Pins.valueOf(1);
         Pins secondPins = Pins.valueOf(9);
@@ -91,4 +110,24 @@ class SpareTest {
                 () -> assertThat(actual.score()).isEqualTo(beforeScore.score() + firstPins.count())
         );
     }
+
+    @DisplayName("이전이 Strike 일 때, calculateAdditionalScore 의 결과로 알맞는 Score 인스턴스를 반한하는지 테스트")
+    @Test
+    void 반환_calculateAdditionalScore_Strike_일_경우() {
+        // given
+        Pins firstPins = Pins.valueOf(1);
+        Pins secondPins = Pins.valueOf(9);
+
+        // when
+        State spare = Spare.of(firstPins, secondPins);
+        Score beforeScore = Score.strike();
+        Score actual = spare.calculateAdditionalScore(beforeScore);
+
+        // then
+        assertAll(
+                () -> assertThat(actual.isFinish()).isTrue(),
+                () -> assertThat(actual.score()).isEqualTo(beforeScore.score() + spare.score().score())
+        );
+    }
+
 }
