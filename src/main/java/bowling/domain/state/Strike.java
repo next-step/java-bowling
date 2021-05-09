545a676c4de327f8efa2edbdc59414cac735f2f4
김우재 <kwj1270@naver.com>
2021-05-09 20:37:22 +0900

    feat:Strike 인스턴스 생성 기능 구현

--- src/main/java/bowling/domain/state/Strike.java
+++ src/main/java/bowling/domain/state/Strike.java
@@ -1,4 +1,29 @@
 package bowling.domain.state;
 
-public class Strike {
+import bowling.domain.score.Score;
+
+public class Strike extends Finish {
+
+    private static final int FULL = 10;
+
+    private Strike() { }
+
+    public static State initialize() {
+        return new Strike();
+    }
+
+    @Override
+    public Score score() {
+        return null;
+    }
+
+    @Override
+    public Score calculateAdditionalScore(Score score) {
+        return null;
+    }
+
+    @Override
+    public String description() {
+        return null;
+    }
 }
