3bf0810cd22f7389ba4337898753bc2b63aa9b11
김우재 <kwj1270@naver.com>
2021-05-09 21:09:56 +0900

    test:FirstBowl 인스턴스에 null 입력시 예외처리 테스트작성

--- src/test/java/bowling/domain/state/FirstBowlTest.java
+++ src/test/java/bowling/domain/state/FirstBowlTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FirstBowlTest {
@@ -22,6 +23,18 @@ class FirstBowlTest {
                 () -> assertThat(firstBowl).isNotNull(),
                 () -> assertThat(firstBowl).isInstanceOf(FirstBowl.class)
         );
+    }
+
+    @DisplayName("FirstBowl 인스턴스에 null 입력시 예외처리 테스트")
+    @Test
+    void 검증_null() {
+        // given
+        Pins pins = null;
+
+        // when and then
+        assertThatThrownBy(() -> FirstBowl.from(pins))
+                .isInstanceOf(PinsNullPointerException.class)
+                .hasMessage("Pins 인스턴스가 null 입니다.");
     }
 
 }
