fe632453637e64701c86d9919f608f3894a46167
김우재 <kwj1270@naver.com>
2021-05-09 21:27:02 +0900

    test:Spare 인스턴스 생성시 null 입력 시 예외처리 테스트작성

--- src/test/java/bowling/domain/state/SpareTest.java
+++ src/test/java/bowling/domain/state/SpareTest.java
@@ -1,9 +1,11 @@
 package bowling.domain.state;
 
+import bowling.exception.PinsNullPointerException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SpareTest {
@@ -25,20 +27,16 @@ class SpareTest {
         );
     }
 
-    @DisplayName("Spare 인스턴스 생성 여부 테스트")
+    @DisplayName("Spare 인스턴스 생성시 null 입력 시 예외처리 테스트")
     @Test
-    void 생성() {
+    void 검증_null() {
         // given
-        Pins firstPins = Pins.valueOf(0);
-        Pins secondPins = Pins.valueOf(10);
+        Pins firstPins = null;
+        Pins secondPins = null;
 
-        // when
-        State spare = Spare.of(firstPins, secondPins);
-
-        // then
-        assertAll(
-                () -> assertThat(spare).isNotNull(),
-                () -> assertThat(spare).isInstanceOf(Spare.class)
-        );
+        // when and then
+        assertThatThrownBy(() -> Spare.of(firstPins, secondPins))
+                .isInstanceOf(PinsNullPointerException.class)
+                .hasMessage("Pins 인스턴스가 null 입니다.");
     }
 }
