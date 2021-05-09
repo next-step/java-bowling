e254310faa10f0b742362fa5033fa6bbfdc49260
김우재 <kwj1270@naver.com>
2021-05-09 15:40:25 +0900

    test:BonusScoreCount 인스턴스 생성 여부 테스트작성

---
+++ src/main/java/bowling/domain/BonusScoreCount.java
@@ -0,0 +1,5 @@
+package bowling.domain;
+
+public final class BonusScoreCount {
+
+}
---
+++ src/test/java/bowling/domain/ScoreTest.java
@@ -0,0 +1,5 @@
+package bowling.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
