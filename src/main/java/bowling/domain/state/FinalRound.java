58b3902d4b378ce6864a5428bd517e7c1ffd4f4b
김우재 <kwj1270@naver.com>
2021-05-09 23:54:46 +0900

    feat:FinalRound 인스턴스 생성 기능 구현

--- src/main/java/bowling/domain/state/FinalRound.java
+++ src/main/java/bowling/domain/state/FinalRound.java
@@ -1,4 +1,30 @@
 package bowling.domain.state;
 
-public class FinalRound {
+import java.util.ArrayList;
+import java.util.List;
+
+public final class FinalRound {
+
+    private static final int START = 0;
+    private static final int ORIGINAL_FINISH = 2;
+    private static final int BONUS_FINISH = 3;
+
+    private static final List<FinalRound> CACHE;
+
+    private final int opportunity;
+
+    static {
+        CACHE = new ArrayList<>();
+        for (int opportunity = START; opportunity <= BONUS_FINISH; opportunity++) {
+            CACHE.add(new FinalRound(opportunity));
+        }
+    }
+
+    private FinalRound(final int opportunity) {
+        this.opportunity = opportunity;
+    }
+
+    public static final FinalRound initialize() {
+        return CACHE.get(START);
+    }
 }
