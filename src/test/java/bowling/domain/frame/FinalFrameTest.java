fce3c806fc53d25cdcd5e38b6bfe1cea5855fabf
김우재 <kwj1270@naver.com>
2021-05-10 02:48:17 +0900

    test:FinalFrame이 상황에 맞게 Score 반환 여부 테스트

--- src/test/java/bowling/domain/frame/FinalFrameTest.java
+++ src/test/java/bowling/domain/frame/FinalFrameTest.java
@@ -1,6 +1,10 @@
 package bowling.domain.frame;
 
+import bowling.domain.score.Score;
 import bowling.domain.state.Pins;
+import bowling.domain.state.State;
+import bowling.domain.state.States;
+import bowling.domain.state.running.Ready;
 import bowling.exception.NoActionBowlException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,6 +18,19 @@ class FinalFrameTest {
     @DisplayName("FinalFrame 인스턴스 생성 여부 테스트")
     @Test
     void 생성() {
+        // when
+        Frame finalFrame = FinalFrame.initialize();
+
+        // then
+        assertAll(
+                () -> assertThat(finalFrame).isNotNull(),
+                () -> assertThat(finalFrame).isInstanceOf(FinalFrame.class)
+        );
+    }
+
+    @DisplayName("FinalFrame 인스턴스가 알맞는 score 반환 여부 테스트")
+    @Test
+    void 반환_score() {
         // when
         Frame finalFrame = FinalFrame.initialize();
 
@@ -113,4 +130,162 @@ class FinalFrameTest {
                 .hasMessage("현재 상태에서는 더 이상 볼을 던질 수 없습니다.");
     }
 
+    @DisplayName("miss 일 때, FinalFrame 인스턴스가 알맞는 Score 반환 기능 테스트")
+    @Test
+    void 반환_score_miss() {
+        // given
+        Pins firstPins = Pins.valueOf(9);
+        Pins secondPins = Pins.valueOf(0);
+
+        // when
+        Frame finalFrame = FinalFrame.initialize();
+        finalFrame.bowl(firstPins);
+        finalFrame.bowl(secondPins);
+
+        // then
+        assertAll(
+                () -> assertThat(finalFrame.score()).isNotNull(),
+                () -> assertThat(finalFrame.score()).isInstanceOf(Score.class),
+                () -> assertThat(finalFrame.score().score()).isEqualTo(firstPins.count() + secondPins.count())
+        );
+
+    }
+
+    @DisplayName("spare and firstBowl 일 때, FinalFrame 인스턴스가 알맞는 Score 반환 기능 테스트")
+    @Test
+    void 반환_score_spare_and_firstBowl() {
+        // given
+        Pins firstPins = Pins.valueOf(9);
+        Pins secondPins = Pins.valueOf(1);
+        Pins thirdPins = Pins.valueOf(9);
+
+        // when
+        Frame finalFrame = FinalFrame.initialize();
+        finalFrame.bowl(firstPins);
+        finalFrame.bowl(secondPins);
+        finalFrame.bowl(thirdPins);
+
+        // then
+        assertAll(
+                () -> assertThat(finalFrame.score()).isNotNull(),
+                () -> assertThat(finalFrame.score()).isInstanceOf(Score.class),
+                () -> assertThat(finalFrame.score().score()).isEqualTo(19)
+        );
+    }
+
+
+    @DisplayName("spare and strike 일 때, FinalFrame 인스턴스가 알맞는 Score 반환 기능 테스트")
+    @Test
+    void 반환_score_spare_and_strike() {
+        // given
+        Pins firstPins = Pins.valueOf(9);
+        Pins secondPins = Pins.valueOf(1);
+        Pins thirdPins = Pins.valueOf(10);
+
+        // when
+        Frame finalFrame = FinalFrame.initialize();
+        finalFrame.bowl(firstPins);
+        finalFrame.bowl(secondPins);
+        finalFrame.bowl(thirdPins);
+
+        // then
+        assertAll(
+                () -> assertThat(finalFrame.score()).isNotNull(),
+                () -> assertThat(finalFrame.score()).isInstanceOf(Score.class),
+                () -> assertThat(finalFrame.score().score()).isEqualTo(20)
+        );
+    }
+
+
+    @DisplayName("strike_and_miss 일때, FinalFrame 인스턴스가 알맞는 Score 반환 기능 테스트")
+    @Test
+    void 반환_score_strike_and_miss() {
+        // given
+        Pins firstPins = Pins.valueOf(10);
+        Pins secondPins = Pins.valueOf(9);
+        Pins thirdPins = Pins.valueOf(0);
+
+        // when
+        Frame finalFrame = FinalFrame.initialize();
+        finalFrame.bowl(firstPins);
+        finalFrame.bowl(secondPins);
+        finalFrame.bowl(thirdPins);
+
+        // then
+        assertAll(
+                () -> assertThat(finalFrame.score()).isNotNull(),
+                () -> assertThat(finalFrame.score()).isInstanceOf(Score.class),
+                () -> assertThat(finalFrame.score().score()).isEqualTo(19)
+        );
+
+    }
+
+    @DisplayName("strike_and_spare 일때, FinalFrame 인스턴스가 알맞는 Score 반환 기능 테스트")
+    @Test
+    void 반환_score_strike_and_spare() {
+        // given
+        Pins firstPins = Pins.valueOf(10);
+        Pins secondPins = Pins.valueOf(9);
+        Pins thirdPins = Pins.valueOf(1);
+
+        // when
+        Frame finalFrame = FinalFrame.initialize();
+        finalFrame.bowl(firstPins);
+        finalFrame.bowl(secondPins);
+        finalFrame.bowl(thirdPins);
+
+        // then
+        assertAll(
+                () -> assertThat(finalFrame.score()).isNotNull(),
+                () -> assertThat(finalFrame.score()).isInstanceOf(Score.class),
+                () -> assertThat(finalFrame.score().score()).isEqualTo(20)
+        );
+
+    }
+
+    @DisplayName("strike_and_strike 일때, FinalFrame 인스턴스가 알맞는 Score 반환 기능 테스트")
+    @Test
+    void 반환_score_strike_and_strike_and_firstBowl() {
+        // given
+        Pins firstPins = Pins.valueOf(10);
+        Pins secondPins = Pins.valueOf(10);
+        Pins thirdPins = Pins.valueOf(9);
+
+        // when
+        Frame finalFrame = FinalFrame.initialize();
+        finalFrame.bowl(firstPins);
+        finalFrame.bowl(secondPins);
+        finalFrame.bowl(thirdPins);
+
+        // then
+        assertAll(
+                () -> assertThat(finalFrame.score()).isNotNull(),
+                () -> assertThat(finalFrame.score()).isInstanceOf(Score.class),
+                () -> assertThat(finalFrame.score().score()).isEqualTo(29)
+        );
+
+    }
+
+    @DisplayName("strike_and_strike 일때, FinalFrame 인스턴스가 알맞는 Score 반환 기능 테스트")
+    @Test
+    void 반환_score_strike_and_strike_and_strike() {
+        // given
+        Pins firstPins = Pins.valueOf(10);
+        Pins secondPins = Pins.valueOf(10);
+        Pins thirdPins = Pins.valueOf(10);
+
+        // when
+        Frame finalFrame = FinalFrame.initialize();
+        finalFrame.bowl(firstPins);
+        finalFrame.bowl(secondPins);
+        finalFrame.bowl(thirdPins);
+
+        // then
+        assertAll(
+                () -> assertThat(finalFrame.score()).isNotNull(),
+                () -> assertThat(finalFrame.score()).isInstanceOf(Score.class),
+                () -> assertThat(finalFrame.score().score()).isEqualTo(30)
+        );
+
+    }
 }
