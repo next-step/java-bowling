18d33215fd82d151fbcc958b5f7edfcec849115b
김우재 <kwj1270@naver.com>
2021-05-11 00:15:33 +0900

    feat:Frames 인스턴스 생성 기능 구현

--- src/main/java/bowling/domain/frame/Frames.java
+++ src/main/java/bowling/domain/frame/Frames.java
@@ -1,4 +1,30 @@
 package bowling.domain.frame;
 
-public class Frames {
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public final class Frames {
+
+    private static final int FIRST_FRAME = 0;
+
+    private final List<Frame> frames;
+    private Frame current;
+
+    public Frames() {
+        this.frames = generateFrames();
+        this.current = frames.get(FIRST_FRAME);
+    }
+
+    private final List<Frame> generateFrames() {
+        final List<Frame> frames = IntStream.range(Frame.START_SEQUENCE, Frame.LAST_SEQUENCE)
+                .mapToObj(NormalFrame::from)
+                .collect(Collectors.toList());
+        frames.add(FinalFrame.initialize());
+        return frames;
+    }
+
+    public static Frames initialize() {
+        return new Frames();
+    }
 }
