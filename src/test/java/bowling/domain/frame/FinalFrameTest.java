0a134e8b5de152ba437a6906d6df0197f399be69
HJ-Woo <hjwoou@gmail.com>
2021-05-10 14:29:24 +0900

    refactor: Frame의 text fixture는 test case 내부로 이동

--- src/test/java/bowling/domain/frame/FinalFrameTest.java
+++ src/test/java/bowling/domain/frame/FinalFrameTest.java
@@ -13,14 +13,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class FinalFrameTest {
     public static final FinalFrame FRAME = FinalFrame.of();
-    public static final FinalFrame STRIKE_FRAME = FinalFrame.of(STRIKE_PIN, ONE_STRIKE_RESULTS);
-    public static final FinalFrame DOUBLE_STRIKE_FRAME = FinalFrame.of(SPARE_PIN, DOUBLE_STRIKE_RESULTS);
-    public static final FinalFrame SPARE_FRAME = FinalFrame.of(SPARE_PIN, ONE_SPARE_RESULTS);
-    public static final FinalFrame MISS_FRAME = FinalFrame.of(MISS_PIN, MISS_RESULTS);
-    public static final FinalFrame GUTTER_FRAME = FinalFrame.of(GUTTER_PIN, GUTTER_RESULTS);
-    public static final FinalFrame ONE_ROLL_FRAME = FinalFrame.of(THREE_HIT_PIN, THREE_HIT_RESULTS);
-    public static final FinalFrame SPARE_NEXT_FRAME = FinalFrame.of(THIRD_THREE_HIT_PIN, SPARE_THIRD_THREE_RESULTS);
-    public static final FinalFrame STRIKE_NEXT_FRAME = FinalFrame.of(THIRD_THREE_HIT_PIN, STRIKE_THIRD_THREE_RESULTS);
     public FinalFrame frame;
 
     @BeforeEach
@@ -42,7 +34,7 @@ public class FinalFrameTest {
     @Test
     void 스트라이크프레임() {
         Frame resultFrame = frame.roll(HitNumber.of(10));
-        assertThat(resultFrame).isEqualTo(STRIKE_FRAME);
+        assertThat(resultFrame).isEqualTo(FinalFrame.of(STRIKE_PIN, ONE_STRIKE_RESULTS));
         assertThat(resultFrame.isFinished()).isFalse();
         assertThat(resultFrame.totalScore()).isEqualTo(Score.of(10, 0));
     }
@@ -50,7 +42,7 @@ public class FinalFrameTest {
     @Test
     void 스트라이크아닌한번친프레임() {
         Frame resultFrame = frame.roll(HitNumber.of(3));
-        assertThat(resultFrame).isEqualTo(ONE_ROLL_FRAME);
+        assertThat(resultFrame).isEqualTo(FinalFrame.of(THREE_HIT_PIN, THREE_HIT_RESULTS));
         assertThat(resultFrame.isFinished()).isFalse();
         assertThat(resultFrame.totalScore()).isEqualTo(Score.of(3));
     }
@@ -58,7 +50,7 @@ public class FinalFrameTest {
     @Test
     void 스페어프레임() {
         Frame resultFrame = frame.roll(HitNumber.of(3)).roll(HitNumber.of(7));
-        assertThat(resultFrame).isEqualTo(SPARE_FRAME);
+        assertThat(resultFrame).isEqualTo(FinalFrame.of(SPARE_PIN, ONE_SPARE_RESULTS));
         assertThat(resultFrame.isFinished()).isFalse();
         assertThat(resultFrame.totalScore()).isEqualTo(Score.of(10, 0));
     }
@@ -66,7 +58,7 @@ public class FinalFrameTest {
     @Test
     void 거터프레임() {
         Frame resultFrame = frame.roll(HitNumber.of(0));
-        assertThat(resultFrame).isEqualTo(GUTTER_FRAME);
+        assertThat(resultFrame).isEqualTo(FinalFrame.of(GUTTER_PIN, GUTTER_RESULTS));
         assertThat(resultFrame.isFinished()).isFalse();
         assertThat(resultFrame.totalScore()).isEqualTo(Score.of());
     }
@@ -74,7 +66,7 @@ public class FinalFrameTest {
     @Test
     void 미스프레임() {
         Frame resultFrame = frame.roll(HitNumber.of(0)).roll(HitNumber.of(0));
-        assertThat(resultFrame).isEqualTo(MISS_FRAME);
+        assertThat(resultFrame).isEqualTo(FinalFrame.of(MISS_PIN, MISS_RESULTS));
         assertThat(resultFrame.isFinished()).isTrue();
         assertThat(resultFrame.totalScore()).isEqualTo(Score.of());
     }
@@ -82,7 +74,7 @@ public class FinalFrameTest {
     @Test
     void 더블스트라이크() {
         Frame resultFrame = frame.roll(HitNumber.of(10)).roll(HitNumber.of(10));
-        assertThat(resultFrame).isEqualTo(DOUBLE_STRIKE_FRAME);
+        assertThat(resultFrame).isEqualTo(FinalFrame.of(SPARE_PIN, DOUBLE_STRIKE_RESULTS));
         assertThat(resultFrame.isFinished()).isFalse();
         assertThat(resultFrame.totalScore()).isEqualTo(Score.of(20));
     }
@@ -90,7 +82,7 @@ public class FinalFrameTest {
     @Test
     void 스트라이크후_재투구() {
         Frame resultFrame = frame.roll(HitNumber.of(10)).roll(HitNumber.of(10)).roll(HitNumber.of(3));
-        assertThat(resultFrame).isEqualTo(STRIKE_NEXT_FRAME);
+        assertThat(resultFrame).isEqualTo(FinalFrame.of(THIRD_THREE_HIT_PIN, STRIKE_THIRD_THREE_RESULTS));
         assertThat(resultFrame.isFinished()).isTrue();
         assertThat(resultFrame.totalScore()).isEqualTo(Score.of(23));
     }
@@ -98,7 +90,7 @@ public class FinalFrameTest {
     @Test
     void 스페어후_재투구() {
         Frame resultFrame = frame.roll(HitNumber.of(3)).roll(HitNumber.of(7)).roll(HitNumber.of(3));
-        assertThat(resultFrame).isEqualTo(SPARE_NEXT_FRAME);
+        assertThat(resultFrame).isEqualTo(FinalFrame.of(THIRD_THREE_HIT_PIN, SPARE_THIRD_THREE_RESULTS));
         assertThat(resultFrame.isFinished()).isTrue();
         assertThat(resultFrame.totalScore()).isEqualTo(Score.of(13));
         System.out.println(resultFrame);
--- src/test/java/bowling/domain/frame/NormalFrameTest.java
+++ src/test/java/bowling/domain/frame/NormalFrameTest.java
@@ -17,11 +17,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class NormalFrameTest {
     private static final int MAX_INDEX = 10;
     public static final NormalFrame FRAME = NormalFrame.of();
-    public static final NormalFrame STRIKE_FRAME = NormalFrame.of(STRIKE_PIN, StrikeTest.STRIKE);
-    public static final NormalFrame SPARE_FRAME = NormalFrame.of(SPARE_PIN, SPARE_3);
-    public static final NormalFrame MISS_FRAME = NormalFrame.of(MISS_PIN, MISS);
-    public static final NormalFrame GUTTER_FRAME = NormalFrame.of(GUTTER_PIN, GUTTER);
-    public static final NormalFrame ONE_ROLL_FRAME = NormalFrame.of(THREE_HIT_PIN, ONE_HIT_3);
     public NormalFrame frame;
 
     @BeforeEach
@@ -43,7 +38,7 @@ public class NormalFrameTest {
     @Test
     void 스트라이크프레임() {
         Frame resultFrame = frame.roll(HitNumber.of(10));
-        assertThat(resultFrame).isEqualTo(STRIKE_FRAME);
+        assertThat(resultFrame).isEqualTo(NormalFrame.of(STRIKE_PIN, StrikeTest.STRIKE));
         assertThat(resultFrame.isFinished()).isTrue();
         assertThat(resultFrame.canAccumulate()).isTrue();
         assertThat(resultFrame.totalScore()).isEqualTo(ScoreTest.STRIKE);
@@ -52,7 +47,7 @@ public class NormalFrameTest {
     @Test
     void 스트라이크아닌한번친프레임() {
         Frame resultFrame = frame.roll(HitNumber.of(3));
-        assertThat(resultFrame).isEqualTo(ONE_ROLL_FRAME);
+        assertThat(resultFrame).isEqualTo(NormalFrame.of(THREE_HIT_PIN, ONE_HIT_3));
         assertThat(resultFrame.isFinished()).isFalse();
         assertThat(resultFrame.canAccumulate()).isFalse();
         assertThat(resultFrame.totalScore()).isEqualTo(Score.of(3));
@@ -61,7 +56,7 @@ public class NormalFrameTest {
     @Test
     void 거터프레임() {
         Frame resultFrame = frame.roll(HitNumber.of(0));
-        assertThat(resultFrame).isEqualTo(GUTTER_FRAME);
+        assertThat(resultFrame).isEqualTo(NormalFrame.of(GUTTER_PIN, GUTTER));
         assertThat(resultFrame.isFinished()).isFalse();
         assertThat(resultFrame.canAccumulate()).isFalse();
         assertThat(resultFrame.totalScore()).isEqualTo(ScoreTest.GUTTER);
@@ -70,7 +65,7 @@ public class NormalFrameTest {
     @Test
     void 스페어프레임() {
         Frame resultFrame = frame.roll(HitNumber.of(3)).roll(HitNumber.of(7));
-        assertThat(resultFrame).isEqualTo(SPARE_FRAME);
+        assertThat(resultFrame).isEqualTo(NormalFrame.of(SPARE_PIN, SPARE_3));
         assertThat(resultFrame.isFinished()).isTrue();
         assertThat(resultFrame.canAccumulate()).isTrue();
         assertThat(resultFrame.totalScore()).isEqualTo(Score.of(10, 1));
@@ -79,7 +74,7 @@ public class NormalFrameTest {
     @Test
     void 미스프레임() {
         Frame resultFrame = frame.roll(HitNumber.of(0)).roll(HitNumber.of(0));
-        assertThat(resultFrame).isEqualTo(MISS_FRAME);
+        assertThat(resultFrame).isEqualTo(NormalFrame.of(MISS_PIN, MISS));
         assertThat(resultFrame.isFinished()).isTrue();
         assertThat(resultFrame.canAccumulate()).isFalse();
         assertThat(resultFrame.totalScore()).isEqualTo(ScoreTest.GUTTER);
@@ -87,19 +82,19 @@ public class NormalFrameTest {
 
     @Test
     void 스트라이크의추가점수() {
-        Score score = STRIKE_FRAME.accumulate(3).totalScore();
+        Score score = NormalFrame.of(STRIKE_PIN, StrikeTest.STRIKE).accumulate(3).totalScore();
         assertThat(score).isEqualTo(Score.of(13, 1));
     }
 
     @Test
     void 스페어의추가점수() {
-        Score score = SPARE_FRAME.accumulate(5).totalScore();
+        Score score = NormalFrame.of(SPARE_PIN, SPARE_3).accumulate(5).totalScore();
         assertThat(score).isEqualTo(Score.of(15, 0));
     }
 
     @Test
     void 미스의추가점수는그대로(){
-        Score score = MISS_FRAME.accumulate(5).totalScore();
+        Score score = NormalFrame.of(MISS_PIN, MISS).accumulate(5).totalScore();
         assertThat(score).isEqualTo(ScoreTest.GUTTER);
     }
 }
