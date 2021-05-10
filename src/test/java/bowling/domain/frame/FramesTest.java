487dd8e3fe13257801d455fd3c7eebb87bb8e584
김우재 <kwj1270@naver.com>
2021-05-11 00:17:13 +0900

    test:Frames 인스턴스가 현재 Frame 의 순서를 반환하는지 테스트작성

--- src/test/java/bowling/domain/frame/FramesTest.java
+++ src/test/java/bowling/domain/frame/FramesTest.java
@@ -15,4 +15,15 @@ class FramesTest {
 
         assertThat(frames).isNotNull();
     }
+
+
+    @DisplayName("Frames 인스턴스가 현재 Frame 의 순서를 반환하는지 테스트")
+    @Test
+    void 반환_순서() {
+        // when
+        Frames frames = Frames.initialize();
+
+        // then
+        assertThat(frames.sequence()).isEqualTo(1);
+    }
 }
