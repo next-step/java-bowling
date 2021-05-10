cb83d6311fcde7432ab51707e8a8993e16c22fb7
김우재 <kwj1270@naver.com>
2021-05-10 09:58:41 +0900

    feat:NormalFrame 인스턴스 생성 기능 구현

--- src/main/java/bowling/domain/frame/NormalFrame.java
+++ src/main/java/bowling/domain/frame/NormalFrame.java
@@ -1,4 +1,36 @@
 package bowling.domain.frame;
 
-public class NormalFrame {
+import bowling.domain.score.Score;
+import bowling.domain.state.Pins;
+import bowling.domain.state.State;
+import bowling.domain.state.running.Ready;
+
+public class NormalFrame extends Frame {
+
+    private State state;
+    private Frame next;
+
+    private NormalFrame() {
+        this.state = Ready.initialize();
+        this.next = null;
+    }
+
+    public static Frame initialize() {
+        return new NormalFrame();
+    }
+
+    @Override
+    public Frame bowl(Pins fallPins) {
+        return null;
+    }
+
+    @Override
+    public boolean isFinish() {
+        return false;
+    }
+
+    @Override
+    public Score score() {
+        return null;
+    }
 }
