0c27ad62710979f655659633ecdf655fe0991b79
김우재 <kwj1270@naver.com>
2021-05-11 22:20:30 +0900

    feat:FrameResult 인스턴스 생성 여부 테스트작성

--- src/main/java/bowling/domain/frame/FrameResult.java
+++ src/main/java/bowling/domain/frame/FrameResult.java
@@ -1,4 +1,18 @@
 package bowling.domain.frame;
 
-public class FrameResult {
+import bowling.domain.score.Score;
+
+public final class FrameResult {
+
+    private final Frame frame;
+    private final Score score;
+
+    private FrameResult(final Frame frame, final Score score) {
+        this.frame = frame;
+        this.score = score;
+    }
+
+    public static FrameResult of(final Frame frame, final Score score) {
+        return new FrameResult(frame, score);
+    }
 }
