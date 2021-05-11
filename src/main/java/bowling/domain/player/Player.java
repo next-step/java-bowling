75ff378632ad8ea9344c5b9cd088f81f8acdf1f0
김우재 <kwj1270@naver.com>
2021-05-11 20:22:06 +0900

    feat:Player 인스턴스 생성 기능 구현

--- src/main/java/bowling/domain/player/Player.java
+++ src/main/java/bowling/domain/player/Player.java
@@ -1,5 +1,18 @@
 package bowling.domain.player;
 
+import bowling.domain.frame.Frames;
+
 public final class Player {
 
+    private final Name name;
+    private final Frames frames;
+
+    public Player(final Name name) {
+        this.name = name;
+        this.frames = Frames.initialize();
+    }
+
+    public static final Player from(final Name name) {
+        return new Player(name);
+    }
 }
--- src/test/java/bowling/domain/player/PlayerTest.java
+++ src/test/java/bowling/domain/player/PlayerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
@@ -20,4 +21,6 @@ class PlayerTest {
         // then
         assertThat(player).isNotNull();
     }
+
+
 }
