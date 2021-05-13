684970b1b647949bfbd45aa4b5f5ae69ae9ffb57
김우재 <kwj1270@naver.com>
2021-05-14 00:02:55 +0900

    test:PlayerCount 인스턴스에 부적절한 값 입력시 예외처리 여부 테스트작성

--- src/test/java/bowling/domain/player/PlayerCountTest.java
+++ src/test/java/bowling/domain/player/PlayerCountTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerCountTest {
@@ -19,5 +20,25 @@ class PlayerCountTest {
 
         // then
         assertThat(playerCount).isNotNull();
+    }
+
+    @DisplayName("PlayerCount 인스턴스에 부적절한 값 입력시 예외처리 여부 테스트")
+    @Test
+    void 검증_0이하의_값() {
+        // given
+        int zero = 0;
+        int negative = -1;
+
+        // when and then
+        assertAll(
+                () -> assertThatThrownBy(() -> new PlayerCount(zero))
+                        .isInstanceOf(InvalidPlayerCountException.class)
+                        .hasMessage("PlayerCount 에 대해 알맞지 않은 크기 (%s)가 입력 되었습니다."),
+
+                () -> assertThatThrownBy(() -> new PlayerCount(negative))
+                        .isInstanceOf(InvalidPlayerCountException.class)
+                        .hasMessage("PlayerCount 에 대해 알맞지 않은 크기 (%s)가 입력 되었습니다.")
+        );
+
     }
 }
