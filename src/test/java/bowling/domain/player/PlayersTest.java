ff2fe1e4b88cb494dc2f2428533b3c5eb4fce973
김우재 <kwj1270@naver.com>
2021-05-14 01:03:45 +0900

    test:Players 인스턴스 생성 여부 테스트작성

---
+++ src/test/java/bowling/domain/player/PlayersTest.java
@@ -0,0 +1,27 @@
+package bowling.domain.player;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayersTest {
+
+    @DisplayName("Players 인스턴스 생성 여부 테스트")
+    @Test
+    void 생성() {
+        // given
+        List<Player> playerList = new ArrayList<>();
+
+        // when
+        Players players = new Players(playerList);
+
+        // then
+        assertThat(players).isNotNull();
+    }
+
+}
