67d2ec273a58d9ea35b15d97fa541355b3d5df65
김우재 <kwj1270@naver.com>
2021-05-14 09:47:17 +0900

    test:Players 인스턴스 생성에 Null 입력시 예외처리 여부 테스트작성

--- src/test/java/bowling/domain/player/PlayersTest.java
+++ src/test/java/bowling/domain/player/PlayersTest.java
@@ -26,4 +26,18 @@ class PlayersTest {
     }
 
 
+    @DisplayName("Players 인스턴스 생성에 Null 입력시 예외처리 여부 테스트")
+    @Test
+    void 검증_null() {
+        // given
+        List<Player> nullList = null;
+
+        // when and then
+        assertThatThrownBy(() -> new Players(nullList))
+                .isInstanceOf(PlayerListNullPointerException.class)
+                .hasMessage("List<Player> 인스턴스가 null 입니다.");
+
+    }
+
+
 }
