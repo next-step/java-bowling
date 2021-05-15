b04800c15f40dc162def09f91cfb0f7616f50804
gyunghoe <ehrudghl123@naver.com>
2021-05-15 23:49:09 +0900

    feat(reset): clean source code

--- src/main/java/bowling/BowlingMain.java
+++ src/main/java/bowling/BowlingMain.java
@@ -1,9 +1,7 @@
 package bowling;
 
-import bowling.controller.BowlingController;
-
 public class BowlingMain {
     public static void main(String[] args) {
-        new BowlingController().playBowling();
+
     }
 }
