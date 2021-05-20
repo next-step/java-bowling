e5d6cf16087a01f82520130b913935c4b2e1f6c1
gyunghoe <ehrudghl123@naver.com>
2021-05-21 03:04:36 +0900

    refact(view): refact output view

--- src/main/java/bowling/controller/BowlingController.java
+++ src/main/java/bowling/controller/BowlingController.java
@@ -2,19 +2,19 @@ package bowling.controller;
 
 import bowling.domain.Bowling;
 import bowling.view.InputView;
-//import bowling.view.OutputView;
+import bowling.view.OutputView;
 
 public class BowlingController {
 
     private InputView inputView = new InputView();
-//    private OutputView outputView = new OutputView();
+    private OutputView outputView = new OutputView();
 
     public void play() {
         Bowling bowling = new Bowling(inputView.getName());
-//        outputView.printGame(bowlingGame);
+        outputView.printGame(bowling);
         while (!bowling.isEnd()) {
             bowling.play(inputView.getPoint(bowling.frameCount()));
-//            outputView.printGame(bowlingGame);
+            outputView.printGame(bowling);
         }
     }
 }
