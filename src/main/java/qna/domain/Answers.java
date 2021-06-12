c166e93d7bbfbb8e34617830bcc8d7aaeec82e41
suhyunsim <cnc1106@naver.com>
2021-06-13 03:39:15 +0900

    feat: Answers에 답변 전체 삭제 메소드 추가

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -1,19 +1,23 @@
 package qna.domain;
 
 import org.hibernate.annotations.Where;
+import qna.exception.CannotDeleteException;
 
 import javax.persistence.CascadeType;
 import javax.persistence.OneToMany;
 import javax.persistence.OrderBy;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
+import static qna.exception.CannotDeleteException.QUESTION_WITH_OTHERS_ANSWER;
 
 public class Answers {
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
     @Where(clause = "deleted = false")
     @OrderBy("id ASC")
-    private List<Answer> answers;
+    private final List<Answer> answers;
 
     public Answers() {
         this(new ArrayList<>());
@@ -27,4 +31,45 @@ public class Answers {
         answers.add(answer);
     }
 
+    public DeleteHistories deleteAllAnswers(User loginUser) {
+        DeleteHistories deleteHistories = new DeleteHistories();
+        validateDeletableAnswers(loginUser);
+        for (Answer answer : answers) {
+            deleteHistories.add(answer.deleteAnswer(loginUser));
+        }
+        return deleteHistories;
+    }
+
+    private void validateDeletableAnswers(User loginUser) {
+        for (Answer answer : answers) {
+            validateDeletableAnswer(answer, loginUser);
+        }
+    }
+
+    private void validateDeletableAnswer(Answer answer, User loginUser) {
+        if (!answer.isDeletable(loginUser)) {
+            throw new CannotDeleteException(QUESTION_WITH_OTHERS_ANSWER);
+        }
+    }
+
+    public int size() {
+        return answers.size();
+    }
+
+    public List<Answer> answers() {
+        return answers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Answers answers1 = (Answers) o;
+        return Objects.equals(answers, answers1.answers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(answers);
+    }
 }
