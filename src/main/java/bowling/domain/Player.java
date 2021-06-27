fbf6bbc675db7eec6b51ae8ee7f9b4ed407c6b02
ad7549 <ad7549@gmail.com>
2021-06-27 13:12:27 +0900

    feat: Update Player
    Player가Game을 포함하도록 수정

--- src/main/java/bowling/domain/Player.java
+++ src/main/java/bowling/domain/Player.java
@@ -1,21 +1,41 @@
 package bowling.domain;
 
+import java.util.List;
+
 public class Player {
     private static final int MAX_NAME_LENGTH = 3;
     private static final String OVER_MAX_NAME_LENGTH_MESSAGE =
             String.format("이름은 %d글자까지 가능합니다.", MAX_NAME_LENGTH);
 
     private final String name;
+    private final Game game;
 
-    public Player(final String name) {
+    public Player(String name) {
+        this(name, Game.init());
+    }
+
+    private Player(String name, Game game) {
         if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException(OVER_MAX_NAME_LENGTH_MESSAGE);
         }
 
         this.name = name;
+        this.game = game;
     }
 
     public String name() {
         return name;
+    }
+
+    public boolean playing() {
+        return game.playing();
+    }
+
+    public Player play(int knockedPinsCount) {
+        return new Player(name, game.play(knockedPinsCount));
+    }
+
+    public List<Frame> frames() {
+        return game.frames();
     }
 }
