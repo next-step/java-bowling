c93a47c6e1dc61e934109895de681435fda67c87
nokchax <nokchax@gmail.com>
2021-07-31 18:45:51 +0900

    [B] refactor: change test annotation to jupiter

--- src/main/java/bowling/domain/pin/DownedPin.java
+++ src/main/java/bowling/domain/pin/DownedPin.java
@@ -1,10 +1,23 @@
 package bowling.domain.pin;
 
 public class DownedPin {
+    private static final int MIN_NUM_OF_DOWNED_PINS = 0;
+    private static final int MAX_NUM_OF_DOWNED_PINS = 0;
+
     private final int numOfDownedPins;
 
     private DownedPin(int numOfDownedPins) {
+        validate(numOfDownedPins);
+
         this.numOfDownedPins = numOfDownedPins;
+    }
+
+    private void validate(int numOfDownedPins) {
+        if (numOfDownedPins < MIN_NUM_OF_DOWNED_PINS || numOfDownedPins > MAX_NUM_OF_DOWNED_PINS) {
+            throw new IllegalArgumentException(
+                    "Number of downed pins should range in " + MIN_NUM_OF_DOWNED_PINS + " ~ " + MAX_NUM_OF_DOWNED_PINS
+            );
+        }
     }
 
     public static DownedPin from(int numOfDownedPins) {
--- src/test/java/bowling/domain/pin/DownedPinTest.java
+++ src/test/java/bowling/domain/pin/DownedPinTest.java
@@ -1,7 +1,7 @@
 package bowling.domain.pin;
 
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,18 +9,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("볼링 핀에 대한 클래스 테스트")
-public class DownedPinTest {
+class DownedPinTest {
 
     @DisplayName("쓰러진 볼링핀을 표현하는 Pin 클래스는 쓰러진 핀의 수를 가지고 초기화 한다")
     @Test
-    public void init() {
+    void init() {
         assertThat(DownedPin.from(0)).isInstanceOf(DownedPin.class);
     }
 
     @DisplayName("쓰러진 핀의 값이 0 보다 작거나 10 보다 크면 예외를 발생시킨다")
     @ValueSource(ints = {-1, 11})
     @ParameterizedTest
-    public void initException(int numOfDownedPins) {
+    void initException(int numOfDownedPins) {
         assertThatThrownBy(() ->DownedPin.from(numOfDownedPins)).isInstanceOf(IllegalArgumentException.class);
     }
 
