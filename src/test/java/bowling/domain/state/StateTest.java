dc28b595388f07f58045f6b7f761654575bba1ff
nokchax <nokchax@gmail.com>
2021-07-31 19:07:08 +0900

    [R] test: test downPins exception

--- src/test/java/bowling/domain/state/StateTest.java
+++ src/test/java/bowling/domain/state/StateTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("프레임 상태 표현을 위한 클래스 테스트")
 class StateTest {
@@ -16,6 +17,14 @@ class StateTest {
         DownedPin downedPin = DownedPin.from(5);
 
         assertThat(someState.downPins(downedPin)).isInstanceOf(State.class);
+    }
+
+    @DisplayName("상태에 쓰러진 null 핀 정보를 넘기면 예외를 발생 시킨다")
+    @Test
+    void downPinsException() {
+        State someState = new SomeState();
+
+        assertThatThrownBy(() -> someState.downPins(null)).isInstanceOf(IllegalArgumentException.class);
     }
 
     class SomeState extends State {
