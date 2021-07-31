c96d5bdd3cded0210f204434787d946134d20cee
nokchax <nokchax@gmail.com>
2021-07-31 19:08:05 +0900

    [G] feat: impl throw exception when DownPin is null

--- src/main/java/bowling/domain/state/State.java
+++ src/main/java/bowling/domain/state/State.java
@@ -2,10 +2,19 @@ package bowling.domain.state;
 
 import bowling.domain.pin.DownedPin;
 
+import java.util.Objects;
+
 public abstract class State {
 
     public State downPins(DownedPin downedPin) {
+        validate(downedPin);
         return nextState(downedPin);
+    }
+
+    private void validate(DownedPin downedPin) {
+        if (Objects.isNull(downedPin)) {
+            throw new IllegalArgumentException("DownedPin can't be null");
+        }
     }
 
     protected abstract State nextState(DownedPin downedPin);
