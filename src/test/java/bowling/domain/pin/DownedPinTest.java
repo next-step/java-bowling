43a8c8b6129be9404981c65f70d0375d132b24d7
nokchax <nokchax@gmail.com>
2021-07-31 18:38:17 +0900

    [R] test: test init exception of DownedPins

--- src/test/java/bowling/domain/pin/DownedPinTest.java
+++ src/test/java/bowling/domain/pin/DownedPinTest.java
@@ -2,9 +2,11 @@ package bowling.domain.pin;
 
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("볼링 핀에 대한 클래스 테스트")
 public class DownedPinTest {
@@ -17,8 +19,9 @@ public class DownedPinTest {
 
     @DisplayName("쓰러진 핀의 값이 0 보다 작거나 10 보다 크면 예외를 발생시킨다")
     @ValueSource(ints = {-1, 11})
-    public void initException(int downedPins) {
-        assertThat(DownedPin.from(0)).isInstanceOf(DownedPin.class);
+    @ParameterizedTest
+    public void initException(int numOfDownedPins) {
+        assertThatThrownBy(() ->DownedPin.from(numOfDownedPins)).isInstanceOf(IllegalArgumentException.class);
     }
 
 }
