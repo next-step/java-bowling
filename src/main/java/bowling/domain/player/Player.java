809ca857001627458d5e778cd790fdd7d5498ba4
nokchax <nokchax@gmail.com>
2021-08-01 16:17:28 +0900

    [G] feat: impl name String validation

--- src/main/java/bowling/domain/player/Player.java
+++ src/main/java/bowling/domain/player/Player.java
@@ -1,12 +1,22 @@
 package bowling.domain.player;
 
+import bowling.util.StringUtils;
+
 public class Player {
     private static final int LIMIT_LENGTH_OF_NAME = 3;
 
     private final String name;
 
     public Player(String name) {
+        validate(name);
+
         this.name = name;
+    }
+
+    private void validate(String name) {
+        if (StringUtils.isEmpty(name) || name.length() != LIMIT_LENGTH_OF_NAME) {
+            throw new IllegalStateException("Name must be " + LIMIT_LENGTH_OF_NAME + " length chars");
+        }
     }
 
     public static Player from(String name) {
