335d42e2a63d05c775bb932126be49917d89e5f4
nokchax <nokchax@gmail.com>
2021-08-01 16:15:38 +0900

    [G] feat: impl isEmpty of String class

--- src/main/java/bowling/util/StringUtils.java
+++ src/main/java/bowling/util/StringUtils.java
@@ -6,6 +6,6 @@ public class StringUtils {
     private StringUtils() {}
 
     public static boolean isEmpty(String stringValue) {
-        return false;
+        return Objects.isNull(stringValue) || stringValue.isEmpty();
     }
 }
