209e5a4fcdfe8a34963988e82c265dcdfcdf1c04
nokchax <nokchax@gmail.com>
2021-08-01 21:06:40 +0900

    [B] refactor: rename param

--- src/main/java/bowling/dto/StateDtos.java
+++ src/main/java/bowling/dto/StateDtos.java
@@ -8,14 +8,14 @@ import java.util.stream.Collectors;
 public class StateDtos {
     private final List<StateDto> stateDtos;
 
-    public StateDtos(List<State> state) {
-        stateDtos = state.stream()
+    public StateDtos(List<State> states) {
+        stateDtos = states.stream()
                 .map(StateDto::from)
                 .collect(Collectors.toList());
     }
 
-    public static StateDtos from(List<State> state) {
-        return new StateDtos(state);
+    public static StateDtos from(List<State> states) {
+        return new StateDtos(states);
     }
 
     public List<StateDto> getStateDtos() {
