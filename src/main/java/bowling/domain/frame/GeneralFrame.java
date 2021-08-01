82a2b261774e1644d4f2f22894251f1527044dd2
nokchax <nokchax@gmail.com>
2021-08-01 09:57:04 +0900

    [G] feat: feat init GeneralFrame

--- src/main/java/bowling/domain/frame/GeneralFrame.java
+++ src/main/java/bowling/domain/frame/GeneralFrame.java
@@ -1,13 +1,15 @@
 package bowling.domain.frame;
 
+import bowling.domain.state.Preparation;
 import bowling.domain.state.State;
 
 public class GeneralFrame extends Frame {
-    protected GeneralFrame(State state) {
+
+    private GeneralFrame(State state) {
         super(state);
     }
 
-    public static Object init() {
-        return null;
+    public static GeneralFrame init() {
+        return new GeneralFrame(Preparation.instance());
     }
 }
