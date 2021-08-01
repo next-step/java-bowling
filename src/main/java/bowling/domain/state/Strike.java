8360424ee65cf311b35d7d03edf9f86b2f5e0298
nokchax <nokchax@gmail.com>
2021-08-01 20:26:13 +0900

    [B] refactor: arrange method

--- src/main/java/bowling/domain/state/Strike.java
+++ src/main/java/bowling/domain/state/Strike.java
@@ -6,6 +6,7 @@ import java.util.List;
 import static bowling.domain.pin.DownedPins.MAX_NUM_OF_DOWNED_PINS;
 
 public class Strike extends EndState {
+
     public static Strike instance() {
         return new Strike();
     }
