fdb9e471a7992d7b1a1700e0a5e5741d1a71a02d
nokchax <nokchax@gmail.com>
2021-08-01 16:19:02 +0900

    [R] test: test init BowlingPlayer

--- src/main/java/bowling/domain/player/BowlingPlayer.java
+++ src/main/java/bowling/domain/player/BowlingPlayer.java
@@ -5,4 +5,8 @@ import bowling.domain.frame.Frames;
 public class BowlingPlayer {
     private Player player;
     private Frames frames;
+
+    public static Object init(String name) {
+        return null;
+    }
 }
---
+++ src/test/java/bowling/domain/player/BowlingPlayerTest.java
@@ -0,0 +1,17 @@
+package bowling.domain.player;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("볼링 플레이어 테스트")
+class BowlingPlayerTest {
+
+    @DisplayName("볼링 플레이어는 이름을 가지고 초기화 한다")
+    @Test
+    void init() {
+        assertThat(BowlingPlayer.init("NOK")).isInstanceOf(BowlingPlayer.class);
+    }
+
+}
