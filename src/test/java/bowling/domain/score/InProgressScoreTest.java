2a58772b4480d97f8600db008bf586233e621f98
nokchax <nokchax@gmail.com>
2021-08-02 18:47:48 +0900

    [R] test: test throw exception when input is invalid

--- src/test/java/bowling/domain/score/InProgressScoreTest.java
+++ src/test/java/bowling/domain/score/InProgressScoreTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("계산중인 스코어 테스트")
 class InProgressScoreTest {
@@ -19,5 +20,11 @@ class InProgressScoreTest {
     void isCalculable() {
         InProgressScore inProgressScore = InProgressScore.init(10, 2);
         assertThat(inProgressScore.isCalculable()).isFalse();
+    }
+
+    @DisplayName("계산중이 스코어에 남은 카운트는 3 이상이 될 수 없다.") // 스트라이크라고 하더라도 추가적으로 필요한 점수는 2회분
+    @Test
+    void initException() {
+        assertThatThrownBy(() -> InProgressScore.init(10, 3)).isInstanceOf(IllegalArgumentException.class);
     }
 }
