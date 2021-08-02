c7d76bfc4a0eba7fd3b61452f2c15dadd90a2468
nokchax <nokchax@gmail.com>
2021-08-02 18:46:13 +0900

    [R] test: test isCalculable of InProgressScore

--- src/test/java/bowling/domain/score/InProgressScoreTest.java
+++ src/test/java/bowling/domain/score/InProgressScoreTest.java
@@ -11,7 +11,13 @@ class InProgressScoreTest {
     @DisplayName("계산중인 스코어는 계산 완료까지 필요한 남은 카운트와, 현재의 점수를 가지고 초기화한다")
     @Test
     void init() {
-        assertThat(InProgressScore.init(10, 3)).isInstanceOf(InProgressScore.class);
+        assertThat(InProgressScore.init(10, 2)).isInstanceOf(InProgressScore.class);
     }
 
+    @DisplayName("계산중인 스코어는 계산이 불가능 하다")
+    @Test
+    void isCalculable() {
+        InProgressScore inProgressScore = InProgressScore.init(10, 2);
+        assertThat(inProgressScore.isCalculable()).isFalse();
+    }
 }
