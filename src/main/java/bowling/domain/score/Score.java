1205336abe3efa423f40a1f862e1bb3caad32ed6
nokchax <nokchax@gmail.com>
2021-08-02 18:57:17 +0900

    [G] feat: impl init exception of score

--- src/main/java/bowling/domain/score/Score.java
+++ src/main/java/bowling/domain/score/Score.java
@@ -1,10 +1,19 @@
 package bowling.domain.score;
 
 public abstract class Score {
+    private static final int SCORE_LIMIT = 30;
     protected final int score;
 
     protected Score(int score) {
+        validate(score);
+
         this.score = score;
+    }
+
+    private void validate(int score) {
+        if (score > SCORE_LIMIT) {
+            throw new IllegalArgumentException("Score can't over " + SCORE_LIMIT);
+        }
     }
 
     public abstract boolean isCalculable();
