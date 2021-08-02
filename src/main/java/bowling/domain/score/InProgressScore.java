7d2bf05121a81e564c98ae39668c3d23444b6951
nokchax <nokchax@gmail.com>
2021-08-02 19:18:58 +0900

    [G] feat: impl score equals

--- src/main/java/bowling/domain/score/InProgressScore.java
+++ src/main/java/bowling/domain/score/InProgressScore.java
@@ -1,5 +1,7 @@
 package bowling.domain.score;
 
+import java.util.Objects;
+
 public class InProgressScore extends Score {
     private static final int LEFT_COUNT_LIMIT = 2;
 
@@ -35,5 +37,19 @@ public class InProgressScore extends Score {
         }
 
         return InProgressScore.init(score + anotherScore.score, leftCount - 1);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        InProgressScore that = (InProgressScore) o;
+        return leftCount == that.leftCount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), leftCount);
     }
 }
