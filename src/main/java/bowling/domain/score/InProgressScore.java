f6e0ab33fecd33a4fc5cf4536abee770eeded5a4
nokchax <nokchax@gmail.com>
2021-08-02 18:49:21 +0900

    [G] feat: impl throw exception when input is over limit

--- src/main/java/bowling/domain/score/InProgressScore.java
+++ src/main/java/bowling/domain/score/InProgressScore.java
@@ -1,10 +1,22 @@
 package bowling.domain.score;
 
 public class InProgressScore extends Score {
+    private static final int LEFT_COUNT_LIMIT = 2;
+
     private final int leftCount;
+
     protected InProgressScore(int score, int leftCount) {
         super(score);
+
+        validate(leftCount);
+
         this.leftCount = leftCount;
+    }
+
+    private void validate(int leftCount) {
+        if (leftCount > LEFT_COUNT_LIMIT) {
+            throw new IllegalArgumentException("Left count can't exceed " + LEFT_COUNT_LIMIT);
+        }
     }
 
     public static InProgressScore init(int score, int leftCount) {
