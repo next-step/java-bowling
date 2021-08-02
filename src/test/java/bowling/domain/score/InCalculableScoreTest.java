84f13e9c7267984df066e297149d073a3f543234
nokchax <nokchax@gmail.com>
2021-08-02 19:38:04 +0900

    [R] test: test isCalculable of IncalculableScore

--- src/test/java/bowling/domain/score/InCalculableScoreTest.java
+++ src/test/java/bowling/domain/score/InCalculableScoreTest.java
@@ -8,10 +8,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("계산 불가능한 스코어 테스트")
 class InCalculableScoreTest {
 
-    @DisplayName("계싼 불가능한 스코어는 점수 없이 초기화 가능하다")
+    @DisplayName("계산 불가능한 스코어는 점수 없이 초기화 가능하다")
     @Test
     void init() {
         assertThat(InCalculableScore.init()).isInstanceOf(InCalculableScore.class);
+    }
+
+    @DisplayName("계산 불가능한 스코어는 계산이 불가능하다")
+    @Test
+    void isCalculable() {
+        assertThat(InCalculableScore.init().isCalculable()).isFalse();
     }
 
 }
