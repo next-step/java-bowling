8d77f7b35eaa25122f46212392fcbe7e562a99f4
nokchax <nokchax@gmail.com>
2021-08-03 18:50:10 +0900

    [R] test: test throw exception when init param is null

--- src/test/java/bowling/domain/player/BowlingPlayersTest.java
+++ src/test/java/bowling/domain/player/BowlingPlayersTest.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("볼링 게임 참가자를 표현하기 위한 클래스 테스트")
 class BowlingPlayersTest {
@@ -16,6 +17,12 @@ class BowlingPlayersTest {
     void init() {
         List<String> names = Arrays.asList("NOK", "CHA", "KKH");
         assertThat(BowlingPlayers.init(names)).isInstanceOf(BowlingPlayers.class);
+    }
+
+    @DisplayName("이름 리스트가 null 일 경우 예외를 발생 시킨다")
+    @Test
+    void initException() {
+        assertThatThrownBy(() -> BowlingPlayers.init(null)).isInstanceOf(IllegalArgumentException.class);
     }
 
 }
