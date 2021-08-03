7efecd354b7e2249261c20c2fd85ab1781794c8d
nokchax <nokchax@gmail.com>
2021-08-03 18:54:07 +0900

    [B] refactor: use fixture

--- src/test/java/bowling/BowlingGameTest.java
+++ src/test/java/bowling/BowlingGameTest.java
@@ -3,9 +3,7 @@ package bowling;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
+import static bowling.domain.Fixture.PLAYER_NAMES;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("볼링 게임을 표현하기 위한 클래스 테스트")
@@ -14,8 +12,7 @@ class BowlingGameTest {
     @DisplayName("볼링 게임은 참가자 이름을 가지고 초기화 한다")
     @Test
     void init() {
-        List<String> names = Arrays.asList("NOK", "CHA", "KKH");
-        assertThat(BowlingGame.init(names)).isInstanceOf(BowlingGame.class);
+        assertThat(BowlingGame.init(PLAYER_NAMES)).isInstanceOf(BowlingGame.class);
     }
 
 }
