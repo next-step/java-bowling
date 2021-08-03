42dcbb5106b1a45b8278776ad265346709773e59
nokchax <nokchax@gmail.com>
2021-08-03 18:53:29 +0900

    [G] test: test get FirstPlayer of BowlingPlayers

--- src/test/java/bowling/domain/Fixture.java
+++ src/test/java/bowling/domain/Fixture.java
@@ -2,6 +2,9 @@ package bowling.domain;
 
 import bowling.domain.pin.DownedPins;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Fixture {
 
     public static final DownedPins DOWNED_PINS_10 = DownedPins.from(10);
@@ -10,6 +13,9 @@ public class Fixture {
     public static final DownedPins DOWNED_PINS_3 = DownedPins.from(3);
     public static final DownedPins DOWNED_PINS_2 = DownedPins.from(2);
     public static final DownedPins DOWNED_PINS_0 = DownedPins.from(0);
+
+
+    public static final List<String> PLAYER_NAMES = Arrays.asList("NOK", "CHA", "KKH");
 
     private Fixture() {}
 }
--- src/test/java/bowling/domain/player/BowlingPlayersTest.java
+++ src/test/java/bowling/domain/player/BowlingPlayersTest.java
@@ -3,9 +3,7 @@ package bowling.domain.player;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
+import static bowling.domain.Fixture.PLAYER_NAMES;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -15,14 +13,21 @@ class BowlingPlayersTest {
     @DisplayName("초기화를 위해서 이름 리스트가 필요하다")
     @Test
     void init() {
-        List<String> names = Arrays.asList("NOK", "CHA", "KKH");
-        assertThat(BowlingPlayers.init(names)).isInstanceOf(BowlingPlayers.class);
+        assertThat(BowlingPlayers.init(PLAYER_NAMES)).isInstanceOf(BowlingPlayers.class);
     }
 
     @DisplayName("이름 리스트가 null 일 경우 예외를 발생 시킨다")
     @Test
     void initException() {
         assertThatThrownBy(() -> BowlingPlayers.init(null)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("첫번째 플레이어 테스트")
+    @Test
+    void firstPlayer() {
+        BowlingPlayers bowlingPlayers = new BowlingPlayers(PLAYER_NAMES);
+
+        assertThat(bowlingPlayers.firstPlayer().getName()).isEqualTo(PLAYER_NAMES.get(0));
     }
 
 }
