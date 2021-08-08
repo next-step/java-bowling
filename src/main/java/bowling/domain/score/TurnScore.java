6348898dd6f82441fc79221c605d883eec4e72e2
darkant99 <darkant99@naver.com>
2021-08-09 04:09:50 +0900

    [FEAT] Score 프로덕션 & 테스트 코드

---
+++ src/main/java/bowling/domain/score/TurnScore.java
@@ -0,0 +1,33 @@
+package bowling.domain.score;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public final class TurnScore extends Score {
+    private static final int MIN_VALUE = 0;
+    private static final int MAX_VALUE = 10;
+    private static final Map<Integer, TurnScore> CACHED =
+            IntStream.rangeClosed(MIN_VALUE, MAX_VALUE)
+                    .boxed()
+                    .collect(
+                            Collectors.toMap(
+                                    iValue -> iValue,
+                                    TurnScore::new)
+                    );
+
+    private TurnScore(final int score) {
+        super(score);
+    }
+
+    public static TurnScore max() {
+        return of(MAX_VALUE);
+    }
+
+    public static TurnScore of(int score) {
+        if (!CACHED.containsKey(score)) {
+            throw new IllegalArgumentException("유효하지 않은 스코어 입니다.");
+        }
+        return CACHED.get(score);
+    }
+}
---
+++ src/test/java/bowling/domain/score/TurnScoreTest.java
@@ -0,0 +1,30 @@
+package bowling.domain.score;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class TurnScoreTest {
+    @ValueSource(ints = {
+            0, 1, 10
+    })
+    @ParameterizedTest
+    @DisplayName("score 유효성 검사 통과")
+    void ofTest(int score) {
+        assertThatCode(() -> TurnScore.of(score))
+                .doesNotThrowAnyException();
+    }
+
+    @ValueSource(ints = {
+            -1, 11
+    })
+    @ParameterizedTest
+    @DisplayName("score가 0 미만이거나 10 초과일떄")
+    void ofInvalidTest(int score) {
+        assertThatThrownBy(() -> TurnScore.of(score))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
