5832114459f89636c293c39a91192c302f97a2c2
jayce6190 <jayce6190@gmail.com>
2021-08-12 13:06:51 +0900

    refactor: ResultState 삭제 Common State로 통합

--- src/main/java/bowling/domain/state/CommonState.java
+++ src/main/java/bowling/domain/state/CommonState.java
@@ -1,11 +1,13 @@
 package bowling.domain.state;
 
 import bowling.domain.pin.Pins;
+import bowling.domain.score.CommonScore;
+import bowling.domain.score.Score;
 
 import java.util.Collections;
 import java.util.List;
 
-public interface CommonState extends ResultState {
+public interface CommonState {
 
     CommonState hitPins(Pins pins);
 
@@ -13,6 +15,34 @@ public interface CommonState extends ResultState {
 
     default List<CommonState> getState() {
         return Collections.singletonList(this);
+    }
+
+    default Score score() {
+        return CommonScore.ofBase();
+    }
+
+    default Score addScore(Score score) {
+        if (score.isCompute()) {
+            return score;
+        }
+
+        return addBonusScore(score);
+    }
+
+    default Score addBonusScore(Score score) {
+        return score;
+    }
+
+    default boolean isFinish() {
+        return false;
+    }
+
+    default boolean isMiss() {
+        return false;
+    }
+
+    default boolean isAllHit() {
+        return false;
     }
 
 }
