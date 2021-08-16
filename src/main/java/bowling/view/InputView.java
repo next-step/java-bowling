21dea40d1b5104e9572277e17f4857b9233f3df2
choibyunghyeon <choi8608@gmail.com>
2021-08-17 08:03:56 +0900

    feat: 플레이어 이름 입력

---
+++ src/main/java/bowling/view/InputView.java
@@ -0,0 +1,14 @@
+package bowling.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static String inputPlayerName() {
+        System.out.println("플레이어 이름은(3 english letters)?: ");
+
+        return SCANNER.nextLine();
+    }
+}
