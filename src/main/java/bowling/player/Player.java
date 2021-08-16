b13d62136c51a5c67c8aa2d4e70d07e834dcb259
shpark <bingbingpa@gmail.com>
2021-08-16 19:02:41 +0900

    feat: Player 이름을 입력 받는다.

---
+++ src/main/java/bowling/player/Player.java
@@ -0,0 +1,27 @@
+package bowling.player;
+
+import bowling.exception.IllegalPayerNameException;
+
+import java.util.regex.Pattern;
+
+public class Player {
+
+    private static final Pattern NAME_PATTERN = Pattern.compile("^[a-zA-Z]{3}$");
+
+    private final String name;
+
+    private Player(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    public static Player of(String name) {
+        return new Player(name);
+    }
+
+    private void validate(String name) {
+        if (!NAME_PATTERN.matcher(name).find()) {
+            throw new IllegalPayerNameException(name);
+        }
+    }
+}
