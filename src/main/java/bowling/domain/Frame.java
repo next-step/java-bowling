f9b9557581c4451b286d3fa4718ddc8c9eb5ec2f
choibyunghyeon <choi8608@gmail.com>
2021-08-17 07:20:24 +0900

    feat: 프레임

---
+++ src/main/java/bowling/domain/Frame.java
@@ -0,0 +1,34 @@
+package bowling.domain;
+
+public class Frame {
+
+    private final PitchResult first;
+    private PitchResult second;
+
+    private Frame(final PitchResult first, final PitchResult second) {
+        createValidation(first, second);
+
+        this.first = first;
+        this.second = second;
+    }
+
+    private void createValidation(final PitchResult first, final PitchResult second) {
+        first.add(second);
+    }
+
+    private Frame(final PitchResult first) {
+        this.first = first;
+    }
+
+    public static Frame of(final PitchResult first, final PitchResult second) {
+        return new Frame(first, second);
+    }
+
+    public static Frame of(final PitchResult first) {
+        return new Frame(first);
+    }
+
+    public Frame second(final PitchResult second) {
+        return Frame.of(this.first, second);
+    }
+}
