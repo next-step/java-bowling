8e2e9046a047a0e948ecd8b513e0ec05ae724fea
shirohoo <shirohoo4102@gmail.com>
2021-08-20 01:24:52 +0900

    feat: Frames

---
+++ src/main/java/bowling/frame/Frames.java
@@ -0,0 +1,42 @@
+package bowling.frame;
+
+import bowling.pin.Pin;
+import bowling.state.Ready;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Frames {
+    private static final int LIMIT_FRAME_OF_BOWLING_GAME = 10;
+
+    private final List<Frame> frames;
+
+    private Frames() {
+        this.frames = new ArrayList<>(LIMIT_FRAME_OF_BOWLING_GAME);
+        frames.add(NormalFrame.init(Ready.init()));
+    }
+
+    public static Frames init() {
+        return new Frames();
+    }
+
+    public void pitch(final Pin pin) {
+        Frame frame = getLastFrame();
+        frame.play(pin);
+        ifItNormalFrameProceedNextFrame(frame);
+    }
+
+    private void ifItNormalFrameProceedNextFrame(final Frame frame) {
+        if (frame instanceof NormalFrame) {
+            ((NormalFrame) frame).proceed(frames);
+        }
+    }
+
+    private Frame getLastFrame() {
+        return frames.get(frames.size() - 1);
+    }
+
+    public boolean isEnd() {
+        return getLastFrame().hasTurn();
+    }
+}
