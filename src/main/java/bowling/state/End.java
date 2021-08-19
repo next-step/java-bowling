a40c06778f7ef9b682b8f5ae4ff15bbfaf14e142
shirohoo <shirohoo4102@gmail.com>
2021-08-20 01:34:57 +0900

    feat: End

---
+++ src/main/java/bowling/state/End.java
@@ -0,0 +1,35 @@
+package bowling.state;
+
+import bowling.exception.EndStateException;
+import bowling.pin.Pin;
+
+import java.util.Collections;
+import java.util.List;
+
+public class End implements State {
+    private End() {}
+
+    public static End init() {
+        return new End();
+    }
+
+    @Override
+    public State nextPitch(final Pin pin) {
+        throw new EndStateException("더 이상 진행할 수 없습니다.");
+    }
+
+    @Override
+    public List<Integer> getScore() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isEnd() {
+        return true;
+    }
+
+    @Override
+    public boolean isClean() {
+        return false;
+    }
+}
