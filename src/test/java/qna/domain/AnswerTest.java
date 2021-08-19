8233aa9ae5e8ca98f3e53f08adc3f934ffc7d0ba
SeungJae Lee <lsj8367@naver.com>
2021-08-19 17:07:57 +0900

    test: 생성, 예외 단위 테스트 추가

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,26 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    void 같은지_테스트() throws CannotDeleteException {
+        A1.isOwner(UserTest.JAVAJIGI);
+        assertThat(A1.getWriter()).isEqualTo(UserTest.JAVAJIGI);
+    }
+
+    @Test
+    void 예외처리() {
+        assertThatThrownBy(() -> {
+            A1.isOwner(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+
+    }
 }
--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,6 +1,9 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+
 public class UserTest {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+
 }
