40be5696a93698a920512fd07b24c90fe2493f6c
shirohoo <shirohoo4102@gmail.com>
2021-08-20 23:07:02 +0900

    feat: 화면출력 코드 작성 중 2

--- src/main/java/bowling/BowlingConsole.java
+++ src/main/java/bowling/BowlingConsole.java
@@ -2,22 +2,22 @@ package bowling;
 
 import bowling.frame.Frames;
 import bowling.pin.Pin;
-
-import java.util.Scanner;
+import bowling.player.Player;
+import bowling.view.InputView;
+import bowling.view.ResultView;
 
 public class BowlingConsole {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("플레이어 이름은(3 english letters)?: ");
-        String name = sc.nextLine();
-
+        String name = InputView.playerName();
+        Player player = Player.init(name);
         Frames frames = Frames.init();
-        while (!frames.isEnd()) {
-            System.out.print("투구: ");
-            Pin hitCount = Pin.from(Integer.parseInt(sc.nextLine()));
-            frames.pitch(hitCount);
+        BowlingFacade facade = BowlingFacade.of(player, frames);
 
-            System.out.println(frames);
+        int i = 1;
+        while (!frames.isEnd()) {
+            ResultView.scoreBoard(name);
+            Pin hitCount = Pin.from(InputView.pitch(i++));
+            frames.pitch(hitCount);
         }
     }
 }
