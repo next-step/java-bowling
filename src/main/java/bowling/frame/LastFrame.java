d429b5ec816ce69517f65442140ec96abb9e669c
shirohoo <shirohoo4102@gmail.com>
2021-08-20 17:14:40 +0900

    feat: LastFrame

---
+++ src/main/java/bowling/frame/LastFrame.java
@@ -0,0 +1,31 @@
+package bowling.frame;
+
+import bowling.pin.Pin;
+import bowling.state.End;
+import bowling.state.State;
+
+public class LastFrame implements Frame {
+    private State state;
+
+    private boolean hasTurn = true;
+
+    private LastFrame(final State state) {
+        this.state = state;
+    }
+
+    public static LastFrame init(final State state) {
+        return new LastFrame(state);
+    }
+
+    @Override
+    public void play(final Pin pin) {
+        if (state.nextPitch(pin) instanceof End) {
+            this.hasTurn = false;
+        }
+    }
+
+    @Override
+    public boolean hasTurn() {
+        return !state.isEnd();
+    }
+}
