04dfb7329ca483a35add0de3b242cb604050e68f
shirohoo <shirohoo4102@gmail.com>
2021-08-20 18:57:57 +0900

    feat: Player.init

---
+++ src/main/java/bowling/player/Player.java
@@ -0,0 +1,24 @@
+package bowling.player;
+
+import java.util.Objects;
+
+public class Player {
+    private static final int LIMIT_NAME_LENGTH = 3;
+
+    private String name;
+
+    private Player(String name) {
+        this.name = name;
+    }
+
+    public static Player init(String name) {
+        verifyName(Objects.requireNonNull(name));
+        return new Player(name);
+    }
+
+    private static void verifyName(String name) {
+        if(name.length() != LIMIT_NAME_LENGTH){
+            throw new IllegalArgumentException(String.format("선수 이름은 %s글자 여야 합니다 !", LIMIT_NAME_LENGTH));
+        }
+    }
+}
