6027c44f2a96c7063bcd178a028b87165ad362b1
shirohoo <shirohoo4102@gmail.com>
2021-08-20 16:50:17 +0900

    feat: LastStateDecorator

---
+++ src/main/java/bowling/state/LastStateDecorator.java
@@ -0,0 +1,72 @@
+package bowling.state;
+
+import bowling.pin.Pin;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static bowling.state.LastStateProxy.LIMIT_PITCH_COUNT;
+
+public class LastStateDecorator implements State {
+    private final List<State> states;
+
+    private LastStateDecorator() {
+        this.states = new ArrayList<>(LIMIT_PITCH_COUNT);
+        states.add(Ready.init());
+    }
+
+    public static LastStateDecorator init() {
+        return new LastStateDecorator();
+    }
+
+    @Override
+    public State nextPitch(final Pin pin) {
+        final State updateState = popLastState().nextPitch(pin);
+        states.add(updateState);
+        nextState(updateState);
+        return this;
+    }
+
+    private void nextState(final State lastState) {
+        if (lastState.isClean()) {
+            states.add(Ready.init());
+        }
+    }
+
+    private State popLastState() {
+        return states.remove(states.size() - 1);
+    }
+
+    private State lastState() {
+        return states.get(states.size() - 1);
+    }
+
+    public void tailStateCheck() {
+        if (lastState() instanceof Progress) {
+            states.add(popLastState().nextPitch(Pin.from(Pin.MIN_COUNT_OF_PIN)));
+        }
+        if (lastState() instanceof Ready) {
+            popLastState();
+        }
+    }
+
+    @Override
+    public List<Integer> getScore() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isEnd() {
+        return !isExtraChance();
+    }
+
+    @Override
+    public boolean isClean() {
+        return false;
+    }
+
+    private boolean isExtraChance() {
+        return !lastState().isEnd();
+    }
+}
