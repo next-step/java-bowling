71ca7a6f0f56670a2145f986ac4aadcf25f4ae94
shpark <bingbingpa@gmail.com>
2021-08-21 10:58:20 +0900

    feat: 사용자로부터 숫자를 입력 받는다.

---
+++ src/main/java/bowling/bowling/Pins.java
@@ -0,0 +1,33 @@
+package bowling.bowling;
+
+import bowling.exception.IllegalPinNumberException;
+
+public class Pins {
+
+    private static final int MAX_PIN_NUMBER = 10;
+    private static final int MIN_PIN_NUMBER = 0;
+
+    private final int pin;
+
+    private Pins(int pin) {
+        this.pin = pin;
+    }
+
+    public static Pins of(int pin) {
+        validate(pin);
+        return new Pins(pin);
+    }
+
+    public boolean isStrike() {
+        return pin == MAX_PIN_NUMBER;
+    }
+
+    private static void validate(int pin) {
+        if (pin > MAX_PIN_NUMBER || pin < MIN_PIN_NUMBER) {
+            throw new IllegalPinNumberException(pin);
+        }
+    }
+
+
+}
+
--- src/test/java/bowling/player/PlayerTest.java
+++ src/test/java/bowling/player/PlayerTest.java
@@ -1,18 +1,19 @@
 package bowling.player;
 
 import bowling.exception.IllegalPayerNameException;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PlayerTest {
 
     @DisplayName("플레이어 이름은 영문 3글자여야 한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"플레이어", "한글", "세글자", "!@#", "123", "1234567"})
+    @ValueSource(strings = {"플레이어", "한글", "세글자", "!@#", "123", "1234567", ""})
     void create_player(String name) {
-        Assertions.assertThatThrownBy(() -> Player.of(name))
+        assertThatThrownBy(() -> Player.of(name))
                 .isInstanceOf(IllegalPayerNameException.class);
     }
 
