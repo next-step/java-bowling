accee07375112bb332d703b8c83745ad702be54a
shirohoo <shirohoo4102@gmail.com>
2021-08-22 17:18:11 +0900

    test: PlayerTest

--- src/test/java/bowling/player/PlayerTest.java
+++ src/test/java/bowling/player/PlayerTest.java
@@ -1,5 +1,6 @@
 package bowling.player;
 
+import bowling.dto.PlayerDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,5 +10,16 @@ public class PlayerTest {
         Assertions.assertThatThrownBy(() -> {
             Player.init("PJSS");
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void init() throws Exception {
+        Assertions.assertThat(Player.init("PJS")).isInstanceOf(Player.class);
+    }
+
+    @Test
+    void convert() throws Exception {
+        Player pjs = Player.init("PJS");
+        Assertions.assertThat(pjs.convert()).isInstanceOf(PlayerDto.class);
     }
 }
