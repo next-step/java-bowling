ada5af820eb7f031f2cf358e6f392b147778e77f
shirohoo <shirohoo4102@gmail.com>
2021-08-22 22:05:42 +0900

    feat: 누락된 final 추가

--- src/main/java/bowling/state/LastStateProxy.java
+++ src/main/java/bowling/state/LastStateProxy.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class LastStateProxy implements State {
     public static final int LIMIT_PITCH_COUNT = 3;
 
-    private LastStateDecorator state;
+    private final LastStateDecorator state;
 
     private int pitchCount;
 
