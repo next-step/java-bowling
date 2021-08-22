09ebc2b2cceda654d3442dcaf562b05101686c81
PIH6585 <pih6585@naver.com>
2021-08-22 23:14:33 +0900

    feat : 질문 vo 수정

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -3,93 +3,119 @@ package qna.domain;
 import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import qna.CannotDeleteException;
+
 @Entity
 public class Question extends AbstractEntity {
-    @Column(length = 100, nullable = false)
-    private String title;
+	@Column(length = 100, nullable = false)
+	private String title;
 
-    @Lob
-    private String contents;
+	@Lob
+	private String contents;
 
-    @ManyToOne
-    @JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"))
-    private User writer;
+	@ManyToOne
+	@JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"))
+	private User writer;
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
-    @Where(clause = "deleted = false")
-    @OrderBy("id ASC")
-    private List<Answer> answers = new ArrayList<>();
+	@OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+	@Where(clause = "deleted = false")
+	@OrderBy("id ASC")
+	private final List<Answer> answers = new ArrayList<>();
 
-    private boolean deleted = false;
+	private boolean deleted = false;
 
-    public Question() {
-    }
+	public Question() {
+	}
 
-    public Question(String title, String contents) {
-        this.title = title;
-        this.contents = contents;
-    }
+	public Question(String title, String contents) {
+		this.title = title;
+		this.contents = contents;
+	}
 
-    public Question(long id, String title, String contents) {
-        super(id);
-        this.title = title;
-        this.contents = contents;
-    }
+	public Question(long id, String title, String contents) {
+		super(id);
+		this.title = title;
+		this.contents = contents;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public Question setTitle(String title) {
-        this.title = title;
-        return this;
-    }
+	public Question setTitle(String title) {
+		this.title = title;
+		return this;
+	}
 
-    public String getContents() {
-        return contents;
-    }
+	public String getContents() {
+		return contents;
+	}
 
-    public Question setContents(String contents) {
-        this.contents = contents;
-        return this;
-    }
+	public Question setContents(String contents) {
+		this.contents = contents;
+		return this;
+	}
 
-    public User getWriter() {
-        return writer;
-    }
+	public User getWriter() {
+		return writer;
+	}
 
-    public Question writeBy(User loginUser) {
-        this.writer = loginUser;
-        return this;
-    }
+	public Question writeBy(User loginUser) {
+		this.writer = loginUser;
+		return this;
+	}
 
-    public void addAnswer(Answer answer) {
-        answer.toQuestion(this);
-        answers.add(answer);
-    }
+	public void addAnswer(Answer answer) {
+		answer.toQuestion(this);
+		answers.add(answer);
+	}
 
-    public boolean isOwner(User loginUser) {
-        return writer.equals(loginUser);
-    }
+	public boolean isOwner(User loginUser) {
+		return writer.equals(loginUser);
+	}
 
-    public Question setDeleted(boolean deleted) {
-        this.deleted = deleted;
-        return this;
-    }
+	public Question setDeleted(boolean deleted) {
+		this.deleted = deleted;
+		return this;
+	}
 
-    public boolean isDeleted() {
-        return deleted;
-    }
+	public boolean isDeleted() {
+		return deleted;
+	}
 
-    public List<Answer> getAnswers() {
-        return answers;
-    }
+	public List<Answer> getAnswers() {
+		return answers;
+	}
 
-    @Override
-    public String toString() {
-        return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
-    }
+	public List<DeleteHistory> deleteQuestion(User loginUser) throws CannotDeleteException {
+		checkAuthority(loginUser);
+		Answers answers = new Answers(getAnswers(), loginUser);
+		List<DeleteHistory> deleteHistories = new ArrayList<>();
+		deleted(deleteHistories);
+		answers.deleteAnswer(deleteHistories);
+		return deleteHistories;
+	}
+
+	public List<DeleteHistory> deleted(List<DeleteHistory> deleteHistories) {
+		setDeleted(true);
+		deleteHistories.add(new DeleteHistory(ContentType.QUESTION, getId(), writer, LocalDateTime.now()));
+		return deleteHistories;
+	}
+
+	private void checkAuthority(User loginUser) throws CannotDeleteException {
+		if (!writer.equals(loginUser)) {
+			throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
+	}
+
 }
