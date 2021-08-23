1fd3f607b5e56f90f4557a3927c00395995a62e1
SeungJae Lee <lsj8367@naver.com>
2021-08-23 17:11:35 +0900

    feat: 조건에 따른 출력형식 맞춤

--- src/main/java/bowling/domain/Pins.java
+++ src/main/java/bowling/domain/Pins.java
@@ -1,9 +1,12 @@
 package bowling.domain;
 
+import bowling.exception.CannotBeBiggerThanMax;
+import bowling.exception.CannotBeLessThanZero;
+
 public class Pins {
     private static final int MIN_PIN = 0;
-    private static final int MAX_PIN = 10;
-
+    private static final int MAX_PINS = 10;
+    private static final String GUTTER = "-";
 
     private final int falledPins;
 
@@ -21,9 +24,53 @@ public class Pins {
             throw new CannotBeLessThanZero(falledPins);
         }
 
-        if (falledPins > MAX_PIN) {
+        if (falledPins > MAX_PINS) {
             throw new CannotBeBiggerThanMax(falledPins);
         }
     }
+
+    public boolean isStrike() {
+        return this.falledPins == MAX_PINS;
+    }
+
+    public int totalPins(Pins secondPins) {
+        int totalPins = this.falledPins + secondPins.falledPins;
+        validPins(totalPins);
+        return totalPins;
+    }
+
+    public boolean isSpear(Pins secondPins) {
+        return totalPins(secondPins) == MAX_PINS;
+    }
+
+    public boolean isMiss(Pins secondPins) {
+        return totalPins(secondPins) < MAX_PINS;
+    }
+
+    public String display() {
+        if(isStrike()) {
+            return "X";
+        }
+
+        return falledPins + " | ";
+    }
+
+    public String display(Pins secondPins) {
+        String result = gutterCheck(falledPins);
+
+        if (isSpear(secondPins)) {
+            return result + " | /";
+        }
+
+        return result + " | " + gutterCheck(secondPins.falledPins);
+    }
+
+    private String gutterCheck(int falledPins) {
+        if (falledPins == MIN_PIN) {
+            return GUTTER;
+        }
+        return String.valueOf(falledPins);
+    }
+
 
 }
