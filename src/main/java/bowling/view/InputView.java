92a547c80334427c46b94fbe13d2e63ee8ba6b5e
SeungJae Lee <lsj8367@naver.com>
2021-08-24 17:12:20 +0900

    feat: 이름 삽입 추가

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -6,7 +6,12 @@ public class InputView {
     private static Scanner sc = new Scanner(System.in);
 
     public static String inputName() {
+        System.out.println("플레이어 이름은(3 english letters)?: ");
         return sc.next();
+    }
+
+    public static int inputPins() {
+        return sc.nextInt();
     }
 
 }
