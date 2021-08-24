4f34cd5ad8d72615c8a21a991422266b0e43d1ae
kdh85 <modudamba@naver.com>
2021-08-24 16:47:28 +0900

    test : step1 Question 클래스 리팩토링에 대한 테스트코드 작성 삭제대상을 생성할때 사용자검증을 테스트코드로 생성. 삭제대상 생성을 통해서 객체를 비교하여 검증하는 테스트코드 생성.

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,51 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
 public class QuestionTest {
-    public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-    public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+  public static final Question Q1 = new Question("title1", "contents1").writeBy(
+      UserTest.JAVAJIGI);
+  public static final Question Q2 = new Question("title2", "contents2").writeBy(
+      UserTest.SANJIGI);
+
+  @DisplayName("질문의 작성자가 본인인지 검증.")
+  @Test
+  void ownerValidation() {
+    assertThat(Q1.isOwner(UserTest.SANJIGI)).isFalse();
+    assertThat(Q1.isOwner(UserTest.JAVAJIGI)).isTrue();
+  }
+
+  @DisplayName("질문 삭제가 불가능한 경우 검증.")
+  @Test
+  void removeValidation() {
+    assertThatThrownBy(() -> Q1.findDeleteQuestionAndAnswers(UserTest.SANJIGI, ofId(1L).getId()))
+        .isInstanceOf(CannotDeleteException.class);
+  }
+
+  @DisplayName("질문삭제 대상 콜랙션을 생성.")
+  @Test
+  void create() throws CannotDeleteException {
+    List<DeleteHistory> deleteObject = Q1.findDeleteQuestionAndAnswers(
+        UserTest.JAVAJIGI, ofId(1L).getId());
+
+    assertThat(deleteObject.size()).isEqualTo(1);
+
+    List<DeleteHistory> otherDeleteObject = Q2.findDeleteQuestionAndAnswers(
+        UserTest.SANJIGI, ofId(2L).getId());
+
+    assertThat(deleteObject).isNotEqualTo(otherDeleteObject);
+  }
+
+  public static Question ofId(long questionId) {
+    return new Question(questionId, "title1", "contents1").writeBy(
+        UserTest.JAVAJIGI);
+  }
 }
