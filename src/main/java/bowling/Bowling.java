3dfd19525ded2abc8b0b7d838f17f7c18b4c9d5c
SeungJae Lee <lsj8367@naver.com>
2021-08-24 17:21:21 +0900

    feat: 볼링 점수판 그리기 구현

--- src/main/java/bowling/Bowling.java
+++ src/main/java/bowling/Bowling.java
@@ -1,10 +1,106 @@
 package bowling;
 
+import bowling.domain.Board;
+import bowling.domain.Player;
+import bowling.domain.frame.Frame;
+import bowling.domain.frame.LastFrame;
+import bowling.domain.frame.NormalFrame;
+import bowling.domain.state.Spare;
+import bowling.domain.state.State;
+import bowling.domain.state.StateFactory;
 import bowling.view.InputView;
+import bowling.view.ResultView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bowling {
+    private static final int MAX_NORMAL_FRAME = 9;
+    private static final int LAST_FRAME = 10;
+    private static List<Frame> frames = new ArrayList<>();
+    private static List<State> resultList = new ArrayList<>();
 
     public static void main(String[] args) {
-        String name = InputView.inputName();
+        Player player = Player.of(InputView.inputName());
+        Board board = new Board(player.addName());
+        ResultView.currentResult(board);
+
+        normalFrame(board, frames);
+
+        lastFrame(board, resultList);
+    }
+
+    private static void normalFrame(Board board, List<Frame> frames) {
+        int count = 0;
+        int startFrame = 1;
+        while (count < MAX_NORMAL_FRAME) {
+            ResultView.pitchedBall(startFrame);
+            State state = StateFactory.first(InputView.inputPins());
+            board.addFrame(startFrame, state);
+
+            spare(startFrame, board, state);
+
+            frames.add(NormalFrame.of(state));
+
+            ResultView.currentResult(board);
+            startFrame++;
+            count++;
+        }
+    }
+
+    private static void lastFrame(Board board, List<State> resultList) {
+        ResultView.pitchedBall(LAST_FRAME);
+
+        State state = StateFactory.first(InputView.inputPins());
+
+        if(state.isFinished()) {
+            firstStrike(resultList, state);
+        }
+
+        if (!state.isFinished()) {
+            secondSpare(resultList, state);
+        }
+
+        LastFrame lastFrame = LastFrame.of(resultList);
+        board.addLast(LAST_FRAME, lastFrame.printStr());
+        ResultView.currentResult(board);
+    }
+
+    private static void secondSpare(List<State> list, State state) {
+        state = state.nextPitch(InputView.inputPins());
+
+        list.add(state);
+
+        if(state instanceof Spare) {
+            ResultView.pitchedBall(LAST_FRAME);
+            state = StateFactory.last(InputView.inputPins());
+            list.add(state);
+        }
+    }
+
+    private static State firstStrike(List<State> list, State state) {
+        list.add(state);
+        ResultView.pitchedBall(LAST_FRAME);
+        state = StateFactory.first(InputView.inputPins());
+
+        if (!state.isFinished()) {
+            ResultView.pitchedBall(LAST_FRAME);
+            state = state.nextPitch(InputView.inputPins());
+            list.add(state);
+            return state;
+        }
+
+        list.add(state);
+        ResultView.pitchedBall(LAST_FRAME);
+        state = StateFactory.last(InputView.inputPins());
+        list.add(state); //마지막 3번째 결과
+        return state;
+    }
+
+    private static void spare(int startFrame, Board board, State state) {
+        if(!state.isFinished()) {
+            ResultView.pitchedBall(startFrame);
+            state = state.nextPitch(InputView.inputPins());
+            board.addFrame(startFrame, state);
+        }
     }
 }
