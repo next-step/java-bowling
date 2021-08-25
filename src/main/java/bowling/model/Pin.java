e9233727d2ea958bc6cf0d4723fa430da1d4dd72
PIH6585 <pih6585@naver.com>
2021-08-25 09:07:04 +0900

    feat : 핀 추가

---
+++ src/main/java/bowling/model/Pin.java
@@ -0,0 +1,51 @@
+package bowling.model;
+
+import java.util.Objects;
+
+public class Pin {
+
+	public static final String POSITIVE_ERROR_MESSAGE = "핀은 양수만 가능 합니다.";
+	public static final String MAX_OVER_ERROR_MESSAGE = "최대 핀의 개수는 10개 입니다.";
+	public static final int MIN_PIN = 0;
+	public static final int MAX_PIN = 10;
+
+	private final int pin;
+
+	public Pin(int pin) {
+		checkPositive(pin);
+		checkMaxPinOver(pin);
+		this.pin = pin;
+	}
+
+	private void checkMaxPinOver(int pin) {
+		if (pin > MAX_PIN) {
+			throw new IllegalArgumentException(MAX_OVER_ERROR_MESSAGE);
+		}
+	}
+
+	private void checkPositive(int pin) {
+		if (pin < MIN_PIN) {
+			throw new IllegalArgumentException(POSITIVE_ERROR_MESSAGE);
+		}
+	}
+
+	public int getPin() {
+		return pin;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Pin pin1 = (Pin)o;
+		return pin == pin1.pin;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(pin);
+	}
+
+}
---
+++ src/test/java/bowling/model/PinTest.java
@@ -0,0 +1,32 @@
+package bowling.model;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PinTest {
+
+	@Test
+	@DisplayName("핀을 생성하면 핀이 생성된다.")
+	public void createPin() {
+		Pin pin = new Pin(5);
+
+		assertThat(pin).isEqualTo(new Pin(5));
+		assertThat(pin.getPin()).isEqualTo(5);
+	}
+
+	@Test
+	@DisplayName("핀은 음수를 입력하면 예외가 발생된다.")
+	public void checkPositive() {
+		assertThatThrownBy(() -> new Pin(-1))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	@DisplayName("핀은 10을 넘으면 예외가 발생된다.")
+	public void checkMaxPinOver() {
+		assertThatThrownBy(() -> new Pin(11))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+}
