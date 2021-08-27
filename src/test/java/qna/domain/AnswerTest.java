00a4536c52b14cb4c1575035ce18a54815cac8b2
seondongpyo <seondongpyo@gmail.com>
2021-08-28 00:20:12 +0900

    test(Answer): 답변 작성자와 로그인 사용자가 일치하지 않으면 답변을 삭제할 수 없다

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -5,15 +5,24 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import qna.CannotDeleteException;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
 
-    @DisplayName("답변 작성자와 로그인 사용자가 일치하면 답변을 삭제할 수 있다.")
+    @DisplayName("답변 작성자와 로그인 사용자가 일치하면, 답변을 삭제할 수 있다.")
     @Test
     void deletable() throws Exception {
         Answer answer = AnswerTest.A1;
         answer.delete(UserTest.JAVAJIGI);
         assertThat(answer.isDeleted()).isTrue();
+    }
+
+    @DisplayName("답변 작성자와 로그인 사용자가 일치하지 않으면, 답변 삭제 시 CannotDeleteException 예외가 발생한다.")
+    @Test
+    void notDeletable() {
+        assertThatThrownBy(() -> AnswerTest.A2.delete(UserTest.JAVAJIGI))
+            .isInstanceOf(CannotDeleteException.class);
     }
 }
