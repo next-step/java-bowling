dcdbe71de244d11f7f072a44c88dd227f6642b59
pih6585 <pih6585@naver.com>
2021-08-28 17:25:24 +0900

    test : score 계산 일급컬렉션 추가에 따른 테스트

---
+++ src/test/java/bowling/model/ScoreTest.java
@@ -0,0 +1,58 @@
+package bowling.model;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import bowling.model.frame.FinalFrame;
+import bowling.model.frame.Frame;
+import bowling.model.frame.NormalFrame;
+
+public class ScoreTest {
+
+	@Test
+	@DisplayName("프레임의 정보를 입력하면 스코어가 생성된다.")
+	public void createScore() {
+		Score score = Score.createScore(getFrames());
+
+		assertThat(score).isEqualTo(Score.createScore(getFrames()));
+	}
+
+	@Test
+	@DisplayName("프레임의 정보를 입력하면 스코어 점수를 알수 있다.")
+	public void getScore() {
+		Score score = Score.createScore(getFrames());
+
+		assertThat(score.getScore()).containsExactly(9,18,-1);
+	}
+
+	private List<Frame> getFrames() {
+		List<Frame> frames = new ArrayList<>();
+		frames.add(getNormalFrame(1, 7,2));
+		frames.add(getNormalFrame(2, 8,1));
+		frames.add(getFinalFrame(10, 6,4));
+		IntStream.range(0, 2)
+			.forEach(i -> frames.get(i).bringNextFrame(frames.get(i + 1)));
+		return frames;
+	}
+
+	private Frame getNormalFrame(int frameNumber, int firstPin, int secondPin) {
+		NormalFrame normalFrame = new NormalFrame(frameNumber);
+		normalFrame.playGame(firstPin);
+		normalFrame.playGame(secondPin);
+		return normalFrame;
+	}
+
+	private Frame getFinalFrame(int frameNumber, int firstPin, int secondPin) {
+		FinalFrame finalFrame = new FinalFrame(frameNumber);
+		finalFrame.playGame(firstPin);
+		finalFrame.playGame(secondPin);
+		return finalFrame;
+	}
+
+}
