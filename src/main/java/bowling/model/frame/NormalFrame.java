7c51217af0280be9a802977b6206ee62c5de3643
pih6585 <pih6585@naver.com>
2021-08-28 16:01:29 +0900

    feat : 노멀 프레임 스코어 계산 식 및 이전 값 전달 추가

--- src/main/java/bowling/model/frame/NormalFrame.java
+++ src/main/java/bowling/model/frame/NormalFrame.java
@@ -9,6 +9,8 @@ public class NormalFrame extends Frame {
 	private static final String FRAME_RANGE_ERROR_MESSAGE = "일반 프레임은 1~9 까지 입니다.";
 	private static final int MAX_FRAME_NUMBER = 9;
 	private static final int MIN_FRAME_NUMBER = 1;
+	private static final int CUSTOM_SECTION_POINT = -1;
+	private static final int FIRST_INDEX = 0;
 
 	private final NormalPlay normalPlay;
 
@@ -39,4 +41,25 @@ public class NormalFrame extends Frame {
 		return normalPlay.isGameEnd();
 	}
 
+	@Override
+	int getStrikeAndSpareNextScore(boolean strike, boolean spare, int sumScore) {
+		if (spare && playResult.isGameStart()) {
+			return sumScore + playResult.findScore(FIRST_INDEX);
+		}
+		if (strike && playResult.isGameStart()) {
+			return nextFrame.getStrikeAndSpareNextScore(false, true, sumScore + playResult.findTotalScore());
+		}
+		return CUSTOM_SECTION_POINT;
+	}
+
+	@Override
+	public int getGameScore() {
+		if (!isGameEnd()) {
+			return CUSTOM_SECTION_POINT;
+		}
+		if (!(playResult.isStrike() || playResult.isSpare())) {
+			return playResult.findTotalScore();
+		}
+		return nextFrame.getStrikeAndSpareNextScore(playResult.isStrike(), playResult.isSpare(), 10);
+	}
 }
--- src/test/java/bowling/model/frame/NormalFrameTest.java
+++ src/test/java/bowling/model/frame/NormalFrameTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import bowling.model.Pin;
-
 class NormalFrameTest {
 
 	@Test
@@ -34,7 +32,7 @@ class NormalFrameTest {
 		NormalFrame normalFrame = new NormalFrame(1);
 		normalFrame.playGame(5);
 
-		assertThat(normalFrame.playResult.findScore(0)).isEqualTo(new Pin(5));
+		assertThat(normalFrame.playResult.findScore(0)).isEqualTo(5);
 	}
 
 	@Test
@@ -61,4 +59,98 @@ class NormalFrameTest {
 
 		assertThat(normalFrame2.isGameEnd()).isTrue();
 	}
+
+	@Test
+	@DisplayName("노멀 프레임이 스트라이크나 스페어가 아니면 -1을 반환한다.")
+	public void getNotStrikeAndNotSpare() {
+		NormalFrame normalFrame = new NormalFrame(1);
+		int result = normalFrame.getStrikeAndSpareNextScore(false, false, 10);
+
+		assertThat(result).isEqualTo(-1);
+	}
+
+	@Test
+	@DisplayName("노멀 프레임이 스페어이면 이전점수와 해당 프레임의 첫번째 점수를 합산한다.")
+	public void getSpareByNextScore() {
+		NormalFrame normalFrame = new NormalFrame(1);
+		normalFrame.playGame(5);
+		int result = normalFrame.getStrikeAndSpareNextScore(false, true, 10);
+
+		assertThat(result).isEqualTo(15);
+	}
+
+	@Test
+	@DisplayName("노멀 프레임이 스페어이면 아전과 해당 프레임 합산점수 와 다음 프레임의 첫번째 점수를 합산한다.")
+	public void getStrikeByNextScore() {
+		NormalFrame normalFrame2 = new NormalFrame(2);
+		normalFrame2.playGame(6);
+
+		NormalFrame normalFrame = new NormalFrame(1);
+		normalFrame.playGame(5);
+		normalFrame.playGame(5);
+		normalFrame.bringNextFrame(normalFrame2);
+		int result = normalFrame.getStrikeAndSpareNextScore(true, false, 10);
+
+		assertThat(result).isEqualTo(26);
+	}
+
+	@Test
+	@DisplayName("노멀프레임 점수중 스트라이크 스페어가 아니면 해당 점수만 계산한다.")
+	public void getGameScoreNotStrikeAndSpare() {
+		NormalFrame normalFrame = new NormalFrame(1);
+		normalFrame.playGame(5);
+		normalFrame.playGame(4);
+
+		assertThat(normalFrame.getGameScore()).isEqualTo(9);
+	}
+
+	@Test
+	@DisplayName("노멀레임 점수중 스페어이면 다음 프레임 1번공의 점수와 합산을 계산한다.")
+	public void getGameScoreBySpare() {
+		NormalFrame normalFrame2 = new NormalFrame(2);
+		normalFrame2.playGame(6);
+
+		NormalFrame normalFrame = new NormalFrame(1);
+		normalFrame.playGame(4);
+		normalFrame.playGame(6);
+		normalFrame.bringNextFrame(normalFrame2);
+
+		assertThat(normalFrame.getGameScore()).isEqualTo(16);
+	}
+
+	@Test
+	@DisplayName("노멀레임 점수중 스트라이크이면 다다음 프레임 1번공의 점수와 합산을 계산한다.")
+	public void getGameScoreByStrike() {
+		NormalFrame normalFrame3 = new NormalFrame(3);
+		normalFrame3.playGame(9);
+
+		NormalFrame normalFrame2 = new NormalFrame(2);
+		normalFrame2.playGame(6);
+		normalFrame2.playGame(4);
+		normalFrame2.bringNextFrame(normalFrame3);
+
+		NormalFrame normalFrame = new NormalFrame(1);
+		normalFrame.playGame(10);
+		normalFrame.bringNextFrame(normalFrame2);
+
+		assertThat(normalFrame.getGameScore()).isEqualTo(29);
+	}
+
+	@Test
+	@DisplayName("노멀레임 점수중 더블이면 다다음 프레임 1번공의 점수와 합산을 계산한다.")
+	public void getGameScoreByDouble() {
+		NormalFrame normalFrame3 = new NormalFrame(3);
+		normalFrame3.playGame(9);
+
+		NormalFrame normalFrame2 = new NormalFrame(2);
+		normalFrame2.playGame(10);
+		normalFrame2.bringNextFrame(normalFrame3);
+
+		NormalFrame normalFrame = new NormalFrame(1);
+		normalFrame.playGame(10);
+		normalFrame.bringNextFrame(normalFrame2);
+
+		assertThat(normalFrame.getGameScore()).isEqualTo(29);
+	}
+
 }
