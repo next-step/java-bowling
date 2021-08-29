5d6c76854edf5df5aeaa5aa4c35a4f08e23aa0d2
shpark <bingbingpa@gmail.com>
2021-08-29 19:40:34 +0900

    feat: 점수 표시 view

--- src/main/java/bowling/view/ResultView.java
+++ src/main/java/bowling/view/ResultView.java
@@ -3,11 +3,14 @@ package bowling.view;
 import bowling.domain.frame.Frame;
 import bowling.domain.frame.Frames;
 import bowling.domain.player.Player;
+import bowling.exception.CannotCalculateException;
 
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class ResultView {
+
+    private static int totalScore = 0;
 
     private ResultView() {
     }
@@ -15,6 +18,7 @@ public class ResultView {
     public static void printBoard(Player player, Frames frames) {
         printBoardHead();
         printBoardFrame(player, frames);
+        printBoardScore(frames);
     }
 
     private static void printBoardHead() {
@@ -30,8 +34,31 @@ public class ResultView {
 
         frameList.forEach(f -> System.out.printf("   " + String.format("%-3s", f.getState()) + String.format("%3s", "|")));
 
-        IntStream.range(frameList.size(), 10)
+        emptyBoard(frameList.size());
+    }
+
+    private static void printBoardScore(Frames frames) {
+        List<Frame> frameList = frames.getFrames();
+        totalScore = 0;
+        System.out.printf("|      |");
+
+        frameList.forEach(f -> System.out.printf("   " + String.format("%-3s", getScore(f)) + String.format("%3s", "|")));
+
+        emptyBoard(frameList.size());
+    }
+
+    private static void emptyBoard(int start) {
+        IntStream.range(start, 10)
                 .forEach(i -> System.out.printf("        |"));
         System.out.println();
+    }
+
+    private static String getScore(Frame frame) {
+        try {
+            totalScore += frame.getScore().getScore();
+            return String.valueOf(totalScore);
+        } catch (CannotCalculateException e) {
+            return "";
+        }
     }
 }
