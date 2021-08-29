872fc30a4cee34e136af2f764495995e0861024d
kdh85 <modudamba@naver.com>
2021-08-29 23:55:09 +0900

    feat : step2 볼링게임 다음 공굴림 상태 객체 생성 두번째 공을 굴려서 스패어, 미스의 상태를 반환하는 객체 생성.

---
+++ src/main/java/bowling/state/NextPitch.java
@@ -0,0 +1,28 @@
+package bowling.state;
+
+import bowling.pin.Pin;
+
+public class NextPitch extends Running {
+
+  private final Pin firstPin;
+
+  public NextPitch(final Pin firstPin) {
+    this.firstPin = firstPin;
+  }
+
+  @Override
+  public State nextPitch(final int fallenPin) {
+    Pin currentPin = Pin.from(fallenPin);
+
+    if (currentPin.isSpare(firstPin)) {
+      return new Spare(firstPin, currentPin);
+    }
+
+    return new Miss(firstPin, currentPin);
+  }
+
+  @Override
+  public String score() {
+    return "";
+  }
+}
