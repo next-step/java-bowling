947da40252f4b06bd322ca13957322cc2b8c8e6c
kdh85 <modudamba@naver.com>
2021-08-29 23:55:34 +0900

    test : step2 볼링게임 다음 공굴림 상태 객체 테스트코드 생성 두번째 공을 굴려서 스패어, 미스의 상태를 반환하는 객체 생성 테스트코드 작성.

---
+++ src/test/java/bowling/state/NextPitchTest.java
@@ -0,0 +1,28 @@
+package bowling.state;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import bowling.pin.PinTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class NextPitchTest {
+
+  @DisplayName("처음공과 두번째공을 굴려서 스페어가 반환되는지 확인한다.")
+  @ParameterizedTest
+  @CsvSource(value = {"1,9,/","2,8,/","3,7,/","4,6,/","5,5,/","6,4,/","7,3,/","8,2,/","9,1,/"})
+  void firstBallIsSpare(int first, int second, String spareMark) {
+    NextPitch firstBall = new NextPitch(PinTest.from(first));
+
+    assertThat(firstBall.nextPitch(second).score()).contains(first+"|"+spareMark);
+  }
+
+  @DisplayName("처음공과 두번째공을 굴려서 미스가 반환되는지 확인한다.")
+  @ParameterizedTest
+  @CsvSource(value = {"1,8","2,7","3,6","4,5","5,4","6,3","7,2","8,1"})
+  void firstBallIsMiss(int first, int second) {
+    NextPitch firstBall = new NextPitch(PinTest.from(first));
+    assertThat(firstBall.nextPitch(second).score()).contains(first+"|"+second);
+  }
+}
