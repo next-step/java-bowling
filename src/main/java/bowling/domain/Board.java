f1d69ad53f969bc99c6a9ca87792b9b56429a3cc
lsj8367 <lsj8367@naver.com>
2021-08-29 12:21:13 +0900

    refactor: 출력 클래스 리팩토링

--- src/main/java/bowling/domain/Board.java
+++ src/main/java/bowling/domain/Board.java
@@ -4,9 +4,15 @@ import bowling.domain.state.State;
 import java.util.List;
 
 public class Board {
-    private static final int MAX_FRAME = 10;
+    private static final String STRIKE = "X";
+    private static final String SPARE = "/";
+    private static final String GUTTER = "-";
     private static final String SPACE = " ";
-    private static final String INIT_FRAME = "      |";
+    private static final String BOUNDARY_LINE = "|";
+    private static final String INIT_FRAME = "      "+ BOUNDARY_LINE;
+
+    private static final int MAX_FRAME = 10;
+    private static final int MIN_PIN = 0;
 
     private final List<String> boards;
 
@@ -22,7 +28,11 @@ public class Board {
     }
 
     public void addFrame(int frameNum, State state) {
-        boards.set(frameNum, SPACE + state.display());
+        boards.set(frameNum, spareOrMiss(state));
+    }
+
+    public void addStrike(int frameNum) {
+        boards.set(frameNum, isStrike());
     }
 
     public void addLast(int frameNum, String state) {
@@ -33,5 +43,37 @@ public class Board {
         return boards;
     }
 
+    private String isStrike() {
+        return "   " + STRIKE + "  " + BOUNDARY_LINE;
+    }
+
+    public String spareOrMiss(State state) {
+        String pins = gutterCheck(state.firstPins());
+
+        if ((state.firstPins() + state.secondPins()) == 10) {
+            return "  " + pins + BOUNDARY_LINE + SPARE + SPACE + BOUNDARY_LINE;
+        }
+        return "  " + pins + BOUNDARY_LINE + gutterCheck(state.secondPins()) + SPACE + BOUNDARY_LINE;
+    }
+
+    public String lastStrike() {
+        return STRIKE + BOUNDARY_LINE;
+    }
+
+    public String lastSpareOrMiss(State state) {
+        String pins = gutterCheck(state.firstPins());
+
+        if ((state.firstPins() + state.secondPins()) == 10) {
+            return pins + BOUNDARY_LINE + SPARE + BOUNDARY_LINE;
+        }
+        return SPACE + pins + BOUNDARY_LINE + gutterCheck(state.secondPins()) + SPACE + BOUNDARY_LINE;
+    }
+
+    private String gutterCheck(int falledPins) {
+        if (falledPins == MIN_PIN) {
+            return GUTTER;
+        }
+        return String.valueOf(falledPins);
+    }
 
 }
