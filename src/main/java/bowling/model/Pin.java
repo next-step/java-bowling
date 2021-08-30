eb6352d009da0b5845a79ac1f4984fcfc7d5fea1
pih6585 <pih6585@naver.com>
2021-08-30 11:16:44 +0900

    refactor :  pin에서 max값 계산 추가

--- src/main/java/bowling/model/Pin.java
+++ src/main/java/bowling/model/Pin.java
@@ -33,6 +33,15 @@ public class Pin {
 		return pin;
 	}
 
+	public boolean isMaxPin() {
+		return pin == MAX_PIN;
+	}
+
+	public boolean isMaxPin(Pin secondPin) {
+		checkMaxPinOver((this.pin + secondPin.getPin()));
+		return (this.pin + secondPin.getPin()) == MAX_PIN;
+	}
+
 	@Override
 	public boolean equals(Object o) {
 		if (this == o)
--- src/test/java/bowling/model/PinTest.java
+++ src/test/java/bowling/model/PinTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class PinTest {
 
@@ -31,5 +33,23 @@ class PinTest {
 	public void checkMaxPinOver() {
 		assertThatThrownBy(() -> new Pin(11))
 			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@ParameterizedTest
+	@DisplayName("핀의 수가 10개면 true 이다")
+	@CsvSource(value = {"10,true", "9,false"})
+	public void isMaxPin(int firstPin, boolean maxPin) {
+		Pin pin = new Pin(firstPin);
+
+		assertThat(pin.isMaxPin()).isEqualTo(maxPin);
+	}
+
+	@ParameterizedTest
+	@DisplayName("핀의 수와 입력받은 수의 합이 10개면 true 이다")
+	@CsvSource(value = {"2,8,true", "4,6,true", "2,7,false", "3,6,false"})
+	public void isMaxPinBySecondPin(int firstPin, int secondPin, boolean maxPin) {
+		Pin pin = new Pin(firstPin);
+
+		assertThat(pin.isMaxPin(new Pin(secondPin))).isEqualTo(maxPin);
 	}
 }
