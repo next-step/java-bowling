38330871b406e336e33f85fc413faaf74cdf21cd
pih6585 <pih6585@naver.com>
2021-08-31 23:48:04 +0900

    refactor : int 변수를 Pin 으로 수정

--- src/test/java/bowling/model/StateGeneratorTest.java
+++ src/test/java/bowling/model/StateGeneratorTest.java
@@ -12,9 +12,9 @@ class StateGeneratorTest {
 	@DisplayName("점수 하나만 있을시 스트라이크, 커터, 기타 점수를 환산한다.")
 	public void getOneScore() {
 		assertAll(
-			() -> assertThat(ScoreGenerator.scoreGenerator(10)).isEqualTo("X"),
-			() -> assertThat(ScoreGenerator.scoreGenerator(0)).isEqualTo("-"),
-			() -> assertThat(ScoreGenerator.scoreGenerator(5)).isEqualTo("5")
+			() -> assertThat(ScoreGenerator.scoreGenerator(new Pin(10))).isEqualTo("X"),
+			() -> assertThat(ScoreGenerator.scoreGenerator(new Pin(0))).isEqualTo("-"),
+			() -> assertThat(ScoreGenerator.scoreGenerator(new Pin(5))).isEqualTo("5")
 		);
 	}
 
@@ -22,10 +22,10 @@ class StateGeneratorTest {
 	@DisplayName("점수가 2개 있을시 스트라이크, 커터, 기타 점수를 환산한다.")
 	public void getTwoScore() {
 		assertAll(
-			() -> assertThat(ScoreGenerator.scoreGenerator(10, 0)).isEqualTo("X|-"),
-			() -> assertThat(ScoreGenerator.scoreGenerator(4, 6)).isEqualTo("4|/"),
-			() -> assertThat(ScoreGenerator.scoreGenerator(3, 4)).isEqualTo("3|4"),
-			() -> assertThat(ScoreGenerator.scoreGenerator(3, 0)).isEqualTo("3|-")
+			() -> assertThat(ScoreGenerator.scoreGenerator(new Pin(10), new Pin(0))).isEqualTo("X|-"),
+			() -> assertThat(ScoreGenerator.scoreGenerator(new Pin(4), new Pin(6))).isEqualTo("4|/"),
+			() -> assertThat(ScoreGenerator.scoreGenerator(new Pin(3), new Pin(4))).isEqualTo("3|4"),
+			() -> assertThat(ScoreGenerator.scoreGenerator(new Pin(3), new Pin(0))).isEqualTo("3|-")
 		);
 	}
 
@@ -33,15 +33,15 @@ class StateGeneratorTest {
 	@DisplayName("점수가 3개 있을시 스트라이크, 커터, 기타 점수를 환산한다.")
 	public void getThreeScore() {
 		assertAll(
-			() -> assertThat(ScoreGenerator.scoreGenerator(10, 10, 10)).isEqualTo("X|X|X"),
-			() -> assertThat(ScoreGenerator.scoreGenerator(10, 10, 4)).isEqualTo("X|X|4"),
-			() -> assertThat(ScoreGenerator.scoreGenerator(10, 10, 0)).isEqualTo("X|X|-"),
-			() -> assertThat(ScoreGenerator.scoreGenerator(10, 0, 10)).isEqualTo("X|-|X"),
-			() -> assertThat(ScoreGenerator.scoreGenerator(10, 4, 6)).isEqualTo("X|4|/"),
-			() -> assertThat(ScoreGenerator.scoreGenerator(10, 4, 5)).isEqualTo("X|4|5"),
-			() -> assertThat(ScoreGenerator.scoreGenerator(4, 6, 10)).isEqualTo("4|/|X"),
-			() -> assertThat(ScoreGenerator.scoreGenerator(4, 6, 7)).isEqualTo("4|/|7"),
-			() -> assertThat(ScoreGenerator.scoreGenerator(4, 6, 0)).isEqualTo("4|/|-")
+			() -> assertThat(ScoreGenerator.scoreGenerator(new Pin(10), new Pin(10), new Pin(10))).isEqualTo("X|X|X"),
+			() -> assertThat(ScoreGenerator.scoreGenerator(new Pin(10), new Pin(10), new Pin(4))).isEqualTo("X|X|4"),
+			() -> assertThat(ScoreGenerator.scoreGenerator(new Pin(10), new Pin(10), new Pin(0))).isEqualTo("X|X|-"),
+			() -> assertThat(ScoreGenerator.scoreGenerator(new Pin(10), new Pin(0), new Pin(10))).isEqualTo("X|-|X"),
+			() -> assertThat(ScoreGenerator.scoreGenerator(new Pin(10), new Pin(4), new Pin(6))).isEqualTo("X|4|/"),
+			() -> assertThat(ScoreGenerator.scoreGenerator(new Pin(10), new Pin(4), new Pin(5))).isEqualTo("X|4|5"),
+			() -> assertThat(ScoreGenerator.scoreGenerator(new Pin(4), new Pin(6), new Pin(10))).isEqualTo("4|/|X"),
+			() -> assertThat(ScoreGenerator.scoreGenerator(new Pin(4), new Pin(6), new Pin(7))).isEqualTo("4|/|7"),
+			() -> assertThat(ScoreGenerator.scoreGenerator(new Pin(4), new Pin(6), new Pin(0))).isEqualTo("4|/|-")
 		);
 	}
 
