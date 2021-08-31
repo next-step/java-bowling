41be2a151d0dd253ceceeceb2a403da9983619dd
pih6585 <pih6585@naver.com>
2021-08-31 22:06:38 +0900

    refactor : 다음 프레임 계산 method 수정에 따른 수정

--- src/main/java/bowling/model/PlayResult.java
+++ src/main/java/bowling/model/PlayResult.java
@@ -13,6 +13,7 @@ public class PlayResult {
 	private static final int FIRST_INDEX_SIZE = 1;
 	private static final int SECOND_INDEX_SIZE = 2;
 	private static final int BONUS_INDEX_SIZE = 3;
+	private static final int MAX_PIN = 10;
 
 	private final List<Pin> gameResult;
 
@@ -22,20 +23,6 @@ public class PlayResult {
 
 	public int findScore(int index) {
 		return gameResult.get(index).getPin();
-	}
-
-	public boolean isMaxPin() {
-		return gameResult.get(FIRST_INDEX).isMaxPin();
-	}
-
-	public boolean isMaxPin(Pin secondPin) {
-		return gameResult.get(FIRST_INDEX).add(secondPin).isMaxPin();
-	}
-
-	public int findTotalScore() {
-		return gameResult.stream()
-			.mapToInt(Pin::getPin)
-			.sum();
 	}
 
 	public String getGameResult() {
@@ -56,8 +43,39 @@ public class PlayResult {
 		return gameResult.size() > FIRST_INDEX;
 	}
 
-	public boolean isSecondPlay() {
-		return gameResult.size() >= SECOND_INDEX_SIZE;
+	public int countGame() {
+		return gameResult.size();
+	}
+
+	public int calculateFrame(PlayResult beforeResult) {
+		if (beforeResult.isSpare() && isGameStart()) {
+			return MAX_PIN + findScore(FIRST_INDEX);
+		}
+		if (beforeResult.isStrike() && countGame() > 1) {
+			return MAX_PIN + (findScore(FIRST_INDEX) + findScore(SECOND_INDEX));
+		}
+		return -1;
+	}
+
+	public int calculateDouble(PlayResult beforeResult) {
+		if (isGameStart()) {
+			return (MAX_PIN + beforeResult.getTotalScore()) + findScore(FIRST_INDEX);
+		}
+		return -1;
+	}
+
+	public int getTotalScore() {
+		return gameResult.stream()
+			.mapToInt(Pin::getPin)
+			.sum();
+	}
+
+	public boolean isMaxPin() {
+		return gameResult.get(FIRST_INDEX).isMaxPin();
+	}
+
+	public boolean isMaxPin(Pin secondPin) {
+		return gameResult.get(FIRST_INDEX).add(secondPin).isMaxPin();
 	}
 
 	public boolean isStrike() {
@@ -72,16 +90,6 @@ public class PlayResult {
 			return false;
 		}
 		return isMaxPin(gameResult.get(SECOND_INDEX));
-	}
-
-	public int findScoreNextStep() {
-		if (isStrike()) {
-			return 2;
-		}
-		if (isSpare()) {
-			return 1;
-		}
-		return -1;
 	}
 
 	@Override
--- src/test/java/bowling/model/PlayResultTest.java
+++ src/test/java/bowling/model/PlayResultTest.java
@@ -36,7 +36,7 @@ class PlayResultTest {
 	public void findGameTotalScore() {
 		PlayResult playResult = new PlayResult(getGameResult());
 
-		assertThat(playResult.findTotalScore()).isEqualTo(8);
+		assertThat(playResult.getTotalScore()).isEqualTo(8);
 	}
 
 	@Test
@@ -67,28 +67,6 @@ class PlayResultTest {
 	}
 
 	@Test
-	@DisplayName("게임의 결과수가 2개미만이면 2번째 플레이는 false 이다.")
-	public void isSecondPlayByOneSize() {
-		List<Pin> pins = new ArrayList<>();
-		pins.add(new Pin(5));
-		PlayResult playResult = new PlayResult(pins);
-
-		assertThat(playResult.isSecondPlay()).isFalse();
-	}
-
-	@Test
-	@DisplayName("게임의 결과수가 2개이상이면 2번째 플레이는 true 이다.")
-	public void isSecondPlay() {
-		List<Pin> pins2 = new ArrayList<>();
-		pins2.add(new Pin(4));
-		pins2.add(new Pin(6));
-
-		PlayResult playResult2 = new PlayResult(pins2);
-
-		assertThat(playResult2.isSecondPlay()).isTrue();
-	}
-
-	@Test
 	@DisplayName("첫번째 볼이 10점이면 스트라이크 이다.")
 	public void isStrike() {
 		List<Pin> pins = new ArrayList<>();
@@ -109,29 +87,6 @@ class PlayResultTest {
 		PlayResult playResult = new PlayResult(pins);
 
 		assertThat(playResult.isSpare()).isTrue();
-	}
-
-	@Test
-	@DisplayName("첫번째 볼이 스트라이크이면 다음 프레임은 2단계이다.")
-	public void findScoreNextStepByStrike() {
-		List<Pin> pins = new ArrayList<>();
-		pins.add(new Pin(10));
-
-		PlayResult playResult = new PlayResult(pins);
-
-		assertThat(playResult.findScoreNextStep()).isEqualTo(2);
-	}
-
-	@Test
-	@DisplayName("결과가  스페어 이면 다음 프레임은 1단계이다.")
-	public void findScoreNextStepBySpare() {
-		List<Pin> pins = new ArrayList<>();
-		pins.add(new Pin(4));
-		pins.add(new Pin(6));
-
-		PlayResult playResult = new PlayResult(pins);
-
-		assertThat(playResult.findScoreNextStep()).isEqualTo(1);
 	}
 
 	private List<Pin> getGameResult() {
