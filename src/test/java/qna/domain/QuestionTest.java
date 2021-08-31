06bd335416b14a2bcfccd0f5dc2d679c45107edf
Dongle <dongle@r2v.io>
2021-08-31 20:55:16 +0900

    test: Question answers delete 테스트 코드 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,27 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class QuestionTest {
+
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    void deleteAnswer() {
+        // Given
+        Answer givenA1 = AnswerTest.A1;
+        Q1.addAnswer(givenA1);
+
+        // When
+        Q1.deleteAnswer(new ArrayList<>());
+
+        // Then
+        assertThat(givenA1.isDeleted()).isTrue();
+    }
+
+
 }
