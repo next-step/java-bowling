41a986dd0d1ce3dc1466b5bfb1627c9dbd0c6f56
kdh85 <modudamba@naver.com>
2021-08-31 10:56:07 +0900

    style : step2 볼링게임 FirstPitch 코드 정리 개행 수정.

--- src/main/java/bowling/state/FirstPitch.java
+++ src/main/java/bowling/state/FirstPitch.java
@@ -13,13 +13,12 @@ public class FirstPitch extends Running{
   public State nextPitch(final int fallenPin) {
     Pin currentPin = Pin.from(fallenPin);
 
-
     if (currentPin.isStrike()) {
       totalPin = PIN_MAX;
       return new Strike(currentPin);
     }
+    
     totalPin = Pin.from(PIN_MIN).totalDownPin(currentPin);
-
     return new NextPitch(currentPin);
   }
 
