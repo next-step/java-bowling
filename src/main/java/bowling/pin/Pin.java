207ff668f1e6946836fa0658139c849868d1f0c6
kdh85 <modudamba@naver.com>
2021-08-31 10:47:48 +0900

    refactor : step2 볼링게임 핀객체 상수 리팩토링 사용중인 메세지들을 상수 처리함.

--- src/main/java/bowling/pin/Pin.java
+++ src/main/java/bowling/pin/Pin.java
@@ -9,6 +9,9 @@ public class Pin {
   private static final String STRIKE = "X";
   private static final String SEPARATOR = "|";
   private static final String SPARE = "/";
+  private static final String MSG_ERROR_PIN_MAX = "입력받은 핀의 수가 %d보다 클 수 없습니다.";
+  private static final String MSG_ERROR_PIN_MIN = "입력받은 핀의 수가 %d보다 작을 수 없습니다.";
+  private static final String MSG_ERROR_DOWN_PIN_MAX = "쓰러트린 핀의 총합이 %d보다 클 수 없습니다.";
 
   private final int fallenPin;
 
@@ -23,17 +26,17 @@ public class Pin {
 
   private static void validationPins(final int fallenPin) {
     if (fallenPin > MAX_PINS) {
-      throw new IllegalArgumentException(String.format("입력받은 핀의 수가 %d보다 클 수 없습니다.", MAX_PINS));
+      throw new IllegalArgumentException(String.format(MSG_ERROR_PIN_MAX, MAX_PINS));
     }
 
     if (fallenPin < MIN_PINS) {
-      throw new IllegalArgumentException(String.format("입력받은 핀의 수가 %d보다 작을 수 없습니다.", MIN_PINS));
+      throw new IllegalArgumentException(String.format(MSG_ERROR_PIN_MIN, MIN_PINS));
     }
   }
 
   public int totalDownPin(final Pin firstPin) {
     if (this.fallenPin + firstPin.fallenPin > MAX_PINS) {
-      throw new IllegalArgumentException(String.format("쓰러트린 핀의 총합이 %d보다 클 수 없습니다.", MAX_PINS));
+      throw new IllegalArgumentException(String.format(MSG_ERROR_DOWN_PIN_MAX, MAX_PINS));
     }
     return this.fallenPin + firstPin.fallenPin;
   }
