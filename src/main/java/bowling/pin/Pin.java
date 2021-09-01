8dc4a44e5d032db43c94432cd6d92ae5c8194c0e
kdh85 <modudamba@naver.com>
2021-09-01 13:11:16 +0900

    refactor : step2 볼링게임 핀객체 리팩토링 순수하게 핀에 대한 수만 관리하는 책임을 주기 위해, 스트라이크, 스페어, 미스 의 상태 판정을 피치클래스로쪽으로 이관함.

--- src/main/java/bowling/pin/Pin.java
+++ src/main/java/bowling/pin/Pin.java
@@ -6,9 +6,7 @@ public class Pin {
 
   private static final int MAX_PINS = 10;
   private static final int MIN_PINS = 0;
-  private static final String STRIKE = "X";
-  private static final String SEPARATOR = "|";
-  private static final String SPARE = "/";
+
   private static final String MSG_ERROR_PIN_MAX = "입력받은 핀의 수가 %d보다 클 수 없습니다.";
   private static final String MSG_ERROR_PIN_MIN = "입력받은 핀의 수가 %d보다 작을 수 없습니다.";
   private static final String MSG_ERROR_DOWN_PIN_MAX = "쓰러트린 핀의 총합이 %d보다 클 수 없습니다.";
@@ -47,33 +45,6 @@ public class Pin {
     }
     return this.fallenPin;
   }
-
-  public boolean isStrike() {
-    return this.fallenPin == MAX_PINS;
-  }
-
-  public boolean isSpare(final Pin firstPin) {
-    return totalDownPin(firstPin) == MAX_PINS;
-  }
-
-  public boolean isMiss(final Pin firstPin) {
-    return totalDownPin(firstPin) < MAX_PINS;
-  }
-
-  public String score() {
-    if (isStrike()) {
-      return STRIKE;
-    }
-    return String.valueOf(this.fallenPin);
-  }
-
-  public String score(Pin first) {
-    if (isSpare(first)) {
-      return first.fallenPin + SEPARATOR + SPARE;
-    }
-    return first.fallenPin + SEPARATOR + totalDownPin(first);
-  }
-
 
   @Override
   public boolean equals(final Object o) {
