79e633cf05a28a397ff53d370b9888253143d2ad
Oh Sang-Woo <sangw0804@likelion.org>
2021-09-01 20:53:38 +0900

    refactor: service 에 있던 로직들을 domain 모델로 이동 (#599)

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,32 @@
 package qna.domain;
 
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("정상 삭제")
+    public void delete() {
+        assertThat(A1.isDeleted()).isFalse();
+
+        A1.delete(UserTest.JAVAJIGI);
+
+        assertThat(A1.isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("질문 작성자가 아니면 Exception")
+    public void deleteByNoWriter() {
+        assertThatThrownBy(() -> {
+            A1.delete(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,31 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    @DisplayName("정상 삭제")
+    public void delete() {
+        assertThat(Q1.isDeleted()).isFalse();
+
+        Q1.delete(UserTest.JAVAJIGI);
+
+        assertThat(Q1.isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("질문 작성자가 아니면 Exception")
+    public void deleteByNoWriter() {
+        assertThatThrownBy(() -> {
+            Q1.delete(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
 }
