f59a130b1f51c4cc324773ee31107697d80e8247
ckdgus08 <ckdgus0808@naver.com>
2021-09-02 01:13:37 +0900

    refactor: Answers 맴버 final 선언

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class Answers {
-    private List<Answer> answers;
+    private final List<Answer> answers;
 
     private Answers(List<Answer> answers) {
         this.answers = answers;
