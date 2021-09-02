962fb92f6a91f9419c5669030b1bf3a312aa5b6c
jaeyoungjang2 <lion85488@gmail.com>
2021-09-02 10:38:13 +0900

    feat: 사용자로 부터 입력 받는 부분 구현

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -1,2 +1,14 @@
-package bowling.view;public class InputView {
+package bowling.view;
+
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final String MESSAGE_PLAYER_NAME = "플레이어 이름은(3 english letters)?: ";
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static String requirePlayerName() {
+        System.out.println(MESSAGE_PLAYER_NAME);
+        return sc.nextLine();
+    }
 }
