1c55f5f65d0278f0d74187deb610636d2e277891
jaeyoungjang2 <lion85488@gmail.com>
2021-09-02 10:39:07 +0900

    feat: 메인 클래스 구현

--- src/main/java/bowling/BowlingApp.java
+++ src/main/java/bowling/BowlingApp.java
@@ -1,2 +1,15 @@
-package bowling;public class BowlingApp {
+package bowling;
+
+import bowling.domain.BowlingGame;
+import bowling.domain.Player;
+import bowling.view.InputView;
+import bowling.view.ResultView;
+
+public class BowlingApp {
+    public static void main(String[] args) {
+        Player player = new Player(InputView.requirePlayerName());
+        ResultView.printResultColumn();
+
+        BowlingGame bowlingGame = new BowlingGame(player.getName());
+    }
 }
