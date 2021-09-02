167550f5ed76ae4a233d2226805242fd75b30c72
kdh85 <modudamba@naver.com>
2021-09-02 14:34:15 +0900

    refactor : step2 볼링게임 프레임 콜랙션 리팩토링 객체를 생성하면서 그자체를 dto로 만들었는데, dto에서 결과값만 넣어서 콜랙션으로 반환하도록 리팩토링함.

--- src/main/java/bowling/frame/Frames.java
+++ src/main/java/bowling/frame/Frames.java
@@ -3,33 +3,34 @@ package bowling.frame;
 import bowling.dto.FrameDto;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Frames {
 
   private static final int FIRST_FRAME = 1;
 
-  private final LinkedList<FrameDto> frames;
+  private final LinkedList<Frame> frames;
 
   public Frames() {
     this.frames = new LinkedList<>();
-    frames.add(FrameDto.from(new NormalFrame(FIRST_FRAME)));
+    frames.add(new NormalFrame(FIRST_FRAME));
   }
 
   public void pitch(final int pin) {
 
-    Frame currentFrame = frames.getLast().getFrame().play(pin);
+    Frame currentFrame = frames.getLast().play(pin);
 
     addNewFrame(currentFrame);
   }
 
   private void addNewFrame(final Frame currentFrame) {
     if (isNotSameFrame(currentFrame)) {
-      frames.add(FrameDto.from(currentFrame));
+      frames.add(currentFrame);
     }
   }
 
   private boolean isNotSameFrame(final Frame currentFrame) {
-    return !currentFrame.equals(frames.getLast().getFrame());
+    return !currentFrame.equals(frames.getLast());
   }
 
   public int size() {
@@ -37,10 +38,12 @@ public class Frames {
   }
 
   public List<FrameDto> resultList() {
-    return frames;
+    return frames.stream()
+        .map(frame -> FrameDto.from(frame.getScore()))
+        .collect(Collectors.toList());
   }
 
   public boolean isEnd() {
-    return frames.getLast().getFrame().isGameEnd();
+    return frames.getLast().isGameEnd();
   }
 }
