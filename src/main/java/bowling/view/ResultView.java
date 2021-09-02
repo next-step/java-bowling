34c34832bf78273ea0d52773062873b48fab5ffc
jaeyoungjang2 <lion85488@gmail.com>
2021-09-02 10:38:41 +0900

    feat: Frame Column 출력 부분 구현

--- src/main/java/bowling/view/ResultView.java
+++ src/main/java/bowling/view/ResultView.java
@@ -1,2 +1,9 @@
-package bowling.view;public class ResultView {
+package bowling.view;
+
+public class ResultView {
+    private static final String RESULT_COLUMN = "| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |";
+
+    public static void printResultColumn() {
+        System.out.println(RESULT_COLUMN);
+    }
 }
