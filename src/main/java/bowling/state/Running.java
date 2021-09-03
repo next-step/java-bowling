81f3b592c86e623a2fb94214b7ff9928b7ede83d
kdh85 <modudamba@naver.com>
2021-09-03 15:19:16 +0900

    feat : step3 볼링게임 인터페이스 메소드 추가에 따른 추상클래스 변경 볼을 치는 중인 상태일 경우는 스코어가 호출되는 경우가 발생하면 안되기 때문에 에러를 리턴하도록 추가함.

--- src/main/java/bowling/state/Running.java
+++ src/main/java/bowling/state/Running.java
@@ -1,9 +1,16 @@
 package bowling.state;
 
+import bowling.score.Score;
+
 public abstract class Running implements State {
 
   @Override
   public boolean isFinish() {
     return false;
+  }
+
+  @Override
+  public Score score() {
+    throw new RuntimeException();
   }
 }
