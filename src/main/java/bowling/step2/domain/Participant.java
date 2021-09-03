64331e01ca03623c104928702a36ce75acd9383b
kang <rkdals213@naver.com>
2021-09-04 00:27:15 +0900

    feat: 이름 validation 추가

--- src/main/java/bowling/step2/domain/Participant.java
+++ src/main/java/bowling/step2/domain/Participant.java
@@ -3,8 +3,22 @@ package bowling.step2.domain;
 public class Participant {
     private final String name;
 
+    private final int MIN_NAME_LENGTH = 1;
+    private final int MAX_NAME_LENGTH = 3;
+
     private Participant(String name) {
+        validateName(name);
         this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (nameLengthNotInRange(name)) {
+            throw new RuntimeException("이름은 1에서 3자리까지 가능합니다.");
+        }
+    }
+
+    private boolean nameLengthNotInRange(String name) {
+        return name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH;
     }
 
     public static Participant of(String name) {
