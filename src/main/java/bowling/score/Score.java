8fad5c1d0cf3a967817bd8866ba503f843cf289f
kdh85 <modudamba@naver.com>
2021-09-03 13:56:24 +0900

    feat : step3 볼링게임 스코어 객체 생성 점수와 보너스 적용 볼카운트를 가진 스코어 객체를 생성함. 볼카운트 적용에 대한 검증을 추가.

--- src/main/java/bowling/score/Score.java
+++ src/main/java/bowling/score/Score.java
@@ -1,5 +1,73 @@
 package bowling.score;
 
+import java.util.Objects;
+
 public class Score {
 
+  private static final int MAX_SCORE = 10;
+  private static final int DEFAULT_BALL_COUNT = 0;
+  private static final int SPARE_BALL_COUNT = 1;
+  private static final int STRIKE_BALL_COUNT = 2;
+  private static final String MSG_ERROR_LIMIT_BALL_COUNT = "계산할 수 없습니다.";
+
+  private final int score;
+
+  private final int ballCount;
+
+  public Score(final int score) {
+    this(score, DEFAULT_BALL_COUNT);
+  }
+
+  private Score(final int score, final int ballCount) {
+    this.score = score;
+    this.ballCount = ballCount;
+  }
+
+  public static Score strike() {
+    return new Score(MAX_SCORE, STRIKE_BALL_COUNT);
+  }
+
+  public static Score spare() {
+    return new Score(MAX_SCORE, SPARE_BALL_COUNT);
+  }
+
+  public static Score from(final int score){
+    return new Score(score, DEFAULT_BALL_COUNT);
+  }
+
+  public Score sum(final int pinCount) {
+    validationBallCount();
+    return new Score(this.score + pinCount, ballCount - SPARE_BALL_COUNT);
+  }
+
+  private void validationBallCount() {
+    if(this.ballCount == DEFAULT_BALL_COUNT){
+      throw new IllegalArgumentException(MSG_ERROR_LIMIT_BALL_COUNT);
+    }
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    final Score score1 = (Score) o;
+    return score == score1.score && ballCount == score1.ballCount;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(score, ballCount);
+  }
+
+  @Override
+  public String toString() {
+    return "Score{" +
+        "score=" + score +
+        ", ballCount=" + ballCount +
+        '}';
+  }
 }
