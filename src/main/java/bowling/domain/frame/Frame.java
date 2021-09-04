e1a74d30b9701d1549424d8ef3cd6b41c5f9cb59
ChangSubKwak <paradise1732@gmail.com>
2021-09-04 22:07:28 +0900

    feat: 프레임 추상클래스(Frame) 작성

---
+++ src/main/java/bowling/domain/frame/Frame.java
@@ -0,0 +1,70 @@
+package bowling.domain.frame;
+
+import bowling.domain.score.Score;
+import bowling.domain.score.Scores;
+
+import java.util.Objects;
+
+public abstract class Frame {
+    public static final Score TEN_SCORE = Score.from(10);
+    protected static final String OUT_OF_FRAME_SCORE_EXCEPTION_STATEMENT = "프레임 내 점수값의 합이 범위를 벗어났습니다";
+    protected static final Score ZERO_SCORE = Score.from(0);
+    protected static final String STRIKE_SYMBOL = "X";
+    protected static final String SEPARATOR_SYMBOL = "|";
+    protected static final String SPARE_SYMBOL = "/";
+    protected static final String GUTTER_SYMBOL = "-";
+
+    protected final Scores scores;
+
+    public Frame(Score... scores) {
+        this.scores = Scores.of(scores);
+    }
+
+    public Frame(Scores scores) {
+        this.scores = scores;
+    }
+
+    public void addScore(Score score) {
+        scores.addScore(score);
+        validate();
+    }
+
+    protected String changeScoreToSymbol(final Score score) {
+        if (Objects.isNull(score)) {
+            return "";
+        }
+
+        if (score.equals(ZERO_SCORE)) {
+            return GUTTER_SYMBOL;
+        }
+
+        if (score.equals(TEN_SCORE)) {
+            return STRIKE_SYMBOL;
+        }
+
+        return score.score() + "";
+    }
+
+    protected String changeScoreToSpare(final Score firstTryScore) {
+        return changeScoreToSymbol(firstTryScore) + SEPARATOR_SYMBOL + SPARE_SYMBOL;
+    }
+
+    public String toScoreSymbol() {
+        Score firstTryScore = scores.firstTryScore();
+        Score secondTryScore = scores.secondTryScore();
+
+        if (Objects.isNull(secondTryScore)) {
+            return changeScoreToSymbol(firstTryScore);
+        }
+
+        if (firstTryScore.isEqualTenAfterAdd(secondTryScore)) {
+            return changeScoreToSpare(firstTryScore);
+        }
+
+        return changeScoreToSymbol(firstTryScore) + SEPARATOR_SYMBOL + changeScoreToSymbol(secondTryScore);
+    }
+
+    protected abstract void validate();
+
+    public abstract boolean isNextFrame();
+}
