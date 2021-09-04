523cd533352785e5e9bb4e47152c99d54c41849b
ChangSubKwak <paradise1732@gmail.com>
2021-09-04 20:59:47 +0900

    fix: 함수명을 @DisplayName을 이용하여 변경

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,14 +1,15 @@
 package qna.domain;
 
-import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import qna.CannotDeleteException;
 
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-
-import qna.CannotDeleteException;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class QuestionTest {
@@ -27,8 +28,9 @@ public class QuestionTest {
     }
 
 
+    @DisplayName("질문_데이터를_삭제처리_하였을_때_데이터를_삭제하는_것이_아니라_데이터의_상태를_변경한다")
     @Test
-    public void 질문_데이터를_삭제처리_하였을_때_데이터를_삭제하는_것이_아니라_데이터의_상태를_변경한다() {
+    public void changeStatusOfDataTest() {
         assertThat(question.isDeleted()).isFalse();
         question.delete(UserTest.JAVAJIGI);
         assertThat(question).isNotNull();
@@ -36,24 +38,27 @@ public class QuestionTest {
         assertThat(question.isDeleted()).isTrue();
     }
 
+    @DisplayName("로그인_사용자와_질문한_사람이_같은_경우_삭제_가능하다")
     @Test
-    public void 로그인_사용자와_질문한_사람이_같은_경우_삭제_가능하다() {
+    public void deleteTestIfLoginUserIsEqualToQuestionerTest() {
         User loginUser = UserTest.JAVAJIGI;
         assertThat(question.isDeleted()).isFalse();
         question.delete(loginUser);
         assertThat(question.isDeleted()).isTrue();
     }
 
+    @DisplayName("답변이_없는_경우_삭제_가능하다")
     @Test
-    public void 답변이_없는_경우_삭제_가능하다() {
+    public void deleteTestIfNoAnswerTest() {
         assertThat(question.getAnswers().size()).isEqualTo(0);
         assertThat(question.isDeleted()).isFalse();
         question.delete(UserTest.JAVAJIGI);
         assertThat(question.isDeleted()).isTrue();
     }
 
+    @DisplayName("질문자와_답변글의_모든_답변자가_같은_경우_삭제가_가능하다")
     @Test
-    public void 질문자와_답변글의_모든_답변자가_같은_경우_삭제가_가능하다() {
+    public void deleteTestIfQuestionerIsEqualToAllAnswerersTest() {
         question.addAnswer(answer1);
         assertThat(question.getWriter()).isEqualTo(answer1.getWriter());
         assertThat(question.isDeleted()).isFalse();
@@ -61,8 +66,9 @@ public class QuestionTest {
         assertThat(question.isDeleted()).isTrue();
     }
 
+    @DisplayName("질문을_삭제할_때_답변_또한_삭제해야_하며_답변의_삭제_또한_삭제_상태_deleted로_변경한다")
     @Test
-    public void 질문을_삭제할_때_답변_또한_삭제해야_하며_답변의_삭제_또한_삭제_상태_deleted로_변경한다() {
+    public void mustChangeStatusAndDeleteQuestionAndAnswersTest() {
         question.addAnswer(answer1);
 
         assertThat(question.isDeleted()).isFalse();
@@ -72,16 +78,18 @@ public class QuestionTest {
         assertThat(answer1.isDeleted()).isTrue();
     }
 
+    @DisplayName("질문자와_답변자가_다른_경우_답변을_삭제할_수_없다")
     @Test
-    public void 질문자와_답변자가_다른_경우_답변을_삭제할_수_없다() {
+    public void cannotDeleteIfQuestionerIsDifferentFromAnswerer() {
         question.addAnswer(answer2);
         assertThatThrownBy(() -> {
             question.delete(UserTest.JAVAJIGI);
         }).isInstanceOf(CannotDeleteException.class);
     }
 
+    @DisplayName("질문과_답변_삭제_이력에_대한_정보를_DeleteHistory를_활용해_남긴다")
     @Test
-    public void 질문과_답변_삭제_이력에_대한_정보를_DeleteHistory를_활용해_남긴다() {
+    public void writeDeletionAboutQuestionAndAnswerToDeleteHistory() {
         question.addAnswer(answer1);
         List<DeleteHistory> deleteHistories = question.delete(UserTest.JAVAJIGI);
         assertThat(deleteHistories.contains(DeleteHistory.newQuestion(question.getId(), question.getWriter()))).isTrue();
