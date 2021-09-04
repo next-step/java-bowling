2c3ad947d0a61131959e8521ce23af29e7b80c54
kdh85 <modudamba@naver.com>
2021-09-04 20:54:57 +0900

    refactor : step3 볼링게임 running 상태 클래스 메소드 리팩토링 exception 메세지 작성.

--- src/main/java/bowling/state/Running.java
+++ src/main/java/bowling/state/Running.java
@@ -4,6 +4,8 @@ import bowling.score.Score;
 
 public abstract class Running implements State {
 
+  private static final String MSG_ERROR_NOT_FINISH = "아직 종료되지 않은 상태입니다.";
+
   @Override
   public boolean isFinish() {
     return false;
@@ -11,6 +13,6 @@ public abstract class Running implements State {
 
   @Override
   public Score score() {
-    throw new RuntimeException();
+    throw new RuntimeException(MSG_ERROR_NOT_FINISH);
   }
 }
