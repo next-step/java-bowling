a772b47489ee8e8c3d4bfe8102e26ddb5d653723
hyune <choi8608@gmail.com>
2021-09-05 01:20:48 +0900

    refactor: 코드 정리

--- src/main/java/bowling/domain/exception/InvalidMethodCallException.java
+++ src/main/java/bowling/domain/exception/InvalidMethodCallException.java
@@ -2,7 +2,7 @@ package bowling.domain.exception;
 
 public class InvalidMethodCallException extends RuntimeException {
 
-	private static final String MESSAGE = "잘못된 메소드 호출입니다.ㅐ";
+	private static final String MESSAGE = "잘못된 메소드 호출입니다.";
 
 	public InvalidMethodCallException() {
 		super(MESSAGE);
