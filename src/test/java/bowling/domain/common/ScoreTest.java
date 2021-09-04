62420073f102a9e67f062377925540744be7580a
hyune <choi8608@gmail.com>
2021-09-04 16:50:34 +0900

    test: 점수 계산

--- src/test/java/bowling/domain/common/ScoreTest.java
+++ src/test/java/bowling/domain/common/ScoreTest.java
@@ -2,12 +2,11 @@ package bowling.domain.common;
 
 import static org.assertj.core.api.Assertions.*;
 
+import bowling.domain.common.exception.InvalidCaculateScoreException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
-import bowling.domain.common.exception.InvalidCaculateScoreException;
 
 @DisplayName("점수")
 class ScoreTest {
@@ -15,7 +14,7 @@ class ScoreTest {
 	@DisplayName("생성")
 	@ParameterizedTest
 	@CsvSource({
-		"10,1"
+			"10,1"
 	})
 	void create(final int score, final int left) {
 		// given
@@ -30,10 +29,10 @@ class ScoreTest {
 	@DisplayName("생성 - 유효하지 않음")
 	@ParameterizedTest
 	@CsvSource({
-		"-1,0",
-		"31,0",
-		"0,-1",
-		"0,4",
+			"-1,0",
+			"31,0",
+			"0,-1",
+			"0,4",
 	})
 	void create_invalid(final int score, final int left) {
 		// given
@@ -43,5 +42,22 @@ class ScoreTest {
 
 		// then
 
+	}
+
+	@DisplayName("계산")
+	@ParameterizedTest
+	@CsvSource({
+			"5,0,5",
+			"10,0,10",
+	})
+	void calculate(final int score, final int left, final int expected) {
+		// given
+
+		// when
+		final Score newScore = new Score(score, left);
+
+		// then
+		assertThat(newScore.getLeftCount()).isEqualTo(left);
+		assertThat(newScore.getValue()).isEqualTo(expected);
 	}
 }
