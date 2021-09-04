b25c958e0324fe6ff444dba64a905c64851e5ef0
ChangSubKwak <paradise1732@gmail.com>
2021-09-04 21:03:00 +0900

    fix: validateAnswer을 validate로 변경

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -1,14 +1,10 @@
 package qna.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.ForeignKey;
-import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
-
 import qna.CannotDeleteException;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
+
+import javax.persistence.*;
 
 @Entity
 public class Answer extends AbstractEntity {
@@ -35,11 +31,11 @@ public class Answer extends AbstractEntity {
     public Answer(Long id, User writer, Question question, String contents) {
         super(id);
 
-        if(writer == null) {
+        if (writer == null) {
             throw new UnAuthorizedException();
         }
 
-        if(question == null) {
+        if (question == null) {
             throw new NotFoundException();
         }
 
@@ -48,18 +44,13 @@ public class Answer extends AbstractEntity {
         this.contents = contents;
     }
 
-    public Answer setDeleted(boolean deleted) {
-        this.deleted = deleted;
-        return this;
-    }
-
     public DeleteHistory delete(User loginUser) throws CannotDeleteException {
-        validateAnswer(loginUser);
+        validate(loginUser);
         setDeleted(true);
         return DeleteHistory.newAnswer(getId(), getWriter());
     }
 
-    private void validateAnswer(User loginUser) throws CannotDeleteException {
+    private void validate(User loginUser) throws CannotDeleteException {
         if (!isOwner(loginUser)) {
             throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
         }
@@ -67,6 +58,11 @@ public class Answer extends AbstractEntity {
 
     public boolean isDeleted() {
         return deleted;
+    }
+
+    public Answer setDeleted(boolean deleted) {
+        this.deleted = deleted;
+        return this;
     }
 
     public boolean isOwner(User writer) {
