abf1dd005be53225bec72278a02a4ade543487e0
ChangSubKwak <paradise1732@gmail.com>
2021-09-05 23:22:47 +0900

    fix: 플레이어 이름을 Player에서 관리함에 따라 수정

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -1,15 +1,11 @@
 package bowling.view;
 
-import java.util.Objects;
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String INPUT_PLAYER_NAME = "플레이어 이름은(3 english letters)?: ";
     private static final String INPUT_N_FRAME_THROW = "%d프레임 투구: ";
-    private static final String PLAYER_NAME_NULL_OR_EMPTY_EXCEPTION_STATEMENT = "플레이어 이름이 널이거나 빈 문자입니다";
-    private static final String PLAYER_NAME_LENGTH_EXCEPTION_STATEMENT = "플레이어 이름의 길이가 3자가 아닙니다";
-    private static final int PLAYER_NAME_LENGTH = 3;
 
     private String inputStringValue() {
         return scanner.nextLine();
@@ -23,19 +19,7 @@ public class InputView {
 
     public String inputPlayerName() {
         System.out.println(INPUT_PLAYER_NAME);
-        String playerName = inputStringValue();
-        validate(playerName);
-        return playerName;
-    }
-
-    private void validate(String playerName) {
-        if (Objects.isNull(playerName) || playerName.isEmpty()) {
-            throw new IllegalArgumentException(PLAYER_NAME_NULL_OR_EMPTY_EXCEPTION_STATEMENT);
-        }
-
-        if (playerName.length() == PLAYER_NAME_LENGTH) {
-            throw new IllegalArgumentException(PLAYER_NAME_LENGTH_EXCEPTION_STATEMENT);
-        }
+        return inputStringValue();
     }
 
     public int inputNFrameThrow(int frameNumber) {
@@ -43,7 +27,6 @@ public class InputView {
         System.out.printf(INPUT_N_FRAME_THROW, frameNumber);
         return inputIntValue();
     }
-
 
     public void scannerClose() {
         scanner.close();
