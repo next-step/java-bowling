92c14e8392c52f20cf2117d05afc8840a31d381b
kdh85 <modudamba@naver.com>
2021-09-05 17:41:17 +0900

    feat : step3 볼링게임 게임결과 객체 생성 게임의 누적점수 연산과 결과를 담을 객체 생성.

---
+++ src/main/java/bowling/score/ScoreBoard.java
@@ -0,0 +1,35 @@
+package bowling.score;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ScoreBoard {
+
+  private final LinkedList<ScoreResult> scoreResults;
+
+  private int totalScore = 0;
+
+  public ScoreBoard() {
+    this.scoreResults = new LinkedList<>();
+  }
+
+  public void addScoreResult(ScoreResult scoreResult){
+    if(!scoreResult.isNotCheckScore()){
+      totalScore = scoreResult.addTotalScore(totalScore);
+    }
+    scoreResults.add(scoreResult);
+  }
+
+  public List<ScoreResult> getScoreResults() {
+    return Collections.unmodifiableList(scoreResults);
+  }
+
+  @Override
+  public String toString() {
+    return "ScoreBoard{" +
+        "scoreResults=" + scoreResults +
+        ", totalScore=" + totalScore +
+        '}';
+  }
+}
