57498c0695f949278ce421fb5f2437612798a1b6
kdh85 <modudamba@naver.com>
2021-09-05 17:47:46 +0900

    refactor : step3 볼링게임 프래임 dto 파라메터 추가로 인한 리팩토링 누적 합산 점수를 추가하기 위한 코드 리팩토링.

--- src/main/java/bowling/dto/FrameDto.java
+++ src/main/java/bowling/dto/FrameDto.java
@@ -6,16 +6,23 @@ public class FrameDto {
 
   private final String scoreMark;
 
-  private FrameDto(final String scoreMark) {
+  private final int totalScore;
+
+  private FrameDto(final String scoreMark, final int totalScore) {
     this.scoreMark = scoreMark;
+    this.totalScore = totalScore;
   }
 
-  public static FrameDto from(final String scoreMark) {
-    return new FrameDto(scoreMark);
+  public static FrameDto from(final ScoreResultDto scoreBoard) {
+    return new FrameDto(scoreBoard.getScoreMessage(),scoreBoard.getTotalScore());
   }
 
-  public String frameScoreMark() {
+  public String getScoreMark() {
     return scoreMark;
+  }
+
+  public int getTotalScore() {
+    return totalScore;
   }
 
   @Override
