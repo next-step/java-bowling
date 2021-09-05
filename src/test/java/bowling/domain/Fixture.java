1d3076167759ddc6320df4d17059910eabe6248f
ChangSubKwak <paradise1732@gmail.com>
2021-09-05 23:14:36 +0900

    test: 프레임 테스트를 위한 픽스쳐 클래스 작성

---
+++ src/test/java/bowling/domain/Fixture.java
@@ -0,0 +1,8 @@
+package bowling.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Fixture {
+    public static final List<Integer> NINE_STRIKE_STATE = Arrays.asList(10, 10, 10, 10, 10, 10, 10, 10, 10);
+}
