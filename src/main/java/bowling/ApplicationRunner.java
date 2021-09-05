b18afea1f37cb6017f66c0cabe6faa29dfbf39e8
kimdonggyu-dev <donggyu.dev@gmail.com>
2021-09-05 20:25:37 +0900

    feat: 이름 및 프레임 출력

--- src/main/java/bowling/ApplicationRunner.java
+++ src/main/java/bowling/ApplicationRunner.java
@@ -1,6 +1,7 @@
 package bowling;
 
 import bowling.view.BowlingConsoleInputView;
+import bowling.view.BowlingConsoleOutputView;
 
 import java.util.Scanner;
 
@@ -10,11 +11,13 @@ public class ApplicationRunner {
         Scanner scanner = new Scanner(System.in);
 
         BowlingConsoleInputView inputView = new BowlingConsoleInputView(scanner);
+        BowlingConsoleOutputView outputView = new BowlingConsoleOutputView();
+
         String playerText = inputView.enterPlayer();
 
         for (int i = 1; i <= 10; i++) {
             String scoreText = inputView.enterScore(i);
-
+            outputView.print(playerText);
         }
     }
 }
--- src/main/java/bowling/view/BowlingConsoleOutputView.java
+++ src/main/java/bowling/view/BowlingConsoleOutputView.java
@@ -2,5 +2,18 @@ package bowling.view;
 
 public class BowlingConsoleOutputView {
 
+    public void print(final String player) {
+        header();
+        score(player);
+    }
 
+    private void header() {
+        System.out.println("| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |");
+    }
+
+    private void score(final String player) {
+        System.out.printf("|  %s |      |      |      |      |      |      |      |      |      |      |", player);
+        System.out.println();
+        System.out.println();
+    }
 }
