a924334b07cc036081cb3f6a32531d7fe00295eb
YooInKeun <keun0390@naver.com>
2021-09-06 12:00:14 +0900

    refactor: QnAService의 질문 관련 비즈니스 로직 및 테스트 코드, Question 도메인으로 이동

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,67 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("질문 테스트")
 public class QuestionTest {
-    public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-    public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @DisplayName("다른 유저의 질문을 삭제하려고 하면 예외가 발생한다.")
+    @Test
+    void deleteOtherUserQuestionExceptionTest() {
+        // given
+        Question question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
+
+        // when, then
+        assertThatExceptionOfType(CannotDeleteException.class)
+                .isThrownBy(() -> question.deleteAndGenerateHistories(UserTest.SANJIGI, 1L))
+                .withMessage("질문을 삭제할 권한이 없습니다.");
+    }
+
+    @DisplayName("자신의 질문을 삭제할 때, 답변자 중 다른 유저가 있으면 예외가 발생한다.")
+    @Test
+    void deleteMyQuestionAndOtherUserAnswerExceptionTest() {
+        // given
+        Question question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        Answer answer = new Answer(1L, UserTest.SANJIGI, question, "Answers Contents2");
+        question.addAnswer(answer);
+
+        // when, then
+        assertThatExceptionOfType(CannotDeleteException.class)
+                .isThrownBy(() ->  question.deleteAndGenerateHistories(UserTest.JAVAJIGI, 1L))
+                .withMessage("답변을 삭제할 권한이 없습니다.");
+    }
+
+    @DisplayName("답변이 없는 내 질문은 정상적으로 삭제된다.")
+    @Test
+    void deleteMyQuestionTest() throws CannotDeleteException {
+        // given
+        Question question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
+
+        // when
+        question.deleteAndGenerateHistories(UserTest.JAVAJIGI, 1L);
+
+        // then
+        assertTrue(question.isDeleted());
+    }
+
+    @DisplayName("질문자와 답변자가 모두 자신이면 질문과 답변은 정상적으로 삭제된다.")
+    @Test
+    void deleteMyQuestionAndAnswersTest() throws CannotDeleteException {
+        // given
+        Question question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        Answer answer = new Answer(1L, UserTest.JAVAJIGI, question, "Answers Contents1");
+        question.addAnswer(answer);
+
+        // when
+        question.deleteAndGenerateHistories(UserTest.JAVAJIGI, 1L);
+
+        // then
+        assertTrue(question.isDeleted());
+        assertTrue(answer.isDeleted());
+    }
 }
