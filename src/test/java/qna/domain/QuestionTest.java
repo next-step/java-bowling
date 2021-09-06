6311bc0e609506714f1dcc55359df876227a9e41
YooInKeun <keun0390@naver.com>
2021-09-07 00:15:24 +0900

    refactor: 중복되는 given 코드 제거

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,5 +1,6 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import qna.CannotDeleteException;
@@ -10,12 +11,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @DisplayName("질문 테스트")
 public class QuestionTest {
 
+    private Question question;
+
+    @BeforeEach
+    void setUp() {
+        question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
+    }
+
     @DisplayName("다른 유저의 질문을 삭제하려고 하면 예외가 발생한다.")
     @Test
     void deleteOtherUserQuestionExceptionTest() {
-        // given
-        Question question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
-
         // when, then
         assertThatExceptionOfType(CannotDeleteException.class)
                 .isThrownBy(() -> question.deleteAndGenerateHistories(UserTest.SANJIGI, 1L))
@@ -26,7 +31,6 @@ public class QuestionTest {
     @Test
     void deleteMyQuestionAndOtherUserAnswerExceptionTest() {
         // given
-        Question question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
         Answer answer = new Answer(1L, UserTest.SANJIGI, question, "Answers Contents2");
         question.addAnswer(answer);
 
@@ -39,9 +43,6 @@ public class QuestionTest {
     @DisplayName("답변이 없는 내 질문은 정상적으로 삭제된다.")
     @Test
     void deleteMyQuestionTest() {
-        // given
-        Question question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
-
         // when
         question.deleteAndGenerateHistories(UserTest.JAVAJIGI, 1L);
 
@@ -52,8 +53,6 @@ public class QuestionTest {
     @DisplayName("질문자와 답변자가 모두 자신이면 질문과 답변은 정상적으로 삭제된다.")
     @Test
     void deleteMyQuestionAndAnswersTest() {
-        // given
-        Question question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
         Answer answer = new Answer(1L, UserTest.JAVAJIGI, question, "Answers Contents1");
         question.addAnswer(answer);
 
