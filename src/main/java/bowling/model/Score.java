e12aabc8ca36f10350ab0307efc6cf6a95a26836
YooInKeun <keun0390@naver.com>
2021-09-07 21:02:53 +0900

    feat: 두 번째 점수값 셋팅 및 값 범위 유효성 검사 기능 추가

--- src/main/java/bowling/model/Score.java
+++ src/main/java/bowling/model/Score.java
@@ -1,8 +1,8 @@
 package bowling.model;
 
 public class Score {
-    private static final int MIN_FIRST = 0;
-    private static final int MAX_FIRST = 10;
+    private static final int MIN = 0;
+    private static final int MAX = 10;
 
     private final int first;
     private int second;
@@ -14,9 +14,23 @@ public class Score {
     }
 
     private void validateFirstScoreRange(int first) {
-        if (first < MIN_FIRST || first > MAX_FIRST) {
-            throw new IllegalArgumentException(String.format("첫 번쨰 볼링 점수는 %d점 이상 %d점 이하여야 합니다.",
-                    MIN_FIRST, MAX_FIRST));
+        if (first < MIN || first > MAX) {
+            throw new IllegalArgumentException(String.format("첫 번쨰 볼링 점수는 %d점 이상 %d점 이하여야 합니다.", MIN, MAX));
+        }
+    }
+
+    public void setSecond(int second) {
+        validateSecondScoreRange(second);
+    }
+
+    private void validateSecondScoreRange(int second) {
+        if (second < MIN) {
+            throw new IllegalArgumentException(String.format("두 번째 볼링 점수가 %d점 미만일 수 없습니다.", MIN));
+        }
+
+        int sum = first + second;
+        if (sum > MAX) {
+            throw new IllegalArgumentException(String.format("총 볼링 점수가 %d점을 초과할 수 없습니다.", MAX));
         }
     }
 }
