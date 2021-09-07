f213e3d2cd8632027cc32bff035d6709a777b4d7
YooInKeun <keun0390@naver.com>
2021-09-07 21:05:12 +0900

    test: 두 번째 점수 최소값 유효성 검사 테스트 기능 추가

--- src/test/java/bowling/model/ScoreTest.java
+++ src/test/java/bowling/model/ScoreTest.java
@@ -1,6 +1,7 @@
 package bowling.model;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,10 +13,22 @@ public class ScoreTest {
     @DisplayName("첫 번째 볼링 점수가 0점 이상 10점 이하가 아니면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(ints = {-1, 11})
-    void outOfRangeFirstScoreExceptionTest(int score) {
+    void outOfRangeFirstScoreExceptionTest(int first) {
         // when, then
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Score(score))
+                .isThrownBy(() -> new Score(first))
                 .withMessage("첫 번쨰 볼링 점수는 0점 이상 10점 이하여야 합니다.");
+    }
+
+    @DisplayName("두 번째 볼링 점수가 0점 미만이면 예외가 발생한다.")
+    @Test
+    void minSecondScoreExceptionTest() {
+        // given
+        Score score = new Score(0);
+
+        // when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> score.setSecond(-1))
+                .withMessage("두 번째 볼링 점수가 0점 미만일 수 없습니다.");
     }
 }
