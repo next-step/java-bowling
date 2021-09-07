5d76b0666ab4c3223b3f4871f1266444aa473c64
beadoer1 <beadoer1@gmail.com>
2021-09-07 17:08:18 +0900

    refactor(Answers) : Question 의 answers 일급 컬렉션의 test fail 생성

---
+++ src/main/java/qna/domain/Answers.java
@@ -0,0 +1,13 @@
+package qna.domain;
+
+import java.util.List;
+
+public class Answers {
+
+    public void add(Answer a1) {
+    }
+
+    public List<DeleteHistory> delete(User javajigi) {
+        return null;
+    }
+}
---
+++ src/test/java/qna/domain/AnswersTest.java
@@ -0,0 +1,43 @@
+package qna.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import java.util.List;
+
+public class AnswersTest {
+
+    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("Answers 삭제 처리 후 DeleteHistory List 반환")
+    void answersDeleteHistories() {
+        // given
+        Answers answers = new Answers();
+        answers.add(A1);
+
+        // when
+        List<DeleteHistory> deleteHistories = answers.delete(UserTest.JAVAJIGI);
+
+        //then
+        Assertions.assertThat(deleteHistories.get(0))
+                .isEqualTo(new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1"));
+    }
+
+    @Test
+    @DisplayName("Answer 삭제 실패 : loginUser 외 댓글 작성자 여부 확인")
+    void delete_fail_different_comment_user() {
+        // given
+        Answers answers = new Answers();
+        answers.add(A1);
+        answers.add(A2);
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> answers.delete(UserTest.JAVAJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessageContaining("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
+}
