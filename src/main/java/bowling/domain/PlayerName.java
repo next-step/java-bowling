30dc164945abed7d158b6b99f6e8f77c170c7df0
jaeyoungjang2 <lion85488@gmail.com>
2021-09-07 17:53:17 +0900

    state 변경

--- src/main/java/bowling/domain/PlayerName.java
+++ src/main/java/bowling/domain/PlayerName.java
@@ -4,7 +4,6 @@ public class PlayerName {
     private static final String NAME_LENGTH_ERROR_MESSAGE = "이름은 3글자로 지정해야 합니다.";
     private final String name;
 
-
     public PlayerName(String name) {
         validNameCheck(name);
         this.name = name;
