15afb81639d83ff86a6838333658e1c145044b41
YooInKeun <keun0390@naver.com>
2021-09-07 20:52:34 +0900

    test: 첫 번째 점수 유효성 테스트하는 기능 추가

---
+++ src/test/java/bowling/model/ScoreTest.java
@@ -0,0 +1,21 @@
+package bowling.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+@DisplayName("볼링 게임 점수 테스트")
+public class ScoreTest {
+
+    @DisplayName("첫 번째 볼링 점수가 0점 이상 10점 이하가 아니면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 11})
+    void outOfRangeFirstScoreExceptionTest(int score) {
+        // when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Score(score))
+                .withMessage("첫 번쨰 볼링 점수는 0점 이상 10점 이하여야 합니다.");
+    }
+}
