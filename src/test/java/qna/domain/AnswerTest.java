38fad5a6a69a3cdf954093cff390c27307959505
beadoer1 <beadoer1@gmail.com>
2021-09-07 17:30:51 +0900

    refactor(Answer) : Answer 삭제 관련 test fail 작성

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,33 @@
 package qna.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import java.time.LocalDateTime;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("Answer 삭제 처리 후 DeleteHistory 반환")
+    void delete(){
+        // when
+        DeleteHistory deleteHistory = A1.delete(UserTest.JAVAJIGI);
+
+        //then
+        Assertions.assertThat(deleteHistory)
+                .isEqualTo(new DeleteHistory(ContentType.ANSWER, A1.getId(), A1.getWriter(), LocalDateTime.now()));
+    }
+
+    @Test
+    @DisplayName("Answer 삭제 실패 : loginUser 외 댓글 작성자 여부 확인")
+    void delete_fail_different_comment_user() {
+        // when, then
+        Assertions.assertThatThrownBy(() -> A2.delete(UserTest.JAVAJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessageContaining("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
 }
