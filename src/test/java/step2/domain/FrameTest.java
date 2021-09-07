b317a8dc7ee3ff2a836898d5acd6717be11913b2
jaeyoungjang2 <lion85488@gmail.com>
2021-09-07 18:52:34 +0900

    test: 볼링핀 넘어뜨리는 기능에 대한 테스트 구현

--- src/test/java/step2/domain/FrameTest.java
+++ src/test/java/step2/domain/FrameTest.java
@@ -1,4 +1,16 @@
-import static org.junit.jupiter.api.Assertions.*;
+package step2.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 class FrameTest {
-  
+    @DisplayName("볼링핀 넘어뜨렸을 떄의 경우 결과 확인")
+    @Test
+    void knockDownBowlingPins() {
+        Frame frame = new Frame();
+        frame.knockDown(4);
+        assertThat(frame).isEqualTo(new Frame(6,4));
+    }
 }
