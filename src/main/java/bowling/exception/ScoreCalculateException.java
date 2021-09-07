da9148b639d5ab175c70d446cf1aef6604bb2d15
kdh85 <modudamba@naver.com>
2021-09-07 16:21:41 +0900

    style : step3 볼링게임 코드 컨밴션 수행 미사용 오버라이드 메소드 정리하여 반영.

--- src/main/java/bowling/exception/ScoreCalculateException.java
+++ src/main/java/bowling/exception/ScoreCalculateException.java
@@ -2,10 +2,6 @@ package bowling.exception;
 
 public class ScoreCalculateException extends RuntimeException {
 
-  public ScoreCalculateException() {
-    super();
-  }
-
   public ScoreCalculateException(final String message) {
     super(message);
   }
