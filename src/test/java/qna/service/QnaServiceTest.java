615d776479364545b1d5b2e8cbf1a9bf6ecb89f9
gunny6026 <gunny6026@naver.com>
2021-09-07 10:36:56 +0900

    test : Answer delete 예외 테스트

--- src/test/java/qna/service/QnaServiceTest.java
+++ src/test/java/qna/service/QnaServiceTest.java
@@ -54,9 +54,17 @@ public class QnaServiceTest {
 
     @Test
     @DisplayName("로그인 사용자와 질문한 사람이 다른 경우 질문을 삭제 할 수 없다.")
-    public void delete_실패_() throws CannotDeleteException {
+    public void delete_실패_1() throws CannotDeleteException {
         assertThatThrownBy(
                 () -> question.delete(UserTest.SANJIGI)).isInstanceOf(CannotDeleteException.class
+        );
+    }
+
+    @Test
+    @DisplayName("질문자와 답변자가 다른 경우 답변을 삭제 할 수 없다.")
+    public void delete_실패_2() throws CannotDeleteException {
+        assertThatThrownBy(
+                () -> answer.delete(UserTest.SANJIGI)).isInstanceOf(CannotDeleteException.class
         );
     }
 
