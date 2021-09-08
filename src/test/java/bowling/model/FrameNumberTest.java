a1f67031fc78bab4e0ec30895284b78ac7809490
YooInKeun <keun0390@naver.com>
2021-09-08 09:48:31 +0900

    test: FrameNumber의 다음 프레임 번호 관련 조회 메소드에 대한 테스트 코드 추가

--- src/test/java/bowling/model/FrameNumberTest.java
+++ src/test/java/bowling/model/FrameNumberTest.java
@@ -1,21 +1,72 @@
 package bowling.model;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("볼링 프레임 번호 테스트")
 public class FrameNumberTest {
 
-    @DisplayName("프레임 번호가 1미만 10 초과이면 예외가 발생한다.")
+    @DisplayName("프레임 번호가 1 이상 10 이하가 아니면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(ints = {0, 11})
-    void outOfBoundFrameNumberExceptionTest(int number) {
+    void outOfRangeFrameNumberExceptionTest(int number) {
         // given
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new FrameNumber(number))
-                .withMessage("프레임 번호는 1이상 10 이하이어야 합니다.");
+                .withMessage("프레임 번호는 1 이상 10 이하이어야 합니다.");
+    }
+
+    @DisplayName("첫번째 프레임 번호는 1이다.")
+    @Test
+    void firstFrameNumberTest() {
+        // given, when, then
+        assertEquals(FrameNumber.first(), new FrameNumber(1));
+    }
+
+    @DisplayName("next() 메소드를 통해 다음 프레임 번호를 생성할 수 있다.")
+    @Test
+    void nextFrameNumberTest() {
+        // given, when, then
+        assertEquals(FrameNumber.first().next(), new FrameNumber(2));
+    }
+
+    @DisplayName("canMakeNext() 메소드를 통해 다음 프레임 번호 생성 가능 여부를 조회할 수 있다.")
+    @ParameterizedTest
+    @CsvSource(value = {"9:true", "10:false"}, delimiter = ':')
+    void canMakeNextFrameNumberTest(int number, boolean result) {
+        // given, when, then
+        assertSame(new FrameNumber(number).canMakeNext(), result);
+    }
+
+    @DisplayName("isNextFinal() 메소드를 통해 다음 프레임 번호가 마지막 번호인지 여부를 조회할 수 있다.")
+    @ParameterizedTest
+    @CsvSource(value = {"8:false", "9:true", "10:false"}, delimiter = ':')
+    void isNextFinalFrameNumberTest(int number, boolean result) {
+        // given, when, then
+        assertSame(new FrameNumber(number).isNextFinal(), result);
+    }
+
+    @DisplayName("isUnder(), isEqual(), isOver() 메소드를 통해 숫자를 비교할 수 있다.")
+    @Test
+    void comparingFrameNumberTest() {
+        // given
+        FrameNumber number = new FrameNumber(5);
+
+        // when, then
+        assertTrue(number.isUnder(6));
+        assertFalse(number.isUnder(5));
+
+        assertTrue(number.isEqual(5));
+        assertFalse(number.isEqual(4));
+        assertFalse(number.isEqual(6));
+
+        assertTrue(number.isOver(4));
+        assertFalse(number.isOver(5));
     }
 }
