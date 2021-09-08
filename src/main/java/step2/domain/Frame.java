25983e7cb440fbb00c6267f645c804abe84a3037
jaeyoungjang2 <lion85488@gmail.com>
2021-09-08 12:15:09 +0900

    feat: Frame 인터페이스 구현

--- src/main/java/step2/domain/Frame.java
+++ src/main/java/step2/domain/Frame.java
@@ -1,2 +1,16 @@
-package step2.domain;public interface Frame {
+package step2.domain;
+
+import java.util.List;
+
+public interface Frame {
+    void knockDown(int numOfPin);
+
+    void isPossible();
+
+    int score();
+
+    boolean isFinish();
+
+    List<Integer> scoreInfo();
 }
+
