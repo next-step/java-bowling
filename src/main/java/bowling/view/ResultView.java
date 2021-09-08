f17c41cbd8015ccee3a14d662375ffb49881035b
ChangSubKwak <paradise1732@gmail.com>
2021-09-09 01:22:34 +0900

    feat: 누적 점수를 출력하는 기능 추가

--- src/main/java/bowling/view/ResultView.java
+++ src/main/java/bowling/view/ResultView.java
@@ -5,7 +5,11 @@ import java.util.stream.IntStream;
 
 public class ResultView {
     private static final String OUTPUT_FRAME_FORMAT = "  %02d  |";
+    private static final String OUTPUT_EMPTY_FRAME = "|      |";
+    private static final String OUTPUT_EMPTY_HALF_FRAME = "      |";
+    private static final String OUTPUT_PLAYER_NAME_FORMAT = "|  %3s |";
     private static final String OUTPUT_SCORE_FORMAT = "  %-3s |";
+    private static final String OUTPUT_LAST_SCORE_FORMAT = " %-5s|";
     private static final int FRAME_NUMBER_START = 1;
     private static final int FRAME_NUMBER_END = 10;
 
@@ -22,8 +26,18 @@ public class ResultView {
 
     public void outputScores(String playerName, List<String> scores) {
         outputFrames();
-        System.out.printf("|  %3s |", playerName);
-        scores.forEach(this::outputScore);
+        System.out.printf(OUTPUT_PLAYER_NAME_FORMAT, playerName);
+        if (scores.size() != FRAME_NUMBER_END) {
+            scores.forEach(this::outputScore);
+            IntStream.range(scores.size(), FRAME_NUMBER_END)
+                .forEach(i -> outputEmptyHalfFrame());
+        }
+
+        if (scores.size() == FRAME_NUMBER_END) {
+            IntStream.range(FRAME_NUMBER_START - 1, FRAME_NUMBER_END - 1)
+                .forEach(i -> outputScore(scores.get(i)));
+            System.out.printf(OUTPUT_LAST_SCORE_FORMAT, scores.get(FRAME_NUMBER_END - 1));
+        }
         System.out.print(System.lineSeparator());
     }
 
@@ -31,4 +45,19 @@ public class ResultView {
         System.out.printf(OUTPUT_SCORE_FORMAT, score);
     }
 
+    public void outputCumulativeScores(List<Integer> cumulativeScores) {
+        System.out.print(OUTPUT_EMPTY_FRAME);
+        cumulativeScores.forEach(this::outputCumulativeScore);
+        IntStream.range(cumulativeScores.size(), FRAME_NUMBER_END)
+            .forEach(i -> outputEmptyHalfFrame());
+        System.out.print(System.lineSeparator());
+    }
+
+    private void outputCumulativeScore(Integer cumulativeScore) {
+        System.out.printf(OUTPUT_SCORE_FORMAT, cumulativeScore);
+    }
+
+    private void outputEmptyHalfFrame() {
+        System.out.print(OUTPUT_EMPTY_HALF_FRAME);
+    }
 }
