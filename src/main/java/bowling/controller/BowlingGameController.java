6132a595e1061f0e30aaf9464698a9b863cbb25e
ChangSubKwak <paradise1732@gmail.com>
2021-09-09 01:24:28 +0900

    refactor: 누적 점수와 결과문자열 클래스를 추가함에 따른 수정

--- src/main/java/bowling/controller/BowlingGameController.java
+++ src/main/java/bowling/controller/BowlingGameController.java
@@ -3,7 +3,10 @@ package bowling.controller;
 import java.util.ArrayList;
 
 import bowling.domain.Player;
+import bowling.domain.Results;
 import bowling.domain.frame.Frames;
+import bowling.domain.score.AccumulatedScores;
+import bowling.domain.score.Score;
 import bowling.view.InputView;
 import bowling.view.ResultView;
 
@@ -14,7 +17,9 @@ public class BowlingGameController {
         Frames frames = Frames.from(new ArrayList<>());
         Player player = Player.from(inputView.inputPlayerName());
 
-        resultView.outputScores(player.name(), frames.results());
+        resultView.outputScores(player.name(), Results.from(frames).results());
+        resultView.outputCumulativeScores(new ArrayList<>());
+
         executeGame(frames, player.name());
         inputView.scannerClose();
     }
@@ -22,13 +27,13 @@ public class BowlingGameController {
     private static void executeGame(final Frames frames, final String playerName) {
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
-
-        int frameNumber = 1;
-        while (frameNumber <= Frames.TOTAL_FRAME_NUMBER) {
-            int downPinNumber = inputView.inputNFrameThrow(frameNumber);
+        AccumulatedScores accumulatedScores = AccumulatedScores.from(frames);
+        while (!frames.isFinish()) {
+            int downPinNumber = inputView.inputNFrameThrow(frames.frameNumber());
+            accumulatedScores.addScore(Score.from(downPinNumber));
             frames.throwBalls(downPinNumber);
-            frameNumber = frames.nextFrameNumber();
-            resultView.outputScores(playerName, frames.results());
+            resultView.outputScores(playerName, Results.from(frames).results());
+            resultView.outputCumulativeScores(accumulatedScores.getCumulativeScores(frames));
         }
     }
 }
