29046084e0285c1b70c33699380ef2f57f830218
shryu <>
2021-09-09 01:53:21 +0900

    etc : finalFrame

---
+++ src/test/java/bowling/FInalFrameTest.java
@@ -0,0 +1,17 @@
+package bowling;
+
+import bowling.domain.FinalFrame;
+import bowling.domain.Frame;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class FInalFrameTest {
+    Frame frame;
+
+
+}
