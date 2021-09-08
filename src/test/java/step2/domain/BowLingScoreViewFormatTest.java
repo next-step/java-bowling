8433d40363424c02aa29e32eea44ee37e441f790
jaeyoungjang2 <lion85488@gmail.com>
2021-09-08 12:18:56 +0900

    test: bowlingScreenViewFormat 테스트 구현 스크린에 나타나는 포맷으로 결과값이 잘 변경되는지 확인

--- src/test/java/step2/domain/BowLingScoreViewFormatTest.java
+++ src/test/java/step2/domain/BowLingScoreViewFormatTest.java
@@ -3,17 +3,16 @@ package step2.domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import step2.view.BowLingScoreView;
 
 import java.util.Arrays;
 import java.util.List;
 
-class BowLingScoreViewTest {
+class BowLingScoreViewFormatTest {
     @DisplayName("spair 결과 테스트")
     @Test
     void strikeViewTest() {
         List<Integer> bowlingScores = Arrays.asList(10);
-        List<String> actual = BowLingScoreView.transferBowlingScoreViewFormat(bowlingScores);
+        List<String> actual = BowLingScoreViewFormat.transferBowlingScoreViewFormat(bowlingScores);
         Assertions.assertThat(actual.get(0)).isEqualTo("X");
     }
 
@@ -21,7 +20,7 @@ class BowLingScoreViewTest {
     @Test
     void spairViewTest() {
         List<Integer> bowlingScores = Arrays.asList(4,6);
-        List<String> actual = BowLingScoreView.transferBowlingScoreViewFormat(bowlingScores);
+        List<String> actual = BowLingScoreViewFormat.transferBowlingScoreViewFormat(bowlingScores);
         Assertions.assertThat(actual.get(1)).isEqualTo("/");
     }
 
@@ -29,7 +28,7 @@ class BowLingScoreViewTest {
     @Test
     void scoreViewTest() {
         List<Integer> bowlingScores = Arrays.asList(4,6);
-        List<String> actual = BowLingScoreView.transferBowlingScoreViewFormat(bowlingScores);
+        List<String> actual = BowLingScoreViewFormat.transferBowlingScoreViewFormat(bowlingScores);
         Assertions.assertThat(actual.get(0)).isEqualTo("4");
     }
 }
