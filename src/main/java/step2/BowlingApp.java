119453535b3d1771253bca1648f9e49266a50a79
jaeyoungjang2 <lion85488@gmail.com>
2021-09-08 12:20:45 +0900

    feat: BowlingApp 클래스 생성 main 클래스 생성

--- src/main/java/step2/BowlingApp.java
+++ src/main/java/step2/BowlingApp.java
@@ -1,2 +1,11 @@
-package step2.domain;public class BowlingApp {
+package step2;
+
+import step2.controller.BowlingGameController;
+import step2.view.InputView;
+
+public class BowlingApp {
+    public static void main(String[] args) {
+        BowlingGameController bowlingGameController = new BowlingGameController(InputView.requirePlayerName());
+        bowlingGameController.run();
+    }
 }
