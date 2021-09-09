136b54a9bd5a2d59210b2edd2209770ed84aa2ff
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-09 22:49:17 +0900

    feat: 질문자와 답변자가 다른 경우 Exception 기능 구현 및 테스트

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -1,9 +1,13 @@
 package qna.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import qna.CannotDeleteException;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
-
-import javax.persistence.*;
 
 @Entity
 public class Answer extends AbstractEntity {
@@ -41,6 +45,12 @@ public class Answer extends AbstractEntity {
         this.writer = writer;
         this.question = question;
         this.contents = contents;
+    }
+
+    public void deleteAnswer(User writer) {
+        if (!isOwner(writer)) {
+            throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+        }
     }
 
     public Answer setDeleted(boolean deleted) {
--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,22 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("삭제 요청한 유저가 답변글 작성 유저가 아닐 경우 Exception이 발생해야 한다.")
+    void failByLoginAndAnswerIdDismatchTest() {
+
+        // when & then
+        assertThatExceptionOfType(CannotDeleteException.class)
+            .isThrownBy(() -> A1.deleteAnswer(UserTest.SANJIGI))
+            .withMessageMatching("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
 }
