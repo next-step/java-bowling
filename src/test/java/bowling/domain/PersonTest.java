f97dd5645a634932a261c021c2213a011c8ebab7
sk1737030 <sk1737030@naver.com>
2021-09-09 23:14:08 +0900

    test: person테스트 ㅜ가

--- src/test/java/bowling/domain/PersonTest.java
+++ src/test/java/bowling/domain/PersonTest.java
@@ -1,8 +1,11 @@
 package bowling.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
 
 class PersonTest {
 
@@ -14,4 +17,24 @@ class PersonTest {
 
         assertThat(person.name()).isEqualTo(givenName);
     }
+
+
+    @DisplayName("영어가 아닌 문자가 이름으로 들어가면 IllegalArgument 발생")
+    @Test
+    void createPersonWithIllegalArgumentException() {
+        // Given
+        String givenName = "박박박";
+
+        assertThatIllegalArgumentException().isThrownBy(() -> Person.from(givenName));
+    }
+
+    @DisplayName("3자리 이상의 문자가 들어가면 IllegalArgumentException")
+    @Test
+    void createPersonMoreThan3lettersWithIllegalArgumentException() {
+        // Given
+        String givenName = "ABCD";
+
+        assertThatIllegalArgumentException().isThrownBy(() -> Person.from(givenName));
+    }
+
 }
