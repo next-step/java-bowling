13e1e7845556e912288934e5956d8bf2aa5a4dc2
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-10 00:22:48 +0900

    feat: 일급컬랙션 객체생성 오류 수정

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -25,7 +25,7 @@ public class Question extends AbstractEntity {
     private User writer;
 
     @Embedded
-    private Answers answers;
+    private Answers answers = new Answers();
 
     private boolean deleted = false;
 
