ea279daaaf4b9f0f0ff12118fb195eef055f7ac0
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-10 02:19:38 +0900

    refactor

--- src/main/java/qna/service/QnAService.java
+++ src/main/java/qna/service/QnAService.java
@@ -14,6 +14,7 @@ import qna.domain.User;
 
 @Service("qnaService")
 public class QnAService {
+
     private static final Logger log = LoggerFactory.getLogger(QnAService.class);
 
     @Resource(name = "questionRepository")
@@ -28,7 +29,7 @@ public class QnAService {
     @Transactional(readOnly = true)
     public Question findQuestionById(Long id) {
         return questionRepository.findByIdAndDeletedFalse(id)
-                .orElseThrow(NotFoundException::new);
+            .orElseThrow(NotFoundException::new);
     }
 
     @Transactional
