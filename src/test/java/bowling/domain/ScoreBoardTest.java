9fbf24c3b601ef959f1f4faa31ce0dfa9ed9eb5a
sk1737030 <sk1737030@naver.com>
2021-09-09 18:59:28 +0900

    test: 직관적이게 테스트 변경

--- src/test/java/bowling/domain/ScoreBoardTest.java
+++ src/test/java/bowling/domain/ScoreBoardTest.java
@@ -8,20 +8,25 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class ScoreBoardTest {
 
-    @DisplayName("스페어 처리시 보너스를 포함한 파이널 프레임을 계산 할 수 있다.")
+    @DisplayName("final Frame에서 2구 내에 스페어 처리시 bonus frame의 점수들을 확인 할 수 있다.")
     @Test
     void writeFinalFrame() {
+        // Given
         ScoreBoard scoreBoard = ScoreBoard.create();
-        Person person = Person.from("Phobi");
+        Person person = Person.from("PSH");
         Frame fi = FinalFrame.create();
-        scoreBoard.write(person, fi);
 
         while (fi.hasNextRound()) {
-            fi = fi.roll(5);
             scoreBoard.write(person, fi);
+            fi = fi.roll(5);
         }
 
+        // When
         Frames frames = scoreBoard.framesOfPerson(person);
+        Frame bonusFrame = frames.get(2);
+
+        // Then
         assertThat(frames.size()).isEqualTo(3);
+        assertThat(bonusFrame.numberOfDownedPins()).isEqualTo(5);
     }
 }
