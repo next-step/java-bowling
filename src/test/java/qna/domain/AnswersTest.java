03983ffd03a7e29328555d2fff090f55546d6efb
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-09 23:54:52 +0900

    test: Answers answer 일괄 삭제 Exception 테스트

--- src/test/java/qna/domain/AnswersTest.java
+++ src/test/java/qna/domain/AnswersTest.java
@@ -1,6 +1,7 @@
 package qna.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,6 +26,21 @@ class AnswersTest {
 
         // then
         assertThat(results).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("답변 중 작성자와 일치하지 않는 답변이 있어 Exception이 발생해야 한다.")
+    void deleteAnswersFailTest() {
+
+        // given
+        List<Answer> inputs = Arrays.asList(AnswerTest.A1, AnswerTest.A2);
+        Answers answers = new Answers(inputs);
+        User writer = UserTest.JAVAJIGI;
+
+        // then
+        assertThatExceptionOfType(CannotDeleteException.class)
+            .isThrownBy(() -> answers.deleteAll(writer))
+            .withMessageMatching("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
     }
 
 }
