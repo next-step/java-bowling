b4146e356b3441183b008aa6f3e6fe9176bf57ad
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-09 22:38:33 +0900

    refactor: CannotDeleteException 반환 메소드 분리

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -1,10 +1,17 @@
 package qna.domain;
 
-import org.hibernate.annotations.Where;
-
-import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import org.hibernate.annotations.Where;
 import qna.CannotDeleteException;
 
 @Entity
@@ -72,14 +79,18 @@ public class Question extends AbstractEntity {
         answers.add(answer);
     }
 
-    public void deleteQuestion(User loginUser) throws CannotDeleteException {
-        if (!isOwner(loginUser)) {
-            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
-        }
+    public void deleteQuestion(User loginUser) {
+        checkCannotDelete(loginUser);
     }
 
     public boolean isOwner(User loginUser) {
         return writer.equals(loginUser);
+    }
+
+    private void checkCannotDelete(User loginUser) {
+        if (!isOwner(loginUser)) {
+            throw new CannotDeleteException();
+        }
     }
 
     public Question setDeleted(boolean deleted) {
