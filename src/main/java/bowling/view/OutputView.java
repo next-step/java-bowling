76fd708bf97f17fe4abbb03676cf38cb0b0cbca6
rladowl92 <rladowl92@naver.com>
2021-09-10 01:56:16 +0900

    refactor: 메소드 분리

--- src/main/java/bowling/view/OutputView.java
+++ src/main/java/bowling/view/OutputView.java
@@ -1,5 +1,6 @@
 package bowling.view;
 
+import bowling.domain.frame.FinalFrame;
 import bowling.domain.frame.NormalFrame;
 import bowling.domain.player.Player;
 
@@ -7,28 +8,38 @@ import java.util.List;
 
 public class OutputView {
     private static final String TABLE_HEADER = "| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |";
+    private static final String COLUMN_INIT = "|";
     private static final String COLUMN_SEPARATOR = "  %3s |";
+    private static final String EMPTY_SCORE = "  ";
+    private static final int NORMAL_FRAME_SIZE = 9;
 
     public void printBoard(Player player) {
         System.out.println(TABLE_HEADER);
-        System.out.printf("|" + COLUMN_SEPARATOR, player.getPlayerName());
+        System.out.printf(COLUMN_INIT + COLUMN_SEPARATOR, player.getPlayerName());
 
-        List<NormalFrame> normalFrames;
-        for (int i = 0; i < 9; i++) {
-            normalFrames = player.getFrames().getNormalFrames();
-            if (i < normalFrames.size()
-                    && normalFrames.get(i).getNormalScores().getScores().size() > 0) {
-                System.out.printf(COLUMN_SEPARATOR, normalFrames.get(i).getNormalScores().toString());
-            } else {
-                System.out.printf(COLUMN_SEPARATOR, "  ");
-            }
+        for (int i = 0; i < NORMAL_FRAME_SIZE; i++) {
+            printNormalFrames(player, i);
         }
-
-        if(player.getFrames().getFinalFrames().size() !=0){
-            System.out.printf(COLUMN_SEPARATOR, player.getFrames().getFinalFrames().get(0).getFinalScores().toString());
-        } else {
-            System.out.printf(COLUMN_SEPARATOR, "  ");
-        }
+        printFinalFrames(player);
         System.out.println();
+    }
+
+    private void printNormalFrames(Player player, int i) {
+        List<NormalFrame> normalFrames = player.getFrames().getNormalFrames();
+        if (i < normalFrames.size()
+                && normalFrames.get(i).getNormalScores().getScores().size() > 0) {
+            System.out.printf(COLUMN_SEPARATOR, normalFrames.get(i).getNormalScores().toString());
+            return;
+        }
+        System.out.printf(COLUMN_SEPARATOR, EMPTY_SCORE);
+    }
+
+    private void printFinalFrames(Player player) {
+        List<FinalFrame> finalFrames = player.getFrames().getFinalFrames();
+        if(finalFrames.size() !=0){
+            System.out.printf(COLUMN_SEPARATOR, finalFrames.get(0).getFinalScores().toString());
+            return;
+        }
+        System.out.printf(COLUMN_SEPARATOR, EMPTY_SCORE);
     }
 }
