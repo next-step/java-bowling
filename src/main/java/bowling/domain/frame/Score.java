65ea86fa45b3a20e23bc429a9cbde8a49acf88ed
YONGJU <odetsuo@gmail.com>
2021-09-09 23:49:10 +0900

    fix : 남은 투구 수

--- src/main/java/bowling/domain/frame/Score.java
+++ src/main/java/bowling/domain/frame/Score.java
@@ -10,6 +10,7 @@ public class Score {
     private static final int REMAINING_STRIKE = 2;
     private static final int REMAINING_SPARE = 1;
     private static final int NOT_NEED_MORE = 0;
+    private static final int FINAL_STRIKE_OR_SPARE = 20;
 
     private final int value;
     private final int countOfRemaining;
@@ -19,28 +20,45 @@ public class Score {
         this.countOfRemaining = countOfRemaining;
     }
 
+    public static Score ofNone() {
+        return new Score(NONE, NOT_NEED_MORE);
+    }
+
     public static Score of(int value) {
         return new Score(value, NOT_NEED_MORE);
     }
 
     public static Score of(Rollings rollings) {
-        int countOfRemaining = getCountOfRemaining(rollings);
-        return new Score(rollings.sum(), countOfRemaining);
+        return new Score(rollings.sum(), countOfRemaining(rollings));
     }
 
-    private static int getCountOfRemaining(Rollings rollings) {
+    private static int countOfRemaining(Rollings rollings) {
+        if (rollings.sum() > FINAL_STRIKE_OR_SPARE) {
+            return NOT_NEED_MORE;
+        }
         if (rollings.isStrike()) {
             return REMAINING_STRIKE;
         }
-        return rollings.isSpare() ? REMAINING_SPARE : NOT_NEED_MORE;
-
+        if (rollings.isSpare()) {
+            return REMAINING_SPARE;
+        }
+        return rollings.allRolled() ? NOT_NEED_MORE : REMAINING_SPARE;
     }
 
     public Score plus(int bonus) {
+        validateFixed();
+        return new Score(this.value + bonus, this.countOfRemaining - 1);
+    }
+
+    public Score plus(Rollings rollings) {
+        validateFixed();
+        return new Score(this.value + rollings.sum(), this.countOfRemaining - 1);
+    }
+
+    private void validateFixed() {
         if (isFixed()) {
             throw new ScoreFixedException();
         }
-        return new Score(this.value + bonus, this.countOfRemaining - 1);
     }
 
     public boolean isFixed() {
