ea2e6ee794f619e84052ae469c7a61ca75d5964f
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-09 22:38:18 +0900

    refactor: CannotDeleteException message 갖도록 변경

--- src/main/java/qna/CannotDeleteException.java
+++ src/main/java/qna/CannotDeleteException.java
@@ -1,9 +1,16 @@
 package qna;
 
-public class CannotDeleteException extends Exception {
+public class CannotDeleteException extends RuntimeException {
     private static final long serialVersionUID = 1L;
+
+    private static final String CANNOT_DELETE_ERROR_MESSAGE = "질문을 삭제할 권한이 없습니다.";
 
     public CannotDeleteException(String message) {
         super(message);
     }
+
+    public CannotDeleteException() {
+        this(CANNOT_DELETE_ERROR_MESSAGE);
+    }
+
 }
