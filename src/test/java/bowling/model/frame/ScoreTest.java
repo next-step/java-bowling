c539633b89ebd9e0573f7377eefa958ff77c5c23
YooInKeun <keun0390@naver.com>
2021-09-10 11:08:10 +0900

    test: Score 값의 범위에 대한 유효성 테스트하는 코드 추가

---
+++ src/test/java/bowling/model/frame/ScoreTest.java
@@ -0,0 +1,31 @@
+package bowling.model.frame;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+@DisplayName("볼링 점수 테스트")
+public class ScoreTest {
+
+    @DisplayName("볼링 점수가 0점 미만 300점 초과면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 301})
+    void outOfRangeScoreExceptionTest(int score) {
+        // given, when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> Score.of(score))
+                .withMessage("볼링 점수는 0점 이상 300점 이하이어야 합니다.");
+
+        // given, when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> Score.strike(score))
+                .withMessage("볼링 점수는 0점 이상 300점 이하이어야 합니다.");
+
+        // given, when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> Score.spare(score))
+                .withMessage("볼링 점수는 0점 이상 300점 이하이어야 합니다.");
+    }
+}
