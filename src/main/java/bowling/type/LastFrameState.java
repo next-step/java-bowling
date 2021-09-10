d19a8c5f84bac9f80f45cd451452339353d57c05
yook0328 <yook0328@gmail.com>
2021-09-10 13:04:04 +0900

    feat: 1~9번 프레임 및 10번 프레임에 대한 객체 생성 및 마지막 프레임에 대한 remainpin 산출 로직 생성.

---
+++ src/main/java/bowling/type/LastFrameState.java
@@ -0,0 +1,78 @@
+package bowling.type;
+
+import bowling.domain.Shot;
+import bowling.util.BowlingUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum LastFrameState {
+    FIRST_SHOT {
+        @Override
+        public boolean isThisState(List<Shot> shots) {
+            return shots.size() == 0;
+        }
+
+        @Override
+        public int remainPins(int downPins) {
+            return 10;
+        }
+    }, SECOND_SHOT {
+        @Override
+        public boolean isThisState(List<Shot> shots) {
+            return shots.size() == 1 && shots.get(0).getDownCount() < 10;
+        }
+
+        @Override
+        public int remainPins(int downPins) {
+            return 10 - downPins;
+        }
+    }, FIRST_STRIKE {
+        @Override
+        public boolean isThisState(List<Shot> shots) {
+            int sum = BowlingUtils.sum(shots);
+            return shots.size() >= 1 && sum >= 10 && sum < 20;
+        }
+
+        @Override
+        public int remainPins(int downPins) {
+            return 20 - downPins;
+        }
+    }, SPARE {
+        @Override
+        public boolean isThisState(List<Shot> shots) {
+            int sum = BowlingUtils.sum(shots);
+            return shots.size() == 2 && sum == 10;
+        }
+
+        @Override
+        public int remainPins(int downPins) {
+            return 10;
+        }
+    }, DOUBLE_STRIKE {
+        @Override
+        public boolean isThisState(List<Shot> shots) {
+            int sum = BowlingUtils.sum(shots);
+            return sum == 20;
+        }
+
+        @Override
+        public int remainPins(int downPins) {
+            return 10;
+        }
+    };
+
+    public abstract boolean isThisState(List<Shot> shots);
+
+    public abstract int remainPins(int downPins);
+
+
+    public static int calculateRemainPins(List<Shot> shots) {
+        LastFrameState currentState = Arrays.stream(LastFrameState.values())
+                .filter((state) -> state.isThisState(shots))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("it is invalid situation"));
+
+        return currentState.remainPins(BowlingUtils.sum(shots));
+    }
+}
---
+++ src/main/java/bowling/view/NormalFrameString.java
@@ -0,0 +1,63 @@
+package bowling.view;
+
+import bowling.domain.Shot;
+import bowling.util.BowlingUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum  NormalFrameString {
+    ONE(1) {
+        @Override
+        public String result(List<Shot> shots) {
+            if (isStrike(shots)) {
+                return STRIKE;
+            }
+            return shots.get(FIRST).toString();
+        }
+    }, TWO(2) {
+        @Override
+        public String result(List<Shot> shots) {
+            String first = shots.get(FIRST).toString();
+            String second = shots.get(SECOND).toString();
+
+            if (isSpare(shots)) {
+                return first + SEPARATOR + SPARE;
+            }
+
+            return first + SEPARATOR + second;
+        }
+    };
+    private int size;
+
+    private static final int ALL_PIN_COUNT = 10;
+    private static final int FIRST = 0;
+    private static final int SECOND = 1;
+    private static final String STRIKE = "X";
+    private static final String SPARE = "/";
+    private static final String SEPARATOR = "|";
+
+    NormalFrameString(int size) {
+        this.size = size;
+    }
+
+    abstract public String result(List<Shot> shots);
+
+    boolean isSpare(List<Shot> shots) {
+        int sum = BowlingUtils.sum(shots);
+        return shots.size() == 2 && sum == ALL_PIN_COUNT;
+    }
+
+    boolean isStrike(List<Shot> shots) {
+        int sum = BowlingUtils.sum(shots);
+        return shots.size() == 1 && sum == ALL_PIN_COUNT;
+    }
+
+    public static String getString(List<Shot> shots) {
+        return Arrays.stream(NormalFrameString.values())
+                .filter((frameString -> frameString.size == shots.size()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("shot list size is not matched Any state"))
+                .result(shots);
+    }
+}
