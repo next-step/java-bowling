fc50f1ba3b1e27c65f15b661ab6a487f01edb357
harris <harrisleesh@gmail.com>
2021-09-10 10:10:36 +0900

    feat : Create 1st collection Answers

---
+++ src/main/java/qna/domain/Answers.java
@@ -0,0 +1,41 @@
+package qna.domain;
+
+import org.hibernate.annotations.Where;
+import qna.CannotDeleteException;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import java.util.ArrayList;
+import java.util.List;
+
+@Embeddable
+public class Answers {
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    @Where(clause = "deleted = false")
+    @OrderBy("id ASC")
+    private List<Answer> answers = new ArrayList<>();
+
+    public Answers() {
+    }
+
+    public Answers(List<Answer> answers) {
+        this.answers = answers;
+    }
+
+    public void add(Answer answer) {
+        answers.add(answer);
+    }
+
+    public void delete(User loginUser) throws CannotDeleteException {
+        for (Answer answer : answers) {
+            answer.delete(loginUser);
+        }
+    }
+
+    public List<DeleteHistory> deleteHistory() {
+        return DeleteHistory.of(answers);
+    }
+}
