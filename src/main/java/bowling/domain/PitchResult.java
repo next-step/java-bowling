4ea8b252defe055fe3927aff4184e03c3e49e614
seondongpyo <seondongpyo@gmail.com>
2021-09-10 09:15:09 +0900

    feat: 입력 받은 쓰러뜨린 볼링 핀의 숫자에 따라 결과를 저장한다

---
+++ src/main/java/bowling/domain/PitchResult.java
@@ -0,0 +1,49 @@
+package bowling.domain;
+
+import java.util.Objects;
+
+public class PitchResult {
+
+    private static final int MIN_COUNT = 0;
+    private static final int MAX_COUNT = 10;
+
+    private final int fallenPins;
+    private final boolean isSpare;
+
+    private PitchResult(int fallenPins, boolean isSpare) {
+        validateFallenPins(fallenPins);
+        this.fallenPins = fallenPins;
+        this.isSpare = isSpare;
+    }
+
+    public static PitchResult of(int fallenPins) {
+        return new PitchResult(fallenPins, false);
+    }
+
+    public static PitchResult spare(int fallenPins) {
+        if (fallenPins == MIN_COUNT) {
+            throw new IllegalArgumentException();
+        }
+        return new PitchResult(fallenPins, true);
+    }
+
+    private void validateFallenPins(int fallenPins) {
+        if (fallenPins < MIN_COUNT || fallenPins > MAX_COUNT) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PitchResult that = (PitchResult) o;
+        return fallenPins == that.fallenPins && isSpare == that.isSpare;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fallenPins, isSpare);
+    }
+
+}
---
+++ src/test/java/bowling/domain/PitchResultTest.java
@@ -0,0 +1,40 @@
+package bowling.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PitchResultTest {
+
+    @DisplayName("쓰러뜨린 핀의 개수로 투구 결과를 생성한다.")
+    @Test
+    void create() {
+        assertThat(PitchResult.of(5)).isEqualTo(PitchResult.of(5));
+    }
+
+    @DisplayName("쓰러뜨린 핀의 개수가 0 이상 10 이하여야 투구 결과를 생성할 수 있다.")
+    @ValueSource(ints = {-1, 11})
+    @ParameterizedTest
+    void invalid(int fallenPins) {
+        assertThatThrownBy(() -> PitchResult.of(fallenPins))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("스페어 결과를 생성한다.")
+    @Test
+    void spare() {
+        assertThat(PitchResult.spare(5)).isEqualTo(PitchResult.spare(5));
+    }
+
+    @DisplayName("스페어 결과 생성 시, 쓰러뜨린 핀의 개수는 0이 될 수 없다.")
+    @Test
+    void invalidSpare() {
+        assertThatThrownBy(() -> PitchResult.spare(0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
