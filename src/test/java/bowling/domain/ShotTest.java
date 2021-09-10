9fa027ee972bd06ac5cfef2b7b06d083e4d2ce0e
yook0328 <yook0328@gmail.com>
2021-09-10 13:02:27 +0900

    test: Shot 객체 테스트 작성.

---
+++ src/test/java/bowling/domain/ShotTest.java
@@ -0,0 +1,17 @@
+package bowling.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class ShotTest {
+    @DisplayName("유효하지 않는 샷 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 10})
+    public void invalidShotTest(int input) {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Shot(input));
+    }
+
+}
