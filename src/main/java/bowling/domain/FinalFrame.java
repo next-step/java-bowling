b3997b5ada4c1143ff6cd2691d8d0970353470f3
류성호 <>
2021-09-11 05:24:58 +0900

    feat : 마지막 프레임 기능 구현

--- src/main/java/bowling/domain/FinalFrame.java
+++ src/main/java/bowling/domain/FinalFrame.java
@@ -1,5 +1,22 @@
 package bowling.domain;
 
-public class FinalFrame {
+public class FinalFrame extends Frame{
 
+    private int finalScore;
+
+    public FinalFrame(int hitNumberOfPin) {
+        super(hitNumberOfPin);
+    }
+
+    public void finalBall(int hitNumberOfPin) {
+        validateFinalBall();
+        finalScore = hitNumberOfPin;
+        score.finalScore(hitNumberOfPin);
+    }
+
+    private void validateFinalBall() {
+        if (!isSpare() && !isStrike()) {
+            throw new RuntimeException("마지막 공은 스트라이크 또는 스페어인 경우 가능합니다.");
+        }
+    }
 }
--- src/test/java/bowling/FInalFrameTest.java
+++ src/test/java/bowling/FInalFrameTest.java
@@ -1,17 +1,48 @@
 package bowling;
 
 import bowling.domain.FinalFrame;
-import bowling.domain.Frame;
+import bowling.domain.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class FInalFrameTest {
-    Frame frame;
+    Player player;
+
+    @BeforeEach
+    void init() {
+        String name = "RSH";
+        player = new Player(name);
+        player.createEmptyFrame();
+
+        player.createEmptyFrame();
+    }
+
+    @Test
+    @DisplayName("마지막 프레임 마지막 공")
+    void finalBall() {
+        FinalFrame finalFrame = player.finalFrame(2);
+        finalFrame.secondBall(8);
+        finalFrame.finalBall(10);
+
+        assertThat(finalFrame.scores().contains("2") &&
+                finalFrame.scores().contains("8") &&
+                finalFrame.scores().contains("10")).isTrue();
+    }
+
+    @Test
+    @DisplayName("마지막 프레임 마지막 공 에러")
+    void finalBallValidate() {
+        FinalFrame finalFrame = player.finalFrame(2);
+        finalFrame.secondBall(3);
+
+        assertThatThrownBy(() ->
+                finalFrame.finalBall(10))
+                .isInstanceOf(RuntimeException.class);
+    }
 
 
 }
