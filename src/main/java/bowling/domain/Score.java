e9aa197a6a390b029def9da67969722f0abc301d
류성호 <>
2021-09-11 05:25:30 +0900

    feat : 마지막 프레임 점수 추가

--- src/main/java/bowling/domain/Score.java
+++ src/main/java/bowling/domain/Score.java
@@ -2,16 +2,21 @@ package bowling.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Score {
     private static final int MAX_PIN_NUMBER = 10;
+    private static final int GUTTER_PIN_NUMBER = 0;
     private static final int FIRST_INDEX = 0;
     private static final int SECOND_INDEX = 1;
-    private static final int SCORES_MAX_INDEX = 2;
+    private static final int FINAL_INDEX = 2;
+    private static final int SCORES_SECOND_SIZE = 2;
 
     private List<Integer> scores = new ArrayList<>();
 
     public Score(int firstBall) {
+        validateNumberOfPin(firstBall);
+
         scores.add(firstBall);
     }
 
@@ -28,14 +33,60 @@ public class Score {
     }
 
     public int secondScore() {
-        if (scores.size() < SCORES_MAX_INDEX) {
-            throw new IllegalArgumentException("두번째 점수 입력해주세요.");
-        }
+        validateExistSecondScore();
 
         return scores.get(SECOND_INDEX);
     }
 
+    public void finalScore(int hitNumberOfPin) {
+        validateNumberOfPin(hitNumberOfPin);
+        scores.add(FINAL_INDEX, hitNumberOfPin);
+    }
+
+    private void validateNumberOfPin(int hitNumberOfPin) {
+        if (hitNumberOfPin > MAX_PIN_NUMBER) {
+            throw new IllegalArgumentException("핀의 최고 갯수는 10개 입니다.");
+        }
+    }
+
+    private void validateExistSecondScore() {
+        if (scores.size() < SCORES_SECOND_SIZE) {
+            throw new IllegalArgumentException("두번째 점수 입력해주세요.");
+        }
+    }
+
     public List<Integer> scores() {
         return scores;
+    }
+
+    public Status frameStatus() {
+        if (scores.get(FIRST_INDEX) == MAX_PIN_NUMBER) {
+            return Status.STRIKE;
+        }
+
+        validateExistSecondScore();
+
+        if (scores.get(FIRST_INDEX) + scores.get(SECOND_INDEX) == MAX_PIN_NUMBER) {
+            return Status.SPARE;
+        }
+
+        if (scores.get(FIRST_INDEX) == GUTTER_PIN_NUMBER || scores.get(SECOND_INDEX) == GUTTER_PIN_NUMBER) {
+            return Status.GUTTER;
+        }
+
+        return Status.MISS;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Score score = (Score) o;
+        return Objects.equals(scores, score.scores);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(scores);
     }
 }
