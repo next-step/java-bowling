bae76f6717030f84e342b6539699f77afa4581b8
Gyeongseo Woo <ugs1575@gmail.com>
2021-09-10 15:13:03 +0900

    refactor: 공통 예외처리 클래스 분리

---
+++ src/main/java/bowling/BowlingValidator.java
@@ -0,0 +1,14 @@
+package bowling;
+
+public class BowlingValidator {
+    public static void isBlank(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("빈값입니다.다시 입력해주세요.");
+        }
+    }
+
+    public static int changeToInt(String pinCount) {
+        return Integer.parseInt(pinCount.trim());
+    }
+
+}
