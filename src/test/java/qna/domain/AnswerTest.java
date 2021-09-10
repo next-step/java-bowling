8f36863ace603b2cb9419df583d4f1de31bd378e
harris <harrisleesh@gmail.com>
2021-09-10 10:09:03 +0900

    test : Create answer delete test and validate exception

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,26 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("Answer delete check")
+    void delete() throws CannotDeleteException {
+        A1.delete(UserTest.JAVAJIGI);
+        assertTrue(A1.isDeleted());
+    }
+
+    @Test
+    @DisplayName("delete validate user and throw exception")
+    void validate(){
+        assertThrows(CannotDeleteException.class, () -> A1.delete(UserTest.SANJIGI));
+    }
 }
