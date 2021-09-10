c1708bc8a0b3ac85c9bf99fde077cbed0e8c1ae0
yook0328 <yook0328@gmail.com>
2021-09-10 13:12:10 +0900

    feat: 메인 메서드 작성.

---
+++ src/main/java/bowling/Application.java
@@ -0,0 +1,11 @@
+package bowling;
+
+import bowling.controller.GameController;
+
+public class Application {
+    public static void main(String... args) {
+        GameController gameController = new GameController();
+
+        gameController.run();
+    }
+}
