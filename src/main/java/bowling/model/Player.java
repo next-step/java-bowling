0a40620e359db35d587a5b03034081765a39a5f7
YooInKeun <keun0390@naver.com>
2021-09-10 22:52:04 +0900

    feat: 플레이어 생성 및 이름 패턴 유효성 검사 추가

---
+++ src/main/java/bowling/model/Player.java
@@ -0,0 +1,23 @@
+package bowling.model;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Player {
+    private static final int NAME_LENGTH = 3;
+    private static final Pattern NAME_PATTERN = Pattern.compile(String.format("^[a-zA-Z]{%d}$", NAME_LENGTH));
+
+    private final String name;
+
+    public Player(String name) {
+        validateNamePattern(name);
+        this.name = name;
+    }
+
+    private void validateNamePattern(String name) {
+        Matcher matcher = NAME_PATTERN.matcher(name);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException(String.format("플레이어 이름은 알파벳 %d글자로 이루어져야 합니다.", NAME_LENGTH));
+        }
+    }
+}
