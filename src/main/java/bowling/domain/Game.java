e6ab9c72bf98b03d7774aaff2ab560ea927885ec
yook0328 <yook0328@gmail.com>
2021-09-10 17:23:14 +0900

    refactor: frame을 다가지고 시작하던 방식에서 하나씩 ㅅ추가하는 방식으로 수정., 이름 값을 직접 가지지 않고  User객체가 가지도록 수정.

--- src/main/java/bowling/domain/Game.java
+++ src/main/java/bowling/domain/Game.java
@@ -1,34 +1,20 @@
 package bowling.domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Game {
-    private static final Pattern NAME_VALID_PATTERN = Pattern.compile("^[a-zA-Z]{1,3}$");
+    private static final int MAX_FRAME_SIZE = 10;
+    private static final int LAST_FRAME_INDEX = 9;
 
-    private final String username;
-    private List<Frame> frames;
 
-    public Game(String username) {
-        Matcher nameMatcher = NAME_VALID_PATTERN.matcher(username);
-        if (!nameMatcher.matches()) {
-            throw new IllegalArgumentException("잘못된 유형의 이름이 들어왔습니다.");
-        }
+    private final User user;
+    private List<Frame> frames = new ArrayList<>();
 
-        this.username = username;
-
-        initFrames();
-    }
-
-    public void initFrames() {
-        this.frames = IntStream.range(0, 9)
-                .mapToObj((index) -> new NormalFrame())
-                .collect(Collectors.toList());
-        this.frames.add(new LastFrame());
+    public Game(User user) {
+        this.user = user;
+        frames.add(new NormalFrame());
     }
 
     public void play(Shot shot) {
@@ -36,17 +22,34 @@ public class Game {
             throw new IllegalStateException("Game is already finished");
         }
 
-        Frame currentFrame = frames.stream().filter((frame -> !frame.isFinished()))
-                .findFirst()
-                .orElseThrow(() -> new IllegalStateException("All frame is finished"));
-
+        int currentIndex = frames.size() - 1;
+        Frame currentFrame = frames.get(currentIndex);
         currentFrame.playShot(shot);
+
+        prepareNext(currentFrame);
+    }
+
+    public void prepareNext(Frame currentFrame) {
+        if (isFinished()) {
+            return;
+        }
+
+        if (!currentFrame.isFinished()) {
+            return;
+        }
+
+        if (frames.size() < LAST_FRAME_INDEX) {
+            frames.add(new NormalFrame());
+            return;
+        }
+
+        frames.add(new LastFrame());
     }
 
     public boolean isFinished() {
-        return !this.frames
-                .stream()
-                .anyMatch((frame) -> !frame.isFinished());
+        return this.frames.size() == MAX_FRAME_SIZE
+                && this.frames.get(LAST_FRAME_INDEX)
+                .isFinished();
     }
 
     public boolean isNotFinished() {
@@ -54,14 +57,22 @@ public class Game {
     }
 
     public int getCurrentFrameIndex() {
-        return (int)frames.stream().filter((frame -> frame.isFinished())).count();
+        return frames.size();
+    }
+
+    public int getRemainFrameCount() {
+        return MAX_FRAME_SIZE - frames.size();
     }
 
     public List<Frame> getFrames() {
         return Collections.unmodifiableList(frames);
     }
 
-    public String getUsername() {
-        return username;
+    public User getUser() {
+        return user;
+    }
+
+    public static Game of(User user) {
+        return new Game(user);
     }
 }
