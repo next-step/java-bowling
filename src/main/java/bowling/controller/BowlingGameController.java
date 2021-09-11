77effbc344603ea9eaa51881dee469559beefec1
ChangSubKwak <paradise1732@gmail.com>
2021-09-11 09:45:53 +0900

    fix: 누적점수 결과를 외부에서 관리하도록 수정

--- src/main/java/bowling/controller/BowlingGameController.java
+++ src/main/java/bowling/controller/BowlingGameController.java
@@ -1,11 +1,12 @@
 package bowling.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import bowling.domain.Player;
 import bowling.domain.Results;
 import bowling.domain.frame.Frames;
-import bowling.domain.score.AccumulatedScores;
+import bowling.domain.score.PureScores;
 import bowling.domain.score.Score;
 import bowling.view.InputView;
 import bowling.view.ResultView;
@@ -27,13 +28,16 @@ public class BowlingGameController {
     private static void executeGame(final Frames frames, final String playerName) {
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
-        AccumulatedScores accumulatedScores = AccumulatedScores.from(frames);
+        List<Integer> cumulatedScores = new ArrayList<>();
+        PureScores pureScores = PureScores.from(frames, cumulatedScores);
+
         while (!frames.isFinish()) {
             int downPinNumber = inputView.inputNFrameThrow(frames.frameNumber());
-            accumulatedScores.addScore(Score.from(downPinNumber));
+            pureScores.addScore(Score.from(downPinNumber));
             frames.throwBalls(downPinNumber);
-            resultView.outputScores(playerName, Results.from(frames).results());
-            resultView.outputCumulativeScores(accumulatedScores.getCumulativeScores(frames));
+            resultView.outputScores(playerName, frames.results());
+            cumulatedScores = pureScores.getCumulativeScores(frames, cumulatedScores);
+            resultView.outputCumulativeScores(cumulatedScores);
         }
     }
 }
