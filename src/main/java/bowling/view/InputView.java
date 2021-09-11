8240645e7d1491374a59324926abf0fd1a071d0b
yook0328 <yook0328@gmail.com>
2021-09-11 22:59:00 +0900

    refactor: 사용자 갯수 추가 및 텍스트 라벨 일부 수정.

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -1,17 +1,24 @@
 package bowling.view;
 
+import bowling.domain.User;
+
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    public static String extractUsername() {
-        System.out.print("플레이어 이름은(3 english letters)?: ");
+    public static String extractUsername(int index) {
+        System.out.print("플레이어" + index + "의 이름은(3 english letters)?: ");
         return SCANNER.nextLine();
     }
 
-    public static int extractFrameResult(int frameIndex) {
-        System.out.println((frameIndex) + "프레임 투구 : ");
+    public static int extractFrameResult(User user) {
+        System.out.println(user + "'s turn : ");
+        return Integer.parseInt(SCANNER.nextLine());
+    }
+
+    public static int extractPlayerCount() {
+        System.out.print("How many people? ");
         return Integer.parseInt(SCANNER.nextLine());
     }
 }
