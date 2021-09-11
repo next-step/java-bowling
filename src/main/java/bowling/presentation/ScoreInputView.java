d0c746f4a269e6b648424a9a0d95a86c997eee5e
sangnyyy <dltkdals2202@gmail.com>
2021-09-12 00:14:57 +0900

    feat : 점수를 입력할 수 있는 View 생성

--- src/main/java/bowling/presentation/ScoreInputView.java
+++ src/main/java/bowling/presentation/ScoreInputView.java
@@ -1,2 +1,21 @@
-package bowling.presentation;public class ScoreInputView {
+package bowling.presentation;
+
+import java.util.Scanner;
+
+public class ScoreInputView {
+
+    private static final String QUESTION = "%d프레임 투구 : ";
+
+    private ScoreInputView() {
+    }
+
+    public static ScoreInputView create() {
+        return new ScoreInputView();
+    }
+
+    public int input(int round) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf(QUESTION, round);
+        return sc.nextInt();
+    }
 }
