a51aa15104f643444b5f58fab24209aef6b96637
sangnyyy <dltkdals2202@gmail.com>
2021-09-11 10:25:35 +0900

    feat : 플레이어 이름을 입력받기 위한 View 생성

---
+++ src/main/java/bowling/presentation/PlayerInputView.java
@@ -0,0 +1,21 @@
+package bowling.presentation;
+
+import java.util.Scanner;
+
+public class PlayerInputView {
+
+    private static final String QUESTION = "플레이어 이름은(3 english letters)?: ";
+
+    private PlayerInputView() {
+    }
+
+    public static PlayerInputView create() {
+        return new PlayerInputView();
+    }
+
+    public String input() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(QUESTION);
+        return sc.nextLine();
+    }
+}
