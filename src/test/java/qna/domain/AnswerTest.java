1a356efb17102e11dd5d8cb01c4f20b2424d23c9
YuHyeon <klyhyeon@globalorder.co.kr>
2021-09-12 01:30:08 +0900

    test: answer isOwner 테스트 작성

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,17 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    public void isOwner_테스트() {
+        Question q = new Question();
+        Answer answer = new Answer(1L, new User(2L, "1", "12","name","email"), q, "content");
+        assertThat(answer.isOwner(new User(1L, "2", "23", "name1", "email1"))).isFalse();
+    }
 }
