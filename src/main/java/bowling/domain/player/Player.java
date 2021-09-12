8a1f7743eab6384bb5d44036e802a9c958940b67
ChangSubKwak <paradise1732@gmail.com>
2021-09-12 22:56:54 +0900

    refactor: 패키지 이동

--- src/main/java/bowling/domain/player/Player.java
+++ src/main/java/bowling/domain/player/Player.java
@@ -1,6 +1,9 @@
 package bowling.domain;
 
+import java.util.List;
 import java.util.Objects;
+
+import bowling.domain.frame.Frames;
 
 public class Player {
     private static final String PLAYER_NAME_NULL_OR_EMPTY_EXCEPTION_STATEMENT = "플레이어 이름이 널이거나 빈 문자입니다";
@@ -8,14 +11,16 @@ public class Player {
     private static final int PLAYER_NAME_LENGTH = 3;
 
     private final String name;
+    private final Frames frames;
 
-    private Player(String name) {
+    private Player(String name, Frames frames) {
         validate(name);
         this.name = name;
+        this.frames = frames;
     }
 
-    public static Player from(String name) {
-        return new Player(name);
+    public static Player from(String name, Frames frames) {
+        return new Player(name, frames);
     }
 
     private void validate(String name) {
@@ -29,6 +34,14 @@ public class Player {
     }
 
     public String name() {
-        return this.name;
+        return name;
+    }
+
+    public Frames frames() {
+        return frames;
+    }
+
+    public List<String> results() {
+        return Results.from(frames).results();
     }
 }
--- src/test/java/bowling/domain/player/PlayerTest.java
+++ src/test/java/bowling/domain/player/PlayerTest.java
@@ -2,31 +2,35 @@ package bowling.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import bowling.domain.frame.Frames;
 
 class PlayerTest {
     @DisplayName("3자리 문자열을 입력 받으면, Player객체를 생성한다")
     @Test
     void createTest() {
-        assertThat(Player.from("KCS")).isInstanceOf(Player.class);
+        assertThat(Player.from("KCS", Frames.from(new ArrayList<>()))).isInstanceOf(Player.class);
     }
 
     @DisplayName("2자리 문자열을 입력 받으면, 예외를 던진다")
     @Test
     void exceptionTest1() {
-        assertThatThrownBy(() -> Player.from("KC")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Player.from("KC", Frames.from(new ArrayList<>()))).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("빈문자를 입력 받으면, 예외를 던진다")
     @Test
     void exceptionTest2() {
-        assertThatThrownBy(() -> Player.from("")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Player.from("", Frames.from(new ArrayList<>()))).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("널을 입력 받으면, 예외를 던진다")
     @Test
     void exceptionTest3() {
-        assertThatThrownBy(() -> Player.from(null)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Player.from(null, Frames.from(new ArrayList<>()))).isInstanceOf(IllegalArgumentException.class);
     }
 }
