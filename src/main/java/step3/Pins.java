536d6c757531f3b501a38a436720196ab6001647
jaeyoungjang2 <lion85488@gmail.com>
2021-09-12 20:00:43 +0900

    feat: 볼링핀 클래스 구현

--- src/main/java/step3/Pins.java
+++ src/main/java/step3/Pins.java
@@ -1,2 +1,47 @@
-package step3;public class Pins {
+package step3;
+
+import java.util.Objects;
+import step3.exceptions.PinNumberExecption;
+
+public class Pins {
+    private static final int MAX = 10;
+    private static final int MIN = 0;
+
+    private int remainPins;
+
+    public Pins() {
+        this(MAX);
+    }
+
+    public Pins(int numOfPin) {
+        this.remainPins = numOfPin;
+    }
+
+    public void bowl(int falledPins) {
+        this.remainPins -= falledPins;
+        validPin();
+    }
+
+    private void validPin() {
+        if (remainPins < MIN || remainPins > MAX) {
+            throw new PinNumberExecption();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pins pins = (Pins) o;
+        return remainPins == pins.remainPins;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(remainPins);
+    }
 }
