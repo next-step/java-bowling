2a5521c26b846ed0c7d2f5b8a0259b85dcd254a1
jaeyoungjang2 <lion85488@gmail.com>
2021-09-12 21:15:25 +0900

    feat: FirstBowl 클래스 구현

--- src/main/java/step3/FirstBowl.java
+++ src/main/java/step3/FirstBowl.java
@@ -1,2 +1,16 @@
-package step3;public class FirstBowl {
+package step3;
+
+public class FirstBowl implements State {
+    private final int pins;
+
+    public FirstBowl(int firstFalledPins) {
+        pins = firstFalledPins;
+    }
+
+    public State bowl(int secondFalledPins) {
+        if (pins + secondFalledPins == 10) {
+            return new Spair();
+        }
+        return new Miss(pins, secondFalledPins);
+    }
 }
