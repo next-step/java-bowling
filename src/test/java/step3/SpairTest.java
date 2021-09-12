a66b24a86ecbb20f75669f21f8c6e5a5be33f89b
jaeyoungjang2 <lion85488@gmail.com>
2021-09-12 21:38:37 +0900

    test: Frame을 처음 생성했을 때 state 상태는 Ready와 같다.

---
+++ src/test/java/step3/SpairTest.java
@@ -0,0 +1,17 @@
+package step3;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SpairTest {
+    @DisplayName("Spair 상태에서는 추가적으로 공을 던질 수 없습니다.")
+    @Test
+    void spairBowlTest() {
+        Spair spair = new Spair();
+        Assertions.assertThatThrownBy(() -> {
+            spair.bowl(4);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
