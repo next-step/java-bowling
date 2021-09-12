7f2c50be7e2cfa39b16007a29c2cbea665fd5a35
iiaii <jhun9409@naver.com>
2021-09-12 21:37:15 +0900

    [step2] feat: 뷰에서 사용할 볼링 투구 기호 PitchSign enum으로 분리

--- src/main/java/bowling/view/FramesConsoleOutput.java
+++ src/main/java/bowling/view/FramesConsoleOutput.java
@@ -9,12 +9,7 @@ import java.util.stream.Stream;
 
 public class FramesConsoleOutput {
 
-    private static final int MIN = 0;
-    private static final int MAX = 10;
     private static final String FRAMES_TOP = "| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |\n";
-    private static final String STRIKE = "X";
-    private static final String SPARE = "/";
-    private static final String GUTTER = "-";
     private static final String FRAME_DELIMITER = " |  ";
     private static final String PITCH_VALUE_DELIMITER = "|";
     private static final String PREFIX = "|  ";
@@ -39,37 +34,8 @@ public class FramesConsoleOutput {
 
     private static String convertToSigns(final List<Integer> pitchValues) {
         return String.format("%-3s", IntStream.range(0, pitchValues.size())
-                .mapToObj(i -> convertToSign(pitchValues, i))
+                .mapToObj(i -> PitchSign.convertToSign(pitchValues, i))
                 .collect(Collectors.joining(PITCH_VALUE_DELIMITER)));
-    }
-
-    private static String convertToSign(final List<Integer> pitchValues, final int i) {
-        int pitchValue = pitchValues.get(i);
-        if (isGutter(pitchValue)) {
-            return GUTTER;
-        }
-        if (isSpare(pitchValues, i)) {
-            return SPARE;
-        }
-        if (isStrike(pitchValue)) {
-            return STRIKE;
-        }
-        return String.valueOf(pitchValue);
-    }
-
-    private static boolean isGutter(final int pitchValue) {
-        return pitchValue == MIN;
-    }
-
-    private static boolean isSpare(final List<Integer> pitchValues, final int i) {
-        if (i == 0) {
-            return false;
-        }
-        return pitchValues.get(i - 1) + pitchValues.get(i) == MAX;
-    }
-
-    private static boolean isStrike(final int pitchValue) {
-        return pitchValue == MAX;
     }
 
 }
