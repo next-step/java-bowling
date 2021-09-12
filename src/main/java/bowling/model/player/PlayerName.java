1a0ad21a77f9eba2205d97ed38c71135603cd772
YooInKeun <keun0390@naver.com>
2021-09-13 00:17:37 +0900

    refactor: Player 객체가 번호, 이름을 갖도록 수정

---
+++ src/main/java/bowling/model/player/PlayerName.java
@@ -0,0 +1,28 @@
+package bowling.model.player;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PlayerName {
+    private static final int NAME_LENGTH = 3;
+    private static final Pattern NAME_PATTERN = Pattern.compile(String.format("^[a-zA-Z]{%d}$", NAME_LENGTH));
+
+    private final String name;
+
+    public PlayerName(String name) {
+        validateNamePattern(name);
+
+        this.name = name;
+    }
+
+    private void validateNamePattern(String name) {
+        Matcher matcher = NAME_PATTERN.matcher(name);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException(String.format("플레이어 이름은 알파벳 %d글자로 이루어져야 합니다.", NAME_LENGTH));
+        }
+    }
+
+    public String name() {
+        return name;
+    }
+}
