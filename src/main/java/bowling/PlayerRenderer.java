67e6732ffcc1dd5485d34da1358d3a01eeb1d712
채윤병 <ybchae0801@naver.com>
2021-09-13 07:54:29 +0900

    chore: 렌더링 포맷 상수화

--- src/main/java/bowling/PlayerRenderer.java
+++ src/main/java/bowling/PlayerRenderer.java
@@ -8,21 +8,25 @@ import java.util.stream.Collectors;
 
 public class PlayerRenderer {
 
+    private static final String PLAYER_NAME_RENDERING_FORMAT = "  %s ";
+    private static final String PLAYER_TOTAL_STATE_RENDERING_FORMAT = "|%s|%s|";
+    private static final String FRAME_DELIMITER = "|";
+
     private final String playerName;
     private final List<FrameStateRenderer> frameStateRenderers;
 
     public PlayerRenderer(PlayerName playerName, Frames frames) {
-        this.playerName = String.format("  %s ", playerName);
+        this.playerName = String.format(PLAYER_NAME_RENDERING_FORMAT, playerName);
         this.frameStateRenderers = frames.toFrameStateRenderers();
     }
 
     public String render() {
-        return String.format("|%s|%s|", playerName, renderStates());
+        return String.format(PLAYER_TOTAL_STATE_RENDERING_FORMAT, playerName, renderStates());
     }
 
     private String renderStates() {
         return frameStateRenderers.stream()
                 .map(FrameStateRenderer::renderState)
-                .collect(Collectors.joining("|"));
+                .collect(Collectors.joining(FRAME_DELIMITER));
     }
 }
