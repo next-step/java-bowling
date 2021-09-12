f8ab8d21d8fede2337e3a88d52e81c5bbf6b91a1
jaeyoungjang2 <lion85488@gmail.com>
2021-09-12 19:47:05 +0900

    feat: 초기화

--- src/main/java/step3/exceptions/UserNameLengthError.java
+++
@@ -1,8 +0,0 @@
-package step3.exceptions;
-
-public class UserNameLengthError extends IllegalArgumentException {
-    private static final String PLAYER_NAME_LENGTH_ERROR_MESSAGE = "플레이어이의 이름은 3글자로 이루어져야 합니다.";
-    public UserNameLengthError() {
-        super(PLAYER_NAME_LENGTH_ERROR_MESSAGE);
-    }
-}
