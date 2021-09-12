6bff450c5e928af02375395685cc7c3857f708cc
ChangSubKwak <paradise1732@gmail.com>
2021-09-12 23:12:20 +0900

    fix: 여러 플레이어에 대하여 입력 처리를 위해 수정

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -1,11 +1,17 @@
 package bowling.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
-    private static final String INPUT_PLAYER_NAME = "플레이어 이름은(3 english letters)?: ";
-    private static final String INPUT_N_FRAME_THROW = "%d프레임 투구: ";
+
+    private static final String INPUT_HOW_MANY_PEOPLE = "How many people? ";
+    private static final String INPUT_PLAYER_NAME = "플레이어 %d의 이름은(3 english letters)?: ";
+    // private static final String INPUT_N_FRAME_THROW = "%d프레임 투구: ";
+    private static final String INPUT_PLAYER_TURN = "%s's turn : ";
 
     private String inputStringValue() {
         return scanner.nextLine();
@@ -17,14 +23,25 @@ public class InputView {
         return intValue;
     }
 
-    public String inputPlayerName() {
-        System.out.println(INPUT_PLAYER_NAME);
+    public List<String> inputPlayerNames() {
+        System.out.print(INPUT_HOW_MANY_PEOPLE);
+        int numberOfPlayers = inputIntValue();
+
+        List<String> playerNames = new ArrayList<>();
+        IntStream.rangeClosed(1, numberOfPlayers)
+            .forEach(i -> playerNames.add(inputPlayerName(i)));
+
+        return playerNames;
+    }
+
+    public String inputPlayerName(int orderNumber) {
+        System.out.printf(INPUT_PLAYER_NAME, orderNumber);
         return inputStringValue();
     }
 
-    public int inputNFrameThrow(int frameNumber) {
+    public int inputPlayerTurnPinNumber(String playerName) {
         System.out.print(System.lineSeparator());
-        System.out.printf(INPUT_N_FRAME_THROW, frameNumber);
+        System.out.printf(INPUT_PLAYER_TURN, playerName);
         return inputIntValue();
     }
 
