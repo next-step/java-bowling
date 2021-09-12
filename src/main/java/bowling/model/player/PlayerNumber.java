7e4fb0f6058022de9d1d5ec95f110bf141d5385e
YooInKeun <keun0390@naver.com>
2021-09-13 00:23:22 +0900

    test: PlayerNumber 생성 기능에 대한 테스트 코드 추가

--- src/main/java/bowling/model/player/PlayerNumber.java
+++ src/main/java/bowling/model/player/PlayerNumber.java
@@ -19,6 +19,10 @@ public class PlayerNumber {
         }
     }
 
+    public int value() {
+        return number;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
