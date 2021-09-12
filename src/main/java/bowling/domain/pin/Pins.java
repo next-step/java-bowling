10cf63f9777822bf3289dd9714af3be868dae77a
shineoov <shineoov88@gmail.com>
2021-09-12 14:13:36 +0900

    refactor: 매직 넘버 상수로 분리 - 피드백 반영

--- src/main/java/bowling/domain/pin/Pins.java
+++ src/main/java/bowling/domain/pin/Pins.java
@@ -10,6 +10,7 @@ public final class Pins {
     private static final int FIRST_PIN_INDEX = 0;
     private static final int SECOND_PIN_INDEX = 1;
     private static final int THIRD_PIN_INDEX = 2;
+    private static final int MIN_PINS_SIZE =2;
     private static final int MAX_PINS_SIZE = 3;
     private static final int MAX_TWO_PINS_SUM_IF_NOT_STRIKE = 10;
 
@@ -34,10 +35,6 @@ public final class Pins {
         pins.add(Pin.valueOf(knockDownNumber));
     }
 
-    public boolean isEmpty() {
-        return pins.isEmpty();
-    }
-
     public boolean isStrike() {
         if (pins.isEmpty()) {
             return false;
@@ -46,14 +43,14 @@ public final class Pins {
     }
 
     public boolean isSpare() {
-        if (pins.size() < 2) {
+        if (pins.size() < MIN_PINS_SIZE) {
             return false;
         }
         return firstPin().sum(secondPin()) == MAX_TWO_PINS_SUM_IF_NOT_STRIKE;
     }
 
     public boolean isSecondPinWrong() {
-        if (pins.size() < 2) {
+        if (pins.size() < MIN_PINS_SIZE) {
             return false;
         }
         if (isStrike()) {
