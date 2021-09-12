2dc0b6ceae5adb552f403683f325913f316e70a4
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-13 00:27:23 +0900

    refactor: 매직넘버 상수 변환

--- src/main/java/bowling/domain/bowling/Pin.java
+++ src/main/java/bowling/domain/bowling/Pin.java
@@ -5,11 +5,14 @@ public class Pin {
     private static final String INVALID_PIN_VALUE_ERROR_MESSAGE = "핀은 0~10 사이의 값만 저장할 수 있다.";
     private static final String INVALID_SECOND_PIN_ERROR_MESSAGE = "첫번째 핀이 쓰러뜨리고 남은 핀 개수만 저장할 수 있다.";
 
+    private static final int PIN_MIN_VALUE = 0;
+    private static final int PIN_MAX_VALUE = 10;
+
     private final int first;
     private final int second;
 
     private Pin(int first) {
-        this(first, 0);
+        this(first, PIN_MIN_VALUE);
     }
 
     private Pin(int first, int second) {
@@ -31,13 +34,13 @@ public class Pin {
     }
 
     private static void checkPinInputValue(int pin) {
-        if (pin < 0 || pin > 10) {
+        if (pin < PIN_MIN_VALUE || pin > PIN_MAX_VALUE) {
             throw new RuntimeException(INVALID_PIN_VALUE_ERROR_MESSAGE);
         }
     }
 
     private static void checkSecondPin(Pin first, int second) {
-        if (second > 10 - first.first) {
+        if (second > Math.subtractExact(PIN_MAX_VALUE, first.first)) {
             throw new RuntimeException(INVALID_SECOND_PIN_ERROR_MESSAGE);
         }
     }
