bed5d24dbde6a8ca85289d5d6fb51bf542ff19a8
iiaii <jhun9409@naver.com>
2021-09-13 00:41:50 +0900

    [step3] feat: 프레임별 점수 제공하도록 뷰, 메인 수정

--- src/main/java/bowling/BowlingGame.java
+++ src/main/java/bowling/BowlingGame.java
@@ -3,6 +3,7 @@ package bowling;
 import bowling.domain.Frames;
 import bowling.domain.Pitch;
 import bowling.domain.Player;
+import bowling.dto.FrameStatusDto;
 import bowling.view.FramesConsoleOutput;
 import bowling.view.PitchConsoleInput;
 import bowling.view.PlayerConsoleInput;
@@ -12,11 +13,15 @@ public class BowlingGame {
     public static void main(String[] args) {
         Player player = new Player(PlayerConsoleInput.askName());
         Frames frames = new Frames();
-        FramesConsoleOutput.print(player.name(), frames.allFramePitchValues());
+        FramesConsoleOutput.print(player.name(), toFrameStatusDto(frames));
         do {
             frames.addPitch(new Pitch(PitchConsoleInput.askPitch(frames.currentFrameNumber())));
-            FramesConsoleOutput.print(player.name(), frames.allFramePitchValues());
+            FramesConsoleOutput.print(player.name(), toFrameStatusDto(frames));
         } while (!frames.isEnd());
+    }
+
+    private static FrameStatusDto toFrameStatusDto(final Frames frames) {
+        return new FrameStatusDto(frames.allFramePitchValues(), frames.scores());
     }
 
 }
--- src/main/java/bowling/view/FramesConsoleOutput.java
+++ src/main/java/bowling/view/FramesConsoleOutput.java
@@ -1,5 +1,7 @@
 package bowling.view;
 
+import bowling.dto.FrameStatusDto;
+
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -9,21 +11,33 @@ import java.util.stream.Stream;
 
 public class FramesConsoleOutput {
 
-    private static final String FRAMES_TOP = "| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |\n";
+    private static final String FRAMES_TOP = "| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |";
+    private static final String FRAME_FORMAT = "%-3s";
     private static final String FRAME_DELIMITER = " |  ";
     private static final String PITCH_VALUE_DELIMITER = "|";
-    private static final String PREFIX = "|  ";
-    private static final String SUFFIX = " |\n";
+    private static final String PITCHES_PREFIX = "|  ";
+    private static final String PITCHES_SUFFIX = " |";
+    private static final String SCORES_PREFIX = "|      |  ";
+    private static final String SCORES_SUFFIX = " |\n";
 
-    public static void print(final String playerName, final List<List<Integer>> allFramePitchValues) {
-        System.out.println(getFrames(playerName, allFramePitchValues));
+    public static void print(final String playerName, final FrameStatusDto frameStatusDto) {
+        System.out.println(FRAMES_TOP);
+        System.out.println(getFramePitches(playerName, frameStatusDto.getAllFramePitchValues()));
+        System.out.println(getFrameScores(frameStatusDto.getScores()));
     }
 
-    private static String getFrames(final String playerName, final List<List<Integer>> allFramePitchValues) {
+    private static String getFrameScores(final List<Integer> scores) {
+        return scores.stream()
+                .map(score -> score == 0 ? "" : String.valueOf(score))
+                .map(score -> String.format(FRAME_FORMAT, score))
+                .collect(Collectors.joining(FRAME_DELIMITER, SCORES_PREFIX, SCORES_SUFFIX));
+    }
+
+    private static String getFramePitches(final String playerName, final List<List<Integer>> allFramePitchValues) {
         return Stream.of(Collections.singletonList(playerName), allFramePitchValues(allFramePitchValues))
                 .flatMap(Collection::stream)
                 .map(String::valueOf)
-                .collect(Collectors.joining(FRAME_DELIMITER, FRAMES_TOP + PREFIX, SUFFIX));
+                .collect(Collectors.joining(FRAME_DELIMITER, PITCHES_PREFIX, PITCHES_SUFFIX));
     }
 
     private static List<String> allFramePitchValues(final List<List<Integer>> allFramePitchValues) {
@@ -33,7 +47,7 @@ public class FramesConsoleOutput {
     }
 
     private static String convertToSigns(final List<Integer> pitchValues) {
-        return String.format("%-3s", IntStream.range(0, pitchValues.size())
+        return String.format(FRAME_FORMAT, IntStream.range(0, pitchValues.size())
                 .mapToObj(i -> PitchSign.convertToSign(pitchValues, i))
                 .collect(Collectors.joining(PITCH_VALUE_DELIMITER)));
     }
