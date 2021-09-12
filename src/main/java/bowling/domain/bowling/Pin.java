4c0af3843dcaf835c9a594649afe83fca8e22666
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-13 00:25:16 +0900

    feat: 두번째 핀 저장시 남은 핀 이상 들어오면 Exception 구현 및 테스트

--- src/main/java/bowling/domain/bowling/Pin.java
+++ src/main/java/bowling/domain/bowling/Pin.java
@@ -2,12 +2,19 @@ package bowling.domain.bowling;
 
 public class Pin {
 
-    private static final String INVALIE_PIN_VALUE_ERROR_MESSAGE = "핀은 0~10 사이의 값만 저장할 수 있다.";
+    private static final String INVALID_PIN_VALUE_ERROR_MESSAGE = "핀은 0~10 사이의 값만 저장할 수 있다.";
+    private static final String INVALID_SECOND_PIN_ERROR_MESSAGE = "첫번째 핀이 쓰러뜨리고 남은 핀 개수만 저장할 수 있다.";
 
-    private final int pin;
+    private final int first;
+    private final int second;
 
-    private Pin(int pin) {
-        this.pin = pin;
+    private Pin(int first) {
+        this(first, 0);
+    }
+
+    private Pin(int first, int second) {
+        this.first = first;
+        this.second = second;
     }
 
     public static Pin of(int pin) {
@@ -16,10 +23,24 @@ public class Pin {
         return new Pin(pin);
     }
 
+    public static Pin from(Pin input, int second) {
+        checkPinInputValue(second);
+        checkSecondPin(input, second);
+
+        return new Pin(input.first, second);
+    }
+
     private static void checkPinInputValue(int pin) {
         if (pin < 0 || pin > 10) {
-            throw new RuntimeException(INVALIE_PIN_VALUE_ERROR_MESSAGE);
+            throw new RuntimeException(INVALID_PIN_VALUE_ERROR_MESSAGE);
         }
     }
+
+    private static void checkSecondPin(Pin first, int second) {
+        if (second > 10 - first.first) {
+            throw new RuntimeException(INVALID_SECOND_PIN_ERROR_MESSAGE);
+        }
+    }
+
 
 }
--- src/test/java/bowling/domain/bowling/PinTest.java
+++ src/test/java/bowling/domain/bowling/PinTest.java
@@ -3,6 +3,7 @@ package bowling.domain.bowling;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -17,6 +18,20 @@ class PinTest {
         assertThatExceptionOfType(RuntimeException.class)
             .isThrownBy(() -> Pin.of(input))
             .withMessageMatching("핀은 0~10 사이의 값만 저장할 수 있다.");
+    }
+
+    @Test
+    @DisplayName("두번째 핀 정보가 입력될 때 남은 핀보다 많이 들어오면 Exception이 발생해야 한다.")
+    void pinSaveFailBySecondPinTest() {
+
+        // given
+        Pin input = Pin.of(8);
+        int second = 3;
+
+        // when & then
+        assertThatExceptionOfType(RuntimeException.class)
+            .isThrownBy(() -> Pin.from(input, second))
+            .withMessageMatching("첫번째 핀이 쓰러뜨리고 남은 핀 개수만 저장할 수 있다.");
     }
 
 }
