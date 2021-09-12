4e5ac401e6e376b2cc040d76a9cb608efcc7c0d1
jaeyoungjang2 <lion85488@gmail.com>
2021-09-12 21:17:15 +0900

    feat: Ready 클래스 구현

--- src/main/java/step3/Ready.java
+++ src/main/java/step3/Ready.java
@@ -1,2 +1,10 @@
-package step3;public class Ready {
+package step3;
+
+public class Ready implements State {
+    public State bowl(int falledPins) {
+        if (falledPins == 10) {
+            return new Strike();
+        }
+        return new FirstBowl(falledPins);
+    }
 }
