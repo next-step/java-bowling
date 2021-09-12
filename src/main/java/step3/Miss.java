61ac30f2fa3b653bf52aa54138689b5780701201
jaeyoungjang2 <lion85488@gmail.com>
2021-09-12 21:17:02 +0900

    feat: Miss 클래스 구현

--- src/main/java/step3/Miss.java
+++ src/main/java/step3/Miss.java
@@ -1,2 +1,11 @@
-package step3;public class Miss {
+package step3;
+
+public class Miss implements State {
+    private int firstPins;
+    private int secondPins;
+
+    public Miss(int firstPins, int secondPins) {
+        this.firstPins = firstPins;
+        this.secondPins = secondPins;
+    }
 }
