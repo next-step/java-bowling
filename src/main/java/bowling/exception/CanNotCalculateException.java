094a48a860143b472aea6e1b682e5007a58f081a
shineoov <shineoov88@gmail.com>
2021-09-13 01:45:25 +0900

    refactor: Exception Class final 키워드 추가

--- src/main/java/bowling/exception/CanNotCalculateException.java
+++ src/main/java/bowling/exception/CanNotCalculateException.java
@@ -1,6 +1,6 @@
 package bowling.exception;
 
-public class CanNotCalculateException extends IllegalArgumentException {
+public final class CanNotCalculateException extends IllegalArgumentException {
 
     private static final String MESSAGE = "점수를 계산할 수 없습니다.";
 
--- src/main/java/bowling/exception/FrameNotCorrectException.java
+++ src/main/java/bowling/exception/FrameNotCorrectException.java
@@ -1,6 +1,6 @@
 package bowling.exception;
 
-public class FrameNotCorrectException extends IllegalArgumentException {
+public final class FrameNotCorrectException extends IllegalArgumentException {
 
     private static final String MESSAGE = "프레임에 입력된 값이 올바르지 않습니다.";
 
