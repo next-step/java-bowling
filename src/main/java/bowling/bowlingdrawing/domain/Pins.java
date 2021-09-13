f782264f9698afd0f13b5fd13abd13bd6e2306e5
beadoer1 <beadoer1@gmail.com>
2021-09-13 10:56:36 +0900

    feat(Pins) : Pins 생성자 및 허용 범위 초과 검증 메서드 구현

--- src/main/java/bowling/bowlingdrawing/domain/Pins.java
+++ src/main/java/bowling/bowlingdrawing/domain/Pins.java
@@ -1,10 +1,37 @@
 package bowling.bowlingdrawing.domain;
 
+import bowling.bowlingdrawing.exception.CustomException;
+
+import java.util.Objects;
+
 public class Pins {
 
-    private int pins;
+    public static final int MINIMUM_PINS = 0;
+    public static final int MAXIMUM_PINS = 10;
 
+    private final int pins;
 
     public Pins(int pins) {
+        validateOutOfRange(pins);
+        this.pins = pins;
+    }
+
+    private void validateOutOfRange(int pins) {
+        if (pins < MINIMUM_PINS || pins > MAXIMUM_PINS) {
+            throw new CustomException("pins의 개수가 허용 범위를 벗어났습니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pins)) return false;
+        Pins pins1 = (Pins) o;
+        return pins == pins1.pins;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pins);
     }
 }
