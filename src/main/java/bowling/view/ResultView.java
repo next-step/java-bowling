6de4eaf08f0695eef844c9fc2bb60eb9b6bf2d77
hiro <kiryuinh32@gmail.com>
2021-09-14 01:09:59 +0900

    feat: 출력을 위한 view 클래스 작성

---
+++ src/main/java/bowling/view/ResultView.java
@@ -0,0 +1,36 @@
+package bowling.view;
+
+import bowling.domain.Frame;
+import bowling.domain.Frames;
+import bowling.domain.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class ResultView {
+
+    public static void print(Player player) {
+        System.out.println("| NAME|  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |");
+        System.out.printf("| %s |      |      |      |      |      |      |      |      |      |      | \n", player.name());
+        System.out.println();
+    }
+
+    public static void print(Player player, Frames frames) {
+        System.out.println("| NAME|  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |");
+        Stack<Frame> frameStack = frames.getFrames();
+        List<String> frameScores = new ArrayList<>();
+        for (Frame frame : frameStack) {
+            frameScores.add(frame.score());
+        }
+        System.out.print("|  " + player.name() + "|");
+
+        for (Frame frame : frameStack) {
+            System.out.print(frame.score());
+        }
+        System.out.println();
+        System.out.println();
+    }
+
+
+}
