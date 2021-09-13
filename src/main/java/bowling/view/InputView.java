0d40fe5eb64903148e21556491e5d103dc4cff8f
seondongpyo <seondongpyo@gmail.com>
2021-09-14 05:37:12 +0900

    feat: 볼링 게임에 참여할 플레이어의 수를 입력 받는다

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -9,6 +9,11 @@ public class InputView {
     private InputView() {
     }
 
+    public static int numberOfPlayers() {
+        System.out.print("How many people? ");
+        return SCANNER.nextInt();
+    }
+
     public static String playerName() {
         System.out.print("플레이어 이름은(3 english letters)? : ");
         return SCANNER.next();
