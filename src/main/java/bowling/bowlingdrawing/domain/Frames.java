7d5baeec0a216d22647586fd667617a692d5c51e
beadoer1 <beadoer1@gmail.com>
2021-09-13 22:17:21 +0900

    feat(FramesTest) : pitch 메서드 및 10Frame 초과, 게임 끝 검증 메서드 구현

---
+++ src/main/java/bowling/bowlingdrawing/domain/Frames.java
@@ -0,0 +1,50 @@
+package bowling.bowlingdrawing.domain;
+
+import bowling.bowlingdrawing.exception.CustomException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Frames {
+
+    private final List<Frame> frames = new ArrayList<>();
+
+    public void pitch(Pitching pitching) {
+        validateGameEnd();
+        validateOverTenFrames();
+
+        if (frames.isEmpty()) {
+            addNewFrame(pitching);
+            return;
+        }
+
+        Frame currentFrame = frames.get(frames.size() - 1);
+        if (currentFrame.done()) {
+            addNewFrame(pitching);
+            return;
+        }
+
+        currentFrame.secondPitching(pitching);
+    }
+
+    private void validateGameEnd() {
+        if (frames.size() == 10 && frames.get(frames.size()-1).end()) {
+            throw new CustomException("이미 게임이 끝났습니다.");
+        }
+    }
+
+    private void validateOverTenFrames() {
+        if (frames.size() == 10) {
+            throw new CustomException("최대 Frame(10) 초과");
+        }
+    }
+
+    private void addNewFrame(Pitching pitching) {
+        Frame firstFrame = Frame.of(pitching);
+        frames.add(firstFrame);
+    }
+
+    public List<Frame> frames() {
+        return frames;
+    }
+}
--- src/test/java/bowling/bowlingdrawing/domain/FramesTest.java
+++ src/test/java/bowling/bowlingdrawing/domain/FramesTest.java
@@ -1,9 +1,11 @@
 package bowling.bowlingdrawing.domain;
 
 import bowling.bowlingdrawing.exception.CustomException;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -35,6 +37,31 @@ class FramesTest {
         }
         // then
         assertThatThrownBy(() -> frames.pitch(Pitching.of(10)))
-                .isInstanceOf(CustomException.class);
+                .isInstanceOf(CustomException.class)
+                .hasMessageContaining("최대 Frame(10) 초과");
+    }
+
+    @Test
+    @DisplayName("게임 종료 확인")
+    void validate_game_over() {
+        // given
+        Frames frames = new Frames();
+        Pitching pitching = Pitching.first(10);
+        List<Pitching> pitchings = new ArrayList<>();
+        frames.pitch(pitching);
+        pitchings.add(pitching);
+        // when
+        for (int i = 0; i < 9; i++) {
+            pitchings.add(pitchings.get(i).next(10));
+            frames.pitch(pitchings.get(pitchings.size()-1));
+        }
+
+        pitchings.add(pitchings.get(pitchings.size() - 1).next(10));
+        pitchings.add(pitchings.get(pitchings.size() - 1).next(10));
+
+        // then
+        assertThatThrownBy(() -> frames.pitch(Pitching.of(10)))
+                .isInstanceOf(CustomException.class)
+                .hasMessageContaining("이미 게임이 끝났습니다.");
     }
 }
