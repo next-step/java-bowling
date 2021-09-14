f44f8620e724389878372bf5b9f476736d229ee0
류성호 <>
2021-09-14 19:34:22 +0900

    feat : 10프레임 객체 FinalFrame 객체 구현

--- src/test/java/bowling/FInalFrameTest.java
+++ src/test/java/bowling/FInalFrameTest.java
@@ -1,6 +1,7 @@
 package bowling;
 
 import bowling.domain.FinalFrame;
+import bowling.domain.NormalFrame;
 import bowling.domain.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,32 +12,52 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class FInalFrameTest {
     Player player;
+    NormalFrame frame;
+
 
     @BeforeEach
     void init() {
         String name = "RSH";
         player = new Player(name);
-        player.createEmptyFrame();
+        frame = (NormalFrame) player.startFrame();
 
-        player.createEmptyFrame();
+        for (int i = 0; i < 8; i++) {
+            frame = (NormalFrame) frame.nextFrame(10);
+        }
+    }
+
+    @Test
+    @DisplayName("마지막 프레임 생성")
+    void createFinalFrame() {
+        frame = (NormalFrame) frame.nextFrame(10);
+        FinalFrame finalFrame = (FinalFrame) frame.nextFrame(10);
+        assertThat(finalFrame).isEqualTo(new FinalFrame(10));
+    }
+
+    @Test
+    @DisplayName("마지막 프레임 9번째 프레임에서 생성 오류")
+    void createFinalFrameException() {
+        assertThatThrownBy(() -> {
+            FinalFrame finalFrame = (FinalFrame) frame.nextFrame(10);
+        }).isInstanceOf(ClassCastException.class);
     }
 
     @Test
     @DisplayName("마지막 프레임 마지막 공")
     void finalBall() {
-        FinalFrame finalFrame = player.finalFrame(2);
+        frame = (NormalFrame) frame.nextFrame(10);
+        FinalFrame finalFrame = (FinalFrame) frame.nextFrame(2);
         finalFrame.secondBall(8);
         finalFrame.finalBall(10);
 
-        assertThat(finalFrame.scores().contains("2") &&
-                finalFrame.scores().contains("8") &&
-                finalFrame.scores().contains("10")).isTrue();
+        assertThat(finalFrame.frameScore()).isEqualTo(20);
     }
 
     @Test
     @DisplayName("마지막 프레임 마지막 공 에러")
     void finalBallValidate() {
-        FinalFrame finalFrame = player.finalFrame(2);
+        frame = (NormalFrame) frame.nextFrame(10);
+        FinalFrame finalFrame = (FinalFrame) frame.nextFrame(2);
         finalFrame.secondBall(3);
 
         assertThatThrownBy(() ->
