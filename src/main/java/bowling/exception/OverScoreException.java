63ff1c44da778dd3f22239debec75e42242a535c
hiro <kiryuinh32@gmail.com>
2021-09-15 00:48:51 +0900

    style: 코드 개행 추가

--- src/main/java/bowling/exception/OverScoreException.java
+++ src/main/java/bowling/exception/OverScoreException.java
@@ -2,6 +2,7 @@ package bowling.exception;
 
 public class OverScoreException extends RuntimeException {
     private static String MESSAGE = "점수를 잘못 입력했습니다 총 점수 : %s";
+
     public OverScoreException(int totalScore) {
         super(String.format(MESSAGE, totalScore));
     }
--- src/main/java/bowling/view/ResultView.java
+++ src/main/java/bowling/view/ResultView.java
@@ -32,5 +32,4 @@ public class ResultView {
         System.out.println();
     }
 
-
 }
