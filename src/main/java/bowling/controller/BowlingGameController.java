202e828dbf72c295b1ea5861174de2934b2142b2
ChangSubKwak <paradise1732@gmail.com>
2021-09-14 18:39:11 +0900

    fix: Frames클래스를 분리함에 AllFrames 객체 추가 및 디비터의 법칙 유지를 위한 수정

--- src/main/java/bowling/controller/BowlingGameController.java
+++ src/main/java/bowling/controller/BowlingGameController.java
@@ -1,8 +1,6 @@
 package bowling.controller;
 
-import java.util.List;
-import java.util.stream.IntStream;
-
+import bowling.domain.frame.AllFrames;
 import bowling.domain.player.Player;
 import bowling.domain.player.Players;
 import bowling.domain.score.PureScores;
@@ -11,39 +9,46 @@ import bowling.domain.score.TotalScoreBoard;
 import bowling.view.InputView;
 import bowling.view.ResultView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class BowlingGameController {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
-        Players players = Players.init(inputView.inputPlayerNames());
-        resultView.outputScores(players);
-        executeGame(players);
+        List<String> playerNames = inputView.inputPlayerNames();
+
+        AllFrames allFrames = AllFrames.from(new ArrayList<>(), playerNames.size());
+        Players players = Players.init(playerNames, allFrames);
+
+        resultView.outputScores(players, allFrames);
+        executeGame(players, allFrames);
         inputView.scannerClose();
     }
 
-    private static void executeGame(Players players) {
-        while (!players.isGameFinish()) {
+    private static void executeGame(Players players, AllFrames allFrames) {
+        while (!allFrames.isGameFinish()) {
             IntStream.range(0, players.numberOfPlayers())
-                .forEach(playerNo -> loopInputAndOutput(players, playerNo));
+                .forEach(playerNo -> loopInputAndOutput(players, allFrames, playerNo));
         }
     }
 
-    private static void loopInputAndOutput(Players players, int playerNo) {
+    private static void loopInputAndOutput(Players players, AllFrames allFrames, int playerNo) {
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
-        Player player = players.players().get(playerNo);
+        Player player = players.nthOf(playerNo);
         TotalScoreBoard totalScoreBoard = players.totalScoreBoard();
         boolean isNext = false;
         while (!isNext) {
-            List<Integer> cumulatedScores = totalScoreBoard.cumulativeScoresOfPlayers().get(playerNo);
-            PureScores pureScores = totalScoreBoard.pureScoresOfPlayers().get(playerNo);
+            List<Integer> cumulatedScores = totalScoreBoard.nthCumulativeScoresOf(playerNo);
+            PureScores pureScores = totalScoreBoard.nthPureScoresOf(playerNo);
             int downPinNumber = inputView.inputPlayerTurnPinNumber(player.name());
             pureScores.addScore(Score.from(downPinNumber));
-            player.frames().throwBalls(downPinNumber);
-            totalScoreBoard.cumulativeScoresOfPlayers()
-                .set(playerNo, pureScores.getCumulativeScores(player.frames(), cumulatedScores));
-            resultView.outputScores(players);
-            isNext = player.frames().isNext();
+            allFrames.throwBallOfNthPlayer(playerNo, downPinNumber);
+            totalScoreBoard.updateNthCumulativeScoresOf(playerNo, pureScores.getCumulativeScores(allFrames.nthFramesOf(playerNo), cumulatedScores));
+            resultView.outputScores(players, allFrames);
+            isNext = allFrames.isNextOfNthPlayer(playerNo);
         }
     }
 }
