db7a1f33a5281ae975a6efc824d70af8e7751357
iiaii <jhun9409@naver.com>
2021-09-15 00:04:01 +0900

    [step4] feat: 4단계 요구사항 볼링점수판 구현

--- src/main/java/bowling/BowlingGame.java
+++ src/main/java/bowling/BowlingGame.java
@@ -1,26 +1,36 @@
 package bowling;
 
-import bowling.domain.Frames;
-import bowling.domain.Pitch;
-import bowling.domain.Player;
+import bowling.domain.*;
 import bowling.dto.FrameStatusDto;
 import bowling.view.FramesConsoleOutput;
 import bowling.view.PitchConsoleInput;
 import bowling.view.PlayerConsoleInput;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class BowlingGame {
 
     public static void main(String[] args) {
-        Player player = new Player(PlayerConsoleInput.askName());
-        Frames frames = new Frames();
-        FramesConsoleOutput.print(player.name(), toFrameStatusDto(frames));
+        PlayerCount playerCount = new PlayerCount(PlayerConsoleInput.askPlayerCount());
+        Players players = new Players(PlayerConsoleInput.askNames(playerCount.count()));
+        Lane lane = new Lane(playerCount);
+        FramesConsoleOutput.print(players.names(), toFrameStatusDtos(lane));
         do {
-            frames.addPitch(new Pitch(PitchConsoleInput.askPitch(frames.currentFrameNumber())));
-            FramesConsoleOutput.print(player.name(), toFrameStatusDto(frames));
-        } while (!frames.isEnd());
+            lane.addPitch(new Pitch(PitchConsoleInput.askPitch(lane.currentPlayerName(players))));
+            FramesConsoleOutput.print(players.names(), toFrameStatusDtos(lane));
+        } while (!lane.isAllEnd());
+
     }
 
-    private static FrameStatusDto toFrameStatusDto(final Frames frames) {
+    private static List<FrameStatusDto> toFrameStatusDtos(final Lane lane) {
+        return lane.allFrames()
+                .stream()
+                .map(BowlingGame::toFramesStatusDto)
+                .collect(Collectors.toList());
+    }
+
+    private static FrameStatusDto toFramesStatusDto(final Frames frames) {
         return new FrameStatusDto(frames.allFramePitchValues(), frames.scores());
     }
 
