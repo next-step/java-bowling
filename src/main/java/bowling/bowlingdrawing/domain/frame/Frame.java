8cc890ec6cab31a6fc8b64f6cc93c1880daede9a
beadoer1 <beadoer1@gmail.com>
2021-09-14 12:31:08 +0900

    feat(Frame) : Frame 정적 팩토리 메서드 필요성 없다고 판단 생성자로 변경

--- src/main/java/bowling/bowlingdrawing/domain/frame/Frame.java
+++ src/main/java/bowling/bowlingdrawing/domain/frame/Frame.java
@@ -9,13 +9,17 @@ public class Frame {
     private final Pitching firstPitching;
     private Pitching secondPitching;
 
-    private Frame(Pitching firstPitching, Pitching secondPitching) {
+    public Frame(Pitching firstPitching, Pitching secondPitching) {
         validateSumIsOverTen(firstPitching, secondPitching);
         this.firstPitching = firstPitching;
         this.secondPitching = secondPitching;
     }
 
-    private void validateSumIsOverTen(Pitching firstPitching, Pitching secondPitching) {
+    public Frame(Pitching firstPitching) {
+        this(firstPitching, null);
+    }
+
+    public void validateSumIsOverTen(Pitching firstPitching, Pitching secondPitching) {
         if (secondPitching == null) {
             return;
         }
@@ -23,14 +27,6 @@ public class Frame {
         if (firstPitching.sum(secondPitching) > 10) {
             throw new CustomException("Frame 전체 pin 개수가 10개를 초과합니다.");
         }
-    }
-
-    public static Frame of(Pitching firstPitching, Pitching secondPitching) {
-        return new Frame(firstPitching, secondPitching);
-    }
-
-    public static Frame of(Pitching firstPitching) {
-        return of(firstPitching, null);
     }
 
     public void secondPitching(Pitching secondPitching) {
