cec73401e2281401fc38486916566863886bc88d
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-14 14:25:34 +0900

    feat: 10프레임용 다음 핀 저장 기능 구현 및 테스트

--- src/main/java/bowling/domain/Pin/FinalPins.java
+++ src/main/java/bowling/domain/Pin/FinalPins.java
@@ -4,14 +4,23 @@ public class FinalPins {
 
     private final Pin first;
     private final Pin second;
+    private final Pin bonus;
 
-    private FinalPins(Pin first, Pin second) {
+    private FinalPins(Pin first, Pin second, Pin bonus) {
         this.first = first;
         this.second = second;
+        this.bonus = bonus;
     }
 
     public static FinalPins ofFirst(Pin pin) {
-        return new FinalPins(pin, null);
+        return new FinalPins(pin, null, null);
+    }
+
+    public FinalPins ofNext(Pin pin) {
+        if (second == null) {
+            return new FinalPins(first, pin, null);
+        }
+        return new FinalPins(first, second, pin);
     }
 
 }
--- src/test/java/bowling/domain/Pin/FinalPinsTest.java
+++ src/test/java/bowling/domain/Pin/FinalPinsTest.java
@@ -20,4 +20,39 @@ class FinalPinsTest {
         // then
         assertThat(result).isInstanceOf(FinalPins.class);
     }
+
+    @Test
+    @DisplayName("마지막 프레임 핀의 두번째 핀을 저장할 수 있다.")
+    void finalPinNextTest() {
+
+        // given
+        Pin first = Pin.of(6);
+        FinalPins pins = FinalPins.ofFirst(first);
+        Pin next = Pin.of(4);
+
+        // when
+        FinalPins result = pins.ofNext(next);
+
+        // then
+        assertThat(result).isInstanceOf(FinalPins.class);
+    }
+
+    @Test
+    @DisplayName("마지막 프레임 핀의 마지막 핀을 저장할 수 있다.")
+    void finalPinLastTest() {
+
+        // given
+        Pin first = Pin.of(6);
+        FinalPins firstPins = FinalPins.ofFirst(first);
+        Pin next = Pin.of(4);
+        FinalPins secondPins = firstPins.ofNext(next);
+        Pin last = Pin.of(5);
+
+        // when
+        FinalPins result = secondPins.ofNext(last);
+
+        // then
+        assertThat(result).isInstanceOf(FinalPins.class);
+    }
+
 }
