1489e58a6a8471b9aeebd98e52de4e00894764e4
beadoer1 <beadoer1@gmail.com>
2021-09-14 12:52:52 +0900

    feat(FinalFrame) : 생성자 및 equals, hashcode 구현

--- src/main/java/bowling/bowlingdrawing/domain/frame/FinalFrame.java
+++ src/main/java/bowling/bowlingdrawing/domain/frame/FinalFrame.java
@@ -6,4 +6,42 @@ import java.util.Objects;
 
 public class FinalFrame{
 
+    private final Frame finalFrame;
+    private Pitching bonusPitching1;
+    private Pitching bonusPitching2;
+
+    public FinalFrame(Pitching pitching) {
+        this.finalFrame = new Frame(pitching);
+    }
+
+    public void secondPitching(Pitching pitching2) {
+    }
+
+    public void bonusPitch(Pitching pitching2) {
+    }
+
+    public boolean end() {
+        return false;
+    }
+
+    public boolean strike() {
+        return false;
+    }
+
+    public boolean spare() {
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FinalFrame)) return false;
+        FinalFrame that = (FinalFrame) o;
+        return Objects.equals(finalFrame, that.finalFrame) && Objects.equals(bonusPitching1, that.bonusPitching1) && Objects.equals(bonusPitching2, that.bonusPitching2);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(finalFrame, bonusPitching1, bonusPitching2);
+    }
 }
