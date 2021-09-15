e26012fcca575e71d381688b8c65745a66ef3bd5
beadoer1 <beadoer1@gmail.com>
2021-09-15 21:48:16 +0900

    refactor(FramesTest) : (step2) Game 클래스 삭제를 위한 FramesTest test fail 구현

--- src/test/java/bowling/bowlingdrawing/domain/frame/FramesTest.java
+++ src/test/java/bowling/bowlingdrawing/domain/frame/FramesTest.java
@@ -26,6 +26,18 @@ class FramesTest {
     }
 
     @Test
+    @DisplayName("Frames 에 Pitching 을 통한 Frame 생성")
+    void pitch1() {
+        // given
+        Frames frames = new Frames();
+        // when
+        frames.pitch(10);
+        // then
+        assertThat(frames.frames().get(0))
+                .isEqualTo(new NormalFrame(Pitching.first(10)));
+    }
+
+    @Test
     @DisplayName("게임 종료 확인")
     void validate_game_over() {
         // given
@@ -44,6 +56,26 @@ class FramesTest {
         frames.pitch(pitchings.get(pitchings.size()-1));
         pitchings.add(pitchings.get(pitchings.size() - 1).next(10));
         frames.pitch(pitchings.get(pitchings.size()-1));
+
+        // then
+        assertThatThrownBy(() -> frames.pitch(Pitching.of(10)))
+                .isInstanceOf(CustomException.class)
+                .hasMessageContaining("이미 게임이 끝났습니다.");
+    }
+
+    @Test
+    @DisplayName("게임 종료 확인")
+    void validate_game_over1() {
+        // given
+        Frames frames = new Frames();
+        frames.pitch(10);
+        // when
+        for (int i = 0; i < 9; i++) {
+            frames.pitch(10);
+        }
+
+        frames.pitch(10);
+        frames.pitch(10);
 
         // then
         assertThatThrownBy(() -> frames.pitch(Pitching.of(10)))
