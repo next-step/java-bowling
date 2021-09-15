6cee5d965b18ac989f1e543bc0e20e8a2abc3bf7
beadoer1 <beadoer1@gmail.com>
2021-09-15 12:21:46 +0900

    refactor(Game) : Pitchings 필요 없다 판단 currentPitching 으로 대체하는 test 생성

--- src/test/java/bowling/bowlingdrawing/domain/GameTest.java
+++ src/test/java/bowling/bowlingdrawing/domain/GameTest.java
@@ -1,5 +1,6 @@
 package bowling.bowlingdrawing.domain;
 
+import bowling.bowlingdrawing.domain.pitching.Pitching;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,5 +18,17 @@ class GameTest {
         // then
         assertThat(game.frames().frames().size()).isEqualTo(1);
         assertThat(game.pitchings().pitchings().size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("pitch() 호출 시 frames, currentPitching 추가 확인")
+    void pitch1() {
+        // given
+        Game game = new Game();
+        // when
+        game.pitch(9);
+        // then
+        assertThat(game.frames().frames().size()).isEqualTo(1);
+        assertThat(game.currentPitching()).isEqualTo(Pitching.of(9));
     }
 }
