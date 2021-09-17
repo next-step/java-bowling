55e5163f4c7f0ec0d567f575b9603f2cc273e852
rladowl92 <rladowl92@naver.com>
2021-09-17 20:00:34 +0900

    refactor: view 호출부분 main으로 이동

--- src/main/java/bowling/BowlingApplication.java
+++ src/main/java/bowling/BowlingApplication.java
@@ -1,20 +1,34 @@
 package bowling;
 
+import bowling.domain.player.Player;
 import bowling.domain.player.Players;
-import bowling.service.BowlingService;
 import bowling.view.InputView;
 import bowling.view.OutputView;
 
 public class BowlingApplication {
-    public static void main(String[] args) {
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
 
+    public static void main(String[] args) {
         int playerCount = inputView.playerCount();
         Players players = inputView.players(playerCount);
         outputView.printBoard(players);
 
-        BowlingService bowlingService = new BowlingService();
-        bowlingService.play(players);
+        while (!players.finish()) {
+            playFrame(players);
+        }
+    }
+
+    private static void playFrame(Players players) {
+        players.getPlayers()
+                .forEach(player -> playTurn(players, player));
+    }
+
+    private static void playTurn(Players players, Player player) {
+        do {
+            int score = inputView.next(player);
+            player.next(score);
+            outputView.printBoard(players);
+        } while (!player.turnFinish());
     }
 }
