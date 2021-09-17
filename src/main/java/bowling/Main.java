834ab9f5f33e034dca4b3d9501c94810a90291c9
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-17 14:47:43 +0900

    refactor: 메소드 분리

--- src/main/java/bowling/Main.java
+++ src/main/java/bowling/Main.java
@@ -10,6 +10,7 @@ import bowling.domain.frame.Frames;
 import bowling.domain.frame.NormalFrame;
 import bowling.domain.score.Pin;
 import bowling.domain.user.User;
+import java.util.stream.IntStream;
 
 public class Main {
 
@@ -18,27 +19,33 @@ public class Main {
         Frame firstFrame = NormalFrame.createFirstFrame();
         printStartBoard(user);
 
-
-        for (int i=1; i<=9; i++) {
-            Frame nowFrame = Frame.getLastFrame(firstFrame);
-            printSingleScore(user, Frames.creatByFirstFrame(firstFrame), i, nowFrame);
-            if (nowFrame.isNextScore()) {
-                printSingleScore(user, Frames.creatByFirstFrame(firstFrame), i, nowFrame);
-            }
-            nowFrame.createNextFrame();
-        }
+        IntStream.range(1, 10)
+            .forEach(round -> normalView(user, firstFrame, round));
         Frame lastFrame = Frame.getLastFrame(firstFrame);
-        printSingleScore(user, Frames.creatByFirstFrame(firstFrame), 10, lastFrame);
-        printSingleScore(user, Frames.creatByFirstFrame(firstFrame), 10, lastFrame);
-        if (lastFrame.isNextScore()) {
-            printSingleScore(user, Frames.creatByFirstFrame(firstFrame), 10, lastFrame);
+        finalView(user, firstFrame, lastFrame);
+    }
+
+    private static void normalView(User user, Frame firstFrame, int i) {
+        Frame nowFrame = Frame.getLastFrame(firstFrame);
+        printSingleScore(user, Frames.creatByFirstFrame(firstFrame), i, nowFrame);
+        if (nowFrame.isNextScore()) {
+            printSingleScore(user, Frames.creatByFirstFrame(firstFrame), i, nowFrame);
         }
+        nowFrame.createNextFrame();
     }
 
     private static void printSingleScore(User user, Frames frames, int i, Frame nowFrame) {
         Pin pin = Pin.of(inputNextFrameShot(i));
         nowFrame.updateScoreByPin(pin);
         printResult(user, frames);
+    }
+
+    private static void finalView(User user, Frame firstFrame, Frame lastFrame) {
+        printSingleScore(user, Frames.creatByFirstFrame(firstFrame), 10, lastFrame);
+        printSingleScore(user, Frames.creatByFirstFrame(firstFrame), 10, lastFrame);
+        if (lastFrame.isNextScore()) {
+            printSingleScore(user, Frames.creatByFirstFrame(firstFrame), 10, lastFrame);
+        }
     }
 
 }
