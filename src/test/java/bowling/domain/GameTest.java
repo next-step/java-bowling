b6da2a7ffe5eac8021c297c623c086b42a9509c3
harris <harrisleesh@gmail.com>
2021-09-18 22:59:52 +0900

    test : Create game test - nextFrame function test - isOver function test - pitch and nextFrame function test

---
+++ src/test/java/bowling/domain/GameTest.java
@@ -0,0 +1,53 @@
+package bowling.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game(new Player("LSH"));
+    }
+
+    @Test
+    @DisplayName("처음 생성한 게임에서 nextFrame()함수 호출 시 1번 프레임을 리턴한다.")
+    void nextFrame() {
+        Frame frame = game.nextFrame();
+        assertEquals(frame.order(), 1);
+    }
+
+    @Test
+    @DisplayName("10번의 frame이 완료하고 isOver()함수를 호출 시 True 임을 확인한다.")
+    void isOver() {
+        for (int i = 0; i < 10; i++) {
+            assertFalse(game.isOver());
+            game.pitch(10);
+        }
+        assertTrue(game.isOver());
+    }
+
+    @Test
+    @DisplayName("10점 미만의 핏칭을 한번 했을 때 핏칭 전과 후의 프레임이 같음 확인한다.")
+    void pitch() {
+        Frame beforePitch = game.nextFrame();
+        game.pitch(5);
+        Frame afterPitch = game.nextFrame();
+        assertEquals(beforePitch, afterPitch);
+    }
+
+    @Test
+    @DisplayName("10점 핏칭을 한번 했을 때 핏칭 전과 후의 프레임이 다름 확인한다.")
+    void pitchDifferentFrame() {
+        Frame beforePitch = game.nextFrame();
+        game.pitch(10);
+        Frame afterPitch = game.nextFrame();
+        assertNotEquals(beforePitch, afterPitch);
+        assertEquals(beforePitch.order() + 1, afterPitch.order());
+    }
+}
