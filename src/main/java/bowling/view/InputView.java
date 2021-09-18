6851fb8d57b59d53103caf569527c98c2730832b
boss.back <boss.back@kakaocorp.com>
2021-09-18 15:25:59 +0900

    fix: 입력 재귀로 수정

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -10,38 +10,26 @@ public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static UserName getUserName() {
-        while (true) {
-            try {
-                return makeUserName();
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
+        UserName userName = null;
+        try {
+            System.out.print("플레이어 이름은(3 english letters)?:");
+            String name = SCANNER.next();
+            userName = new UserName(name);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            getUserName();
         }
-    }
-
-    private static UserName makeUserName() {
-        System.out.print("플레이어 이름은(3 english letters)?:");
-        String name = SCANNER.next();
-        UserName userName = new UserName(name);
         return userName;
     }
 
     public static void enterBowlPins(Frame frame) {
-        while (true) {
-            try {
-                downPins(frame);
-                return;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
+        try {
+            System.out.print(frame.findCurrentIndex() + "프레임 투구: ");
+            int downPin = SCANNER.nextInt();
+            frame.bowl(downPin);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            enterBowlPins(frame);
         }
     }
-
-
-    private static void downPins(Frame frame) {
-        System.out.print(frame.findCurrentIndex() + "프레임 투구: ");
-        int downPin = SCANNER.nextInt();
-        frame.bowl(downPin);
-    }
-
 }
