415ebc98471f5ec21dd7e05782773f9a1be80125
harris <harrisleesh@gmail.com>
2021-09-18 23:36:27 +0900

    feat : Create 10th Frame third pitch logic

--- src/main/java/bowling/domain/Frame.java
+++ src/main/java/bowling/domain/Frame.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Frame {
+    public static final int LAST_FRAME = 10;
+    public static final int STRIKE = 10;
     private int order;
     private List<Pitch> pitches = new ArrayList<>();
     public static final int FIRST = 0;
     public static final int SECOND = 1;
+    public static final int THIRD = 2;
 
     public Frame(int order) {
         this.order = order;
@@ -18,7 +21,9 @@ public class Frame {
     }
 
     public void pitch(int score) {
-        validate(score);
+        if (!isLastFrame()) {
+            validate(score);
+        }
         pitches.add(new Pitch(score));
     }
 
@@ -30,6 +35,13 @@ public class Frame {
         if (scoreSum > 10) {
             throw new IllegalArgumentException("한 프레임의 점수합이 10을 넘을 수 없습니다.");
         }
+        if (pitches.size() >= 2) {
+            throw new IllegalArgumentException("한 프레임당 2개의 핏칭을 할 수 있습니다.");
+        }
+    }
+
+    private boolean isLastFrame() {
+        return order == LAST_FRAME;
     }
 
     public Pitch first() {
@@ -40,11 +52,31 @@ public class Frame {
         return pitches.get(SECOND);
     }
 
+    public Pitch third() {
+        if (!isLastFrame()) {
+            throw new IllegalArgumentException("마지막 프레임에서만 세번째 핏칭을 할 수 있습니다.");
+        }
+        return pitches.get(THIRD);
+    }
+
     public boolean remain() {
+        if(isLastFrame()){
+            return remainPitchInLastFrame();
+        }
         if (isStrike()) {
             return false;
         }
         if (pitches.size() != 2) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean remainPitchInLastFrame() {
+        if (pitches.size() < 2) {
+            return true;
+        }
+        if (pitches.size() == 2 && (first().score() == STRIKE || isSpare())) {
             return true;
         }
         return false;
@@ -58,7 +90,7 @@ public class Frame {
     }
 
     public boolean isStrike() {
-        if (pitches.size() == 1 && first().score() == 10) {
+        if (pitches.size() == 1 && first().score() == STRIKE) {
             return true;
         }
         return false;
