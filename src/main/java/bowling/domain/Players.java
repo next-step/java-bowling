e8bae79400e9220fd2f25b97618724acea45b693
kimdonggyu-dev <donggyu.dev@gmail.com>
2021-09-22 15:58:10 +0900

    test: players 테스트 추가

--- src/main/java/bowling/domain/Players.java
+++ src/main/java/bowling/domain/Players.java
@@ -4,6 +4,7 @@ import qna.exception.NotFoundException;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Players {
@@ -49,5 +50,18 @@ public class Players {
 
     public List<Player> elements() {
         return Collections.unmodifiableList(this.players);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Players players1 = (Players) o;
+        return Objects.equals(players, players1.players);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(players);
     }
 }
