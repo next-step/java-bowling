1e82aab85c266e6672aaefa44f354960fd919dcb
hiro <kiryuinh32@gmail.com>
2021-09-23 05:27:49 +0900

    feat: Pitch 클래스 구현

--- src/test/java/bowling/domain/frame/PitchTest.java
+++ src/test/java/bowling/domain/frame/PitchTest.java
@@ -1,28 +1,27 @@
-package bowling.domain;
+package bowling.domain.frame;
 
 import bowling.exception.OverScoreException;
 import bowling.exception.PitchException;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class PitchTest {
 
     @Test
     @DisplayName("Pitch 의 값이 10 보다 크다면 PitchException")
     void pitch_over_10_exception() {
-        assertThatThrownBy(() -> new Pitch(11))
+        assertThatThrownBy(() -> Pitch.from(11))
                 .isInstanceOf(PitchException.class);
     }
 
     @Test
     @DisplayName("Pitch 의 값이 0 보다 작다면 PitchException")
     void pitch_under_1_exception() {
-        assertThatThrownBy(() -> new Pitch(-1))
+        assertThatThrownBy(() -> Pitch.from(-1))
                 .isInstanceOf(PitchException.class);
     }
 
@@ -36,23 +35,22 @@ class PitchTest {
     @Test
     @DisplayName("Pitch 의 값이 -1 이 아니라면 false")
     void pitch_value_not_minus_one() {
-        Pitch pitch = new Pitch(1);
+        Pitch pitch = Pitch.from(1);
         assertThat(pitch.nonRecode()).isFalse();
     }
 
     @Test
     @DisplayName("Pitch 의 값이 10 이라면 true")
     void pitch_value_strike() {
-        Pitch pitch = new Pitch(10);
+        Pitch pitch = Pitch.from(10);
         assertThat(pitch.isStrike()).isTrue();
     }
 
     @Test
-    @Disabled
     @DisplayName("Pitch 2개의 합이 10 이상 이라면, OverScoreException")
     void pitch_sum_over_ten() {
-        Pitch first = new Pitch(5);
-        Pitch second = new Pitch(7);
+        Pitch first = Pitch.from(5);
+        Pitch second = Pitch.from(7);
 
         assertThatThrownBy(() -> first.checkTotal(second))
                 .isInstanceOf(OverScoreException.class);
@@ -63,13 +61,13 @@ class PitchTest {
     void pitch_sum_spare() {
         assertAll(
                 () -> {
-                    Pitch first = new Pitch(5);
-                    Pitch second = new Pitch(5);
+                    Pitch first = Pitch.from(5);
+                    Pitch second = Pitch.from(5);
                     assertThat(first.isSpare(second)).isTrue();
                 },
                 () -> {
-                    Pitch first = new Pitch(1);
-                    Pitch second = new Pitch(2);
+                    Pitch first = Pitch.from(1);
+                    Pitch second = Pitch.from(2);
                     assertThat(first.isSpare(second)).isFalse();
                 }
 
