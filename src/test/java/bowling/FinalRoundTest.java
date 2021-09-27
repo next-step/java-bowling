5ac44f56aa31d715bca8e42473ea4d10b2f9e039
Gyeongseo Woo <ugs1575@gmail.com>
2021-09-27 22:58:46 +0900

    refactor: 마지막 프레임 테스트 수정

--- src/test/java/bowling/FinalRoundTest.java
+++ src/test/java/bowling/FinalRoundTest.java
@@ -1,2 +1,130 @@
-package bowling;public class FinalRoundTest {
+package bowling;
+
+import bowling.model.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.LinkedList;
+
+import static bowling.model.Score.ofSpare;
+import static bowling.model.Score.ofStrike;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FinalRoundTest {
+    @Test
+    public void 스트라이크나_스페어가_아니면_프레임은_끝난다() throws CannotBowlException {
+        //given
+        int firstPin = 2;
+        State state = new FirstBowl(firstPin);
+        LinkedList<Score> scores = new LinkedList<>();
+        scores.add(new Score(2, 1));
+
+        //when
+        FinalRound finalRound = new FinalRound();
+        finalRound.bowl(firstPin);
+
+        //then
+        assertThat(finalRound).isEqualTo(new FinalRound(state, scores));
+        assertFalse(finalRound.isFinish());
+
+
+        //given
+        int secondPin = 3;
+        state = new Miss(new Point(firstPin + secondPin));
+        scores = new LinkedList<>();
+
+        //when
+        finalRound.bowl(secondPin);
+
+        //then
+        assertThat(finalRound).isEqualTo(new FinalRound(state, scores));
+        assertTrue(finalRound.isFinish());
+    }
+
+    @Test
+    public void 스트라이크_보너스프레임이_주어진다() throws CannotBowlException {
+        //given
+        int firstPin = 10;
+        State state = new Strike();
+        LinkedList<Score> scores = new LinkedList<>();
+        scores.add(ofStrike());
+
+        //when
+        FinalRound finalRound = new FinalRound();
+        finalRound.bowl(firstPin);
+
+        //then
+        assertThat(finalRound).isEqualTo(new FinalRound(state, scores));
+
+
+        //given
+        int secondPin = 10;
+        state = new SecondStrike();
+        scores = new LinkedList<>();
+        scores.add(new Score(20, 1));
+
+        //when
+        finalRound.bowl(secondPin);
+
+        //then
+        assertThat(finalRound).isEqualTo(new FinalRound(state, scores));
+
+        //given
+        int thirdPin = 10;
+        state = new ThirdStrike();
+        scores = new LinkedList<>();
+
+        //when
+        finalRound.bowl(thirdPin);
+
+        //then
+        assertThat(finalRound).isEqualTo(new FinalRound(state, scores));
+    }
+
+
+    @Test
+    public void 스페어_보너스프레임이_주어진다() throws CannotBowlException {
+        //given
+        int firstPin = 9;
+        State state = new FirstBowl(firstPin);
+        LinkedList<Score> scores = new LinkedList<>();
+        scores.add(new Score(9, 1));
+
+        //when
+        FinalRound finalRound = new FinalRound();
+        finalRound.bowl(firstPin);
+
+        //then
+        assertThat(finalRound).isEqualTo(new FinalRound(state, scores));
+
+
+        //given
+        int secondPin = 1;
+        state = new Spare();
+        scores = new LinkedList<>();
+        scores.add(ofSpare());
+
+        //when
+        finalRound.bowl(secondPin);
+
+        //then
+        assertThat(finalRound).isEqualTo(new FinalRound(state, scores));
+
+
+        //given
+        int thirdPin = 10;
+        state = new ThirdStrike();
+        scores = new LinkedList<>();
+
+        //when
+        finalRound.bowl(thirdPin);
+
+        //then
+        assertThat(finalRound).isEqualTo(new FinalRound(state, scores));
+    }
+
 }
