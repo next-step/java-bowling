ce4695532990b86a1e86ed014e68335177cfb44b
newchk610@gmail.com <gprms@1208>
2021-09-27 16:57:52 +0900

    refactor: Question 삭제 리펙토링

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,8 +1,43 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
 
+    @Test
+    @DisplayName("작성자가 글을 지운다.(답변없음)")
+    void deleteQuestionOwner() throws CannotDeleteException {
+        Q1.delete(UserTest.JAVAJIGI);
+        assertThat(Q1.isDeleted()).isTrue();
+    }
 
+    @Test
+    @DisplayName("작성자가 아닌 사람이 글을 지운다.")
+    void deleteQuestionNotOwner() throws CannotDeleteException {
+        assertThatThrownBy(() -> Q1.delete(UserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    @DisplayName("답변이 있는 글을 지우려고 함(자신이 쓴 답변)")
+    void deleteQuestionHavingMyAnswer() throws CannotDeleteException {
+        Q1.addAnswer(AnswerTest.A1);
+        Q1.delete(UserTest.JAVAJIGI);
+        assertThat(Q1.isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("답변이 있는 글을 지우려고 함(남이 쓴 답변)")
+    void deleteQuestionHavingOthersAnswer() throws CannotDeleteException {
+        Q1.addAnswer(AnswerTest.A2);
+        assertThatThrownBy(() -> Q1.delete(UserTest.JAVAJIGI))
+                .isInstanceOf(CannotDeleteException.class);
+    }
 }
