923f817724a40550790824e0944acda3a80e2ff2
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-30 00:37:01 +0900

    feat: NormalScore first pin 저장 기능 구현 및 테스트

--- src/main/java/bowling/domain/score/NormalScore.java
+++ src/main/java/bowling/domain/score/NormalScore.java
@@ -1,13 +1,19 @@
 package bowling.domain.score;
 
+import java.util.Objects;
+
 public class NormalScore implements Score {
 
-    private Pin first;
-    private Pin second;
+    private final Pin first;
+    private final Pin second;
 
     private NormalScore(Pin first, Pin second) {
         this.first = first;
         this.second = second;
+    }
+
+    static Score of(Pin first, Pin second) {
+        return new NormalScore(first, second);
     }
 
     public static Score emptyScore() {
@@ -16,12 +22,32 @@ public class NormalScore implements Score {
 
     @Override
     public Score saveNextPin(Pin pin) {
+        if (Objects.isNull(first)) {
+            return new NormalScore(pin, null);
+        }
         return null;
     }
 
     @Override
     public boolean isNextStorable() {
         return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        NormalScore that = (NormalScore) o;
+        return Objects.equals(first, that.first) && Objects.equals(second, that.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
     }
 
 }
--- src/test/java/bowling/domain/score/NormalScoreTest.java
+++ src/test/java/bowling/domain/score/NormalScoreTest.java
@@ -1,7 +1,6 @@
 package bowling.domain.score;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,14 +11,31 @@ class NormalScoreTest {
     @DisplayName("빈 socre를 생성할 수 있다.")
     void createEmptyScoreTest() {
 
-        // given & when
+        // given
+        Score expected = NormalScore.emptyScore();
+
+        // when
         Score result = NormalScore.emptyScore();
 
         // then
-        assertAll(
-            () -> assertThat(result).isInstanceOf(Score.class),
-            () -> assertThat(result).isInstanceOf(NormalScore.class)
-        );
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("첫번째 pin이 비어있다면 저장할 수 있다.")
+    void saveFirstPinTest() {
+
+        // given
+        Pin first = Pin.of(3);
+        Score input = NormalScore.emptyScore();
+
+        Score expected = NormalScore.of(first, null);
+
+        // when
+        Score result = input.saveNextPin(first);
+
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 
 }
