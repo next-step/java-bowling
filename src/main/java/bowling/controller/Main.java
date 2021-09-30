6aa836671ec5ba33f7b3dae50c3b9d30ec94d51f
Gyeongseo Woo <ugs1575@gmail.com>
2021-09-30 10:45:06 +0900

    refactor: 컨트롤러 점수값 받아오도록 추가 하고 전체적인 로직 변경

--- src/main/java/bowling/controller/Main.java
+++ src/main/java/bowling/controller/Main.java
@@ -1,54 +1,37 @@
 package bowling.controller;
 
+import bowling.CannotBowlException;
 import bowling.model.BowlingGame;
 import bowling.model.State;
 import bowling.model.User;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import static bowling.view.InputView.ask;
 import static bowling.view.InputView.askDigit;
 import static bowling.view.OutputView.changeScore;
-import static bowling.view.OutputView.printBowlingScore;
+import static bowling.view.OutputView.printResult;
 
 public class Main {
-    public static final int MAX_ROUND = 10;
-    public static final int SECOND_TRY = 2;
-
-    public static List<State> bowlingResults = new ArrayList<>();
-    public static List<List<String>> allResults = new ArrayList<>();
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CannotBowlException {
         String userName = ask("플레이어 이름은(3 english letters)?:");
         User user = new User(userName);
 
         BowlingGame game = new BowlingGame();
-        for (int i = 1; i <= MAX_ROUND; i++) {
-            List<String> pointList = new ArrayList<>();
-            allResults.add(pointList);
-            playRound(game, user, i);
+        List<Integer> scoreResult = new ArrayList<>();
+        LinkedList<String> stateResult = new LinkedList<>();
+        while (!game.isEndGame()) {
+            int pinCount = askDigit(game.getFrameNo() + "프레임 투구 : ");
+
+            State state = game.bowl(pinCount);
+            List<Integer> scores = game.getScore();
+
+            stateResult.add(changeScore(stateResult, state, pinCount));
+            scoreResult.addAll(scores);
+
+            printResult(userName, stateResult, scoreResult);
         }
-    }
-
-    private static void playRound(BowlingGame game, User user, int index) {
-        int tryCount = 0;
-        int maxRound = SECOND_TRY;
-
-        while (tryCount++ < maxRound) {
-            int pinCount = askDigit(index + "프레임 투구 : ");
-            maxRound += game.play(pinCount);
-
-            getLastList().add(changeScore(pinCount, getBowlingResult()));
-            printBowlingScore(user.getName(), allResults);
-        }
-    }
-
-    private static State getBowlingResult() {
-        return bowlingResults.get(bowlingResults.size() - 1);
-    }
-
-    private static List<String> getLastList() {
-        return allResults.get(allResults.size()-1);
     }
 }
