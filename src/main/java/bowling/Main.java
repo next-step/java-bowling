26976515cee8187c47bd278921e6dc4cb3ddb707
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-30 16:34:10 +0900

    feat: score board 출력기능 구현

--- src/main/java/bowling/Main.java
+++ src/main/java/bowling/Main.java
@@ -2,11 +2,13 @@ package bowling;
 
 import bowling.domain.user.User;
 import bowling.view.InputView;
+import bowling.view.ResultView;
 
 public class Main {
 
     public static void main(String[] args) {
         User user = User.of(InputView.inputUsername());
+        ResultView.printScoreBoard();
     }
 
 }
--- src/main/java/bowling/view/ResultView.java
+++ src/main/java/bowling/view/ResultView.java
@@ -1,9 +1,27 @@
 package bowling.view;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class ResultView {
+
+    private static final String SCORE_NAME_BOARD = "| NAME |";
+    private static final String SCORE_BOARD_FORMAT = "  %02d  |";
 
     private ResultView() {
     }
 
+    public static void printScoreBoard() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(SCORE_NAME_BOARD);
+        sb.append(IntStream.rangeClosed(1, 10)
+            .mapToObj(ResultView::toBoardFormat)
+            .collect(Collectors.joining()));
+        System.out.println(sb.toString());
+    }
+
+    private static String toBoardFormat(int num) {
+        return String.format(SCORE_BOARD_FORMAT, num);
+    }
 
 }
