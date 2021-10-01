2b6b6fd68081742138c4e5b46d27d3bab1dbc76c
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-10-01 11:22:07 +0900

    feat: State 상태패턴 interface 기능 목록 구현

---
+++ src/main/java/bowling/domain/state/State.java
@@ -0,0 +1,14 @@
+package bowling.domain.state;
+
+import bowling.domain.score.Pin;
+import bowling.domain.score.Score;
+
+public interface State {
+
+    boolean isFinished();
+
+    Score createScore();
+
+    State bowl(Pin pin);
+
+}
