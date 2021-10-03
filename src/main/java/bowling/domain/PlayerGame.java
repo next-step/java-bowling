34663a2a1fd346a9b60dc34c5e4910c212d7ec94
조원진 <>
2021-10-03 14:09:07 +0900

    feat(PlayerGame) : 플레이어 이름과, 프레임 List를 갖는 플레이어게임 클래스 추가

---
+++ src/main/java/bowling/domain/PlayerGame.java
@@ -0,0 +1,55 @@
+package bowling.domain;
+
+import bowling.domain.frame.BasicFrame;
+import bowling.domain.frame.FinalFrame;
+import bowling.domain.frame.Frame;
+import bowling.domain.frame.FrameNumber;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class PlayerGame {
+    private final Name name;
+
+    private List<Frame> frames = new ArrayList<>();
+
+    public PlayerGame(final String playerName) {
+        this.name = new Name(playerName);
+        this.frames.add(new BasicFrame(FrameNumber.of(FrameNumber.START_NUMBER)));
+    }
+
+    public boolean isGameEnd() {
+        Frame currentFrame = getCurrentFrame();
+        FrameNumber frameNumber = currentFrame.getFrameNumber();
+        if (!frameNumber.isFinalNumber()) {
+            return false;
+        }
+        FinalFrame finalFrame = (FinalFrame) currentFrame;
+        return finalFrame.isFinished();
+    }
+
+    public Frame play(final int pinCount) {
+        Frame currentFrame = getCurrentFrame();
+        Frame frame = currentFrame.play(PinCount.of(pinCount));
+        if (!Objects.equals(currentFrame, frame)) {
+            frames.add(frame);
+        }
+        return frame;
+    }
+
+    public Frame getCurrentFrame() {
+        return frames.get(frames.size() - 1);
+    }
+
+    public String getName() {
+        return name.getValue();
+    }
+
+    public List<FrameResult> getFrameResults() {
+        return frames.stream()
+                .map(Frame::makeResult)
+                .collect(Collectors.toList());
+    }
+}
