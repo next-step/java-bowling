400aedc24d72ccc9f3b3824f4180d7ddd4273189
jaeyoungjang2 <lion85488@gmail.com>
2021-10-05 22:09:56 +0900

    feat: bowlingGameController 클래스 생성 컨트롤러 생성

--- src/main/java/step4/BowlingGameController.java
+++ src/main/java/step4/BowlingGameController.java
@@ -1,2 +1,25 @@
-package step4;public class BowlingGameController {
+package step4;
+
+import java.util.ArrayList;
+import java.util.List;
+import step4.view.InputView;
+import step4.view.ResultView;
+
+public class BowlingGameController {
+    public static void run() {
+        int numOfPeople = InputView.numOfPeople();
+        List<String> personNames = new ArrayList<>();
+        for (int i = 0; i < numOfPeople; i++) {
+            String nameOfPerson = InputView.nameOfPerson();
+            personNames.add(nameOfPerson);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            ResultView.printColumn();
+            for (String personName : personNames) {
+                InputView.throwBowl(personName);
+            }
+        }
+
+    }
 }
