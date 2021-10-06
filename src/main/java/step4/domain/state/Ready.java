2c60322b6d80d239ea4295a0ac3ae0145a0a9477
jaeyoungjang2 <lion85488@gmail.com>
2021-10-06 10:07:58 +0900

    feat: Ready 클래스 생성 Frame이 시작되고, 볼링공을 굴리기 전 상태를 나타냄

--- src/main/java/step4/domain/state/Ready.java
+++ src/main/java/step4/domain/state/Ready.java
@@ -1,2 +1,10 @@
-package step4.domain.state;public class Ready {
+package step4.domain.state;
+
+public class Ready {
+    public State throwBowl(int falledPins) {
+        if (falledPins == 10) {
+            return new Strike();
+        }
+        return new FirstBowl(falledPins);
+    }
 }
