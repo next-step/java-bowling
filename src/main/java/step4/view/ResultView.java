88a8273c4ac03e6839f00385eb45d08bb5f673ae
jaeyoungjang2 <lion85488@gmail.com>
2021-10-06 10:08:38 +0900

    feat: ResultView 생성

--- src/main/java/step4/view/ResultView.java
+++ src/main/java/step4/view/ResultView.java
@@ -2,9 +2,13 @@ package step4.view;
 
 public class ResultView {
     private static String RESULT_COLUMN = "| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |";
-
+    private static String PERSON_NNAME = "|  %3s |";
 
     public static void printColumn() {
         System.out.println(RESULT_COLUMN);
+    }
+
+    public static void printName(String personName) {
+        System.out.printf(PERSON_NNAME, personName);
     }
 }
