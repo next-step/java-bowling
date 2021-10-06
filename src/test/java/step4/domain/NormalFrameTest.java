118ccc04d33a5bb3e616408d354785527b54db84
jaeyoungjang2 <lion85488@gmail.com>
2021-10-06 11:14:48 +0900

    test: Frame별로 공을 던질때 마다 State 변경 확인 test 생성

--- src/test/java/step4/domain/NormalFrameTest.java
+++ src/test/java/step4/domain/NormalFrameTest.java
@@ -1,4 +1,49 @@
+package step4.domain;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import step4.domain.state.FirstBowl;
+import step4.domain.state.LastBowl;
+import step4.domain.state.Spair;
+import step4.domain.state.Strike;
+
 class NormalFrameTest {
-  
+    @DisplayName("Ready state -> Strike state 상태 변경 확인")
+    @Test
+    void strikeTest() {
+        NormalFrame normalFrame = new NormalFrame();
+        normalFrame.throwBowl(10);
+        Assertions.assertThat(normalFrame.state()).isEqualTo(new Strike());
+    }
+
+    @DisplayName("Ready state -> FirstBowl state 상태 변경 확인")
+    @Test
+    void firstBowlTest() {
+        NormalFrame normalFrame = new NormalFrame();
+        normalFrame.throwBowl(4);
+        Assertions.assertThat(normalFrame.state()).isEqualTo(new FirstBowl(4));
+    }
+
+    @DisplayName("FirstBowl state -> LastBowl state 상태 변경 확인")
+    @Test
+    void lastBowlTest() {
+        NormalFrame normalFrame = new NormalFrame();
+        normalFrame.throwBowl(4);
+        normalFrame.throwBowl(4);
+        Assertions.assertThat(normalFrame.state()).isEqualTo(new LastBowl(8));
+    }
+
+    @DisplayName("FirstBowl state -> Spair state 상태 변경 확인")
+    @Test
+    void spairTest() {
+        NormalFrame normalFrame = new NormalFrame();
+        normalFrame.throwBowl(4);
+        normalFrame.throwBowl(6);
+        Assertions.assertThat(normalFrame.state()).isEqualTo(new Spair());
+    }
+
+
 }
