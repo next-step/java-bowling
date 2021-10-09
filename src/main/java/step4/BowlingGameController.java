99b3aa2eafbe0c15ee825d57f4c05a5f0ec6b19c
jaeyoungjang2 <lion85488@gmail.com>
2021-10-09 11:31:54 +0900

    feat: 볼링게임에 참가하는 사람 수를 유저로 부터 input으로 받게하는 로직 추가

--- src/main/java/step4/BowlingGameController.java
+++ src/main/java/step4/BowlingGameController.java
@@ -1,5 +1,7 @@
 package step4;
 
+import java.util.ArrayList;
+import java.util.List;
 import step4.domain.Frame;
 import step4.domain.Frames;
 import step4.domain.NormalFrame;
@@ -10,42 +12,52 @@ import step4.view.ResultView;
 public class BowlingGameController {
     public static void run() {
         int numOfPeople = InputView.numOfPeople();
-        String nameOfPerson = InputView.nameOfPerson();
+        List<String> players = new ArrayList<>();
+        Frames frames = new Frames();
+        Frame initInfoFrame = new NormalFrame(0);
+        for (int i = 0; i < numOfPeople; i++) {
+            players.add(InputView.nameOfPerson());
+        }
 
         ResultView.printMainColumn();
-        ResultView.printFirstColumn(nameOfPerson);
-        ResultView.printEmptyColumn(10);
+        for (String nameOfPerson : players) {
+            ResultView.printFirstColumn(nameOfPerson);
+//            ResultView.printEmptyColumn(10);
+            ResultView.printEmptyColumn(10 - initInfoFrame.round());
+            ResultView.printFirstColumn("");
+            int round = printTotalScore(initInfoFrame);
+            ResultView.printEmptyColumn(10 - round);
+        }
 
-        Frames frames = new Frames();
+
+
         Frame frame = new NormalFrame(1);
         frames.add(frame);
 
-        while (!frame.isGameEnd()) {
-            int falledPins = InputView.throwBowl(nameOfPerson);
-            frame.throwBowl(falledPins);
-            ResultView.printMainColumn();
-            ResultView.printFirstColumn(nameOfPerson);
-
-            printSymbol(frames);
-            ResultView.printEmptyColumn(10 - frame.round());
-            ResultView.printFirstColumn("");
-            int round = printTotalScore(frames);
-            ResultView.printEmptyColumn(10 - round);
-
-            if (frame.round() != 10 && frame.isFinish()) {
-                frame = frame.createFrame(frame.round() + 1);
-                frames.add(frame);
-            }
-        }
+//        while (!frame.isGameEnd()) {
+//            int falledPins = InputView.throwBowl(nameOfPerson);
+//            frame.throwBowl(falledPins);
+//            ResultView.printMainColumn();
+//            ResultView.printFirstColumn(nameOfPerson);
+//            printSymbol(frames);
+//            ResultView.printEmptyColumn(10 - frame.round());
+//            ResultView.printFirstColumn("");
+//            int round = printTotalScore(frames);
+//            ResultView.printEmptyColumn(10 - round);
+//
+//            if (frame.round() != 10 && frame.isFinish()) {
+//                frame = frame.createFrame(frame.round() + 1);
+//                frames.add(frame);
+//            }
+//        }
 
     }
 
-    private static void printSymbol(Frames frames) {
-        Frame nextFrame = frames.ofFirst();
-        while (nextFrame != null) {
+    private static void printSymbol(Frame currentFrame) {
+        while (currentFrame != null) {
             try {
-                ResultView.printSymbol(nextFrame);
-                nextFrame = nextFrame.next();
+                ResultView.printSymbol(currentFrame);
+                currentFrame = currentFrame.next();
             } catch (NeedAdditionalFrameException e) {
                 return;
             }
@@ -54,16 +66,15 @@ public class BowlingGameController {
 
     }
 
-    private static int printTotalScore(Frames frames) {
-        Frame testFrame = frames.ofFirst();
+    private static int printTotalScore(Frame currentFrame) {
         int totalScore = 0;
         int round = 0;
-        while (testFrame != null) {
+        while (currentFrame != null) {
             try {
-                round ++;
-                totalScore += Integer.parseInt(testFrame.getScore().getScore());
+                totalScore += Integer.parseInt(currentFrame.getScore().getScore());
                 ResultView.printResult(totalScore);
-                testFrame = testFrame.next();
+                currentFrame = currentFrame.next();
+                round ++;
             } catch (NeedAdditionalFrameException e) {
                 return round;
             }
--- src/main/java/step4/view/ResultView.java
+++ src/main/java/step4/view/ResultView.java
@@ -3,7 +3,6 @@ package step4.view;
 import step4.domain.Frame;
 
 public class ResultView {
-    private static String RESULT_COLUMN = "| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |";
     private static String FIRST_COLUMN = "|  %4s |";
     private static String MIDDLE_COLUMN = " %3s  |";
 
