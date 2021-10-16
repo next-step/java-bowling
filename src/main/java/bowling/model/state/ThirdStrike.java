2c0e8564297dad7c99580ccccd6803ebc5392abb
Gyeongseo Woo <ugs1575@gmail.com>
2021-10-16 14:10:11 +0900

    feature: ThirdStrikeTest 추가

--- src/main/java/bowling/model/state/ThirdStrike.java
+++ src/main/java/bowling/model/state/ThirdStrike.java
@@ -1,8 +1,17 @@
 package bowling.model.state;
 
+import bowling.CannotBowlException;
+import bowling.model.Pin;
+
 public class ThirdStrike extends Finished{
+    private final Pin firstPins;
+
+    public ThirdStrike() {
+        this.firstPins = new Pin(10);
+    }
+
     @Override
-    public State bowl(int countOfPin) {
-        return null;
+    public State bowl(int firstPins) throws CannotBowlException {
+        throw new CannotBowlException();
     }
 }
