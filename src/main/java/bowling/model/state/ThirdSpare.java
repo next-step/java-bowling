c01a6e50026d8292f82e6df3f424063038440d62
Gyeongseo Woo <ugs1575@gmail.com>
2021-10-16 14:09:47 +0900

    feature: ThirdSpareTest 추가

--- src/main/java/bowling/model/state/ThirdSpare.java
+++ src/main/java/bowling/model/state/ThirdSpare.java
@@ -1,10 +1,17 @@
 package bowling.model.state;
 
 import bowling.CannotBowlException;
+import bowling.model.Pin;
 
 public class ThirdSpare extends Finished {
+    private final Pin firstPins;
+
+    public ThirdSpare() {
+        this.firstPins = new Pin(10);
+    }
+
     @Override
-    public State bowl(int countOfPin) throws CannotBowlException {
-        return null;
+    public State bowl(int firstPins) throws CannotBowlException {
+        throw new CannotBowlException();
     }
 }
