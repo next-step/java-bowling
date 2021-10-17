044979ff090de70bde4999348b8578ca7e337623
Gyeongseo Woo <ugs1575@gmail.com>
2021-10-18 00:52:05 +0900

    refactor: 컨트롤러 로직 변경

--- src/main/java/bowling/controller/Main.java
+++ src/main/java/bowling/controller/Main.java
@@ -4,24 +4,23 @@ import static bowling.view.InputView.ask;
 import static bowling.view.InputView.askDigit;
 import static bowling.view.OutputView.printResult;
 
-import bowling.model.User;
+import bowling.model.BowlingGame;
 import bowling.model.frame.Board;
-import bowling.model.frame.Frame;
-import bowling.model.frame.NormalFrame;
 
 public class Main {
     public static void main(String[] args) {
-        String userName = ask("플레이어 이름은(3 english letters)?:");
-        User user = new User(userName);
+        BowlingGame bowlingGame = new BowlingGame();
+        int numberOfPlayers = askDigit("How many people?");
+        for (int i = 1; i <= numberOfPlayers; i++) {
+            String userName = ask("플레이어 이름은(3 english letters)?:");
+            bowlingGame.register(userName);
+        }
 
-        Frame currentFrame = new NormalFrame(1);
-        Frame firstFrame = currentFrame;
-
-        while (!currentFrame.isFinish()) {
-            int falledPins = askDigit(currentFrame.getNo() + "프레임 투구 : ");
-            currentFrame = currentFrame.bowl(falledPins);
-            Board board = firstFrame.createBoard();
-            printResult(userName, board);
+        while (!bowlingGame.isEndGame()) {
+            String playerName = bowlingGame.getName();
+            int falledPins = askDigit(playerName + "의 " + bowlingGame.getNo() + "프레임 투구 : ");
+            Board board = bowlingGame.bowl(falledPins);
+            printResult(playerName, board);
         }
     }
 }
