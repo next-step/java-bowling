7a4ace6ca57dcff98e2afcac9f41494d691f705a
조원진 <>
2021-10-18 21:39:00 +0900

    feat(BasicFrame) : 점수 계산, 보너스 점수 계산 기능 추가

--- src/main/java/bowling/domain/frame/BasicFrame.java
+++ src/main/java/bowling/domain/frame/BasicFrame.java
@@ -2,12 +2,17 @@ package bowling.domain.frame;
 
 import bowling.domain.FrameResult;
 import bowling.domain.PinCount;
+import bowling.domain.Score;
 import bowling.domain.state.State;
 import bowling.domain.state.StateFactory;
+
+import java.util.Objects;
 
 public class BasicFrame extends AbstractFrame {
 
     private State state;
+
+    private Frame next;
 
     public BasicFrame(final FrameNumber frameNumber) {
         super(frameNumber);
@@ -18,7 +23,8 @@ public class BasicFrame extends AbstractFrame {
     public Frame play(final PinCount pinCount) {
         state = this.state.play(pinCount);
         if (state.isFinished()) {
-            return createFrame();
+            next = createFrame();
+            return next;
         }
         return this;
     }
@@ -32,7 +38,29 @@ public class BasicFrame extends AbstractFrame {
     }
 
     @Override
+    public Score calculateScore() {
+        Score score = state.getScore();
+        if (score.canCalculate() || hasNotNext()) {
+            return score;
+        }
+        return next.plusBonusScore(score);
+    }
+
+    @Override
+    public Score plusBonusScore(final Score beforeScore) {
+        Score score = state.calculateBonusScore(beforeScore);
+        if (score.canCalculate() || hasNotNext()) {
+            return score;
+        }
+        return next.plusBonusScore(score);
+    }
+
+    private boolean hasNotNext() {
+        return Objects.isNull(next);
+    }
+
+    @Override
     public FrameResult makeResult() {
-        return new FrameResult(state.showIndication());
+        return new FrameResult(state.showIndication(), calculateScore());
     }
 }
--- src/test/java/bowling/domain/frame/BasicFrameTest.java
+++ src/test/java/bowling/domain/frame/BasicFrameTest.java
@@ -1,10 +1,13 @@
 package bowling.domain.frame;
 
+import bowling.domain.Left;
 import bowling.domain.PinCount;
+import bowling.domain.Score;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class BasicFrameTest {
     @DisplayName("플레이 후 프레임이 종료되지 않으면 같은 프레임을 반환")
@@ -32,6 +35,60 @@ public class BasicFrameTest {
         Frame next = frame.play(PinCount.of(5));
         next = next.play(PinCount.of(5));
         assertThat(next.getFrameNumber()).isEqualTo(frameNumber.next());
+    }
+
+    @DisplayName("한 번만 투구 했을 경우 canCalculate false 리턴")
+    @Test
+    void canCalculate_ReturnFalse_IfOncePitched() {
+        Frame frame = new BasicFrame(FrameNumber.of(1));
+        Score score = frame.play(PinCount.of(5))
+                .calculateScore();
+        assertThat(score.canCalculate()).isFalse();
+    }
+
+    @DisplayName("스트라이크 후 한 번만 투구 했을 경우 canCalculate false 리턴")
+    @Test
+    void canCalculate_ReturnFalse_IfStrikeAndOncePitched() {
+        Frame frame = new BasicFrame(FrameNumber.of(1));
+        Score score = frame.play(PinCount.MAX_PINS)
+                .play(PinCount.MAX_PINS)
+                .calculateScore();
+        assertThat(score.canCalculate()).isFalse();
+    }
+
+    @DisplayName("미스일 때 점수 계산")
+    @Test
+    void calculateScore_ReturnScore_IfMiss() {
+        Frame frame = new BasicFrame(FrameNumber.of(1));
+        frame.play(PinCount.of(6)).play(PinCount.of(3));
+        assertAll(
+                () -> assertThat(frame.calculateScore()).isEqualTo(new Score(9, Left.ZERO)),
+                () -> assertThat(frame.calculateScore().canCalculate()).isTrue()
+        );
+    }
+
+    @DisplayName("스페어 후 투구 한 경우 점수 계산")
+    @Test
+    void calculateScore_ReturnScore_IfSpareAndOncePitched() {
+        Frame frame = new BasicFrame(FrameNumber.of(1));
+        Frame next = frame.play(PinCount.of(6)).play(PinCount.of(4));
+        next.play(PinCount.of(8));
+        assertAll(
+                () -> assertThat(frame.calculateScore()).isEqualTo(new Score(18, Left.ZERO)),
+                () -> assertThat(frame.calculateScore().canCalculate()).isTrue()
+        );
+    }
+
+    @DisplayName("스트라이크 후 두 번 투구 한 경우 점수 계산")
+    @Test
+    void calculateScore_ReturnScore_IfStrikeAndTwicePitched() {
+        Frame frame = new BasicFrame(FrameNumber.of(1));
+        Frame next = frame.play(PinCount.MAX_PINS).play(PinCount.MAX_PINS);
+        next.play(PinCount.MAX_PINS);
+        assertAll(
+                () -> assertThat(frame.calculateScore()).isEqualTo(new Score(30, Left.ZERO)),
+                () -> assertThat(frame.calculateScore().canCalculate()).isTrue()
+        );
     }
 
 }
