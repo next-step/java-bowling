b296ed53af37c528f849af158dc2a88539052e9c
조원진 <>
2021-10-18 21:39:43 +0900

    feat(Frame) : 점수 계산, 보너스 점수 계산 기능 추가

--- src/main/java/bowling/domain/frame/Frame.java
+++ src/main/java/bowling/domain/frame/Frame.java
@@ -2,9 +2,14 @@ package bowling.domain.frame;
 
 import bowling.domain.FrameResult;
 import bowling.domain.PinCount;
+import bowling.domain.Score;
 
 public interface Frame {
     Frame play(PinCount pinCount);
+
+    Score calculateScore();
+
+    Score plusBonusScore(Score score);
 
     FrameNumber getFrameNumber();
 
