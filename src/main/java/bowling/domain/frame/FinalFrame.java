e47e90029deb8a27da0a4ba854ba7b30df262cc9
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-10-21 13:16:24 +0900

    feat: bonus 필드 추가

--- src/main/java/bowling/domain/frame/FinalFrame.java
+++ src/main/java/bowling/domain/frame/FinalFrame.java
@@ -6,24 +6,28 @@ import bowling.domain.score.Pin;
 import bowling.domain.score.Score;
 import bowling.domain.state.State;
 import bowling.domain.state.running.Ready;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class FinalFrame implements Frame {
 
     private final int round;
     private State state;
+    private List<State> bonus;
 
-    private FinalFrame(int round, State state) {
+    private FinalFrame(int round, State state, List<State> bonus) {
         this.round = round;
         this.state = state;
+        this.bonus = bonus;
     }
 
     public static Frame from(int round, State state) {
-        return new FinalFrame(round, state);
+        return new FinalFrame(round, state, new ArrayList<>());
     }
 
     public static Frame createFinalFrame() {
-        return new FinalFrame(FINAL_ROUND, new Ready());
+        return new FinalFrame(FINAL_ROUND, new Ready(), new ArrayList<>());
     }
 
     @Override
