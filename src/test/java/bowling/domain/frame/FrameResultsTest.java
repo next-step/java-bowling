668c9fe04a00d62f6e35852e848cf2a270385973
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-10-24 03:16:46 +0900

    refactor: 테스트 위치 변경

--- src/test/java/bowling/domain/frame/FrameResultsTest.java
+++ src/test/java/bowling/domain/frame/FrameResultsTest.java
@@ -7,7 +7,6 @@ import bowling.domain.state.State;
 import bowling.domain.state.finish.Strike;
 import java.util.LinkedList;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class FrameResultsTest {
@@ -58,77 +57,6 @@ class FrameResultsTest {
 
         // then
         assertThat(result).isEqualTo(expected);
-    }
-
-    @Test
-    @DisplayName("이전 totalScore를 받아 현재 totalScore를 변환할 수 있다.")
-    void addTotalScoreTest() {
-
-        // given
-        int beforeTotalScore = 20;
-
-        FrameResult expected = FrameResult.of(30, 10, "X");
-
-        // when
-        FrameResult result = FrameResult.ofScoreAndDesc(10, "X");
-        result.addTotalScore(beforeTotalScore);
-
-        // then
-        assertThat(result).isEqualTo(expected);
-    }
-
-    @Test
-    @DisplayName("현재 score가 계산 불가능한 상황이면 totalScore를 더하지 않는다.")
-    void notAddTotalScoreTest() {
-
-        // given
-        int beforeTotalScore = 20;
-
-        FrameResult expected = FrameResult.of(-1, -1, "");
-
-        // when
-        FrameResult result = FrameResult.ofScoreAndDesc(-1, "");
-        result.addTotalScore(beforeTotalScore);
-
-        // then
-        assertThat(result).isEqualTo(expected);
-    }
-
-    @Nested
-    @DisplayName("totalSocre String 반환 기능")
-    class totalScoreToStringTest {
-
-        @Test
-        @DisplayName("total score가 계산 가능한 상황이면 반환해야한다.")
-        void success() {
-
-            // given
-            FrameResult frameResult = FrameResult.of(30, 10, "X");
-
-            String expected = "30";
-
-            // when
-            String result = frameResult.totalScoreToString();
-
-            // then
-            assertThat(result).isEqualTo(expected);
-        }
-
-        @Test
-        @DisplayName("total score가 계산 불가능한 상황이면 공백을 반환해야 한다.")
-        void fail() {
-
-            // given
-            FrameResult frameResult = FrameResult.of(-1, 10, "X");
-
-            String expected = "";
-
-            // when
-            String result = frameResult.totalScoreToString();
-
-            // then
-            assertThat(result).isEqualTo(expected);
-        }
     }
 
 }
