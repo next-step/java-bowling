62ab4053d9322de54a6b0552c2f58273aa6e7b82
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-10-27 14:27:08 +0900

    feat: 기존 UserFrame 제거 및 포괄적인 UserFrameResult 객체 생성
    - user : 진행 유저
    - firstFrame : 첫번째 frame
    - frameResults : frame 결과물

---
+++ src/main/java/bowling/domain/userframeresult/UserFrameResult.java
@@ -0,0 +1,23 @@
+package bowling.domain.userframeresult;
+
+import bowling.domain.frame.Frame;
+import bowling.domain.frame.FrameResults;
+import bowling.domain.user.User;
+
+public class UserFrameResult {
+
+    private final User user;
+    private final Frame firstFrame;
+    private FrameResults frameResults;
+
+    private UserFrameResult(User user, Frame firstFrame, FrameResults frameResults) {
+        this.user = user;
+        this.firstFrame = firstFrame;
+        this.frameResults = frameResults;
+    }
+
+    public static UserFrameResult fromUserAndFirstFrame(User user, Frame firstFrame) {
+        return new UserFrameResult(user, firstFrame, FrameResults.createFrameResultsByFirstFrame(firstFrame));
+    }
+
+}
