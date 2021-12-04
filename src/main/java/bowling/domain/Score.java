c7655f002befab83e36c02a81e9508920ce89161
csh0034 <csh0034@gmail.com>
2021-12-04 15:20:36 +0900

    refactor: Score toString 구현

--- src/main/java/bowling/domain/Score.java
+++ src/main/java/bowling/domain/Score.java
@@ -70,4 +70,12 @@ public class Score {
 	public int hashCode() {
 		return Objects.hash(score, left);
 	}
+
+	@Override
+	public String toString() {
+		return "Score{" +
+			"score=" + score +
+			", left=" + left +
+			'}';
+	}
 }
