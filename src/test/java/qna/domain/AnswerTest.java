50cdf99710a5571fdc5e25a4093d19d2d2667f50
Yang DoHyeok <didehgur1@naver.com>
2021-12-07 20:49:38 +0900

    Refactor : 질문 삭제기능 리팩토링 (#717)
    Co-authored-by: marty.ni <marty.ni@kakaocorp.com>

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,29 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @DisplayName("답변의 작성자가 User와 다르면 CannotDeleteException")
+    @Test
+    void deleteAnswerTest() {
+        assertThatThrownBy(() -> A1.delete(UserTest.SANJIGI)).isInstanceOf(CannotDeleteException.class);
+        assertThatThrownBy(() -> A2.delete(UserTest.JAVAJIGI)).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @DisplayName("답변의 작성자가 user와 같으면 deleted가 true 가 된다.")
+    @Test
+    void deleteTest() {
+        A1.delete(UserTest.JAVAJIGI);
+
+        assertThat(A1.isDeleted()).isTrue();
+    }
+
 }
