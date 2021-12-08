90228556cce44c973c435ed4a633eacc69881c2d
marty.ni <marty.ni@kakaocorp.com>
2021-12-08 23:35:10 +0900

    Feat : Miss 구현

--- src/main/java/bowling/domain/state/Finished.java
+++ src/main/java/bowling/domain/state/Finished.java
@@ -1,6 +1,14 @@
 package bowling.domain.state;
 
+import bowling.domain.Pin;
+
 public abstract class Finished implements State {
+
+    @Override
+    public State bowl(Pin pin) {
+        throw new UnsupportedOperationException();
+    }
+
     @Override
     public boolean isFinished() {
         return true;
--- src/main/java/bowling/domain/state/Miss.java
+++ src/main/java/bowling/domain/state/Miss.java
@@ -6,12 +6,21 @@ import java.util.Objects;
 
 public class Miss extends Finished {
 
+    private static final String NO_MISS_EXCEPTION_MESSAGE = "쓰러진 핀의 갯수가 10개면 Spare 처리가 되어야 합니다.";
+
     private final Pin firstPin;
     private final Pin secondPin;
 
     public Miss(Pin firstPin, Pin secondPin) {
+        validPins(firstPin, secondPin);
         this.firstPin = firstPin;
         this.secondPin = secondPin;
+    }
+
+    private void validPins(Pin firstPin, Pin secondPin) {
+        if (firstPin.add(secondPin).isMaxCount()) {
+            throw new IllegalArgumentException(NO_MISS_EXCEPTION_MESSAGE);
+        }
     }
 
     @Override
