3992599a9586ecb8bc5f2f1f3659e686cb78b3e4
marty.ni <marty.ni@kakaocorp.com>
2021-12-12 16:00:03 +0900

    Feat : FrameResults 생성

--- src/test/java/bowling/domain/result/FrameResultsTest.java
+++ src/test/java/bowling/domain/result/FrameResultsTest.java
@@ -1,5 +1,18 @@
 package bowling.domain.result;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class FrameResultsTest {
 
+    @DisplayName("정상 생성 테스트")
+    @Test
+    void createTest() {
+        assertThat(new FrameResults(Arrays.asList(new FrameResult("5"))))
+                .isEqualTo(new FrameResults(Arrays.asList(new FrameResult("5"))));
+    }
 }
