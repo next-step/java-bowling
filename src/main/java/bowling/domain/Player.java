637da0bef65c835756b20b628acd73e6aeb3aabb
doorisopen <doorwasopen@gmail.com>
2021-12-13 23:29:47 +0900

    feat: step2 플레이어를 생성한다

---
+++ src/main/java/bowling/domain/Player.java
@@ -0,0 +1,28 @@
+package bowling.domain;
+
+import java.util.Objects;
+
+public class Player {
+    private final Name name;
+
+    private Player(String name) {
+        this.name = Name.from(name);
+    }
+
+    public static Player from(String name) {
+        return new Player(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
