c66f9a6dc59c75563a8d75920cf8bdf99e6ac528
Han <hankim@tapasmedia.co>
2021-12-13 21:45:38 +0900

    test : add test

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,16 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    void to_delete() {
+        assertThat(A1.isDeleted()).isFalse();
+        assertThat(A1.toDeleted().isDeleted()).isTrue();
+    }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,31 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    void to_delete() {
+        assertThat(Q1.isDeleted()).isFalse();
+        assertThat(Q1.toDeleted().isDeleted()).isTrue();
+    }
+
+    @Test
+    void get_delete_history() {
+        assertThat(Q2.getDeleteHistory().size()).isEqualTo(0);
+
+        Q2.toDeleted();
+        assertThat(Q2.getDeleteHistory().size()).isEqualTo(1);
+    }
+
+    @Test
+    void get_delete_history_with_answers() {
+        Q2.addAnswer(AnswerTest.A1);
+        Q2.toDeleted();
+        assertThat(Q2.getDeleteHistory().size()).isEqualTo(2);
+    }
 }
