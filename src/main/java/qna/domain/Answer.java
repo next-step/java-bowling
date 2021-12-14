095601d13ea8b230e087412d91d9a0fd7d203d37
DongHyo <kdhyo98@gmail.com>
2021-12-15 01:16:30 +0900

    feat : 답글 삭제 구현

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -1,12 +1,18 @@
 package qna.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import org.springframework.transaction.annotation.Transactional;
+import qna.CannotDeleteException;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
 
-import javax.persistence.*;
-
 @Entity
 public class Answer extends AbstractEntity {
+
     @ManyToOne(optional = false)
     @JoinColumn(foreignKey = @ForeignKey(name = "fk_answer_writer"))
     private User writer;
@@ -30,17 +36,27 @@ public class Answer extends AbstractEntity {
     public Answer(Long id, User writer, Question question, String contents) {
         super(id);
 
-        if(writer == null) {
+        if (writer == null) {
             throw new UnAuthorizedException();
         }
 
-        if(question == null) {
+        if (question == null) {
             throw new NotFoundException();
         }
 
         this.writer = writer;
         this.question = question;
         this.contents = contents;
+    }
+
+    @Transactional(rollbackFor = CannotDeleteException.class)
+    public Answer delete(User user) throws CannotDeleteException {
+        if (!this.isOwner(user)) {
+            throw new CannotDeleteException("답글을 삭제할 권한이 없습니다.");
+        }
+        
+        deleted = true;
+        return this;
     }
 
     public Answer setDeleted(boolean deleted) {
@@ -72,4 +88,5 @@ public class Answer extends AbstractEntity {
     public String toString() {
         return "Answer [id=" + getId() + ", writer=" + writer + ", contents=" + contents + "]";
     }
+
 }
--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,23 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    void 작성자가_아닐시_예외처리() {
+        assertThatExceptionOfType(CannotDeleteException.class)
+            .isThrownBy(() -> A1.delete(UserTest.SANJIGI));
+    }
+
+    @Test
+    void 삭제성공() throws CannotDeleteException {
+         assertThat(A1.delete(UserTest.JAVAJIGI).isDeleted()).isTrue();
+    }
 }
