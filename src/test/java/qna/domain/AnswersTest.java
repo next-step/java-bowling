1a508f7ac132b1d5156a729810d24b44318ba182
soosue <yunsoo0510@daum.net>
2021-12-15 09:47:54 +0900

    [Test] Answer 테스트 코드 추가

--- src/test/java/qna/domain/AnswersTest.java
+++ src/test/java/qna/domain/AnswersTest.java
@@ -1,5 +1,6 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -9,13 +10,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AnswersTest {
 
+    private final Answers answers =
+            Answers.from(
+                    Arrays.asList(
+                            new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1"),
+                            new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2")));
+
+    @DisplayName("답변을 모두 삭제")
     @Test
     void deleteAll() {
-        Answers answers = Answers.from(
-                Arrays.asList(
-                        new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1"),
-                        new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2")));
-
         List<Answer> answerList = answers.value();
 
         answerList.forEach(answer -> assertThat(answer.isDeleted()).isFalse());
@@ -24,5 +27,4 @@ public class AnswersTest {
         assertThat(answerList.size()).isGreaterThan(0);
         answerList.forEach(answer -> assertThat(answer.isDeleted()).isTrue());
     }
-
 }
