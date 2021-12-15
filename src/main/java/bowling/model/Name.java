8b0cac2ca03ee19a3b44c449f551780bcb3d4cbc
jjoy <ekdnlt1995@gmail.com>
2021-12-15 21:45:29 +0900

    feat : 플레이어 이름 Name 객체 개발

---
+++ src/main/java/bowling/model/Name.java
@@ -0,0 +1,20 @@
+package bowling.model;
+
+import java.util.regex.Pattern;
+
+public class Name {
+
+    private static final String NAME_PATTERN = "^[a-zA-Z]{3}$";
+    private final String name;
+
+    public Name(String name) {
+        this.name = name.trim();
+        checkPatternMatch(this.name);
+    }
+
+    private void checkPatternMatch(String name) {
+        if(!Pattern.matches(NAME_PATTERN,name)) {
+            throw new IllegalArgumentException("이름은 영어로 3글자여야 합니다.");
+        }
+    }
+}
