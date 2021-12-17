242c3b35b8e410aa27b6dda2df5deb8a8e049502
giyeon95 <giyeon15@gmail.com>
2021-12-17 23:10:11 +0900

    delete: `ExceptionWrapper` utils 성 메소드 삭제 (기존 Exception을 RuntimeException 으로 변경)

--- src/main/java/qna/utils/ConsumerWithException.java
+++
@@ -1,7 +0,0 @@
-package qna.utils;
-
-@FunctionalInterface
-public interface ConsumerWithException<T, E extends Exception> {
-
-    void accept(T t) throws E;
-}
--- src/main/java/qna/utils/FunctionWithException.java
+++
@@ -1,7 +0,0 @@
-package qna.utils;
-
-@FunctionalInterface
-public interface FunctionWithException<T, R, E extends Exception> {
-
-    R apply(T t) throws E;
-}
