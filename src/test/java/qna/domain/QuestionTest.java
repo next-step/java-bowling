65ca26218785091c57fffe90b0f972f5c7270da0
giyeon95 <giyeon15@gmail.com>
2021-12-18 21:11:37 +0900

    tests: 질문 삭제 테스트 케이스 보강
    ```
    비어 있는 테스트가 있네요!!
    
    _Originally posted by @heesunjeong in https://github.com/next-step/java-bowling/pull/738#discussion_r771795699_
    ```

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,10 +1,12 @@
 package qna.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
 
 public class QuestionTest {
 
@@ -18,11 +20,17 @@ public class QuestionTest {
     }
 
     @Test
+    @DisplayName("삭제하는 사용자가 저자와 같다면, 정상적으로 삭제된다.")
     void deleteTest() {
         q1.delete(UserTest.JAVAJIGI);
-
         assertThat(q1.isDeleted()).isTrue();
+    }
 
+    @Test
+    @DisplayName("삭제시 저자와 다르다면, 예외가 발생한다.")
+    void deleteExceptionTest() {
+        assertThatExceptionOfType(CannotDeleteException.class)
+            .isThrownBy(() -> q1.delete(UserTest.SANJIGI));
     }
 
     @Test
