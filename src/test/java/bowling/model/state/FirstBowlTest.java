86ac50251187ab4fe1209a61348f11ad7f3f436a
jjoy <ekdnlt1995@gmail.com>
2021-12-18 18:32:24 +0900

    feat : 두 타구 합이 10 넘을 때 예외발생 테스트

--- src/test/java/bowling/model/state/FirstBowlTest.java
+++ src/test/java/bowling/model/state/FirstBowlTest.java
@@ -6,7 +6,19 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
 class FirstBowlTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"6,5", "9,7"})
+    @DisplayName("2번째 타구 까지 쓰러뜨린 핀이 10이 넘으면 예외발생 테스트")
+    void pinSumOverTenExceptionTest(int firstPin, int secondPin) {
+        FirstBowl firstBowl = new FirstBowl(new Pins(firstPin));
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> firstBowl.bowl(secondPin))
+                .withMessage("투구로 쓰러진 핀의 총 개수는 10개를 넘을 수 없습니다.");
+    }
 
     @ParameterizedTest
     @CsvSource(value = {"5,5", "4,6", "7,3"})
