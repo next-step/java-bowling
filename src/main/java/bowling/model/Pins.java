8a426d1afd7b63f22a0c2e93e4887d87b063576c
jjoy <ekdnlt1995@gmail.com>
2021-12-18 17:31:39 +0900

    feat : Pins 객체 개발

---
+++ src/main/java/bowling/model/Pins.java
@@ -0,0 +1,47 @@
+package bowling.model;
+
+public class Pins {
+
+    private static final int MAX_PIN_COUNT = 10;
+    private static final int MIN_PIN_COUNT = 0;
+
+    private final int knockedDownPin;
+
+    public Pins(int knockedDownPin) {
+        checkPinCount(knockedDownPin);
+        this.knockedDownPin = knockedDownPin;
+    }
+
+    private void checkPinCount(int knockedDownPin) {
+        if(knockedDownPin < MIN_PIN_COUNT || knockedDownPin > MAX_PIN_COUNT) {
+            throw new IllegalArgumentException("투구 값은 0과 10 사이의 값이어야 합니다.");
+        }
+    }
+
+    public Pins(int knockedDownPin1, int knockedDownPin2) {
+        checkPinCount(knockedDownPin1, knockedDownPin2);
+        this.knockedDownPin = knockedDownPin1 + knockedDownPin2;
+    }
+
+    private void checkPinCount(int knockedDownPin1, int knockedDownPin2) {
+        if(knockedDownPin1 + knockedDownPin2 > MAX_PIN_COUNT) {
+            throw new IllegalArgumentException("투구로 쓰러진 핀의 총 개수는 10개를 넘을 수 없습니다.");
+        }
+    }
+
+    public static Pins knockedDown(int knockedDownPin) {
+        return new Pins(knockedDownPin);
+    }
+
+    public boolean isSpare(Pins firstPin) {
+        return new Pins(this.knockedDownPin, firstPin.knockedDownPin).isStrike();
+    }
+
+    public boolean isStrike() {
+        return this.knockedDownPin == MAX_PIN_COUNT;
+    }
+
+    public int getKnockedDownPin() {
+        return this.knockedDownPin;
+    }
+}
