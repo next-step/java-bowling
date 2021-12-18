9476f249dcb052745369a66b261d9b767d05da4a
jjoy <ekdnlt1995@gmail.com>
2021-12-19 03:45:25 +0900

    feat : 입력받은 값이 숫자인지 체크하는 validation 추가

--- src/main/java/bowling/game/BowlingGameApp.java
+++ src/main/java/bowling/game/BowlingGameApp.java
@@ -6,17 +6,21 @@ import bowling.model.Name;
 
 public class BowlingGameApp {
     public static void main(String[] args) {
-        Name name = new Name(InputView.getStringValue("플레이어 이름은 (3 english letters)? : "));
-        GameResults gameResults = new GameResults();
-        ResultView.printResults(name,gameResults);
-        BowlingGame game = new BowlingGame(name);
-        while(game.canPlay()){
-            gameResults.set(game.play());
+        try {
+            Name name = new Name(InputView.getStringValue("플레이어 이름은 (3 english letters)? : "));
+            GameResults gameResults = new GameResults();
             ResultView.printResults(name,gameResults);
-        }
-        if(game.bonusGame()) {
-            gameResults.set(game.play());
-            ResultView.printResults(name,gameResults);
+            BowlingGame game = new BowlingGame(name);
+            while(game.canPlay()){
+                gameResults.set(game.play());
+                ResultView.printResults(name,gameResults);
+            }
+            if(game.bonusGame()) {
+                gameResults.set(game.play());
+                ResultView.printResults(name,gameResults);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage() + " 게임을 종료하겠습니다.");
         }
     }
 }
--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -1,14 +1,24 @@
 package bowling.view;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class InputView {
 
     private static Scanner scanner = new Scanner(System.in);
+    private static final Pattern intPattern = Pattern.compile("-?\\d+");
 
     public static int getIntValue(String message) {
         System.out.print(message);
-        return scanner.nextInt();
+        String input = scanner.nextLine();
+        checkIsInt(input);
+        return Integer.parseInt(input);
+    }
+
+    private static void checkIsInt(String input) {
+        if(!intPattern.matcher(input).matches()) {
+            throw new IllegalArgumentException("값이 숫자가 아닙니다.");
+        }
     }
 
     public static String getStringValue(String message) {
