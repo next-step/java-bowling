9e231be0faa15bcb68814953cf7e21fc2df5e5e1
orgojy <giveawesome@gmail.com>
2021-12-20 22:57:19 +0900

    chore(QuestionTest.java): 테스트 케이스의 한글 주석 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,15 +1,18 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import qna.CannotDeleteException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-class QuestionTest {
+@DisplayName("질문 테스트")
+public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
 
+    @DisplayName("질문 삭제")
     @Test
     void delete() throws CannotDeleteException {
         // given
@@ -20,6 +23,7 @@ class QuestionTest {
         assertThat(question.isDeleted()).isTrue();
     }
 
+    @DisplayName("질문 삭제시 예외 확인 - 본인이 작성한 질문이 아닌 경우")
     @Test
     void deleteFailed() {
         // given
