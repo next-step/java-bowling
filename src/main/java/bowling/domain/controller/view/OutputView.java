6da5d0cd84d7f1c650d95225938b5de9c654ba52
ydh6226 <ydh6226@naver.com>
2021-12-21 21:43:00 +0900

    feat: OutputView 구현

--- src/main/java/bowling/domain/controller/view/OutputView.java
+++ src/main/java/bowling/domain/controller/view/OutputView.java
@@ -14,41 +14,45 @@ import static java.lang.String.format;
 
 public class OutputView {
 
-    private static final StringBuilder STRING_BUILDER = new StringBuilder();
+    private static final StringBuilder STRING_BUILDER = new StringBuilder(1000);
     private static final int NUMBER_OF_FRAME = 10;
-    private static final String BOARD_OF_HEAD = "| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |\n|";
-    private static final String BOARD_FORMAT = " %-4s |";
-    private static final String BOARD_EMPTY = "      |";
     private static final int FIRST_PIN_INDEX = 0;
+
+    private static final String BOARD_HEAD = "| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |\n";
+    private static final String BOARD_FORMAT = " %-4s |";
+    private static final String BOARD_CLOSED_FORMAT = "| %-4s |";
+    private static final String BOARD_EMPTY = "      |";
+    private static final String BOARD_CLOSED_EMPTY = "|      |";
 
     private OutputView() {
     }
 
     public static void showBowlingGame(BowlingGame bowlingGame) {
-        List<Bowling> bowlings = bowlingGame.bowlings();
-        bowlings.forEach(OutputView::showBowling);
-
+        STRING_BUILDER.append(BOARD_HEAD);
+        bowlingGame.bowlings()
+                .forEach(OutputView::appendBowlingView);
+        flush();
     }
 
-    public static void showBowling(Bowling bowling) {
-        System.out.print(BOARD_OF_HEAD);
-        append(bowling.nameOfParticipant());
-
+    private static void appendBowlingView(Bowling bowling) {
+        appendParticipantName(bowling.nameOfParticipant());
         appendBowlView(bowling.bowls());
-        appendNewLine();
-
         appendScoreView(bowling.scores());
-        appendNewLine();
-        appendNewLine();
-        flushBuilder();
+    }
+
+    private static void appendParticipantName(String nameOfParticipant) {
+        String nameView = format(BOARD_CLOSED_FORMAT, nameOfParticipant);
+        STRING_BUILDER.append(nameView);
     }
 
     private static void appendBowlView(List<Bowl> bowls) {
-        bowls.forEach(bowl -> append(getBowlView(bowl)));
+        for (Bowl bowl : bowls) {
+            STRING_BUILDER.append(format(BOARD_FORMAT, getBowlView(bowl)));
+        }
         appendEmptyBody(bowls);
     }
 
-    public static String getBowlView(Bowl bowl) {
+    private static String getBowlView(Bowl bowl) {
         List<Pin> pins = bowl.pins();
         if (pins.size() == 1) {
             return pinView(pins.get(0));
@@ -79,19 +83,16 @@ public class OutputView {
         return String.valueOf(pin.getHitCount());
     }
 
-    public static void append(String string) {
-        STRING_BUILDER.append(format(BOARD_FORMAT, string));
-    }
-
     private static void appendEmptyBody(List<?> objects) {
         int size = objects.size();
         for (int number = size; number < NUMBER_OF_FRAME; number++) {
             STRING_BUILDER.append(BOARD_EMPTY);
         }
+        STRING_BUILDER.append("\n");
     }
 
     private static void appendScoreView(List<Score> scores) {
-        STRING_BUILDER.append("|      |");
+        STRING_BUILDER.append(BOARD_CLOSED_EMPTY);
         for (Score score : scores) {
             STRING_BUILDER.append(getScoreView(score));
         }
@@ -100,16 +101,12 @@ public class OutputView {
 
     private static String getScoreView(Score score) {
         if (score.canCalculate()) {
-            return String.format(BOARD_FORMAT, score.value());
+            return format(BOARD_FORMAT, score.value());
         }
         return BOARD_EMPTY;
     }
 
-    private static void appendNewLine() {
-        STRING_BUILDER.append("\n");
-    }
-
-    private static void flushBuilder() {
+    private static void flush() {
         System.out.println(STRING_BUILDER);
         STRING_BUILDER.setLength(0);
     }
