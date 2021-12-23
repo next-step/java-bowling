6a7802d2bf38cf8f2a79b0a9c5ff6b52a8ac4c8e
soo <yunsoo0510@daum.net>
2021-12-24 03:40:58 +0900

    [Feat] Main, UI 구현

---
+++ src/main/java/bowling/Main.java
@@ -0,0 +1,32 @@
+package bowling;
+
+import bowling.view.InputView;
+import bowling.view.OutputView;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Players players = new Players(Arrays.asList(InputView.readPlayerName()));
+        OutputView.printBowlingBoard(players);
+
+        while (!players.isEndGame()) {
+            playBowlingGames(players);
+        }
+    }
+
+    private static void playBowlingGames(Players players) {
+        for (Player player : players.value()) {
+            playBowlingGameAndPrintScore(players, player);
+        }
+    }
+
+    private static void playBowlingGameAndPrintScore(Players players, Player player) {
+        player.prepareFrame();
+        while (player.isNotCurrentFrameEnd()) {
+            int knockedOutCount = InputView.readKnockedOutCountOf(player.name());
+            player.bowl(knockedOutCount);
+            OutputView.printBowlingBoard(players);
+        }
+    }
+}
