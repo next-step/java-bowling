a024105d5a691495763383d5ecb9b831e7662ae6
soo <yunsoo0510@daum.net>
2021-12-23 21:54:13 +0900

    [Feat] KnockedPinCount 테스트코드 작성 및 구현

---
+++ src/main/java/bowling/KnockedPinCount.java
@@ -0,0 +1,36 @@
+package bowling;
+
+public class KnockedPinCount {
+    public static final String INVALID_KNOCK_OUT_COUNT_MESSAGE = "쓰러뜨린 핀의 개수는 0~10개입니다.";
+
+    private static final int MIN_VALUE = 0;
+    private static final int MAX_VALUE = 10;
+
+    private final int value;
+
+    public KnockedPinCount(int count) {
+        if (count < MIN_VALUE || count > MAX_VALUE) {
+            throw new IllegalArgumentException(INVALID_KNOCK_OUT_COUNT_MESSAGE);
+        }
+        value = count;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        KnockedPinCount that = (KnockedPinCount) o;
+
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value;
+    }
+}
---
+++ src/test/java/bowling/KnockedPinCountTest.java
@@ -0,0 +1,24 @@
+package bowling;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class KnockedPinCountTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 10})
+    void 쓰러뜨린_핀의_개수는_0에서_10개_동등성가짐(int count) {
+        assertThat(new KnockedPinCount(count)).isEqualTo(new KnockedPinCount(count));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 11})
+    void 쓰러뜨린_핀의_개수는_음수거나_11개이상일수없다_예외발생(int count) {
+        assertThatThrownBy(() -> new KnockedPinCount(count))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(KnockedPinCount.INVALID_KNOCK_OUT_COUNT_MESSAGE);
+    }
+}
