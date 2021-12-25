73716919c831f36fa050834c2219e6425f433b66
orgojy <giveawesome@gmail.com>
2021-12-26 00:05:57 +0900

    feat(Player): `Player` 생성

---
+++ src/main/java/bowling/domain/Player.java
@@ -0,0 +1,48 @@
+package bowling.domain;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+public class Player {
+    private static final Pattern NAME_REGEX = Pattern.compile("^[a-zA-Z]{3}$");
+
+    private final String name;
+
+    private Player(String name) {
+        this.name = name;
+    }
+
+    public static Player create(String name) {
+        validateName(name);
+        return new Player(name);
+    }
+
+    private static void validateName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("플레이어 이름이 null이 전달되었습니다.");
+        }
+        if (!NAME_REGEX.matcher(name).matches()) {
+            throw new IllegalArgumentException("플레이어 이름은 영문이고 3자리만 허용합니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/bowling/domain/PlayerTest.java
@@ -0,0 +1,29 @@
+package bowling.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PlayerTest {
+
+    @DisplayName("플레이어 이름이 영어 3자일 경우 생성 검증")
+    @ParameterizedTest
+    @ValueSource(strings = {"abc", "ABC", "aBc"})
+    void create(String name) {
+        // when & then
+		assertThat(Player.create(name)).isNotNull();
+    }
+
+    @DisplayName("플레이어 이름이 영어 3자가 아닐 경우 예외 발생")
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"a", "aB", "ABCD", "123", "김수민"})
+    void createWithInvalidName(String invalidName) {
+        // when & then
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> Player.create(invalidName));
+    }
+}
