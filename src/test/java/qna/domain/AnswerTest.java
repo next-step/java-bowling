1f8538afcb611c4067b1a9fe52178dd352c38081
jiseok-choi <53139084+jiseok-choi@users.noreply.github.com>
2021-12-26 14:41:31 +0900

    [볼링] 1단계 - 질문 삭제하기 기능 리팩토링 (#760)
    * refactor : 서비스 로직 도메인으로 분리
    
    * test : 테스트코드 추가

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,19 @@
 package qna.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("delete_성공")
+    public void delete_success() throws Exception {
+        Answer answer = A1;
+        answer.canDelete(UserTest.JAVAJIGI);
+        answer.setDeleted(true);
+        Assertions.assertThat(answer.isDeleted()).isTrue();
+    }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,37 @@
 package qna.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    @DisplayName("delete_다른_사람이_쓴_글")
+    public void delete_other_writer() throws Exception {
+        Question question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        Assertions.assertThatThrownBy(() -> {
+            question.delete(UserTest.SANJIGI, question.getId());
+        }).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    @DisplayName("delete_성공_질문자_답변자_같음")
+    public void delete_success_same_user() throws Exception {
+        Question question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        question.delete(UserTest.JAVAJIGI, question.getId());
+        Assertions.assertThat(question.isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("delete_답변_중_다른_사람이_쓴_글")
+    public void delete_answers_other_writer() throws Exception {
+        Question question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        Assertions.assertThatThrownBy(() -> {
+            question.delete(UserTest.SANJIGI, question.getId());
+        }).isInstanceOf(CannotDeleteException.class);
+    }
 }
