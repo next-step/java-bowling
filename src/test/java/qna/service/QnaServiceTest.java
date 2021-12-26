b2ea2cafa3e9bbacba5c2767f2718f29eb0e2817
jeje <las9897@gmail.com>
2021-12-27 00:36:40 +0900

    refactor: QnaService domain 적용

--- src/test/java/qna/service/QnaServiceTest.java
+++ src/test/java/qna/service/QnaServiceTest.java
@@ -42,48 +42,48 @@ public class QnaServiceTest {
 
     @Test
     public void delete_성공() throws Exception {
-//        when(questionRepository.findByIdAndDeletedFalse(question.getId())).thenReturn(Optional.of(question));
-//
-//        assertThat(question.isDeleted()).isFalse();
-//        qnAService.deleteQuestion(UserTest.JAVAJIGI, question.getId());
-//
-//        assertThat(question.isDeleted()).isTrue();
-//        verifyDeleteHistories();
+        when(questionRepository.findByIdAndDeletedFalse(question.getId())).thenReturn(Optional.of(question));
+
+        assertThat(question.isDeleted()).isFalse();
+        qnAService.deleteQuestion(UserTest.JAVAJIGI, question.getId());
+
+        assertThat(question.isDeleted()).isTrue();
+        verifyDeleteHistories();
     }
 
     @Test
     public void delete_다른_사람이_쓴_글() throws Exception {
-//        when(questionRepository.findByIdAndDeletedFalse(question.getId())).thenReturn(Optional.of(question));
-//
-//        assertThatThrownBy(() -> {
-//            qnAService.deleteQuestion(UserTest.SANJIGI, question.getId());
-//        }).isInstanceOf(CannotDeleteException.class);
+        when(questionRepository.findByIdAndDeletedFalse(question.getId())).thenReturn(Optional.of(question));
+
+        assertThatThrownBy(() -> {
+            qnAService.deleteQuestion(UserTest.SANJIGI, question.getId());
+        }).isInstanceOf(CannotDeleteException.class);
     }
 
     @Test
     public void delete_성공_질문자_답변자_같음() throws Exception {
-//        when(questionRepository.findByIdAndDeletedFalse(question.getId())).thenReturn(Optional.of(question));
-//
-//        qnAService.deleteQuestion(UserTest.JAVAJIGI, question.getId());
-//
-//        assertThat(question.isDeleted()).isTrue();
-//        assertThat(answer.isDeleted()).isTrue();
-//        verifyDeleteHistories();
+        when(questionRepository.findByIdAndDeletedFalse(question.getId())).thenReturn(Optional.of(question));
+
+        qnAService.deleteQuestion(UserTest.JAVAJIGI, question.getId());
+
+        assertThat(question.isDeleted()).isTrue();
+        assertThat(answer.isDeleted()).isTrue();
+        verifyDeleteHistories();
     }
 
     @Test
     public void delete_답변_중_다른_사람이_쓴_글() throws Exception {
-//        when(questionRepository.findByIdAndDeletedFalse(question.getId())).thenReturn(Optional.of(question));
-//
-//        assertThatThrownBy(() -> {
-//            qnAService.deleteQuestion(UserTest.SANJIGI, question.getId());
-//        }).isInstanceOf(CannotDeleteException.class);
+        when(questionRepository.findByIdAndDeletedFalse(question.getId())).thenReturn(Optional.of(question));
+
+        assertThatThrownBy(() -> {
+            qnAService.deleteQuestion(UserTest.SANJIGI, question.getId());
+        }).isInstanceOf(CannotDeleteException.class);
     }
 
     private void verifyDeleteHistories() {
-//        List<DeleteHistory> deleteHistories = Arrays.asList(
-//                new DeleteHistory(ContentType.QUESTION, question.getId(), question.getWriter(), LocalDateTime.now()),
-//                new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now()));
-//        verify(deleteHistoryService).saveAll(deleteHistories);
+        List<DeleteHistory> deleteHistories = Arrays.asList(
+                new DeleteHistory(ContentType.QUESTION, question.getId(), question.getWriter(), LocalDateTime.now()),
+                new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now()));
+        verify(deleteHistoryService).saveAll(deleteHistories);
     }
 }
