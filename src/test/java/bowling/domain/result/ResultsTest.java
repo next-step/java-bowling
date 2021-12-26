07d78e58a21b79f45d59bd626598f81aac8a744d
LeeJeongGi <apple4rhk@naver.com>
2021-12-26 16:43:51 +0900

    refactor : 생성 테스트 새로운 방식으로 검증

--- src/test/java/bowling/domain/result/ResultsTest.java
+++ src/test/java/bowling/domain/result/ResultsTest.java
@@ -3,15 +3,14 @@ package bowling.domain.result;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class ResultsTest {
 
     @Test
     @DisplayName("생성 테스트")
     void createTest() {
-        Results results = new Results();
+        assertDoesNotThrow(() -> new Results());
 
-        assertThat(results.getGameResults().size()).isEqualTo(10);
     }
 }
