657e070497b9915d5916072fffc4e781d37081bf
kangyonghyun <hmoon826@gmail.com>
2021-12-26 18:05:48 +0900

    (refactor) 매직넘버 상수로 변환, 검증 테스트 추가

--- src/main/java/bowling/domain/BowlingPins.java
+++ src/main/java/bowling/domain/BowlingPins.java
@@ -4,11 +4,14 @@ import java.util.Objects;
 
 public class BowlingPins {
 
-    public static final String ERROR_PINS_VALUE_MSG = "볼링 핀은 1-10 까지 입니다";
+    private static final String ERROR_PINS_VALUE_MSG = "볼링 핀은 0-10 까지 입니다";
+    private static final int MIN_PINS = 0;
+    private static final int MAX_PINS = 10;
+
     private final int count;
 
     public BowlingPins(int count) {
-        if (!(1 <= count && count <= 10)) {
+        if (!(MIN_PINS <= count && count <= MAX_PINS)) {
             throw new IllegalArgumentException(ERROR_PINS_VALUE_MSG);
         }
         this.count = count;
--- src/test/java/bowling/domain/BowlingPinsTest.java
+++ src/test/java/bowling/domain/BowlingPinsTest.java
@@ -2,8 +2,11 @@ package bowling.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BowlingPinsTest {
 
@@ -12,6 +15,14 @@ public class BowlingPinsTest {
     void create() {
         BowlingPins pins = new BowlingPins(10);
         assertThat(pins).isEqualTo(new BowlingPins(10));
+    }
+
+    @ParameterizedTest
+    @DisplayName("볼링 핀 검증 0-10 아닐 경우 -> IllegalArgumentException 반환")
+    @ValueSource(ints = {-1, 11})
+    void invalid(int pin) {
+        assertThatThrownBy(() -> new BowlingPins(pin))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }
