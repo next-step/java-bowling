fac31aee48e37f9c80d4b4c44e2145a7873569e2
jeje <las9897@gmail.com>
2021-12-26 23:44:09 +0900

    refacotor: Question domain에서 삭제 기능 적용

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -1,6 +1,7 @@
 package qna.domain;
 
 import org.hibernate.annotations.Where;
+import qna.CannotDeleteException;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -8,6 +9,9 @@ import java.util.List;
 
 @Entity
 public class Question extends AbstractEntity {
+
+    private static final String MESSAGE_NO_PERMISSION = "질문을 삭제할 권한이 없습니다.";
+
     @Column(length = 100, nullable = false)
     private String title;
 
@@ -37,6 +41,27 @@ public class Question extends AbstractEntity {
         super(id);
         this.title = title;
         this.contents = contents;
+    }
+
+    public void deleteByUser(User loginUser) throws CannotDeleteException {
+        validationWriter(loginUser);
+        for (Answer answer : answers) {
+            if (!answer.isOwner(loginUser)) {
+                throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+            }
+        }
+
+        this.deleted = true;
+
+        for (Answer answer : answers) {
+            answer.setDeleted(true);
+        }
+    }
+
+    private void validationWriter(User loginUser) throws CannotDeleteException {
+        if (!writer.equals(loginUser)) {
+            throw new CannotDeleteException(MESSAGE_NO_PERMISSION);
+        }
     }
 
     public String getTitle() {
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,57 @@
 package qna.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+
+    @Test
+    @DisplayName("작성자와 이름이 다를 경우")
+    void deleteByAnotherUser() {
+        //then
+        assertThatThrownBy(() -> Q1.deleteByUser(UserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class);
+    }
+
+
+    @Test
+    @DisplayName("작성자와 이름이 같을 경우")
+    void deleteByWriter() throws CannotDeleteException {
+        //when
+        Q1.deleteByUser(UserTest.JAVAJIGI);
+        //then
+        assertThat(Q1.isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("작성자와 이름이 같지만, 다른 사람이 쓴 답변이 있을 경우")
+    void deleteWithAnotherAnswer() {
+        //given
+        Q1.addAnswer(AnswerTest.A2);
+        //then
+        assertThatThrownBy(() -> Q1.deleteByUser(UserTest.JAVAJIGI))
+                .isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    @DisplayName("작성자와 이름이 같고, 작성자가 쓴 답변만 있을 경우")
+    void deleteWithWriterAnswer() throws CannotDeleteException {
+        //given
+        Q2.addAnswer(AnswerTest.A2);
+        //when
+        Q2.deleteByUser(UserTest.SANJIGI);
+        //then
+        assertThat(Q2.isDeleted()).isTrue();
+        assertThat(AnswerTest.A2.isDeleted()).isTrue();
+
+    }
+
 }
