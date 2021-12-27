958e61631aa6fc395392e2f616aecc7e02909f35
sid <sid.seo@kakaocorp.com>
2021-12-28 02:21:48 +0900

    feature: 최대 3글자의 이름 Name

---
+++ src/main/java/bowling/domain/Player.java
@@ -0,0 +1,43 @@
+package bowling.domain;
+
+import java.util.Objects;
+
+import bowling.engin.Name;
+
+public class Player implements Name {
+    private final String name;
+
+    private Player(String name) {
+        this.name = name;
+    }
+
+    public static Player of(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("name must not be null or empty.");
+        }
+
+        if (name.length() > MAXIMUM_LENGTH) {
+            throw new IllegalArgumentException("the maximum length of name is 3");
+        }
+
+        return new Player(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ src/main/java/bowling/engin/Name.java
@@ -0,0 +1,5 @@
+package bowling.engin;
+
+public interface Name extends StringWrapper {
+    int MAXIMUM_LENGTH = 3;
+}
