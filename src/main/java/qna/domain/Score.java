bba90828d3c3b2e80391c3d8a8f2c76ab3df9e88
orgojy <giveawesome@gmail.com>
2021-12-28 22:44:22 +0900

    feat: 투구 점수 (생성/반환/검증) 기능 구현

---
+++ src/main/java/qna/domain/Score.java
@@ -0,0 +1,94 @@
+package qna.domain;
+
+import java.util.Objects;
+
+public class Score {
+    public static final int READY_SCORE_VALUE = -1;
+    public static final int MIN_SCORE = 0;
+    public static final int MAX_SCORE = 30;
+    private static final int READY_REMAINING_PITCHES = -1;
+    private static final int MAX_EXTRA_PITCHES = 2;
+    private static final int MIN_EXTRA_PITCHES = 0;
+    private static final int NO_REMAINING_PITCHES = 0;
+
+    private final int value;
+    private final int remainingPitches;
+
+    private Score(int value, int remainingPitches) {
+        this.value = value;
+        this.remainingPitches = remainingPitches;
+    }
+
+    public static Score withStrike() {
+        return create(10, 2);
+    }
+
+    public static Score withSpare() {
+        return create(10, 1);
+    }
+
+    public static Score withNonLeftScore(int value) {
+        return create(value, NO_REMAINING_PITCHES);
+    }
+
+    public static Score withReady() {
+        return new Score(READY_SCORE_VALUE, READY_REMAINING_PITCHES);
+    }
+
+    public static Score create(int value, int remainingPitches) {
+        validate(value, remainingPitches);
+        return new Score(value, remainingPitches);
+    }
+
+    private static void validate(int value, int remainingPitches) {
+        if (value < MIN_SCORE || value > MAX_SCORE) {
+            throw new IllegalArgumentException(String.format("전달된 점수(%d)는 %d <= x <= %d 이어야합니다.", value, MIN_SCORE, MAX_SCORE));
+        }
+        if (remainingPitches < MIN_EXTRA_PITCHES || remainingPitches > MAX_EXTRA_PITCHES) {
+            throw new IllegalArgumentException(
+                    String.format("전달된 남은 투구(%d)는 %d <= x <= %d 이어야합니다.", remainingPitches, MIN_EXTRA_PITCHES, MAX_EXTRA_PITCHES)
+            );
+        }
+    }
+
+    public Score bowl(Score nextValue) {
+        int sum = this.value + nextValue.value;
+        return create(sum, remainingPitches - 1);
+    }
+
+    public int getFinalValue() {
+        if (!hasFinalScore()) {
+            throw new IllegalStateException(String.format("투구가 %d회 남아있는 점수입니다. 최종 점수를 구할 수 없습니다.", remainingPitches));
+        }
+        return this.value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public boolean hasFinalScore() {
+        return remainingPitches == NO_REMAINING_PITCHES;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Score score = (Score) o;
+        return value == score.value && remainingPitches == score.remainingPitches;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, remainingPitches);
+    }
+
+    @Override
+    public String toString() {
+        return "Score{" +
+                "value=" + value +
+                ", priorScoreCount=" + remainingPitches +
+                '}';
+    }
+}
---
+++ src/test/java/qna/domain/ScoreTest.java
@@ -0,0 +1,140 @@
+package qna.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class ScoreTest {
+
+    @DisplayName("Score 생성")
+    @Test
+    void create() {
+        // when & then
+        assertAll(
+                () -> assertThat(Score.create(1, 0)).isNotNull(),
+                () -> assertThat(Score.withNonLeftScore(8)).isNotNull(),
+                () -> assertThat(Score.withSpare()).isNotNull(),
+                () -> assertThat(Score.withStrike()).isNotNull(),
+                () -> assertThat(Score.withReady()).isNotNull()
+        );
+    }
+
+    @DisplayName("Ready Score 확인")
+    @Test
+    void createReadyScore() {
+        // given
+        Score readyScore = Score.withReady();
+        // when & then
+        assertAll(
+                () -> assertThat(readyScore.getValue()).isEqualTo(Score.READY_SCORE_VALUE),
+                () -> assertThat(readyScore.hasFinalScore()).isFalse(),
+                () -> assertThatExceptionOfType(IllegalStateException.class)
+                        .isThrownBy(readyScore::getFinalValue)
+        );
+    }
+
+    @DisplayName("잘못된 점수로 Score 생성 예외")
+    @ParameterizedTest
+    @ValueSource(ints = {Integer.MIN_VALUE, Score.MIN_SCORE - 1, Score.MAX_SCORE + 1, Integer.MAX_VALUE})
+    void createFailedWithInvalidValue(int invalidValue) {
+        // given
+        int validExtraPitches = 2;
+        // when & then
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> Score.create(invalidValue, validExtraPitches)
+        );
+    }
+
+    @DisplayName("잘못된 추가 투구로 Score 생성 예외")
+    @ParameterizedTest
+    @ValueSource(ints = {Integer.MIN_VALUE, -1, 3, Integer.MAX_VALUE})
+    void createFailedWithInvalidExtraPitches(int invalidExtraPitches) {
+        // given
+        int validValue = 5;
+        // when & then
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> Score.create(validValue, invalidExtraPitches)
+        );
+    }
+
+    @DisplayName("추가 투구가 없는 투구 이후 점수 확인")
+    @Test
+    void bowlWithNonLeftScore() {
+        // given
+        Score score = Score.withNonLeftScore(5);
+        // when & then
+        assertAll(
+                () -> assertThat(score).isEqualTo(Score.create(5, 0)),
+                () -> assertThat(score.getFinalValue()).isEqualTo(5)
+        );
+    }
+
+    @DisplayName("Spare 이후, 추가 1회 투구 점수 합산")
+    @Test
+    void bowlWithSpareNextScore() {
+        // given
+        Score spareScore = Score.withSpare();
+        // when
+        Score finalScore = spareScore.bowl(Score.withNonLeftScore(5));
+        // then
+        assertAll(
+                () -> assertThat(finalScore).isEqualTo(Score.create(10 + 5, 0)),
+                () -> assertThat(finalScore.getFinalValue()).isEqualTo(15)
+        );
+    }
+
+    @DisplayName("Strike 이후, 추가 2회 투구 점수 합산")
+    @Test
+    void bowlWithStrikeNextScore() {
+        // given
+        Score strikeScore = Score.withStrike();
+        // when
+        Score middleScore = strikeScore.bowl(Score.withNonLeftScore(5));
+        // then
+        assertThat(middleScore).isEqualTo(Score.create(10 + 5, 1));
+        // when
+        Score finalScore = middleScore.bowl(Score.withNonLeftScore(3));
+        // then
+        assertAll(
+                () -> assertThat(finalScore).isEqualTo(Score.create(10 + 5 + 3, 0)),
+                () -> assertThat(finalScore.getFinalValue()).isEqualTo(18)
+        );
+    }
+
+    @DisplayName("투구가 남아있는 경우, 최종 점수 반환 예외 발생과 정상 반환 확인")
+    @Test
+    void getFinalValue() {
+        // given
+        int spareValue = 10;
+        Score spareScore = Score.create(spareValue, 1);
+        // when & then
+        assertThatExceptionOfType(IllegalStateException.class)
+                .isThrownBy(spareScore::getFinalValue);
+        // given
+        int basicScore = 3;
+        // when
+        Score finalScore = spareScore.bowl(Score.withNonLeftScore(basicScore));
+        // then
+        assertThat(finalScore.getFinalValue()).isEqualTo(spareValue + basicScore);
+    }
+
+    @DisplayName("투구가 남아있는 경우, 점수 반환 확인")
+    @Test
+    void getValue() {
+        // given
+        int spareValue = 10;
+        Score spareScore = Score.create(10, 1);
+        // when & then
+        assertThat(spareScore.getValue()).isEqualTo(spareValue);
+        // given
+        int basicScore = 3;
+        // when
+        Score finalScore = spareScore.bowl(Score.withNonLeftScore(basicScore));
+        // then
+        assertThat(finalScore.getValue()).isEqualTo(spareValue + basicScore);
+    }
+}
