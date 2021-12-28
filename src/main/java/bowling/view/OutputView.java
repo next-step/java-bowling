8898b3f6175bf4a77e8fb8f2cb53d4bbc69db864
orgojy <giveawesome@gmail.com>
2021-12-29 03:53:28 +0900

    feat(OutputView.java): 볼링 점수판 총합계 출력 기능 추가

--- src/main/java/bowling/view/OutputView.java
+++ src/main/java/bowling/view/OutputView.java
@@ -2,6 +2,7 @@ package bowling.view;
 
 import bowling.domain.BowlingGame;
 import bowling.domain.FrameIndex;
+import bowling.domain.Score;
 import bowling.domain.frame.Frame;
 
 import java.util.List;
@@ -14,6 +15,9 @@ public final class OutputView {
     private static final String PLAYER_NAME_FORMAT = "|  %s |";
     private static final String RESULT_FRAME_FORMAT = "  %-3s |";
     private static final String EMPTY_FRAME_FORMAT = "      |";
+    private static final String SCORE_LEFT_PADDING_FORMAT = "|      |";
+    private static final String SCORE_RESULT_FORMAT = "  %-3s |";
+    private static final String SCORE_EMPTY_FORMAT = "      |";
 
     private OutputView() {
     }
@@ -23,14 +27,16 @@ public final class OutputView {
     }
 
     private static String currentStatus(BowlingGame bowlingGame) {
-        return String.format("%s%n%s%s%n", SCORE_HEAD_FORMAT, playerName(bowlingGame.getPlayerName()), body(bowlingGame.getFrames()));
+        return String.format("%s%n%s%s%n%s%n", SCORE_HEAD_FORMAT,
+                playerName(bowlingGame.getPlayerName()), symbolBody(bowlingGame.getFrames()), scoreBody(bowlingGame.getFrames())
+        );
     }
 
     private static String playerName(String playerName) {
         return String.format(PLAYER_NAME_FORMAT, playerName);
     }
 
-    private static String body(List<Frame> frames) {
+    private static String symbolBody(List<Frame> frames) {
         return resultBody(frames) + baseBody(frames);
     }
 
@@ -44,5 +50,33 @@ public final class OutputView {
         return frames.stream()
                 .map(frame -> String.format(RESULT_FRAME_FORMAT, frame.symbol()))
                 .collect(joining());
+    }
+
+    private static String scoreBody(List<Frame> frames) {
+        return String.format("%s%s%s", SCORE_LEFT_PADDING_FORMAT, totalScoreBody(frames), baseBody(frames));
+    }
+
+    private static String totalScoreBody(List<Frame> frames) {
+        return frames.stream()
+                .map(frame -> currentTotalScoreBody(frames, frame))
+                .collect(joining());
+    }
+
+    private static String currentTotalScoreBody(List<Frame> frames, Frame frame) {
+        if (frame.score() != Score.INCOMPUTABLE_SCORE_VALUE) {
+            return totalScore(frames, frames.indexOf(frame) + 1);
+        }
+        return SCORE_EMPTY_FORMAT;
+    }
+
+    private static String totalScore(List<Frame> frames, int limit) {
+        return String.format(SCORE_RESULT_FORMAT, totalScoreByLimit(frames, limit));
+    }
+
+    private static int totalScoreByLimit(List<Frame> frames, int limit) {
+        return frames.stream()
+                .limit(limit)
+                .mapToInt(Frame::score)
+                .sum();
     }
 }
