d28ab68262bea584336bce36d77ac115a68c5d31
dongHyo <kdhyo98@gmail.com>
2021-12-30 02:58:38 +0900

    refactor : view 리팩토링 진행

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -6,8 +6,8 @@ public class InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    private static final String INPUT_PLAYER_NAME = "플레이어 이름은(3 english letters)?:";
-    private static final String INPUT_PINS = "%d프레임 투구 : ";
+    private static final String INPUT_PLAYER_NAME = "플레이어 이름은(3 english letters)? : ";
+    private static final String INPUT_PINS = "%d 프레임 투구 : ";
 
     public static String name() {
         System.out.print(INPUT_PLAYER_NAME);
--- src/main/java/bowling/view/OutputView.java
+++ src/main/java/bowling/view/OutputView.java
@@ -12,8 +12,10 @@ public class OutputView {
     private static final String NAME_MASSAGE = "|  %-4s|";
     private static final String EMPTY_MASSAGE = "      |";
     private static final String SCORE_MASSAGE = "  %-4s|";
+    private static final String MAX_SCORE_MASSAGE = " %-5s|";
     private static final String JOIN_DELIMITER = "";
-
+    private static final int MARKING_SIZE = 10;
+    private static final int DEFAULT_MARK_LENGTH = 3;
 
     public static void result(Player player) {
         header();
@@ -21,25 +23,37 @@ public class OutputView {
         frame(player.frames());
     }
 
-    private static void frame(List<Frame> frames) {
-        List<String> marking = frames.stream()
-            .map(frame -> String.format(SCORE_MASSAGE, frame.mark()))
-            .collect(Collectors.toCollection(ArrayList::new));
-
-        while (marking.size() < 10) {
-            marking.add(EMPTY_MASSAGE);
-        }
-        System.out.println(String.join(JOIN_DELIMITER, marking));
-        System.out.println();
+    private static void header() {
+        System.out.println(HEADER_MASSAGE);
     }
 
     private static void playerName(String player) {
         System.out.print(String.format(NAME_MASSAGE, player));
     }
 
-    private static void header() {
-        System.out.println(HEADER_MASSAGE);
+    private static void frame(List<Frame> frames) {
+        System.out.println(String.join(JOIN_DELIMITER, framesAsMarking(frames)));
+        System.out.println();
     }
 
+    private static List<String> framesAsMarking(List<Frame> frames) {
+        List<String> marking = frames.stream()
+            .map(OutputView::format)
+            .collect(Collectors.toCollection(ArrayList::new));
+
+        while (marking.size() < MARKING_SIZE) {
+            marking.add(EMPTY_MASSAGE);
+        }
+
+        return marking;
+    }
+
+    private static String format(Frame frame) {
+        String mark = frame.mark();
+        if (mark.length() > DEFAULT_MARK_LENGTH) {
+            return String.format(MAX_SCORE_MASSAGE, mark);
+        }
+        return String.format(SCORE_MASSAGE, mark);
+    }
 
 }
