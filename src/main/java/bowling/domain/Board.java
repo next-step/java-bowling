28693c3860196f203a22935a4a6aa29e0641a941
minjoon_lee <minjoon1995@naver.com>
2022-01-04 23:04:02 +0900

    add : Board, Frame, Player, Score domain add

--- src/main/java/bowling/domain/Board.java
+++ src/main/java/bowling/domain/Board.java
@@ -1,31 +1,11 @@
 package bowling.domain;
-
-import bowling.domain.frame.Frame;
-import bowling.domain.frame.LastFrame;
-import bowling.domain.frame.NormalFrame;
-
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-
 public class Board {
-    public static int BOARD_MAX_SIZE = 10;
+    private List frames = new ArrayList<>();
 
-    private final List<Frame> frames = new ArrayList<>();
-
-    public void init() {
-        for (int index = 0; index < 9; index++) {
-            frames.add(new NormalFrame());
-        }
-        frames.add(new LastFrame());
+    public void add(Frame frame) {
+        frames.add(frame);
     }
 
-    public List<Frame> getFrames() {
-        return Collections.unmodifiableList(frames);
-    }
-
-    public Frame getRecentFrame(int index) {
-        return frames.get(index);
-    }
 }
-
---
+++ src/main/java/bowling/domain/Frame.java
@@ -0,0 +1,29 @@
+package bowling.domain;
+
+public class Frame {
+    private Score firstScore;
+    private Score secondScore;
+    private Score thirdScore;
+
+    public Frame(Score firstScore) {
+        this.firstScore = firstScore;
+    }
+
+    public void setSecondScore(Score secondScore) {
+        this.secondScore = secondScore;
+    }
+
+    private boolean isStrike() {
+        if (firstScore.isStrike()) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isSpare() {
+        if (!firstScore.isStrike() && firstScore.add(secondScore).equals(new Score(10))) {
+            return true;
+        }
+        return false;
+    }
+}
