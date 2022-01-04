b92a11f336f0298b06d0e845cad59aebf873d8b4
minjoon_lee <minjoon1995@naver.com>
2022-01-04 23:06:09 +0900

    feature : player name validator add

--- src/main/java/bowling/domain/Player.java
+++ src/main/java/bowling/domain/Player.java
@@ -4,6 +4,13 @@ public class Player {
     private String name;
 
     public Player(String name) {
+        checkNameLength(name);
         this.name = name;
+    }
+
+    private void checkNameLength(String name) {
+        if (name.length() != 3) {
+            throw new IllegalArgumentException("name's length must be 3");
+        }
     }
 }
