605eea79c8d3ab8a52fa1d38c4f1e3e45a44380a
mj950425 <minjoon1995@naver.com>
2022-01-08 23:47:44 +0900

    refactoring : stream으로 대체

--- src/main/java/bowling/controller/Controller.java
+++ src/main/java/bowling/controller/Controller.java
@@ -4,22 +4,25 @@ import bowling.domain.*;
 import bowling.view.Input;
 import bowling.view.Output;
 
+import java.util.stream.IntStream;
+
 public class Controller {
     public static void main(String[] args) {
         Board board = new Board();
         Player player = new Player(Input.inputPlayerName());
-        Output.outputBoard(board,player);
+        Output.outputBoard(board, player);
 
-        for (int index = 1; index < 10; index++) {
-            Score firstScore = new Score(Input.inputScore(index));
+
+        IntStream.range(1, 10).forEach(i -> {
+            Score firstScore = new Score(Input.inputScore(i));
             Frame frame = new NormalFrame(firstScore);
             board.add(frame);
-            Output.outputBoard(board,player);
+            Output.outputBoard(board, player);
             if (!firstScore.isStrike()) {
-                frame.setSecondScore(new Score(Input.inputScore(index)));
-                Output.outputBoard(board,player);
+                frame.setSecondScore(new Score(Input.inputScore(i)));
+                Output.outputBoard(board, player);
             }
-        }
+        });
 
         Score firstScore = new Score(Input.inputScore(10));
         Frame frame = new LastFrame(firstScore);
@@ -28,6 +31,9 @@ public class Controller {
         frame.setSecondScore(new Score(Input.inputScore(10)));
         Output.outputBoard(board, player);
         if (frame.isStrike()) {
+            frame.setThirdScore(new Score(Input.inputScore(10)));
+        }
+        if (frame.isSpare()) {
             frame.setThirdScore(new Score(Input.inputScore(10)));
         }
         Output.outputBoard(board, player);
--- src/main/java/bowling/view/Output.java
+++ src/main/java/bowling/view/Output.java
@@ -3,50 +3,30 @@ package bowling.view;
 import bowling.domain.Board;
 import bowling.domain.Player;
 
+import java.util.stream.IntStream;
+
 public class Output {
     public static void outputBoard(Board board, Player player) {
         System.out.println("| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |");
         StringBuilder sb = new StringBuilder();
         System.out.print("|  " + player.getName() + " |");
-        for (int index = 1; index < 11; index++) {
-            if (index < board.getFrames().size()+1) {
-                if (index == 10) {
-                    sb.append(" " + board.getFrames().get(index-1).convert());
-                }
-                if (index != 10) {
-                    sb.append("  " + board.getFrames().get(index-1).convert());
-                }
+        IntStream.rangeClosed(0, 9).forEach(index -> {
+            if (board.isRecorded(index) && board.getFrames().get(index).isLastFrame()) {
+                sb.append(" " + board.getFrames().get(index).convert());
             }
-            for (int i = sb.length(); i < 7 * index -1; i++) {
-                sb.append(" ");
+            if (board.isRecorded(index) && !board.getFrames().get(index).isLastFrame()) {
+                sb.append("  " + board.getFrames().get(index).convert());
             }
+
+            IntStream.rangeClosed(sb.length(), 7 * (index + 1) - 2).forEach(i -> {
+                        sb.append(" ");
+                    }
+            );
             sb.append("|");
-        }
+        });
+
         System.out.print(sb);
         sb.setLength(0);
-
-
-//        for (Frame frame : board.getFrames()) {
-//            if (frame.isLastFrame()) {
-//                sb.append(" ");
-//                sb.append(frame.convert());
-//            }
-//            if (!frame.isLastFrame()) {
-//                sb.append("  ");
-//                sb.append(frame.convert());
-//                for (int i = sb.length(); i < 6; i++) {
-//                    sb.append(" ");
-//                }
-//            }
-//            sb.append("|");
-//            System.out.print(sb);
-//            sb.setLength(0);
-//        }
         System.out.println();
     }
-
-    public static void outputLastFrame(Board board, Player player) {
-
-    }
-
 }
