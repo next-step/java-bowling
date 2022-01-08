3300f4723978be072155892cffb07d5797ef19f1
mj950425 <minjoon1995@naver.com>
2022-01-08 19:12:02 +0900

    fix : 아직 치지 않은 볼링넘버도 빈칸으로 출력

--- src/main/java/bowling/view/Output.java
+++ src/main/java/bowling/view/Output.java
@@ -1,25 +1,51 @@
 package bowling.view;
 
 import bowling.domain.Board;
-import bowling.domain.Frame;
 import bowling.domain.Player;
 
 public class Output {
     public static void outputBoard(Board board, Player player) {
         System.out.println("| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |");
         StringBuilder sb = new StringBuilder();
-        System.out.print("|  "+player.getName()+" |");
-        for (Frame frame : board.getFrames()) {
-            sb.append("  ");
-            sb.append(frame.convert());
-            for (int i = sb.length(); i < 6; i++) {
+        System.out.print("|  " + player.getName() + " |");
+        for (int index = 1; index < 11; index++) {
+            if (index < board.getFrames().size()+1) {
+                if (index == 10) {
+                    sb.append(" " + board.getFrames().get(index-1).convert());
+                }
+                if (index != 10) {
+                    sb.append("  " + board.getFrames().get(index-1).convert());
+                }
+            }
+            for (int i = sb.length(); i < 7 * index -1; i++) {
                 sb.append(" ");
             }
             sb.append("|");
-            System.out.print(sb.toString());
-            sb.setLength(0);
         }
+        System.out.print(sb);
+        sb.setLength(0);
+
+
+//        for (Frame frame : board.getFrames()) {
+//            if (frame.isLastFrame()) {
+//                sb.append(" ");
+//                sb.append(frame.convert());
+//            }
+//            if (!frame.isLastFrame()) {
+//                sb.append("  ");
+//                sb.append(frame.convert());
+//                for (int i = sb.length(); i < 6; i++) {
+//                    sb.append(" ");
+//                }
+//            }
+//            sb.append("|");
+//            System.out.print(sb);
+//            sb.setLength(0);
+//        }
         System.out.println();
+    }
+
+    public static void outputLastFrame(Board board, Player player) {
 
     }
 
