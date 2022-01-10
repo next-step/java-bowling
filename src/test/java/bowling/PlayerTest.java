8c69d14ea25da1d9b6e875b6b3a9ba861be3a780
minjoon_lee <minjoon1995@naver.com>
2022-01-10 13:06:59 +0900

    add : player test 코드 추가

---
+++ src/test/java/bowling/PlayerTest.java
@@ -0,0 +1,16 @@
+package bowling;
+
+import bowling.domain.Player;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PlayerTest {
+    @Test
+    void checkNameLength() {
+        assertThatThrownBy(() -> {
+            new Player("test");
+        }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("name's length must be 3");
+    }
+}
