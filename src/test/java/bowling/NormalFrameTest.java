5e51906f08735c2278cd3b85533bf5a7b38722ce
minjoon_lee <minjoon1995@naver.com>
2022-01-10 12:58:25 +0900

    add : test code add

--- src/test/java/bowling/NormalFrameTest.java
+++ src/test/java/bowling/NormalFrameTest.java
@@ -6,42 +6,41 @@ import bowling.domain.frame.NormalFrame;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 public class NormalFrameTest {
-
+    @Test
+    void isFirstPitch() {
+        NormalFrame normalFrame = new NormalFrame(new Score(10));
+        assertThat(normalFrame.isFirstPitch()).isEqualTo(true);
+    }
 
     @Test
     void isSpare() {
         Score firstScore = new Score(3);
-        Score secondScore = new Score(7);
-        NormalFrame normalFrame = new NormalFrame(firstScore, secondScore);
-        assertTrue(normalFrame.isSpare(firstScore, secondScore));
+        Frame normalFrame = new NormalFrame(firstScore);
+        Score secondScore = new Score(3);
+        normalFrame.setSecondScore(secondScore);
+        assertThat(normalFrame.isSpare(firstScore,secondScore)).isEqualTo(true);
     }
-
     @Test
     void convertSpare() {
         Frame normalFrame = new NormalFrame(new Score(3));
-        normalFrame.makeScore(new Score(7), 2);
+        normalFrame.setSecondScore(new Score(7));
         assertThat(normalFrame.convert()).isEqualTo("3|/");
     }
-
     @Test
     void convertStrike() {
         Frame normalFrame = new NormalFrame(new Score(10));
         assertThat(normalFrame.convert()).isEqualTo("X");
     }
-
     @Test
     void convertFirstPitch() {
         Frame normalFrame = new NormalFrame(new Score(3));
         assertThat(normalFrame.convert()).isEqualTo("3|");
     }
-
     @Test
     void convert() {
         Frame normalFrame = new NormalFrame(new Score(4));
-        normalFrame.makeScore(new Score(5), 2);
+        normalFrame.setSecondScore(new Score(5));
         assertThat(normalFrame.convert()).isEqualTo("4|5");
     }
 
