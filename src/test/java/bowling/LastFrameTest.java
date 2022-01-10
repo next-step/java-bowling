4f0fae69a6858ab7d58e79c0b319f5d17849fda6
minjoon_lee <minjoon1995@naver.com>
2022-01-10 12:58:25 +0900

    add : test code add

---
+++ src/test/java/bowling/LastFrameTest.java
@@ -0,0 +1,57 @@
+package bowling;
+
+import bowling.domain.Score;
+import bowling.domain.frame.LastFrame;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+public class LastFrameTest {
+    @Test
+    void isLastFrameTest() {
+        LastFrame lastFrame = new LastFrame(new Score(10));
+        assertThat(lastFrame.isLastFrame()).isEqualTo(true);
+    }
+
+    @Test
+    void isFirstPitch() {
+        LastFrame lastFrameA = new LastFrame(new Score(10));
+        LastFrame lastFrameB = new LastFrame(new Score(7));
+        lastFrameB.setSecondScore(new Score(3));
+        assertThat(lastFrameA.isFirstPitch()).isEqualTo(true);
+        assertThat(lastFrameB.isFirstPitch()).isEqualTo(false);
+
+    }
+
+    @Test
+    void isSecondPitch() {
+        LastFrame lastFrameA = new LastFrame(new Score(10));
+        LastFrame lastFrameB = new LastFrame(new Score(7));
+        LastFrame lastFrameC = new LastFrame(new Score(10));
+        lastFrameB.setSecondScore(new Score(3));
+        lastFrameC.setSecondScore(new Score(10));
+        lastFrameC.setThirdScore(new Score(10));
+        assertThat(lastFrameA.isSecondPitch()).isEqualTo(false);
+        assertThat(lastFrameB.isSecondPitch()).isEqualTo(true);
+        assertThat(lastFrameC.isSecondPitch()).isEqualTo(false);
+    }
+
+    @Test
+    void convert() {
+        LastFrame lastFrameA = new LastFrame(new Score(10));
+        LastFrame lastFrameB = new LastFrame(new Score(7));
+        LastFrame lastFrameC = new LastFrame(new Score(10));
+        LastFrame lastFrameD = new LastFrame(new Score(10));
+
+        lastFrameB.setSecondScore(new Score(3));
+        lastFrameC.setSecondScore(new Score(10));
+        lastFrameD.setSecondScore(new Score(3));
+        lastFrameC.setThirdScore(new Score(10));
+        lastFrameD.setThirdScore(new Score(7));
+
+        assertThat(lastFrameA.convert()).isEqualTo("X|");
+        assertThat(lastFrameB.convert()).isEqualTo("7|/|");
+        assertThat(lastFrameC.convert()).isEqualTo("X|X|X");
+        assertThat(lastFrameD.convert()).isEqualTo("X|3|/");
+
+    }
+}
---
+++ src/test/java/bowling/NormalFrameTest.java
@@ -0,0 +1,47 @@
+package bowling;
+
+import bowling.domain.Score;
+import bowling.domain.frame.Frame;
+import bowling.domain.frame.NormalFrame;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+public class NormalFrameTest {
+    @Test
+    void isFirstPitch() {
+        NormalFrame normalFrame = new NormalFrame(new Score(10));
+        assertThat(normalFrame.isFirstPitch()).isEqualTo(true);
+    }
+
+    @Test
+    void isSpare() {
+        Score firstScore = new Score(3);
+        Frame normalFrame = new NormalFrame(firstScore);
+        Score secondScore = new Score(3);
+        normalFrame.setSecondScore(secondScore);
+        assertThat(normalFrame.isSpare(firstScore,secondScore)).isEqualTo(true);
+    }
+    @Test
+    void convertSpare() {
+        Frame normalFrame = new NormalFrame(new Score(3));
+        normalFrame.setSecondScore(new Score(7));
+        assertThat(normalFrame.convert()).isEqualTo("3|/");
+    }
+    @Test
+    void convertStrike() {
+        Frame normalFrame = new NormalFrame(new Score(10));
+        assertThat(normalFrame.convert()).isEqualTo("X");
+    }
+    @Test
+    void convertFirstPitch() {
+        Frame normalFrame = new NormalFrame(new Score(3));
+        assertThat(normalFrame.convert()).isEqualTo("3|");
+    }
+    @Test
+    void convert() {
+        Frame normalFrame = new NormalFrame(new Score(4));
+        normalFrame.setSecondScore(new Score(5));
+        assertThat(normalFrame.convert()).isEqualTo("4|5");
+    }
+
+}
