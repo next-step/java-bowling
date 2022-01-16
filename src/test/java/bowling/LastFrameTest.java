8b266d37d9897b241893e45d95210af85b8bdc11
minjoon_lee <minjoon1995@naver.com>
2022-01-16 22:17:02 +0900

    fix : indent 변경

--- src/test/java/bowling/LastFrameTest.java
+++ src/test/java/bowling/LastFrameTest.java
@@ -12,6 +12,7 @@ public class LastFrameTest {
         LastFrame lastFrame = new LastFrame(new Score(5));
         assertThat(lastFrame.convert()).isEqualTo("5|");
     }
+
     @Test
     void convertStrikeFirstPitch() {
         LastFrame lastFrame = new LastFrame(new Score(10));
@@ -20,23 +21,25 @@ public class LastFrameTest {
 
     @Test
     void convertStrikeSecondPitch() {
-        LastFrame lastFrame = new LastFrame(new Score(10),new Score(10));
+        LastFrame lastFrame = new LastFrame(new Score(10), new Score(10));
         assertThat(lastFrame.convert()).isEqualTo("X|X");
     }
 
     @Test
     void convertSpareSecondPitch() {
-        LastFrame lastFrame = new LastFrame(new Score(3),new Score(7));
+        LastFrame lastFrame = new LastFrame(new Score(3), new Score(7));
         assertThat(lastFrame.convert()).isEqualTo("3|/|");
     }
+
     @Test
     void convertSecondPitch() {
-        LastFrame lastFrame = new LastFrame(new Score(3),new Score(4));
+        LastFrame lastFrame = new LastFrame(new Score(3), new Score(4));
         assertThat(lastFrame.convert()).isEqualTo("3|4");
     }
+
     @Test
     void convertStrikeThirdPitch() {
-        LastFrame lastFrame = new LastFrame(new Score(10),new Score(10),new Score(10));
+        LastFrame lastFrame = new LastFrame(new Score(10), new Score(10), new Score(10));
         assertThat(lastFrame.convert()).isEqualTo("X|X|X");
     }
 }
