7abfa483d4d2731fac404bbf846445d58277832e
imcool2551 <imcool2551@gmail.com>
2022-05-08 00:35:16 +0900

    test: AnswerTest
    1) 다른 사람의 답변 삭제시 예외
    2) 삭제 성공시 deleted 필드 변경과 DeleteHistory 생성
    두 가지 경우를 테스트했습니다.

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,37 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static java.time.LocalDateTime.now;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static qna.domain.UserTest.JAVAJIGI;
+import static qna.domain.UserTest.SANJIGI;
+
 public class AnswerTest {
-    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+    public static final Answer A1 = new Answer(JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+    public static final Answer A2 = new Answer(SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    void delete_다른_사람답변_삭제시_예외() {
+        Answer answer = new Answer(JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+
+        assertThatThrownBy(() -> answer.delete(SANJIGI, new DeleteHistories(), now()))
+                .isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    void delete_정상_동작() throws CannotDeleteException {
+        Answer answer = new Answer(JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+        DeleteHistories deleteHistories = new DeleteHistories();
+
+        assertThat(answer.isDeleted()).isFalse();
+        assertThat(deleteHistories.contains(new DeleteHistory(ContentType.ANSWER, A1.getId(), JAVAJIGI, now())))
+                .isFalse();
+        answer.delete(JAVAJIGI, deleteHistories, now());
+        assertThat(answer.isDeleted()).isTrue();
+        assertThat(deleteHistories.contains(new DeleteHistory(ContentType.ANSWER, A1.getId(), JAVAJIGI, now())))
+                .isTrue();
+    }
 }
