7690a636775227734738e601f980d07d8e656c7b
nara1030 <nara1030@naver.com>
2022-05-12 12:34:06 +0900

    refactor: 질문 삭제 로직 Question 객체(도메인)로 이동

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -1,6 +1,7 @@
 package qna.domain;
 
 import org.hibernate.annotations.Where;
+import qna.CannotDeleteException;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -71,7 +72,14 @@ public class Question extends AbstractEntity {
         answers.add(answer);
     }
 
-    public boolean isOwner(User loginUser) {
+    public void delete(User loginUser) {
+        if (!this.isOwner(loginUser)) {
+            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+        }
+        setDeleted(true);
+    }
+
+    private boolean isOwner(User loginUser) {
         return writer.equals(loginUser);
     }
 
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,23 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    void 다른_사람의_질문_삭제() {
+        assertThatThrownBy(() -> {
+            Q1.delete(Q2.getWriter());
+        }).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    void 자신의_질문_삭제() {
+        Q1.delete(Q1.getWriter());
+    }
 }
