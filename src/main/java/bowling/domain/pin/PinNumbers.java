f2ed4b16379d7aff236c62f36aea0e3c3349d79e
imcool2551 <imcool2551@gmail.com>
2022-05-13 23:25:43 +0900

    refactor+test: PinNumbers
    첫 번째 투구와 두 번째 투구를 각각 설정할 수 있도록 변경했습니다.
    원래 NormalFrame, FinalFrame에서 동시에 사용했지만 NormalFrame에서만 사용하도록 변경했습니다.

--- src/main/java/bowling/domain/pin/PinNumbers.java
+++ src/main/java/bowling/domain/pin/PinNumbers.java
@@ -1,48 +1,33 @@
 package bowling.domain.pin;
 
-import java.util.Objects;
-
-import static bowling.domain.pin.PinNo.MAX_PIN_NO;
-
 public class PinNumbers {
 
     private final PinNo firstNo;
-    private final PinNo secondNo;
+    private PinNo secondNo;
 
-    public PinNumbers(int firstNo, int secondNo) {
-        validateNumbers(firstNo, secondNo);
+    public PinNumbers(int firstNo) {
         this.firstNo = PinNo.of(firstNo);
-        this.secondNo = PinNo.of(secondNo);
     }
 
-    private void validateNumbers(int firstNo, int secondNo) {
-        if (firstNo != MAX_PIN_NO && firstNo + secondNo > MAX_PIN_NO) {
-            throw new IllegalArgumentException("invalid pin numbers: " + firstNo + ", " + secondNo);
+    public boolean isFull() {
+        return firstNo.isMaxNo() || secondNo != null;
+    }
+
+    public void addPin(int pinNo) {
+        if (firstNo.isMaxNo()) {
+            throw new IllegalStateException("can't add extra pin at strike");
         }
+        if (!firstNo.canPlus(pinNo)) {
+            throw new IllegalStateException("can't add pin");
+        }
+        this.secondNo = PinNo.of(pinNo);
     }
 
-    public PinStatus getStatus() {
-        return PinStatus.getStatus(firstNo, secondNo);
+    public PinNo getFirstNo() {
+        return firstNo;
     }
 
-    public int getFirstNo() {
-        return firstNo.getNo();
-    }
-
-    public int getSecondNo() {
-        return secondNo.getNo();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PinNumbers that = (PinNumbers) o;
-        return Objects.equals(firstNo, that.firstNo) && Objects.equals(secondNo, that.secondNo);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(firstNo, secondNo);
+    public PinNo getSecondNo() {
+        return secondNo;
     }
 }
--- src/test/java/bowling/domain/pin/PinNumbersTest.java
+++ src/test/java/bowling/domain/pin/PinNumbersTest.java
@@ -1,5 +1,6 @@
 package bowling.domain.pin;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -7,17 +8,30 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class PinNumbersTest {
+class PinNumbersTest {
 
-    @ParameterizedTest(name = "첫 번째 투구가 스트라이크가 아니면 두 투구의 합이 10을 넘을 수 없다")
-    @CsvSource({"8,4", "7, 5", "1, 10"})
-    void validation(int firstNo, int secondNo) {
-        assertThatThrownBy(() -> new PinNumbers(firstNo, secondNo))
-                .isInstanceOf(IllegalArgumentException.class);
+    @DisplayName("두 번째 투구까지 채우면 isFull true. 스트라이크는 한 번만 투구하면 true")
+    @Test
+    void isFull() {
+        PinNumbers nine = new PinNumbers(9);
+
+        assertThat(nine.isFull()).isFalse();
+        nine.addPin(1);
+        assertThat(nine.isFull()).isTrue();
+        assertThat(new PinNumbers(10).isFull()).isTrue();
     }
 
+    @ParameterizedTest(name = "두 투구의 합이 10을 넘을 수 없다")
+    @CsvSource({"8,4", "7, 5", "1, 10"})
+    void addPin_WhenPinNumberSumIsOver10_ThrowsIllegalStateException(int firstNo, int secondNo) {
+        assertThatThrownBy(() -> new PinNumbers(firstNo).addPin(secondNo))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    @DisplayName("첫 번째 투구가 스트라이크면 핀을 추가할 수 없다")
     @Test
-    void equals() {
-        assertThat(new PinNumbers(5, 3)).isEqualTo(new PinNumbers(5, 3));
+    void addPin_FirstNumberIs10_ThrowsIllegalStateException() {
+        assertThatThrownBy(() -> new PinNumbers(10).addPin(1))
+                .isInstanceOf(IllegalStateException.class);
     }
 }
