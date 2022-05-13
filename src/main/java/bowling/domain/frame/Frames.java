ff6a15cc29fdbec28928bd03e6cfde9229f3b235
imcool2551 <imcool2551@gmail.com>
2022-05-13 23:38:04 +0900

    refactor+test: Frames
    NormalFrame, FinalFrame을 구분하지 않고 List<Frame>을 통해 하나의 리스트로 관리하도록 변경했습니다.
    프레임 추가 또한 구분하지 않고 addPin()을 통해 동일하게 추가합니다.

--- src/main/java/bowling/domain/frame/Frames.java
+++ src/main/java/bowling/domain/frame/Frames.java
@@ -2,56 +2,45 @@ package bowling.domain.frame;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class Frames {
 
-    private static final int INITIAL_NORMAL_FRAME_NO = 1;
-    private static final int NORMAL_FRAME_MAX_SIZE = 9;
+    public static final int MAX_FRAMES_SIZE = 10;
 
-    private final List<Frame> normalFrames = new ArrayList<>();
-    private Frame finalFrame;
+    private final List<Frame> frames = new ArrayList<>();
 
-    public Frames() {
+    public Frames(int pinNo) {
+        frames.add(NormalFrame.init(pinNo));
     }
 
-    public void addNormalFrame(int firstNo, int secondNo) {
-        if (normalFrames.size() == NORMAL_FRAME_MAX_SIZE) {
-            throw new IllegalStateException("normal frame is full. max size is " + NORMAL_FRAME_MAX_SIZE);
+    public void addPin(int pinNo) {
+        if (isFinished()) {
+            throw new IllegalStateException("can't add more pins");
         }
-        if (normalFrames.isEmpty()) {
-            normalFrames.add(new NormalFrame(INITIAL_NORMAL_FRAME_NO, firstNo, secondNo));
+
+        Frame lastFrame = getLastFrame();
+        if (lastFrame.isFull()) {
+            frames.add(lastFrame.nextFrame(pinNo));
             return;
         }
-        normalFrames.add(getLastFrame().next(firstNo, secondNo));
+        lastFrame.addPin(pinNo);
     }
 
-    public void addFinalFrame(int firstNo, int secondNo) {
-        if (normalFrames.size() != NORMAL_FRAME_MAX_SIZE) {
-            throw new IllegalStateException("normal frame has to be full to set final frame");
-        }
-        finalFrame = getLastFrame().next(firstNo, secondNo);
-    }
-
-    public void addExtra(int extraNo) {
-        if (finalFrame == null) {
-            throw new IllegalStateException("final frame must be set to add extra");
-        }
-        if (finalFrame.getClass() != FinalFrame.class) {
-            throw new IllegalStateException("last frame must be final frame to add extra");
-        }
-        ((FinalFrame) finalFrame).addExtra(extraNo);
+    public boolean isFinished() {
+        return frames.size() == MAX_FRAMES_SIZE && getLastFrame().isFull();
     }
 
     private Frame getLastFrame() {
-        return normalFrames.get(normalFrames.size() - 1);
+        return frames.get(frames.size() - 1);
     }
 
-    public List<Frame> getNormalFrames() {
-        return new ArrayList<>(normalFrames);
+    public int currentFrame() {
+        return getLastFrame().isFull()
+                ? frames.size() + 1
+                : frames.size();
     }
 
-    public Optional<Frame> getFinalFrame() {
-        return Optional.ofNullable(finalFrame);
+    public List<Frame> getFrames() {
+        return new ArrayList<>(frames);
     }
 }
--- src/test/java/bowling/domain/frame/FramesTest.java
+++ src/test/java/bowling/domain/frame/FramesTest.java
@@ -1,69 +1,56 @@
 package bowling.domain.frame;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatNoException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class FramesTest {
 
-    @DisplayName("Frames 크기가 최대(10)일 때 append 호출하면 예외")
     @Test
-    void addNormalFrame_WhenCalledAtMaxSize_ThrowsIllegalStateException() {
-        Frames frames = createFullNormalFrames();
+    void addPin_WhenFinished_ThrowsIllegalStateException() {
+        Frames fullFrames = createFullFrames();
 
-        assertThatThrownBy(() -> frames.addNormalFrame(0, 0))
-                .isInstanceOf(IllegalStateException.class);
-    }
-
-    @DisplayName("Frames의 NormalFrame을 다 안 채우고 FinalFrame을 추가하면 예외")
-    @Test
-    void addFinalFrame_WhenNormalFrameNotFull_ThrowsIllegalStateException() {
-        assertThatThrownBy(() -> new Frames().addFinalFrame(0, 0))
+        assertThatThrownBy(() -> fullFrames.addPin(2))
                 .isInstanceOf(IllegalStateException.class);
     }
 
     @Test
-    void addFinalFrame() {
-        Frames frames = createFullNormalFrames();
+    void addPin_Adds9NormalFramesAnd1FinalFrame() {
+        Frames fullFrames = createFullFrames();
 
-        assertThatNoException().isThrownBy(() -> frames.addFinalFrame(0, 0));
-    }
-
-    @DisplayName("Frames의 FinalFrame을 추가 안하고 addExtra 호출하면 예외")
-    @Test
-    void addExtra_WhenFinalFrameIsNull_ThrowsIllegalStateException() {
-        assertThatThrownBy(() -> new Frames().addExtra(0))
-                .isInstanceOf(IllegalStateException.class);
-    }
-
-    @DisplayName("Franes의 FinalFrame이 스트라이크/스페어 아닐 때 addExtra 호출하면 예외")
-    @Test
-    void addExtra_WhenFinalFrameNeitherStrikeNorSpare_ThrowsIllegalStateException() {
-        Frames frames = createFullNormalFrames();
-
-        frames.addFinalFrame(5, 4);
-
-        assertThatThrownBy(() -> frames.addExtra(5))
-                .isInstanceOf(IllegalStateException.class);
+        List<Frame> frames = fullFrames.getFrames();
+        for (int i = 0; i < frames.size() - 1; i++) {
+            assertThat(frames.get(i)).isInstanceOf(NormalFrame.class);
+        }
+        assertThat(frames.get(frames.size() - 1)).isInstanceOf(FinalFrame.class);
     }
 
     @Test
-    void addExtra() {
-        Frames frames = createFullNormalFrames();
-
-        assertThatNoException().isThrownBy(() -> {
-            frames.addFinalFrame(10, 0);
-            frames.addExtra(0);
-        });
+    void isFinished() {
+        assertThat(new Frames(10).isFinished()).isFalse();
+        assertThat(createFullFrames().isFinished()).isTrue();
     }
 
-    private Frames createFullNormalFrames() {
-        Frames frames = new Frames();
+    private Frames createFullFrames() {
+        Frames frames = new Frames(7);
+        frames.addPin(2);
+
         for (int i = 0; i < 9; i++) {
-            frames.addNormalFrame(0, 0);
+            frames.addPin(7);
+            frames.addPin(2);
         }
         return frames;
+    }
+
+    @Test
+    void currentFrame() {
+        Frames frames = new Frames(7);
+
+        assertThat(frames.currentFrame()).isEqualTo(1);
+        frames.addPin(2);
+        assertThat(frames.currentFrame()).isEqualTo(2);
     }
 }
