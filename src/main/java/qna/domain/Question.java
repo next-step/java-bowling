6d4bfe1e33f34ce6ae6e9d1770d3481e3158834a
younlll <youn.lee96@gmail.com>
2022-05-14 23:51:29 +0900

    refactor: indent 1이 되도록 수정

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -97,9 +97,7 @@ public class Question extends AbstractEntity {
 
         List<Answer> answers = getAnswers();
         for (Answer answer : answers) {
-            if (!answer.isOwner(loginUser)) {
-                throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
-            }
+            validateAnswerAuthor(answer, loginUser);
         }
 
         List<DeleteHistory> deleteHistories = new ArrayList<>();
@@ -110,6 +108,12 @@ public class Question extends AbstractEntity {
             deleteHistories.add(new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now()));
         }
         return deleteHistories;
+    }
+
+    private void validateAnswerAuthor(Answer answer, User loginUser) throws CannotDeleteException {
+        if(!answer.isOwner(loginUser)) {
+            throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+        }
     }
 
     @Override
