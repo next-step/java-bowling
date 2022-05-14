72f23945ae8507664a1b9ad7e102e383ffa5ec85
Roman <roman@channel.io>
2022-05-14 18:34:14 +0900

    Refactor & Test :: Qustion 리팩토링 및 테스트 수정

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -1,10 +1,8 @@
 package qna.domain;
 
-import org.hibernate.annotations.Where;
+import java.time.LocalDateTime;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 public class Question extends AbstractEntity {
@@ -18,78 +16,121 @@ public class Question extends AbstractEntity {
     @JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"))
     private User writer;
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
-    @Where(clause = "deleted = false")
-    @OrderBy("id ASC")
-    private List<Answer> answers = new ArrayList<>();
+    private final Answers answers = new Answers();
 
     private boolean deleted = false;
-
-    public Question() {
-    }
 
     public Question(String title, String contents) {
         this.title = title;
         this.contents = contents;
     }
 
-    public Question(long id, String title, String contents) {
+    public Question(long id, String title, String contents, User user) {
         super(id);
         this.title = title;
         this.contents = contents;
+        this.writer = user;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public Question setTitle(String title) {
+    public Question(String title, String contents, User user) {
         this.title = title;
-        return this;
-    }
-
-    public String getContents() {
-        return contents;
-    }
-
-    public Question setContents(String contents) {
         this.contents = contents;
-        return this;
+        this.writer = user;
     }
 
-    public User getWriter() {
-        return writer;
+    public boolean isWrittenBy(User user) {
+        return this.writer == user;
     }
 
-    public Question writeBy(User loginUser) {
-        this.writer = loginUser;
-        return this;
+    public boolean hasAnswerNotWrittenBy(User user) {
+        return answers.hasAnswerNotWrittenBy(user);
     }
 
-    public void addAnswer(Answer answer) {
-        answer.toQuestion(this);
+    public DeleteHistory delete(User user, LocalDateTime at) {
+        this.deleted = true;
+        return new DeleteHistory(ContentType.QUESTION, id, user, at);
+    }
+
+    public DeleteHistories deleteAnswers(User user, LocalDateTime at) {
+        return answers.deleteAllAnswerWrittenBy(user, at);
+    }
+
+    public Question addAnswer(Answer answer) {
         answers.add(answer);
-    }
-
-    public boolean isOwner(User loginUser) {
-        return writer.equals(loginUser);
-    }
-
-    public Question setDeleted(boolean deleted) {
-        this.deleted = deleted;
         return this;
     }
 
     public boolean isDeleted() {
-        return deleted;
-    }
-
-    public List<Answer> getAnswers() {
-        return answers;
+        return this.deleted;
     }
 
     @Override
     public String toString() {
-        return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
+        return "Question [id=" + id + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
     }
+
+//    public static class Builder extends AbstractEntity {
+//        private Long id;
+//        private String title;
+//        private String contents;
+//        private User writer;
+//        private Answers answers;
+//        private boolean deleted;
+//
+//        public Builder id(Long id) {
+//            this.id = id;
+//            return this;
+//        }
+//
+//        public Builder title(String title) {
+//            this.title = title;
+//            return this;
+//        }
+//
+//        public Builder contents(String contents) {
+//            this.contents = contents;
+//            return this;
+//        }
+//
+//        public Builder writer(User writer) {
+//            this.writer = writer;
+//            return this;
+//        }
+//
+//        public Builder answers(Answers answers) {
+//            this.answers = answers;
+//            return this;
+//        }
+//
+//        public Builder deleted(boolean deleted) {
+//            this.deleted = deleted;
+//            return this;
+//        }
+//
+//        public Question build() {
+//            Question question = new Question();
+//            if (this.id != null) {
+//                question.id = this.id;
+//            }
+//            if (this.title != null) {
+//                question.title = this.title;
+//            }
+//            if (this.contents != null) {
+//                question.contents = this.contents;
+//            }
+//            if (this.contents != null) {
+//                question.contents = this.contents;
+//            }
+//            if (this.writer != null) {
+//                question.writer = this.writer;
+//            }
+//            if (this.answers != null) {
+//                question.answers = this.answers;
+//            }
+//            if (this.answers != null) {
+//                question.answers.addAll(this.answers);
+//            }
+//
+//        }
+//    }
 }
