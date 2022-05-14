12862abd0153997f5a2316718c29d90f47d63dde
cooper <pbg0205@naver.com>
2022-05-14 10:29:11 +0900

    feat: State 클래스 테스트 작성

---
+++ src/test/java/bowling/model/state/FirstBowlTest.java
@@ -0,0 +1,43 @@
+package bowling.model.state;
+
+import bowling.model.Pins;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FirstBowlTest {
+
+    private final State firstBowl = FirstBowl.create(Pins.create(5));
+
+    @Test
+    @DisplayName("첫 번째 볼을 생성한다")
+    void create() {
+        assertThat(firstBowl).isInstanceOf(FirstBowl.class);
+    }
+
+    @Test
+    @DisplayName("첫 번째 볼은 프레임을 진행하고 있는 상태이다")
+    void isFrameEndFalse() {
+        assertThat(firstBowl.isFrameEnd()).isFalse();
+    }
+
+    @Test
+    @DisplayName("첫 번째 볼은 쓰러트린 핀을 반환한다")
+    void getSymbol() {
+        assertThat(firstBowl.getSymbol()).isEqualTo("5");
+    }
+
+    @Test
+    @DisplayName("두 시도만에 모든 볼링핀을 쓰러트리면 스페어를 반환한다")
+    void spare() {
+        assertThat(firstBowl.pitch(Pins.create(5))).isInstanceOf(Spare.class);
+    }
+
+    @Test
+    @DisplayName("두 시도만에 모든 볼링핀을 못 쓰러트리면 미스를 반환한다")
+    void miss() {
+        assertThat(firstBowl.pitch(Pins.create(4))).isInstanceOf(Miss.class);
+    }
+
+}
---
+++ src/test/java/bowling/model/state/ReadyTest.java
@@ -0,0 +1,49 @@
+package bowling.model.state;
+
+import bowling.exception.ImpossiblePitchException;
+import bowling.model.Pins;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ReadyTest {
+
+    private final State ready = Ready.create();
+
+    @Test
+    @DisplayName("생성을 확인한다")
+    void create() {
+        assertThat(ready).isInstanceOf(Ready.class);
+    }
+
+    @Test
+    @DisplayName("빈 심볼을 반환한다")
+    void getSymbol() {
+        assertThat(ready.getSymbol()).isEqualTo("");
+    }
+
+    @Test
+    @DisplayName("프레임을 종료된 상태이다")
+    void isFrameEnd() {
+        assertThat(ready.isFrameEnd()).isFalse();
+    }
+
+    @Test
+    @DisplayName("첫 시도에 모든 핀을 제거할 경우 스트라이크를 반환한다")
+    void createStrike() {
+        //when, then
+        assertThat(ready.pitch(Pins.create(10))).isInstanceOf(Strike.class);
+    }
+
+    @Test
+    @DisplayName("첫 시도에 모든핀을 제거하지 않으면 첫번쨰 볼을 반환한다")
+    void createFirstBowl() {
+        //given
+        Pins pitchedPins = Pins.create(9);
+
+        //when, then
+        assertThat(ready.pitch(pitchedPins)).isInstanceOf(FirstBowl.class);
+    }
+
+}
