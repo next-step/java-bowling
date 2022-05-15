54b92b0643b5a131b4579a640a59047dd9d73a76
itdar <thesockerr@gmail.com>
2022-05-15 19:30:42 +0900

    refactor: QnA 서비스레이어에서 도메인레이어로 로직 분리 중 커밋

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -5,6 +5,7 @@ import org.hibernate.annotations.Where;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import qna.CannotDeleteException;
 
 @Entity
 public class Question extends AbstractEntity {
@@ -91,5 +92,29 @@ public class Question extends AbstractEntity {
     @Override
     public String toString() {
         return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
+    }
+
+    public void delete(User user) throws CannotDeleteException {
+        if (!writer.equals(user)) {
+            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+        }
+
+        delete();
+    }
+
+    protected void delete() throws CannotDeleteException {
+        boolean hasDifferentOwner = answers.stream()
+            .anyMatch(answer -> !answer.isOwner(this.writer));
+
+        if (hasDifferentOwner) {
+            throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+        }
+
+        answers.forEach(Answer::delete);
+        this.deleted = true;
+    }
+
+    public void doNotDelete() {
+        this.deleted = false;
     }
 }
