cddde5eb90636c52f874b24f94bee001bef12c3c
wu2ee <gulb1602@gmail.com>
2022-05-16 02:14:29 +0900

    chore : 테스트를 위한 UserTest 상수 추가

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -3,4 +3,5 @@ package qna.domain;
 public class UserTest {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+    public static final User WU22E = new User(3L, "wu22e", "password", "name", "wu22e@slipp.net");
 }
