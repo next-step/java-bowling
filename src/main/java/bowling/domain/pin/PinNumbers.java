6a8552d91a1bb9b0d726214d1e16683886e3b6f2
imcool2551 <imcool2551@gmail.com>
2022-05-16 15:17:48 +0900

    refactor: PinNumbers
    기존에 NormalFrame에서만 사용하던 PinNumbers를 인터페이스로 만들고 NormalFrame, FinalFrame에서 사용할 NormalPinNumbers, FinalPinNumbers를 따로 정의했습니다.
    1. isFull(): 핀이 꽉찼는지 검사
    2. addPin(int pinNo): 핀 추가
    3. toExpression(): 문자열 표현 반환
    4. getResult(): 핀 번호로 프레임 결과 반환 (Miss/Spare/Strike)
    5. spareBonus(): 이전 프레임이 스페어일 경우 합산할 보너스 점수 반환
    6. strikeBonus(): 이전 프레임이 스트라이크일 경우 합산할 보너스 점수 반환

--- src/main/java/bowling/domain/pin/PinNumbers.java
+++ src/main/java/bowling/domain/pin/PinNumbers.java
@@ -1,31 +1,18 @@
 package bowling.domain.pin;
 
-import bowling.domain.util.PinNoPrinter;
+import bowling.domain.frameresult.FrameResult;
 
-public class PinNumbers {
+public interface PinNumbers {
 
-    private final PinNo firstNo;
-    private PinNo secondNo;
+     boolean isFull();
 
-    public PinNumbers(int firstNo) {
-        this.firstNo = PinNo.of(firstNo);
-    }
+     void addPin(int pinNo);
 
-    public boolean isFull() {
-        return firstNo.isMaxNo() || secondNo != null;
-    }
+     String toExpression();
 
-    public void addPin(int pinNo) {
-        if (firstNo.isMaxNo()) {
-            throw new IllegalStateException("can't add extra pin at strike");
-        }
-        if (!firstNo.canPlus(pinNo)) {
-            throw new IllegalStateException("can't add pin");
-        }
-        this.secondNo = PinNo.of(pinNo);
-    }
+     FrameResult getResult();
 
-    public String toExpression() {
-        return PinNoPrinter.print(firstNo, secondNo);
-    }
+     int spareBonus();
+
+     int strikeBonus();
 }
