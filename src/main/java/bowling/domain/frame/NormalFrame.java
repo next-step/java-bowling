9fb2b5832fb093463ca792aa17ee5312710ead24
imcool2551 <imcool2551@gmail.com>
2022-05-16 15:25:10 +0900

    feat+test: NormalFrame
    1~9프레임을 나타내는 NormalFrame 클래스입니다.

--- src/main/java/bowling/domain/frame/NormalFrame.java
+++ src/main/java/bowling/domain/frame/NormalFrame.java
@@ -1,6 +1,9 @@
 package bowling.domain.frame;
 
-import bowling.domain.pin.PinNumbers;
+import bowling.domain.frameresult.FrameResult;
+import bowling.domain.frameresult.Miss;
+import bowling.domain.frameresult.Spare;
+import bowling.domain.pin.NormalPinNumbers;
 
 import static bowling.domain.frame.NormalFrameNo.MIN_NORMAL_FRAME_NO;
 
@@ -8,11 +11,13 @@ public class NormalFrame implements Frame {
 
     private final NormalFrameNo frameNo;
 
-    private final PinNumbers pinNumbers;
+    private final NormalPinNumbers pinNumbers;
+
+    private Frame nextFrame;
 
     NormalFrame(int frameNo, int pinNo) {
         this.frameNo = NormalFrameNo.of(frameNo);
-        this.pinNumbers = new PinNumbers(pinNo);
+        this.pinNumbers = new NormalPinNumbers(pinNo);
     }
 
     public static NormalFrame init(int pinNo) {
@@ -30,15 +35,55 @@ public class NormalFrame implements Frame {
     }
 
     @Override
-    public Frame nextFrame(int pinNo) {
+    public Frame getNextFrame(int pinNo) {
         if (frameNo.isMax()) {
-            return new FinalFrame(pinNo);
+            this.nextFrame = new FinalFrame(pinNo);
+            return nextFrame;
         }
-        return new NormalFrame(frameNo.next(), pinNo);
+        this.nextFrame =  new NormalFrame(frameNo.next(), pinNo);
+        return nextFrame;
     }
 
     @Override
     public String toExpression() {
         return pinNumbers.toExpression();
+    }
+
+    @Override
+    public boolean canGetScore() {
+        if (!isFull()) {
+            return false;
+        }
+
+        FrameResult result = pinNumbers.getResult();
+        if (result instanceof Miss) {
+            return true;
+        }
+        if (result instanceof Spare) {
+            return nextFrame != null;
+        }
+        return nextFrame != null && nextFrame.isFull();
+    }
+
+    @Override
+    public int getScore() {
+        FrameResult result = pinNumbers.getResult();
+        if (result instanceof Miss) {
+            return result.getScoreWithBonus(0);
+        }
+        if (result instanceof Spare) {
+            return result.getScoreWithBonus(nextFrame.spareBonusForPreviousFrame());
+        }
+        return result.getScoreWithBonus(nextFrame.strikeBonusForPreviousFrame());
+    }
+
+    @Override
+    public int spareBonusForPreviousFrame() {
+        return pinNumbers.spareBonus();
+    }
+
+    @Override
+    public int strikeBonusForPreviousFrame() {
+        return pinNumbers.strikeBonus();
     }
 }
--- src/test/java/bowling/domain/frame/NormalFrameTest.java
+++ src/test/java/bowling/domain/frame/NormalFrameTest.java
@@ -9,19 +9,76 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class NormalFrameTest {
 
-    @DisplayName("9번 프레임에서 nextFrame 호출하면 FinalFrame 반환")
+    @DisplayName("9번 프레임에서 getNextFrame 호출하면 FinalFrame 반환")
     @Test
-    void nextFrame_WhenCalledAtMaxFrameNo_ReturnsFinalFrame() {
+    void getNextFrame_WhenCalledAtMaxFrameNo_ReturnsFinalFrame() {
         NormalFrame frame = new NormalFrame(9, 0);
 
-        assertThat(frame.nextFrame(10)).isInstanceOf(FinalFrame.class);
+        assertThat(frame.getNextFrame(10)).isInstanceOf(FinalFrame.class);
     }
 
-    @ParameterizedTest(name = "1~8번 프레임에서 nextFrame 호출하면 NormalFrame 반환")
+    @ParameterizedTest(name = "1~8번 프레임에서 getNextFrame 호출하면 NormalFrame 반환")
     @ValueSource(ints = {1, 8})
-    void nextFrame_WhenCalledNonMaxFrameNo_ReturnsNormalFrame(int frameNo) {
+    void getNextFrame_WhenCalledNonMaxFrameNo_ReturnsNormalFrame(int frameNo) {
         NormalFrame frame = new NormalFrame(frameNo, 0);
 
-        assertThat(frame.nextFrame(0)).isInstanceOf(NormalFrame.class);
+        assertThat(frame.getNextFrame(0)).isInstanceOf(NormalFrame.class);
+    }
+
+    @Test
+    void canGetScore_WhenMiss_ReturnsTrueIfFrameIsFull() {
+        NormalFrame frame = new NormalFrame(9, 0);
+
+        assertThat(frame.canGetScore()).isFalse();
+        frame.addPin(7);
+        assertThat(frame.canGetScore()).isTrue();
+    }
+
+    @Test
+    void canGetScore_WhenSpare_ReturnsTrueIfNextFrameIsPresent() {
+        NormalFrame frame = new NormalFrame(9, 0);
+
+        assertThat(frame.canGetScore()).isFalse();
+        frame.addPin(10);
+        frame.getNextFrame(0);
+        assertThat(frame.canGetScore()).isTrue();
+    }
+
+    @Test
+    void canGetScore_WhenStrike_ReturnsTrueIfNextFrameIsFull() {
+        NormalFrame frame = new NormalFrame(9, 0);
+
+        assertThat(frame.canGetScore()).isFalse();
+        frame.addPin(10);
+        frame.getNextFrame(0).addPin(1);
+        assertThat(frame.canGetScore()).isTrue();
+    }
+
+    @Test
+    void getScore_Miss() {
+        NormalFrame frame = new NormalFrame(9, 5);
+
+        frame.addPin(4);
+
+        assertThat(frame.getScore()).isEqualTo(9);
+    }
+
+    @Test
+    void getScore_Spare() {
+        NormalFrame frame = new NormalFrame(9, 5);
+
+        frame.addPin(5);
+        frame.getNextFrame(5);
+
+        assertThat(frame.getScore()).isEqualTo(15);
+    }
+
+    @Test
+    void getScore_Strike() {
+        NormalFrame frame = new NormalFrame(9, 10);
+
+        frame.getNextFrame(5).addPin(5);
+
+        assertThat(frame.getScore()).isEqualTo(20);
     }
 }
