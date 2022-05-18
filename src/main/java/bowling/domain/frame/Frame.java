be2e99c524e393abe4b3507b44cf9d79a5887b9b
imcool2551 <imcool2551@gmail.com>
2022-05-18 16:51:43 +0900

    refactor: Frame
    Frame인터페이스가 FrameResult를 직접반환하는 대신 계산된 점수를 직접 반환하도록 변경했습니다.

--- src/main/java/bowling/domain/frame/Frame.java
+++ src/main/java/bowling/domain/frame/Frame.java
@@ -1,22 +1,20 @@
 package bowling.domain.frame;
 
-import bowling.domain.frameresult.FrameResult;
-
 import java.util.Optional;
 
 public interface Frame {
-
-    boolean canGetResult();
 
     void addPin(int pinNo);
 
     Frame getNextFrame(int pinNo);
 
-    String toExpression();
+    boolean canGetResult();
 
-    FrameResult getResult();
+
+    Optional<Integer> getResult();
 
     int spareBonusForPreviousFrame();
 
     Optional<Integer> strikeBonusForPreviousFrame();
+    String toExpression();
 }
