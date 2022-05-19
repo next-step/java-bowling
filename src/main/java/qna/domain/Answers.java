7bbf6eea7d1b0c6cf33b1432d65d1ea6aabe9fa9
chkim <cndghks0213@gmail.com>
2022-05-20 05:59:05 +0900

    feat: 여러 Answer을 일급 컬렉션으로 관리

---
+++ src/main/java/qna/domain/Answers.java
@@ -0,0 +1,39 @@
+package qna.domain;
+
+import org.hibernate.annotations.Where;
+import qna.CannotDeleteException;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import java.util.ArrayList;
+import java.util.List;
+
+@Embeddable
+public class Answers extends AbstractEntity {
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    @Where(clause = "deleted = false")
+    @OrderBy("id ASC")
+    private List<Answer> answers = new ArrayList<>();
+
+    public Answers() {
+    }
+
+    public Answers(List<Answer> answers) {
+        this.answers = answers;
+    }
+
+    public List<DeleteHistory> delete(User user) throws CannotDeleteException {
+        List<DeleteHistory> deleteHistories = new ArrayList<>();
+        for (Answer answer : answers) {
+            deleteHistories.add(answer.delete(user));
+        }
+        return deleteHistories;
+    }
+
+    public void addAnswer(Answer answer) {
+        answers.add(answer);
+    }
+}
