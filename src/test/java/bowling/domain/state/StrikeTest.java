65adf0cb7ee5aacf675133d83561579d17b06860
cooper <pbg0205@naver.com>
2022-05-22 00:03:55 +0900

    test: 스트라이크 연산 테스트 추가

--- src/test/java/bowling/domain/state/StrikeTest.java
+++ src/test/java/bowling/domain/state/StrikeTest.java
@@ -1,5 +1,6 @@
 package bowling.domain.state;
 
+import bowling.domain.score.Score;
 import bowling.exception.ImpossiblePitchException;
 import bowling.domain.Pins;
 import org.junit.jupiter.api.DisplayName;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class StrikeTest {
 
@@ -34,6 +36,58 @@ class StrikeTest {
     @DisplayName("투구를 할 수 없다")
     void doNotPitch() {
         assertThatThrownBy(() -> strike.pitch(Pins.create(3))).isInstanceOf(ImpossiblePitchException.class);
+    }
+
+    @Test
+    @DisplayName("스코어 반환을 확인한다")
+    void score() {
+        Score score = strike.score();
+        assertAll(
+                () -> assertThat(score.finishCalculation()).isFalse(),
+                () -> assertThat(score.score()).isEqualTo(10)
+        );
+    }
+
+    @Test
+    @DisplayName("이전 결과가 Miss 이면 이전 스코어를 반환한다")
+    void calculateScoreWhenBeforeStateIsMiss() {
+        Score beforeScore = Score.miss(Pins.create(1));
+        int expectedScore = beforeScore.score();
+
+        Score score = strike.calculateScore(beforeScore);
+
+        assertAll(
+                () -> assertThat(score.finishCalculation()).isTrue(),
+                () -> assertThat(score.score()).isEqualTo(expectedScore)
+        );
+    }
+
+    @Test
+    @DisplayName("이전 결과가 Spare 이면 이전 스코어와 합산을 확인한다")
+    void calculateScoreWhenBeforeStateIsSpare() {
+        Score beforeScore = Score.spare();
+        int expectedScore = beforeScore.score() + strike.score().score();
+
+        Score score = strike.calculateScore(beforeScore);
+
+        assertAll(
+                () -> assertThat(score.finishCalculation()).isTrue(),
+                () -> assertThat(score.score()).isEqualTo(expectedScore)
+        );
+    }
+
+    @Test
+    @DisplayName("이전 결과가 Strike 이면 이전 스코어와 합산을 확인한다")
+    void calculateScoreWhenBeforeStateIsStrike() {
+        Score beforeScore = Score.strike();
+        int expectedScore = beforeScore.score() + strike.score().score();
+
+        Score score = strike.calculateScore(beforeScore);
+
+        assertAll(
+                () -> assertThat(score.finishCalculation()).isFalse(),
+                () -> assertThat(score.score()).isEqualTo(expectedScore)
+        );
     }
 
 }
