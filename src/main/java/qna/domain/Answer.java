b1d8f708b8734a351866515ca557f0beb66876b7
sanghyeon <sanghyeon@finda.co.kr>
2022-05-22 02:18:31 +0900

    feat: Answer

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -1,9 +1,17 @@
 package qna.domain;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import qna.CannotDeleteException;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
-
-import javax.persistence.*;
 
 @Entity
 public class Answer extends AbstractEntity {
@@ -38,38 +46,56 @@ public class Answer extends AbstractEntity {
             throw new NotFoundException();
         }
 
-        this.writer = writer;
-        this.question = question;
-        this.contents = contents;
-    }
+		this.writer = writer;
+		this.question = question;
+		this.contents = contents;
+	}
 
-    public Answer setDeleted(boolean deleted) {
-        this.deleted = deleted;
-        return this;
-    }
+	public boolean isDeleted() {
+		return deleted;
+	}
 
-    public boolean isDeleted() {
-        return deleted;
-    }
+	public DeleteHistory delete(User loginUser) throws CannotDeleteException {
+		Objects.requireNonNull(loginUser);
+		validateWriter(loginUser);
+		this.deleted = true;
+		return new DeleteHistory(ContentType.ANSWER, getId(), writer, LocalDateTime.now());
+	}
 
-    public boolean isOwner(User writer) {
-        return this.writer.equals(writer);
-    }
+	private void validateWriter(User loginUser) throws CannotDeleteException {
+		if (!writer.equals(loginUser)) {
+			throw new CannotDeleteException("다른 사람이 쓴 답변이라 삭제할 수 없습니다.");
+		}
+	}
 
-    public User getWriter() {
-        return writer;
-    }
+	public User getWriter() {
+		return writer;
+	}
 
-    public String getContents() {
-        return contents;
-    }
+	public void toQuestion(Question question) {
+		this.question = question;
+	}
 
-    public void toQuestion(Question question) {
-        this.question = question;
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		if (!super.equals(o))
+			return false;
+		Answer answer = (Answer)o;
+		return deleted == answer.deleted && Objects.equals(writer, answer.writer) && Objects.equals(
+			question, answer.question) && Objects.equals(contents, answer.contents);
+	}
 
-    @Override
-    public String toString() {
-        return "Answer [id=" + getId() + ", writer=" + writer + ", contents=" + contents + "]";
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(super.hashCode(), writer, question, contents, deleted);
+	}
+
+	@Override
+	public String toString() {
+		return "Answer [id=" + getId() + ", writer=" + writer + ", contents=" + contents + "]";
+	}
 }
