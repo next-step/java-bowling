453d6e1c7dc3fe1066f89d03086659a74277fac1
sanghyeon <sanghyeon@finda.co.kr>
2022-05-22 02:18:40 +0900

    feat: Answers

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,49 @@
 package qna.domain;
 
-public class AnswerTest {
-    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import qna.CannotDeleteException;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@DisplayName(value = "답변 테스트")
+class AnswerTest {
+
+    public Answer answer;
+
+    @BeforeEach
+    void setUp() {
+        answer = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+    }
+
+    @Test
+    void 동등성_테스트() {
+        assertThat(answer).isEqualTo(new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1"));
+    }
+
+    @Test
+    void 답변자와_로그인_유저가_같은_경우_삭제() throws CannotDeleteException {
+        answer.delete(UserTest.JAVAJIGI);
+        assertThat(answer.isDeleted()).isTrue();
+    }
+
+    @Test
+    void 로그인_유저가_널이면_예외() {
+        assertThatNullPointerException().isThrownBy(
+            () -> answer.delete(null)
+        );
+
+    }
+
+    @Test
+    void 답변자와_로그인_유저가_다른_경우_예외() {
+        assertThatThrownBy(
+            () -> answer.delete(UserTest.SANJIGI)
+        ).isExactlyInstanceOf(CannotDeleteException.class);
+    }
 }
