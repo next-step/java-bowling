b04419c29021a6eb0e2c1c1c59df2359afd22373
cooper <pbg0205@naver.com>
2022-05-22 00:23:29 +0900

    test: 미스 연산 테스트 추가

--- src/test/java/bowling/domain/state/MissTest.java
+++ src/test/java/bowling/domain/state/MissTest.java
@@ -1,5 +1,6 @@
 package bowling.domain.state;
 
+import bowling.domain.score.Score;
 import bowling.exception.ImpossiblePitchException;
 import bowling.domain.Pins;
 import org.junit.jupiter.api.DisplayName;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class MissTest {
 
@@ -46,6 +48,48 @@ class MissTest {
 
         //then
         assertThat(missIncludedGutter.getSymbol()).isEqualTo("-|4");
+    }
+
+    @Test
+    @DisplayName("이전 결과가 Miss 이면 이전 스코어를 반환한다")
+    void calculateScoreWhenBeforeStateIsMiss() {
+        Score beforeScore = Score.miss(Pins.create(1));
+        int expectedScore = beforeScore.score();
+
+        Score score = miss.calculateScore(beforeScore);
+
+        assertAll(
+                () -> assertThat(score.finishCalculation()).isTrue(),
+                () -> assertThat(score.score()).isEqualTo(expectedScore)
+        );
+    }
+
+    @Test
+    @DisplayName("이전 결과가 Spare 이면 이전 스코어와 합산을 확인한다")
+    void calculateScoreWhenBeforeStateIsSpare() {
+        Score beforeScore = Score.spare();
+        int expectedScore = beforeScore.score() + Pins.create(5).count();
+
+        Score score = miss.calculateScore(beforeScore);
+
+        assertAll(
+                () -> assertThat(score.finishCalculation()).isTrue(),
+                () -> assertThat(score.score()).isEqualTo(expectedScore)
+        );
+    }
+
+    @Test
+    @DisplayName("이전 결과가 Strike 이면 이전 스코어와 합산을 확인한다")
+    void calculateScoreWhenBeforeStateIsStrike() {
+        Score beforeScore = Score.strike();
+        int expectedScore = beforeScore.score() + miss.score().score();
+
+        Score score = miss.calculateScore(beforeScore);
+
+        assertAll(
+                () -> assertThat(score.finishCalculation()).isTrue(),
+                () -> assertThat(score.score()).isEqualTo(expectedScore)
+        );
     }
 
 }
