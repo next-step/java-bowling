0fab8987b8a5f07fa95891c04acd83832fec6162
imcool2551 <imcool2551@gmail.com>
2022-05-21 19:38:27 +0900

    feat+test: Frames
    Main에서 사용하는 Frames클래스입니다. 1~10번 프레임을 리스트로 관리합니다.
    게임 종료 여부는 마지막 FinalFrame이 종료됐는지 여부로 판단하고, 종료전 까지 투구를 할 수 있습니다.
    View에서 사용할 Frame들의 점수와 문자열 표현을 각각 리스트로 반환합니다.

--- src/test/java/bowling/domain/frame/FramesTest.java
+++ src/test/java/bowling/domain/frame/FramesTest.java
@@ -1,8 +1,7 @@
 package bowling.domain.frame;
 
+import bowling.domain.pin.Pin;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -10,61 +9,59 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class FramesTest {
 
     @Test
-    void addPin_WhenFinished_ThrowsIllegalStateException() {
-        Frames fullFrames = createFullFrames();
+    void bowl_WhenFinished_ThrowsIllegalStateException() {
+        Frames frames = createFullFrames();
 
-        assertThatThrownBy(() -> fullFrames.addPin(2))
+        assertThatThrownBy(() -> frames.bowl(Pin.of(1)))
                 .isInstanceOf(IllegalStateException.class);
     }
 
     @Test
-    void addPin_Adds9NormalFramesAnd1FinalFrame() {
-        Frames fullFrames = createFullFrames();
-
-        List<Frame> frames = fullFrames.getFrames();
-        for (int i = 0; i < frames.size() - 1; i++) {
-            assertThat(frames.get(i)).isInstanceOf(NormalFrame.class);
-        }
-        assertThat(frames.get(frames.size() - 1)).isInstanceOf(FinalFrame.class);
-    }
-
-    @Test
-    void isFinished() {
-        assertThat(new Frames(10).isFinished()).isFalse();
-        assertThat(createFullFrames().isFinished()).isTrue();
-    }
-
-    private Frames createFullFrames() {
-        Frames frames = new Frames(7);
-        frames.addPin(2);
-
-        for (int i = 0; i < 9; i++) {
-            frames.addPin(7);
-            frames.addPin(2);
-        }
-        return frames;
-    }
-
-    @Test
     void currentFrame() {
-        Frames frames = new Frames(7);
+        Frames frames = new Frames();
 
-        assertThat(frames.currentFrame()).isEqualTo(1);
-        frames.addPin(2);
+        frames.bowl(Pin.of(4));
+        frames.bowl(Pin.of(5));
+
         assertThat(frames.currentFrame()).isEqualTo(2);
     }
 
     @Test
-    void getScores() {
-        Frames frames = new Frames(1);
+    void scores() {
+        Frames frames = new Frames();
 
-        assertThat(frames.getScores()).isEmpty();
-        frames.addPin(1); // 1frame: 1 + 1 = 2
+        frames.bowl(Pin.of(4));
+        frames.bowl(Pin.of(5));
 
-        frames.addPin(9);
-        frames.addPin(1);
-        frames.addPin(2); // 2frame: 9 + 1 + 2 = 12
-        frames.addPin(7); // 3frame: 2 + 7 = 9
-        assertThat(frames.getScores()).containsExactly(2, 12, 9);
+        frames.bowl(Pin.of(5));
+        frames.bowl(Pin.of(5));
+
+        frames.bowl(Pin.of(5));
+
+        assertThat(frames.scores()).containsExactly(9, 15);
+    }
+
+    @Test
+    void expressions() {
+        Frames frames = new Frames();
+
+        frames.bowl(Pin.of(4));
+        frames.bowl(Pin.of(5));
+
+        frames.bowl(Pin.of(5));
+        frames.bowl(Pin.of(5));
+
+        frames.bowl(Pin.of(5));
+
+        assertThat(frames.expressions()).containsExactly("4|5", "5|/", "5");
+    }
+
+    private Frames createFullFrames() {
+        Frames frames = new Frames();
+        for (int i = 0; i < 10; i++) {
+            frames.bowl(Pin.of(4));
+            frames.bowl(Pin.of(5));
+        }
+        return frames;
     }
 }
