5b483ec0d0070c27fcc56e89006d26efc3062a26
eun jung <62321968+dmswjd4117@users.noreply.github.com>
2022-05-23 12:19:15 +0900

    refactor: 반복되는 부분 분리

--- src/test/java/qna/domain/AnswersTest.java
+++ src/test/java/qna/domain/AnswersTest.java
@@ -1,5 +1,6 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import qna.CannotDeleteException;
@@ -9,29 +10,27 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AnswersTest {
 
+    private Answers answers;
+
+    @BeforeEach
+    void before(){
+        Question question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        answers = new Answers();
+        answers.add(new Answer(0L, UserTest.JAVAJIGI, question, "Answers Contents1"));
+        answers.add(new Answer(1L, UserTest.JAVAJIGI, question, "Answers Contents2"));
+    }
+
     @Test
     @DisplayName("답변자와 질문자가 다를 경우 삭제할수 없다.")
     void 삭제_실패(){
-
-        Answers answers = new Answers();
-        Question question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-        answers.add(new Answer(UserTest.JAVAJIGI, question, "Answers Contents1"));
-        answers.add(new Answer(UserTest.SANJIGI, question, "Answers Contents2"));
-
         assertThatThrownBy(()->{
-            answers.delete(UserTest.JAVAJIGI);
+            answers.delete(UserTest.SANJIGI);
         }).isInstanceOf(CannotDeleteException.class);
     }
 
     @Test
     @DisplayName("답변자와 질문자가 다를 경우 삭제할수 있다.")
     void 삭제_성공_deleteHistory_반환(){
-        Question question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-
-        Answers answers = new Answers();
-        answers.add(new Answer(0L, UserTest.JAVAJIGI, question, "Answers Contents1"));
-        answers.add(new Answer(1L, UserTest.JAVAJIGI, question, "Answers Contents2"));
-
         assertThat(answers.delete(UserTest.JAVAJIGI)).containsExactly(
                 new DeleteHistory(ContentType.ANSWER, 0L, UserTest.JAVAJIGI),
                 new DeleteHistory(ContentType.ANSWER, 1L, UserTest.JAVAJIGI)
@@ -41,10 +40,9 @@ class AnswersTest {
     @Test
     @DisplayName("다른 질문에 대한 답변 추가할 시 예외를 던진다.")
     void 다른질문답변_추가시_예외(){
-        Answers answers = new Answers();
+        Question newQuestion = new Question("new", "new").writeBy(UserTest.JAVAJIGI);
         assertThatThrownBy(()->{
-            answers.add(new Answer(0L, UserTest.JAVAJIGI, QuestionTest.Q2, "Answers Contents1"));
-            answers.add(new Answer(1L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents2"));
+            answers.add(new Answer(0L, UserTest.JAVAJIGI, newQuestion, "Answers Contents1"));
         }).isInstanceOf(IllegalArgumentException.class);
     }
 }
