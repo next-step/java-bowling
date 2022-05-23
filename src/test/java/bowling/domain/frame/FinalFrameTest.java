4e998c471d097e49cf079a991994aee54f143879
imcool2551 <imcool2551@gmail.com>
2022-05-23 18:28:55 +0900

    refactor: FinalFrame
    FinalFrame을 구현할 때 FinalExtraPins 대신 List<State> 를 이용했습니다.
    마지막 프레임의 경우 투구에 따른 경우의 수가 다양하기 때문에 주석을 달았습니다.

--- src/test/java/bowling/domain/frame/FinalFrameTest.java
+++ src/test/java/bowling/domain/frame/FinalFrameTest.java
@@ -18,11 +18,8 @@ class FinalFrameTest {
 
     @ParameterizedTest
     @CsvSource({"5,5,5", "10,3,5"})
-    void bowl_WhenExtraPinIsFull_ThrowsIllegalStateException(int firstPin, int secondPin, int thirdPin) {
-        Frame finalFrame = new FinalFrame()
-                .bowl(Pin.of(firstPin))
-                .bowl(Pin.of(secondPin))
-                .bowl(Pin.of(thirdPin));
+    void bowl_WhenFrameIsFinished_ThrowsIllegalStateException(int firstPin, int secondPin, int thirdPin) {
+        Frame finalFrame = createFinalFrame(firstPin, secondPin, thirdPin);
 
         assertThatThrownBy(() -> finalFrame.bowl(Pin.of(5)))
                 .isInstanceOf(IllegalStateException.class);
@@ -40,27 +37,69 @@ class FinalFrameTest {
     }
 
     @ParameterizedTest
-    @CsvSource({"6,4,9,19", "10,5,3,18"})
+    @CsvSource({"6,4,9,19", "6,4,10,20", "10,5,4,19", "10,5,5,20", "10,10,9,29", "10,10,10,30"})
     void score_SpareOrStrike(int firstPin, int secondPin, int thirdPin, int result) {
-        Frame finalFrame = new FinalFrame()
-                .bowl(Pin.of(firstPin))
-                .bowl(Pin.of(secondPin))
-                .bowl(Pin.of(thirdPin));
+        Frame finalFrame = createFinalFrame(firstPin, secondPin, thirdPin);
 
         Score score = finalFrame.score();
 
         assertThat(score.getScore()).isEqualTo(result);
     }
 
-    @Test
-    void additionalScore() {
+    @ParameterizedTest
+    @CsvSource({"5,5", "0,10", "10,0", "10,10"})
+    void finished_False(int firstPin, int secondPin) {
         Frame finalFrame = new FinalFrame()
-                .bowl(Pin.of(10))
-                .bowl(Pin.of(3))
-                .bowl(Pin.of(5));
+                .bowl(Pin.of(firstPin))
+                .bowl(Pin.of(secondPin));
+
+        assertThat(finalFrame.finished()).isFalse();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"5,5,5", "8,2,9", "10,8,1", "10,8,2", "10,10,9", "10,10,10"})
+    void finished_True(int firstPin, int secondPin, int thirdPin) {
+        Frame finalFrame = createFinalFrame(firstPin, secondPin, thirdPin);
+
+        assertThat(finalFrame.finished()).isTrue();
+    }
+
+    @Test
+    void additionalScore_ForSpare() {
+        Frame finalFrame = createFinalFrame(5, 5, 5);
+
+        Score score = finalFrame.additionalScore(Score.ofSpare());
+
+        assertThat(score.getScore()).isEqualTo(15);
+    }
+
+    @Test
+    void additionalScore_ForStrike() {
+        Frame finalFrame = createFinalFrame(5, 5, 5);
 
         Score score = finalFrame.additionalScore(Score.ofStrike());
 
-        assertThat(score.getScore()).isEqualTo(23);
+        assertThat(score.getScore()).isEqualTo(20);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "8,2,9,8|/|9",
+            "8,2,10,8|/|X",
+            "10,8,1,X|8|1",
+            "10,8,2,X|8|/",
+            "10,10,9,X|X|9",
+            "10,10,10,X|X|X"})
+    void expression(int firstPin, int secondPin, int thirdPin, String result) {
+        Frame finalFrame = createFinalFrame(firstPin, secondPin, thirdPin);
+
+        assertThat(finalFrame.expression()).isEqualTo(result);
+    }
+
+    private Frame createFinalFrame(int firstPin, int secondPin, int thirdPin) {
+        return new FinalFrame()
+                .bowl(Pin.of(firstPin))
+                .bowl(Pin.of(secondPin))
+                .bowl(Pin.of(thirdPin));
     }
 }
