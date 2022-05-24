2b2a3c1bbf2dd16137d8e4afdd0ace97f8a7015c
eun jung <62321968+dmswjd4117@users.noreply.github.com>
2022-05-24 16:11:42 +0900

    feat: 마지막 프레임 생성

---
+++ src/main/java/bowling/Frame/FinalFrame.java
@@ -0,0 +1,98 @@
+package bowling.Frame;
+
+import bowling.bowl.*;
+import bowling.pin.Pins;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FinalFrame implements Frame{
+
+    public static final int MAX_PITCH_CNT = 3;
+    private final int index;
+    private List<Bowl> bowl;
+    private int count;
+
+    private FinalFrame(int index, Bowl bowl, int count) {
+        this.index = index;
+        this.bowl = new ArrayList<>();
+        this.bowl.add(bowl);
+        this.count = count;
+    }
+
+    public FinalFrame(int index) {
+        this(index, new First(), 0);
+    }
+
+
+    @Override
+    public Frame pitch(Pins pins) {
+        if(!canPitch()){
+            throw new IllegalArgumentException("더 이상 투구할 수 없습니다.");
+        }
+
+        count += 1;
+        Bowl curBowl = getCurBowl();
+        Bowl resultBowl = curBowl.pitch(pins);
+        changeBowl(resultBowl);
+
+        if(resultBowl.isStrike() || resultBowl.isSpare()){
+            addBall(new First());
+            return this;
+        }
+
+        return this;
+    }
+
+    private boolean canPitch(){
+        Bowl curBowl = getCurBowl();
+        if(curBowl.isGutter() || curBowl.isMiss()){
+            return false;
+        }
+        if(count == MAX_PITCH_CNT){
+            return false;
+        }
+        if(count == 2 && getCurBowl().isSecond()){
+            return false;
+        }
+        return true;
+    }
+
+    private void addBall(Bowl resultBowl) {
+        bowl.add(resultBowl);
+    }
+
+    private void changeBowl(Bowl resultBowl) {
+        bowl.remove(bowl.size()-1);
+        bowl.add(resultBowl);
+    }
+
+    private Bowl getCurBowl() {
+        return bowl.get(bowl.size()-1);
+    }
+
+    @Override
+    public int getIndex() {
+        return index;
+    }
+
+    @Override
+    public Bowl getBowl() {
+        return bowl.get(0);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return canPitch();
+    }
+
+
+    @Override
+    public String toString(){
+        return "[final frame]" +
+                "\nindex: "+index
+                +"\nbowls: "+bowl
+                +"\ncount: "+count;
+    }
+}
