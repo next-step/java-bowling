a4882f128a498fa74952e6728a727efe6eea6815
cooper <pbg0205@naver.com>
2022-05-24 20:45:34 +0900

    feat: BowlingGames 투구 로직 추가

--- src/main/java/bowling/domain/BowlingGames.java
+++ src/main/java/bowling/domain/BowlingGames.java
@@ -5,14 +5,37 @@ import java.util.List;
 
 public class BowlingGames {
 
+    private final int LAST_PLAYER_TURN_INDEX;
+
     private final List<BowlingGame> bowlingGames;
+
+    private int playerTurnIndex = 0;
 
     private BowlingGames(List<BowlingGame> bowlingGames) {
         this.bowlingGames = Collections.unmodifiableList(bowlingGames);
+        LAST_PLAYER_TURN_INDEX = initLastPlayerTurnIndex(bowlingGames);
+    }
+
+    private int initLastPlayerTurnIndex(List<BowlingGame> bowlingGames) {
+        return bowlingGames.size() - 1;
     }
 
     public static BowlingGames create(List<BowlingGame> bowlingGames) {
         return new BowlingGames(bowlingGames);
+    }
+
+    public void play(Pins pins) {
+        BowlingGame bowlingGame = bowlingGames.get(playerTurnIndex);
+        bowlingGame.pitch(pins);
+        changeNextPlayerTurn();
+    }
+
+    private void changeNextPlayerTurn() {
+        int nextPlayerTurnIndex = playerTurnIndex + 1;
+        if(nextPlayerTurnIndex > LAST_PLAYER_TURN_INDEX) {
+            nextPlayerTurnIndex = 0;
+        }
+        playerTurnIndex = nextPlayerTurnIndex;
     }
 
 }
