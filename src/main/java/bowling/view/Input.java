2059828e46f029486d8e966080e986658f095863
eun jung <62321968+dmswjd4117@users.noreply.github.com>
2022-05-24 16:10:32 +0900

    feat: Input, Output 생성

---
+++ src/main/java/bowling/view/Input.java
@@ -0,0 +1,27 @@
+package bowling.view;
+
+import bowling.BowlingGame;
+import bowling.pin.Pins;
+import bowling.player.Player;
+
+import java.util.Scanner;
+
+public class Input {
+    private static final String PLAYER_NAME_MSG = "플레이어 이름은(3 english letters)?: ";
+    private static final String PIN_HIT_CNT_MSG = "%d 프레임 투구 : ";
+
+    private static final Scanner sc = new Scanner(System.in);
+
+    private Input() {
+    }
+
+    public static Player readPlayerName() {
+        System.out.print(PLAYER_NAME_MSG);
+        return new Player(sc.nextLine());
+    }
+
+    public static Pins readHitPin(BowlingGame bowlingGame) {
+        System.out.printf(PIN_HIT_CNT_MSG, bowlingGame.getFameIndex());
+        return new Pins(Integer.parseInt(sc.nextLine()));
+    }
+}
---
+++ src/main/java/bowling/view/Output.java
@@ -0,0 +1,46 @@
+package bowling.view;
+
+import bowling.BowlingGame;
+import bowling.Frame.Frame;
+import bowling.Frame.Frames;
+import bowling.bowl.Bowl;
+import bowling.player.Player;
+import org.apache.logging.log4j.util.Strings;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Output {
+    private static final String BOARD_HEAD = "| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |";
+    private static final String PLAYER_NAME = "|  %s |";
+    private static final String RESULT_FRAME = "  %-3s |";
+    private static final String EMPTY_RESULT_FRAME = "      |";
+    private static final int MAX_FRAME_INDEX = 10;
+
+    public static void printBoard(BowlingGame bowlingGame){
+        System.out.println(BOARD_HEAD);
+        System.out.println(getPlayerName(bowlingGame.getPlayer()) + getResultBody(bowlingGame.getFrames()));
+
+    }
+
+    private static String getPlayerName(Player player) {
+        return String.format(PLAYER_NAME, player.getName());
+    }
+
+    private static String getResultBody(Frames frames) {
+        String res =  frames.getFrames().stream()
+                .map(Frame::getBowl)
+                .filter(bowl -> !bowl.isFirst())
+                .map(Bowl::getSymbol)
+                .map(symbol -> String.format(RESULT_FRAME, symbol))
+                .collect(Collectors.joining());
+
+        String append = IntStream.range(0, MAX_FRAME_INDEX - frames.getFrameIndex())
+                .mapToObj(i->EMPTY_RESULT_FRAME)
+                .collect(Collectors.joining());
+
+        return res + append;
+    }
+
+
+}
