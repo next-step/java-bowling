c1e7289a31aedc8f5e19b55f723cd4cfeb1732d2
younlll <youn.lee96@gmail.com>
2022-05-26 06:14:06 +0900

    feat: 게임 시작과 출력에 대해 구현

--- src/main/java/bowling/controller/BowlingController.java
+++ src/main/java/bowling/controller/BowlingController.java
@@ -1,10 +1,19 @@
 package bowling.controller;
 
+import bowling.domain.BowlingGame;
 import bowling.domain.Player;
 import bowling.view.InputView;
+import bowling.view.OutputView;
 
 public class BowlingController {
     public void run() {
         Player player = new Player(InputView.inputPlayerName());
+        BowlingGame bowlingGame = new BowlingGame(player);
+        OutputView.printBowlingScoreBoard(bowlingGame);
+
+        while (bowlingGame.isNextPitching()) {
+            bowlingGame.bowl(InputView.inputFallenPins(bowlingGame.getCurrentFrameNumber()));
+            OutputView.printBowlingScoreBoard(bowlingGame);
+        }
     }
 }
