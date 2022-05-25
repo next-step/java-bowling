7b667aa45b2f2839b137dc501dec420dc0a3fd7a
pangnem <sprnd645@gmail.com>
2022-05-26 00:58:14 +0900

    test: deleteHistories.saveTo

--- src/test/java/qna/domain/deleteHistory/DeleteHistoriesTest.java
+++ src/test/java/qna/domain/deleteHistory/DeleteHistoriesTest.java
@@ -1,7 +1,15 @@
 package qna.domain.deleteHistory;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import java.util.List;
 
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class DeleteHistoriesTest {
     public static final DeleteHistories SAME_USER = new DeleteHistories(List.of(
             DeleteHistoryTest.DH1,
@@ -10,4 +18,16 @@ public class DeleteHistoriesTest {
     public static final DeleteHistories ONLY_QUESTION = new DeleteHistories(List.of(
             DeleteHistoryTest.DH1
     ));
+
+    @Mock
+    private DeleteHistoryRepository deleteHistoryRepository;
+
+    @Test
+    void saveTo() {
+        SAME_USER.saveTo(deleteHistoryRepository);
+
+        verify(deleteHistoryRepository).saveAll(List.of(
+                DeleteHistoryTest.DH1, DeleteHistoryTest.DH2
+        ));
+    }
 }
