3531dbbaf974ba24adfdd6de71e06320bc33e8ff
Seongheon Sim <river.water.sea@gmail.com>
2022-05-25 23:07:49 +0900

    fix: Question 테스트 코드 수정
    1. Question.deleteHistories() -> Question.deleteHistory() 로 호출 메소드 변경

--- src/test/java/qna/domain/question/QuestionTest.java
+++ src/test/java/qna/domain/question/QuestionTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import qna.domain.user.UserTest;
 import qna.exception.IsNotDeletedException;
-import qna.domain.question.Question;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -23,6 +22,6 @@ public class QuestionTest {
     @Test
     @DisplayName("삭제되지 않은 질문의 삭제 기록 조회 시 예외 처리")
     void deleteHistoryException() {
-        assertThatThrownBy(Q2::deleteHistories).isExactlyInstanceOf(IsNotDeletedException.class);
+        assertThatThrownBy(Q2::deleteHistory).isExactlyInstanceOf(IsNotDeletedException.class);
     }
 }
