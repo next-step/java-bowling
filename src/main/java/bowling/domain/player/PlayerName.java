174bd945e35fab2b60b20975cedc86c638cf9eab
Minjae lee <0201.mj.lee@gmail.com>
2022-05-26 04:05:12 +0900

    refactor: player name 생성 조건 추가

--- src/main/java/bowling/domain/player/PlayerName.java
+++ src/main/java/bowling/domain/player/PlayerName.java
@@ -4,7 +4,10 @@ package bowling.domain.player;
 import org.apache.commons.lang3.StringUtils;
 
 public class PlayerName {
+    static final int NAME_LENGTH = 3;
+
     private final String name;
+
 
     public PlayerName(String name) {
         validate(name);
@@ -15,5 +18,14 @@ public class PlayerName {
         if (StringUtils.isBlank(name)) {
             throw new IllegalArgumentException("PlayerName은 빈 값일 수 없습니다.");
         }
+
+        if (name.length() != NAME_LENGTH) {
+            throw new IllegalArgumentException(String.format("name(%s)은 길이가 %s 이어야 합니다.", name, NAME_LENGTH));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
--- src/test/java/bowling/domain/player/PlayerNameTest.java
+++ src/test/java/bowling/domain/player/PlayerNameTest.java
@@ -2,6 +2,7 @@ package bowling.domain.player;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -11,6 +12,14 @@ class PlayerNameTest {
     @ParameterizedTest
     @NullAndEmptySource
     void PlayerName은_name없이_생성_될_경우_예외를_발생_시킨다(String name) {
+        assertThatThrownBy(() -> {
+            new PlayerName(name);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"LMJJ", "LM"})
+    void PlayerName은_길이가_유효하지_않으면_예외를_발생_시킨다(String name) {
         assertThatThrownBy(() -> {
             new PlayerName(name);
         }).isInstanceOf(IllegalArgumentException.class);
