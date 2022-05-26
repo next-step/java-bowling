b213a85fb7948104ece4efbce04c0ab6278d7b53
dmswjd4117 <62321968+dmswjd4117@users.noreply.github.com>
2022-05-26 09:20:32 +0900

    refactor: Frames frames 리스트 사이즈 반환하는 함수 추가

--- src/main/java/bowling/Frame/Frames.java
+++ src/main/java/bowling/Frame/Frames.java
@@ -1,7 +1,6 @@
 package bowling.Frame;
 
 import bowling.pin.Pins;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -12,7 +11,6 @@ public class Frames {
     private static final int MAX_FRAME_INDEX = 10;
 
     private List<Frame> frames;
-    private int frameIndex;
 
     public Frames(){
         frames = new ArrayList<>();
@@ -21,14 +19,18 @@ public class Frames {
 
     public void pitch(Pins pins){
         Frame nextFrame = getCurFrame().pitch(pins);
+
         if(nextFrame != getCurFrame()){
             frames.add(nextFrame);
         }
     }
 
+    public int size(){
+        return frames.size();
+    }
 
-    private Frame getCurFrame(){
-        return frames.get(frames.size()-1);
+    public Frame getCurFrame(){
+        return frames.get(size()-1);
     }
 
     public List<Frame> getFrames() {
