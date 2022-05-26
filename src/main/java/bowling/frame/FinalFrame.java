d225d6c62bb4bedf8593e3a517e41bfbda84cffc
dmswjd4117 <62321968+dmswjd4117@users.noreply.github.com>
2022-05-26 17:20:24 +0900

    feat: FinalFrame 현재 점수 계산기능, 전 프레임에 보너스점수 추가기능

---
+++ src/main/java/bowling/frame/FinalFrame.java
@@ -0,0 +1,88 @@
+package bowling.frame;
+
+import bowling.bowl.Bowl;
+import bowling.bowl.Ready;
+import bowling.pin.Pins;
+import bowling.score.Score;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class FinalFrame implements Frame{
+
+    private final int index;
+    private FinalBowls bowls;
+
+    private FinalFrame(int index, FinalBowls bowls) {
+        this.index = index;
+        this.bowls = bowls;
+    }
+
+    public FinalFrame(int index) {
+        this(index, new FinalBowls());
+    }
+
+    @Override
+    public Bowl getBowl() {
+        return bowls.getBowls().get(0);
+    }
+
+    @Override
+    public Frame pitch(Pins pins) {
+        bowls.pitch(pins);
+        return this;
+    }
+
+    @Override
+    public int getIndex() {
+        return index;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return bowls.canProceed();
+    }
+
+    @Override
+    public String getSymbol() {
+        return bowls.getBowls().stream()
+                .map(Bowl::getSymbol)
+                .collect(Collectors.joining("|"));
+    }
+
+    @Override
+    public int score() {
+        if(bowls.canProceed()){
+            return Score.CANNOT_CALCULATE_SCORE;
+        }
+        return bowls.getBowls()
+                .stream()
+                .map(Bowl::score)
+                .mapToInt(Score::getValue)
+                .sum();
+    }
+
+    @Override
+    public int calculateAdditionalScore(Score beforeScore) {
+        Score score = new Score(beforeScore.getValue(), beforeScore.getLeft());
+        for (Bowl curBowl : bowls.getBowls()) {
+            if(curBowl instanceof Ready){
+                return Score.CANNOT_CALCULATE_SCORE;
+            }
+            score = curBowl.calculateScore(score);
+            if (score.isFinished()) {
+                return score.getValue();
+            }
+        }
+        return Score.CANNOT_CALCULATE_SCORE;
+    }
+
+    @Override
+    public String toString(){
+        return "[final frame]" +
+                "\nindex: "+index
+                +"\nbowls: "+
+                bowls.getBowls().stream()
+                .map(Objects::toString)
+                .collect(Collectors.joining(" / "));
+    }
+}
---
+++ src/test/java/bowling/frame/FinalFrameTest.java
@@ -0,0 +1,234 @@
+package bowling.frame;
+
+import bowling.exception.CannotPitchException;
+import bowling.pin.Pins;
+import bowling.score.Score;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class FinalFrameTest {
+
+    /*
+     * calculateAdditionalScore 테스트
+     * */
+    @Test
+    @DisplayName("9번째 프레임이 스트라이크일 때 보너스 점수 계산요청하면 2번의 보너스 투구 점수를 더해야한다.")
+    void 스트라이크_프레임에서_보너스점수_계산(){
+        Frame finalFrame = new FinalFrame(10);
+
+        finalFrame.pitch(new Pins(10));
+        finalFrame.pitch(new Pins(2));
+
+        assertThat(finalFrame.calculateAdditionalScore(Score.strike())).isEqualTo(22);
+    }
+
+    @Test
+    @DisplayName("9번째 프레임에서 스페어일 때  보너스 점수 계산요청하면 1번의 투구 점수를 더해야한다.")
+    void 마지막프레임이_스페어_보너스점수_계산(){
+        Frame finalFrame = new FinalFrame(10);
+
+        finalFrame.pitch(new Pins(10));
+
+        assertThat(finalFrame.calculateAdditionalScore(Score.spare(1, 9))).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("9번째 프레임에서 보너스점수 계산 요청할때 계산할 수 없으면 -1을 반환한다.")
+    void 실패_마지막프레임에서_보너스점수_계산(){
+        Frame finalFrame = new FinalFrame(10);
+
+        finalFrame.pitch(new Pins(5));
+
+        assertThat(finalFrame.calculateAdditionalScore(new Score(30, 2))).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("보너스 점수 계산 요청할 때 계산할 수 없으면 -1을 반환한다.")
+    void 실패_마지막_프레임_점수_계산(){
+        Frame finalFrame = new FinalFrame(10);
+
+        assertThat(finalFrame.calculateAdditionalScore(new Score(30, 2))).isEqualTo(-1);
+    }
+
+
+
+    /*
+    * score 테스트
+    * */
+    @Test
+    @DisplayName("스타라이크일때 점수계산")
+    void 마지막프레임이_스트라이크_점수_계산(){
+        Frame finalFrame = new FinalFrame(10);
+
+        finalFrame.pitch(new Pins(10));
+        finalFrame.pitch(new Pins(2));
+
+        assertThat(finalFrame.score()).isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("스타라이크일때 보너스 투구 하지않았으면 점수계산했을 때 -1리턴한다.")
+    void 마지막프레임이_스트라이크_점수_계산_실패(){
+        Frame finalFrame = new FinalFrame(10);
+
+        finalFrame.pitch(new Pins(10));
+
+        assertThat(finalFrame.score()).isEqualTo(-1);
+    }
+
+    @ParameterizedTest
+    @DisplayName("스페어일때 점수계산")
+    @CsvSource(value = {
+            "7, 3, 1, 11",
+            "1, 9, 1, 11",
+            "2, 8, 10, 20"
+    })
+    void 스페어_점수_계산(int first, int second, int bonus, int res){
+        Frame finalFrame = new FinalFrame(10);
+
+        finalFrame.pitch(new Pins(first));
+        finalFrame.pitch(new Pins(second));
+        finalFrame.pitch(new Pins(bonus));
+
+        assertThat(finalFrame.score()).isEqualTo(res);
+    }
+
+    @Test
+    @DisplayName("스페어일 때 보너스 투구를 하지않았으면 점수를 구할 수 없다.")
+    void 스페어_점수_구할수_없다(){
+        Frame finalFrame = new FinalFrame(10);
+
+        // 스페어
+        finalFrame.pitch(new Pins(1));
+        finalFrame.pitch(new Pins(9));
+
+        assertThat(finalFrame.score()).isEqualTo(-1);
+    }
+
+
+    @Test
+    @DisplayName("10번째 프레임이 거터일때 점수계산")
+    void 마지막프레임_거터_점수_계산(){
+        Frame finalFrame = new FinalFrame(10);
+
+        finalFrame.pitch(new Pins(0));
+        finalFrame.pitch(new Pins(0));
+
+        assertThat(finalFrame.score()).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @DisplayName("10번째 프레임이 미스일때 점수계산")
+    @CsvSource(value = {
+            "4, 3, 7",
+            "1, 3, 4",
+            "2, 1, 3"
+    })
+    void 마지막프레임_미스_점수_계산(int first, int second, int res){
+        Frame finalFrame = new FinalFrame(10);
+
+        finalFrame.pitch(new Pins(first));
+        finalFrame.pitch(new Pins(second));
+
+        assertThat(finalFrame.score()).isEqualTo(res);
+    }
+
+
+    @Test
+    @DisplayName("마지막 프레임이 끝나지 않았을 때 점수를 구하면 -1을 반환한다.")
+    void 마지막프레임_끝나지않을때_점수구하기2(){
+        Frame finalFrame = new FinalFrame(10);
+
+        finalFrame.pitch(new Pins(4));
+
+        assertThat(finalFrame.score()).isEqualTo(-1);
+    }
+
+
+    @Test
+    @DisplayName("마지막 프레임에서 스트라이크면 한 번을 더 투구할 수 있다.")
+    void 마지막_프레임_투구_한번더_스트라이크(){
+        Frame finalFrame = new FinalFrame(10);
+        finalFrame.pitch(new Pins(10));
+
+        finalFrame.pitch(new Pins(4));
+    }
+
+    @Test
+    @DisplayName("마지막 프레임에서 스트라이크시 두번더 투구하면 예외가 생긴다.")
+    void 마지막_프레임_투구_두번더_스트라이크(){
+        Frame finalFrame = new FinalFrame(10);
+        finalFrame.pitch(new Pins(10));
+
+        finalFrame.pitch(new Pins(1));
+
+        assertThatThrownBy(()->{
+            finalFrame.pitch(new Pins(2));
+        }).isInstanceOf(CannotPitchException.class);
+    }
+
+    @Test
+    @DisplayName("마지막 프레임에서 스페어시 한 번을 더 투구할 수 있다.")
+    void 마지막_프레임_투구_한번더_스페어(){
+        Frame finalFrame = new FinalFrame(10);
+
+        finalFrame.pitch(new Pins(4));
+        finalFrame.pitch(new Pins(6));
+
+        finalFrame.pitch(new Pins(5));
+    }
+
+    @ParameterizedTest
+    @DisplayName("마지막 프레임에서 스페어시 두번더 투구하면 예외가 생긴다.")
+    @CsvSource(value = {
+            "0, 10",
+            "1, 9",
+            "2, 8",
+            "3, 7",
+            "4, 6",
+            "5, 5"
+    })
+    void 마지막_프레임_투구_두번더_스페어(int first, int second){
+        Frame finalFrame = new FinalFrame(10);
+
+        finalFrame.pitch(new Pins(first));
+        finalFrame.pitch(new Pins(second));
+
+        finalFrame.pitch(new Pins(1));
+        assertThatThrownBy(()->{
+            finalFrame.pitch(new Pins(2));
+        }).isInstanceOf(CannotPitchException.class);
+    }
+
+
+    @Test
+    @DisplayName("마지막 프레임에서 Miss일때 한 번더 투구하면 예외를 던진다.")
+    void 마지막_프레임_투구_예외_miss(){
+        Frame finalFrame = new FinalFrame(10);
+
+        finalFrame.pitch(new Pins(5));
+        finalFrame.pitch(new Pins(0));
+
+        assertThatThrownBy(()->{
+            finalFrame.pitch(new Pins(5));
+        }).isInstanceOf(CannotPitchException.class);
+    }
+
+    @Test
+    @DisplayName("마지막 프레임에서 gutter 일때 한 번더 투구하면 예외를 던진다.")
+    void 마지막_프레임_투구_예외_gutter(){
+        Frame finalFrame = new FinalFrame(10);
+
+        finalFrame.pitch(new Pins(0));
+        finalFrame.pitch(new Pins(0));
+
+        assertThatThrownBy(()->{
+            finalFrame.pitch(new Pins(5));
+        }).isInstanceOf(CannotPitchException.class);
+    }
+}
