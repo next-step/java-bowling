862091ce3f33eed2314cef9783ee50ba3621cfd1
chkim <cndghks0213@gmail.com>
2022-05-26 22:04:24 +0900

    feat: 두 번의 기회에 모든 공을 쓰러뜨린 상태인 Spare 구현

---
+++ src/main/java/bowling/domain/state/Spare.java
@@ -0,0 +1,72 @@
+package bowling.domain.state;
+
+import bowling.domain.Pins;
+import bowling.domain.Score;
+
+import java.util.Objects;
+
+public class Spare extends Finished {
+
+    private static final String SCORE_FORMAT = "  %s|/ |";
+
+    private final Pins firstPins;
+    private final Pins secondPins;
+
+    public Spare(Pins firstPins, Pins secondPins) {
+        validatePins(firstPins, secondPins);
+        this.firstPins = firstPins;
+        this.secondPins = secondPins;
+    }
+
+    private void validatePins(Pins firstPins, Pins secondPins) {
+        if (!firstPins.isSpare(secondPins)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public boolean canCalculate(Score beforeScore) {
+        beforeScore.bowl(firstPins);
+        if (beforeScore.canCalculate()) {
+            return true;
+        }
+
+        beforeScore.bowl(secondPins);
+        if (beforeScore.canCalculate()) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Score sumBeforeScore(Score beforeScore) {
+        beforeScore.bowl(firstPins);
+        if (beforeScore.canCalculate()) {
+            return beforeScore;
+        }
+        return beforeScore.bowl(secondPins);
+    }
+
+    @Override
+    public Score score() {
+        return new Score(firstPins.totalScore(secondPins), 1);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(SCORE_FORMAT, firstPins);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Spare spare = (Spare) o;
+        return Objects.equals(firstPins, spare.firstPins) && Objects.equals(secondPins, spare.secondPins);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstPins, secondPins);
+    }
+}
