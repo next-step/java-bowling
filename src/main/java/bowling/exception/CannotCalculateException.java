822809640071c949cb3bcba554387e2cf6a31fea
dmswjd4117 <62321968+dmswjd4117@users.noreply.github.com>
2022-05-27 11:22:39 +0900

    feat: 예외 생성

---
+++ src/main/java/bowling/exception/CannotCalculateException.java
@@ -0,0 +1,8 @@
+package bowling.exception;
+
+public class CannotCalculateException extends RuntimeException{
+    private static final String CANNOT_CALCULATE_EXCEPTION = "점수를 생성할 수 없습니다.";
+    public CannotCalculateException(){
+        super(CANNOT_CALCULATE_EXCEPTION);
+    }
+}
---
+++ src/main/java/bowling/exception/CannotPitchException.java
@@ -0,0 +1,8 @@
+package bowling.exception;
+
+public class CannotPitchException extends RuntimeException{
+    private static final String CANNOT_PITCH_EXCEPTION = "더이상 투구를 할 수 없는 상태입니다.";
+    public CannotPitchException(){
+        super(CANNOT_PITCH_EXCEPTION);
+    }
+}
