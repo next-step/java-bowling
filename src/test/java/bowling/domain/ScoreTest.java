5ba79b3038621da2261dc2eeebb846a98d2df1e2
soon <soon_myeong@naver.com>
2022-05-28 14:02:57 +0900

    refactor. score Test 추가

---
+++ src/test/java/bowling/domain/ScoreTest.java
@@ -0,0 +1,14 @@
+package bowling.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class ScoreTest {
+    @Test
+    void bowl_테스트() {
+        assertThat(new Score(5,2).bowl(3).getScore()).isEqualTo(8);
+    }
+}
