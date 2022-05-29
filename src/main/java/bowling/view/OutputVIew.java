bc17f1e05de6d5c0ece7041ebe99626da14e55e2
Seongheon Sim <river.water.sea@gmail.com>
2022-05-30 01:26:54 +0900

    fix: OutputView 클래스 매직넘버 수정

--- src/main/java/bowling/view/OutputVIew.java
+++ src/main/java/bowling/view/OutputVIew.java
@@ -12,10 +12,12 @@ public class OutputVIew {
 
     private static final String FIRST_COLUMN = "| %7s |";
     private static final String OTHER_COLUMN = " %7s |";
-    private static final String BLANK = "";
-    private static final String SPACE_BLANK = " ";
-    private static final String ZERO_STRING = "0";
     private static final String NAME = "NAME";
+    private static final String BLANK = "";
+    private static final int ONE = 1;
+    private static final int START_ROUND = 0;
+    private static final int LAST_ROUND = 9;
+    private static final int LIMITED_ROUND = 10;
 
     private OutputVIew() {
     }
@@ -46,17 +48,17 @@ public class OutputVIew {
     private static void printIndex() {
         AtomicReference<String> message = new AtomicReference<>(String.format(FIRST_COLUMN, NAME));
         IntFunction<String> indexFunc = i -> {
-            int index = i + 1;
-            if (i == 9) {
+            int index = i + ONE;
+            if (i == LAST_ROUND) {
                 return String.valueOf(index);
             }
-            return ZERO_STRING + index;
+            return "" + START_ROUND + index;
         };
         System.out.println(createTable(message, indexFunc));
     }
 
     private static String createTable(AtomicReference<String> message, IntFunction<String> func) {
-        IntStream.range(0, 10)
+        IntStream.range(START_ROUND, LIMITED_ROUND)
                 .mapToObj(func)
                 .forEach(i -> message.getAndAccumulate(String.format(OTHER_COLUMN, i), (s, s2) -> s + s2));
 
