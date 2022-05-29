9c5e96008bf24610aafd5df9ff7c5d823e2ab3f4
chkim <cndghks0213@gmail.com>
2022-05-29 16:47:34 +0900

    feat: equals, hashCode 재정의

--- src/main/java/bowling/domain/User.java
+++ src/main/java/bowling/domain/User.java
@@ -1,5 +1,7 @@
 package bowling.domain;
 
+import java.util.Objects;
+
 public class User {
     private static final String NAME_FORMAT = "|  %s |";
     private static final int NAME_LENGTH = 3;
@@ -19,5 +21,22 @@ public class User {
 
     public String mark() {
         return String.format(NAME_FORMAT, name);
+    }
+
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
