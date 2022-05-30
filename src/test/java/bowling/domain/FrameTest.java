1cc50b75848a50aa05ca1c67ebe8129a961daf0a
hjeom <nara1030@naver.com>
2022-05-31 01:37:59 +0900

    test: 클래스명 수정

--- src/test/java/bowling/domain/FrameTest.java
+++ src/test/java/bowling/domain/FrameTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class Frame1Test {
+class FrameTest {
     private Frame frame;
 
     @BeforeEach
