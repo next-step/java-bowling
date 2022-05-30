da4e48d43f86c8aaa56995cbe3a2683060bf2d1a
younlll <youn.lee96@gmail.com>
2022-05-30 23:52:46 +0900

    feat: 참가자 수 입력 구현 및 int 변환 메서드 추가

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -4,10 +4,16 @@ import bowling.domain.Pitching;
 import java.util.Scanner;
 
 public class InputView {
+    private static final String PLAYER_NUMBER_PROMPT = "How many people? ";
     private static final String PLAYER_NAME_PROMPT = "플레이어 이름은(3 english letters)?: ";
     private static final String INPUT_FALLEN_PINS_MESSAGE = "%d 프레임 투구 : ";
 
     private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static int inputPlayerNumber() {
+        System.out.println(PLAYER_NUMBER_PROMPT);
+        return toInt(SCANNER.nextLine());
+    }
 
     public static String inputPlayerName() {
         System.out.println(PLAYER_NAME_PROMPT);
@@ -16,6 +22,10 @@ public class InputView {
 
     public static Pitching inputFallenPins(int currentFrameNumber) {
         System.out.printf(INPUT_FALLEN_PINS_MESSAGE, currentFrameNumber);
-        return new Pitching(Integer.parseInt(SCANNER.nextLine()));
+        return new Pitching(toInt(SCANNER.nextLine()));
+    }
+
+    private static int toInt(String value) {
+        return Integer.parseInt(value);
     }
 }
