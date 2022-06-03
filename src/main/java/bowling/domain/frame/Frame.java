5f3f2d7353c2f5b87c9d345ed6429350017d99a4
wu2ee <gulb1602@gmail.com>
2022-06-03 23:33:32 +0900

    feat : Frame 인터페이스 점수 및 프레임 넘버 메서드 정의 추가

--- src/main/java/bowling/domain/frame/Frame.java
+++ src/main/java/bowling/domain/frame/Frame.java
@@ -1,6 +1,7 @@
 package bowling.domain.frame;
 
 import bowling.domain.Pins;
+import bowling.domain.Score;
 
 public interface Frame {
     Frame bowl(Pins hitPins);
@@ -9,5 +10,11 @@ public interface Frame {
 
     boolean isFinalFrame();
 
+    FrameNumber frameNumber();
+
     String symbol();
+
+    Score score();
+
+    Score calculateAdditionalScore(Score previousScore);
 }
