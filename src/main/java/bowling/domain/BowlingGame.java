a6c962a951b57c69fc43e2fe5f48f1b49c3a88a6
wlroh <wlroh.paul@gmail.com>
2022-06-05 02:57:08 +0900

    feat: 볼링 게임 객체 구현

---
+++ src/main/java/bowling/domain/BowlingGame.java
@@ -0,0 +1,34 @@
+package bowling.domain;
+
+import bowling.domain.frame.Frame;
+
+public class BowlingGame {
+
+    private final Player player;
+    private final Frames frames;
+
+    public BowlingGame(Player player, Frames frames) {
+        this.player = player;
+        this.frames = Frames.initialize();
+    }
+
+    public void bowling(int hit) {
+        frames.bowling(hit);
+    }
+
+    public Frame currentFrame() {
+        return frames.currentFrame();
+    }
+
+    public boolean isFinish() {
+        return frames.isFinish();
+    }
+
+    public Player player() {
+        return player;
+    }
+
+    public Frames frames() {
+        return frames;
+    }
+}
