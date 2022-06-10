87e1695f4438c28903074159a03f8eb08684c086
seo <youngsuseo@naver.com>
2022-06-11 00:49:38 +0900

    FEAT (bowling) : Develop bowling game (calculate score)
    * Score : 점수 계산 클래스 추가

--- src/main/java/bowling/domain/Score.java
+++ src/main/java/bowling/domain/Score.java
@@ -1,34 +1,58 @@
 package bowling.domain;
 
-public enum Score {
-    STRIKE("X"),
-    SPARE("/");
+import bowling.exception.CannotCalculateException;
 
-    private String symbol;
+import java.util.Objects;
 
-    private static final int STRIKE_SCORE = 10;
+public class Score {
+    private static final int NO_MORE_CALCULATION = 0;
+    private static final int DEFAULT_VALUE = 0;
 
-    Score(String symbol) {
-        this.symbol = symbol;
+    private int score;
+    private int left;
+
+    Score() {
+        this(DEFAULT_VALUE, DEFAULT_VALUE);
     }
 
-    public static String value(int score, boolean spare) {
-        if (score == STRIKE_SCORE) {
-            return STRIKE.symbol;
+    Score(int score, int left) {
+        this.score = score;
+        this.left = left;
+    }
+
+    public Score bowl(int countOfPins) {
+        return new Score(score += countOfPins, left - 1);
+    }
+
+    int getScore() {
+        if (!canCalculateScore()) {
+            throw new CannotCalculateException("점수를 계산 할 수 없습니다.");
         }
+        return this.score;
+    }
 
-        if (spare) {
-            return SPARE.symbol;
+    boolean canCalculateScore() {
+        return left == NO_MORE_CALCULATION;
+    }
+
+    int getLeft() {
+        return left;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
         }
-
-        return String.valueOf(score);
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Score score1 = (Score) o;
+        return score == score1.score && left == score1.left;
     }
 
-    public static boolean strike(String symbol) {
-        return STRIKE.symbol.equals(symbol);
-    }
-
-    public static boolean spare(String symbol) {
-        return SPARE.symbol.equals(symbol);
+    @Override
+    public int hashCode() {
+        return Objects.hash(score, left);
     }
 }
