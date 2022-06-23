f90c6211c8658b2cdfb955b3e95cd8b1eaa81537
guswns1659 <guswns1659@gmail.com>
2022-06-24 07:15:50 +0900

    step2 refactor: 점수판에 출력되는 문자 포맷을 만드는 객체를 OutPut에서 분리

---
+++ src/main/java/bowling/presentation/Format.java
@@ -0,0 +1,78 @@
+package bowling.presentation;
+
+import bowling.domain.frame.FallenPins;
+
+import static bowling.presentation.ScoreFormat.*;
+import static java.math.BigInteger.TEN;
+
+// board에 출력되는 format을 만드는 객체
+public class Format {
+    public static String fallenPinsFormat(FallenPins fallenPins) {
+        if (fallenPins.size() == 1) {
+            return singlePinInBoard(fallenPins);
+        }
+        if (fallenPins.size() == 2) {
+            return doublePinInBoard(fallenPins);
+        }
+        if (fallenPins.size() == 3) {
+            return triplePinInBoard(fallenPins);
+        }
+        return String.format(centerAlignFormat(6), "");
+    }
+
+    private static String singlePinInBoard(FallenPins fallenPins) {
+        return String.format(centerAlignFormat(3), singlePinFormat(fallenPins.first()));
+    }
+
+    private static String singlePinFormat(int score) {
+        return TEN.intValue() == score ? STRIKE.format() : String.valueOf(score);
+    }
+
+    private static String doublePinInBoard(FallenPins fallenPins) {
+        return String.format(centerAlignFormat(4), doublePinFormat(fallenPins.first(), fallenPins.second()));
+    }
+
+    private static String doublePinFormat(int first, int second) {
+        if (first + second == TEN.intValue()) {
+            return first + DELIMITER.format() + SPARE.format();
+        }
+        if (second == 0) {
+            return first + DELIMITER.format() + GUTTER.format();
+        }
+        return singlePinFormat(first) + DELIMITER.format() + singlePinFormat(second);
+    }
+
+    private static String triplePinInBoard(FallenPins fallenPins) {
+        return String.format(centerAlignFormat(5), triplePinFormat(fallenPins.first(), fallenPins.second(), fallenPins.third()));
+    }
+
+    private static String triplePinFormat(int first, int second, int third) {
+        if (first == TEN.intValue() && second == TEN.intValue()) {
+            return STRIKE.format() + DELIMITER.format() + STRIKE.format() + DELIMITER.format() + singlePinFormat(third);
+        }
+        if (first == TEN.intValue()) {
+            return STRIKE.format() + DELIMITER.format() + doublePinFormat(second, third);
+        }
+        return doublePinFormat(first, second) + DELIMITER.format() + singlePinFormat(third);
+    }
+
+    public static String nameProperty() {
+        return DELIMITER.format() + String.format(centerAlignFormat(5), "NAME") + DELIMITER.format();
+    }
+
+    public static String frameIndex(int index) {
+        return String.format(centerAlignFormat(4), String.format("%02d", index)) + DELIMITER.format();
+    }
+
+    public static String playerName(String name) {
+        return DELIMITER.format() + String.format(centerAlignFormat(4), name) + DELIMITER.format();
+    }
+
+    public static String centerAlignFormat(int size) {
+        int MAX_WIDTH = 6;
+        if (size > MAX_WIDTH) {
+            throw new IllegalArgumentException(String.format("size는 %d보다 작아야 합니다. - %d", MAX_WIDTH, size));
+        }
+        return "%" + size + "s" + " ".repeat(MAX_WIDTH - size);
+    }
+}
--- src/main/java/bowling/presentation/OutPut.java
+++ src/main/java/bowling/presentation/OutPut.java
@@ -2,71 +2,18 @@ package bowling.presentation;
 
 import bowling.domain.Board;
 import bowling.domain.Player;
-import bowling.domain.frame.Frame;
-import bowling.domain.frame.FallenPins;
 
-import static bowling.presentation.ScoreFormat.*;
+import static bowling.presentation.ScoreFormat.DELIMITER;
 
 public class OutPut {
-    private static final int TEN = 10;
-
     public static void board(Board board, Player player) {
-        System.out.printf(DELIMITER.format() + centerAlignFormat(5), "NAME");
+        System.out.print(Format.nameProperty());
         board.frames()
-                .forEach(frame -> {
-                    String number = String.format("%02d", frame.index());
-                    System.out.printf(DELIMITER.format() + centerAlignFormat(4), number);
-                });
-        System.out.print("|\n");
-        System.out.printf(DELIMITER.format() + centerAlignFormat(4), player.name());
+                .forEach(frame -> System.out.print(Format.frameIndex(frame.index())));
+        System.out.print("\n");
+        System.out.print(Format.playerName(player.name()));
         board.frames()
-                .forEach(frame -> System.out.printf("|%s", getScoreFormat(frame.pins())));
-        System.out.println("|\n");
-    }
-
-    private static String centerAlignFormat(int size) {
-        int MAX = 6;
-        if (size > MAX) {
-            throw new IllegalArgumentException(String.format("size는 %d보다 작아야 합니다. - %d", MAX, size));
-        }
-        return "%" + size + "s" + " ".repeat(MAX - size);
-    }
-
-    private static String getScoreFormat(FallenPins fallenPins) {
-        if (fallenPins.size() == 1) {
-            return String.format(centerAlignFormat(3), getSingleScoreFormat(fallenPins.first()));
-        }
-        if (fallenPins.size() == 2) {
-            return String.format(centerAlignFormat(4), getDoubleScoreFormat(fallenPins.first(), fallenPins.second()));
-        }
-        if (fallenPins.size() == 3) {
-            if (fallenPins.first() == TEN) {
-                return String.format("%2s|%3s", getSingleScoreFormat(fallenPins.first()), getDoubleScoreFormat(fallenPins.second(), fallenPins.third()));
-            }
-            if (fallenPins.first() == TEN && fallenPins.second() == TEN) {
-                return String.format("%2s|%1s|%1s", getSingleScoreFormat(fallenPins.first()), getSingleScoreFormat(fallenPins.second()), getSingleScoreFormat(fallenPins.third()));
-            }
-            return String.format("%4s|%1s", getDoubleScoreFormat(fallenPins.first(), fallenPins.second()), getSingleScoreFormat(fallenPins.third()));
-        }
-        return String.format(centerAlignFormat(6), "");
-    }
-
-    private static String getDoubleScoreFormat(int first, int second) {
-        if (first + second == TEN) {
-            return first + DELIMITER.format() + SPARE.format();
-        }
-        if (second == 0) {
-            return first + DELIMITER.format() + GUTTER.format();
-        }
-        return getSingleScoreFormat(first) + DELIMITER.format() + getSingleScoreFormat(second);
-    }
-
-    private static String getSingleScoreFormat(int score) {
-        return score == TEN ? STRIKE.format() : String.valueOf(score);
-    }
-
-    public static void score(Frame frame, int score) {
-//        System.out.printf("%s프레임 투구 : %d", frame.index(), score);
-//        System.out.println();
+                .forEach(frame -> System.out.printf(Format.fallenPinsFormat(frame.pins()) + DELIMITER.format()));
+        System.out.println("\n");
     }
 }
