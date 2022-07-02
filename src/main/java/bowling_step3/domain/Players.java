54694f20799d7cff9e7abf458f48c7c7c5024c28
devgony <devgony@gmail.com>
2022-07-03 02:06:20 +0900

    refactor: move plays from Players to App contorller

--- src/main/java/bowling_step3/domain/Players.java
+++ src/main/java/bowling_step3/domain/Players.java
@@ -1,12 +1,6 @@
 package bowling_step3.domain;
 
-import bowling_step3.domain.state.GameOver;
-import bowling_step3.domain.state.Ready;
-import bowling_step3.view.Output;
-
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.*;
 
 public class Players {
     private LinkedList<Player> players;
@@ -19,27 +13,7 @@ public class Players {
         this.players.add(player);
     }
 
-    public void iteratePlays(Integer round) {
-        this.players.stream().forEach(player -> plays(players, round, player));
-    }
-
-    private static void plays(LinkedList<Player> players, Integer i, Player player) {
-        Frames frames = player.frames();
-        Frame frame = frames.current();
-        play(players, i, player, frames, frame);
-    }
-
-    private static void play(LinkedList<Player> players, int round, Player player, Frames frames, Frame frame) {
-        int randomPin = frame.scores().getRandom();
-        frame = frame.play(randomPin);
-        Map<Player, Subtotals> playerSubtotals = new HashMap<>();
-        players.stream().forEach(p -> playerSubtotals.put(p, p.frames().first().createSubtotals()));
-        Output.printPlayerResult(round, frame, player);
-        Output.printPlayersFrames(round, players, playerSubtotals);
-        if (frame.status() instanceof Ready || frame.status() instanceof GameOver) {
-            frames.renewCurrentIndex();
-            return;
-        }
-        play(players, round, player, frames, frame);
+    public LinkedList<Player> players() {
+        return this.players;
     }
 }
