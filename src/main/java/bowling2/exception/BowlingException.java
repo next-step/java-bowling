2f2e3411d7d7369dc50ec86d590c2ccc37cade0f
guswns1659 <guswns1659@gmail.com>
2022-07-28 06:50:46 +0900

    step3 feat: 커스텀 exception, BowlingException 추가

--- src/main/java/bowling2/exception/BowlingException.java
+++ src/main/java/bowling2/exception/BowlingException.java
@@ -2,4 +2,11 @@ package bowling2.exception;
 
 // TODO(jack.comeback) : code 추가
 public class BowlingException extends RuntimeException {
+
+    public BowlingException() {
+    }
+
+    public BowlingException(String message) {
+        super(message);
+    }
 }
