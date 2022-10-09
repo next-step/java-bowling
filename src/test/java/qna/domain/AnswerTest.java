c622327781cfd418b503415b409db23c5e3e8168
hiblue02 <hiblue02@gmail.com>
2022-10-09 11:05:39 +0900

    refactor(AnswerTest, QuestionTest): setup 삭제, 정상 케이스 테스트를 삭제 후 상태 검증으로 변경

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,33 +1,35 @@
 package qna.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import qna.domain.entity.Answer;
 import qna.domain.entity.Question;
 import qna.exception.CannotDeleteException;
 
-import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class AnswerTest {
-    private Answer answer;
+class AnswerTest {
 
-    @BeforeEach
-    void setUp(){
-        Question question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
-        answer =  new Answer(UserTest.JAVAJIGI, question, "Answers Contents1");
-    }
     @Test
     @DisplayName("작성자와 삭제할 사람이 일치하면 삭제")
     void delete_when_writer_equal_delete_user() {
-        assertThatNoException()
-                .isThrownBy(() -> answer.delete(UserTest.JAVAJIGI));
+        //given
+        Question question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        Answer answer = new Answer(UserTest.JAVAJIGI, question, "Answers Contents1");
+        //when
+        answer.delete(UserTest.JAVAJIGI);
+        //then
+        assertThat(answer.isDeleted()).isTrue();
     }
 
     @Test
     @DisplayName("작성자와 삭제할 사람이 일치하지 않으면 CannotDeleteException")
     void delete_when_writer_differ_delete_user() {
+        //given
+        Question question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        Answer answer = new Answer(UserTest.JAVAJIGI, question, "Answers Contents1");
+        //then
         assertThatThrownBy(() -> answer.delete(UserTest.SANJIGI))
                 .isInstanceOf(CannotDeleteException.class);
     }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,34 +1,33 @@
 package qna.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import qna.domain.entity.Answer;
 import qna.domain.entity.Question;
 import qna.exception.CannotDeleteException;
 
-import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class QuestionTest {
-
-    private Question question;
-
-    @BeforeEach
-    void setUp() {
-        question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
-    }
+class QuestionTest {
 
     @Test
     @DisplayName("작성자와 로그인 유저가 일치하고, 답변이 없으면 삭제")
     void writer_equal_delete_user_and_no_answers() {
-        assertThatNoException()
-                .isThrownBy(() -> question.delete(UserTest.JAVAJIGI));
+        //given
+        Question question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        //when
+        question.delete(UserTest.JAVAJIGI);
+        //then
+        assertThat(question.isDeleted()).isTrue();
     }
 
     @Test
     @DisplayName("작성자와 로그인 유저가 다르면 CannotDeleteException")
     void writer_differ_delete_user() {
+        //given
+        Question question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        //then
         assertThatThrownBy(() -> question.delete(UserTest.SANJIGI))
                 .isInstanceOf(CannotDeleteException.class);
     }
@@ -37,6 +36,7 @@ public class QuestionTest {
     @DisplayName("작성자와 로그인 유저는 같고, 답변자는 다르면 CannotDeleteException")
     void writer_equal_delete_user_and_differ_answer_writer() {
         //given
+        Question question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
         Answer answer1 = new Answer(11l, UserTest.SANJIGI, question, "answer1");
         question.addAnswer(answer1);
         //then
@@ -48,10 +48,13 @@ public class QuestionTest {
     @DisplayName("작성자와 로그인 유저는 같고, 답변자도 같으면 삭제")
     void writer_equal_delete_user_and_equal_answer_writer() {
         //given
+        Question question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
         Answer answer1 = new Answer(11l, UserTest.JAVAJIGI, question, "answer1");
         question.addAnswer(answer1);
+        //when
+        question.delete(UserTest.JAVAJIGI);
         //then
-        assertThatNoException().isThrownBy(() -> question.delete(UserTest.JAVAJIGI));
+        assertThat(question.isDeleted()).isTrue();
     }
 
 
