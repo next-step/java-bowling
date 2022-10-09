345e354ea77aa377f2f71039acf9ce7b0a4fc342
JunH <tjdtls690@gmail.com>
2022-10-09 11:54:21 +0900

    :white_check_mark: test : 노멀 프레임 테스트 추가

--- src/test/java/bowling/step2/domain/frame/NormalFrameTest.java
+++ src/test/java/bowling/step2/domain/frame/NormalFrameTest.java
@@ -10,7 +10,27 @@ public class NormalFrameTest {
     
     @Test
     @DisplayName("다음 프레임으로 넘어가기 - strike")
-    void next_frame() {
+    void next_frame_strike() {
         assertThat(NORMAL_FRAME.bowl(10)).isNotEqualTo(NORMAL_FRAME);
+    }
+    
+    @Test
+    @DisplayName("다음 프레임으로 넘어가기 - spare")
+    void next_frame_spare() {
+        final Frame frame = NORMAL_FRAME.bowl(5);
+        assertThat(frame.bowl(5)).isNotEqualTo(NORMAL_FRAME);
+    }
+    
+    @Test
+    @DisplayName("다음 프레임으로 넘어가기 - miss")
+    void next_frame_miss() {
+        final Frame frame = NORMAL_FRAME.bowl(5);
+        assertThat(frame.bowl(4)).isNotEqualTo(NORMAL_FRAME);
+    }
+    
+    @Test
+    @DisplayName("한 번 더 투구하기 - normal")
+    void next_frame_normal() {
+        assertThat(NORMAL_FRAME.bowl(4)).isEqualTo(NORMAL_FRAME);
     }
 }
