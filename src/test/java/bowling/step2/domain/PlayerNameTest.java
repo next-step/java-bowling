6b669de911aee6e87700c6afa5d43eccc706e437
JunH <tjdtls690@gmail.com>
2022-10-09 13:43:55 +0900

    :white_check_mark: test : 숫자 입력 시 예외 던지기 테스트 완료

--- src/test/java/bowling/step2/domain/PlayerNameTest.java
+++ src/test/java/bowling/step2/domain/PlayerNameTest.java
@@ -8,12 +8,22 @@ import org.junit.jupiter.params.provider.EmptySource;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class PlayerNameTest {
+    private static final String PLAYER_NAME_FORMAT_EXCEPTION_MESSAGE = "플레이어 이름 형식이 잘못되었습니다. 다시 입력해주세요.";
+    
     @ParameterizedTest(name = "{displayName}")
     @DisplayName("\"\" 입력 시 예외")
     @EmptySource
     void input_empty_exception(String input) {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new PlayerName(input))
-                .withMessage("올바른 이름이 아닙니다. 다시 입력해주세요");
+                .withMessage(PLAYER_NAME_FORMAT_EXCEPTION_MESSAGE);
+    }
+    
+    @Test
+    @DisplayName("숫자 입력 시 예외")
+    void input_number_exception() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new PlayerName("A2C"))
+                .withMessage(PLAYER_NAME_FORMAT_EXCEPTION_MESSAGE);
     }
 }
