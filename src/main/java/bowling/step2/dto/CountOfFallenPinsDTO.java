81ed8560e64c03edd41f6535aa56c90cbf04dc68
JunH <tjdtls690@gmail.com>
2022-10-09 14:07:52 +0900

    :art: refactor : 투구 결과 입력 폼 상수화

--- src/main/java/bowling/step2/dto/CountOfFallenPinsDTO.java
+++ src/main/java/bowling/step2/dto/CountOfFallenPinsDTO.java
@@ -6,6 +6,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CountOfFallenPinsDTO {
+    private static final String COUNT_OF_FALLEN_PINS_INPUT_FORMAT = "[1-9]|10";
+    
     private final int countOfFallenPins;
     
     public CountOfFallenPinsDTO(final String countOfFallenPins) {
@@ -13,10 +15,11 @@ public class CountOfFallenPinsDTO {
     }
     
     private String checkCountOfFallenPinsFormat(final String countOfFallenPins) {
-        final Matcher matcher = Pattern.compile("[1-9]|10").matcher(countOfFallenPins);
+        final Matcher matcher = Pattern.compile(COUNT_OF_FALLEN_PINS_INPUT_FORMAT).matcher(countOfFallenPins);
         if (!matcher.matches()) {
             throw new CountOfFallenPinsException();
         }
-        return null;
+        
+        return countOfFallenPins;
     }
 }
