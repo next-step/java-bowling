3e55f3805ec22ba6c8f8de61b82a349c92bbc7cc
JunH <tjdtls690@gmail.com>
2022-10-09 13:46:21 +0900

    :white_check_mark: test : 올바른 이름 입력 시 반환 받기 테스트 완료

--- src/test/java/bowling/step2/domain/PlayerNameTest.java
+++ src/test/java/bowling/step2/domain/PlayerNameTest.java
@@ -5,10 +5,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class PlayerNameTest {
     private static final String PLAYER_NAME_FORMAT_EXCEPTION_MESSAGE = "플레이어 이름 형식이 잘못되었습니다. 다시 입력해주세요.";
+    public static final PlayerName PLAYER_NAME = new PlayerName("ABC");
+    
+    @Test
+    @DisplayName("플레이어 이름 반환 받기")
+    void input_player_name() {
+        assertThat(PLAYER_NAME).isNotNull();
+    }
     
     @ParameterizedTest(name = "{displayName}")
     @DisplayName("\"\" 입력 시 예외")
