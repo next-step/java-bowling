6ce31aa3f4db50083c3a22857f824b2b4a0f353b
OhSeungCheol <sc.oh131@gmail.com>
2022-10-13 17:31:00 +0900

    [M1] test: 질문 삭제 테스트 케이스 추가
    참고: https://edu.nextstep.camp/s/DVVUSLFe/ls/AbJswgks

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,7 @@
 package qna.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -14,40 +15,61 @@ public class QuestionTest {
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
 
     @Test
-    @Order(1)
     @DisplayName("작성자 본인이 질문을 삭제 상태로 변경할 수 있는지 테스트")
     public void setDeletedTest1(){
-        assertDoesNotThrow(() -> Q1.setDeleted(UserTest.JAVAJIGI));
+        Question tempQ1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+
+        assertDoesNotThrow(() -> tempQ1.setDeleted(UserTest.JAVAJIGI));
     }
 
     @Test
-    @Order(2)
     @DisplayName("작성자가 아닌 유저가 질문을 삭제할 경우 예외가 발생하는지 테스트")
     public void setDeletedTest2(){
-        assertThrows(CannotDeleteException.class, () -> Q1.setDeleted(UserTest.SANJIGI));
+        Question tempQ1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+
+        assertThrows(CannotDeleteException.class, () -> tempQ1.setDeleted(UserTest.SANJIGI));
     }
 
     @Test
-    @Order(3)
     @DisplayName("질문과 답변글의 작성자가 동일할 때 작성자 본인이 질문을 삭제할 수 있는지 테스트")
     public void setDeletedTest3(){
-        Q1.addAnswer(AnswerTest.A1);
-        assertDoesNotThrow(() -> Q1.setDeleted(UserTest.JAVAJIGI));
+        Question tempQ1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        Answer tempA1 = new Answer(UserTest.JAVAJIGI, tempQ1, "Answers Contents1");
+        tempQ1.addAnswer(tempA1);
+
+        assertDoesNotThrow(() -> tempQ1.setDeleted(UserTest.JAVAJIGI));
     }
 
     @Test
-    @Order(4)
     @DisplayName("질문의 작성자와 다른 유저가 답변글을 달은 질문을 작성자 본인이 삭제할 때 예외가 발생하는지 테스트")
     public void setDeletedTest4(){
-        Q1.addAnswer(AnswerTest.A2);
-        assertThrows(CannotDeleteException.class, () -> Q1.setDeleted(UserTest.JAVAJIGI));
+        Question tempQ1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        Answer tempA2 = new Answer(UserTest.SANJIGI, tempQ1, "Answers Contents2");
+        tempQ1.addAnswer(tempA2);
+
+        assertThrows(CannotDeleteException.class, () -> tempQ1.setDeleted(UserTest.JAVAJIGI));
+    }
+
+    @Test
+    @DisplayName("질문을 삭제했을 때 질문과 연관 답글의 상태가 삭제 상태인지 테스트")
+    public void setDeletedTest5() throws CannotDeleteException {
+        Question tempQ2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+        Answer tempA2 = new Answer(UserTest.SANJIGI, tempQ2, "Answers Contents2");
+        tempQ2.addAnswer(tempA2);
+        tempQ2.setDeleted(UserTest.SANJIGI);
+
+        assertThat(tempQ2.isDeleted()).isTrue();
+        tempQ2.getAnswers().getAnswers().forEach(answer->assertThat(answer.isDeleted()).isTrue());
     }
 
     @Test
     @DisplayName("질문을 삭제했을 때 반환되는 히스토리의 개수가 질문 + 답글 개수와 일치하는지 테스트")
-    public void setDeletedTest5() throws CannotDeleteException {
-        Q2.addAnswer(AnswerTest.A2);
-        assertThat(Q2.setDeleted(UserTest.SANJIGI)).hasSize(2);
+    public void setDeletedTest6() throws CannotDeleteException {
+        Question tempQ2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+        Answer tempA2 = new Answer(UserTest.SANJIGI, tempQ2, "Answers Contents2");
+        tempQ2.addAnswer(tempA2);
+
+        assertThat(tempQ2.setDeleted(UserTest.SANJIGI)).hasSize(2);
     }
 
 }
