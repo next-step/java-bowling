d1545e607f3fbb3bad527981e864a5ad64ed36f9
JunH <tjdtls690@gmail.com>
2022-10-13 22:13:48 +0900

    :bug: fix : 10프레임 점수 계산 시 버그 발생한 부분 수정

--- src/main/java/bowling/step2/domain/Referee.java
+++ src/main/java/bowling/step2/domain/Referee.java
@@ -9,20 +9,38 @@ import java.util.stream.Collectors;
 public class Referee {
     private static final int STOP_SCORE = -1;
     private static final int MAX_PER_SCORE = 10;
+    private static final int COUNT_OF_MAX_PINS = 10;
     
     private int cumulativeScore;
     private boolean isCalculateStop;
     
     public String calculateCumulativeScore(final PlayerDTO playerDTO, final int index) {
         final List<List<Score>> allScores = getAllScores(playerDTO);
-        if (allScores.size() <= index) {
+        if (isExceedIndex(index, allScores)) {
             return String.valueOf(STOP_SCORE);
         }
         
         if (isFinalFrame(playerDTO, index)) {
-            return String.valueOf(cumulativeScore + getSumScores(allScores.get(index)));
+            return getFinalFrameScore(allScores, index);
         }
         return parseCumulativeScore(index, allScores);
+    }
+    
+    private String getFinalFrameScore(final List<List<Score>> allScores, final int index) {
+        final List<Score> scores = allScores.get(index);
+        if (isReadyContains(scores) || isNotMissContains(scores)) {
+            return String.valueOf(STOP_SCORE);
+        }
+        
+        return String.valueOf(cumulativeScore + getSumScores(scores, scores.size()));
+    }
+    
+    private boolean isNotMissContains(final List<Score> scores) {
+        return scores.size() == 1 || (scores.size() == 2 && getSumScores(scores, scores.size()) >= COUNT_OF_MAX_PINS);
+    }
+    
+    private boolean isExceedIndex(final int index, final List<List<Score>> allScores) {
+        return allScores.size() <= index;
     }
     
     private boolean isFinalFrame(final PlayerDTO playerDTO, final int index) {
@@ -55,28 +73,28 @@ public class Referee {
         if (isSpare(currentScores)) {
             return MAX_PER_SCORE + getOneNextScore(allScores, index + 1);
         }
-        return getSumScores(currentScores);
+        return getSumScores(currentScores, currentScores.size());
     }
     
     private List<Score> checkCalculateStop(final List<List<Score>> allScores, final int index) {
         final List<Score> currentScores = allScores.get(index);
-        if (isReadyContains(currentScores) || (isNotFirstFrame(index) && isPriorScoreStrike(allScores.get(index - 1)) && isNormal(currentScores))) {
+        if (isReadyContains(currentScores) || isNinthScoreStop(allScores, index, currentScores)) {
             isCalculateStop = true;
         }
         
         return currentScores;
     }
     
+    private boolean isNinthScoreStop(final List<List<Score>> allScores, final int index, final List<Score> currentScores) {
+        return isNotFirstFrame(index) && isStrike(allScores.get(index - 1)) && isNormal(currentScores);
+    }
+    
     private boolean isNotFirstFrame(final int index) {
         return index != 0;
     }
     
-    private boolean isPriorScoreStrike(final List<Score> scores) {
-        return scores.get(0).isStrike();
-    }
-    
     private boolean isNormal(final List<Score> scores) {
-        return scores.size() == 1 && !scores.get(0).isStrike();
+        return scores.size() == 1 && !isStrike(scores);
     }
     
     private boolean isReadyContains(final List<Score> nextScores) {
@@ -85,8 +103,8 @@ public class Referee {
                 .anyMatch(score -> score == STOP_SCORE);
     }
     
-    private boolean isStrike(final List<Score> currentScores) {
-        return currentScores.get(0).isStrike();
+    private boolean isStrike(final List<Score> scores) {
+        return scores.get(0).isStrike();
     }
     
     private boolean isSpare(final List<Score> currentScores) {
@@ -95,22 +113,22 @@ public class Referee {
     
     private int getTwoNextScore(final List<List<Score>> allScores, final int index) {
         final List<Score> nextScores = checkCalculateStop(allScores, index);
-        
-        if (nextScores.size() == 2) {
-            return getSumScores(nextScores);
+        if (nextScores.size() >= 2) {
+            return getSumScores(nextScores, 2);
         }
         
         return nextScores.get(0).getFallenPins() + getOneNextScore(allScores, index + 1);
     }
     
-    private int getSumScores(final List<Score> scores) {
+    private int getSumScores(final List<Score> scores, int limit) {
         return scores.stream()
                 .mapToInt(Score::getFallenPins)
+                .limit(limit)
                 .sum();
     }
     
     private int getOneNextScore(final List<List<Score>> allScores, final int index) {
-        if (allScores.size() <= index) {
+        if (isExceedIndex(index, allScores)) {
             return STOP_SCORE;
         }
         
--- src/test/java/bowling/step2/domain/RefereeTest.java
+++ src/test/java/bowling/step2/domain/RefereeTest.java
@@ -51,14 +51,69 @@ public class RefereeTest {
     }
     
     @Test
-    @DisplayName("누적 점수 구하기 - 마지막 프레임")
-    void stop_calculate_final_frame() {
+    @DisplayName("누적 점수 구하기 - 9 프레임 스트라이크")
+    void get_cumulative_score_ninth_frame_strike() {
+        IntStream.rangeClosed(1, 8).forEach(count -> player.bowl(new CountOfFallenPinsDTO("10")));
+        player.bowl(new CountOfFallenPinsDTO("10"));
+        player.bowl(new CountOfFallenPinsDTO("10"));
+        player.bowl(new CountOfFallenPinsDTO("10"));
+        player.bowl(new CountOfFallenPinsDTO("10"));
+        final String cumulativeScore = referee.calculateCumulativeScore(playerDTO, 8);
+        assertThat(cumulativeScore).isEqualTo("30");
+    }
+    
+    @Test
+    @DisplayName("누적 점수 구하기 마지막 프레임 - 스트라이크 3번")
+    void get_cumulative_score_final_frame_three_strike() {
+        IntStream.rangeClosed(1, 9).forEach(count -> player.bowl(new CountOfFallenPinsDTO("10")));
+        player.bowl(new CountOfFallenPinsDTO("10"));
+        player.bowl(new CountOfFallenPinsDTO("10"));
+        player.bowl(new CountOfFallenPinsDTO("10"));
+        final String cumulativeScore = referee.calculateCumulativeScore(playerDTO, 9);
+        assertThat(cumulativeScore).isEqualTo("30");
+    }
+    
+    @Test
+    @DisplayName("누적 점수 구하기 마지막 프레임 - 스트라이크 => 스페어")
+    void get_cumulative_score_final_frame_strike_spare() {
+        IntStream.rangeClosed(1, 9).forEach(count -> player.bowl(new CountOfFallenPinsDTO("10")));
+        player.bowl(new CountOfFallenPinsDTO("10"));
+        player.bowl(new CountOfFallenPinsDTO("5"));
+        player.bowl(new CountOfFallenPinsDTO("5"));
+        final String cumulativeScore = referee.calculateCumulativeScore(playerDTO, 9);
+        assertThat(cumulativeScore).isEqualTo("20");
+    }
+    
+    @Test
+    @DisplayName("누적 점수 구하기 마지막 프레임 - 스트라이크 => 미스")
+    void get_cumulative_score_final_frame_strike_miss() {
         IntStream.rangeClosed(1, 9).forEach(count -> player.bowl(new CountOfFallenPinsDTO("10")));
         player.bowl(new CountOfFallenPinsDTO("10"));
         player.bowl(new CountOfFallenPinsDTO("5"));
         player.bowl(new CountOfFallenPinsDTO("4"));
         final String cumulativeScore = referee.calculateCumulativeScore(playerDTO, 9);
         assertThat(cumulativeScore).isEqualTo("19");
+    }
+    
+    @Test
+    @DisplayName("누적 점수 구하기 마지막 프레임 - 스페어 => 노멀")
+    void get_cumulative_score_final_frame_spare_normal() {
+        IntStream.rangeClosed(1, 9).forEach(count -> player.bowl(new CountOfFallenPinsDTO("10")));
+        player.bowl(new CountOfFallenPinsDTO("5"));
+        player.bowl(new CountOfFallenPinsDTO("5"));
+        player.bowl(new CountOfFallenPinsDTO("4"));
+        final String cumulativeScore = referee.calculateCumulativeScore(playerDTO, 9);
+        assertThat(cumulativeScore).isEqualTo("14");
+    }
+    
+    @Test
+    @DisplayName("누적 점수 구하기 마지막 프레임 - 미스")
+    void get_cumulative_score_final_frame_miss() {
+        IntStream.rangeClosed(1, 9).forEach(count -> player.bowl(new CountOfFallenPinsDTO("10")));
+        player.bowl(new CountOfFallenPinsDTO("5"));
+        player.bowl(new CountOfFallenPinsDTO("4"));
+        final String cumulativeScore = referee.calculateCumulativeScore(playerDTO, 9);
+        assertThat(cumulativeScore).isEqualTo("9");
     }
     
     @Test
@@ -75,8 +130,53 @@ public class RefereeTest {
     void stop_calculate_spare() {
         player.bowl(new CountOfFallenPinsDTO("4"));
         player.bowl(new CountOfFallenPinsDTO("6"));
-        player.bowl(new CountOfFallenPinsDTO("6"));
         final String cumulativeScore = referee.calculateCumulativeScore(playerDTO, 0);
-        assertThat(cumulativeScore).isEqualTo("16");
+        assertThat(cumulativeScore).isEqualTo("-1");
+    }
+    
+    @Test
+    @DisplayName("누적 점수 계산 정지 조건 마지막 프레임 - 스트라이크 => 노멀")
+    void stop_calculate_final_frame_strike_normal() {
+        IntStream.rangeClosed(1, 9).forEach(count -> player.bowl(new CountOfFallenPinsDTO("10")));
+        player.bowl(new CountOfFallenPinsDTO("10"));
+        player.bowl(new CountOfFallenPinsDTO("5"));
+        final String cumulativeScore = referee.calculateCumulativeScore(playerDTO, 9);
+        assertThat(cumulativeScore).isEqualTo("-1");
+    }
+    
+    @Test
+    @DisplayName("누적 점수 계산 정지 조건 마지막 프레임 - 스트라이크 => 레디")
+    void stop_calculate_final_frame_strike_ready() {
+        IntStream.rangeClosed(1, 9).forEach(count -> player.bowl(new CountOfFallenPinsDTO("10")));
+        player.bowl(new CountOfFallenPinsDTO("10"));
+        final String cumulativeScore = referee.calculateCumulativeScore(playerDTO, 9);
+        assertThat(cumulativeScore).isEqualTo("-1");
+    }
+    
+    @Test
+    @DisplayName("누적 점수 계산 정지 조건 마지막 프레임 - 스페어 => 레디")
+    void stop_calculate_final_frame_spare_ready() {
+        IntStream.rangeClosed(1, 9).forEach(count -> player.bowl(new CountOfFallenPinsDTO("10")));
+        player.bowl(new CountOfFallenPinsDTO("5"));
+        player.bowl(new CountOfFallenPinsDTO("5"));
+        final String cumulativeScore = referee.calculateCumulativeScore(playerDTO, 9);
+        assertThat(cumulativeScore).isEqualTo("-1");
+    }
+    
+    @Test
+    @DisplayName("누적 점수 계산 정지 조건 마지막 프레임 - 레디")
+    void stop_calculate_final_frame_ready() {
+        IntStream.rangeClosed(1, 9).forEach(count -> player.bowl(new CountOfFallenPinsDTO("10")));
+        final String cumulativeScore = referee.calculateCumulativeScore(playerDTO, 9);
+        assertThat(cumulativeScore).isEqualTo("-1");
+    }
+    
+    @Test
+    @DisplayName("누적 점수 계산 정지 조건 마지막 프레임 - 노멀")
+    void stop_calculate_final_frame_normal() {
+        IntStream.rangeClosed(1, 9).forEach(count -> player.bowl(new CountOfFallenPinsDTO("10")));
+        player.bowl(new CountOfFallenPinsDTO("5"));
+        final String cumulativeScore = referee.calculateCumulativeScore(playerDTO, 9);
+        assertThat(cumulativeScore).isEqualTo("-1");
     }
 }
