dd46ee41b122d6347dd4088264045ec18aad38bd
michol.lee <lovethefeel@gmail.com>
2022-10-17 12:27:58 +0900

    refactor: 답변 검증 로직 추가

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -1,9 +1,11 @@
 package qna.domain;
 
+import qna.CannotDeleteException;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 public class Answer extends AbstractEntity {
@@ -24,10 +26,12 @@ public class Answer extends AbstractEntity {
     }
 
     public Answer(User writer, Question question, String contents) {
+
         this(null, writer, question, contents);
     }
 
     public Answer(Long id, User writer, Question question, String contents) {
+
         super(id);
 
         if(writer == null) {
@@ -43,33 +47,62 @@ public class Answer extends AbstractEntity {
         this.contents = contents;
     }
 
-    public Answer setDeleted(boolean deleted) {
+    void delete(final User loginUser) throws CannotDeleteException {
+
+        this.validateUser(loginUser);
+        this.setDeleted(true);
+    }
+
+    private Answer setDeleted(final boolean deleted) {
+
         this.deleted = deleted;
         return this;
     }
 
     public boolean isDeleted() {
+
         return deleted;
     }
 
     public boolean isOwner(User writer) {
+
         return this.writer.equals(writer);
     }
 
     public User getWriter() {
+
         return writer;
     }
 
-    public String getContents() {
-        return contents;
+    public void toQuestion(Question question) {
+
+        this.question = question;
     }
 
-    public void toQuestion(Question question) {
-        this.question = question;
+    private void validateUser(final User user) throws CannotDeleteException {
+
+        if (!this.isOwner(user)) {
+            throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Answer answer = (Answer) o;
+        return deleted == answer.deleted && Objects.equals(writer, answer.writer) && Objects.equals(question, answer.question) && Objects.equals(contents, answer.contents);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), writer, question, contents, deleted);
     }
 
     @Override
     public String toString() {
+
         return "Answer [id=" + getId() + ", writer=" + writer + ", contents=" + contents + "]";
     }
 }
