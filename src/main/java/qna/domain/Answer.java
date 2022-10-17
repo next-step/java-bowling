8e80ed5e02edd26564beb9d09c02bd8de58e6189
michol.lee <lovethefeel@gmail.com>
2022-10-17 12:31:14 +0900

    refactor: 답변 equals, hashCode 제거

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -87,20 +87,6 @@ public class Answer extends AbstractEntity {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        Answer answer = (Answer) o;
-        return deleted == answer.deleted && Objects.equals(writer, answer.writer) && Objects.equals(question, answer.question) && Objects.equals(contents, answer.contents);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), writer, question, contents, deleted);
-    }
-
-    @Override
     public String toString() {
 
         return "Answer [id=" + getId() + ", writer=" + writer + ", contents=" + contents + "]";
