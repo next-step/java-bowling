870b4c6f3e8e5e64ffedd75e9bf4d9081a303d2f
jinukix <jinukix@styleshare.kr>
2022-10-18 21:07:52 +0900

    feat: Answer Test

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,28 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.BDDAssertions.then;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("Answers 삭제 성공 | 작성자가 본인")
+    void deleteSuccess() throws CannotDeleteException {
+        A1.delete(UserTest.JAVAJIGI);
+        then(A1.isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Answer 삭제 실패 | 작성자가 아님")
+    void deleteFailByIsNotWriter() {
+        assertThatThrownBy(() -> A1.delete(UserTest.SANJIGI))
+            .isInstanceOf(CannotDeleteException.class)
+            .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
 }
