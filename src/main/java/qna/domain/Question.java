88802220adc5f553924f983a15a9dd533e1df5fc
nooose <hd15807@gmail.com>
2022-10-21 00:01:43 +0900

    refactor: 재정의 메서드 위치 변경
    중간에 위치하고 있어 변경했다.

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -88,11 +88,6 @@ public class Question extends AbstractEntity {
         return answers;
     }
 
-    @Override
-    public String toString() {
-        return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
-    }
-
     public void delete(User loginUser) throws CannotDeleteException {
         if (!isOwner(loginUser)) {
             throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
@@ -112,5 +107,10 @@ public class Question extends AbstractEntity {
 
         deleteHistories.addAll(answers.deleteHistories());
         return deleteHistories;
+    }
+
+    @Override
+    public String toString() {
+        return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
     }
 }
