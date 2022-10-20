83511f33d3671933801b03718b9dd898d191e6a6
nooose <hd15807@gmail.com>
2022-10-20 23:57:13 +0900

    test: 요구사항 리스트에 맞게 테스트 코드 작성

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,30 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.exception.UnAuthorizedException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @DisplayName("자신의 쓴 답글이면 삭제할 수 있다.")
+    @Test
+    void delete() {
+        Answer answer = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+        answer.delete(UserTest.JAVAJIGI);
+
+        assertThat(answer.isDeleted()).isTrue();
+    }
+
+    @DisplayName("답글이 자신의 답글이 아니라면 삭제할 수 없다.")
+    @Test
+    void deleteException() {
+        Answer answer = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+        assertThatThrownBy(() -> answer.delete(UserTest.SANJIGI))
+                .isInstanceOf(UnAuthorizedException.class);
+    }
 }
---
+++ src/test/java/qna/domain/AnswersTest.java
@@ -0,0 +1,41 @@
+package qna.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AnswersTest {
+    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @DisplayName("일급 컬렉션 내 답글을 삭제 처리할 수 있다.")
+    @Test
+    void deleteAll() {
+        Answer answer1 = new Answer(1L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+        Answer answer2 = new Answer(2L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+        Answers answers = new Answers(List.of(answer1, answer2));
+
+        answers.delete(UserTest.JAVAJIGI);
+
+        assertThat(answer1.isDeleted()).isTrue();
+        assertThat(answer2.isDeleted()).isTrue();
+    }
+
+    @DisplayName("삭제된 답글의 정보를 리스트로 받을 수 있다.")
+    @Test
+    void delete() {
+        Answer answer1 = new Answer(1L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+        Answer answer2 = new Answer(2L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+        Answers answers = new Answers(List.of(answer1, answer2));
+        DeleteHistory deleteHistory1 = new DeleteHistory(ContentType.ANSWER, answer1.getId(), answer1.getWriter(), LocalDateTime.now());
+        DeleteHistory deleteHistory2 = new DeleteHistory(ContentType.ANSWER, answer2.getId(), answer2.getWriter(), LocalDateTime.now());
+
+        answers.delete(UserTest.JAVAJIGI);
+
+        assertThat(answers.deleteHistories()).containsExactly(deleteHistory1, deleteHistory2);
+    }
+}
