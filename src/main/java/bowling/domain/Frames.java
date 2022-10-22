0c51986df22c0a6d2d1b59ad34461e8d53ddd7e3
Wordbe <seonghojin3@gmail.com>
2022-10-23 05:03:34 +0900

    feat: Frames 추가

---
+++ src/main/java/bowling/domain/Frames.java
@@ -0,0 +1,89 @@
+package bowling.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static java.util.Collections.unmodifiableList;
+import static java.util.Objects.isNull;
+
+public class Frames {
+    public static final int MAX_FRAME_NUMBER = 10;
+
+    private final List<Frame> frames;
+
+    Frames(List<Frame> frames) {
+        if (isNull(frames) || frames.isEmpty()) {
+            throw new IllegalArgumentException("frames 가 null 이거나 비어있을 수 없습니다.");
+        }
+        this.frames = frames;
+    }
+
+    public static Frames init() {
+        return new Frames(List.of(NormalFrame.init()));
+    }
+
+    public List<Frame> frames() {
+        return unmodifiableList(frames);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Frames)) return false;
+
+        Frames frames1 = (Frames) o;
+
+        return Objects.equals(frames, frames1.frames);
+    }
+
+    @Override
+    public int hashCode() {
+        return frames != null ? frames.hashCode() : 0;
+    }
+
+    public Frames bowl(Score score) {
+        Frame frame = lastFrame().bowl(score);
+        return update(lastIndex(), frame);
+    }
+
+    public Frames next() {
+        if (!lastFrame().isFinished()) {
+            return this;
+        }
+
+        if (lastFrameNumber() == MAX_FRAME_NUMBER - 1) {
+            return add(FinalFrame.init());
+        }
+
+        return add(NormalFrame.init());
+    }
+
+    public boolean isOver() {
+        return lastFrameNumber() > MAX_FRAME_NUMBER;
+    }
+
+    public int lastFrameNumber() {
+        return lastIndex() + 1;
+    }
+
+    private Frames add(Frame frame) {
+        List<Frame> result = new ArrayList<>(frames);
+        result.add(frame);
+        return new Frames(result);
+    }
+
+    private Frames update(int frameIndex, Frame frame) {
+        List<Frame> result = new ArrayList<>(frames);
+        result.set(frameIndex, frame);
+        return new Frames(result);
+    }
+
+    private Frame lastFrame() {
+        return frames.get(lastIndex());
+    }
+
+    private int lastIndex() {
+        return frames.size() - 1;
+    }
+}
---
+++ src/test/java/bowling/domain/FramesTest.java
@@ -0,0 +1,94 @@
+package bowling.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FramesTest {
+
+    @DisplayName("Frames 초기화는 일반프레임을 초기화해서 생성한다")
+    @Test
+    void init() {
+        assertThat(Frames.init()).isEqualTo(new Frames(List.of(normalFrame())));
+    }
+
+    @DisplayName("투구한 결과가 반환된다")
+    @Test
+    void bowl() {
+        Frames result = Frames.init()
+                .bowl(new Score(10));
+
+        assertThat(result).isEqualTo(new Frames(List.of(normalFrame().bowl(new Score(10)))));
+    }
+
+    @DisplayName("다음 Frames 를 반환한다")
+    @Test
+    void next() {
+        Frames result = Frames.init()
+                .bowl(new Score(10))
+                .next();
+
+        Frames expected = new Frames(List.of(
+                normalFrame().bowl(new Score(10)),
+                normalFrame()));
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("9번째 다음은 최종프레임을 넣어 Frames 를 반환한다")
+    @Test
+    void next_when9Frames() {
+        Frames result = frames(9).next();
+
+        List<Frame> frameList = frameList(9);
+        frameList.add(finalFrame());
+        Frames expected = new Frames(frameList);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("Frames 가 끝났는지 여부를 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "11,false",
+            "12,true"
+    })
+    void isOver(int frameCount, boolean expected) {
+        assertThat(frames(frameCount).isOver()).isEqualTo(expected);
+    }
+
+    @DisplayName("마지막 프레임넘버를 반환한다")
+    @Test
+    void lastFrameNumber() {
+        assertThat(frames(5).lastFrameNumber()).isEqualTo(6);
+    }
+
+    private Frame normalFrame() {
+        return NormalFrame.init();
+    }
+
+    private Frame finalFrame() {
+        return FinalFrame.init();
+    }
+
+    private Frames frames(int frameCount) {
+        Frames result = Frames.init();
+        for (int i = 0; i < frameCount; i++) {
+            result = result.bowl(new Score(10));
+            result = result.next();
+        }
+        return result;
+    }
+
+    private List<Frame> frameList(int frameCount) {
+        List<Frame> result = new ArrayList<>();
+        for (int i = 0; i < frameCount; i++) {
+            result.add(normalFrame().bowl(new Score(10)));
+        }
+        return result;
+    }
+}
