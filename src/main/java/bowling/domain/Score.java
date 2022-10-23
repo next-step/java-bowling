81188a534f7e4dfb8a0d2d5a58b332d1374075e2
Wordbe <seonghojin3@gmail.com>
2022-10-23 18:57:54 +0900

    fix: MIN 상수 추가

--- src/main/java/bowling/domain/Score.java
+++ src/main/java/bowling/domain/Score.java
@@ -1,13 +1,16 @@
 package bowling.domain;
 
+import static java.lang.String.format;
+
 public class Score {
+    private static final int MIN = 0;
     private static final int MAX = 10;
 
     private final int score;
 
     public Score(int score) {
-        if (score < 0 || score > MAX) {
-            throw new IllegalArgumentException(String.format("볼링 점수는 1에서 %s 사이이어야 합니다.", MAX));
+        if (score < MIN || score > MAX) {
+            throw new IllegalArgumentException(format("볼링 점수는 %s에서 %s 사이이어야 합니다.", MIN, MAX));
         }
 
         this.score = score;
