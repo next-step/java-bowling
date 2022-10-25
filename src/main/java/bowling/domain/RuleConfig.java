c405900c163de39481da2b319cc19ec3855cd393
OhSeungCheol <sc.oh131@gmail.com>
2022-10-25 13:31:27 +0900

    [M2] feat: 기본 게임 룰 상수 추가
    참고: https://github.com/next-step/java-bowling/pull/885

---
+++ src/main/java/bowling/domain/RuleConfig.java
@@ -0,0 +1,8 @@
+package bowling.domain;
+
+public class RuleConfig {
+    public static final int NUMBER_OF_PIN = 10;
+    public static final int NUMBER_OF_FRAME_OF_BOWLING = 10;
+    public static final int PITCH_COUNT = 2;
+    public static final int NAME_LENGTH = 3;
+}
