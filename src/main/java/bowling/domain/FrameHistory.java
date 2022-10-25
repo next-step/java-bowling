3ad53a6ff3735c297fee36504b96f49f13b504a3
OhSeungCheol <sc.oh131@gmail.com>
2022-10-25 15:15:28 +0900

    [M2] feat: 프레임 결과에 대한 기록 클래스 추가
    7. 프레임의 결과를 기록하는 기능 개발
    
    참고: https://github.com/next-step/java-bowling/pull/885

---
+++ src/main/java/bowling/domain/FrameHistory.java
@@ -0,0 +1,58 @@
+package bowling.domain;
+
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FrameHistory {
+    private final List<PitchHistory> pitchHistories = new ArrayList<>();
+    private FrameHistory nextFrameHistory = null;
+
+    public void record(PitchResultEnum pitchResultEnum, int downPinCount){
+        PitchHistory pitchHistory = new PitchHistory(pitchResultEnum, downPinCount);
+        FrameHistory lastHistory = getLastHistory();
+        lastHistory.addPitchHistory(pitchHistory);
+    }
+
+    public FrameHistory getNextFrameHistory() {
+        return nextFrameHistory;
+    }
+
+    public List<PitchHistory> getPitchHistories() {
+        return pitchHistories;
+    }
+
+    public int getLastIndex(){
+        return getLastIndex(0);
+    }
+
+    public void addHistory(){
+        if(CollectionUtils.isEmpty(pitchHistories)){
+            return;
+        }
+        FrameHistory lastHistory = getLastHistory();
+        lastHistory.nextFrameHistory = new FrameHistory();
+    }
+
+    private FrameHistory getLastHistory(){
+        if(this.nextFrameHistory == null){
+            return this;
+        }
+        FrameHistory nextFrameHistory = this.nextFrameHistory;
+        return nextFrameHistory.getLastHistory();
+    }
+
+    private int getLastIndex(int currentIndex){
+        if(nextFrameHistory == null){
+            return currentIndex;
+        }
+        currentIndex++;
+        return nextFrameHistory.getLastIndex(currentIndex);
+    }
+
+    private void addPitchHistory(PitchHistory pitchHistory){
+        pitchHistories.add(pitchHistory);
+    }
+
+}
--- src/main/java/bowling/domain/RuleConfig.java
+++ src/main/java/bowling/domain/RuleConfig.java
@@ -2,7 +2,7 @@ package bowling.domain;
 
 public class RuleConfig {
     public static final int NUMBER_OF_PIN = 10;
-    public static final int NUMBER_OF_FRAME_OF_BOWLING = 10;
+    public static final int NUMBER_OF_FRAME = 10;
     public static final int PITCH_COUNT = 2;
     public static final int NAME_LENGTH = 3;
 }
