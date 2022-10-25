a67c72d6cf0578d8729cc825a04011999edd7eeb
OhSeungCheol <sc.oh131@gmail.com>
2022-10-25 16:17:11 +0900

    [M2] feat: 예외처리 추가
    참고: https://edu.nextstep.camp/s/DVVUSLFe/ls/l8jJozTD

--- src/main/java/bowling/domain/Frame.java
+++ src/main/java/bowling/domain/Frame.java
@@ -5,6 +5,8 @@ import bowling.view.InputView;
 import bowling.view.ResultView;
 
 public class Frame {
+    private static final String WRONG_INPUT_STRING = "Wrong Input: downPinCount";
+
     private int restPin = RuleConfig.NUMBER_OF_PIN;
     private int restPitchCount = RuleConfig.PITCH_COUNT;
 
@@ -39,6 +41,8 @@ public class Frame {
 
 
     protected PitchResultEnum downPin(int downPinCount){
+        checkDownPinCount(downPinCount);
+
         restPin -= downPinCount;
         restPitchCount--;
 
@@ -51,5 +55,11 @@ public class Frame {
 
     private boolean isTryOut() {
         return this.restPitchCount <= 0;
+    }
+
+    private void checkDownPinCount(int downPinCount){
+        if(restPin < downPinCount){
+            throw new IllegalArgumentException(WRONG_INPUT_STRING);
+        }
     }
 }
