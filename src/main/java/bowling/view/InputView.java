1cac8afa7367610bbe28104fe8b4d620b613eae6
Wordbe <seonghojin3@gmail.com>
2022-10-28 01:32:44 +0900

    fix: static 상수명 대문자로 변경

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -6,21 +6,21 @@ import bowling.domain.PlayerName;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
     private static final String NAME_QUESTION = "플레이어 이름은(3 english letters)?: ";
     private static final String FALLEN_PINS_QUESTION_FORMAT = "%s프레임 투구 : ";
 
     public static void closeScan() {
-        scanner.close();
+        SCANNER.close();
     }
 
     public static PlayerName scanName() {
         System.out.print(NAME_QUESTION);
-        return new PlayerName(scanner.nextLine());
+        return new PlayerName(SCANNER.nextLine());
     }
 
     public static Pins scanFallenPins(int frameNumber) {
         System.out.printf(FALLEN_PINS_QUESTION_FORMAT, frameNumber);
-        return Pins.of(Integer.parseInt(scanner.nextLine()));
+        return Pins.of(Integer.parseInt(SCANNER.nextLine()));
     }
 }
