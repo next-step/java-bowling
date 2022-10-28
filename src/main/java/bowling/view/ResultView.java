7acf57c7acf433b5004c05880386bf4065f7e46b
OhSeungCheol <sc.oh131@gmail.com>
2022-10-28 13:08:37 +0900

    [M2] feat: 신규 클래스를 활용하도록 ResultView 수정
    참고: https://github.com/next-step/java-bowling/pull/896

--- src/main/java/bowling/view/ResultView.java
+++ src/main/java/bowling/view/ResultView.java
@@ -1,56 +1,69 @@
 package bowling.view;
 
-import bowling.domain.FrameHistory;
-import bowling.domain.PitchHistory;
-import bowling.domain.PitchResult;
-import bowling.domain.Player;
+import bowling.domain.BowlingGame;
+import bowling.domain.Frame;
+import bowling.domain.Records;
+import bowling.domain.Scores;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResultView {
     private static String INIT_SCORE_BOARD_STRING = "| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |";
-    private static String SEPARATE = "|";
     private static String NAME_STRING = "|  %s |";
-    private static String STRIKE_STRING = " X   ";
-    private static String SPARE_STRING = "/ ";
-    private static String MISS_STRING = " ";
-    private static String GUTTER_STRING = "- ";
-    private static String NONE_STRING = " ";
-    private static String VOID_STRING = "";
+    private static String STRIKE_STRING = "  X   |";
+    private static String SPARE_STRING = "  %s|/ |";
+    private static String GUTTER_STRING = "-";
+    private static String FIRST_PITCH_STRING = "  %s|";
+    private static String SECOND_PITCH_STRING = "  %s|%s |";
 
-
-    public static void printScoreBoard(FrameHistory frameHistory, Player player){
+    public static void printScoreBoard(BowlingGame game) {
         System.out.println(INIT_SCORE_BOARD_STRING);
-        System.out.printf(NAME_STRING, player.getName());
-        while(frameHistory != null){
-            printScore(frameHistory);
-            frameHistory = frameHistory.getNextFrameHistory();
-        }
+        System.out.printf(NAME_STRING, game.getPlayer().getName());
+        printScore(game.getRecords());
         System.out.println();
     }
 
-    private static void printScore(FrameHistory frameHistory){
-        System.out.print(NONE_STRING);
-        for(PitchHistory pitchHistory : frameHistory.getPitchHistories()){
-            System.out.print(makeResultString(pitchHistory) + SEPARATE);
+    private static void printScore(Records records) {
+        for (Frame frame : records.getFrames()) {
+            printScore(frame.getScores());
         }
     }
 
-    private static String makeResultString(PitchHistory pitchHistory){
-        if (PitchResult.STRIKE == pitchHistory.getResult()) {
-            return STRIKE_STRING;
+    private static void printScore(Scores scores) {
+        if (scores.isStrike()) {
+            System.out.print(STRIKE_STRING);
+            return;
         }
-        if (PitchResult.SPARE == pitchHistory.getResult()) {
-            return SPARE_STRING;
+
+        List<String> downPins = getDownPinsString(scores);
+        if (scores.isSpare()) {
+            System.out.printf(SPARE_STRING, downPins.get(0));
+            return;
         }
-        if (PitchResult.MISS == pitchHistory.getResult()) {
-            return pitchHistory.getDownPin() + MISS_STRING;
+
+        if (downPins.size() == 1) {
+            System.out.printf(FIRST_PITCH_STRING, downPins.get(0));
+            return;
         }
-        if (PitchResult.GUTTER == pitchHistory.getResult()) {
+
+        if (downPins.size() == 2) {
+            System.out.printf(SECOND_PITCH_STRING, downPins.get(0), downPins.get(1));
+            return;
+        }
+    }
+
+    private static List<String> getDownPinsString(Scores scores) {
+        return scores.getDownPins()
+                .stream()
+                .map(ResultView::convertScore)
+                .collect(Collectors.toList());
+    }
+
+    private static String convertScore(int score) {
+        if (score == 0) {
             return GUTTER_STRING;
         }
-        if (PitchResult.NONE == pitchHistory.getResult()) {
-            return NONE_STRING + pitchHistory.getDownPin();
-        }
-        return VOID_STRING;
+        return String.valueOf(score);
     }
-
 }
