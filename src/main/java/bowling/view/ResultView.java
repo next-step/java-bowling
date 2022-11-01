95927a95cd525fc5ee9509fd0378c50b7ebe0eda
OhSeungCheol <sc.oh131@gmail.com>
2022-11-01 19:47:26 +0900

    [M3] feat: 누적 점수 출력 기능 개발
    참고: https://edu.nextstep.camp/s/DVVUSLFe/ls/RmTfcj3f

--- src/main/java/bowling/view/ResultView.java
+++ src/main/java/bowling/view/ResultView.java
@@ -1,6 +1,7 @@
 package bowling.view;
 
 import bowling.domain.*;
+import bowling.exception.NotReadyException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,15 +10,38 @@ public class ResultView {
     private static String INIT_SCORE_BOARD_STRING = "| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |";
     private static String NAME_STRING = "|  %s |";
     private static String GUTTER_STRING = "-";
+    private static String TOTAL_SCORE_STRING = "\n|      |";
+    private static String TOTAL_SCORE_RIGHT_STRING = "  |";
+    private static String DOULBE_BLANK = "  ";
 
     public static void printScoreBoard(BowlingGame game) {
         System.out.println(INIT_SCORE_BOARD_STRING);
         System.out.printf(NAME_STRING, game.getPlayer().getName());
-        printScore(game.getRecords());
+        printPinScore(game.getRecords());
+        try {
+            printTotalScores(game.getRecords());
+        } catch (NotReadyException ignored) {
+        }
         System.out.println();
     }
 
-    private static void printScore(Records records) {
+    private static void printTotalScores(Records records) {
+        System.out.print(TOTAL_SCORE_STRING);
+        int totalScore;
+        for (int index = 0; index < records.getRecordCount(); index++) {
+            totalScore = records.getTotalScore(index);
+            System.out.print(DOULBE_BLANK + getTotalScoreString(totalScore) + TOTAL_SCORE_RIGHT_STRING);
+        }
+    }
+
+    private static String getTotalScoreString(int totalScore) {
+        if (totalScore < 10) {
+            return "0" + totalScore;
+        }
+        return "" + totalScore;
+    }
+
+    private static void printPinScore(Records records) {
         for (Frame frame : records.getFrames()) {
             printScore(frame.getScores());
             printBonusScore(frame);
