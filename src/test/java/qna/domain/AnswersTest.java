26a6d6059747f9563c031ebbeb373332796cb2d0
boradol2 <boradol0902@gmail.com>
2022-11-05 00:11:31 +0900

    refactor(Answers): 테스트 끼리 로직에 영향받지 않게 테스트 구현.

--- src/test/java/qna/domain/AnswersTest.java
+++ src/test/java/qna/domain/AnswersTest.java
@@ -10,11 +10,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AnswersTest {
-    private static final Answers answers = new Answers();
 
     @Test
     @DisplayName("본인의 질문에 본인이 작성한 답변인 것만 있을 때, 삭제 성공하고 질문 삭제 히스토리에 추가된다.")
     void deleteAnswersSuccessTest() throws CannotDeleteException {
+        Answers answers = new Answers();
         answers.add(AnswerTest.A1);
         List<DeleteHistory> deleteHistories = answers.deleteAll(UserTest.JAVAJIGI);
         assertThat(deleteHistories).hasSize(1);
@@ -24,6 +24,7 @@ class AnswersTest {
     @Test
     @DisplayName("본인의 질문에 본인 이외의 답변 작성자가 있을시, 삭제 실패와 에러발생 한다.")
     void deleteAnswersFailByIsNotWriter() {
+        Answers answers = new Answers();
         answers.add(AnswerTest.A2);
         assertThatThrownBy(() -> answers.deleteAll(UserTest.JAVAJIGI))
                 .isInstanceOf(CannotDeleteException.class)
