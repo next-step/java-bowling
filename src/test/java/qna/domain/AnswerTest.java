36274bb8a8f2ecf045d903ceea29e10a6ad020fe
Yunsik-Choi <choiys0311@gmail.com>
2022-11-05 02:14:02 +0900

    ⛑️ test : AnswerTest set_delete 질문의 상태를 삭제 상태로 변경한다.

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -2,6 +2,7 @@ package qna.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import qna.UnAuthorizedException;
@@ -14,5 +15,17 @@ public class AnswerTest {
     @Test
     void validate_writer() {
         assertThatThrownBy(() -> A1.delete(UserTest.SANJIGI)).isInstanceOf(UnAuthorizedException.class);
+    }
+
+    @DisplayName("질문의 상태를 삭제 상태로 변경한다.")
+    @Test
+    void set_delete() {
+        Answer answer = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "content");
+
+        Assertions.assertAll(
+                () -> assertThat(answer.isDeleted()).isFalse(),
+                () -> assertThat(answer.setDeleted(true).isDeleted()).isTrue(),
+                () -> assertThat(answer.setDeleted(false).isDeleted()).isFalse()
+        );
     }
 }
