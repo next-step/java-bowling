f952d65988ed6acfdbe2ef8193fa8ccd5e9bf9cc
suchanmyoung <aud248@naver.com>
2022-11-05 20:23:49 +0900

    refactor: test 메서드명 추가

--- src/test/java/step1/qna/domain/AnswerTest.java
+++ src/test/java/step1/qna/domain/AnswerTest.java
@@ -9,9 +9,9 @@ public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
 
-    @DisplayName("")
+    @DisplayName("다른 사람의 답변이 있는 경우 삭제 시 예외처리")
     @Test
-    void test() {
+    void other_answer_delete_throw_exception() {
         Assertions.assertThatThrownBy(() -> A1.validateAnswerAuthentication(UserTest.SANJIGI))
                 .isInstanceOf(CannotDeleteException.class)
                 .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
