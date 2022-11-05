753f0e4406699f5e143800da460246dd522d7f09
boradol <58449849+boradol@users.noreply.github.com>
2022-11-05 13:41:05 +0900

    🎳1단계 - 질문 삭제하기 기능 리팩토링 (#906)
    * docs(README.md): step 1. 기능 명세.
    
    * docs(QnaService): Service 에서 도메인으로 비즈니스 이동할 코드 삭제
    
    * docs(Question, Answer): 도메인에 비즈니스 로직 이동.
    
    1. delete true 변환
    2. DeleteHistory 내역 추가시킴
    3. validate 구현.
    4. 불필요한 getter/setter 코드 삭제.
    
    * docs(Answers): 일급컬렉션 생성.
    
    1. @Embedded, @Embeddable 사용.
    
    * docs(README.md): 리뷰 요구사항 명세.
    
    * refactor(Answer, Question): 기본생성자 살리기.
    
    * refactor(Question): 질문 삭제 테스트 추가
    
    1. 질문의 답변이 질문작성자만 답변하였을 때 삭제 성공
    2. 질문과 답변은 delete -> true
    3. 삭제 내역은 질문과 답변 각각 1개씩 해서 총 2개 생김.
    
    * refactor(Answers): 테스트 끼리 로직에 영향받지 않게 테스트 구현.
    
    * refactor(AnswerTest): 오탈자 수정.

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,28 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("본인이 작성한 답변이면 삭제 성공 한다.")
+    void deleteAnswerSuccess() throws CannotDeleteException {
+        A1.delete(UserTest.JAVAJIGI);
+        assertThat(A1.isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("본인이 답변 작성자가 아니면 삭제 실패하고 에러 발생 한다.")
+    void deleteAnswerFailByIsNotWriter() {
+        assertThatThrownBy(() -> A1.delete(UserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,51 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static qna.domain.AnswerTest.A1;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    @DisplayName("질문작성자가 답변이 없는 질문을 삭제하면, 삭제상태가 되고 삭제 내역을 반환해야 한다.")
+    void deleteQuestionSuccessTest() throws CannotDeleteException {
+        List<DeleteHistory> deleteHistories = Q1.delete(UserTest.JAVAJIGI);
+        assertThat(Q1.isDeleted()).isTrue();
+        assertThat(deleteHistories).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("질문작성자가 자신의 답변만 있는 질문을 삭제하면, 질문과 답변은 삭제상태가 되고 각각의 삭제 내역을 반환해야 한다.")
+    void deleteQuestionSuccessExistsAnswerTest() throws CannotDeleteException {
+        Q1.addAnswer(A1);
+        List<DeleteHistory> deleteHistories = Q1.delete(UserTest.JAVAJIGI);
+        assertThat(Q1.isDeleted()).isTrue();
+        assertThat(A1.isDeleted()).isTrue();
+        assertThat(deleteHistories).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("질문작성자가 아니면, 삭제할 권한이 없어 에러 발생한다.")
+    void deleteQuestionFailTest() {
+        assertThatThrownBy(() -> Q1.delete(UserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("질문을 삭제할 권한이 없습니다.");
+    }
+
+    @Test
+    @DisplayName("질문에 다른 사람이 쓴 답변이 있으면 질문 삭제하지 못하고 에러 발생한다.")
+    void deleteQuestionFailExistsAnswerTest() {
+        Q1.addAnswer(AnswerTest.A2);
+        assertThatThrownBy(() -> Q1.delete(UserTest.JAVAJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
 }
