aa5901b55ecbb0dc3c030f0b5340e14f9c40a7d8
jimbaemon <jimbae@kakao.com>
2022-11-06 20:50:29 +0900

    feat: 답변 soft delete 처리.

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -74,6 +74,7 @@ public class Answer extends AbstractEntity {
         if (!loginUser.equals(writer)) {
             throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
         }
+        this.deleted = true;
     }
 
     @Override
--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationEventPublisher;
 import qna.CannotDeleteException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
@@ -13,8 +16,14 @@ public class AnswerTest {
 
     @Test
     void 로그인한_사용자와_답변자가_같은경우만_삭제가능() {
-        Assertions.assertThatThrownBy(() -> A1.delete(UserTest.SANJIGI, eventPublisher))
+        assertThatThrownBy(() -> A1.delete(UserTest.SANJIGI, eventPublisher))
                 .isInstanceOf(CannotDeleteException.class)
                 .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
+
+    @Test
+    void 삭제_성공() throws CannotDeleteException {
+        A1.delete(UserTest.JAVAJIGI, eventPublisher);
+        assertThat(A1.isDeleted()).isTrue();
     }
 }
