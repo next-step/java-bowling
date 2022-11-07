fedcaaacdb63b347f96c9f6174caa649bcbb4ea1
hiblue02 <hiblue02@gmail.com>
2022-11-07 23:14:25 +0900

    feat(InputView) : 게임 참가자 수를 입력 받는 기능 추가

--- src/main/java/bowling/view/InputView.java
+++ src/main/java/bowling/view/InputView.java
@@ -10,9 +10,15 @@ public class InputView {
 
     }
 
-    public static String askPlayerName(){
-        System.out.printf("플레이어 이름은(3 english letters)?:");
+    public static String askPlayerName(int i){
+        System.out.printf("플레이어 "+ i +"의 이름은(3 english letters)?:");
         return SCANNER.nextLine();
     }
 
+    public static int askHowManyPeople() {
+        System.out.printf("How many people? : ");
+        int many = SCANNER.nextInt();
+        SCANNER.nextLine();
+        return many;
+    }
 }
