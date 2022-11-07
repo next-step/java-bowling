b9d51026fd7f11ac3b80123fdeac6859c58f6001
hiblue02 <hiblue02@gmail.com>
2022-11-08 00:48:40 +0900

    test(Frame): endOfTurn 테스트 케이스 추가

--- src/test/java/bowling/domain/FramesTest.java
+++ src/test/java/bowling/domain/FramesTest.java
@@ -52,4 +52,50 @@ class FramesTest {
         );
 
     }
+
+    @Test
+    @DisplayName("스트라이크면 턴 종료")
+    void end_of_turn_strike() {
+        //given
+        Frames frames = new Frames();
+        //when
+        frames.bowl( a -> new Pin(10));
+        //then
+        assertThat(frames.endOfTurn()).isTrue();
+    }
+
+    @Test
+    @DisplayName("스페어면 턴 종료")
+    void end_of_turn_spare() {
+        //given
+        Frames frames = new Frames();
+        //when
+        frames.bowl( a -> new Pin(1));
+        frames.bowl( a -> new Pin(9));
+        //then
+        assertThat(frames.endOfTurn()).isTrue();
+    }
+    @Test
+    @DisplayName("miss면 턴 종료")
+    void end_of_turn_miss() {
+        //given
+        Frames frames = new Frames();
+        //when
+        frames.bowl( a -> new Pin(5));
+        frames.bowl( a -> new Pin(3));
+        //then
+        assertThat(frames.endOfTurn()).isTrue();
+    }
+
+    @Test
+    @DisplayName("한번 던지면 턴 종료 아님")
+    void not_end_of_turn_one_bowl() {
+        //given
+        Frames frames = new Frames();
+        //when
+        frames.bowl( a -> new Pin(5));
+        //then
+        assertThat(frames.endOfTurn()).isFalse();
+    }
+
 }
