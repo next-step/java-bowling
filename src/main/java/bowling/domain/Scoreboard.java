45159183646aff590734da9f9da2929c265b3dfb
Yunsik-Choi <choiys0311@gmail.com>
2022-11-08 16:13:55 +0900

    ⚡feat : Scoreboard addScore

--- src/main/java/bowling/domain/Scoreboard.java
+++ src/main/java/bowling/domain/Scoreboard.java
@@ -1,13 +1,40 @@
 package bowling.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 public class Scoreboard {
 
+    private static final int FIRST_ROUND = 1;
+    private static final int LAST_ROUND = 10;
     private final Name name;
+    private final List<Frame> frames;
 
     public Scoreboard(Name name) {
         this.name = name;
+        this.frames = new ArrayList<>();
+        for (int i = FIRST_ROUND; i < LAST_ROUND; i++) {
+            this.frames.add(new DefaultFrame());
+        }
+        this.frames.add(new LastFrame());
+    }
+
+    public void addScore(Score score, Round round) {
+        this.frames.get(round.index()).addScore(score);
+    }
+
+    public List<Frame> frames() {
+        return Collections.unmodifiableList(this.frames);
+    }
+
+    public Frame frame(Round round) {
+        return this.frames.get(round.index());
+    }
+
+    public Name name() {
+        return this.name;
     }
 
     @Override
