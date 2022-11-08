d745d0279f618859b523c720f498a7d4897f0d5b
yunhwan <pyh8618@gmail.com>
2022-11-09 00:41:39 +0900

    test : AnswerTest 추가

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,27 @@
 package qna.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    void delete_성공() throws CannotDeleteException {
+        DeleteHistory delete = A1.delete(UserTest.JAVAJIGI);
+        assertThat(A1.isDeleted()).isTrue();
+        assertThat(delete).isEqualTo(new DeleteHistory(ContentType.ANSWER, A1.getId(), A1.getWriter(), LocalDateTime.now()));
+    }
+
+    @Test
+    void delete_작성자와_다른_유저가_삭제시_예외발생() {
+        assertThatThrownBy(() -> A1.delete(UserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class);
+    }
 }
