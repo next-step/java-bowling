a3066b8266d1b4d7ef1c07cfa21485321f1effbd
gumsu <chavi9@naver.com>
2022-11-08 15:58:42 +0900

    refactor: pins 볼링핀 캐싱

--- src/main/java/bowling/Pins.java
+++ src/main/java/bowling/Pins.java
@@ -1,15 +1,38 @@
 package bowling;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Pins {
 
-    private static final int MIN_PINS = 0;
-    private static final int MAX_PINS = 10;
+    public static final int MIN_PINS = 0;
+    public static final int MAX_PINS = 10;
+    private static final Map<Integer, Pins> cache = new HashMap<>();
+
+    static {
+        for (int index = MIN_PINS; index < MAX_PINS; index++) {
+            cache.put(index, new Pins(index));
+        }
+    }
 
     private int falledPins;
 
     public Pins(int falledPins) {
         validPins(falledPins);
         this.falledPins = falledPins;
+    }
+
+    public static Pins from(int falledPins) {
+        if (falledPins > MAX_PINS) {
+            throw new IllegalArgumentException(
+                String.format("볼링 핀은 최대 10을 넘을 수 없습니다. 현재 쓰러진 핀 수는 %d", falledPins));
+        }
+
+        if (falledPins < MIN_PINS) {
+            throw new IllegalArgumentException(
+                String.format("볼링 핀은 최초 0 미만이 될 수 없습니다. 현재 쓰러진 핀 수는 %d", falledPins));
+        }
+        return cache.get(falledPins);
     }
 
     private void validPins(int falledPins) {
