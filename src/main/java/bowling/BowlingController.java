a0d730c2f48689d8436b491504c1cf19d5fb3da7
Cheeze <cheeze.ball@kakaocorp.com>
2022-11-08 10:32:47 +0900

    docs() : README 기능 목록 추가

---
+++ src/main/java/bowling/BowlingController.java
@@ -0,0 +1,8 @@
+package bowling;
+
+public class BowlingController {
+
+    public void play() {
+        InputView inputView = new InputView();
+    }
+}
---
+++ src/main/java/bowling/InputView.java
@@ -0,0 +1,5 @@
+package bowling;
+
+public class InputView {
+    
+}
