637f7da79b98903767f13bc25b9cc004bc7cca82
bada.kang <kangbada0728@gmail.com>
2022-11-08 20:08:17 +0900

    refactor: answers 를 Embedded로 변경

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -1,11 +1,25 @@
 package qna.domain;
 
+import org.hibernate.annotations.Where;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.stream.Collectors.toUnmodifiableList;
 
+@Embeddable
 public class Answers {
-    private final List<Answer> answers;
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    @Where(clause = "deleted = false")
+    @OrderBy("id ASC")
+    private List<Answer> answers = new ArrayList<>();
+
+    public Answers() {
+    }
 
     public Answers(List<Answer> answers) {
         this.answers = answers;
@@ -15,6 +29,10 @@ public class Answers {
         return answers.stream()
                 .map(answer -> answer.delete(deleteBy))
                 .collect(toUnmodifiableList());
+    }
+
+    public void add(Answer answer) {
+        answers.add(answer);
     }
 
 }
