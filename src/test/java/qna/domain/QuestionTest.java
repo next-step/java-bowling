a54e56a81616de9fbcc688f30d84d56fb5235be4
moonstal1506 <moonstal1506@gmail.com>
2022-11-09 20:09:29 +0900

    test: 질문과 답변 삭제 이력에 대한 정보를 DeleteHistory를 활용해 남기는지 테스트

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import qna.CannotDeleteException;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -19,8 +19,7 @@ public class QuestionTest {
     @DisplayName("로그인 사용자와 질문한 사람이 같은 경우 삭제 가능하다.")
     @Test
     void deleteByLoginUser() {
-        List<DeleteHistory> deleteHistories = Q1.delete(JAVAJIGI);
-        assertThat(deleteHistories).hasSize(1);
+        Q1.delete(JAVAJIGI);
         assertThat(Q1.isDeleted()).isTrue();
     }
 
@@ -50,5 +49,17 @@ public class QuestionTest {
         assertThatThrownBy(() -> {
             Q2.delete(SANJIGI);
         }).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @DisplayName("질문과 답변 삭제 이력에 대한 정보를 DeleteHistory를 활용해 남긴다.")
+    @Test
+    void deleteHistory() {
+        Q1.addAnswer(A1);
+
+        assertThat(Q1.delete(JAVAJIGI))
+                .containsExactly(
+                        new DeleteHistory(ContentType.QUESTION, null, JAVAJIGI, LocalDateTime.now()),
+                        new DeleteHistory(ContentType.ANSWER, null, JAVAJIGI, LocalDateTime.now())
+                );
     }
 }
