64bcd3e63a34af42d5dcea9dbc62edad928d84e5
jwoh <jwoh@wemakeprice.com>
2022-11-09 20:25:01 +0900

    test : 도메인별 삭제 테스트

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,31 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    public void 삭제_성공() throws CannotDeleteException
+    {
+        User user = UserTest.JAVAJIGI;
+
+        A1.delete(user);
+
+        assertThat(A1.isDeleted()).isTrue();
+    }
+
+    @Test
+    public void 삭제_실패()
+    {
+        User user = UserTest.SANJIGI;
+
+        assertThatThrownBy(() ->
+            A1.delete(user)).isInstanceOf(CannotDeleteException.class);
+    }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,33 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    public void 삭제_성공() throws CannotDeleteException
+    {
+        User user = UserTest.JAVAJIGI;
+
+        Q1.delete(user);
+
+        assertThat(Q1.isDeleted()).isTrue();
+    }
+
+    @Test
+    public void 삭제_실패()
+    {
+        User user = UserTest.SANJIGI;
+
+        assertThatThrownBy(() ->
+            Q1.delete(user)).isInstanceOf(CannotDeleteException.class);
+    }
+
+
 }
