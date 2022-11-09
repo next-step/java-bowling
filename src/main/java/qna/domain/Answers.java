305d10f0d0747f8c8d3dafc1e115239760cf256f
moonstal1506 <moonstal1506@gmail.com>
2022-11-09 19:39:07 +0900

    refactor: Answers 일급 콜렉션 생성 후 answers 삭제 로직 이동

---
+++ src/main/java/qna/domain/Answers.java
@@ -0,0 +1,30 @@
+package qna.domain;
+
+import org.hibernate.annotations.Where;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Embeddable
+public class Answers {
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    @Where(clause = "deleted = false")
+    @OrderBy("id ASC")
+    private List<Answer> answers = new ArrayList<>();
+
+    public void add(Answer answer) {
+        answers.add(answer);
+    }
+
+    public List<DeleteHistory> delete(User loginUser) {
+        return answers.stream()
+                .map(answer -> answer.delete(loginUser))
+                .collect(Collectors.toList());
+    }
+}
