7b12c395e05483e36d6b0e429fafbabb09064879
leejihyung <nave369@naver.com>
2022-11-10 22:33:38 +0900

    test : delete logic test

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,43 @@
 package qna.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    void delete_성공() throws Exception {
+        Question question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        List<DeleteHistory> deleteHistories = question.delete(UserTest.JAVAJIGI);
+
+        assertThat(question.isDeleted()).isTrue();
+        assertThat(deleteHistories.size()).isEqualTo(1);
+    }
+
+    @Test
+    void delete_실패_질문을삭제할권한이없습니다() throws Exception {
+        Question question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        assertThatThrownBy(() -> {
+            question.delete(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    void delete_성공_질문자와답변글의모든답변자가같은경우에삭제가능하다() throws Exception {
+        Question question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        Answer answer = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+        question.addAnswer(answer);
+
+        List<DeleteHistory> deleteHistories = question.delete(UserTest.JAVAJIGI);
+
+        assertThat(question.isDeleted()).isTrue();
+        assertThat(deleteHistories.size()).isEqualTo(2);
+    }
 }
