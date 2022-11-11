b2d8219db7e94417bcc35e225573dfd2e57d5c2a
kingkk31 <kingkk31@naver.com>
2022-11-12 00:41:06 +0900

    feat: 마지막프레임 클래스 추가

---
+++ src/main/java/bowling/frame/FinalFrame.java
@@ -0,0 +1,85 @@
+package bowling.frame;
+
+import bowling.FallenPins;
+import bowling.ResultMark;
+import bowling.exception.EndedFrameException;
+
+public class FinalFrame implements Frame {
+
+    private FallenPins firstFallenPins;
+    private FallenPins secondFallenPins;
+    private FallenPins thirdFallenPins;
+
+    @Override
+    public Frame update(FallenPins fallenPins) {
+        if (isFinish()) {
+            throw new EndedFrameException();
+        }
+
+        if (firstFallenPins == null) {
+            firstFallenPins = fallenPins;
+            return this;
+        }
+
+        if (secondFallenPins == null) {
+            secondFallenPins = fallenPins;
+            return this;
+        }
+
+        thirdFallenPins = fallenPins;
+        return this;
+    }
+
+    @Override
+    public String getResult() {
+        ResultMark firstResultMark = ResultMark.getResultMark(firstFallenPins, null);
+        ResultMark secondResultMark = ResultMark.getResultMark(secondFallenPins, firstFallenPins);
+        ResultMark thirdResultMark = ResultMark.getResultMark(thirdFallenPins, null);
+
+        String firstResult = getMark(firstResultMark, firstFallenPins);
+        String secondResult = getMark(secondResultMark, secondFallenPins);
+        String thirdResult = getMark(thirdResultMark, thirdFallenPins);
+
+        String result = firstResult + RESULT_DELIMITER + secondResult + RESULT_DELIMITER + thirdResult;
+        return result.replace(RESULT_DELIMITER + ResultMark.EMPTY.getMark(), ResultMark.EMPTY.getMark());
+    }
+
+    public boolean isFinish() {
+        if (hasBonus()) {
+            return isNormalRoundFinish() && thirdFallenPins != null;
+        }
+        return isNormalRoundFinish();
+    }
+
+    private boolean isNormalRoundFinish() {
+        return firstFallenPins != null && secondFallenPins != null;
+    }
+
+    private boolean hasBonus() {
+        if (isHasStrike()) {
+            return true;
+        }
+        return isHasSpare();
+    }
+
+    private boolean isHasStrike() {
+        if (firstFallenPins != null
+                && ResultMark.STRIKE.equals(ResultMark.getResultMark(firstFallenPins, null))) {
+            return true;
+        }
+        return secondFallenPins != null
+                && ResultMark.STRIKE.equals(ResultMark.getResultMark(secondFallenPins, null));
+    }
+
+    private boolean isHasSpare() {
+        return ResultMark.SPARE.equals(ResultMark.getResultMark(secondFallenPins, firstFallenPins));
+    }
+
+    private String getMark(ResultMark resultMark, FallenPins fallenPins) {
+        if (ResultMark.MISS.equals(resultMark)) {
+            return String.valueOf(fallenPins.getCountOfPin());
+        }
+        return resultMark.getMark();
+    }
+
+}
---
+++ src/test/java/bowling/frame/FinalFrameTest.java
@@ -0,0 +1,136 @@
+package bowling.frame;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import bowling.FallenPins;
+import org.junit.jupiter.api.Test;
+
+class FinalFrameTest {
+
+    @Test
+    void 첫번째_투구에_모든_핀_쓰러뜨리면_스트라이크() {
+        Frame frame = new FinalFrame();
+        frame = frame.update(FallenPins.of(10));
+
+        assertThat(frame.getResult()).isEqualTo("X  ");
+    }
+
+    @Test
+    void 첫번째_투구_후_핀이_남아있고_두번째_투구에_모든_핀_쓰러뜨리면_스페어() {
+        Frame frame = new FinalFrame();
+        frame = frame.update(FallenPins.of(8));
+        frame = frame.update(FallenPins.of(2));
+
+        assertThat(frame.getResult()).isEqualTo("8|/ ");
+    }
+
+    @Test
+    void 스페어_후_보너스_투구() {
+        Frame frame = new FinalFrame();
+        frame = frame.update(FallenPins.of(8));
+        frame = frame.update(FallenPins.of(2));
+        frame = frame.update(FallenPins.of(10));
+
+        assertThat(frame.getResult()).isEqualTo("8|/|X");
+    }
+
+    @Test
+    void 스트라이크_후_두번째_투구() {
+        Frame frame = new FinalFrame();
+        frame = frame.update(FallenPins.of(10));
+        frame = frame.update(FallenPins.of(0));
+
+        assertThat(frame.getResult()).isEqualTo("X|- ");
+    }
+
+
+    @Test
+    void 두번째_투구_후에도_모든_핀을_쓰러뜨리지_못하면_미스() {
+        Frame frame = new FinalFrame();
+        frame = frame.update(FallenPins.of(8));
+        frame = frame.update(FallenPins.of(1));
+
+        assertThat(frame.getResult()).isEqualTo("8|1 ");
+    }
+
+    @Test
+    void 첫번째_투구_시_하나도_핀을_못쓰러뜨리면_첫번째_거터() {
+        Frame frame = new FinalFrame();
+        frame = frame.update(FallenPins.of(0));
+
+        assertThat(frame.getResult()).isEqualTo("-  ");
+    }
+
+    @Test
+    void 두번째_투구_시_하나도_핀을_못쓰러뜨리면_두번째_거터() {
+        Frame frame = new FinalFrame();
+        frame = frame.update(FallenPins.of(7));
+        frame = frame.update(FallenPins.of(0));
+
+        assertThat(frame.getResult()).isEqualTo("7|- ");
+    }
+
+    @Test
+    void 첫번째_투구_시_스트라이크는_아닐때_첫번째_점수_표기() {
+        Frame frame = new FinalFrame();
+        frame = frame.update(FallenPins.of(4));
+
+        assertThat(frame.getResult()).isEqualTo("4  ");
+    }
+
+    @Test
+    void 프레임_추가_투구_스트라이크로부터_존재하는_상황에서_종료됨() {
+        Frame frame = new FinalFrame();
+        frame = frame.update(FallenPins.of(10));
+        frame = frame.update(FallenPins.of(0));
+        frame = frame.update(FallenPins.of(4));
+
+        assertThat(frame.isFinish()).isTrue();
+    }
+
+    @Test
+    void 프레임_추가_투구_스페어로부터_존재하는_상황에서_종료됨() {
+        Frame frame = new FinalFrame();
+        frame = frame.update(FallenPins.of(2));
+        frame = frame.update(FallenPins.of(8));
+        frame = frame.update(FallenPins.of(10));
+
+        assertThat(frame.isFinish()).isTrue();
+    }
+
+    @Test
+    void 프레임_추가_투구_스트라이크로부터_존재하는_상황에서_진행중() {
+        Frame frame = new FinalFrame();
+        frame = frame.update(FallenPins.of(10));
+        frame = frame.update(FallenPins.of(10));
+
+        assertThat(frame.isFinish()).isFalse();
+    }
+
+    @Test
+    void 프레임_추가_투구_스페어로부터_존재하는_상황에서_진행중() {
+        Frame frame = new FinalFrame();
+        frame = frame.update(FallenPins.of(7));
+        frame = frame.update(FallenPins.of(3));
+
+        assertThat(frame.isFinish()).isFalse();
+    }
+
+    @Test
+    void 프레임_추가_투구_미존재하는_상황에서_종료됨() {
+        Frame frame = new FinalFrame();
+        frame = frame.update(FallenPins.of(7));
+        frame = frame.update(FallenPins.of(1));
+
+        assertThat(frame.isFinish()).isTrue();
+    }
+
+    @Test
+    void 프레임_추가_투구_미존재하는_상황에서_진행중() {
+        Frame frame = new FinalFrame();
+        frame = frame.update(FallenPins.of(7));
+
+        assertThat(frame.isFinish()).isFalse();
+    }
+
+}
