c2958f92816967644982049d3e03d56080b98b9d
yunhwan <pyh8618@gmail.com>
2022-11-13 02:45:47 +0900

    refactor : OutputView 출력 로직 개선
    HitRecord에 각 투구 당시의 상황에 따른 볼링친 갯수와 그에따른 볼링용어를 기록하도록 함

--- src/main/java/bowling/view/OutputView.java
+++ src/main/java/bowling/view/OutputView.java
@@ -9,56 +9,53 @@ public class OutputView {
     private static final String FORMAT_SHAPE = "  %-4s|";
     private static final String BONUS_SHAPE = "  %-6s|";
     private static final String SEPARATOR = "|";
-    private static final int MAX_HIT_COUNT = 10;
 
     private OutputView() {
     }
 
 
     public static void printScoreScreen(Username username, Frames frames) {
-        List<Frame> framesList = frames.getFrames();
         System.out.println(BASE_SCORE_SCREEN);
-        StringBuilder sb = new StringBuilder();
-        sb.append(String.format("|" + FORMAT_SHAPE, username.getName()));
-        for (int i = 0; i < framesList.size(); i++) {
-            Frame frame = framesList.get(i);
-            String hitShape = frameScore(frame.getHitRecords());
 
-            if (i == framesList.size() - 1) {
-                sb.append(String.format(BONUS_SHAPE, hitShape));
-                continue;
-            }
+        StringBuilder sb = new StringBuilder();
+        sb.append(makeUserFormat(username));
+
+        List<Frame> framesList = frames.getFrames();
+        String hitShape = "";
+        for (int i = 0; i < framesList.size() - 1; i++) {
+            Frame frame = framesList.get(i);
+            hitShape = makeFrameScoreShape(frame.getHitRecords());
             sb.append(String.format(FORMAT_SHAPE, hitShape));
         }
+        sb.append(String.format(BONUS_SHAPE, hitShape));
+
+        sb.append("\n");
         System.out.println(sb);
-        System.out.println();
     }
 
-    private static String frameScore(List<HitRecord> hitRecords) {
+    private static String makeUserFormat(Username username) {
+        return String.format(SEPARATOR + FORMAT_SHAPE, username.getName());
+    }
+
+    private static String makeFrameScoreShape(List<HitRecord> hitRecords) {
         StringBuilder hitBuilder = new StringBuilder();
-        int hitCount = 0;
-        for (int index = 0; index < hitRecords.size(); index++) {
+        HitRecord hitRecordFirst = hitRecords.get(0);
+        hitBuilder.append(retrieveHitShape(hitRecordFirst));
+
+        for (int index = 1; index < hitRecords.size(); index++) {
             HitRecord hitRecord = hitRecords.get(index);
-            hitCount += hitRecord.getHitCount();
-            if (index != 0) {
-                hitBuilder.append(SEPARATOR);
-                if (hitCount == MAX_HIT_COUNT) {
-                    hitBuilder.append(BowilingTerm.SPARE.shape());
-                    continue;
-                }
-            }
-
-            if (index != 1 && hitRecord.hitAll()) {
-                hitBuilder.append(BowilingTerm.STRIKE.shape());
-                continue;
-            }
-
-            if (hitRecord.hitZero()) {
-                hitBuilder.append(BowilingTerm.GUTTER.shape());
-                continue;
-            }
-            hitBuilder.append(hitRecord.getHitCount());
+            hitBuilder.append(SEPARATOR);
+            hitBuilder.append(retrieveHitShape(hitRecord));
         }
+
         return hitBuilder.toString();
+    }
+
+    private static String retrieveHitShape(HitRecord hitRecord) {
+        BowilingTerm bowilingTerm = hitRecord.getBowilingTerm();
+        if (bowilingTerm.equals(BowilingTerm.MISS)) {
+            return String.valueOf(hitRecord.getHitCount());
+        }
+        return bowilingTerm.shape();
     }
 }
