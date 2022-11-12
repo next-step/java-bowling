03255b9c7f2de698c4ee2a3b68de4cf65c1aff6c
ChaeHyeon <cohys7@gmail.com>
2022-11-13 01:24:14 +0900

    refactor: 상수화

--- src/main/java/bowling/domain/FrameResult.java
+++ src/main/java/bowling/domain/FrameResult.java
@@ -7,10 +7,10 @@ import java.util.Arrays;
 import java.util.function.Function;
 
 public enum FrameResult {
-    STRIKE(1, (score) -> score.equals(new Score(10))),
-    SPARE(2, (score) -> score.equals(new Score(10))),
-    MISS(2, (score) -> !score.equals(new Score(0))),
-    GUTTER(2, (score) -> score.equals(new Score(0)));
+    STRIKE(1, (score) -> score.equals(Constants.TEN)),
+    SPARE(2, (score) -> score.equals(Constants.TEN)),
+    MISS(2, (score) -> !score.equals(Constants.ZERO)),
+    GUTTER(2, (score) -> score.equals(Constants.ZERO));
 
     private final int count;
     private final Function<Score, Boolean> isInRange;
@@ -29,5 +29,10 @@ public enum FrameResult {
 
     private static boolean isMatch(Scores scores, FrameResult result) {
         return result.count == scores.size() && result.isInRange.apply(scores.sum());
+    }
+
+    private static class Constants {
+        public static final Score TEN = new Score(10);
+        public static final Score ZERO = new Score(0);
     }
 }
