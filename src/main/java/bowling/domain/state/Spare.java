bdc77894f51d16e6b52c437b174e5290da0fc148
michol.lee <lovethefeel@gmail.com>
2022-11-12 22:37:09 +0900

    feat: 상태 관리 객체

---
+++ src/main/java/bowling/domain/state/Spare.java
@@ -0,0 +1,54 @@
+package bowling.domain.state;
+
+import bowling.domain.Pin;
+import bowling.domain.Score;
+
+import java.util.List;
+
+public class Spare extends State {
+
+    private final Pin firstPin;
+    private final Pin secondPin;
+
+    public Spare(Pin firstPin, Pin secondPin) {
+
+        this.firstPin = firstPin;
+        this.secondPin = secondPin;
+    }
+
+    @Override
+    public State bowl(Pin pin) {
+
+        throw new IllegalArgumentException("더 이상 공을 던질 수 없습니다.");
+    }
+
+    @Override
+    public boolean isFinished() {
+
+        return true;
+    }
+
+    @Override
+    public Score getScore() {
+
+        return new Score(firstPin.count() + secondPin.count(), 1);
+    }
+
+    @Override
+    public Score calculateAdditionalScore(Score scoreV2) {
+
+        Score score = scoreV2.add(firstPin.count());
+
+        if (score.canCalculateScore()) {
+            return score;
+        }
+
+        return score.add(secondPin.count());
+    }
+
+    @Override
+    public List<Pin> pins() {
+
+        return List.of(firstPin, secondPin);
+    }
+}
