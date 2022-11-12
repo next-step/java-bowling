8b198f8f4c786cf3343f9b55315db81f2440e2b2
michol.lee <lovethefeel@gmail.com>
2022-11-12 22:46:14 +0900

    refactor(test): 볼링 핀, 프레임, 점수 테스트

--- src/test/java/bowling/domain/LastFrameTest.java
+++ src/test/java/bowling/domain/LastFrameTest.java
@@ -1,35 +1,100 @@
 package bowling.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+@DisplayName("마지막 프레임 테스트")
 class LastFrameTest {
 
-    @Test
-    void last_frame() {
+    private Frame frame;
 
-        assertThatThrownBy(() -> new LastFrame(11))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("마지막 프레임의 번호를 초과하였습니다.");
+    @BeforeEach
+    void setUp() {
+
+        frame = LastFrame.ready();
     }
 
-    @DisplayName("마지막 프레임에서 스패어 처리를 한다.")
+    @DisplayName("스트라이크인 경우 한 번 더 투구할 수 있다.")
     @Test
-    void create_frame() {
+    void fitch_strike() {
 
-        final LastFrame lastFrame = new LastFrame(1);
-        lastFrame.pitch(1);
-        lastFrame.pitch(9);
+        frame.bowl(10);
 
-        assertAll(
-                () -> assertThat(lastFrame.isEmpty()).isFalse(),
-                () -> assertThat(lastFrame.canPitch()).isFalse(),
-                () -> assertThat(lastFrame.status()).isEqualTo(ScoreType.SPARE),
-                () -> assertThat(lastFrame.pinsSize()).isEqualTo(2)
-        );
+        assertThat(frame.canBowl()).isTrue();
+    }
+
+    @DisplayName("스패어인 경우 한 번 더 투구할 수 있다.")
+    @Test
+    void fitch_spare() {
+
+        frame.bowl(7);
+        frame.bowl(3);
+
+        assertDoesNotThrow(() -> frame.bowl(5));
+    }
+
+    @DisplayName("투구가 끝나지 않은 상황에서는 프레임은 끝났다고 판단하지 않는다.")
+    @Test
+    void end_frame() {
+
+        frame.bowl(5);
+
+        assertThat(frame.isFinished()).isFalse();
+    }
+
+    @DisplayName("투구가 끝나면 프레임은 끝났다고 판단한다.")
+    @Test
+    void end_frame2() {
+
+        frame.bowl(5);
+        frame.bowl(3);
+
+        assertThat(frame.isFinished()).isTrue();
+    }
+
+    @DisplayName("10|0 인 경우 더 투구할 수 있다.")
+    @Test
+    void end_frame3() {
+
+        frame.bowl(10);
+        frame.bowl(0);
+
+        assertThat(frame.isFinished()).isFalse();
+    }
+
+    @DisplayName("10|0|9 인 경우 끝이다.")
+    @Test
+    void end_frame4() {
+
+        frame.bowl(10);
+        frame.bowl(0);
+        frame.bowl(9);
+
+        assertThat(frame.isFinished()).isTrue();
+    }
+
+    @DisplayName("5|3 인 경우 끝이다.")
+    @Test
+    void miss_frame() {
+
+        frame.bowl(5);
+        frame.bowl(3);
+
+        assertThat(frame.canBowl()).isFalse();
+    }
+
+    @DisplayName("10|10|10 인 경우 끝이다.")
+    @Test
+    void end_Frame() {
+
+        frame.bowl(10);
+        frame.bowl(10);
+        frame.bowl(10);
+
+        assertThat(frame.isFinished()).isTrue();
     }
 }
--- src/test/java/bowling/domain/NormalFrameTest.java
+++ src/test/java/bowling/domain/NormalFrameTest.java
@@ -1,55 +1,144 @@
 package bowling.domain;
 
+import bowling.domain.state.Strike;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("일반 프레임 테스트")
 class NormalFrameTest {
 
-    @DisplayName("1에서 9사이의 frame만 생성 가능하다.")
-    @ParameterizedTest
+    private Frame frame;
+
+    @BeforeEach
+    void setUp() {
+
+        frame = NormalFrame.ready();
+    }
+
+    @DisplayName("스트라이크인 경우 한 번 더 투구할 수 없다.")
+    @Test
+    void strike_bowl() {
+
+        frame.bowl(10);
+
+        assertThat(frame.canBowl()).isFalse();
+    }
+
+    @DisplayName("스패어 상태에서 한 번 더 투구할 수 없다.")
+    @Test
+    void spare_bowl() {
+
+        frame.bowl(7);
+        frame.bowl(3);
+
+        assertThat(frame.canBowl()).isFalse();
+    }
+
+    @DisplayName("투구가 끝난 상황에서 한 번 더 투구할 수 없다.")
+    @Test
+    void pitch_end() {
+
+        frame.bowl(5);
+        frame.bowl(3);
+
+        assertThat(frame.canBowl()).isFalse();
+    }
+
+    @DisplayName("프레임의 번호는 0번 또는 10번이 될 수 없다.")
+    @ParameterizedTest(name = "{displayName} 입력값={0}")
     @ValueSource(ints = {0, 10})
-    void normal_frame(final int number) {
-
-        assertThatThrownBy(() -> new NormalFrame(number))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("사이의 숫자만 가능합니다.");
+    void normalFrameException(int input) {
+        assertThatThrownBy(() -> NormalFrame.next(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("노멀 프레임에서 스패어 처리를 한다.")
+    @DisplayName("10개를 쓰러뜨린 경우 프레임의 상태는 스트라이크다.")
     @Test
-    void create_frame() {
+    void state() {
 
-        final NormalFrame normalFrame = new NormalFrame(1);
-        normalFrame.pitch(1);
-        normalFrame.pitch(9);
+        frame.bowl(10);
 
-        assertAll(
-                () -> assertThat(normalFrame.isEmpty()).isFalse(),
-                () -> assertThat(normalFrame.canPitch()).isFalse(),
-                () -> assertThat(normalFrame.status()).isEqualTo(ScoreType.SPARE),
-                () -> assertThat(normalFrame.pinsSize()).isEqualTo(2)
-        );
+        assertThat(frame.getState()).isInstanceOf(Strike.class);
     }
 
-    @DisplayName("다음 프레임를 생성한다.")
+    @DisplayName("Strike 인 경우 다음 프레임이 생성된다.")
     @Test
-    void next_frame() {
+    void strike_next_frame() {
 
-        final NormalFrame normalFrame = new NormalFrame(1);
-        normalFrame.pitch(1);
-        normalFrame.pitch(9);
-        final Frame nextFrame = normalFrame.nextFrame();
+        frame.bowl(10);
+
+        final Frame nextFrame = frame.getNextFrame();
+
+        assertThat(nextFrame).isNotNull();
+    }
+
+    @DisplayName("Spare 인 경우 다음 프레임이 생성된다.")
+    @Test
+    void spare_next_frame() {
+
+        frame.bowl(0);
+        frame.bowl(10);
+
+        final Frame nextFrame = frame.getNextFrame();
+
+        assertThat(nextFrame).isNotNull();
+    }
+
+    @DisplayName("Miss 인 경우 다음 프레임이 생성된다.")
+    @Test
+    void miss_next_frame() {
+
+        frame.bowl(0);
+        frame.bowl(10);
+
+        final Frame nextFrame = frame.getNextFrame();
+
+        assertThat(nextFrame).isNotNull();
+    }
+
+    @ParameterizedTest(name = "[MISS] 첫 번째 프레임에서 {0}|{1} 인 경우 점수는 {2}이다.")
+    @CsvSource({"0,0,0", "1,2,3", "0,9,9"})
+    void missScore(final int first,final int second, final int expected) {
+
+        frame.bowl(first);
+        frame.bowl(second);
+
+        assertThat(frame.getIntScore()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "첫 번째 프레임에서 Strike 이고 다음 Spare {0}|{1} 인 경우 첫 번째 프레임의 점수는 {2}이다.")
+    @CsvSource({"8,2,20"})
+    void score_calc(final int first, final int second, final int expected) {
+
+        frame.bowl(10);
+        final Frame nextFrame = frame.getNextFrame();
+        nextFrame.bowl(first);
+        nextFrame.bowl(second);
+
+        assertThat(frame.getIntScore()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "첫 번째 프레임에서 Strike 이고 다음 프레임이 Miss {0}|{1} 인 경우 첫 번째 프레임의 점수는 {2}이고 두 번째 프레임의 점수는 {3}이다.")
+    @CsvSource({"0,5,15,5", "1,2,13,3", "0,9,19,9"})
+    void score_calc2(final int first, final int second, final int firstExpected, final int secondExpected) {
+
+        frame.bowl(10);
+        final Frame nextFrame = frame.getNextFrame();
+        nextFrame.bowl(first);
+        nextFrame.bowl(second);
 
         assertAll(
-                () -> assertThat(nextFrame.isEmpty()).isTrue(),
-                () -> assertThat(nextFrame.canPitch()).isTrue(),
-                () -> assertThat(nextFrame.pinsSize()).isEqualTo(0)
+                () -> assertEquals(firstExpected, frame.getIntScore()),
+                () -> assertEquals(secondExpected, nextFrame.getIntScore())
         );
     }
 }
