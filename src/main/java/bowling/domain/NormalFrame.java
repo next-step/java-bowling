9460a1069b999a4c6c1e7b695460facecf8079ad
michol.lee <lovethefeel@gmail.com>
2022-11-12 22:39:15 +0900

    refactor: 인터페이스 수정

--- src/main/java/bowling/domain/NormalFrame.java
+++ src/main/java/bowling/domain/NormalFrame.java
@@ -1,22 +1,35 @@
 package bowling.domain;
 
+import bowling.domain.state.Ready;
+import bowling.domain.state.State;
+
+import java.util.List;
+
 public class NormalFrame implements Frame {
 
+    private static final int NO_SCORE = -1;
     private static final int MIN_FRAME_NUMBER = 1;
     private static final int MAX_FRAME_NUMBER = 9;
+
+    private State state = new Ready();
+    private Score score = state.getScore();
     private final int number;
-    private final Score score;
+    private Frame nextFrame;
 
-    public NormalFrame() {
-
-        this(1);
-    }
-
-    public NormalFrame(final int number) {
+    private NormalFrame(final int number) {
 
         checkSize(number);
         this.number = number;
-        this.score = new Score();
+    }
+
+    public static NormalFrame ready() {
+
+        return new NormalFrame(MIN_FRAME_NUMBER);
+    }
+
+    public static NormalFrame next(final int number) {
+
+        return new NormalFrame(number);
     }
 
     private void checkSize(final int number) {
@@ -26,55 +39,103 @@ public class NormalFrame implements Frame {
         }
     }
 
-    @Override
-    public void pitch(final int number) {
+    private void validateNextFrame() {
 
-        score.add(new Pin(number));
+        if (nextFrame == null) {
+            throw new IllegalArgumentException("점수를 계산할 수 없습니다.");
+        }
     }
 
     @Override
-    public int getNumber() {
+    public int getFrameNumber() {
 
         return number;
     }
 
     @Override
-    public boolean isEmpty() {
+    public boolean isLastFrame() {
 
-        return score.pins().isEmpty();
+        return false;
     }
 
     @Override
-    public int pinNumber(int index) {
+    public boolean canBowl() {
 
-        return score.getPin(index);
+        return !state.isFinished();
     }
 
     @Override
-    public int pinsSize() {
+    public void bowl(final int number) {
 
-        return score.pins().size();
-    }
+        state = state.bowl(new Pin(number));
 
-    @Override
-    public boolean canPitch() {
-
-        return !score.frameEnd() && !status().strikeOrSpare();
+        if (state.isFinished()) {
+            score = state.getScore();
+            nextFrame = nextFrame();
+        }
     }
 
     @Override
     public Frame nextFrame() {
 
         if (number == MAX_FRAME_NUMBER) {
-            return new LastFrame(number + 1);
+            nextFrame = LastFrame.ready();
+            return nextFrame;
         }
 
-        return new NormalFrame(number + 1);
+        nextFrame = new NormalFrame(number + 1);
+        return nextFrame;
     }
 
     @Override
-    public ScoreType status() {
+    public int getIntScore() {
 
-        return score.status();
+        if (score.canCalculateScore()) {
+            return score.getScore();
+        }
+
+        try {
+            validateNextFrame();
+            score = nextFrame.calculateAdditionalScore(score);
+            return score.getScore();
+        } catch (IllegalArgumentException e) {
+            return NO_SCORE;
+        }
+    }
+
+    @Override
+    public Score calculateAdditionalScore(final Score beforeScore) {
+
+       final Score score = state.calculateAdditionalScore(beforeScore);
+
+        if (!score.canCalculateScore()) {
+            validateNextFrame();
+            return nextFrame.calculateAdditionalScore(score);
+        }
+
+        return score;
+    }
+
+    @Override
+    public State getState() {
+
+        return state;
+    }
+
+    @Override
+    public List<State> getStates() {
+
+        return List.of(state);
+    }
+
+    @Override
+    public boolean isFinished() {
+
+        return !canBowl();
+    }
+
+    @Override
+    public Frame getNextFrame() {
+        return nextFrame;
     }
 }
