a54344a4dd4d87865ea093dc49c45c00a5ec4755
bada.kang <kangbada0728@gmail.com>
2022-11-12 20:23:21 +0900

    refactor: 필드명을 축약하지 않고 표현
    https://github.com/next-step/java-bowling/pull/957#discussion_r1020679945

--- src/main/java/bowling/InputView.java
+++ src/main/java/bowling/InputView.java
@@ -3,16 +3,16 @@ package bowling;
 import java.util.Scanner;
 
 public class InputView {
-    private static Scanner sc = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
     public static String inputPlayerName() {
         System.out.print("플레이어 이름은(3 english letters)?: ");
-        return sc.nextLine();
+        return scanner.nextLine();
     }
 
     public static int inputKnockDownCount(int frameOrderToThrow) {
         System.out.printf("\n%d프레임 투구 : ", frameOrderToThrow);
-        return Integer.parseInt(sc.nextLine());
+        return Integer.parseInt(scanner.nextLine());
     }
 
 
