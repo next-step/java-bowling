0cb3f7a83eec1ae4c092660327361a1196417400
OhSeungCheol <sc.oh131@gmail.com>
2022-11-14 00:14:39 +0900

    [M4] refactor: Frames 일급 컬렉션 추가
    참고: https://github.com/next-step/java-bowling/pull/933

---
+++ src/main/java/bowling/domain/Frames.java
@@ -0,0 +1,175 @@
+package bowling.domain;
+
+import bowling.exception.NotReadyException;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Frames {
+    private final List<Frame> frames = new ArrayList<>();
+
+    public List<Frame> getFrames() {
+        return frames;
+    }
+
+    public void record(int downPintCount) {
+        Frame readyFrame = getReadyFrame();
+        readyFrame.record(downPintCount);
+    }
+
+    public boolean isEndFrame(int frameIndex) {
+        if (frames.size() <= frameIndex) {
+            return false;
+        }
+        return frames.get(frameIndex).isEndFrame();
+    }
+
+    public int getRecordCount() {
+        return frames.size();
+    }
+
+
+    public boolean isEndRecord() {
+        return isLastFrame() && isEndLastFrame();
+    }
+
+    public boolean isEndLastFrame() {
+        return getLatestFrame().isEndFrame();
+    }
+
+    public int getTotalScore() {
+        return getTotalScore(frames.size() - 1);
+    }
+
+    public int getTotalScore(int targetIndex) {
+        int totalScore = 0;
+        for (int frameIndex = 0; frameIndex <= targetIndex; frameIndex++) {
+            totalScore += getFrameScore(frameIndex);
+        }
+        return totalScore;
+    }
+
+    public boolean isReadyFrameScore(int frameIndex) {
+        if (isValidFrameIndex(frameIndex)) {
+            return false;
+        }
+        Frame frame = frames.get(frameIndex);
+        if (!frame.isEndFrame()) {
+            return false;
+        }
+        if (frame.isValidBonusScore()) {
+            return isReadyBonusScore(frameIndex);
+        }
+        return true;
+    }
+
+    private Frame getReadyFrame() {
+        Frame frame = getLatestFrame();
+        if (frame.isEndFrame()) {
+            frame = getNewFrame();
+            frames.add(frame);
+        }
+        return frame;
+    }
+
+    private boolean isValidFrameIndex(int frameIndex) {
+        return frames.size() <= frameIndex;
+    }
+
+    private boolean isReadyBonusScore(int frameIndex) {
+        Frame frame = frames.get(frameIndex);
+        if (frame.isStrike()) {
+            return isReadyTwoPitchScore(frameIndex + 1);
+        }
+        if (frame.isSpare()) {
+            return isReadyOnePitchScore(frameIndex + 1);
+        }
+        return true;
+    }
+
+    private boolean isReadyOnePitchScore(int frameIndex) {
+        if (isValidFrameIndex(frameIndex)) {
+            return false;
+        }
+        Frame frame = frames.get(frameIndex);
+        return frame.getTryCount() >= 1;
+    }
+
+    private boolean isReadyTwoPitchScore(int frameIndex) {
+        if (isValidFrameIndex(frameIndex)) {
+            return false;
+        }
+        Frame frame = frames.get(frameIndex);
+
+        if (frame.isValidBonusScore() && frame.isStrike()) {
+            return isReadyOnePitchScore(frameIndex + 1);
+        }
+        return frame.getTryCount() >= 2;
+    }
+
+    private int getFrameScore(int index) {
+        checkValidFrameIndex(index);
+        Frame frame = frames.get(index);
+        return frame.getPinScore() + getBonusScore(index);
+    }
+
+    private int getBonusScore(int index) {
+        checkValidFrameIndex(index);
+        Frame frame = frames.get(index);
+        if (!frame.isValidBonusScore()) {
+            return 0;
+        }
+        if (frame.isStrike()) {
+            return getStrikeBonusScore(index + 1);
+        }
+        if (frame.isSpare()) {
+            return getSpareBonusScore(index + 1);
+        }
+        return 0;
+    }
+
+    private int getStrikeBonusScore(int index) {
+        checkValidFrameIndex(index);
+        Frame frame = frames.get(index);
+
+        int bonusScore = 0;
+        bonusScore += getSpareBonusScore(index);
+        if (frame.isValidBonusScore() && frame.isStrike()) {
+            return bonusScore + getSpareBonusScore(index + 1);
+        }
+        return bonusScore + frame.getSecondPitchScore();
+    }
+
+    private int getSpareBonusScore(int index) {
+        checkValidFrameIndex(index);
+        Frame frame = frames.get(index);
+        return frame.getFirstPitchScore();
+    }
+
+    private void checkValidFrameIndex(int frameIndex) {
+        if (frameIndex >= frames.size()) {
+            throw new NotReadyException("frameIndex must more less than frame size");
+        }
+    }
+
+    private Frame getNewFrame() {
+        if (frames.size() == RuleConfig.NUMBER_OF_FRAME - 1) {
+            return new FinalFrame();
+        }
+        return new Frame();
+    }
+
+    private Frame getLatestFrame() {
+        if (CollectionUtils.isEmpty(frames)) {
+            Frame frame = new Frame();
+            frames.add(frame);
+            return frame;
+        }
+        return frames.get(frames.size() - 1);
+    }
+
+    private boolean isLastFrame() {
+        return getRecordCount() >= RuleConfig.NUMBER_OF_FRAME;
+    }
+}
--- src/main/java/bowling/domain/Record.java
+++ src/main/java/bowling/domain/Record.java
@@ -1,184 +1,53 @@
 package bowling.domain;
 
-import bowling.exception.NotReadyException;
-import org.springframework.util.CollectionUtils;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class Record {
-    private final List<Frame> frames = new ArrayList<>();
+    private final Frames frames;
     private final Player player;
 
     public Record(Player player) {
+        this.frames = new Frames();
         this.player = player;
-    }
-
-    public List<Frame> getFrames() {
-        return frames;
     }
 
     public String getPlayerName() {
         return player.getName();
     }
 
+    public void record(int downPintCount) {
+        frames.record(downPintCount);
+    }
+
+    public List<Frame> getFrames() {
+        return frames.getFrames();
+    }
+
     public boolean isEndFrame(int frameIndex) {
-        if (frames.size() <= frameIndex) {
-            return false;
-        }
-        return frames.get(frameIndex).isEndFrame();
+        return frames.isEndFrame(frameIndex);
     }
 
     public int getRecordCount() {
-        return frames.size();
-    }
-
-    public void record(int downPintCount) {
-        Frame readyFrame = getReadyFrame();
-        readyFrame.record(downPintCount);
-    }
-
-    public boolean isLastFrame() {
-        return getRecordCount() >= RuleConfig.NUMBER_OF_FRAME;
-    }
-
-    public boolean isEndRecord() {
-        return isLastFrame() && isEndLastFrame();
+        return frames.getRecordCount();
     }
 
     public boolean isEndLastFrame() {
-        return getLatestFrame().isEndFrame();
+        return frames.isEndLastFrame();
     }
 
     public int getTotalScore() {
-        return getTotalScore(frames.size() - 1);
+        return frames.getTotalScore();
     }
 
     public int getTotalScore(int targetIndex) {
-        int totalScore = 0;
-        for (int frameIndex = 0; frameIndex <= targetIndex; frameIndex++) {
-            totalScore += getFrameScore(frameIndex);
-        }
-        return totalScore;
+        return frames.getTotalScore(targetIndex);
     }
 
     public boolean isReadyFrameScore(int frameIndex) {
-        if (isValidFrameIndex(frameIndex)) {
-            return false;
-        }
-        Frame frame = frames.get(frameIndex);
-        if (!frame.isEndFrame()) {
-            return false;
-        }
-        if (frame.isValidBonusScore()) {
-            return isReadyBonusScore(frameIndex);
-        }
-        return true;
+        return frames.isReadyFrameScore(frameIndex);
     }
 
-    private boolean isValidFrameIndex(int frameIndex) {
-        return frames.size() <= frameIndex;
+    public boolean isEndRecord() {
+        return frames.isEndRecord();
     }
-
-    private boolean isReadyBonusScore(int frameIndex) {
-        Frame frame = frames.get(frameIndex);
-        if (frame.isStrike()) {
-            return isReadyTwoPitchScore(frameIndex + 1);
-        }
-        if (frame.isSpare()) {
-            return isReadyOnePitchScore(frameIndex + 1);
-        }
-        return true;
-    }
-
-    private boolean isReadyOnePitchScore(int frameIndex) {
-        if (isValidFrameIndex(frameIndex)) {
-            return false;
-        }
-        Frame frame = frames.get(frameIndex);
-        return frame.getTryCount() >= 1;
-    }
-
-    private boolean isReadyTwoPitchScore(int frameIndex) {
-        if (isValidFrameIndex(frameIndex)) {
-            return false;
-        }
-        Frame frame = frames.get(frameIndex);
-
-        if (frame.isValidBonusScore() && frame.isStrike()) {
-            return isReadyOnePitchScore(frameIndex + 1);
-        }
-        return frame.getTryCount() >= 2;
-    }
-
-    private int getFrameScore(int index) {
-        checkValidFrameIndex(index);
-        Frame frame = frames.get(index);
-        return frame.getPinScore() + getBonusScore(index);
-    }
-
-    private int getBonusScore(int index) {
-        checkValidFrameIndex(index);
-        Frame frame = frames.get(index);
-        if (!frame.isValidBonusScore()) {
-            return 0;
-        }
-        if (frame.isStrike()) {
-            return getStrikeBonusScore(index + 1);
-        }
-        if (frame.isSpare()) {
-            return getSpareBonusScore(index + 1);
-        }
-        return 0;
-    }
-
-    private int getStrikeBonusScore(int index) {
-        checkValidFrameIndex(index);
-        Frame frame = frames.get(index);
-
-        int bonusScore = 0;
-        bonusScore += getSpareBonusScore(index);
-        if (frame.isValidBonusScore() && frame.isStrike()) {
-            return bonusScore + getSpareBonusScore(index + 1);
-        }
-        return bonusScore + frame.getSecondPitchScore();
-    }
-
-    private int getSpareBonusScore(int index) {
-        checkValidFrameIndex(index);
-        Frame frame = frames.get(index);
-        return frame.getFirstPitchScore();
-    }
-
-    private void checkValidFrameIndex(int frameIndex) {
-        if (frameIndex >= frames.size()) {
-            throw new NotReadyException("frameIndex must more less than frame size");
-        }
-    }
-
-    private Frame getReadyFrame() {
-        Frame frame = getLatestFrame();
-        if (frame.isEndFrame()) {
-            frame = getNewFrame();
-            frames.add(frame);
-        }
-        return frame;
-    }
-
-    private Frame getNewFrame() {
-        if (frames.size() == RuleConfig.NUMBER_OF_FRAME - 1) {
-            return new FinalFrame();
-        }
-        return new Frame();
-    }
-
-    private Frame getLatestFrame() {
-        if (CollectionUtils.isEmpty(frames)) {
-            Frame frame = new Frame();
-            frames.add(frame);
-            return frame;
-        }
-        return frames.get(frames.size() - 1);
-    }
-
 }
