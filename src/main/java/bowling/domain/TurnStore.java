216ff4eef4e2e0ec30bb22eaa9164c6cbccb1252
OhSeungCheol <sc.oh131@gmail.com>
2022-11-14 00:23:49 +0900

    [M4] refactor: 불필요 메소드 삭제
    참고: https://github.com/next-step/java-bowling/pull/933

--- src/main/java/bowling/domain/TurnStore.java
+++ src/main/java/bowling/domain/TurnStore.java
@@ -14,34 +14,22 @@ public class TurnStore {
     }
 
     private void setNextTurn(Records records) {
-        if (isEndGame(records)) {
+        if (records.isEndRecords()) {
             return;
         }
 
-        if (isEndFrames(records)) {
+        if (records.isEndFrames(currentFrameIndex)) {
             setNextFrame();
         }
 
-        if (isEndPlayerFrame(records)) {
+        if (records.isEndPlayerFrame(currentFrameIndex, currentLaneIndex)) {
             setNextLane();
         }
-    }
-
-    private boolean isEndGame(Records records) {
-        return records.isEndRecords();
-    }
-
-    private boolean isEndFrames(Records records) {
-        return records.isEndFrames(currentFrameIndex);
     }
 
     private void setNextFrame() {
         currentLaneIndex = 0;
         currentFrameIndex++;
-    }
-
-    private boolean isEndPlayerFrame(Records records) {
-        return records.isEndPlayerFrame(currentFrameIndex, currentLaneIndex);
     }
 
     private void setNextLane() {
