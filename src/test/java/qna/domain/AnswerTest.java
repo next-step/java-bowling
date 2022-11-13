9d833808368f5fb32fa19eb44aeae733ae279048
lee <wnghkdcjdtns@naver.com>
2022-11-13 23:42:56 +0900

    -test 테스트 케이스 추가

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,27 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+
+    @Test
+    public void delete_답변_중_다른_사람이_쓴_글(){
+        assertThatThrownBy(() -> {
+            A1.isOwner(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    public void delete_성공_질문자_답변자_같음() throws Exception {
+        A1.isOwner(UserTest.JAVAJIGI);
+        assertThat(A1.isDeleted()).isTrue();
+    }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,33 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+
+    @Test
+    public void delete_성공() throws Exception {
+        assertThat(Q1.isDeleted()).isFalse();
+        Q1.questionDeleteHistories(UserTest.JAVAJIGI.getId());
+        assertThat(Q1.isDeleted()).isTrue();
+    }
+
+    @Test
+    public void delete_다른_사람이_쓴_글(){
+        assertThatThrownBy(() -> {
+            Q1.isOnwer(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    public void delete_성공_질문자_같음() throws Exception {
+        Q2.isOnwer(UserTest.SANJIGI);
+        assertThat(Q2.isDeleted()).isTrue();
+    }
 }
