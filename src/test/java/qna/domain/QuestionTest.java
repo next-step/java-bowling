43537522cd30c0c970ec143edea8b6dfc7201335
ellie-klm <ellie.klm@kakaocorp.com>
2022-11-13 19:00:04 +0900

    refactor: 테스트 코드 오류 수정

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -4,14 +4,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import qna.CannotDeleteException;
 
 public class QuestionTest {
-    public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-    public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+    public static Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+    public static Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @AfterEach
+    void clear() {
+       Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+       Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+    }
 
     @Test
+    @DisplayName("질문 삭제 성공")
     void testDeleteSuccess() throws CannotDeleteException {
         List<DeleteHistory> deleteHistories = Q1.deleteBy(UserTest.JAVAJIGI);
 
@@ -20,6 +29,7 @@ public class QuestionTest {
     }
 
     @Test
+    @DisplayName("다른 사람이 작성한 질문은 삭제할 수 없다.")
     void testDeleteFailByOtherUser() {
         assertThatThrownBy(() -> Q1. deleteBy(UserTest.SANJIGI))
             .isInstanceOf(CannotDeleteException.class)
@@ -27,6 +37,7 @@ public class QuestionTest {
     }
 
     @Test
+    @DisplayName("다른 사람이 쓴 답변이 존재하는 질문은 삭제할 수 없다.")
     void testDeleteFailByExistingOtherAnswers() {
         Q1.addAnswer(AnswerTest.A2);
         assertThatThrownBy(() -> Q1.deleteBy(UserTest.JAVAJIGI))
