03015cfafbab2d95b2ffbb029af150e83ec35619
ChaeHyeon <cohys7@gmail.com>
2022-11-13 13:26:49 +0900

    feat: 프레임 끝나면 새 프레임 추가하기

--- src/main/java/bowling/domain/Frames.java
+++ src/main/java/bowling/domain/Frames.java
@@ -1,6 +1,7 @@
 package bowling.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Frames {
@@ -11,11 +12,24 @@ public class Frames {
         frames.add(new Frame());
     }
 
+    public Frames(Frame... frames) {
+        this.frames = new ArrayList<>();
+        this.frames.addAll(Arrays.asList(frames));
+    }
+
     public List<Frame> getFrames() {
         return frames;
     }
 
     public void addScore(int index, Score score) {
         frames.get(index).addScore(score);
+    }
+
+    public boolean end(int index) {
+        boolean isEnd = this.frames.get(index).end();
+        if (isEnd) {
+            frames.add(new Frame());
+        }
+        return isEnd;
     }
 }
--- src/test/java/bowling/domain/FramesTest.java
+++ src/test/java/bowling/domain/FramesTest.java
@@ -16,4 +16,17 @@ public class FramesTest {
         frames.addScore(0, new Score(1));
         assertThat(frames.getFrames().get(0).getScores()).isEqualTo(new Scores(1));
     }
+
+    @Test
+    void 프레임_완료_전() {
+        Frames frames = new Frames();
+        assertThat(frames.end(0)).isFalse();
+    }
+
+    @Test
+    void 프레임_완료_후_추가() {
+        Frames frames = new Frames(new Frame(new Scores(1, 2)));
+        assertThat(frames.end(0)).isTrue();
+        assertThat(frames.getFrames()).hasSize(2);
+    }
 }
