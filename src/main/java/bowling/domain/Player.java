88977ea0ce6b8128c010f07a29fdbcd137bdec88
bada.kang <kangbada0728@gmail.com>
2022-11-13 20:21:21 +0900

    update: 정규식을 상수로 만들어 사용
    https://github.com/next-step/java-bowling/pull/957#discussion_r1020846263

--- src/main/java/bowling/domain/Player.java
+++ src/main/java/bowling/domain/Player.java
@@ -6,13 +6,16 @@ import bowling.dto.PlayerDto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Player {
+    private static final Pattern isAlphabeticThreeWord = Pattern.compile("^[a-zA-Z]{3}$");
+
     private final String name;
     private final Frame frame;
 
     public Player(String name) {
-        if (!name.matches("^[a-zA-Z]{3}$")) {
+        if (!isAlphabeticThreeWord.matcher(name).matches()) {
             throw new IllegalArgumentException("이름은 영문자로 세글자이어야 합니다.");
         }
         this.name = name;
