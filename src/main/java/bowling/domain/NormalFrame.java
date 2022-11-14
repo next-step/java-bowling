090709a5399c2eb0d61c44586033f78fbbb1043e
Jackson Hong <jacksonhong0111@gmail.com>
2022-11-14 17:00:03 +0900

    [refactor] 상수 추가

--- src/main/java/bowling/domain/NormalFrame.java
+++ src/main/java/bowling/domain/NormalFrame.java
@@ -1,12 +1,15 @@
 package bowling.domain;
 
 import static bowling.BowlingApp.getPinCalculateStrategy;
+import static bowling.domain.Frames.LAST_FRAME_ORDER;
 
 public class NormalFrame extends DefaultFrame {
 
+    public static final int MAX_SCORE_SIZE = 2;
+
     public NormalFrame(Scores scores) {
         super(scores);
-        if (scores.size() > 2) {
+        if (scores.size() > MAX_SCORE_SIZE) {
             throw new IllegalArgumentException("score size cannot be bigger than 2");
         }
     }
@@ -18,7 +21,7 @@ public class NormalFrame extends DefaultFrame {
     @Override
     public Frame nextRound() {
         int nextOrder = order + 1;
-        if (nextOrder == 10) {
+        if (nextOrder == LAST_FRAME_ORDER) {
             return new FinalFrame(new Scores(getPinCalculateStrategy()), nextOrder);
         }
         return new NormalFrame(new Scores(getPinCalculateStrategy()), nextOrder);
