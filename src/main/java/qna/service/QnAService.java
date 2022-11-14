f717dcaa1d92e141f945758cdc5fc43f70126649
hungryjayy <juwonson2068@naver.com>
2022-11-14 16:00:04 +0900

    refactor: Extract validate method to answer domain

--- src/main/java/qna/service/QnAService.java
+++ src/main/java/qna/service/QnAService.java
@@ -39,9 +39,7 @@ public class QnAService {
 
         List<Answer> answers = question.getAnswers();
         for (Answer answer : answers) {
-            if (!answer.isOwner(loginUser)) {
-                throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
-            }
+            answer.validateAnswerExists(loginUser);
         }
 
         List<DeleteHistory> deleteHistories = new ArrayList<>();
