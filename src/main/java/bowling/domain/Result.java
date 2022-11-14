627c05770c3fcfc647b0c162e67b21730107ee1a
seungwoo.song <seungwoo.song@nhnpayco.com>
2022-11-14 21:39:35 +0900

    refactor(Result.java): 원시타입 랩핑

--- src/main/java/bowling/domain/Result.java
+++ src/main/java/bowling/domain/Result.java
@@ -7,37 +7,45 @@ public enum Result {
     MISS;
 
     public static Result from(Frame frame, int count) {
+        return from(frame, PinCount.of(count));
+    }
+
+    public static Result from(Frame frame, PinCount pinCount) {
         int round = frame.size();
 
         if (round == 0 || round == 2) {
-            return Result.of(count);
+            return Result.of(pinCount);
         }
 
         if (round == 1) {
-            return Result.second(frame.getLastCount(), count);
+            return Result.second(frame.getLastCount(), pinCount);
         }
 
         throw new IllegalArgumentException("투구는 3회 이하여야 합니다.");
     }
 
-    private static Result of(int count) {
-        if (count == 10) {
+    private static Result of(PinCount pinCount) {
+        if (pinCount.isTen()) {
             return STRIKE;
         }
 
-        if (count == 0) {
+        if (pinCount.isZero()) {
             return GUTTER;
         }
 
         return MISS;
     }
 
-    private static Result second(int beforeCount, int currentCount) {
-        if (currentCount == 0) {
+    public static Result of(int count) {
+        return of(PinCount.of(count));
+    }
+
+    private static Result second(PinCount beforeCount, PinCount currentCount) {
+        if (currentCount.isZero()) {
             return GUTTER;
         }
 
-        if (beforeCount + currentCount == 10) {
+        if (currentCount.sum(beforeCount).isTen()) {
             return SPARE;
         }
 
