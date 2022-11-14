8c3b81c1923213b07519205b10fdcbbe94108db6
정종은 | Brian | JJ <with.beth@gmail.com>
2022-11-14 14:26:40 +0900

    🚀 1단계 - 질문 삭제하기 기능 리팩토링 (#960)
    * docs(README.md): add feature, programming requirements
    
    * docs(README.md): add TODO
    
    * feat(Answers): 1급 컬렉션 작성
    
    * feat(Answer): Answer Entity 삭제 기능 작성
    
    * feat(Question): Question Entity 삭제 기능 작성
    
    * docs(README.md): TODO 작성목록 갱신

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,43 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    private Answer answer;
+
+    @BeforeEach
+    void setUp() {
+        answer = new Answer(100L, UserTest.JAVAJIGI, QuestionTest.Q2, "JPA Answer");
+    }
+
+    @DisplayName("로그인 사용자와 답변한 사람이 다른 경우, 답변 삭제 불가능 해야 한다")
+    @Test
+    void delete_differentUser() {
+        assertThatExceptionOfType(CannotDeleteException.class)
+            .isThrownBy(() -> answer.delete(UserTest.SANJIGI))
+            .withMessageContaining("답변을 삭제할 권한이 없습니다");
+    }
+
+    @DisplayName("로그인 사용자와 답변한 사람이 같은 경우, 답변만 삭제 가능 해야 한다")
+    @Test
+    void delete_sameUser() throws CannotDeleteException {
+        assertThat(answer.isDeleted()).isFalse();
+
+        final DeleteHistory deleteHistory = answer.delete(UserTest.JAVAJIGI);
+        assertThat(answer.isDeleted()).isTrue();
+        assertThat(deleteHistory).isEqualTo(
+            new DeleteHistory(ContentType.ANSWER, 100L, UserTest.JAVAJIGI, LocalDateTime.now()));
+    }
+
+
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,66 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    private Question question;
+
+    @BeforeEach
+    void setUp() {
+        question = new Question(100L, "JPA", "JPA Content").writeBy(UserTest.JAVAJIGI);
+    }
+
+    @DisplayName("로그인 사용자와 질문한 사람이 다른 경우, 질문 삭제 불가능 해야 한다")
+    @Test
+    void delete_differentUser() {
+        assertThatExceptionOfType(CannotDeleteException.class)
+            .isThrownBy(() -> question.delete(UserTest.SANJIGI))
+            .withMessageContaining("질문을 삭제할 권한이 없습니다");
+    }
+
+    @DisplayName("로그인 사용자와 질문한 사람이 같은 경우, 질문 삭제 가능 해야 한다")
+    @Test
+    void delete_sameUser() throws CannotDeleteException {
+        assertThat(question.isDeleted()).isFalse();
+
+        final List<DeleteHistory> deleteHistories = question.delete(UserTest.JAVAJIGI);
+        assertThat(question.isDeleted()).isTrue();
+        assertThat(deleteHistories).containsExactly(
+            new DeleteHistory(ContentType.QUESTION, 100L, UserTest.JAVAJIGI, LocalDateTime.now()));
+    }
+
+    @DisplayName("다른 사람이 쓴 답변이 존재하는 경우, 질문 삭제 불가능 해야 한다")
+    @Test
+    void delete_containsOtherUserAnswers() {
+        question.addAnswer(new Answer(1L, UserTest.SANJIGI, question,"It's me again, but.."));
+
+        assertThatExceptionOfType(CannotDeleteException.class)
+            .isThrownBy(() -> question.delete(UserTest.JAVAJIGI))
+            .withMessageContaining("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다");
+    }
+
+    @DisplayName("본인 외에 다른 사람이 쓴 답변이 존재하지 않는 경우, 질문과 답변 모두 삭제 가능 해야 한다")
+    @Test
+    void delete_onlyContainsOwnAnswers() throws CannotDeleteException {
+        question.addAnswer(new Answer(1L, UserTest.JAVAJIGI, question,"It's me again, but.."));
+
+        final List<DeleteHistory> deleteHistories = question.delete(UserTest.JAVAJIGI);
+        assertThat(question.isDeleted()).isTrue();
+        assertThat(deleteHistories).containsExactly(
+            new DeleteHistory(ContentType.QUESTION, 100L, UserTest.JAVAJIGI, LocalDateTime.now()),
+            new DeleteHistory(ContentType.ANSWER, 1L, UserTest.JAVAJIGI, LocalDateTime.now()));
+    }
+
+
 }
