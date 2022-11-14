90429373f7771255d5802beefc59759553976c4e
hungryjayy <juwonson2068@naver.com>
2022-11-14 15:50:57 +0900

    test: Add question method

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,20 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    @DisplayName("다른 사람이 쓴 글의 경우 CannotDeleteException을 throw한다.")
+    public void validate_다른_사람이_쓴_글() {
+        assertThatThrownBy(() -> {
+            Q1.validateAnswerAuthority(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
 }
