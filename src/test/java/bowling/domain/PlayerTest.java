e77ba69344fa09e86616b7481df22f565c752a4e
devcs96 <katd6@naver.com>
2022-11-14 22:30:00 +0900

    :white_check_mark: test(domain) : player 결과값에 유저네임이 존재하는지 테스트

--- src/test/java/bowling/domain/PlayerTest.java
+++ src/test/java/bowling/domain/PlayerTest.java
@@ -2,6 +2,7 @@ package bowling.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 class PlayerTest {
@@ -9,6 +10,15 @@ class PlayerTest {
     @Test
     void shouldValidatePlayer() {
         assertThrowsExactly(IllegalArgumentException.class, () -> new Player(null, null));
+    }
+
+    @Test
+    void shouldReturnResultWithUsername() {
+        Player player = new Player(new Username("kcs"), new Bowling());
+
+        ScoreResult result = player.bowl(10);
+
+        assertThat(result.isSameUsername(new Username("kcs"))).isTrue();
     }
 
 }
