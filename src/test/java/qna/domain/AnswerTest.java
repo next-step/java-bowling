9331c1a1727b0076ff12b48a9add40bf0d378519
Myunghwan Kim <samkim2626@gmail.com>
2022-11-14 21:04:39 +0900

    refactor: Answer와 Question로 delete 역할 위임, 히스토리 반환하도록 수정

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,28 @@
 package qna.domain;
 
-public class AnswerTest {
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @DisplayName("답변자가 로그인 사용자와 같을 경우 정상 삭제")
+    @Test
+    void deleteTest() throws Exception {
+        A1.delete(UserTest.JAVAJIGI);
+
+        assertThat(A1.isDeleted()).isTrue();
+    }
+
+    @DisplayName("답변자가 로그인 사용자와 다를 경우 예외 발생")
+    @Test
+    void cannotDeleteTest() {
+        assertThatThrownBy(() -> A1.delete(UserTest.SANJIGI))
+            .isExactlyInstanceOf(CannotDeleteException.class)
+            .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,28 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @DisplayName("질문 작성자가 로그인 사용자와 같을 경우 삭제")
+    @Test
+    void deleteTest() throws Exception {
+        Q1.delete(UserTest.JAVAJIGI);
+
+        assertThat(Q1.isDeleted()).isTrue();
+    }
+
+    @DisplayName("질문 작성자가 로그인 사용자와 다를 경우 예외 발생")
+    @Test
+    void cannotDeleteTest() {
+        assertThatThrownBy(() -> Q1.delete(UserTest.SANJIGI))
+            .isExactlyInstanceOf(CannotDeleteException.class)
+            .hasMessage("질문을 삭제할 권한이 없습니다.");
+    }
 }
