e8103d4f45308d11f5d4e27e36c715bd35044aab
hungryjayy <juwonson2068@naver.com>
2022-11-14 16:02:37 +0900

    test: Add question validate answer exists test

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -15,6 +15,16 @@ public class QuestionTest {
     public void validate_다른_사람이_쓴_글() {
         assertThatThrownBy(() -> {
             Q1.validateQuestionAuthority(UserTest.SANJIGI);
-        }).isInstanceOf(CannotDeleteException.class);
+        }).isInstanceOf(CannotDeleteException.class)
+                .hasMessageContaining("질문을 삭제할 권한이 없습니다.");
+    }
+
+    @Test
+    @DisplayName("다른 사람이 쓴 답변이 있는 경우 CannotDeleteException을 throw한다.")
+    public void validate_다른_사람의_답변_있는_글() {
+        assertThatThrownBy(() -> {
+            Q1.validateAnswerExists(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class)
+                .hasMessageContaining("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
     }
 }
