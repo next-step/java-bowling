f6e62d9ece21235470a28f8bbef27b6fd13276f6
kingkk31 <kingkk31@naver.com>
2022-11-15 18:12:01 +0900

    feat: input & output 용 view 추가

---
+++ src/main/java/bowling/view/InputView.java
@@ -0,0 +1,40 @@
+package bowling.view;
+
+import bowling.FallenPins;
+import bowling.Player;
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private InputView() {
+
+    }
+
+    public static Player getPlayer() {
+        System.out.print("플레이어 이름은(3 english letters)?: ");
+        return new Player(scanner.nextLine());
+    }
+
+    public static FallenPins getFallenPins(int numberOfFrame) {
+        FallenPins fallenPins = null;
+        while (fallenPins == null) {
+            System.out.printf("%d프레임 투구 : ", numberOfFrame);
+            fallenPins = getFallenPins();
+        }
+        return fallenPins;
+    }
+
+    private static FallenPins getFallenPins() {
+        try {
+            String numberOfFallenPin = scanner.nextLine();
+            return FallenPins.of(Integer.parseInt(numberOfFallenPin));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("입력값을 확인하고 다시 입력해주세요.");
+            return null;
+        }
+    }
+
+}
---
+++ src/main/java/bowling/view/ResultView.java
@@ -0,0 +1,42 @@
+package bowling.view;
+
+import bowling.Player;
+import bowling.frame.BowlingFrames;
+
+public class ResultView {
+
+    private static final int WIDTH = 6;
+
+    private ResultView() {
+
+    }
+
+    public static void printFrame(BowlingFrames bowlingFrames, Player player) {
+        printHeader(bowlingFrames.getSize());
+        printRow(bowlingFrames, player);
+    }
+
+    private static void printRow(BowlingFrames bowlingFrames, Player player) {
+        System.out.printf("|%s|", getColumn(player.getName()));
+        for (int i = 0; i < bowlingFrames.getSize(); i++) {
+            System.out.printf("%s|", getColumn(bowlingFrames.getFrame(i).getResult()));
+        }
+        System.out.println();
+    }
+
+    private static void printHeader(int frameSize) {
+        System.out.print("| NAME |");
+        for (int i = 1; i <= frameSize; i++) {
+            System.out.printf("%s|", getColumn(String.format("%02d", i)));
+        }
+        System.out.println();
+    }
+
+    private static String getColumn(String data) {
+        int blankSize = WIDTH - data.length();
+        String leftBlank = " ".repeat(blankSize/2 + (blankSize%2));
+        String rightBlank = " ".repeat(blankSize/2);
+        return leftBlank + data + rightBlank;
+    }
+
+}
