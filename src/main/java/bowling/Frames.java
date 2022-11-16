4cffcd9fcf254e2bc98a40002892c9d074a7497e
yunhwan <pyh8618@gmail.com>
2022-11-17 02:33:57 +0900

    refactor : score를 저장하고 miss가 났을 때 점수를 계산하도록 추가

--- src/main/java/bowling/Frames.java
+++ src/main/java/bowling/Frames.java
@@ -1,13 +1,15 @@
 package bowling;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Frames {
-    private final List<Frame> frames;
+    private final LinkedList<Frame> frames;
 
     public Frames(int size) {
-        this.frames = new ArrayList<>();
+        this.frames = new LinkedList<>();
         for (int i = 0; i < size - 1; i++) {
             this.frames.add(new Frame());
         }
@@ -16,13 +18,21 @@ public class Frames {
 
     public FrameNumber pitching(FrameNumber frameNumber, int hitCount) {
         Frame frame = frames.get(frameNumber.retrieveIndexNumber());
-        frame.hitBowlingPin(hitCount);
+        BowilingTerm bowilingTerm = frame.hitBowlingPin(hitCount);
+
+        if (!bowilingTerm.equals(BowilingTerm.STRIKE)) {
+            ListIterator<Frame> frameListIterator = frames.listIterator(frameNumber.retrieveIndexNumber());
+            while (frameListIterator.hasPrevious()) {
+                Frame previous = frameListIterator.previous();
+                previous.calculateBonus(hitCount);
+            }
+        }
+
         if (frame.finishFrame()) {
             return frameNumber.next();
         }
         return frameNumber;
     }
-
 
     public List<Frame> getFrames() {
         return frames;
