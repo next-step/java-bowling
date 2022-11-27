3758d09608a0eab37906029ae05e00257e5c68ab
isakchoe <isakchoe@naver.com>
2022-11-27 20:56:37 +0900

    Test: AnswerTest
    - 질문작성자, 답변작성자 다를때 삭제 안되는지 테스트

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,24 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+
+    @Test
+    @DisplayName("질문작성자와 답변작성자가 다를 때, 답변과 질문 삭제 안되는 테스트 ")
+    public void ownerExceptionTest() throws Exception {
+
+        assertThatThrownBy(() -> {
+            A1.checkDelete(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
+
+
 }
