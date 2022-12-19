71fd55b26d83dc2e51f8b530e9db117a14457285
moonstal1506 <moonstal1506@gmail.com>
2022-12-20 00:08:57 +0900

    feat: 마지막 프레임이 끝났는지 반환

--- src/main/java/bowling/model/frame/FinalFrame.java
+++ src/main/java/bowling/model/frame/FinalFrame.java
@@ -1,5 +1,39 @@
 package bowling.model.frame;
 
+import bowling.model.Pin;
+import bowling.model.state.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FinalFrame implements Frame {
 
+    private final List<State> states = new ArrayList<>();
+    private int roundNumber = 0;
+
+    public FinalFrame() {
+        states.add(new Ready());
+    }
+
+    public void bowl(Pin pin) {
+        roundNumber++;
+        State currentState = states.get(states.size() - 1);
+        State state = currentState.bowl(pin);
+        states.remove(states.size() - 1);
+        states.add(state);
+
+        if (state.isFinished()) {
+            states.add(new Ready());
+        }
+    }
+
+    public boolean isFinished() {
+        if (roundNumber == 3) {
+            return true;
+        }
+        if (roundNumber == 2 && states.get(0) instanceof Miss) {
+            return true;
+        }
+        return false;
+    }
 }
