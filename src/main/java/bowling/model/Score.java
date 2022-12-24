ee9dcacd6363c6bd40c3e8d328fb0e69415969f5
moonstal1506 <moonstal1506@gmail.com>
2022-12-24 22:07:17 +0900

    feat: Score 점수 계산

---
+++ src/main/java/bowling/model/Score.java
@@ -0,0 +1,31 @@
+package bowling.model;
+
+import java.util.Objects;
+
+public class Score {
+
+    private final int score;
+    private final int leftCount;
+
+    public Score(int score, int leftCount) {
+        this.score = score;
+        this.leftCount = leftCount;
+    }
+
+    public Score bowl(int score) {
+        return new Score(this.score + score, leftCount - 1);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Score score1 = (Score) o;
+        return score == score1.score && leftCount == score1.leftCount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score, leftCount);
+    }
+}
