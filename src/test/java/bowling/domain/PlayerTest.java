893b2353ee5d0e094a54e96b822ef948bba8f351
gmelon <hsh1769@naver.com>
2023-01-01 17:42:07 +0900

    test: Player 정상 생성 테스트 케이스 추가

--- src/test/java/bowling/domain/PlayerTest.java
+++ src/test/java/bowling/domain/PlayerTest.java
@@ -1,11 +1,18 @@
 package bowling.domain;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PlayerTest {
+
+    @Test
+    void 생성_valid() {
+        assertThat(new Player("HSH").name()).isEqualTo("HSH");
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"abcd", "ab"})
